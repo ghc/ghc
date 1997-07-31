5bb05c2781a5b8b0803af9cbbfe1aee028eb593c
sof <unknown>
1997-07-31 01:50:46 +0000

    [project @ 1997-07-31 01:46:24 by sof] Moved to main compiler regression test directory

--- ghc/compiler/tests/ccall/cc003.hs
+++
@@ -1,9 +0,0 @@
---!!! cc003 -- ccall with unresolved polymorphism (should fail)
---!!! not anymore (as of 0.29, result type will default to ())
-module Test where
-
-import GlaExts
-
-fubar :: PrimIO Int
-fubar = _ccall_ f `seqPrimIO` _ccall_ b
-		     --^ result type of f "lost" (never gets generalised)
--- ghc/compiler/tests/stranal/moo.lhs
+++
@@ -1,6 +0,0 @@
-> module Test where
-> data Moo a b = Msimple | Mcompl (Moo b a)
-
-
-> idMoo :: Moo a -> Moo a
-> idMoo x = x
