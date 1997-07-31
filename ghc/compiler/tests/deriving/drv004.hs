1369cd5bc5f167b3b635fba26db0357a1c2a00de
sof <unknown>
1997-07-31 02:33:32 +0000

    [project @ 1997-07-31 02:33:07 by sof] Moved to main compiler regression test directory

--- ghc/compiler/tests/deriving/drv004.hs
+++
@@ -1,8 +0,0 @@
---!!! simple example of deriving Ord (and, implicitly, Eq)
---
-module ShouldSucceed where
-
-data Foo a b
-  = C1 a Int
-  | C2 b Double
-  deriving Ord
--- ghc/compiler/tests/deriving/drv007.hs
+++
@@ -1,4 +0,0 @@
---!!! buggy deriving with function type, reported by Sigbjorn Finne
-module ShouldSucceed where
-
-data Foo = Foo (Int -> Int) deriving Eq
