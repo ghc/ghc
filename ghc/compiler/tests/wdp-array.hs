0223e7466c2ee9fb735fa4f5e5b537408133700f
sof <unknown>
1997-08-14 18:33:26 +0000

    [project @ 1997-08-14 18:33:23 by sof] Moved to tests/etc

--- ghc/compiler/tests/wdp-array.hs
+++
@@ -1,4 +0,0 @@
-import MiniPrel
-
-a :: Array Int Int
-a = array (1,100) ((1 := 1) : [i := i * a!(i-1) | i <- [2..100]])
--- ghc/compiler/tests/wdp-prel-insts.hs
+++
@@ -1,8 +0,0 @@
--- what error do you get if you redefined PreludeCore instances?
-
-module Test where
-
-f x@(a,b) y@(c,d) = x == y
-
-instance Eq (a,b) where
-    (m,n) == (o,p) = m == o
