c644f64d994c18006f12323d9981473c7c8ba3df
sof <unknown>
1997-09-03 23:39:45 +0000

    [project @ 1997-09-03 23:39:45 by sof] Clean up the right ugen file

--- ghc/mk/suffix.mk
+++ ghc/mk/suffix.mk
@@ -1,6 +1,6 @@
 #################################################################################
 #
-#	 $Id: suffix.mk,v 1.1 1997/03/14 08:00:37 simonpj Exp $
+#	 $Id: suffix.mk,v 1.2 1997/09/03 23:39:45 sof Exp $
 #
 #		GHC-specific suffix rules
 #
@@ -15,7 +15,7 @@
 
 parser/%.h parser/%.c parser/U_%.hs : parser/%.ugn
 	@$(RM) $@ parser/$*.hs parser/U_$*.hs parser/$*.h
-	$(UGEN) $< || $(RM) parser/$*.h parser/$*.hs
+	$(UGEN) $< || $(RM) parser/$*.h parser/U_$*.hs
 	@$(MV) -f parser/$*.hs parser/U_$*.hs
 	@chmod 444 parser/$*.h parser/U_$*.hs
 
