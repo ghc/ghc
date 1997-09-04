83401d9289e609957fe0df23338dff706f69c54f
sof <unknown>
1997-09-04 19:51:32 +0000

    [project @ 1997-09-04 19:51:32 by sof] Turn all ugen generated files into read-only

--- ghc/mk/suffix.mk
+++ ghc/mk/suffix.mk
@@ -1,6 +1,6 @@
 #################################################################################
 #
-#	 $Id: suffix.mk,v 1.2 1997/09/03 23:39:45 sof Exp $
+#	 $Id: suffix.mk,v 1.3 1997/09/04 19:51:32 sof Exp $
 #
 #		GHC-specific suffix rules
 #
@@ -14,8 +14,8 @@
 #
 
 parser/%.h parser/%.c parser/U_%.hs : parser/%.ugn
-	@$(RM) $@ parser/$*.hs parser/U_$*.hs parser/$*.h
-	$(UGEN) $< || $(RM) parser/$*.h parser/U_$*.hs
+	@$(RM) $@ parser/$*.c parser/$*.hs parser/U_$*.hs parser/$*.h
+	$(UGEN) $< || $(RM) parser/$*.h parser/$*.c parser/$*.hs
 	@$(MV) -f parser/$*.hs parser/U_$*.hs
-	@chmod 444 parser/$*.h parser/U_$*.hs
+	@chmod 444 parser/$*.h parser/$*.c parser/U_$*.hs
 
