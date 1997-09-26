7908b385efb69e1c252414ad5a1c24c5d9708e04
simonm <unknown>
1997-09-26 10:03:57 +0000

    [project @ 1997-09-26 10:03:57 by simonm] oops, forgot to add this file

---
+++ ghc/tests/mk/should_run.mk
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+# template for should_run tests.
+
+HS_SRCS = $(wildcard *.hs)
+BINS = $(patsubst %.o,%.bin,$(HS_OBJS))
+RUNTESTS = $(patsubst %.bin,%.run,$(BINS))
+
+SRC_RUNTEST_OPTS += -o1 $*.stdout -o2 $*.stderr -x 0
+
+all :: $(RUNTESTS)
+
+%.run : %.bin
+	$(RUNTEST) $< $(RUNTEST_OPTS)
+
+%.bin : %.o
+	$(HC) $(HC_OPTS) $< -o $@
+
+CLEAN_FILES += $(BINS)
