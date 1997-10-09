e0af91a3dd6abafa313bf7eacf68e1179f41e136
simonm <unknown>
1997-10-09 16:50:35 +0000

    [project @ 1997-10-09 16:50:35 by simonm] Fix the 'set -e' business for subdirectories.  Too late for 2.08 unfortunately.

--- mk/target.mk
+++ mk/target.mk
@@ -1012,8 +1012,8 @@ all docs runtests boot TAGS clean veryclean maintainer-clean install info ::
 	@echo "===fptools== Recursively making \`$@' in $(SUBDIRS) ..."
 	@echo "PWD = $(shell pwd)"
 	@echo "------------------------------------------------------------------------"
-	@case '${MFLAGS}' in -*[ik]*) set -e;; *) set +e;; esac
-	@for i in $(SUBDIRS) ; do \
+	@case '${MFLAGS}' in -*[ik]*) set +e;; *) set -e;; esac; \
+	for i in $(SUBDIRS) ; do \
 	  echo "------------------------------------------------------------------------"; \
 	  echo "==fptools== $(MAKE) $@;"; \
 	  echo " in $(shell pwd)/$$i"; \
@@ -1026,7 +1026,7 @@ all docs runtests boot TAGS clean veryclean maintainer-clean install info ::
 	@echo "------------------------------------------------------------------------"
 
 dist ::
-	@case '${MFLAGS}' in -*[ik]*) set -e;; *) set +e;; esac
+	@case '${MFLAGS}' in -*[ik]*) set +e;; *) set -e;; esac; \
 	for i in $(SUBDIRS) ; do \
 	  $(MKDIRHIER_PREFIX)mkdirhier $(SRC_DIST_DIR)/$$i; \
 	  $(MAKE) -C $$i $(MFLAGS) $@ SRC_DIST_DIR=$(SRC_DIST_DIR)/$$i; \
@@ -1054,8 +1054,8 @@ all docs runtests TAGS clean veryclean maintainer-clean install ::
 	@echo "===fptools== Recursively making \`$@' for ways: $(WAYS) ..."
 	@echo "PWD = $(shell pwd)"
 	@echo "------------------------------------------------------------------------"
-	@case '${MFLAGS}' in -*[ik]*) set -e;; *) set +e;; esac
-	@for i in $(WAYS) ; do \
+	@case '${MFLAGS}' in -*[ik]*) set +e;; *) set -e;; esac; \
+	for i in $(WAYS) ; do \
 	  echo "------------------------------------------------------------------------"; \
 	  echo "==fptools== $(MAKE) way=$$i $@;"; \
 	  echo "PWD = $(shell pwd)"; \
