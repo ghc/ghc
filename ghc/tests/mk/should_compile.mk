ad4dc1e5e36985465601ac589eb69603aa91d019
simonm <unknown>
1997-12-17 13:10:31 +0000

    [project @ 1997-12-17 13:10:30 by simonm] use '----' instead of '***' so as not to get confuesd with error messages

--- ghc/tests/mk/should_compile.mk
+++ ghc/tests/mk/should_compile.mk
@@ -6,7 +6,7 @@ HS_SRCS = $(wildcard *.hs)
 SRC_RUNTEST_OPTS += -o1 $*.stdout -o2 $*.stderr -x 0
 
 %.o : %.hs
-	@echo \*\*\* Testing for successful compilation of $<
+	@echo ---- Testing for successful compilation of $<
 	@$(RUNTEST) $(HC) $(RUNTEST_OPTS) -- $(HC_OPTS) -c $< -o $@
 
 all :: $(HS_OBJS)
--- ghc/tests/mk/should_fail.mk
+++ ghc/tests/mk/should_fail.mk
@@ -6,7 +6,7 @@ HS_SRCS = $(wildcard *.hs)
 SRC_RUNTEST_OPTS += -o1 $*.stdout -o2 $*.stderr -x 1
 
 %.o : %.hs
-	@echo \*\*\* Testing for failure to compile $<
+	@echo ---- Testing for failure to compile $<
 	@$(RUNTEST) $(HC) $(RUNTEST_OPTS) -- $(HC_OPTS) -c $< -o $@
 
 all :: $(HS_OBJS)
