fd6ca99bd3a00f6fdfd5d2663c2e5dab8b605b09
simonm <unknown>
1998-04-14 09:29:26 +0000

    [project @ 1998-04-14 09:29:26 by simonm] add comment about why NON_POSIX_SOURCE is required.

--- ghc/tests/codeGen/should_run/cg035.hs
+++ ghc/tests/codeGen/should_run/cg035.hs
@@ -1,5 +1,9 @@
 {-# OPTIONS -optc-DNON_POSIX_SOURCE #-}
 
+-- The above option is required because 'erf' isn't a POSIX function, and we
+-- need a prototype for it in order to compile the following code correctly.
+-- Defining NON_POSIX_SOURCE tells the RTS not to define _POSIX_SOURCE.
+
 module Main (main) where
 
 import IOExts ( unsafePerformIO )
