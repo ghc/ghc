dd953727029ed80d226c390bd168a3bcdf98ea0b
simonm <unknown>
1998-08-07 16:05:27 +0000

    [project @ 1998-08-07 16:05:27 by simonm] oops, put the OPTIONS at the top of the file.

--- ghc/tests/typecheck/should_compile/tc085.hs
+++ ghc/tests/typecheck/should_compile/tc085.hs
@@ -1,6 +1,6 @@
+{-# OPTIONS -fglasgow-exts #-}
 --!!! From a bug report from Satnam.
 --!!! To do with re-exporting importees from PreludeGla* modules.
-{-# OPTIONS -fglasgow-exts #-}
 module ShouldSucceed ( module IOExts, module PrelGHC ) where
 
 --OLD:   import GlaExts
