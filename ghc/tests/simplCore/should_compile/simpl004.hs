bd387bc1e9bb8ceb5fcb4619b85d38d7fdb2a9b8
simonm <unknown>
1998-08-07 15:22:03 +0000

    [project @ 1998-08-07 15:22:02 by simonm] add regression test for bug in mkDupableAlt.

---
+++ ghc/tests/simplCore/should_compile/simpl004.hs
@@ -0,0 +1,18 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module ShouldCompile where
+
+import Ix
+import GlaExts
+
+
+f ixs@(_, ix_end) frozen# =
+ let
+  n# = 
+   case (
+	 if null (range ixs)
+	  then 0
+	  else 1
+        ) of { I# x -> x }
+ in
+ (# frozen#, False #)
