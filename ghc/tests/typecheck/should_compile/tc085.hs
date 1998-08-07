5d3d42838c7846d24bf6dc8158fe1d830ab465af
simonm <unknown>
1998-08-07 15:39:45 +0000

    [project @ 1998-08-07 15:39:45 by simonm] need -fglasgow-exts to get IOExts.

--- ghc/tests/typecheck/should_compile/tc085.hs
+++ ghc/tests/typecheck/should_compile/tc085.hs
@@ -1,5 +1,6 @@
 --!!! From a bug report from Satnam.
 --!!! To do with re-exporting importees from PreludeGla* modules.
+{-# OPTIONS -fglasgow-exts #-}
 module ShouldSucceed ( module IOExts, module PrelGHC ) where
 
 --OLD:   import GlaExts
