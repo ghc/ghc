f2b73283356e3aefbfc984d0d2962f9f5f8c142c
sof <unknown>
1998-08-14 13:17:48 +0000

    [project @ 1998-08-14 13:17:48 by sof] Unused (the real one lives in fptools/)

--- mk/acconfig.h
+++
@@ -1,9 +0,0 @@
-/* Leave this blank line here -- autoheader needs it! */
-
-
-/* Define this if your time.h defines altzone */
-#undef HAVE_ALTZONE
-
-/* Define as the type of timezone.  */
-#undef TYPE_TIMEZONE
-
