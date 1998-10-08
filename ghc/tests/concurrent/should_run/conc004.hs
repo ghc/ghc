f78ec0c4bd506f3ec61582b09f38c2a1111dcb89
simonm <unknown>
1998-10-08 11:37:16 +0000

    [project @ 1998-10-08 11:37:15 by simonm] fix conc004 for new type of forkIO. add conc007: killThread test.

--- ghc/tests/concurrent/should_run/conc004.hs
+++ ghc/tests/concurrent/should_run/conc004.hs
@@ -10,7 +10,7 @@ main = do
    mvar <- newEmptyMVar
 
    let 
-   	spawner :: (IO () -> IO ()) -> Int -> IO ()
+   	spawner :: (IO () -> IO ThreadId) -> Int -> IO ()
    	spawner c 0 = putMVar mvar ()
    	spawner c n = do { c (spawner c (n-1)); return ()}
 
---
+++ ghc/tests/concurrent/should_run/conc007.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module Main where
+
+import Concurrent
+import IOExts
+
+choose :: a -> a -> IO a
+choose a b = do
+   ready <- newMVar ()
+   answer <- newEmptyMVar
+   a_id <- forkIO (a `seq` takeMVar ready >> putMVar answer a)
+   b_id <- forkIO (b `seq` takeMVar ready >> putMVar answer b)
+   it <- takeMVar answer
+   killThread a_id
+   killThread b_id
+   return it
+
+main = do
+   let big = sum [1..]
+       small = sum [1..42]
+   test1 <- choose big small
+   test2 <- choose small big
+   print (test1,test2)
