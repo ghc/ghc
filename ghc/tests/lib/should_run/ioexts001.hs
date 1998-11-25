3a0ea74ec5cbad533a666e333dcc426991c2b46c
simonm <unknown>
1998-11-25 16:44:05 +0000

    [project @ 1998-11-25 16:44:05 by simonm] add simple IORef test.

---
+++ ghc/tests/lib/should_run/ioexts001.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module Main where
+
+import IOExts
+
+loop r 0 = return ()
+loop r c = loop r (c-1) >> writeIORef r 42
+
+main = newIORef 0 >>= \r -> loop r 1000000
+
+
