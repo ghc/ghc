9308dacd4e5faef80a9ee98c86aa6725a88d2ea5
simonm <unknown>
1999-05-10 10:12:28 +0000

    [project @ 1999-05-10 10:12:25 by simonm] Test fixes.

--- ghc/tests/numeric/should_run/num002.hs
+++ ghc/tests/numeric/should_run/num002.hs
@@ -9,7 +9,7 @@ main =
    putStrLn (showInt (343023920121::Integer) [])
    putStrLn (showInt (3430239::Int) [])
    putStrLn (showInt (1212 :: Int) [])
-   putStrLn (showInt (591125662431 `div` (517::Int)) [])
+   putStrLn (showInt (5911256624 `div` (517::Int)) [])
    -- showInt just works over naturals, wrap it up inside
    -- a use of Numeric.showSigned to show negative nums.
    putStrLn (showSigned (showInt) 0 (-111::Int) [])
