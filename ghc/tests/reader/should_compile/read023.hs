9956bafe44c39e07dff0c569dedc5cef3b41dbc7
simonmar <unknown>
1999-06-28 15:43:12 +0000

    [project @ 1999-06-28 15:43:12 by simonmar] a couple more weird cases for lexing of qualified names.

--- ghc/tests/reader/should_compile/read023.hs
+++ ghc/tests/reader/should_compile/read023.hs
@@ -1,9 +1,12 @@
 module ShouldCompile where
 
 -- M.<keyword> isn't a qualified identifier
-f = Just.let x=id in x
+f  = Just.let x=id in x
+f' = Just.\1 where (.\) = ($)
 
 -- M.{as,hiding,qualified} *are* qualified identifiers
-g = ShouldCompile.as
+g  = ShouldCompile.as
+g' = (ShouldCompile.!)
 
 as x = x
+(!) x = x
