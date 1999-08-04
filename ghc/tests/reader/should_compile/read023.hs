d5ed82a30ee2ef9a5c20eb205de0850ad88665f4
simonmar <unknown>
1999-08-04 15:43:34 +0000

    [project @ 1999-08-04 15:43:34 by simonmar] GHC deviates from the Haskell report on a couple of the finer points of lexical syntax, so comment out a couple of the tests here.

--- ghc/tests/reader/should_compile/read023.hs
+++ ghc/tests/reader/should_compile/read023.hs
@@ -2,10 +2,22 @@ module ShouldCompile where
 
 -- M.<keyword> isn't a qualified identifier
 f  = Just.let x=id in x
-f' = Just.\1 where (.\) = ($)
 
--- M.{as,hiding,qualified} *are* qualified identifiers
-g  = ShouldCompile.as
+-- ---------------------------------------------------------------------------
+-- we changed the behaviour of this one in GHC, but the following test
+-- is strictly speaking legal Haskell:
+
+-- f' = Just.\1 where (.\) = ($)
+
+-- -----------------------------------------------------
+-- M.{as,hiding,qualified} *are* qualified identifiers,
+-- but GHC deviates from the Haskell report here.
+
+-- g  = ShouldCompile.as
+
+-- ---------------------------------------------------------------------------
+-- special symbols (!, -) can be qualified to make varids.
+
 g' = (ShouldCompile.!)
 
 as x = x
