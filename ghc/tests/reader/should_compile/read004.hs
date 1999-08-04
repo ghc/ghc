aa75ede16e31baaf0853478c324a6ff6264ff78d
simonmar <unknown>
1999-08-04 15:47:34 +0000

    [project @ 1999-08-04 15:47:33 by simonmar] Fix up expr001.hs and move it into should_compile

--- ghc/tests/reader/should_compile/read004.hs
+++ ghc/tests/reader/should_compile/read004.hs
@@ -8,10 +8,9 @@ Date: Wed, 9 Oct 91 17:31:46 BST
 
 OK, I've fixed that little problem by disallowing,
 -}
-module Test where
 
 f x = x + if True then 1 else 2
-f x = x + 1::Int
+g x = x + 1::Int
 
 -- (the conditional/sig need to be parenthesised).  If this is
 -- problematic, let me know!
