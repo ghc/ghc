179e1b1c951f9a99367b23c2ae0daae94b5a3421
simonmar <unknown>
1999-11-03 15:01:51 +0000

    [project @ 1999-11-03 15:01:51 by simonmar] remove bogus #include

--- ghc/rts/StgRun.h
+++ ghc/rts/StgRun.h
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------------------
- * $Id: StgRun.h,v 1.4 1999/11/02 15:06:04 simonmar Exp $
+ * $Id: StgRun.h,v 1.5 1999/11/03 15:01:51 simonmar Exp $
  *
  * (c) The GHC Team, 1998-1999
  *
@@ -10,9 +10,8 @@
 #ifndef STGRUN_H
 #define STGRUN_H
 
-#include "Storage.h"  /* for {Open,Close}Nursery functions */
-
 extern StgThreadReturnCode StgRun(StgFunPtr f, StgRegTable *basereg);
+
 EXTFUN(StgReturn);
 
 #endif STGRUN_H
