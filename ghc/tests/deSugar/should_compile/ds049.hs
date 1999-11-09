532fc33149d79e65f27459e359b4d861e599761f
simonmar <unknown>
1999-11-09 11:38:28 +0000

    [project @ 1999-11-09 11:38:28 by simonmar] Test for lit-lits in patterns.

---
+++ ghc/tests/deSugar/should_compile/ds049.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module ShouldCompile where
+
+-- !!! test lit-lits in patterns
+
+import Addr
+
+litlit_int    (``1''      :: Int)    = 42
+litlit_word   (``1''      :: Word)   = 42
+litlit_char   (`` '\n' '' :: Char)   = 42
+litlit_addr   (``NULL''   :: Addr)   = 42
+litlit_float  (``1.0''    :: Float)  = 42
+litlit_double (``1.0''    :: Double) = 42
