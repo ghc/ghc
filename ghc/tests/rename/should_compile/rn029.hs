9b828bafef4209f72a43c0a289be85c9fd8003e8
simonpj <unknown>
1999-12-29 12:26:51 +0000

    [project @ 1999-12-29 12:26:51 by simonpj] Improve rn029, remove dup rn033

--- ghc/tests/rename/should_compile/rn029.hs
+++ ghc/tests/rename/should_compile/rn029.hs
@@ -1,8 +1,17 @@
--- !!! Checking that lazy name clashing work.
+-- !!! Checking that lazy name clashing works.
 module ShouldSucceed where
 
-import List ( sort )
+import List ( reverse, sort )
 
-ShouldSucceed.sort :: Int
-ShouldSucceed.sort = 3
+sort :: Int	-- Clashes with List.sort, 
+sort = 4	-- but never used, so OK	
+	
+
+reverse :: Int	-- Clashes with List.reverse, 
+reverse = 3	-- but the only uses are qualified
+
+x = ShouldSucceed.reverse
+
+y = List.reverse
+
 
