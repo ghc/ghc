18a3f7aed8379e2694ff0538899cc64b353519e4
simonmar <unknown>
2000-01-07 17:22:09 +0000

    [project @ 2000-01-07 17:22:09 by simonmar] - set directories using ':=' because the value of $(TOP) can change - remove some old cruft

--- ghc/mk/paths.mk
+++ ghc/mk/paths.mk
@@ -1,16 +1,8 @@
 # -----------------------------------------------------------------------------
+# $Id: paths.mk,v 1.15 2000/01/07 17:22:09 simonmar Exp $
 #
 # ghc project specific make variables
 #
-
-GCap=-optc-DGCap
-#GC2s=-optc-DGC2s
-#GC1s=-optc-DGC1s
-
-MKDEPENDHSSRC 		= $(GHC_UTILS_DIR)/mkdependHS
-UNLIT	 		= $(GHC_UNLIT_DIR)/unlit
-GHC_UNLIT		= $(GHC_UNLIT_DIR)/unlit
-GHC_UNLIT_DIR 		= $(GHC_UTILS_DIR)/unlit
 
 #-----------------------------------------------------------------------------
 # HsTags
@@ -23,37 +15,31 @@ HSTAGS_DIR 		= $(GHC_UTILS_DIR)/hstags
 endif
 
 #-----------------------------------------------------------------------------
-# Ugen
-
-ifdef UseInstalledUtils
-UGEN		= ugen
-else
-UGEN		= $(UGEN_DIR)/ugen
-UGEN_DIR 	= $(GHC_UTILS_DIR)/ugen
-endif
-
-#-----------------------------------------------------------------------------
 # Extra things ``only for'' for the ghc project
 
-GHC_DRIVER_DIR	    	= $(TOP)/driver
-GHC_COMPILER_DIR  	= $(TOP)/compiler
-GHC_RUNTIME_DIR   	= $(TOP)/rts
-GHC_LIB_DIR	 	= $(TOP)/lib
-GHC_INCLUDE_DIR   	= $(TOP)/includes
-GHC_UTILS_DIR	 	= $(TOP)/utils
-GHC_INTERPRETER_DIR 	= $(TOP)/interpreter
+GHC_DRIVER_DIR	    	:= $(TOP)/driver
+GHC_COMPILER_DIR  	:= $(TOP)/compiler
+GHC_RUNTIME_DIR   	:= $(TOP)/rts
+GHC_LIB_DIR	 	:= $(TOP)/lib
+GHC_INCLUDE_DIR   	:= $(TOP)/includes
+GHC_UTILS_DIR	 	:= $(TOP)/utils
+GHC_INTERPRETER_DIR 	:= $(TOP)/interpreter
 
-GHC_INPLACE		= $(GHC_DRIVER_DIR)/ghc-inplace
-GHC_HSCPP_DIR 		= $(GHC_UTILS_DIR)/hscpp
-GHC_HSCPP    		= $(GHC_HSCPP_DIR)/hscpp
-GHC_MKDEPENDHS_DIR	= $(GHC_UTILS_DIR)/mkdependHS
-GHC_MKDEPENDHS 		= $(GHC_MKDEPENDHS_DIR)/mkdependHS-inplace
-GHC_HSP    		= $(GHC_HSP_DIR)/hsp
-GHC_HSP_DIR 		= $(GHC_HSC_DIR)
-GHC_HSC    		= $(GHC_HSC_DIR)/hsc
-GHC_HSC_DIR 		= $(GHC_COMPILER_DIR)
-GHC_SYSMAN    		= $(GHC_RUNTIME_DIR)/gum/SysMan
-GHC_SYSMAN_DIR 		= $(GHC_RUNTIME_DIR)/gum
+GHC_INPLACE		:= $(GHC_DRIVER_DIR)/ghc-inplace
+GHC_HSCPP_DIR 		:= $(GHC_UTILS_DIR)/hscpp
+GHC_HSCPP    		:= $(GHC_HSCPP_DIR)/hscpp
+GHC_MKDEPENDHS_DIR	:= $(GHC_UTILS_DIR)/mkdependHS
+GHC_MKDEPENDHS 		:= $(GHC_MKDEPENDHS_DIR)/mkdependHS-inplace
+GHC_HSP    		:= $(GHC_HSP_DIR)/hsp
+GHC_HSP_DIR 		:= $(GHC_HSC_DIR)
+GHC_HSC    		:= $(GHC_HSC_DIR)/hsc
+GHC_HSC_DIR 		:= $(GHC_COMPILER_DIR)
+GHC_SYSMAN    		:= $(GHC_RUNTIME_DIR)/gum/SysMan
+GHC_SYSMAN_DIR 		:= $(GHC_RUNTIME_DIR)/gum
+
+UNLIT	 		:= $(GHC_UNLIT_DIR)/unlit
+GHC_UNLIT		:= $(GHC_UNLIT_DIR)/unlit
+GHC_UNLIT_DIR 		:= $(GHC_UTILS_DIR)/unlit
 
 #-----------------------------------------------------------------------------
 # Stuff for the C-compiling phase in particular...
