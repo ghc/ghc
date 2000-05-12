b36d01c87fa805dfcf0a9b8cfc3038418139aeed
sewardj <unknown>
2000-05-12 13:49:54 +0000

    [project @ 2000-05-12 13:49:54 by sewardj] 3rd time lucky with DIET_HEP and Cygwin.  I hope.

--- ghc/includes/DietHEP.h
+++ ghc/includes/DietHEP.h
@@ -1,11 +1,11 @@
 
-typedef enum { dh_stdcall, dh_ccall } DHCALLCONV;
-typedef int                           HMODULE;
-typedef char*                         LPCSTR;
+typedef enum { dh_stdcall, dh_ccall } DH_CALLCONV;
+typedef int                           DH_MODULE;
+typedef char*                         DH_LPCSTR;
 
-extern HMODULE LoadLibrary ( LPCSTR modname );
-extern void*   GetProcAddr ( DHCALLCONV cconv, 
-                             HMODULE    hModule, 
-                             LPCSTR     lpProcName );
+extern DH_MODULE DH_LoadLibrary    ( DH_LPCSTR modname );
+extern void*     DH_GetProcAddress ( DH_CALLCONV  cconv, 
+                                     DH_MODULE    hModule, 
+                                     DH_LPCSTR    lpProcName );
 
 
