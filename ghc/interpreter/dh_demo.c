6212ebc63f8e0a92744e4970f548034f51614593
sewardj <unknown>
2000-05-12 15:59:37 +0000

    [project @ 2000-05-12 15:59:37 by sewardj] Cygwin support machinery for manufacturing DietHEP.dll and building a small demo program (dh_demo.exe).

---
+++ ghc/interpreter/dh_demo.c
@@ -0,0 +1,24 @@
+
+
+#include <stdio.h>
+#include <assert.h>
+#include <windows.h>
+#include "../includes/DietHEP.h"
+
+int main ( int argc, char** argv )
+{
+   {
+   DH_MODULE hModule;
+   void(*proc)(int);
+
+   hModule = DH_LoadLibrary("FooBar");   /* note no .hs */
+   assert(hModule != 0);
+   proc = DH_GetProcAddress ( dh_ccall, hModule, "wurble" );
+   assert(proc);
+
+   proc(44);
+   proc(45);
+   proc(46);
+   }
+   return 0;
+}
