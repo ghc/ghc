3a68f09199fb656512347c57f4a7a4f1215a36bd
simonpj <unknown>
2000-05-23 11:51:13 +0000

    [project @ 2000-05-23 11:51:13 by simonpj] Add tests for result type sigs

---
+++ ghc/tests/typecheck/should_compile/tc105.hs
@@ -0,0 +1,13 @@
+-- !!! Scoped type variables in result signatures
+module ShouldCompile where
+
+import PrelST
+import PrelArr
+
+f:: ST s Int
+f:: ST s Int = do
+    v <- newSTRef 5
+    let g :: ST s Int
+             -- ^ should be in scope
+        g = readSTRef v
+    g
