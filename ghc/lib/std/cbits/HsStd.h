9b6f49a25a636a2ab7e16d12c119e087c3caba77
panne <unknown>
2000-05-31 12:04:49 +0000

    [project @ 2000-05-31 12:04:49 by panne] Use new way of #including for packages

---
+++ ghc/lib/std/cbits/HsStd.h
@@ -0,0 +1,13 @@
+/* -----------------------------------------------------------------------------
+ * $Id: HsStd.h,v 1.1 2000/05/31 12:04:49 panne Exp $
+ *
+ * Definitions for package `std' which are visible in Haskell land.
+ *
+ * ---------------------------------------------------------------------------*/
+
+#ifndef HSSTD_H
+#define HSSTD_H
+
+#include "stgio.h"
+
+#endif
