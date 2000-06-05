b661cfa6aad800a94dcc25d9d88981b4a387af6e
panne <unknown>
2000-06-05 20:54:29 +0000

    [project @ 2000-06-05 20:54:29 by panne] Added tests for gcdInt/gcdInteger

---
+++ ghc/tests/numeric/should_run/arith013.hs
@@ -0,0 +1,13 @@
+-- Test gcdInt/gcdInteger
+
+import PrelBase	( gcdInt )
+import PrelNum	( gcdInteger )
+
+main :: IO ()
+main = do
+   test gcdInt     [                       -42, 0, 105             ]
+   test gcdInteger [-12193263111263526900, -42, 0, 105, 1234567890 ]
+
+
+test :: Integral a => (a -> a -> a) -> [a] -> IO ()
+test f xs = mapM_ print [ (a, b, f a b) | a <- xs, b <- reverse xs, a /= 0  || b /= 0 ]
