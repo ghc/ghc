cc6dcd92f1f55e5f51e0e82535b3653c211caed9
simonpj <unknown>
2000-10-02 16:06:48 +0000

    [project @ 2000-10-02 16:06:48 by simonpj] Add check for unboxed function arguments

---
+++ ghc/tests/typecheck/should_fail/tcfail087.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- !!! Check that unboxed tuples can't be function arguments
+module ShouldFail where
+
+data Ex = Ex (# Int,Int #)
+
+f :: (# Int,Int #) -> Int
+f x = error "urk"
+
+g (# x,y #) = x
+
+
