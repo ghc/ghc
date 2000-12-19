53389e36dc0471b5058cf310e9709e13f41fdbc8
simonpj <unknown>
2000-12-19 10:50:51 +0000

    [project @ 2000-12-19 10:50:51 by simonpj] Add two tests

---
+++ ghc/tests/typecheck/should_fail/tcfail089.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- !!! Check non-constructors in patterns fail tidily
+-- !!! The -O made ghc 4.08 go into a loop!
+-- Unfortunately the -O has to go in the Makefile
+
+module ShouldFail where
+
+compute :: String -> String
+compute ("hd" ++ _) = "_"
---
+++ ghc/tests/typecheck/should_run/tcrun008.hs
@@ -0,0 +1,28 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- !!! Check that record selectors for polymorphic fields work right
+
+module Main where
+
+import IO
+
+class Foo a where
+  bar :: a -> [a]
+
+instance Foo Int where
+  bar x = replicate x x
+
+instance Foo Bool where
+  bar x = [x, not x]
+
+data Record = R {
+     blub :: Foo a => a -> [a]
+    }
+
+main = do { let r = R {blub = bar}
+	  ; print (blub r (3::Int)) 
+	  ; print (blub r True)
+	  }
+
+
+
