4f2f158a207255e419eb2f74be3f4e760a605ade
simonmar <unknown>
2001-01-14 15:46:53 +0000

    [project @ 2001-01-14 15:46:53 by simonmar] Add a slightly stronger test for Time that the ones in tests/io.  This one reads the current time, converts it to CalendarTime and back again, and tests that all three times converted to strings compare equal.

---
+++ ghc/tests/lib/should_run/time002.hs
@@ -0,0 +1,16 @@
+import Time
+
+-- !!! check that we can read the current ClockTime, convert it
+-- !!! to CalendarTime and back again, and that all three times when
+-- !!! converted to strings compare equal.
+
+main = do
+  t <- getClockTime
+  let clock = show t
+  c <- toCalendarTime t
+  let cal = calendarTimeToString c
+  let t2 = toClockTime c
+      clock2 = show t2
+  if (clock == cal && clock == clock2)
+	then putStrLn "Ok."
+	else putStrLn "Failed."
