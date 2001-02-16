8487ac4cb58905610cfe8c1ab949dc43b67e134e
sewardj <unknown>
2001-02-16 12:07:10 +0000

    [project @ 2001-02-16 12:07:10 by sewardj] Remove ghc-$(ProjectVersion) from GhcBinDistPrlScripts, since it's not a perl script any more.

--- ghc/mk/config.mk
+++ ghc/mk/config.mk
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# $Id: config.mk,v 1.11 2001/02/16 11:39:37 sewardj Exp $
+# $Id: config.mk,v 1.12 2001/02/16 12:07:10 sewardj Exp $
 #
 # GHC project configuration
 #
@@ -11,7 +11,7 @@
 GhcMainDir = ghc
 GhcBinDistDirs = ghc hslibs
 GhcBinDistDocs = ghc/docs/set
-GhcBinDistPrlScripts = ghc-$(ProjectVersion)
+GhcBinDistPrlScripts = 
 GhcBinDistLibPrlScripts = ghc-asm ghc-split
 GhcBinDistBins = hp2ps
 
