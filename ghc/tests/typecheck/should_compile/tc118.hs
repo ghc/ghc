72aac89b9ca98591b7f936e80021749b5d8dff76
simonpj <unknown>
2001-02-22 13:16:48 +0000

    [project @ 2001-02-22 13:16:48 by simonpj] Add more tests

---
+++ ghc/tests/typecheck/should_compile/tc118.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- !!! An instance decl with a context containing a free type variable
+-- The interest here is that there's a "b" in the instance decl
+-- context that isn't mentioned in the instance head.  
+
+module ShouldCompile where
+
+class HasConverter a b | a -> b where
+   convert :: a -> b
+ 
+data Foo a = MkFoo a
+
+instance (HasConverter a b,Show b) => Show (Foo a) where
+   show (MkFoo value) = show (convert value)
+
