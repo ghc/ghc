062a5e06556ea4fd7b1d2c107ef07dd9b849e4c4
panne <unknown>
2001-03-31 17:05:58 +0000

    [project @ 2001-03-31 17:05:58 by panne] Disabled part of the test dealing with LARGE tuples, which don't work with GHC (traditionally). I know this is not optimal, but I fear that the situation won't change soon.

--- ghc/tests/deSugar/should_compile/ds018.hs
+++ ghc/tests/deSugar/should_compile/ds018.hs
@@ -1,4 +1,4 @@
--- !!! ds018 -- explicit lists and tuples
+-- !!! ds018 -- explicit lists and tuples (with disabled LARGE tuples!)
 --
 module ShouldCompile where
 
@@ -8,6 +8,10 @@ f x y z = [x,y,z,x,y,z]
 f2 x y	= []
 
 g1 x y  = ()
+
+{- Although GHC *should* provide arbitrary tuples, it currently doesn't
+   and probably won't in the near future, so this test is only a reminder.
+
 g x y z = (x,y,z,x,y,z,
 	   x,y,z,x,y,z,
 	   x,y,z,x,y,z,
@@ -33,6 +37,7 @@ g x y z = (x,y,z,x,y,z,
 	   x,y,z,x,y,z,
 	   x,y,z,x,y,z,
 	   x,y,z,x,y,z) -- hey, we love big tuples
+-}
 
 -- pats
 
@@ -40,9 +45,11 @@ fa [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z] = x
 
 fb [] = []
 
+{- See above
 ga (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,
     aa,ab,ac,ad,ae,af,ag,ah,ai,aj,ak,al,am,
     an,ao,ap,aq,ar,as,at,au,av,aw,ax,ay,az) = x
+-}
 
 gb () x = x
 gb2 ()  = ()
