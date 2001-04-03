e7a201bcbf6e431924559aadb1e1b7e962a0f006
simonmar <unknown>
2001-04-03 15:51:54 +0000

    [project @ 2001-04-03 15:51:54 by simonmar] add missing file

---
+++ ghc/tests/mk/ghci.mk
@@ -0,0 +1,17 @@
+#-----------------------------------------------------------------------------
+# template for should_run tests.
+
+SCRIPTS = $(wildcard *.script)
+
+SRC_RUNTEST_OPTS += -x 0 \
+	$(foreach i,$(wildcard $*.stdout),-o1 $(i)) \
+	$(foreach i,$(wildcard $*.stderr),-o2 $(i))
+
+RUNTESTS = $(filter-out $(OMITTED_RUNTESTS), $(patsubst %.script,%.run,$(SCRIPTS)))
+
+all :: $(RUNTESTS)
+
+%.run : %.script
+	HC=$(HC) HC_OPTS="$(HC_OPTS)" $(RUNTEST) $(HC) -i$< $(RUNTEST_OPTS) -- --interactive
+
+include $(TOP)/mk/target.mk
