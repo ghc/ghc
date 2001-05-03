0e5a48cc13843ff5d31963c711900652cc896649
simonpj <unknown>
2001-05-03 08:18:35 +0000

    [project @ 2001-05-03 08:18:35 by simonpj] Add a couple of tests

---
+++ ghc/tests/typecheck/should_run/tcrun011.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- !!! Existential data tyes
+-- Hugs didn't like this one
+
+module Main (main) where
+
+
+class MyClass a b where
+	foo :: a -> b -> Int
+
+data Special = forall b. (MyClass Int b)=> MkSpecial b
+data General a = forall b. (MyClass a b)=> MkGeneral b
+
+instance MyClass Int Bool where
+   foo x False = -x
+   foo x True  = x
+
+xs :: [General Int]
+xs = [MkGeneral True, MkGeneral False]
+
+main = print [foo (3::Int) x | MkGeneral x <- xs]
+	-- Without the (::Int) part we get an 
+	-- incomprehensible error message :-(
