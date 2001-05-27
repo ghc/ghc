b8c0cb8af43302d06eb0de66fa3958c0c17d7278
sof <unknown>
2001-05-27 23:55:07 +0000

    [project @ 2001-05-27 23:55:07 by sof] Added GHC_TOUCHY_DIR and GHC_TOUCHY

--- ghc/mk/paths.mk
+++ ghc/mk/paths.mk
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# $Id: paths.mk,v 1.30 2001/03/23 16:36:22 simonmar Exp $
+# $Id: paths.mk,v 1.31 2001/05/27 23:55:07 sof Exp $
 #
 # ghc project specific make variables
 #
@@ -26,10 +26,12 @@ GHC_UTILS_DIR	 	:= $(TOP)/utils
 GHC_INTERPRETER_DIR 	:= $(TOP)/interpreter
 
 GHC_UNLIT_DIR 		:= $(GHC_UTILS_DIR)/unlit
+GHC_TOUCHY_DIR 		:= $(GHC_UTILS_DIR)/touchy
 GHC_MANGLER_DIR 	:= $(GHC_DRIVER_DIR)/mangler
 GHC_SPLIT_DIR	 	:= $(GHC_DRIVER_DIR)/split
 
 GHC_UNLIT		= $(GHC_UNLIT_DIR)/unlit$(EXE_SUFFIX)
+GHC_TOUCHY              = $(GHC_TOUCHY_DIR)/touchy$(EXE_SUFFIX)
 GHC_MANGLER		= $(GHC_MANGLER_DIR)/ghc-asm
 GHC_SPLIT		= $(GHC_SPLIT_DIR)/ghc-split
 
