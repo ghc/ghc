4871afef95d1a9bf9d83889a08aaaa10227b57d2
simonmar <unknown>
2001-06-28 09:57:33 +0000

    [project @ 2001-06-28 09:57:32 by simonmar] include version.mk before boilerplate.mk, so that boilerplate.mk has the project name and version available.
    Override libdir and datadir in fptools/mk/config.mk to add the project
    name and version.
    
    Suggested by: Manuel

--- ghc/mk/boilerplate.mk
+++ ghc/mk/boilerplate.mk
@@ -10,6 +10,8 @@
 # Remember, TOP is the top level of the innermost level
 # (FPTOOLS_TOP is the fptools top)
 
+-include $(TOP)/mk/version.mk
+
 # We need to set TOP to be the TOP that the next level up expects!
 # The TOP variable is reset after the inclusion of the fptools
 # boilerplate, so we stash TOP away first:
@@ -28,7 +30,6 @@ TOP:=$(GHC_TOP)
 
 -include $(TOP)/mk/paths.mk
 -include $(TOP)/mk/opts.mk
--include $(TOP)/mk/version.mk
 -include $(TOP)/mk/suffix.mk
 
 # -----------------------------------------------------------------
