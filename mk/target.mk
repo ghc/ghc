c1ea16ab246700cd031e3c14316ab8ea4524fb75
qrczak <unknown>
2001-07-16 21:01:58 +0000

    [project @ 2001-07-16 21:01:58 by qrczak] Follow the s/includes/include/ change in package.conf.

--- mk/target.mk
+++ mk/target.mk
@@ -374,7 +374,7 @@ endif
 SRC_CC_OPTS += -I$(GHC_INCLUDE_DIR) -I$(GHC_RUNTIME_DIR)
 
 ifeq "$(IS_CBITS_LIB)" "YES"
-override datadir:=$(libdir)/includes
+override datadir:=$(libdir)/include
 INSTALL_DATAS += Hs$(shell perl -e 'print ucfirst "$(PACKAGE)"').h
 else
 SRC_CC_OPTS += -Icbits
