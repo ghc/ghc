f94afcba74c9163f6ad780424e2db2af74b24c80
simonmar <unknown>
2001-07-23 10:43:18 +0000

    [project @ 2001-07-23 10:43:18 by simonmar] merge in changes from rts/Linker.h

--- ghc/includes/Linker.h
+++ ghc/includes/Linker.h
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------------------
- * $Id: Linker.h,v 1.2 2000/12/04 12:31:20 simonmar Exp $
+ * $Id: Linker.h,v 1.3 2001/07/23 10:43:18 simonmar Exp $
  *
  * (c) The GHC Team, 2000
  *
@@ -24,5 +24,8 @@ HsInt loadObj( char *path );
 
 /* resolve all the currently unlinked objects in memory */
 HsInt resolveObjs( void );
+
+/* load a dynamic library */
+char *addDLL( char* path, char* dll_name );
 
 #endif /* LINKER_H */
