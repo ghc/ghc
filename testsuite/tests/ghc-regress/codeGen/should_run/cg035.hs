a4ca1e3e3ff83338546667b0e422a0386702a871
simonmar <unknown>
2001-08-16 13:32:32 +0000

    [project @ 2001-08-16 13:32:32 by simonmar] Bring these tests up to date.

--- testsuite/tests/ghc-regress/codeGen/should_run/cg035.hs
+++ testsuite/tests/ghc-regress/codeGen/should_run/cg035.hs
@@ -1,9 +1,3 @@
-{-# OPTIONS -optc-DNON_POSIX_SOURCE #-}
-
--- The above option is required because 'erf' isn't a POSIX function, and we
--- need a prototype for it in order to compile the following code correctly.
--- Defining NON_POSIX_SOURCE tells the RTS not to define _POSIX_SOURCE.
-
 module Main (main) where
 
 import IOExts ( unsafePerformIO )
