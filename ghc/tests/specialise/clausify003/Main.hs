02e6e233d5325b1485b090049db998e4d7026906
simonmar <unknown>
2001-08-23 10:53:48 +0000

    [project @ 2001-08-23 10:53:46 by simonmar] remove unused tests.

--- ghc/tests/specialise/clausify003/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main ( main ) where
-
-import PreludeClausify (clausify)
-
--- the main program: reads stdin and writes stdout
-main = scc "CAF:main" 
-    do
-	input <- getContents
-	putStr (clausify input)
