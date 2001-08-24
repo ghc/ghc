a1e8f0545d7990ab0a62af90861e73a53558ccc4
simonpj <unknown>
2001-08-24 14:36:44 +0000

    [project @ 2001-08-24 14:36:44 by simonpj] Remove bogus test from tc126

--- testsuite/tests/ghc-regress/typecheck/should_compile/tc126.hs
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc126.hs
@@ -32,13 +32,3 @@ g2 = f (f (f [0::Int]))
 -- The outer f repeats the exercise, so g2::Int
 -- This is the definition that Hugs rejects
 
--- Here is a similar definition rejected by Hugs
--- It complains that the instances are not consistent with the
--- functional dependencies, which isn't true, because
---	(c a) does not unify with (c' a', c' b')
-
-class Foo f a r | f a->r where
-      foo::f->a->r
-
-instance Foo (a->r)     (c a)    (c r)
-instance Foo ((a,b)->r) (c a,c b)(c r)
