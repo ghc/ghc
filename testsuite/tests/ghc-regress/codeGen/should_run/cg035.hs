4eb498d14e55c4a45c138d7fe51b877646b0c7e9
rrt <unknown>
2001-08-31 16:06:46 +0000

    [project @ 2001-08-31 16:06:46 by rrt] Change this test to make it mathematically trivial, but keep the ccall intact. This has two salutary effects:
    1. We can expect to get the same result (sin 0.0 = 0.0) on all systems.
    
    2. sin exists on Windows. erf doesn't. Hence the test now works on Windows.

--- testsuite/tests/ghc-regress/codeGen/should_run/cg035.hs
+++ testsuite/tests/ghc-regress/codeGen/should_run/cg035.hs
@@ -2,10 +2,10 @@ module Main (main) where
 
 import IOExts ( unsafePerformIO )
 
-po :: Double -> Double
-po rd = 0.5 + 0.5 * erf ((rd / 1.04) / sqrt 2)
+c :: Double -> Double
+c x = sin x
   where
-    erf :: Double -> Double
-    erf x = unsafePerformIO (_ccall_ erf x)
+    sin :: Double -> Double
+    sin x = unsafePerformIO (_ccall_ sin x)
 
-main = putStr (shows (po 2.0) "\n")
+main = putStr (shows (c 0.0) "\n")
