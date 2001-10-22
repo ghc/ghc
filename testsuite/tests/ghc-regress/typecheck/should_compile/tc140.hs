cf796b314a75877afe2784442a5cc316f84b5ff0
simonpj <unknown>
2001-10-22 09:35:11 +0000

    [project @ 2001-10-22 09:35:11 by simonpj] Add tc140

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc140.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Make sure for-alls can occur in data types
+
+module Foo where
+
+newtype CPS1 a = CPS1 { unCPS1 :: forall ans . (a -> ans) -> ans }
+newtype CPS2 a = CPS2 (forall ans . (a -> ans) -> ans)
+data 	CPS3 a = CPS3 { unCPS3 :: forall ans . (a -> ans) -> ans }
+data 	CPS4 a = CPS4 (forall ans . (a -> ans) -> ans)
