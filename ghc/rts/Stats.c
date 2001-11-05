70d0bbbc0acb21e51cc88d0c6d496d3915f7077e
sof <unknown>
2001-11-05 05:12:55 +0000

    [project @ 2001-11-05 05:12:55 by sof] merge revision 1.32.4.2 (ghc-5-02-branch)
      TIC_TO_DOUBLE --> TICK_TO_DOUBLE

--- ghc/rts/Stats.c
+++ ghc/rts/Stats.c
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------------------
- * $Id: Stats.c,v 1.33 2001/11/02 11:15:00 simonmar Exp $
+ * $Id: Stats.c,v 1.34 2001/11/05 05:12:55 sof Exp $
  *
  * (c) The GHC Team, 1998-1999
  *
@@ -575,7 +575,7 @@ stat_exit(int alloc)
 		    TICK_TO_DBL(time), TICK_TO_DBL(etime));
 	    fprintf(sf, "  %%GC time     %5.1f%%  (%.1f%% elapsed)\n\n",
 		    TICK_TO_DBL(GC_tot_time)*100/TICK_TO_DBL(time),
-		    TICK_TO_DBL(GCe_tot_time)*100/TIC_TO_DBL(etime));
+		    TICK_TO_DBL(GCe_tot_time)*100/TICK_TO_DBL(etime));
 
 	    if (time - GC_tot_time == 0)
 		ullong_format_string(0, temp, rtsTrue/*commas*/);
