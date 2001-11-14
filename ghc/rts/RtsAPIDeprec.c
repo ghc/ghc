9a558a755387b2a1fc86a738c94d43a5bdd62fda
simonmar <unknown>
2001-11-14 16:34:22 +0000

    [project @ 2001-11-14 16:34:22 by simonmar] use SET_HDR rather than explicitly setting p->header.info.

--- ghc/rts/RtsAPIDeprec.c
+++ ghc/rts/RtsAPIDeprec.c
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------------
- * $Id: RtsAPIDeprec.c,v 1.2 2001/08/14 13:40:09 sewardj Exp $
+ * $Id: RtsAPIDeprec.c,v 1.3 2001/11/14 16:34:22 simonmar Exp $
  *
  * (c) The GHC Team, 1998-2001
  *
@@ -17,7 +17,7 @@ HaskellObj
 rts_mkAddr (HsAddr a)
 {
   StgClosure *p = (StgClosure *)allocate(sizeofW(StgHeader)+1);
-  p->header.info = Azh_con_info;
+  SET_HDR(p, Azh_con_info, CCS_SYSTEM);
   p->payload[0]  = (StgClosure *)a;
   return p;
 }
@@ -27,7 +27,6 @@ rts_getAddr (HaskellObj p)
 {
   if ( p->header.info == Azh_con_info || 
        p->header.info == Azh_static_info ) {
-  
     return (void *)(p->payload[0]);
   } else {
     barf("getAddr: not an Addr");
