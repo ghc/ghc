2acb5952b1714e60e7d79fffc39f9972b4de06bb
simonmar <unknown>
2001-12-10 15:22:12 +0000

    [project @ 2001-12-10 15:22:12 by simonmar] Test that the instances of enumFrom for the basic numeric types are strict (only Int is at the moment, so this is an expected failure).

---
+++ testsuite/tests/ghc-regress/lib/should_run/enum04.hs
@@ -0,0 +1,8 @@
+import Exception
+import Prelude hiding (catch)
+
+main = do 
+  catch	(evaluate [error "" :: Int ..] >> return ())     (\_ -> putStrLn "ok1")
+  catch	(evaluate [error "" :: Integer ..] >> return ()) (\_ -> putStrLn "ok2")
+  catch	(evaluate [error "" :: Float ..] >> return ())   (\_ -> putStrLn "ok3")
+  catch	(evaluate [error "" :: Double ..] >> return ())  (\_ -> putStrLn "ok4")
