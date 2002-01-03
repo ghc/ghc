c69539312121a2e6a13731a28f0bf0b79f37ef3f
simonpj <unknown>
2002-01-03 12:36:05 +0000

    [project @ 2002-01-03 12:36:04 by simonpj] update output, add drvfail002

---
+++ testsuite/tests/ghc-regress/deriving/should_fail/drvfail002.hs
@@ -0,0 +1,19 @@
+{-# OPTIONS -fglasgow-exts -fallow-undecidable-instances #-}
+
+-- The Show instance for S would have form
+--	instance X T c => Show S
+-- which is hard to deal with.  It sent GHC 5.01 into
+-- an infinite loop; now it should be rejected.
+
+module ShouldFail where
+
+data T = T Integer
+
+class X a b | a -> b where
+    f :: a -> b
+
+instance X T c => Show T where
+    show _ = ""
+
+data S = S T deriving Show
+
