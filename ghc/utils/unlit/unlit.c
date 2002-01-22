4b5ef150751f2c3756ab0d7773688abd487d054f
simonmar <unknown>
2002-01-22 15:55:59 +0000

    [project @ 2002-01-22 15:55:59 by simonmar] Fix wibbles in previous commit.

--- ghc/utils/unlit/unlit.c
+++ ghc/utils/unlit/unlit.c
@@ -348,8 +348,8 @@ char **argv; {
         }
 
     /* Prefix the output with line pragmas */
-    if (add_line_prag_long) {
-      fprintf(ostream, "# 1 \"%s\"\n");{-# LINE 1 \"%s\" #-}\n", prefix_str, prefix_str);
+    if (prefix_str) {
+      fprintf(ostream, "#line 1 \"%s\"\n);{-# LINE 1 \"%s\" #-}\n", prefix_str, prefix_str);
     }
 
     unlit(file, istream, ostream);
