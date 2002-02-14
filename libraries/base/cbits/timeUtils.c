d17149711fc1e89863539b6c2c28cf5592748273
sof <unknown>
2002-02-14 07:31:03 +0000

    [project @ 2002-02-14 07:31:03 by sof] Time stubs (for mingw)

---
+++ libraries/base/cbits/timeUtils.c
@@ -0,0 +1,15 @@
+/* 
+ * (c) The University of Glasgow 2002
+ *
+ * Time Runtime Support
+ */
+#include "HsBase.h"
+
+#if defined(mingw32_TARGET_OS) /* to the end */
+
+HsAddr __hscore_timezone( void )
+{ return (HsAddr)&_timezone; }
+
+HsAddr __hscore_tzname( void )
+{ return (HsAddr)_tzname; }
+#endif
---
+++ libraries/base/include/timeUtils.h
@@ -0,0 +1,12 @@
+/*
+ * (c) The University of Glasgow 2002
+ *
+ * Time Runtime Support
+ */
+#ifndef __TIMEUTILS_H__
+#define __TIMEUTILS_H__
+
+extern HsAddr __hscore_timezone( void );
+extern HsAddr __hscore_tzname( void );
+
+#endif /* __DIRUTILS_H__ */
