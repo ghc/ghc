92956368c1bfbaaad870d80b39d008ba3325336e
simonmar <unknown>
2002-03-14 16:26:40 +0000

    [project @ 2002-03-14 16:26:40 by simonmar] Add class Splittable that accidentally got dropped on the floor when we moved over to the new libraries.

--- libraries/base/GHC/Exts.hs
+++ libraries/base/GHC/Exts.hs
@@ -8,7 +8,7 @@
 -- Stability   :  experimental
 -- Portability :  non-portable
 --
--- $Id: Exts.hs,v 1.2 2002/01/02 15:01:27 simonmar Exp $
+-- $Id: Exts.hs,v 1.3 2002/03/14 16:26:40 simonmar Exp $
 --
 -- GHC Extensions: this is the Approved Way to get at GHC-specific stuff.
 --
@@ -25,6 +25,9 @@ module GHC.Exts
 	-- shifty wrappers from GHC.Base
 	shiftL#, shiftRL#, iShiftL#, iShiftRA#, iShiftRL#,
 
+	-- for linear implicit parameters:
+	Splittable(..),
+
         -- and finally, all the unboxed primops of GHC!
         module GHC.Prim
 
@@ -37,3 +40,6 @@ import GHC.Base
 import GHC.Word
 import GHC.Num
 import GHC.Float
+
+class Splittable t where
+  split :: t -> (t,t)
