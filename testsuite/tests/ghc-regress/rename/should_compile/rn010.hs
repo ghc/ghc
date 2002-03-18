a705733f5036ab6d38206415c0fc3a22334be012
simonmar <unknown>
2002-03-18 15:23:46 +0000

    [project @ 2002-03-18 15:23:46 by simonmar] use {-# SOURCE #-} imports for the hand-written ifaces

--- testsuite/tests/ghc-regress/rename/should_compile/rn010.hs
+++ testsuite/tests/ghc-regress/rename/should_compile/rn010.hs
@@ -1,12 +1,12 @@
 module Imp100 where
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
-import Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
+import {-# SOURCE #-} Int10
 
