6fe7be9d3b49ca95f3f31a0ece43a30880a92ed0
simonmar <unknown>
2002-03-18 13:58:30 +0000

    [project @ 2002-03-18 13:58:30 by simonmar] Passing a Float to a varargs C function isn't possible, so pass a Double instead.

--- testsuite/tests/ghc-regress/ccall/should_run/ffi003.hs
+++ testsuite/tests/ghc-regress/ccall/should_run/ffi003.hs
@@ -1,11 +1,11 @@
 {-# OPTIONS -fasm #-}
 
--- !!! Test passing floats to a ccall, was broken in the NCG in 5.02.2
+-- !!! Test passing doubles to a ccall
 
 import Foreign
 import Foreign.C
 
 foreign import ccall unsafe 
-  printf :: CString -> Float -> IO CInt
+  printf :: CString -> Double -> IO CInt
 
 main = withCString "%f" $ \s -> printf s 1.2345
