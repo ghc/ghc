eb3e0fd9ccc6dbd3407562612f7d3069b4ddbf9e
simonmar <unknown>
2002-03-18 15:14:36 +0000

    [project @ 2002-03-18 15:14:36 by simonmar] PrelGHC ==> GHC.Prim

--- testsuite/tests/ghc-regress/typecheck/should_compile/tc085.hs
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc085.hs
@@ -1,10 +1,10 @@
 {-# OPTIONS -fglasgow-exts #-}
 -- !!! From a bug report from Satnam.
 -- !!! To do with re-exporting importees from PreludeGla* modules.
-module ShouldSucceed ( module IOExts, module PrelGHC ) where
+module ShouldSucceed ( module IOExts, module GHC.Prim ) where
 
 import IOExts
-import PrelGHC
+import GHC.Prim
 
 type FooType = Int
 data FooData = FooData
