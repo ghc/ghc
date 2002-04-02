1638122c53b3c8d5b09a921930fee433b6f1a541
simonpj <unknown>
2002-04-02 13:48:41 +0000

    [project @ 2002-04-02 13:48:41 by simonpj] Context-simplification loop tests

---
+++ testsuite/tests/ghc-regress/typecheck/should_run/tcrun021.hs
@@ -0,0 +1,52 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- This bizarre program failed because TcSimplify built a loop of
+-- dictionaries, due to an obscure bug in the way in which superclasses
+-- were added
+
+module Main where
+
+import List
+import FiniteMap
+
+class (Ord oid) => Object o oid | o -> oid where
+
+data Access oid
+
+class (Object o oid) => SecurityModel model o oid | model -> o
+
+class (SecurityModel model o oid) => SecurityPolicy policy model o oid where
+    checkAccess :: policy -> model -> Access oid -> Bool
+    checkAccess _ _ _ = True
+    checkModel :: policy -> model -> Bool
+    checkModel _ _ = True
+
+------------------------------------------------------------
+-- The Linux instance
+------------------------------------------------------------
+
+type LinuxObjectId = Either [String] String
+
+data LinuxObject = File [String] deriving (Eq, Show)
+
+instance Object LinuxObject LinuxObjectId
+
+data LinuxSecurityModel =
+    LinuxSecurityModel { lsmObjectSet   :: FiniteMap LinuxObjectId LinuxObject }
+
+instance (Show a, Show b) => Show (FiniteMap a b) where
+    show fm = show (fmToList fm)
+
+instance Show LinuxSecurityModel where
+    show lsm = "LSM:" ++ "\tObjects:  " ++ show (lsmObjectSet lsm)
+
+instance SecurityModel LinuxSecurityModel LinuxObject LinuxObjectId
+
+data LinuxSecurityPolicy = LinuxSecurityPolicy
+instance SecurityPolicy LinuxSecurityPolicy LinuxSecurityModel LinuxObject LinuxObjectId
+
+model :: FiniteMap LinuxObjectId LinuxObject
+model =  listToFM [ (Left [], File []), (Left ["home"], File ["home"]) ]
+
+main :: IO ()
+main = do { putStrLn (show model) }
