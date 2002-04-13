3440ce3886a8eb8b401edce92f7fe4bc55eced5f
sof <unknown>
2002-04-13 05:11:29 +0000

    [project @ 2002-04-13 05:11:29 by sof] testing MT extension

---
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc037.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import Control.Concurrent
+
+foreign import "sleep" threadsafe sleepBlock :: Int -> IO ()
+
+main :: IO ()
+main = do
+  th <- newEmptyMVar
+  forkIO $ do
+     putStrLn "newThread started"
+     sleepBlock 5
+     putStrLn "newThread back again"
+     putMVar th "5 secs later"
+  yield -- make sure the newly created thread is run.
+  putStrLn "mainThread"
+  x <- takeMVar th
+  putStrLn x
+  putStrLn "\nshutting down"
+  
