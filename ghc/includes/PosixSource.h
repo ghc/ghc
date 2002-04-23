cdb081d94a56066823f3725662d919611061c5fc
ken <unknown>
2002-04-23 17:16:01 +0000

    [project @ 2002-04-23 17:16:01 by ken] #define _POSIX_C_SOURCE 199506L, so that we get pthread stuff properly on alpha-dec-osf3.

--- ghc/includes/PosixSource.h
+++ ghc/includes/PosixSource.h
@@ -1,5 +1,5 @@
 /* -----------------------------------------------------------------------------
- * $Id: PosixSource.h,v 1.1 2001/08/14 13:40:08 sewardj Exp $
+ * $Id: PosixSource.h,v 1.2 2002/04/23 17:16:01 ken Exp $
  *
  * (c) The GHC Team, 1998-2001
  *
@@ -11,7 +11,7 @@
 #define POSIXSOURCE_H
 
 #define _POSIX_SOURCE   1
-#define _POSIX_C_SOURCE 199309L
+#define _POSIX_C_SOURCE 199506L
 #define _ISOC9X_SOURCE
 
 /* Let's be ISO C9X too... */
