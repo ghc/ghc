b51edfa4783ec325ddfa203d55095a74109228fe
simonpj <unknown>
2002-05-10 15:24:29 +0000

    [project @ 2002-05-10 15:24:29 by simonpj] More tests for type validity

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc155.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- The type sig for 'test' is illegal in H98 because of the
+-- partial application of the type sig.
+-- But with -fglasgow-exts it should be OK because when
+-- you expand the type synonyms it's just Int->Int
+-- 	c.f should_fail/tcfail107.hs
+
+module ShouldCompile where
+
+type Thing m = m ()
+
+type Const a b = a
+
+test :: Thing (Const Int) -> Thing (Const Int)
+test = test
+
