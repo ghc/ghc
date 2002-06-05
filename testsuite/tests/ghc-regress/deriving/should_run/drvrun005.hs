2ed68806d2d972379b3bfb202667e5bc6124ff07
simonpj <unknown>
2002-06-05 14:09:46 +0000

    [project @ 2002-06-05 14:09:46 by simonpj] Update expected results

--- testsuite/tests/ghc-regress/deriving/should_run/drvrun005.hs
+++ testsuite/tests/ghc-regress/deriving/should_run/drvrun005.hs
@@ -4,14 +4,17 @@ module Main where
   If a fixity declaration hasn't been supplied for
   an operator, it is defaulted to being "infixl 9".
 
-  The derived Read instances for data types containing
-  left-assoc constructors produces code that causes
-  non-termination if you use 'read' to evaluate them
-  ( (head (reads x)) is cool tho.)
+  OLD:   The derived Read instances for data types containing
+	 left-assoc constructors produces code that causes
+	 non-termination if you use 'read' to evaluate them
+	 ( (head (reads x)) is cool tho.)
 
-  ==> The inferred assoc for :++ below left & the derived
-  Read instance should fail to terminate (with ghc-4.xx,
-  this is exemplified by having the stack overflow.)
+	  ==> The inferred assoc for :++ below left & the derived
+	  Read instance should fail to terminate (with ghc-4.xx,
+	  this is exemplified by having the stack overflow.)
+
+  NEW: the new H98 spec says that we ignore associativity when
+       parsing, so it terminates fine
 -}
 -- infixl 9 :++
 data T = T1 |  T :++ T  deriving (Eq,Show, Read)
