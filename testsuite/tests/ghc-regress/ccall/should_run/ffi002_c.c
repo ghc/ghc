ede13993d6f9db4de1b5abe6ba88321def0c22a5
simonmar <unknown>
2002-07-15 15:26:44 +0000

    [project @ 2002-07-15 15:26:44 by simonmar] Turns out that we *did* have a test that would have shown up the DsForeign bug (rev. 1.70 of ghc/compiler/deSugar/DsForeign.lhs) but it was disabled because it didn't quite work with the current test framework.
    I've hacked the framework a bit and hopefully knocked the test into
    shape so we shouldn't get the same problem again.
    
    MERGE TO STABLE after testing in HEAD

--- testsuite/tests/ghc-regress/ccall/should_run/ffi002_c.c
+++ testsuite/tests/ghc-regress/ccall/should_run/ffi002_c.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "foo_stub.h"
+#include "ffi002_stub.h"
 
 #include "RtsAPI.h"
 
 extern void __stginit_Foo ( void );
 
-int main(void)
+int main(int argc, char *argv[])
 {
   int i;
 
-  char* bogusFlags[1] = { "\0" };
-
-  startupHaskell(0, bogusFlags, __stginit_Foo);
+  startupHaskell(argc, argv, __stginit_Foo);
 
   for (i = 0; i < 5; i++) {
     printf("%d\n", foo(2500));
