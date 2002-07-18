f9b1dabfeaeebc8338e740e2b4c2236b71d241d5
sof <unknown>
2002-07-18 22:01:07 +0000

    [project @ 2002-07-18 22:01:07 by sof] helper functions for mucking about with Win32 consoles

---
+++ libraries/base/include/consUtils.h
@@ -0,0 +1,11 @@
+/* 
+ * (c) The University of Glasgow, 2000-2002
+ *
+ * Win32 Console API helpers.
+ */
+#ifndef __CONSUTILS_H__
+#define __CONSUTILS_H__
+extern int set_console_buffering__(int fd, int cooked);
+extern int set_console_echo__(int fd, int on);
+extern int get_console_echo__(int fd);
+#endif
