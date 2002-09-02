2e44135f7fa81bb6f75e5ad575a884ef81c8ce36
ross <unknown>
2002-09-02 11:20:50 +0000

    [project @ 2002-09-02 11:20:50 by ross] Minor rearrangement: the implementation of System.Exit.exitWith is GHC-specific, while exitFailure is portable.

--- libraries/base/System/Exit.hs
+++ libraries/base/System/Exit.hs
@@ -36,13 +36,13 @@ import Hugs.System
 -- program's caller.  Before it terminates, any open or semi-closed
 -- handles are first closed.
 
-#ifndef __HUGS__
+#ifdef __GLASGOW_HASKELL__
 exitWith :: ExitCode -> IO a
 exitWith ExitSuccess = throw (ExitException ExitSuccess)
 exitWith code@(ExitFailure n) 
   | n == 0 = ioException (IOError Nothing InvalidArgument "exitWith" "ExitFailure 0" Nothing)
   | otherwise = throw (ExitException code)
+#endif  /* __GLASGOW_HASKELL__ */
 
 exitFailure :: IO a
 exitFailure = exitWith (ExitFailure 1)
-#endif  /* __HUGS__ */
