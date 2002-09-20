0933eb0b84c9a434eac707579fa08e77d40bae4b
simonmar <unknown>
2002-09-20 14:07:04 +0000

    [project @ 2002-09-20 14:07:04 by simonmar] rn037 has moved into ../prog002

--- testsuite/tests/ghc-regress/rename/should_compile/rn037.hs
+++
@@ -1,9 +0,0 @@
--- !!! Checking that you can hide a constructor
-module ShouldCompile where
-
-import Rn037Help hiding( C )
-	-- C is the constructor, but we should
-	-- still be able to hide it
-
--- we should still be able to refer to the type constructor, though
-type Foo = T
