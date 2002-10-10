db86a583260389738cc1774f9b37fcb637c1cb58
simonpj <unknown>
2002-10-10 14:10:14 +0000

    [project @ 2002-10-10 14:10:14 by simonpj] Add mdo test

---
+++ testsuite/tests/ghc-regress/mdo/mdo001.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module Main(main) where
+ 
+import Control.Monad.Fix 
+
+t :: IO Int
+t = mdo x <- return (l "1")
+        let l [] = 0
+            l (x:xs) = 1 + l xs
+        return x
+
+main :: IO ()
+main = t >>= print
