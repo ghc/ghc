10e3100a52f219a0e9dbed8366dab75f9e98a229
simonmar <unknown>
2003-01-24 14:08:59 +0000

    [project @ 2003-01-24 14:08:59 by simonmar] Start a new GHCi-only test category, and add our first test

---
+++ testsuite/tests/ghc-regress/ghci/ghci001.hs
@@ -0,0 +1,27 @@
+module Main where
+
+data FourArrow = A | B | C | D | E | ABE | AC | BD | CDE
+		 deriving (Eq)
+
+dom E   = ABE
+
+cod A   = AC
+cod B   = BD
+cod C   = CDE
+cod D   = CDE
+cod E   = CDE
+cod ABE = ABE
+cod AC  = AC
+cod BD  = BD
+cod CDE = CDE
+
+----
+bceFour :: FourArrow -> FourArrow -> String
+bceFour f g
+    | dom f == dom g && cod f == cod g
+    = "it works"
+    | otherwise = error ("Four.bceFour: precondition fails:"
+			  ++ "arrows not parallel"
+			  ++ "\n")
+
+main = print (bceFour E E)
