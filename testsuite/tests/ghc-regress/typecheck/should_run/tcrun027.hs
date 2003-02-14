ae3a3a9deb8b2eed66653a15989c175b1f3a4235
simonpj <unknown>
2003-02-14 10:54:42 +0000

    [project @ 2003-02-14 10:54:42 by simonpj] Add another impicit-param test

---
+++ testsuite/tests/ghc-regress/typecheck/should_run/tcrun027.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Killed GHC 5.04.1
+
+module Main where
+
+type CTPar = ([Double],Int)
+
+us :: (?ctPar :: CTPar) => [Double]
+us = let (d,_) = ?ctPar in d
+
+main = let ?ctPar = ([3.4],2) in print us 
