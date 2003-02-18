341b99a10997618d504274ee03677bbde558b5f5
panne <unknown>
2003-02-18 20:39:56 +0000

    [project @ 2003-02-18 20:39:56 by panne] Guerrilla tactics: Re-export Foreign.Marshal.Pool  >:-)

--- libraries/base/Foreign/Marshal.hs
+++ libraries/base/Foreign/Marshal.hs
@@ -17,10 +17,12 @@ module Foreign.Marshal
         ( module Foreign.Marshal.Alloc
         , module Foreign.Marshal.Array
         , module Foreign.Marshal.Error
+        , module Foreign.Marshal.Pool
         , module Foreign.Marshal.Utils
         ) where
 
 import Foreign.Marshal.Alloc
 import Foreign.Marshal.Array
 import Foreign.Marshal.Error
+import Foreign.Marshal.Pool
 import Foreign.Marshal.Utils
