6662ef4f43940bd939de37340d88fe12a82b098a
sof <unknown>
2003-03-19 19:07:31 +0000

    [project @ 2003-03-19 19:07:31 by sof] threadLabels: not public

--- ghc/rts/ThreadLabels.h
+++ ghc/rts/ThreadLabels.h
@@ -6,11 +6,11 @@
  * Table of thread labels.
  *
  * ---------------------------------------------------------------------------*/
+#ifndef __THREADLABELS_H__
+#define __THREADLABELS_H__
 
 #include "Rts.h"
 #include "Hash.h"
-
-extern HashTable * threadLabels;
 
 void
 initThreadLabelTable(void);
@@ -23,3 +23,5 @@ lookupThreadLabel(StgWord key);
 
 void
 removeThreadLabel(StgWord key);
+
+#endif /* __THREADLABELS_H__ */
