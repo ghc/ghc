03643ba411ac71d69b2fbd5a6c38b0ab7d817c25
simonmar <unknown>
2003-03-26 15:29:49 +0000

    [project @ 2003-03-26 15:29:49 by simonmar] change this test to avoid closing stderr, which confuses GHCi

--- testsuite/tests/ghc-regress/lib/IO/hClose001.hs
+++ testsuite/tests/ghc-regress/lib/IO/hClose001.hs
@@ -1,5 +1,7 @@
 import IO
 
 main = do
-  hClose stderr
-  hPutStr stderr "junk" `catch` \ err -> if isIllegalOperation err then putStr "Okay\n" else error "Not okay\n"
+  h <- openFile "hClose001.tmp" WriteMode
+  hPutStr h "junk" 
+  hClose h
+  hPutStr h "junk" `catch` \ err -> if isIllegalOperation err then putStr "Okay\n" else error "Not okay\n"
