371b4d98a140f98a05633106076b36b993a586cf
ross <unknown>
2003-05-01 10:46:19 +0000

    [project @ 2003-05-01 10:46:19 by ross] Make -t option to the script override the template file, just as -c overrides the compiler, as the the documentation implies.  (Formerly -t added an extra template file, which is not so useful.)

--- ghc/utils/hsc2hs/hsc2hs.sh
+++ ghc/utils/hsc2hs/hsc2hs.sh
@@ -1,10 +1,13 @@
 
+tflag="--template=$HSC2HS_DIR/template-hsc.h"
 for arg do
     case "$arg" in
         -c*)    HSC2HS_EXTRA=;;
         --cc=*) HSC2HS_EXTRA=;;
+	-t*)	tflag=;;
+	--template=*) tflag=;;
         --)     break;;
     esac
 done
 
-$HSC2HS_BINDIR/$HS_PROG -t $HSC2HS_DIR/template-hsc.h $HSC2HS_EXTRA "$@" 
+$HSC2HS_BINDIR/$HS_PROG $tflag $HSC2HS_EXTRA "$@" 
