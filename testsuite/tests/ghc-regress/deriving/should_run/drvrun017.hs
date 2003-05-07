0849c91e3967e1f11a3dc3fc622408ec12323578
simonpj <unknown>
2003-05-07 09:26:14 +0000

    [project @ 2003-05-07 09:26:14 by simonpj] Add test for Show on unboxed types

---
+++ testsuite/tests/ghc-regress/deriving/should_run/drvrun017.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Test Show on unboxed types
+
+module Main where
+import GHC.Base
+
+data Foo = MkFoo Int# Float# Int deriving( Show )
+
+main = print (MkFoo 3# 4.3# 2)
