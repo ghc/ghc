3319c67dd7ef9d2b7a1366000688e8ad0d358d99
igloo <unknown>
2003-05-21 02:58:40 +0000

    [project @ 2003-05-21 02:58:40 by igloo] Added support for newtypes to TH and altered a test for them.

--- testsuite/tests/ghc-regress/th/TH_spliceDecl2.hs
+++ testsuite/tests/ghc-regress/th/TH_spliceDecl2.hs
@@ -1,9 +1,12 @@
--- test splicing of a quoted data declarations
+-- test splicing of quoted data and newtype declarations
 
 module TH_spliceDecl2
 where
 
 import Language.Haskell.THSyntax
 
--- splice a simple quoted declaration
-$([d| data T = C |])
+-- splice a simple quoted declaration (x 2)
+$([d| data T1 = C1 |])
+
+$([d| newtype T2 = C2 String |])
+
