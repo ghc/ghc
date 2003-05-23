1bf81740fe4af95d5dc4e4f428e9f584c4d9d0b6
simonmar <unknown>
2003-05-23 10:21:27 +0000

    [project @ 2003-05-23 10:21:27 by simonmar] Hide this module in the Haddock docs.

--- libraries/base/Foreign/C/TypesISO.hs
+++ libraries/base/Foreign/C/TypesISO.hs
@@ -14,6 +14,7 @@
 --
 -----------------------------------------------------------------------------
 
+-- #hide
 module Foreign.C.TypesISO
 	( -- Integral types, instances of: Eq, Ord, Num, Read, Show, Enum,
 	  -- Typeable, Storable, Bounded, Real, Integral, Bits
