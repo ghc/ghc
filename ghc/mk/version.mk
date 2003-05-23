368407cb7f83d841df6c3fdf8c2700dbe16f26cb
simonmar <unknown>
2003-05-23 11:03:39 +0000

    [project @ 2003-05-23 11:03:39 by simonmar] The HEAD is version 6.1 now.

--- ghc/mk/version.mk
+++ ghc/mk/version.mk
@@ -36,8 +36,8 @@
 
 ProjectName       = The Glorious Glasgow Haskell Compilation System
 ProjectNameShort  = ghc
-ProjectVersion    = 6.0
-ProjectVersionInt = 600
+ProjectVersion    = 6.1
+ProjectVersionInt = 601
 ProjectPatchLevel = 0
 
 # Interface file version (hi-boot files only)
