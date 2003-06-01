8492cd2dbafa94e0387f99ac0285df3630fff6d9
ralf <unknown>
2003-06-01 17:21:00 +0000

    [project @ 2003-06-01 17:20:59 by ralf] Added an example for traversal dimension; started a TODO list here

--- testsuite/tests/ghc-regress/lib/Generics/gmapQ-assoc.hs
+++ testsuite/tests/ghc-regress/lib/Generics/gmapQ-assoc.hs
@@ -1,6 +1,28 @@
 {-# OPTIONS -fglasgow-exts #-}
 
--- Demonstrate inadequacy of a naive variation on gmapQ
+{-
+
+This example demonstrates the inadequacy of an apparantly simpler
+variation on gmapQ. To this end, let us first recall a few facts.
+Firstly, function application (including constructor application) is
+left-associative. This is the reason why we had chosen our generic
+fold to be left-associative too. (In "The Sketch Of a Polymorphic
+Symphony" you can a right-associative generic fold, too.) Secondly,
+lists are right-associative. So we want to query a term by retrieving
+a list of results from the immediate subterms, then the natural
+left-to-right order of traversal needs some extra effort. That is, we
+traverse a left-associative data-structure while we want to construct
+a right-associative one. In the module Data.Generics, we solve the
+problem by a common higher-order trick, that is, we do not cons lists
+during folding but we pass functions on lists starting from the
+identity function and passing [] to the resulting function. This is
+captured by a fancy datatype constructor Q. Below, we shou that we
+get indeed a unnatural right-to-left order if we just apply the simple
+constant datatype constructor instead of Q.
+
+Contributed by Ralf Laemmel, ralf@cwi.nl
+
+-}
 
 module Main where
 import Data.Generics
