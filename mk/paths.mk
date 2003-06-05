dfc15b64151029dd51c423ec5e7baf6096d1ba3f
reid <unknown>
2003-06-05 12:29:31 +0000

    [project @ 2003-06-05 12:29:31 by reid] Add GC_C_OBJS to MOSTLY_CLEAN_FILES

--- mk/paths.mk
+++ mk/paths.mk
@@ -262,7 +262,7 @@ TAGS_C_SRCS=$(C_SRCS)
 #                        that may require extra tools to create.
 #
 #
-MOSTLY_CLEAN_FILES += $(HS_OBJS) $(C_OBJS) $(HSC_C_OBJS)
+MOSTLY_CLEAN_FILES += $(HS_OBJS) $(C_OBJS) $(HSC_C_OBJS) $(GC_C_OBJS)
 CLEAN_FILES        += $(HS_PROG) $(C_PROG) $(SCRIPT_PROG) $(SCRIPT_LINK) \
 		      $(PROG) $(LIBRARY) a.out \
 		      $(DERIVED_HSC_SRCS) \
