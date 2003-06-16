48623389126dd2af825149a67c6039a735696528
simonmar <unknown>
2003-06-16 15:17:28 +0000

    [project @ 2003-06-16 15:17:28 by simonmar] Get up a bit earlier in the morning than the strictness analyser.

--- testsuite/tests/ghc-regress/concurrent/should_run/conc034.hs
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc034.hs
@@ -8,8 +8,12 @@ import Control.Exception
 -- to run" instead).
 
 main = do
-  let a = sum [1..10000] + b
-      b = sum [2..10000] + a
+  let a = last ([1..10000] ++ [b])
+      b = last ([2..10000] ++ [a])
+	-- we have to be careful to ensure that the strictness analyser
+	-- can't see that a and b are both bottom, otherwise the
+	-- simplifier will go to town here, resulting in something like
+	-- a = a and b = a.
   forkIO (print a)
   r <- Control.Exception.try $ print b
   print r
