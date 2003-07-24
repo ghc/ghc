16eadeacd1c56ce2b09c7bb86a67c18502fdd417
simonpj <unknown>
2003-07-24 07:47:32 +0000

    [project @ 2003-07-24 07:47:32 by simonpj] Add a pretty-print test (makes GHC 6.0 loop)

---
+++ testsuite/tests/ghc-regress/lib/PrettyPrint/pp1.hs
@@ -0,0 +1,18 @@
+-- This code used to print an infinite string, by calling 'spaces'
+-- with a negative argument.  There's a patch in the library now,
+-- which makes 'spaces' do somehthing sensible when called with a negative
+-- argument, but it really should not happen at all.
+
+
+module Main where
+
+import Text.PrettyPrint.HughesPJ
+
+
+ncat x y = nest 4 $ cat [ x, y ]
+
+d1 = foldl1 ncat $ take 50 $ repeat $ char 'a'
+d2 = parens $  sep [ d1, text "+" , d1 ]
+
+main = print d2
+
