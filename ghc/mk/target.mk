e6834cad29914f123edb32c20d42b16e3308e667
simonmar <unknown>
2003-07-24 10:47:05 +0000

    [project @ 2003-07-24 10:47:05 by simonmar] Remove ancient/bogus booting-from-hc stuff.

--- ghc/mk/target.mk
+++ ghc/mk/target.mk
@@ -8,17 +8,6 @@
 GHC_TOP := $(TOP)
 TOP:=$(TOP)/..
 
-# When booting from .hc files, remove the suffix rule for 
-# .l?hs -> .o, so that the .hc -> .o is used instead.
-# Also disable the generation of the .hc files, even if
-# the .l?hs files are newer than the .hc ones.
-ifeq "$(GhcWithHscBuiltViaC)" "YES"
-%.$(way_)o  : %.lhs
-%.$(way_)o  : %.hs
-%.$(way_)hc : %.lhs
-%.$(way_)hc : %.hs
-endif
-
 include $(TOP)/mk/target.mk
 
 # Reset TOP
