5429707fa6e78ef143d303938d973f9b67df7802
simonpj <unknown>
2003-07-28 11:59:15 +0000

    [project @ 2003-07-28 11:59:15 by simonpj] Add a new test for higher-rank polymorphism (killed GHC 6.0

---
+++ testsuite/tests/ghc-regress/simplCore/should_compile/simpl009.hs
@@ -0,0 +1,11 @@
+{-# OPTIONS -fglasgow-exts -dcore-lint #-}
+
+-- This one killed GHC 6.0, with a "panic: applyTys"
+-- Only with -O, though
+
+module ShouldCompile where
+
+newtype R = R (forall a. a->a)
+
+foo = case undefined of
+	R f -> f ()
