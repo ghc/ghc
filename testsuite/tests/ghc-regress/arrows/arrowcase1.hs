b038ffa60b038ca1313b4d42de11ed2ca5c4ad37
ross <unknown>
2003-09-20 17:23:15 +0000

    [project @ 2003-09-20 17:23:15 by ross] expand test to include cases of lambdas

--- testsuite/tests/ghc-regress/arrows/arrowcase1.hs
+++ testsuite/tests/ghc-regress/arrows/arrowcase1.hs
@@ -9,3 +9,10 @@ h x = proc (y,z) -> case compare x y of
 	LT -> returnA -< x
 	EQ -> returnA -< y+z
 	GT -> returnA -< z+x
+
+g :: ArrowChoice a => Int -> a (Int,Int) Int
+g x = proc (y,z) -> (case compare x y of
+	LT -> \ a -> returnA -< x+a
+	EQ -> \ b -> returnA -< y+z+b
+	GT -> \ c -> returnA -< z+x
+    ) 1
