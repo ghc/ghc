01ba963749af9c990d51f567be62b01774bbcfb1
simonpj <unknown>
2004-05-05 09:35:40 +0000

    [project @ 2004-05-05 09:35:39 by simonpj] Typeable deriving test

---
+++ testsuite/tests/ghc-regress/deriving/should_fail/drvfail010.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module ShouldFail where
+import Data.Typeable
+
+data A a b c d e f g h i j = A deriving (Typeable)
+	-- Too many args
+
+data B a b = B (a b) deriving (Typeable)
+	-- Non type-kind args
