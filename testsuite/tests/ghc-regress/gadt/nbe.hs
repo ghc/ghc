c488d0074a095d85f4d62ef225d3c7d2dc2f52b8
simonpj <unknown>
2004-09-27 15:53:17 +0000

    [project @ 2004-09-27 15:53:17 by simonpj] Accept output

--- testsuite/tests/ghc-regress/gadt/nbe.hs
+++ testsuite/tests/ghc-regress/gadt/nbe.hs
@@ -55,7 +55,7 @@ var :: Var g t -> g -> t
 var ZVar     (_,t) = t
 var (SVar x) (h,s) = var x h
 
--- eval :: Exp g t -> g -> t
+eval :: Exp g t -> g -> t
 eval (Var x)    g = var x g
 eval (Lam _ e)  g = \a -> eval e (g,a)
 eval (App e e') g = eval e g (eval e' g)
