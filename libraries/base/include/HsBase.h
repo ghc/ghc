1169da69c4a55d2c9ab3fa79e7b57cc2cc3a4f69
dons <unknown>
2004-10-02 07:14:38 +0000

    [project @ 2004-10-02 07:14:38 by dons] Undef PACKAGE_NAME PACKAGE_STRING PACKAGE_BUGREPORT PACKAGE_TARNAME coming in from ghcconfig.h, so that we can use HsBaseConfig.h versions without screenfulls of cpp warnings.

--- libraries/base/include/HsBase.h
+++ libraries/base/include/HsBase.h
@@ -10,6 +10,21 @@
 #define __HSBASE_H__
 
 #include "ghcconfig.h"
+
+/* redefined in HsBaseConfig.h */
+#ifdef PACKAGE_NAME
+# undef PACKAGE_NAME
+#endif
+#ifdef PACKAGE_STRING
+# undef PACKAGE_STRING
+#endif
+#ifdef PACKAGE_BUGREPORT
+# undef PACKAGE_BUGREPORT
+#endif
+#ifdef PACKAGE_TARNAME
+# undef PACKAGE_TARNAME
+#endif
+
 #include "HsBaseConfig.h"
 #include "HsFFI.h"
 
