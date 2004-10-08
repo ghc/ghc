a13e533781d59b36ea65d77fa204838e1ee51a95
simonpj <unknown>
2004-10-08 14:01:24 +0000

    [project @ 2004-10-08 14:00:58 by simonpj] Add a test

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc182.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Tests the "stupid theta" in pattern-matching
+-- when there's an existential as well
+
+module ShouldCompile  where
+
+data (Show a) => Obs a = forall b. LiftObs a b
+
+f :: Show a => Obs a -> String
+f (LiftObs _ _) = "yes"
+                 
+
