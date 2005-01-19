7ec0d5b7fd3f6e8822c799d14b83b2024a26f7db
ross <unknown>
2005-01-19 17:20:31 +0000

    [project @ 2005-01-19 17:20:31 by ross] document the deprecated functions, and redirect mkSet to fromList

--- libraries/base/Data/Set.hs
+++ libraries/base/Data/Set.hs
@@ -1089,53 +1089,66 @@ prop_List xs
 --------------------------------------------------------------------}
 
 {-# DEPRECATED emptySet "Use empty instead" #-}
+-- | Obsolete equivalent of 'empty'.
 emptySet :: Set a
 emptySet = empty
 
-{-# DEPRECATED mkSet "Equivalent to 'foldl' (flip insert) empty'." #-}
+{-# DEPRECATED mkSet "Use fromList instead" #-}
+-- | Obsolete equivalent of 'fromList'.
 mkSet :: Ord a => [a]  -> Set a
-mkSet = List.foldl' (flip insert) empty
+mkSet = fromList
 
 {-# DEPRECATED setToList "Use elems instead." #-}
+-- | Obsolete equivalent of 'elems'.
 setToList :: Set a -> [a] 
 setToList = elems
 
 {-# DEPRECATED unitSet "Use singleton instead." #-}
+-- | Obsolete equivalent of 'singleton'.
 unitSet :: a -> Set a
 unitSet = singleton
 
 {-# DEPRECATED elementOf "Use member instead." #-}
+-- | Obsolete equivalent of 'member'.
 elementOf :: Ord a => a -> Set a -> Bool
 elementOf = member
 
 {-# DEPRECATED isEmptySet "Use null instead." #-}
+-- | Obsolete equivalent of 'null'.
 isEmptySet :: Set a -> Bool
 isEmptySet = null
 
 {-# DEPRECATED cardinality "Use size instead." #-}
+-- | Obsolete equivalent of 'size'.
 cardinality :: Set a -> Int
 cardinality = size
 
 {-# DEPRECATED unionManySets "Use unions instead." #-}
+-- | Obsolete equivalent of 'unions'.
 unionManySets :: Ord a => [Set a] -> Set a
 unionManySets = unions
 
 {-# DEPRECATED minusSet "Use difference instead." #-}
+-- | Obsolete equivalent of 'difference'.
 minusSet :: Ord a => Set a -> Set a -> Set a
 minusSet = difference
 
 {-# DEPRECATED mapSet "Use map instead." #-}
+-- | Obsolete equivalent of 'map'.
 mapSet :: (Ord a, Ord b) => (b -> a) -> Set b -> Set a
 mapSet = map
 
 {-# DEPRECATED intersect "Use intersection instead." #-}
+-- | Obsolete equivalent of 'intersection'.
 intersect :: Ord a => Set a -> Set a -> Set a
 intersect = intersection
 
 {-# DEPRECATED addToSet "Use 'flip insert' instead." #-}
+-- | Obsolete equivalent of @'flip' 'insert'@.
 addToSet :: Ord a => Set a -> a -> Set a
 addToSet = flip insert
 
 {-# DEPRECATED delFromSet "Use `flip delete' instead." #-}
+-- | Obsolete equivalent of @'flip' 'delete'@.
 delFromSet :: Ord a => Set a -> a -> Set a
 delFromSet = flip delete
