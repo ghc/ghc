9bed983abb5e97cea0a3694d256209b45c616552
simonmar <unknown>
2005-02-01 10:26:56 +0000

    [project @ 2005-02-01 10:24:12 by simonmar] Fix up after hs-boot changes

---
+++ testsuite/tests/ghc-regress/rename/should_compile/Rn017.hs
@@ -0,0 +1,13 @@
+module Rn017 where
+
+data Wibble 
+data Wobble
+
+a :: Int -> Int
+a = undefined
+
+b :: Int -> Int
+b = undefined
+
+c :: Int -> Int
+c = undefined
---
+++ testsuite/tests/ghc-regress/rename/should_compile/Rn017.hs-boot
@@ -0,0 +1,8 @@
+module Rn017 where
+
+data Wibble 
+data Wobble
+
+a :: Int -> Int
+b :: Int -> Int
+c :: Int -> Int
