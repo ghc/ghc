a82450059c57ffb0329183efabf99d9338b0abb7
simonpj <unknown>
2005-03-01 05:48:11 +0000

    [project @ 2005-03-01 05:48:11 by simonpj] Add a desugaring test involving existentials

---
+++ testsuite/tests/ghc-regress/deSugar/should_compile/ds055.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- This module requires quite trick desugaring,
+-- because of the context in the existentials
+-- It broke a pre 6.4 GHC
+
+module Foo where
+
+   import Data.Generics
+   import Data.HashTable
+
+   data Item = forall a. (Data a) => Leaf Bool a
+             | forall a. (Data a) => Branch Bool a Int Int
+	     deriving (Typeable)
+
+
+   instance Data Item where
+       gfoldl k z (Leaf b v) = z (Leaf b) `k` v
+       gfoldl k z (Branch b v a1 a2) = z (\x -> Branch b x a1 a2) `k` v
+       --gunfoldl k z c = case constrIndex c of
+       --                                    1 -> k z (Leaf undefined undefined)
+       toConstr (Leaf _ _) = leafConstr
+       toConstr (Branch _ _ _ _) = branchConstr
+       dataTypeOf _ = itemDataType
+
+   itemDataType = mkDataType "Subliminal.Item" [leafConstr, branchConstr]
+   leafConstr = mkConstr itemDataType "Leaf" [] Prefix
+   branchConstr = mkConstr itemDataType "Branch" [] Prefix
+
+
+
