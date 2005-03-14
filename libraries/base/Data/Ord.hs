16246ffadf148962473f5339d4c241f29be8bf26
simonmar <unknown>
2005-03-14 15:52:03 +0000

    [project @ 2005-03-14 15:52:03 by simonmar] doc comparing

--- libraries/base/Data/Ord.hs
+++ libraries/base/Data/Ord.hs
@@ -23,5 +23,12 @@ module Data.Ord (
 import GHC.Base
 #endif
 
+-- | 
+-- > comparing p x y = compare (p x) (p y)
+--
+-- Useful combinator for use in conjunction with the @xxxBy@ family
+-- of functions from "Data.List", for example:
+--
+-- >   ... sortBy (comparing fst) ...
 comparing :: (Ord a) => (b -> a) -> b -> b -> Ordering
 comparing p x y = compare (p x) (p y)
