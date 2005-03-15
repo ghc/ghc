98f34fe900bc9fe1b86e2c01345c00bf579fd0eb
simonmar <unknown>
2005-03-15 12:34:40 +0000

    [project @ 2005-03-15 12:34:40 by simonmar] new unicode test, and accept output

---
+++ testsuite/tests/ghc-regress/lib/Char/unicode002.hs
@@ -0,0 +1,44 @@
+module Main where
+
+
+import Char
+import Numeric
+
+header = "Code C P S U L A D"
+
+preds = [
+  isControl,
+  isPrint,
+  isSpace,
+  isUpper,
+  isLower,
+  isAlpha,
+  isDigit]
+
+prtBool :: Bool -> String
+
+prtBool True  = "T "
+prtBool False = "F "
+
+showCode :: Char -> Int -> String
+
+showCode c w = code ++ pad
+  where
+    code = show (ord c)
+    l = length code
+    spaces = map anytospace [1..]
+    anytospace _ = ' '
+    pad  | l >= w = ""
+         | otherwise = take (w - l) spaces
+
+charCode :: Char -> String
+
+rapply a b = b a
+
+charCode c = (showCode c 5) ++ (foldr1 (++) $ map prtBool $ map (rapply c) preds)
+
+main = do
+    putStrLn header
+    mapM (putStrLn . charCode) [ (chr 0) .. (chr 6553) ]
+
+  
