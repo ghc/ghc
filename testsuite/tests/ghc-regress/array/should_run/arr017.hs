7fcdd33d14c1747638af9626bbdd394ece3139dc
simonmar <unknown>
2005-05-04 15:13:25 +0000

    [project @ 2005-05-04 15:13:25 by simonmar] add test for array crash

---
+++ testsuite/tests/ghc-regress/array/should_run/arr017.hs
@@ -0,0 +1,30 @@
+-- Caused a crash in GHC 6.4 when optimising, due to inlining of runST too early.
+
+-- Spectral Norm benchmark
+
+import Array
+import System(getArgs)
+
+main = do
+        --[arg] <- getArgs
+        --let n = (read arg) - 1 
+        let n = 80
+        let init = listArray (0,n) (repeat 1.0)
+        let (v:u:rest) = drop 19 $ iterate (eval_AtA_times_u n) init
+        let vBv = sum [(u!i)*(v!i) |i<-[0..n]]
+        let vv  = sum [(v!i)*(v!i) |i<-[0..n]]
+        print $ sqrt (vBv/vv)
+
+eval_AtA_times_u n u = eval_At_times_u n v
+    where v = eval_A_times_u n u
+
+eval_A x y = 1.0/((i+j)*(i+j+1)/2+i+1)
+    where i = fromIntegral x
+          j = fromIntegral y
+
+eval_A_times_u n u = accumArray (+) 0 (0,n)
+                     [(i,(eval_A i j) * u!j)|i<-[0..n], j<-[0..n]]
+
+eval_At_times_u n u = accumArray (+) 0 (0,n)
+                      [(i,(eval_A j i) * u!j)|i<-[0..n], j<-[0..n]]
+ 
