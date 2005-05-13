dab0c2ed2e68fcfe30aaff4fffef82e56ec406df
simonmar <unknown>
2005-05-13 08:15:50 +0000

    [project @ 2005-05-13 08:15:50 by simonmar] make this test slightly more robust

--- testsuite/tests/ghc-regress/concurrent/should_run/conc036.hs
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc036.hs
@@ -1,18 +1,22 @@
 module Main where
 
 import Control.Concurrent
+import Control.Exception
+import Prelude hiding (catch)
+import Foreign
+import System.IO
 
 foreign import "sleep" unsafe sleepBlock :: Int -> IO ()
 
 main :: IO ()
 main = do
+  newStablePtr stdout -- prevent stdout being finalized, sigh
   th <- newEmptyMVar
   forkIO $ do
      putStrLn "newThread started"
-     sleepBlock 3
-     putStrLn "newThread back again"
-     putMVar th "5 secs later"
-  threadDelay 1000000 >> putStrLn "mainThread"
+     sleepBlock 1
+     putMVar th "child"
+  threadDelay 500000 >> putMVar th "main" `catch` \_ -> return ()
 	-- tests that the other thread doing an unsafe call to 
 	-- sleep(3) has blocked this thread.  Not sure if this
 	-- is a useful test.
