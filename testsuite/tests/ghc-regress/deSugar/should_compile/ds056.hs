f9471769e3ceb4992c09dd42dee90ba6f02db219
simonpj <unknown>
2005-08-22 08:59:15 +0000

    [project @ 2005-08-22 08:59:15 by simonpj] Slightly better overlap checking

---
+++ testsuite/tests/ghc-regress/deSugar/should_compile/ds056.hs
@@ -0,0 +1,12 @@
+-- Check overlap in n+k patterns
+
+module Foo where
+
+g :: Int -> Int
+g (x+1) = x
+g y     = y	
+g _     = 0	-- Overlapped
+
+h :: Int -> Int
+h (x+1) = x
+h _     = 0	-- Not overlapped
