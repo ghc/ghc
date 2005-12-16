06a9dacd26e489f199c9ad4b2b127e4bd908970e
simonpj <unknown>
2005-12-16 16:03:02 +0000

    [project @ 2005-12-16 16:03:02 by simonpj] Add deriving for infix constructors

---
+++ testsuite/tests/ghc-regress/deriving/should_run/drvrun018.hs
@@ -0,0 +1,10 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Test Show on unboxed types
+
+module Main where
+
+data Foo = Int `MkFoo` Int deriving( Read, Show )
+
+main = do { print (MkFoo 4 5)
+	  ; print (read "3 `MkFoo` 5" :: Foo) }
