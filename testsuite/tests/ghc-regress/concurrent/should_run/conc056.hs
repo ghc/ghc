7111d55908b1b7d80afc953057306b48a8c736c7
simonmar <unknown>
2006-01-05 09:16:28 +0000

    [project @ 2006-01-05 09:16:28 by simonmar] Add test for "scavenge_stack" bug fixed in rev 1.16 of Exception.cmm

---
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc056.hs
@@ -0,0 +1,24 @@
+-- Exposed a bug in 6.4.1, fixed in rev. 1.16 of ghc/rts/Exception.cmm
+
+import Network
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+import Control.Exception
+
+inc :: TVar Int -> STM ()
+inc tv = do
+  v <- readTVar tv
+  writeTVar tv (v + 1)
+
+bad :: MVar () -> IO ()
+bad m = do { connectTo "0.0.0.0" (Service "http"); return () }
+	 `finally` putMVar m ()
+
+main :: IO ()
+main = do
+  tv <- atomically (newTVar 0)
+  m <- newEmptyMVar
+  forkOS (sequence_ $ repeat $ atomically (inc tv))
+  forkOS (bad m)
+  takeMVar m
