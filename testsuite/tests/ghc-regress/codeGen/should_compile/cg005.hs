2cf530f24838bab54b94a370b81125d5a45b84a4
simonmar <unknown>
2006-01-09 10:29:44 +0000

    [project @ 2006-01-09 10:29:44 by simonmar] add a cmm lint failure

---
+++ testsuite/tests/ghc-regress/codeGen/should_compile/cg005.hs
@@ -0,0 +1,19 @@
+module Bug where
+
+import Foreign
+import Foreign.ForeignPtr
+import Data.Char
+
+data PackedString = PS !(ForeignPtr Word8) !Int !Int
+
+(!) :: PackedString -> Int -> Word8
+(PS x s _l) ! i
+    = unsafePerformIO $ withForeignPtr x $ \p -> peekElemOff p (s+i)
+
+w2c :: Word8 -> Char
+w2c = chr . fromIntegral
+
+indexPS :: PackedString -> Int -> Char
+indexPS theps i | i < 0 = error "Negative index in indexPS"
+                | otherwise = w2c $ theps ! i
+
