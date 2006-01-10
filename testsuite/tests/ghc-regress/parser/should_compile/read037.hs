d3b6d6b358c345ff8aee9fc3f4841786cc988427
simonmar <unknown>
2006-01-10 14:11:53 +0000

    [project @ 2006-01-10 14:11:53 by simonmar] comment update

--- testsuite/tests/ghc-regress/parser/should_compile/read037.hs
+++ testsuite/tests/ghc-regress/parser/should_compile/read037.hs
@@ -1,6 +1,6 @@
 module ShouldCompile where
 
--- This file contains several ISO 8859-1 non-breaking space characters,
+-- This file contains several non-breaking space characters,
 -- aka '\xa0'.  The compiler should recognise these as whitespace.
                           
 f = ( + ) 
