acd95bc04ac1800e07104ee37cd3dbcf28581cb5
simonpj <simonpj@microsoft.com>
2006-01-25 09:07:04 +0000

    Add test for bogus unusued-import message

---
+++ testsuite/tests/ghc-regress/rename/should_compile/rn048.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -W #-}
+-- Produced a bogus unused-import warning in versions of GHC 6.6 
+
+module Bug ( Structure (..) ) where
+
+import Data.Ratio ( Rational )
+
+data Structure a where
+   StructCons  :: Int   -> Structure Int
+   StructRatio ::          Structure Rational
+
+
