d8532a2ba3c9b1118fc4884fea8facc506332580
simonpj <simonpj@microsoft.com>
2006-02-01 17:14:51 +0000

    Update to track improvements in typechecker

--- testsuite/tests/ghc-regress/typecheck/should_compile/tc056.hs
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc056.hs
@@ -1,9 +1,6 @@
 -- !!! Duplicate class assertion warning
 
--- ghc 5.04 removes duplicate class assertions,
--- mainly so that for-all-hoisting doesn't give
--- such duplicates... so this test now compiles
--- without warnings
+-- ghc 6.6 now warns about duplicate class assertions,
 
 module ShouldSucceed where
 
