eeb22c33a15bc8b2b2c71739b15b0e39181dbbf9
wolfgang.thaller@gmx.net <unknown>
2006-03-06 03:48:20 +0000

    Use Darwin-compatible x86 assembly syntax in SMP.h (lock/cmpxchg with a slash)

--- ghc/includes/SMP.h
+++ ghc/includes/SMP.h
@@ -63,7 +63,7 @@ cas(StgVolatilePtr p, StgWord o, StgWord n)
 {
 #if i386_HOST_ARCH || x86_64_HOST_ARCH
     __asm__ __volatile__ (
- 	  "lock cmpxchg %3,%1"
+ 	  "lock/cmpxchg %3,%1"
           :"=a"(o), "=m" (*(volatile unsigned int *)p) 
           :"0" (o), "r" (n));
     return o;
