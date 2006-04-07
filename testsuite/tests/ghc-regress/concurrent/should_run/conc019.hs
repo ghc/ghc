a91d97bcf28dc4847303c12c4086b1852d2fe7d5
Simon Marlow <simonmar@microsoft.com>
2006-04-07 08:00:32 +0000

    avoid running out of stack for non-optimised ways

--- testsuite/tests/ghc-regress/concurrent/should_run/conc019.hs
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc019.hs
@@ -1,6 +1,7 @@
 import IO
 import Control.Concurrent
 import Control.Exception
+import Data.List
 
 -- !!! test that a child thread waiting on its own MVar will get killed by
 -- a signal.
@@ -8,5 +9,5 @@ import Control.Exception
 main = do
   forkIO (Control.Exception.catch (do { m <- newEmptyMVar; takeMVar m })
 		          (\e -> putStrLn ("caught: " ++ show e)))
-  let x = sum [1..500000]
+  let x = foldl' (+) 0 [1..500000]
   x `seq` print x
