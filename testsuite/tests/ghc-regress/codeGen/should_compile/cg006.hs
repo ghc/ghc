296011e4f2b7077f71f2afa6c6279d6861f127bd
Simon Marlow <simonmar@microsoft.com>
2006-06-06 11:26:14 +0000

    test for NCG bug

---
+++ testsuite/tests/ghc-regress/codeGen/should_compile/cg006.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS_GHC -cpp -fglasgow-exts #-}
+module ShouldCompile where
+
+-- exposed a bug in the NCG in 6.4.2
+import GHC.Base
+class Unboxable a where
+    writeUnboxable :: MutableByteArray# RealWorld -> a -> State# RealWorld -> State# RealWorld
+    writeUnboxable arr a s  =  writeInt8Array# arr 0# (getTag 0) s
