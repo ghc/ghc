42a28f9df94148e713f34e229f5a636ca203f84b
Simon Marlow <simonmar@microsoft.com>
2006-06-20 08:34:10 +0000

    add decl for stg_block_throwto_ret

--- includes/StgMiscClosures.h
+++ includes/StgMiscClosures.h
@@ -491,6 +491,7 @@ RTS_ENTRY(stg_block_async_void_ret);
 #endif
 RTS_FUN(stg_block_stmwait);
 RTS_FUN(stg_block_throwto);
+RTS_ENTRY(stg_block_throwto_ret);
 RTS_RET_INFO(stg_block_throwto_info);
 
 /* Entry/exit points from StgStartup.cmm */
