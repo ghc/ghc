9b44c3dc814f34950db4f6178f22f7810e3b1b75
simonpj <simonpj@microsoft.com>
2006-08-08 09:15:17 +0000

    Add tc206; edit a couple of others

--- testsuite/tests/ghc-regress/gadt/gadt-dim6.hs
+++ testsuite/tests/ghc-regress/gadt/gadt-dim6.hs
@@ -4,10 +4,10 @@ module ShouldSucceed3 where
 
 
 data T a where 
-  C :: (T b) -> b -> T Int 
+  C :: T b -> b -> T Int 
   D :: T Bool 
 
 -- Tests scoped annotations 
 foo :: T a -> a -> a 
-foo (C (x::b)) (z::a) = z + 1 
+foo (C y (x::b)) (z::a) = z + 1 
 
