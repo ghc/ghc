12054ae15c7e43ea99a596564a875c5454349d81
simonpj <simonpj@microsoft.com>
2006-08-11 13:11:32 +0000

    Update test outpuot

--- testsuite/tests/ghc-regress/typecheck/should_fail/tcfail113.hs
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail113.hs
@@ -1,5 +1,10 @@
 -- Tests kind error messages
 
+-- GHC 6.6 stops on the first kind error it encounters,
+-- so we only get one error report here.  I'll leave 
+-- the other tests in here, though, in case matters improve
+-- again
+
 module ShouldFail where
 
 data T k = T (k Int)
