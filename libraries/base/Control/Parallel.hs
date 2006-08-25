e2c372fe367ba3cb563268f51e44ca5a7a08c63a
Simon Marlow <simonmar@microsoft.com>
2006-08-25 11:06:10 +0000

    add docs for par

--- libraries/base/Control/Parallel.hs
+++ libraries/base/Control/Parallel.hs
@@ -22,7 +22,7 @@ module Control.Parallel (
 import Prelude
 
 #ifdef __GLASGOW_HASKELL__
-import GHC.Conc	( par )
+import qualified GHC.Conc	( par )
 #endif
 
 #if defined(__GRANSIM__)
@@ -54,7 +54,27 @@ parAtForNow (I# w) (I# g) (I# s) (I# p) v x y = case (parAtForNow# x v w g s p y
 #endif
 
 -- Maybe parIO and the like could be added here later.
-#ifndef __GLASGOW_HASKELL__
+
+-- | Indicates that it may be beneficial to evaluate the first
+-- argument in parallel with the second.  Returns the value of the
+-- second argument.
+-- 
+-- @a `par` b@ is exactly equivalent semantically to @b@.
+--
+-- @par@ is generally used when the value of @a@ is likely to be
+-- required later, but not immediately.  Also it is a good idea to
+-- ensure that @a@ is not a trivial computation, otherwise the cost of
+-- spawning it in parallel overshadows the benefits obtained by
+-- running it in parallel.
+--
+-- Note that actual parallelism is only supported by certain
+-- implementations (GHC with the @-threaded@ option, and GPH, for
+-- now).  On other implementations, @par a b = b@.
+--
+par :: a -> b -> b
+#ifdef __GLASGOW_HASKELL__
+par = GHC.Conc.par
+#else
 -- For now, Hugs does not support par properly.
 par a b = b
 #endif
