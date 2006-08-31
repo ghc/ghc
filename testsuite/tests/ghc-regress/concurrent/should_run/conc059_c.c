2f092d404b293d93eccb51733adf029e39e8ecba
Simon Marlow <simonmar@microsoft.com>
2006-08-31 13:23:32 +0000

    fix for Windows

--- testsuite/tests/ghc-regress/concurrent/should_run/conc059_c.c
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc059_c.c
@@ -2,14 +2,25 @@
 #include "conc059_stub.h"
 #include <unistd.h>
 #include <stdio.h>
+#if mingw32_HOST_OS
+#include <windows.h>
+#endif
 
 int main(int argc, char *argv[])
 {
     hs_init(&argc,&argv);
     f(500000);
+#if mingw32_HOST_OS
+    Sleep(100);
+#else
     usleep(100000);
+#endif
     printf("exiting...\n");
     hs_exit();
     printf("exited.\n");
+#if mingw32_HOST_OS
+    Sleep(1000);
+#else
     usleep(1000000);
+#endif
 }
