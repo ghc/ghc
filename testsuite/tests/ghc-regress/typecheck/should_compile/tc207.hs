ae241b61638e65c0b5828a634a25f69da11e9ff0
simonpj <simonpj@microsoft.com>
2006-09-05 10:54:49 +0000

    Add test for dependency analysis in type checking

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc207.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Tests enhanced polymorphism
+
+module ShouldCompile where
+
+foo xs = let 
+	   f :: Eq a => [a] -> [a]
+	   f [] = []
+	   f xs | null (g [True]) = [] 
+		| otherwise       = tail (g xs) 
+
+	   g :: Eq b => [b] -> [b]
+	   g [] = []
+	   g xs | null (f "hello") = [] 
+		| otherwise       = tail (f xs) 
+ 	in f xs
