4ef6b43d8aa140bcdaef95276efdb10f01e78677
simonpj <simonpj@microsoft.com>
2006-09-07 07:56:56 +0000

    Tickle a bug in impredicative polymorphism

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc208.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS -fglasgow-exts -fimplicit-params #-}
+
+-- This program failed to typecheck in an early version of
+-- GHC with impredicative polymorphism, but it was fixed by
+-- doing pre-subsumption in the subsumption check.
+-- Trac bug #821
+
+module ShouldCompile where
+
+type PPDoc = (?env :: Int) => Char
+
+f :: Char -> PPDoc
+f  = succ 
