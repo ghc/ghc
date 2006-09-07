9d80a57ba08fbda42ce82f81b6af4ae554bc56a9
Ian Lynagh <igloo@earth.li>
2006-09-07 11:30:01 +0000

    Use a python timeout for the testsuite when we don't have a threaded RTS

---
+++ testsuite/timeout/timeout.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+import signal
+import sys
+
+secs = int(sys.argv[1])
+cmd = sys.argv[2]
+
+pid = os.fork()
+# XXX error checking
+if pid == 0:
+    # child
+    os.setpgrp()
+    os.execvp('/bin/sh', ['/bin/sh', '-c', cmd])
+else:
+    # parent
+    def handler(signum, frame):
+        os.killpg(pid, signal.SIGKILL) # XXX Kill better like .hs
+        sys.exit(99)
+    old = signal.signal(signal.SIGALRM, handler)
+    signal.alarm(secs)
+    (pid2, res) = os.waitpid(pid, 0)
+    if (os.WIFEXITED(res)):
+        sys.exit(os.WEXITSTATUS(res))
+    else:
+        sys.exit(res)
+
