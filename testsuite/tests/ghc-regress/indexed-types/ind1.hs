31ffbaf3cb13e87c5a91e047c8cb86ee6a7d545d
simonpj <simonpj@microsoft.com>
2006-09-23 08:03:30 +0000

    Add new sub-directory indexed-types to the GHC testsuite

---
+++ testsuite/tests/ghc-regress/indexed-types/ind1.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS -findexed-types #-}
+
+-- Test type families
+
+module ShouldCompile where
+
+data family T a :: *
+
+data instance T Bool = TBool !Bool
+
+class C a where
+  foo :: (a -> a) -> T a -> T a
+
+instance C Bool where
+  foo f (TBool x) = TBool $ f (not x)
