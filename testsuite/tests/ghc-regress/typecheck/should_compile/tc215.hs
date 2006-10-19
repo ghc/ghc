81ea2c04dd97f12297d01aa4e2dc338b9346f38a
Ian Lynagh <igloo@earth.li>
2006-10-19 18:24:40 +0000

    Tests for trac #366 (incomplete pattern warnings and GADTs)

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc215.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns -fglasgow-exts #-}
+
+-- Test for trac #366
+-- The C2 case is impossible due to the types
+
+module ShouldCompile where
+
+data T a where
+    C1 :: T Char
+    C2 :: T Float
+
+exhaustive :: T Char -> Char
+exhaustive C1 = ' '
+
---
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail167.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns -fglasgow-exts #-}
+
+-- Test inspired by trac #366
+-- The C2 case is impossible due to the types
+
+module ShouldCompile where
+
+data T a where
+    C1 :: T Char
+    C2 :: T Float
+
+inaccessible :: T Char -> Char
+inaccessible C1 = ' '
+inaccessible C2 = ' '
+
