10db1142747ca19617574d2880f9fb16de33be0e
Ian Lynagh <igloo@earth.li>
2006-10-20 16:54:49 +0000

    Add test countReaders001 for trac #629 (file locking doesn't count readers)

---
+++ testsuite/tests/ghc-regress/lib/IO/countReaders001.hs
@@ -0,0 +1,16 @@
+-- test for trac #629. We need to keep track of how many readers
+-- there are rather than closing the first read handle causing the
+-- lock to be released.
+
+import IO
+
+file = "countReaders001.txt"
+
+main = do
+  writeFile file "foo"
+
+  h1 <- openFile file ReadMode
+  h2 <- openFile file ReadMode
+  hClose h1
+  try (openFile file AppendMode) >>= print
+
