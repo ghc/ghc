9939d54e459daa9e3f89f7cf49dcbf60635e87bc
simonpj <simonpj@microsoft.com>
2006-11-10 14:04:07 +0000

    New GADT tests, looking at interaction with type classes

--- testsuite/tests/ghc-regress/gadt/josef.hs
+++ testsuite/tests/ghc-regress/gadt/josef.hs
@@ -22,29 +22,29 @@ class Category arr where
   comp :: arr a b -> arr b c -> arr a c
 
 class Category arr =>
-   Terminal terminal arr {- | arr -> terminal -} where
+   Terminal terminal arr | arr -> terminal where
   terminal :: arr a terminal
 
 class Category arr =>
-      ProductCategory prod arr {- | arr -> prod -} where
+      ProductCategory prod arr | arr -> prod  where
   first  :: arr (prod a b) a
   second :: arr (prod a b) b
   pair   :: arr a b -> arr a c -> arr a (prod b c)
 
 class Category arr =>
-      CoproductCategory coprod arr {- | arr -> coprod -} where
+      CoproductCategory coprod arr | arr -> coprod where
   inLeft  :: arr a (coprod a b)
   inRight :: arr b (coprod a b)
   ccase   :: arr a c -> arr b c -> arr (coprod a b) c
 
 class ProductCategory prod arr => 
-      Exponential exp prod arr {- | arr -> exp -} where
+      Exponential exp prod arr | arr -> exp where
   eval   :: arr (prod (exp a b) a) b
   curryA :: arr (prod c a) b -> arr c (exp a b)
 
 
 class (Exponential exp prod arr, Terminal terminal arr) => 
-  CartesianClosed terminal exp prod arr {- | arr -> terminal exp prod -}
+  CartesianClosed terminal exp prod arr | arr -> terminal exp prod 
 
 data V prod env t where
   Z :: V prod (prod env t) t
