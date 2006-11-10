44266ca1bed9eebb97fbe796a2cafb1e34fcc951
simonpj <simonpj@microsoft.com>
2006-11-10 13:59:42 +0000

    Two new typechecker tests that exploit implication constraints

---
+++ testsuite/tests/ghc-regress/typecheck/should_run/TcRun038_B.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module TcRun038_B where
+
+class Foo a where
+  op :: a -> Int
+
+-- Note the (Foo Int) constraint here; and the fact
+-- that there is no (Foo Int) instance in this module
+-- It's in the importing module!
+
+bar :: Foo Int => Int -> Int
+bar x = op x + 7
---
+++ testsuite/tests/ghc-regress/typecheck/should_run/tcrun039.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Test for GADTs and implication constraints
+
+module Main where
+
+data T a where
+  MkT :: Num a => a -> T a
+
+f :: Read a => T a -> String -> a
+f (MkT n) s = n + read s
+
+----------------
+data GADT a where
+  MkG :: Num a => a -> GADT [a]
+
+g :: forall b. Read b => GADT b -> String -> b
+g (MkG n) s = -- Here we know Read [b]
+	      n : (read s)
+
+main = do print (f (MkT (3::Int)) "4")
+          print (g (MkG (3::Int)) "[4,5]")
