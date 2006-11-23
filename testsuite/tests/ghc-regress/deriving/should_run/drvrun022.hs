bd72fec23e6c00ce5fac38e845d1dd9da2a93300
simonpj <simonpj@microsoft.com>
2006-11-23 08:48:05 +0000

    Test that deriving Data does not get confused with z-encoding

---
+++ testsuite/tests/ghc-regress/deriving/should_run/drvrun022.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module Main where
+
+-- GHC 6.4.1 output "testz" in z-encoded form!
+
+import Data.Generics
+
+data TestZ = TestZ { testz :: Int }
+             deriving (Show, Read, Eq, Data, Typeable)
+
+main = print $ constrFields . toConstr $ TestZ { testz = 2 }
