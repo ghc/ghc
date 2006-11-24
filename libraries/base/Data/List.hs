0c9925397ef5e2da09d3d1183cc5a48beb493de5
Don Stewart <dons@cse.unsw.edu.au>
2006-11-24 01:12:49 +0000

    Add an example of the use of unfoldr, following doc feedback from dozer

--- libraries/base/Data/List.hs
+++ libraries/base/Data/List.hs
@@ -855,7 +855,12 @@ rqpart cmp x (y:ys) rle rgt r =
 --
 -- > f' (f x y) = Just (x,y)
 -- > f' z       = Nothing
-
+--
+-- A simple use of unfoldr:
+--
+-- > unfoldr (\b -> if b == 0 then Nothing else Just (b, b-1)) 10
+-- >  [10,9,8,7,6,5,4,3,2,1]
+--
 unfoldr      :: (b -> Maybe (a, b)) -> b -> [a]
 unfoldr f b  =
   case f b of
