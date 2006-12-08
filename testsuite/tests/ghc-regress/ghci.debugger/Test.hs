e954c4d1f33078cf1543ddccea0145c3c12ade85
pepeiborra <pepeiborra@gmail.com>
2006-12-08 17:16:20 +0000

    Added more tests with newtypes

--- testsuite/tests/ghc-regress/ghci.debugger/Test.hs
+++ testsuite/tests/ghc-regress/ghci.debugger/Test.hs
@@ -13,6 +13,18 @@ data List1 a = Nil | a :^ (List1 a)
 newtype MyInt = My Int
   deriving (Eq,Show,Num, Enum)
 
+newtype MkT a = MkT a
+  deriving (Show)
+
+data MkT2 a = MkT2 (MkT a)
+  deriving Show
+
+data Param2 s r = P2 (FakeSTRef r (s(Param2 s r)))
+		| P2Nil
+data FakeSTRef r s = Ref s
+
+testParam2 = O (P2 (Ref P2Nil))
+
 infixr 5 :^ 
 --test T{t=t1} = undefined
 
