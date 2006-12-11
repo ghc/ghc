bc6baf629ff9152737c3f92ac960b5684eb276c7
simonpj <simonpj@microsoft.com>
2006-12-11 16:36:39 +0000

    Add kind-error test inspired by a message from David Roundy

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc221.hs
@@ -0,0 +1,16 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- A program very like this triggered a kind error with GHC 6.6
+
+module Foo where
+
+data PatchSeq p a b where
+    Nil   :: PatchSeq p a b
+    U  :: p a b -> PatchSeq p a b
+    (:-)  :: PatchSeq p a b -> PatchSeq p b c -> PatchSeq p a c
+
+-- is_normal :: PatchSeq p a b -> Bool 
+is_normal Nil = True 
+is_normal (U _) = True 
+is_normal (U _ :- _) = True 
+is_normal _ = False 
---
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail172.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- This one made GHC 6.6 give the very unhelpful error
+-- Foo8.hs:11:10:
+--    Couldn't match kind `?' against `* -> * -> *'
+--    When matching the kinds of `t :: ?' and `t1 :: * -> * -> *'
+--      Expected type: t1
+--      Inferred type: t
+--    In the pattern: Nil
+
+module ShouldFail where
+
+data PatchSeq p a b where
+    Nil   :: PatchSeq p a a
+    U  :: p a b -> PatchSeq p a b
+    (:-)  :: PatchSeq p a b -> PatchSeq p b c -> PatchSeq p a c
+
+-- is_normal :: PatchSeq p a b -> Bool 
+is_normal Nil = True 
+is_normal (U _) = True 
+is_normal (U _ :- _) = True 
+is_normal _ = False 
