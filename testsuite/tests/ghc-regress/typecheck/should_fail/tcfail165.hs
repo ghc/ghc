34de065b4cc8c747e513935419435f0224a2993c
simonpj <simonpj@microsoft.com>
2006-12-13 16:46:01 +0000

    Accept output

--- testsuite/tests/ghc-regress/typecheck/should_fail/tcfail165.hs
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail165.hs
@@ -6,6 +6,8 @@ import Control.Concurrent
 
 -- Attempt to put a polymorphic value in an MVar
 -- Fails, but the error message is worth keeping an eye on
+--
+-- Actually (Dec 06) it succeeds now
 
 foo = do var <- newEmptyMVar :: IO (MVar (forall a. Show a => a -> String))
          putMVar var (show :: forall a. Show a => a -> String)
