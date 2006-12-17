498ef5febf5ddc0100ae1343a652387c49d2a020
Pepe Iborra <mnislaih@gmail.com>
2006-12-17 00:46:17 +0000

    Fix a ghci.debugger test

--- testsuite/tests/ghc-regress/ghci.debugger/dynbk5/TupleN.hs
+++ testsuite/tests/ghc-regress/ghci.debugger/dynbk5/TupleN.hs
@@ -4,7 +4,7 @@ import Language.Haskell.TH
 tuple :: Int -> ExpQ
 tuple n = [|\list -> $(tupE (exprs [|list|])) |]
   where
-    exprs list = [infixE (Just (list))
-                         (varE "!!")
-                         (Just (litE $ integerL (toInteger num)))
-                    | num <- [0..(n - 1)]]
+    exprs list = id [infixE (Just (list))
+                           (varE '(!!))
+                           (Just (litE $ integerL (toInteger num)))
+                      | num <- [0..(n - 1)]]
