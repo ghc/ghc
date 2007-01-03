1de8a034286f570de9589160e62bb5931db346e6
simonpj <simonpj@microsoft.com>
2007-01-03 11:36:57 +0000

    Add more GADT tests, this time concerning contexts in data constructors

---
+++ testsuite/tests/ghc-regress/gadt/gadt21.hs
@@ -0,0 +1,23 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+-- Fails (needs the (Ord a) in TypeSet 
+-- c.f. gadt22.hs
+
+module Expr where
+
+import Data.Set (Set)
+
+data Type a where
+    TypeInt     :: Type Int
+    TypeSet     :: {- Ord a => -} Type a -> Type (Set a)
+    TypeFun     :: Type a -> Type b -> Type (a -> b)
+
+data Expr :: * -> * where
+    Const :: Type a -> a -> Expr a
+
+data DynExpr = forall a. DynExpr (Expr a)
+
+withOrdDynExpr :: DynExpr -> (forall a. Ord a => Expr a -> b) -> Maybe b
+withOrdDynExpr (DynExpr e@(Const (TypeSet _) _)) f = Just (f e)
+withOrdDynExpr (DynExpr e@(Const TypeInt _)) f = Just (f e)
+withOrdDynExpr _ _ = Nothing
