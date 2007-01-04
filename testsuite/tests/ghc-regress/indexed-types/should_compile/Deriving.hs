a81e8065dbab3f36bf6cd7c076824f1a13c39c00
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2007-01-04 01:30:49 +0000

    indexed types: extend Deriving to cover standalone derive

--- testsuite/tests/ghc-regress/indexed-types/should_compile/Deriving.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/Deriving.hs
@@ -5,7 +5,7 @@ module ShouldCompile where
 data family T a
 
 data instance T Int = A | B
-		      deriving Eq
+		    deriving Eq
 
 foo :: T Int -> Bool
 foo x = x == x
@@ -19,9 +19,14 @@ instance Eq (T Char) where
 newtype family S a
 
 newtype instance S Int = S Int
-		         deriving Eq
+		       deriving Eq
 
 newtype family S2 a b
 
 newtype instance S2 Int b = S2 (IO b)
 		          deriving Monad
+
+data family R a
+data instance R [a] = R
+
+derive instance Eq (R [a])
