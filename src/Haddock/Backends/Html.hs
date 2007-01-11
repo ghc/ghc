cd09eedb7eee2c3c0b0084c8ad0f79a4674ed56a
Neil Mitchell <http://www.cs.york.ac.uk/~ndm/>
2007-01-11 18:21:19 +0000

    Delete more stuff that is no longer required

--- src/Haddock/Backends/Html.hs
+++ src/Haddock/Backends/Html.hs
@@ -484,8 +484,8 @@ ppHtmlIndex odir doctitle maybe_package maybe_html_help_format
 	[ if visible then
 	     linkId mod (Just nm) << toHtml (moduleString mod)
 	  else
-	     toHtml (moduleString mod)
-	| (mod, visible) <- entries ])
+	     toHtml mdl
+	| (Module mdl, visible) <- entries ])
 
   initialChars = [ 'A'..'Z' ] ++ ":!#$%&*+./<=>?@\\^|-~"
 
