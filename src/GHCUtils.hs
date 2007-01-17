0ea1e14f473dabe75f9e355dc7d2debeffb0fafc
davve <davve@dtek.chalmers.se>
2007-01-17 21:40:26 +0000

    Add trace_ppr to GHCUtils

--- src/GHCUtils.hs
+++ src/GHCUtils.hs
@@ -1,7 +1,12 @@
 module GHCUtils where
 
+import Debug.Trace
+
+import GHC
 import HsSyn
 import SrcLoc
+import HscTypes
+import Outputable
 
 getMainDeclBinder :: HsDecl name -> Maybe name
 getMainDeclBinder (TyClD d) = Just (tcdName d)
@@ -13,3 +18,8 @@ getMainDeclBinder (SigD d) = sigNameNoLoc d
 getMainDeclBinder (ForD (ForeignImport name _ _)) = Just (unLoc name)
 getMainDeclBinder (ForD (ForeignExport name _ _)) = Nothing
 getMainDeclBinder _ = Nothing
+
+modInfoName = moduleName . mi_module . minf_iface
+modInfoMod  = mi_module . minf_iface 
+
+trace_ppr x y = trace (showSDoc (ppr x)) y
