8f14bd6b3d2e3296cc4b067131b90faa1bf4a3ec
Simon Marlow <simonmar@microsoft.com>
2007-03-05 14:53:43 +0000

    Posix tests moved to the unix package

--- testsuite/tests/ghc-regress/lib/Posix/fileexist01.hs
+++
@@ -1,5 +0,0 @@
--- test System.Posix.fileExist
-import System.Posix
-main = do
-  fileExist "fileexist01.hs" >>= print
-  fileExist "does not exist" >>= print
--- testsuite/tests/ghc-regress/lib/Posix/forkprocess01.hs
+++
@@ -1,9 +0,0 @@
--- Test that we can call exitFailure in a forked process, and have it
--- communicated properly to the parent.
-import System.Exit
-import System.Posix.Process
-main = do
-  p <- forkProcess exitFailure
-  r <- getProcessStatus True False p
-  print r
-
