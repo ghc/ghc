b28f70bfb3557f96b41c36c19a8213287b463de7
nx200 <nx200@cam.ac.uk>
2007-03-09 17:22:01 +0000

    added contract synonym test case

--- testsuite/tests/ghc-regress/esc/head.hs
+++ testsuite/tests/ghc-regress/esc/head.hs
@@ -6,10 +6,10 @@ module Head where
 and1 True x = x
 and1 False x = False
 
-{-# CONTRACT head1 :: {xs | not (null xs)} -> any #-}
+{-# CONTRACT head1 :: {xs | not (null xs)} -> {r | not (null r)} #-}
 -- {-# CONTRACT head1 :: {xs | not1 (null1 xs)} -> {r | r > 0} #-}
-head1 :: [Int] -> Int
-head1 (x:xs) = x
+head1 :: [Int] -> [Int]
+head1 (x:xs) = [x]
 
 not1 True = False
 not1 False = True
@@ -17,9 +17,15 @@ not1 False = True
 null1 [] = True
 null1 xs = False
 
-res1 = and1 True True
+{-# CONTRACT f :: {x | x > 0 && x < 5} -> any #-}
+f :: Int -> Int
+f x = x + 1
+
 
 {-# CONTRACT res2 :: any #-}
 res2 = head1 [5]
 
+res3 = head1 [6]
+
+res4 = head1 []
 
