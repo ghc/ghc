afce490a6962b70d3dd535da6aacad22d927c9e1
nx200 <nx200@cam.ac.uk>
2007-03-27 13:52:14 +0000

    added more examples

--- testsuite/tests/ghc-regress/esc/head.hs
+++ testsuite/tests/ghc-regress/esc/head.hs
@@ -6,10 +6,10 @@ module Head where
 and1 True x = x
 and1 False x = False
 
-{-# CONTRACT head1 :: {xs | not (null xs)} -> {r | not (null r)} #-}
+{-# CONTRACT head1 :: {xs | not1 (null1 xs)} -> {x | True} #-}
 -- {-# CONTRACT head1 :: {xs | not1 (null1 xs)} -> {r | r > 0} #-}
-head1 :: [Int] -> [Int]
-head1 (x:xs) = [x]
+head1 :: [Int] -> Int
+head1 (x:xs) = x
 
 not1 True = False
 not1 False = True
@@ -17,7 +17,7 @@ not1 False = True
 null1 [] = True
 null1 xs = False
 
-{-# CONTRACT f :: {x | x > 0 && x < 5} -> any #-}
+{-# CONTRACT f :: {x | x > 0} -> any #-}
 f :: Int -> Int
 f x = x + 1
 
@@ -25,7 +25,10 @@ f x = x + 1
 {-# CONTRACT res2 :: any #-}
 res2 = head1 [5]
 
+{-# CONTRACT res3 :: {x | True} #-}
 res3 = head1 [6]
+
+res5 = head1 [2]
 
 res4 = head1 []
 
