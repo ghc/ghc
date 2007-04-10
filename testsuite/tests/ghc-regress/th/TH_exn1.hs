6f14d73a3a9982f9a09236a852cca0d7046602d9
Tyson Whitehead <twhitehe@uwo.ca>
2007-04-10 14:03:08 +0000

    Differentiate between use of fail and failure of code (i.e., an exception is thrown)

--- testsuite/tests/ghc-regress/th/TH_exn1.hs
+++ testsuite/tests/ghc-regress/th/TH_exn1.hs
@@ -1,14 +1,11 @@
 {-# OPTIONS_GHC -fth #-}
 
 -- Test error message when the code in a splice
--- fails (e.g. with pattern match failure)
+-- fails in an immediate fashion (e.g. with a
+-- pattern match failure)
 
 module ShouldCompile where
 
 $( case reverse "no" of
 	[] -> return []
  )
-
-
-
-
