11a6f10c72a16c079e6fabe461d1aa479bc53f1f
Simon Marlow <simonmar@microsoft.com>
2007-05-21 14:17:45 +0000

    FIX BUILD: GHC doesn't use filepath (yet)

--- compiler/ghci/InteractiveUI.hs
+++ compiler/ghci/InteractiveUI.hs
@@ -70,7 +70,6 @@ import System.Exit	( exitWith, ExitCode(..) )
 import System.Directory
 import System.IO
 import System.IO.Error as IO
-import System.FilePath
 import Data.Char
 import Data.Dynamic
 import Data.Array
@@ -1794,7 +1793,7 @@ listModuleLine modl line = do
 -- start_bold/end_bold.
 listAround span do_highlight = do
       pwd      <- getEnv "PWD" 
-      contents <- BS.readFile (pwd </> unpackFS file)
+      contents <- BS.readFile (pwd `joinFileName` unpackFS file)
       let 
           lines = BS.split '\n' contents
           these_lines = take (line2 - line1 + 1 + pad_before + pad_after) $ 
