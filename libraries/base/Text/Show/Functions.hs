bbea32c86b1c041db84d8d86f43796c1988f2884
Malcolm.Wallace@cs.york.ac.uk <unknown>
2007-05-24 16:06:15 +0000

    nhc98 version of instance Show (a->b) copied from Prelude

--- libraries/base/Text/Show/Functions.hs
+++ libraries/base/Text/Show/Functions.hs
@@ -22,4 +22,12 @@ import Prelude
 #ifndef __NHC__
 instance Show (a -> b) where
 	showsPrec _ _ = showString "<function>"
+#else
+instance (Show a,Show b) => Show (a->b) where
+  showsPrec d a = showString "<<function>>"
+
+  showsType a = showChar '(' . showsType value  . showString " -> " .
+                               showsType result . showChar ')'
+                where (value,result) = getTypes undefined
+                      getTypes x = (x,a x)
 #endif
