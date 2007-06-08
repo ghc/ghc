2a798464aaa5f8213bbb2dea9e61de5c87e0d3ef
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2007-06-08 04:42:50 +0000

    Split newtype deriving test from other deriving tests

--- testsuite/tests/ghc-regress/indexed-types/should_compile/Deriving.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/Deriving.hs
@@ -15,17 +15,6 @@ data instance T Char = C
 instance Eq (T Char) where
   C == C = False
 
-
-data family S a
-
-newtype instance S Int = S Int
-		       deriving Eq
-
-data family S2 a b
-
-newtype instance S2 Int b = S2 (IO b)
-		          deriving Monad
-
 data family R a
 data instance R [a] = R
 
---
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/DerivingNewType.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -ftype-families -fglasgow-exts #-}
+
+module ShouldCompile where
+
+data family S a
+
+newtype instance S Int = S Int
+		       deriving Eq
+
+data family S2 a b
+
+newtype instance S2 Int b = S2 (IO b)
+		          deriving Monad
+
