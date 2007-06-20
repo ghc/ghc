a2ec087563849c93393306d62b796d72979b7b20
simonpj <simonpj@microsoft.com>
2007-06-20 10:25:22 +0000

    tcrun032 was using an un-documented (and now not implemented) feature of 'deriving'

--- testsuite/tests/ghc-regress/typecheck/should_run/tcrun032.hs
+++ testsuite/tests/ghc-regress/typecheck/should_run/tcrun032.hs
@@ -1,13 +1,18 @@
 {-# OPTIONS -fglasgow-exts -fallow-undecidable-instances #-}
 
 -- This tests the recursive-dictionary stuff.
---
--- The derived instance needs 
 
 module Main where
 
-data Fix f = In (f (Fix f))  deriving( Show, Eq )
-data L x = Nil | Cons Int x  deriving (Show, Eq)
+data Fix f = In (f (Fix f)) 
+
+instance Show (f (Fix f)) => Show (Fix f) where
+  show (In x) = "In " ++ show x	-- No parens, but never mind
+
+instance Eq (f (Fix f)) => Eq (Fix f) where
+  (In x) == (In y) = x==y
+
+data L x = Nil | Cons Int x  deriving( Show, Eq )
 
 main = do { print (In Nil); 
 	    print (In Nil == In Nil) }
