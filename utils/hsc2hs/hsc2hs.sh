d7df1559b3f44603e78062e9eed5c13a453aa7f4
Simon Marlow <simonmar@microsoft.com>
2007-06-22 07:56:02 +0000

    remove unnecessary cruft

--- utils/hsc2hs/hsc2hs.sh
+++ utils/hsc2hs/hsc2hs.sh
@@ -1,13 +1,2 @@
-
 tflag="--template=$HSC2HS_DIR/template-hsc.h"
-for arg do
-    case "$arg" in
-        -c*)    HSC2HS_EXTRA=;;
-        --cc=*) HSC2HS_EXTRA=;;
-	-t*)	tflag=;;
-	--template=*) tflag=;;
-        --)     break;;
-    esac
-done
-
 $HSC2HS_BINDIR/$HS_PROG $tflag $HSC2HS_EXTRA "$@" 
