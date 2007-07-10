5cfe19399eb6fb90fff530e4565ddc436bbf0d22
Ian Lynagh <igloo@earth.li>
2007-07-10 15:21:55 +0000

    Follow error message changes

--- testsuite/tests/ghc-regress/typecheck/should_fail/tcfail117.hs
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail117.hs
@@ -1,6 +1,6 @@
 module ShouldFail where
 
--- Without -fglasgow-exts both of these should fail
+-- Without any extensions enabled both of these should fail
 
 newtype N1 = N1 Int deriving ( Enum )
 data    N2 = N2 Int deriving ( Enum )
