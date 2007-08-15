db7a768882134defdd282bd626b29bead6e62b00
Ian Lynagh <igloo@earth.li>
2007-08-15 17:27:07 +0000

    Use a C program for ghc-inplace

---
+++ compiler/ghc-inplace.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+    char **args;
+    args = malloc(sizeof(char *) * (argc + 3));
+    if (args == NULL) {
+        fprintf(stderr, "Malloc failed\n");
+        exit(1);
+    }
+    args[0] = GHC_PATH;
+    args[1] = "-B" TOP_ABS;
+    args[2] = "-fhardwire-lib-paths";
+    memcpy(args + 3, argv + 1, sizeof(char *) * (argc - 1));
+    args[argc+2] = NULL;
+    execv(GHC_PATH, args);
+    return 0;
+}
