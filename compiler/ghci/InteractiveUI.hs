552eb456fcaeee5b69ec70555b5a269dd7daf535
Pepe Iborra <mnislaih@gmail.com>
2007-08-16 09:49:44 +0000

    Document :stepover in ghci help

--- compiler/ghci/InteractiveUI.hs
+++ compiler/ghci/InteractiveUI.hs
@@ -186,6 +186,7 @@ helpText =
  "   :sprint [<name> ...]        simplifed version of :print\n" ++
  "   :step                       single-step after stopping at a breakpoint\n"++
  "   :step <expr>                single-step into <expr>\n"++
+ "   :stepover                   single-step without following function applications\n"++
  "   :trace                      trace after stopping at a breakpoint\n"++
  "   :trace <expr>               trace into <expr> (remembers breakpoints for :history)\n"++
 
