b141b9823cd1213868be408b98b0895699f8b0a9
David Waern <unknown>
2007-08-17 11:28:28 +0000

    Change project name from Haddock-GHC to Haddock

--- src/Haddock/Version.hs
+++ src/Haddock/Version.hs
@@ -12,7 +12,7 @@ import Paths_haddock ( version )
 import Data.Version  ( showVersion )
 
 projectName, projectUrl :: String
-projectName = "Haddock-GHC"
-projectUrl = "http://www.haskell.org/haddock/"
+projectName = "Haddock"
+projectUrl  = "http://www.haskell.org/haddock/"
 
 projectVersion = showVersion version
