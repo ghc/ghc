bbf274d9ae8b038aaa2fa7d34d2871b056de1f9a
Ian Lynagh <igloo@earth.li>
2007-08-31 17:52:17 +0000

    Follow Cabal's move Distribution.Program -> Distribution.Simple.Program

--- compat/Distribution/Program.hs
+++
@@ -1,3 +0,0 @@
-{-# OPTIONS -cpp #-}
-#include "Cabal/Distribution/Program.hs"
--- dummy comment
---
+++ compat/Distribution/Simple/Program.hs
@@ -0,0 +1,3 @@
+{-# OPTIONS -cpp #-}
+#include "Cabal/Distribution/Simple/Program.hs"
+-- dummy comment
