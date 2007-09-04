cd63e58b9c6f3599b3bbed733978bc8b7d569d56
Tom Schrijvers <tom.schrijvers@cs.kuleuven.be>
2007-09-04 07:57:39 +0000

    equational class context expect_fail is now normal because of bug fix

--- testsuite/tests/ghc-regress/indexed-types/should_compile/InstEqContext2.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/InstEqContext2.hs
@@ -5,7 +5,7 @@ module InstEqContext2  where
 data E v a = E a
 data RValue
 
-instance (Eq a, v ~RValue) => Eq (E v a) where
+instance (Eq a, v ~ RValue) => Eq (E v a) where
     E x == E y  =  x == y
 
 a :: E v Int
