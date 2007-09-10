4f73eb83e7ca1eb23958f8ab7ee65c3ac4bd0908
Roman Leshchinskiy <rl@cse.unsw.edu.au>
2007-09-10 06:49:51 +0000

    And another failing tyfam test

---
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/Simple14.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TypeFamilies, RankNTypes, FlexibleContexts, ScopedTypeVariables #-}
+
+module Simple14 where
+
+data EQ_ x y = EQ_
+
+eqE :: EQ_ x y -> (x~y => EQ_ z z) -> p
+eqE = undefined
+
+eqI :: EQ_ x x
+eqI = undefined
+
+ntI :: (forall p. EQ_ x y -> p) -> EQ_ x y
+ntI = undefined
+
+foo :: forall m n. EQ_ (Maybe m) (Maybe n)
+foo = ntI (`eqE` (eqI :: EQ_ m n))
+-- This works:
+-- foo = ntI (\eq -> eq `eqE` (eqI :: EQ_ m n))
+
