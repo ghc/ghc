40abe56ff1a26b59a2a24105dab2e3bc802b839c
Pepe Iborra <mnislaih@gmail.com>
2007-09-12 09:42:54 +0000

    Avoid Dyn exceptions in break024
    This ensures predictable output (when using :print) in any case

--- testsuite/tests/ghc-regress/ghci.debugger/scripts/break024.hs
+++ testsuite/tests/ghc-regress/ghci.debugger/scripts/break024.hs
@@ -1,11 +1,4 @@
 import Control.Exception as CE
 
-import Data.Typeable
-import Data.Dynamic
-
-data MyException = MyException deriving (Typeable,Show) 
-
-exception_uncaught    = CE.throwDyn MyException
-exceptionIO_uncaught  = CE.throwIO (DynException (toDyn MyException))
-
-exception_caught = CE.try exceptionIO_uncaught
+exception_uncaught = ioError (userError "error")
+exception_caught   = CE.try exception_uncaught
