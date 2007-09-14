64b0862e762152ee10c83c32236dfeb23fe36f49
simonpj <simonpj@microsoft.com>
2007-09-14 12:01:21 +0000

    Test nested forall types (thanks to Claus)

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/tc236.hs
@@ -0,0 +1,8 @@
+{-# OPTIONS -fglasgow-exts #-}
+
+module ShouldCompile where
+
+-- Check that we can have a forall to the right of a double-arrow
+
+f :: forall a. (Num a) => forall b. (Ord b) => a -> b -> b -> a
+f x y z = if y>z then x+1 else x
