742809424904edbefc39dca8233356b25e546b56
Norman Ramsey <nr@eecs.harvard.edu>
2007-09-15 20:17:46 +0000

    add a function to help identify unique predecessors

--- compiler/cmm/CmmZipUtil.hs
+++ compiler/cmm/CmmZipUtil.hs
@@ -1,11 +1,14 @@
 
 module CmmZipUtil
   ( zipPreds
+  , givesUniquePredecessorTo
   )
 where
 import Prelude hiding (last, unzip)
 import ZipCfg
+
 import Maybes
+import UniqSet
 
 -- | Compute the predecessors of each *reachable* block
 zipPreds :: LastNode l => LGraph m l -> BlockEnv BlockSet
@@ -15,3 +18,22 @@ zipPreds g = foldl add emptyBlockEnv (postorder_dfs g)
                        let preds = lookupBlockEnv env sid `orElse` emptyBlockSet
                        in  extendBlockEnv env sid (extendBlockSet preds id))
             env (succs block)
+
+-- | Tell if a graph gives a block a unique predecessor.  For
+-- efficiency, this function is designed to be partially applied.
+
+givesUniquePredecessorTo :: LastNode l => LGraph m l -> BlockId -> Bool
+givesUniquePredecessorTo g = \id -> elemBlockSet id singlePreds
+    -- | accumulates a pair of sets: the set of all blocks containing a single 
+    -- predecessor, and the set of all blocks containing at least two predecessors
+    where (singlePreds, _) = fold_blocks add (emptyBlockSet, emptyBlockSet) g
+          add b (single, multi) = foldl add_pred (single, multi) (succs b)
+          add_pred pair@(single, multi) id =
+              if elemBlockSet id multi then pair
+              else if elemBlockSet id single then
+                       (delOneFromUniqSet single id, extendBlockSet multi id)
+                   else
+                       (extendBlockSet single id, multi)
+              
+    
+
