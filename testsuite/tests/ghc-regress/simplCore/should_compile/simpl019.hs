d80e23faef7071e9625b07939538b3e33c5471bd
simonpj <simonpj@microsoft.com>
2007-09-29 10:20:00 +0000

    Test for Trac #1746

---
+++ testsuite/tests/ghc-regress/simplCore/should_compile/simpl019.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -fglasgow-exts -O2 #-}
+
+-- See Trac #1746
+
+module Foo where
+
+data T a where T :: T a -> T [a]
+
+class C a where
+  f :: a -> ()
+
+instance C (T [a]) where
+  f (T x@(T _)) = f x
