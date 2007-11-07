efb6ce6ef9539bd5584bfdb22e627b0f340211ec
claus.reinke <claus.reinke@talk21.com>
2007-11-07 10:23:14 +0000

    tests for :browse!, :{:}, :set, :show languages/packages
    these tests validate on win/xp.
    
      - if any issues with dos-style line-endings remain,
        please apply dos2unix for now, and let me know
    
      - ghci024 has ghci024.py instead of ghci024.stdout;
        the prepare024 function defined in there generates
        a platform- and version-specific ghci024.stdout
        for the test (see all.T)
    
      - ghci025 uses the (non-advertised) new -s option
        to get sorted :browse output for stable tests
        (to work around #1799). if this isn't sufficient,
        please set normal to ignore_output in all.T for
        now (until #1799 is fixed), and let me know

---
+++ testsuite/tests/ghc-regress/ghci/scripts/ghci024.py
@@ -0,0 +1,127 @@
+import os
+import re
+
+conf_ghc = '/cygdrive/c/fptools/ghc/compiler/stage2/ghc-inplace'
+
+def prepare024( opts ):
+  h = os.popen('echo :q | '+config.compiler+' --interactive -package ghc ')
+  packages = h.read()
+  h.close()
+  packagelist = []
+  for l in packages.splitlines():
+    if re.match('^Loading package',l): 
+      packagelist += [re.sub(r'^Loading package (\S*) .*$',r'\1',l)]
+  packagelist.reverse()
+  packagelist += ['rts']
+
+  outtext = """\
+options currently set: none.
+GHCi-specific dynamic flag settings:
+  -fno-print-explicit-foralls
+  -fprint-bind-result
+  -fno-break-on-exception
+  -fno-break-on-error
+  -fno-print-evld-with-show
+other dynamic, non-language, flag settings:
+  -fno-warn-dodgy-imports
+  -fwarn-duplicate-exports
+  -fno-warn-hi-shadowing
+  -fno-warn-implicit-prelude
+  -fno-warn-incomplete-patterns
+  -fno-warn-incomplete-record-updates
+  -fwarn-missing-fields
+  -fwarn-missing-methods
+  -fno-warn-missing-signatures
+  -fno-warn-name-shadowing
+  -fwarn-overlapping-patterns
+  -fno-warn-simple-patterns
+  -fno-warn-type-defaults
+  -fno-warn-monomorphism-restriction
+  -fno-warn-unused-binds
+  -fno-warn-unused-imports
+  -fno-warn-unused-matches
+  -fwarn-deprecations
+  -fno-warn-orphans
+  -fno-warn-tabs
+  -fno-strictness
+  -fno-full-laziness
+  -fno-liberate-case
+  -fno-spec-constr
+  -fno-cse
+  -fignore-interface-pragmas
+  -fomit-interface-pragmas
+  -fdo-lambda-eta-expansion
+  -fno-ignore-asserts
+  -fno-ignore-breakpoints
+  -fno-do-eta-reduction
+  -fno-case-merge
+  -fno-unbox-strict-fields
+  -fno-dicts-cheap
+  -fno-excess-precision
+  -fasm-mangling
+  -fno-force-recomp
+  -fno-hpc-no-auto
+  -fno-rewrite-rules
+  -fno-run-cps
+  -fno-convert-to-zipper-and-back
+  -fno-vectorise
+  -fno-regs-graph
+  -fno-regs-iterative
+  -fgen-manifest
+  -fembed-manifest
+active language flags:
+  -XImplicitPrelude
+  -XMonomorphismRestriction
+  -XMonoPatBinds
+-- :set -fglasgow-exts
+active language flags:
+  -XPatternGuards
+  -XUnicodeSyntax
+  -XMagicHash
+  -XPolymorphicComponents
+  -XExistentialQuantification
+  -XKindSignatures
+  -XPatternSignatures
+  -XEmptyDataDecls
+  -XParallelListComp
+  -XForeignFunctionInterface
+  -XUnliftedFFITypes
+  -XLiberalTypeSynonyms
+  -XRankNTypes
+  -XTypeOperators
+  -XRecursiveDo
+  -XImplicitPrelude
+  -XGADTs
+  -XTypeFamilies
+  -XMonomorphismRestriction
+  -XMonoPatBinds
+  -XRelaxedPolyRec
+  -XImplicitParams
+  -XScopedTypeVariables
+  -XUnboxedTuples
+  -XStandaloneDeriving
+  -XDeriveDataTypeable
+  -XTypeSynonymInstances
+  -XFlexibleContexts
+  -XFlexibleInstances
+  -XConstrainedClassMethods
+  -XMultiParamTypeClasses
+  -XFunctionalDependencies
+  -XGeneralizedNewtypeDeriving
+active package flags: none
+packages currently loaded:
+  base
+  rts
+-- :set -package ghc
+active package flags:
+  -package ghc
+packages currently loaded:
+"""
+
+  outfile = open(in_testdir('ghci024.stdout'), 'w')
+  outfile.write(outtext)
+  for l in packagelist: outfile.write('  '+l+'\n')
+  outfile.close()
+
+  return;
+
---
+++ testsuite/tests/ghc-regress/ghci/scripts/ghci025.hs
@@ -0,0 +1,23 @@
+
+{-# OPTIONS_GHC -fglasgow-exts #-}
+{-# OPTIONS_GHC -fglasgow-exts #-}
+
+module T where
+
+import qualified Prelude as T(length,Monad)
+import qualified Data.ByteString as T(length)
+import Prelude(length,(+),(=<<),Monad(..),Maybe(..),Eq)
+import Data.Maybe
+import Control.Monad(Monad(..),MonadPlus(..))
+
+length = 0
+
+class N a
+class S a
+
+class C a b where
+  c1 :: N b => a -> b
+  c2 :: (N b,S b) => a -> b
+  c3 :: forall a. a -> b
+  c4 :: a1 -> b
+
