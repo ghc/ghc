6c54855be9394d76775845256835808fca442b78
Clemens Fruhwirth <clemens@endorphin.org>
2008-01-10 09:08:39 +0000

    ghc-inplace defaults to -fhardwire-lib-paths. Change that to -dynload wrapped

--- compiler/ghc-inplace.c
+++ compiler/ghc-inplace.c
@@ -7,22 +7,23 @@
 
 int main(int argc, char **argv) {
     char **args;
-    args = malloc(sizeof(char *) * (argc + 3));
+    args = malloc(sizeof(char *) * (argc + 4));
     if (args == NULL) {
         fprintf(stderr, "Malloc failed\n");
         exit(1);
     }
     args[0] = "GHC_PATH"; /* Gets replaced by sed */
     args[1] = "-BTOP_ABS"; /* Gets replaced by sed */
-    args[2] = "-fhardwire-lib-paths";
+    args[2] = "-dynload";
+    args[3] = "wrapped";
     if ((argc >= 2) && (strcmp(argv[1], "-v") == 0)) {
-        printf("Using %s %s %s\n", args[0], args[1], args[2]);
+        printf("Using %s %s %s %s\n", args[0], args[1], args[2], args[3]);
         fflush(stdout);
     }
-    memcpy(args + 3, argv + 1, sizeof(char *) * (argc - 1));
-    args[argc+2] = NULL;
+    memcpy(args + 4, argv + 1, sizeof(char *) * (argc - 1));
+    args[argc+3] = NULL;
     return run(argv[0],
                "GHC_PATH", /* Gets replaced by sed */
-               argc + 2,
+               argc + 3,
                args);
 }
