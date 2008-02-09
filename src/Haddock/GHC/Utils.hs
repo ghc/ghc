0f28c92189ac023c3555768c2954466650e63849
David Waern <david.waern@gmail.com>
2008-02-09 23:00:36 +0000

    H.GHC.Utils: remove unused imports/exports

--- src/Haddock/GHC/Utils.hs
+++ src/Haddock/GHC/Utils.hs
@@ -4,63 +4,30 @@
 -- (c) Simon Marlow 2003
 --
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module Haddock.GHC.Utils where
 
 
-import Debug.Trace
 import Data.Char
 import qualified Data.Map as Map
 
 import GHC
 import HsSyn
 import SrcLoc
-import HscTypes
 import Outputable
-import Packages
-import UniqFM
 import Name
-
-
--- names
-
-nameOccString = occNameString . nameOccName 
-
-
-nameSetMod n newMod = 
-  mkExternalName (nameUnique n) newMod (nameOccName n) (nameSrcSpan n)
-
-
-nameSetPkg pkgId n = 
-  mkExternalName (nameUnique n) (mkModule pkgId (moduleName mod)) 
-	               (nameOccName n) (nameSrcSpan n)
-  where mod = nameModule n
-
-
--- modules
 
 
 moduleString :: Module -> String
 moduleString = moduleNameString . moduleName 
 
 
-mkModuleNoPkg :: String -> Module
-mkModuleNoPkg str = mkModule (stringToPackageId "") (mkModuleName str)
-
-
-modulePkgStr = packageIdString . modulePackageId
-
-
--- Instances
-
-
 instance (Outputable a, Outputable b) => Outputable (Map.Map a b) where
   ppr m = ppr (Map.toList m)
 
 
--- misc
-
-
+isNameSym :: Name -> Bool
 isNameSym = isSymOcc . nameOccName
 
 
@@ -72,16 +39,13 @@ getMainDeclBinder (ValD d)
         (name:_) -> Just (unLoc name)
 getMainDeclBinder (SigD d) = sigNameNoLoc d
 getMainDeclBinder (ForD (ForeignImport name _ _)) = Just (unLoc name)
-getMainDeclBinder (ForD (ForeignExport name _ _)) = Nothing
+getMainDeclBinder (ForD (ForeignExport _ _ _)) = Nothing
 getMainDeclBinder _ = Nothing
 
-
--- To keep if if minf_iface is re-introduced
---modInfoName = moduleName . mi_module . minf_iface
---modInfoMod  = mi_module . minf_iface 
 
 pretty :: Outputable a => a -> String
 pretty x = show (ppr x defaultUserStyle)
 
 
+trace_ppr :: Outputable a => a -> b -> b
 trace_ppr x y = trace (showSDoc (ppr x)) y
