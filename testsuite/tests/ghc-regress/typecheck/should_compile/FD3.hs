1e4335ae4227421ea2d6f2b9676fc520d49e3d05
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2008-03-13 06:50:06 +0000

    typecheck/FD3: revert to error msg of previous version ofthe compiler

--- testsuite/tests/ghc-regress/typecheck/should_compile/FD3.hs
+++ testsuite/tests/ghc-regress/typecheck/should_compile/FD3.hs
@@ -2,13 +2,6 @@
 
 -- Trac #1795
 
--- This should fail.  It used to fail saying it can't unify
---   'a' with '(String, a)'
--- but now it simply says that it can't deduce 
--- 	(MkA (String, a) a) from the context ()
--- which is fair enough
-
-
 module ShouldCompile where
 
 data A a = A
