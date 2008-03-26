3712e5a712cd7c21bf7ab3d1f3212d41dae20fe6
Ian Lynagh <igloo@earth.li>
2008-03-26 14:10:30 +0000

    In validate settings, make -Werror easier to override

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -1,7 +1,9 @@
 
+WERROR          = -Werror
+
 HADDOCK_DOCS    = YES
-SRC_CC_OPTS     = -Werror
-SRC_HC_OPTS     = -Werror -H64m -Onot -fasm
+SRC_CC_OPTS     = $(WERROR)
+SRC_HC_OPTS     = $(WERROR) -H64m -Onot -fasm
 GhcStage1HcOpts = -O -fasm
 GhcStage2HcOpts = -Onot -fasm
 GhcLibHcOpts    = -O -fasm -dcore-lint
