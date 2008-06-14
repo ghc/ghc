3e280fcada5656e100ba6bb6535bf240acf33f13
Ian Lynagh <igloo@earth.li>
2008-06-14 15:21:31 +0000

    Use -O0 rather than the deprecated -Onot

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -3,9 +3,9 @@ WERROR          = -Werror
 
 HADDOCK_DOCS    = YES
 SRC_CC_OPTS     = $(WERROR)
-SRC_HC_OPTS     = $(WERROR) -H64m -Onot -fasm
+SRC_HC_OPTS     = $(WERROR) -H64m -O0 -fasm
 GhcStage1HcOpts = -O -fasm
-GhcStage2HcOpts = -Onot -fasm
+GhcStage2HcOpts = -O0 -fasm
 GhcLibHcOpts    = -O -fasm -dcore-lint
 GhcLibWays      =
 SplitObjs       = NO
