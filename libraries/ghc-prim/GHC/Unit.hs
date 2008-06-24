99ad3366c194110abf32fcfa567c36c54d32a040
Ian Lynagh <igloo@earth.li>
2008-06-24 14:49:55 +0000

    Add GHC.Unit for the definition of the () type

---
+++ libraries/ghc-prim/GHC/Unit.hs
@@ -0,0 +1,18 @@
+
+{-# OPTIONS_GHC -XNoImplicitPrelude #-}
+
+module GHC.Unit where
+
+import GHC.Generics ()
+
+default ()
+
+{-
+The Unit type is here because GHC.PrimopWrappers needs to use it in a
+type signature.
+-}
+
+-- | The unit datatype @()@ has one non-undefined member, the nullary
+-- constructor @()@.
+data () = ()
+
