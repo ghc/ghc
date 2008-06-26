990faa74e434b1ab5578b36220a336c24cff834b
Ian Lynagh <igloo@earth.li>
2008-06-26 11:25:11 +0000

    Use a program similar to cabal-install to build the libraries This means that we don't have to make a Setup program for each library individually, and also simplifies the build system a bit.

---
+++ libraries/cabal.hs
@@ -0,0 +1,38 @@
+
+module Cabal (main) where
+
+import Data.Maybe
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import Distribution.Simple
+import Distribution.Simple.Utils
+import Distribution.Verbosity
+import System.Directory
+import System.Environment
+import System.FilePath
+
+import qualified Distribution.Make   as Make
+import qualified Distribution.Simple as Simple
+
+setupProg :: FilePath
+setupProg = "./Setup"
+
+main :: IO ()
+main = do
+    let verbosity = verbose
+    exists <- doesFileExist setupProg
+    args <- getArgs
+    if exists then rawSystemExit verbosity setupProg args
+              else do
+        gpdFile <- defaultPackageDesc verbosity
+        gpd <- readPackageDescription verbosity gpdFile
+        let pd = packageDescription gpd
+        case buildType pd of
+            Just Simple    -> Simple.defaultMainArgs                     args
+            Just Make      -> Make.defaultMainArgs                       args
+            Just Configure -> defaultMainWithHooksArgs autoconfUserHooks args
+            _ | pkgName (package pd) == "Cabal" ->
+                              -- Cabal is special...*sigh*
+                              Simple.defaultMainArgs                     args
+              | otherwise  -> die "cabal: Don't know what to do!"
+
