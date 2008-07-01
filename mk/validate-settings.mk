982f6e5405eadb41860772ad0fe13ce0980fad2f
Ian Lynagh <igloo@earth.li>
2008-07-01 17:59:27 +0000

    Add --slow (and --fast) options to validate slow mode is 14% slower than normal. It uses -DDEBUG for the stage 2 compiler, and -XGenerics for the stage 2 compiler and the libraries. I believe that most of the slowdown is actually caused by -XGenerics rather than -DDEBUG.

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -5,11 +5,20 @@ HADDOCK_DOCS    = YES
 SRC_CC_OPTS     = $(WERROR)
 SRC_HC_OPTS     = $(WERROR) -H64m -O0 -fasm
 GhcStage1HcOpts = -O -fasm
-GhcStage2HcOpts = -O0 -fasm $(ValidateHpcFlags)
+
+GhcStage2HcOpts = -O0 -fasm
 GhcLibHcOpts    = -O -fasm -dcore-lint
 GhcLibWays      =
 SplitObjs       = NO
 NoFibWays       =
 STRIP           = :
 GhcBootLibs     = YES
+
+ifeq "$(ValidateHpc)" "YES"
+GhcStage2HcOpts += -fhpc -hpcdir $(FPTOOLS_TOP_ABS)/testsuite/hpc_output/
+endif
+ifeq "$(ValidateSlow)" "YES"
+GhcStage2HcOpts += -XGenerics -DDEBUG
+GhcLibHcOpts    += -XGenerics
+endif
 
