ec05d3bbba14c9c022a9752819f7fd4aa9277e1c
Ian Lynagh <igloo@earth.li>
2008-07-04 23:33:43 +0000

    Improve error messages from pwd

--- utils/pwd/pwd.hs
+++ utils/pwd/pwd.hs
@@ -3,15 +3,21 @@ module Main where
 
 import System.Directory
 import System.Environment
+import System.Exit
+import System.IO
 
 main :: IO ()
-main = do args <- getArgs
-          let escape = case args of
-                       ["quadruple-backslash"] -> escape_quadruple_backslash
-                       ["forwardslash"] -> escape_forwardslash
-                       _ -> error ("pwd: Bad args: " ++ show args)
-          d <- getCurrentDirectory
-          putStr $ concatMap escape d
+main = do
+    args <- getArgs
+    escape <- case args of
+              ["quadruple-backslash"] -> return escape_quadruple_backslash
+              ["forwardslash"] ->        return escape_forwardslash
+              _ -> do hPutStrLn stderr ("Bad args: " ++ show args)
+                      hPutStrLn stderr
+                                "Usage: pwd {forwardslash|quadruple-backslash}"
+                      exitFailure
+    d <- getCurrentDirectory
+    putStr $ concatMap escape d
 
 -- In prog006 we have to escape \ twice, once to get through sed and
 -- again to get through parsing pkg.conf
