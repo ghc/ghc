7faadaf549ac35a2a138481752753062de87b12e
Ian Lynagh <igloo@earth.li>
2008-08-06 19:16:02 +0000

    Move Int, Float and Double into ghc-prim:GHC.Types

--- libraries/ghc-prim/GHC/Types.hs
+++ libraries/ghc-prim/GHC/Types.hs
@@ -13,3 +13,18 @@ data [] a = [] | a : [a]
 
 data Char = C# Char#
 
+data Int = I# Int#
+-- ^A fixed-precision integer type with at least the range @[-2^29 .. 2^29-1]@.
+-- The exact range for a given implementation can be determined by using
+-- 'Prelude.minBound' and 'Prelude.maxBound' from the 'Prelude.Bounded' class.
+
+-- | Single-precision floating point numbers.
+-- It is desirable that this type be at least equal in range and precision
+-- to the IEEE single-precision type.
+data Float      = F# Float#
+
+-- | Double-precision floating point numbers.
+-- It is desirable that this type be at least equal in range and precision
+-- to the IEEE double-precision type.
+data Double     = D# Double#
+
