c9e786696e5c2484ccabf3144358668b823d726b
simonpj <simonpj@microsoft.com>
2008-08-11 11:56:52 +0000

    Test Trac #2494

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/T2494-2.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE Rank2Types, ScopedTypeVariables #-}
+
+-- Trac #2494, should compile ok
+
+module Foo where
+
+foo :: (forall m. Monad m => Maybe (m a) -> Maybe (m a)) -> Maybe a -> Maybe a
+foo _ x = x
+
+{-# RULES
+
+"foo/foo"
+  forall (f :: forall m. Monad m => Maybe (m a) -> Maybe (m a))
+         (g :: forall m. Monad m => Maybe (m a) -> Maybe (m a)) x.
+  foo f (foo g x) = foo (f . g) x
+ #-}
