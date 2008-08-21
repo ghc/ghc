bb555e8974b5c7c1848a5336343d46af0a888fb0
Ian Lynagh <igloo@earth.li>
2008-08-21 20:40:11 +0000

    Fix num013

--- testsuite/tests/ghc-regress/numeric/should_run/num013.hs
+++ testsuite/tests/ghc-regress/numeric/should_run/num013.hs
@@ -6,9 +6,12 @@ import Data.Int
 import Prelude hiding (catch)
 
 main :: IO ()
-main = do print ((minBound :: Int) `div` (-1)) `catch` print
-          print ((minBound :: Int8) `div` (-1)) `catch` print
-          print ((minBound :: Int16) `div` (-1)) `catch` print
-          print ((minBound :: Int32) `div` (-1)) `catch` print
-          print ((minBound :: Int64) `div` (-1)) `catch` print
+main = do print ((minBound :: Int) `div` (-1))   `myCatch` print
+          print ((minBound :: Int8) `div` (-1))  `myCatch` print
+          print ((minBound :: Int16) `div` (-1)) `myCatch` print
+          print ((minBound :: Int32) `div` (-1)) `myCatch` print
+          print ((minBound :: Int64) `div` (-1)) `myCatch` print
+
+myCatch :: IO a -> (ArithException -> IO a) -> IO a
+myCatch = catch
 
