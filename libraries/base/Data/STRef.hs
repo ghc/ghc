96c6210f8a68a37cc69937edf57ff03b4c779cbc
Ian Lynagh <igloo@earth.li>
2008-09-03 20:19:08 +0000

    Remerge concurrent,unique,timeout,st,getopt into base

---
+++ libraries/base/Data/STRef.hs
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.STRef
+-- Copyright   :  (c) The University of Glasgow 2001
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- 
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable (uses Control.Monad.ST)
+--
+-- Mutable references in the (strict) ST monad.
+--
+-----------------------------------------------------------------------------
+
+module Data.STRef (
+        -- * STRefs
+        STRef,          -- abstract, instance Eq
+        newSTRef,       -- :: a -> ST s (STRef s a)
+        readSTRef,      -- :: STRef s a -> ST s a
+        writeSTRef,     -- :: STRef s a -> a -> ST s ()
+        modifySTRef     -- :: STRef s a -> (a -> a) -> ST s ()
+ ) where
+
+import Prelude
+
+#ifdef __GLASGOW_HASKELL__
+import GHC.ST
+import GHC.STRef
+#endif
+
+#ifdef __HUGS__
+import Hugs.ST
+import Data.Typeable
+
+#include "Typeable.h"
+INSTANCE_TYPEABLE2(STRef,stRefTc,"STRef")
+#endif
+
+-- |Mutate the contents of an 'STRef'
+modifySTRef :: STRef s a -> (a -> a) -> ST s ()
+modifySTRef ref f = writeSTRef ref . f =<< readSTRef ref
