b9449db9d1f5b750a5e4000d322f59b1e1108100
simonpj <simonpj@microsof.com>
2008-09-04 14:54:49 +0000

    Test for Trac #470

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/T1470.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances, OverlappingInstances, UndecidableInstances #-}
+
+module Foo where
+
+class Sat a
+class Data ctx a
+instance  Sat (ctx Char)             => Data ctx Char
+instance (Sat (ctx [a]), Data ctx a) => Data ctx [a]
+
+class Data FooD a => Foo a
+
+data FooD a = FooD
+
+instance Foo t => Sat (FooD t)
+
+instance Data FooD a => Foo a
+instance Foo a       => Foo [a]
+instance                Foo [Char]
