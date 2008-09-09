6588cdc94dc1af0846bfc3c4f9cf41e42face9be
simonpj <simonpj@microsoft.com>
2008-09-09 10:16:11 +0000

    Test Trac 2572

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/T2572.hs
@@ -0,0 +1,10 @@
+ {-# OPTIONS LANGUAGE ScopedTypeVariables #-}
+
+-- Trac #2572
+
+module Foo where
+
+type GTypeFun = forall a . a -> ()
+
+gmapType :: Int -> GTypeFun
+gmapType _ (_ :: a) = undefined
