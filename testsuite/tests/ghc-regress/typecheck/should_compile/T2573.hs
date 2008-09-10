7de94fe30208f1da21fd81669a48489e4113d806
simonpj <simonpj@microsof.com>
2008-09-10 10:18:19 +0000

    Fix typo in T2573

--- testsuite/tests/ghc-regress/typecheck/should_compile/T2573.hs
+++ testsuite/tests/ghc-regress/typecheck/should_compile/T2573.hs
@@ -8,6 +8,6 @@ import Data.Generics
 
 class Tag a where
    nCons :: a -> Int
-   nCons {| a :*: b |} _ = 1::a
+   nCons {| a :*: b |} _ = 1
    nCons {| a :+: b |} _ = 1
    nCons {| Unit |}    _ = 1
