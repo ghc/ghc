60dccb046d8fe9e22740a81d1097d32d08fba72b
simonpj@microsoft.com <unknown>
2008-09-18 15:56:02 +0000

    Comments only

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -7,6 +7,10 @@ SRC_HC_OPTS     = $(WERROR) -H64m -O0 -fasm
 GhcStage1HcOpts = -O -fasm
 
 GhcStage2HcOpts = -O -fasm
+# Using -O (rather than -O0) here bringes my validate down from 22mins to 16 mins.
+# Compiling stage2 takes longer, but we gain a faster haddock, faster
+# running of the tests, and faster building of the utils to be installed
+
 GhcLibHcOpts    = -O -fasm -dcore-lint
 GhcLibWays      =
 SplitObjs       = NO
