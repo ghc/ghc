001fd556bd85db365c09c3f03f2a54204f3bedd3
Ian Lynagh <igloo@earth.li>
2008-09-19 14:59:40 +0000

    Fix test regex001 Needed to restrict the type of 'try' now that we have extensible exceptions

--- testsuite/tests/ghc-regress/lib/Regex/regex001.hs
+++ testsuite/tests/ghc-regress/lib/Regex/regex001.hs
@@ -1,8 +1,11 @@
 module Main where
 
-import Control.Exception (try)
+import Control.Exception
 import Text.Regex.Posix
 
 -- caused GHC 6.0 to crash, due to regfree'ing the regex after a
 -- failed regcomp.
-main = sequence_ [ try ("abc" =~~ "[[[" :: IO Bool) | _ <- [1..10000] ]
+main = sequence_
+           [ try ("abc" =~~ "[[[" :: IO Bool) :: IO (Either IOException Bool)
+           | _ <- [1..10000] ]
+
