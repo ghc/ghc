2f591deaf56c0361334c1fa1081d50bd9fb58883
Clemens Fruhwirth <clemens@endorphin.org>
2008-10-13 20:16:08 +0000

    Build fixes for DLLized rts

--- rts/win32/ThrIOManager.c
+++ rts/win32/ThrIOManager.c
@@ -137,7 +137,11 @@ ioManagerStart (void)
     Capability *cap;
     if (io_manager_event == INVALID_HANDLE_VALUE) {
 	cap = rts_lock();
+#if defined(mingw32_HOST_OS) && defined(__PIC__)
+	rts_evalIO(cap,_imp__base_GHCziConc_ensureIOManagerIsRunning_closure,NULL);
+#else
 	rts_evalIO(cap,&base_GHCziConc_ensureIOManagerIsRunning_closure,NULL);
+#endif
 	rts_unlock(cap);
     }
 }
