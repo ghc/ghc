877f16b993fd9dcaa4a5b154e610e64ec1f737f7
jpm@cs.uu.nl <unknown>
2008-11-11 09:50:23 +0000

    updating Haddock documentation
    Fixed the broken link from Data.Generics to Data.Data.

--- libraries/base/Data/Typeable.hs
+++ libraries/base/Data/Typeable.hs
@@ -21,7 +21,7 @@
 -- and one can in turn define a type-safe cast operation. To this end,
 -- an unsafe cast is guarded by a test for type (representation)
 -- equivalence. The module "Data.Dynamic" uses Typeable for an
--- implementation of dynamics. The module "Data.Generics" uses Typeable
+-- implementation of dynamics. The module "Data.Data" uses Typeable
 -- and type-safe cast (but not dynamics) to support the \"Scrap your
 -- boilerplate\" style of generic programming.
 --
