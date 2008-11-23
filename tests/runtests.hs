0bbd1738215a1f69853b385f437c76c1cfcdfc3c
David Waern <david.waern@gmail.com>
2008-11-23 14:33:31 +0000

    HADDOCK_DATA_DIR changed to haddock_datadir

--- tests/runtests.hs
+++ tests/runtests.hs
@@ -47,9 +47,9 @@ test = do
   let mods = filter ((==) ".hs" . takeExtension) contents
   let outdir = "output"
   let mods' = map ("tests" </>) mods
-  runProcess "../dist/build/haddock/haddock" ["--version"] Nothing (Just [("HADDOCK_DATA_DIR", "../.")]) Nothing Nothing Nothing
-  runProcess "../dist/build/haddock/haddock" ["--ghc-version"] Nothing (Just [("HADDOCK_DATA_DIR", "../.")]) Nothing Nothing Nothing
-  handle <- runProcess "../dist/build/haddock/haddock" (["-w", "-o", outdir, "-h", "--optghc=-fglasgow-exts", "--optghc=-w"] ++ mods') Nothing (Just [("HADDOCK_DATA_DIR", "../.")]) Nothing Nothing Nothing
+  runProcess "../dist/build/haddock/haddock" ["--version"] Nothing (Just [("haddock_datadir", "../.")]) Nothing Nothing Nothing
+  runProcess "../dist/build/haddock/haddock" ["--ghc-version"] Nothing (Just [("haddock_datadir", "../.")]) Nothing Nothing Nothing
+  handle <- runProcess "../dist/build/haddock/haddock" (["-w", "-o", outdir, "-h", "--optghc=-fglasgow-exts", "--optghc=-w"] ++ mods') Nothing (Just [("haddock_datadir", "../.")]) Nothing Nothing Nothing
   code <- waitForProcess handle
 
 --  code <- system $ printf "haddock -w -o %s -h --optghc=-fglasgow-exts --optghc=-w %s" outdir (unwords mods')
