f89552dd8ef77dc49eb9f6fe922da72b204f7415
Thomas Schilling <nominolo@googlemail.com>
2008-11-29 16:16:20 +0000

    Haddock really shouldn't try to overwrite files.

--- src/Haddock/GHC.hs
+++ src/Haddock/GHC.hs
@@ -47,7 +47,7 @@ startGhc libDir flags = do
 #endif
     let dynflags' = dopt_set dynflags Opt_Haddock
     let dynflags'' = dynflags' {
-        hscTarget = HscNothing,
+        hscTarget = HscAsm,
         ghcMode   = CompManager,
         ghcLink   = NoLink
       }
