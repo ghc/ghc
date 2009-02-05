0694fc71d6c2db3495ddef71365ee8c968931930
simonpj <simonpj@microsoft.com>
2009-02-05 09:10:29 +0000

    Add test for Trac #2994

---
+++ testsuite/tests/ghc-regress/typecheck/should_fail/T2994.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+-- Trac #2994
+
+module T2994 where
+
+class MonadReader a b 
+
+newtype Reader' r a = Reader' (r -> a)
+
+instance MonadReader (Reader' r)
+
+instance MonadReader r r (Reader' r)
