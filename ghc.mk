a5daccb2a0acb4ee07c745581f9ed91f0d8cde1a
Ian Lynagh <igloo@earth.li>
2009-03-01 14:59:35 +0000

    Install haddock in the new GHC build system

--- ghc.mk
+++ ghc.mk
@@ -11,12 +11,24 @@ else
 utils/haddock_dist_PROG = haddock-real
 
 $(INPLACE_BIN)/haddock: $(INPLACE_BIN)/haddock-real
-	$(RM) -f $@
-	echo '#!$(SHELL)' >> $@
-	echo 'executablename=$(FPTOOLS_TOP_ABS)/$<' >> $@
+	$(RM) -f                                         $@
+	echo '#!$(SHELL)'                             >> $@
+	echo 'executablename=$(FPTOOLS_TOP_ABS)/$<'   >> $@
 	echo 'datadir=$(FPTOOLS_TOP_ABS)/inplace/lib' >> $@
-	cat utils/haddock/haddock.wrapper >> $@
-	$(EXECUTABLE_FILE) $@
+	cat utils/haddock/haddock.wrapper             >> $@
+	$(EXECUTABLE_FILE)                               $@
+
+.PHONY: install_utils/haddock_dist_wrapper
+install: install_utils/haddock_dist_wrapper
+
+install_utils/haddock_dist_wrapper: WRAPPER=$(DESTDIR)$(bindir)/haddock
+install_utils/haddock_dist_wrapper:
+	$(RM) -f                                        $(WRAPPER)
+	echo '#!$(SHELL)'                            >> $(WRAPPER)
+	echo 'executablename=$(bindir)/haddock-real' >> $(WRAPPER)
+	echo 'datadir=$(datadir)'                    >> $(WRAPPER)
+	cat utils/haddock/haddock.wrapper            >> $(WRAPPER)
+	$(EXECUTABLE_FILE)                              $(WRAPPER)
 endif
 
 $(INPLACE_BIN)/$(utils/haddock_dist_PROG): inplace/lib/html
@@ -24,6 +36,8 @@ $(INPLACE_BIN)/$(utils/haddock_dist_PROG): inplace/lib/html
 inplace/lib/html:
 	$(RM) -rf $@
 	cp -a utils/haddock/html $@
+
+utils/haddock_dist_INSTALL = YES
 
 $(eval $(call build-prog,utils/haddock,dist,2))
 
