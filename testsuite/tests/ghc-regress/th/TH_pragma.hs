b93286300379815acf1e38bcbf1ace7b1e91dbd1
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2009-03-24 23:28:01 +0000

    TH: test for pragma support

---
+++ testsuite/tests/ghc-regress/th/TH_pragma.hs
@@ -0,0 +1,10 @@
+module TH_pragma where
+
+
+$( [d| foo :: Int -> Int
+       {-# NOINLINE foo #-}
+       foo x = x + 1    |] )
+
+$( [d| bar :: Num a => a -> a
+       {-# SPECIALISE INLINE [~1] bar :: Float -> Float #-}
+       bar x = x * 10        |] )
