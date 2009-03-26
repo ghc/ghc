ac5afe0c219440ceaac04711b77c9f9454383838
Simon Marlow <marlowsd@gmail.com>
2009-03-26 15:51:57 +0000

    Fix num012 on 32-bit platforms round and truncate are undefined if the result would be outside the range of the target type (#1254).

--- testsuite/tests/ghc-regress/numeric/should_run/num012.hs
+++ testsuite/tests/ghc-regress/numeric/should_run/num012.hs
@@ -2,20 +2,28 @@
 -- Test for trac #921
 
 import GHC.Float
+import Foreign
+import Control.Monad
 
 main :: IO ()
 main = do -- The reported case
           putStrLn (show (map log2 vals))
           -- Smaller failing cases
           print (fromIntegral ((2^31) :: Int) :: Double)
-          print (round ((2^33) :: Double) :: Int)
+          if_not_32 $ print (round ((2^33) :: Double) :: Int)
           print (fromIntegral ((2^31) :: Int) :: Float)
-          print (round ((2^33) :: Float) :: Int)
+          if_not_32 $ print (round ((2^33) :: Float) :: Int)
           -- The underlying failing internal operations
           print (int2Double (2^31))
-          print (double2Int (2^33))
+          if_not_32 $ print (double2Int (2^33))
           print (int2Float (2^31))
-          print (float2Int (2^33))
+          if_not_32 $ print (float2Int (2^33))
+  where
+    -- the value of float2Int x where the result would be outside the
+    -- range of the target is undefined.  We also take the view in GHC
+    -- that round and truncate are similarly undefined when the result
+    -- would be outside the range of the target type (see #1254)
+    if_not_32 = when (sizeOf (undefined::Int) > 4)
 
 log2 x = ceiling log_x
     where log_x :: Double
