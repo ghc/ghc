27bf63eac1ca2acaae2a1766680c404df4d4a239
Ian Lynagh <igloo@earth.li>
2009-04-24 13:21:27 +0000

    Add a test for the first part of #2806

---
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail203.hs
@@ -0,0 +1,50 @@
+-- trac #2806
+
+{-# LANGUAGE MagicHash, UnboxedTuples, BangPatterns #-}
+
+module Foo where
+
+import GHC.Base
+
+pass1 = 'a'
+    where !x = 5#
+
+pass2 = 'a'
+    where !(I# x) = 5
+
+pass3 = 'a'
+    where !(b, I# x) = (True, 5)
+
+pass4 = 'a'
+    where !(# b, I# x #) = (# True, 5 #)
+
+pass5 = 'a'
+    where !(# b, x #) = (# True, 5# #)
+
+fail1 = 'a'
+    where x = 5#
+
+fail2 = 'a'
+    where (I# x) = 5
+
+fail3 = 'a'
+    where (b, I# x) = (True, 5)
+
+fail4 = 'a'
+    where (# b, I# x #) = (# True, 5 #)
+
+fail5 = 'a'
+    where (# b, x #) = (# True, 5# #)
+
+fail6 = 'a'
+    where (I# !x) = 5
+
+fail7 = 'a'
+    where (b, !(I# x)) = (True, 5)
+
+fail8 = 'a'
+    where (# b, !(I# x) #) = (# True, 5 #)
+
+fail9 = 'a'
+    where (# b, !x #) = (# True, 5# #)
+
