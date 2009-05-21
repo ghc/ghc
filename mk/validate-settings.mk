46d2e100dc2e232f5d76ff9fc6a3f11e07d383ce
Ian Lynagh <igloo@earth.li>
2009-05-21 14:19:38 +0000

    Don't overwrite the *OPTS/*Opts variables in mk/validate-settings.mk Overwriting means we lose the -m64 on OS X 64.

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -4,16 +4,16 @@
 WERROR          = -Werror
 
 HADDOCK_DOCS    = YES
-SRC_CC_OPTS     = $(WERROR)
-SRC_HC_OPTS     = $(WERROR) -H64m -O0 -fasm
-GhcStage1HcOpts = -O -fasm
+SRC_CC_OPTS     += $(WERROR)
+SRC_HC_OPTS     += $(WERROR) -H64m -O0 -fasm
+GhcStage1HcOpts += -O -fasm
 
-GhcStage2HcOpts = -O -fasm
+GhcStage2HcOpts += -O -fasm
 # Using -O (rather than -O0) here bringes my validate down from 22mins to 16 mins.
 # Compiling stage2 takes longer, but we gain a faster haddock, faster
 # running of the tests, and faster building of the utils to be installed
 
-GhcLibHcOpts    = -O -fasm -dcore-lint
+GhcLibHcOpts    += -O -fasm -dcore-lint
 GhcLibWays      = v
 SplitObjs       = NO
 NoFibWays       =
