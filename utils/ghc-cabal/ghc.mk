bddd2ae2c4ee764d1434c3237e967bc7240eab54
Ian Lynagh <igloo@earth.li>
2009-05-23 21:35:18 +0000

    Fix warnings in ghc-cabal

--- utils/ghc-cabal/ghc.mk
+++ utils/ghc-cabal/ghc.mk
@@ -57,6 +57,7 @@ $(GHC_CABAL_DIR)_dist-dummy-ghc_PROG    = dummy-ghc$(exeext)
 $(GHC_CABAL_DIR)/dist-dummy-ghc/build/dummy-ghc.hs : $(GHC_CABAL_DIR)/ghc.mk $(MKDIRHIER) mk/config.mk
 	$(MKDIRHIER) $(dir $@)
 	echo "import System.Environment; import System.Cmd; import System.Exit" >$@
+	echo "main :: IO ()" >>$@
 	echo "main = do args <- getArgs; if args == [\"--numeric-version\"] then putStrLn \"$(ProjectVersion)\" else do e <- rawSystem \"$(GHC_STAGE0)\" args; exitWith e" >>$@
 
 # We don't build dummy-ghc with Cabal, so we need to pass -package
