9aeb98b3e5963e6614eb61397ebe6b92821f2a37
Ian Lynagh <igloo@earth.li>
2009-05-23 21:34:51 +0000

    Turn on warnings when validating

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -4,8 +4,8 @@
 WERROR          = -Werror
 
 HADDOCK_DOCS    = YES
-SRC_CC_OPTS     += $(WERROR)
-SRC_HC_OPTS     += $(WERROR) -H64m -O0 -fasm
+SRC_CC_OPTS     += -Wall $(WERROR)
+SRC_HC_OPTS     += -Wall $(WERROR) -H64m -O0 -fasm
 GhcStage1HcOpts += -O -fasm
 
 GhcStage2HcOpts += -O -fasm
