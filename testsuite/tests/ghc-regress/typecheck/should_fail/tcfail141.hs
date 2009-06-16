d4528e72f1024d8d634d74d3ebf9f7c84b162a78
Duncan Coutts <duncan@well-typed.com>
2009-06-16 22:34:24 +0000

    Adjust tcfail141 so it does not import int2Integer# from GHC.Prim Since we're about to remove that primop. Define a version locally (using undefined). It does not have to work since we only need it for its type.

--- testsuite/tests/ghc-regress/typecheck/should_fail/tcfail141.hs
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail141.hs
@@ -4,8 +4,14 @@
 
 module ShouldFail where
 
-import GHC.Prim
+import GHC.Prim (Int#, ByteArray#)
 
 main :: IO ()
 main = let f = int2Integer# 0# in putStrLn ""
+
+
+int2Integer# :: Int# -> (# Int#, ByteArray# #)
+int2Integer# = undefined
+-- This function doesn't have to work!
+-- We just need it for its type.
 
