75e52f5854fa59c1912e85295791dbb057429fe2
Ian Lynagh <igloo@earth.li>
2009-06-17 01:08:22 +0000

    Handle end-of-line in a way that is also OSX-friendly

--- testsuite/mk/boilerplate.mk
+++ testsuite/mk/boilerplate.mk
@@ -28,15 +28,15 @@ $(call canonicalise,$1)
 endef
 
 define get-ghc-rts-field # $1 = rseult variable, $2 = field name
-$1 := $$(shell $$(TEST_HC) +RTS --info | grep '^ .("$2",' | sed -e 's/.*", *"//' -e 's/")\r\?$$$$//')
+$1 := $$(shell $$(TEST_HC) +RTS --info | grep '^ .("$2",' | tr -d '\r' | sed -e 's/.*", *"//' -e 's/")$$$$//')
 endef
 
 define get-ghc-field # $1 = rseult variable, $2 = field name
-$1 := $$(shell $$(TEST_HC) --info | grep '^ .("$2",' | sed -e 's/.*", *"//' -e 's/")\r\?$$$$//')
+$1 := $$(shell $$(TEST_HC) --info | grep '^ .("$2",' | tr -d '\r' | sed -e 's/.*", *"//' -e 's/")$$$$//')
 endef
 
 define get-ghc-feature-bool # $1 = rseult variable, $2 = field name
-SHELL_RES := $$(shell $$(TEST_HC) --info | grep '^ .("$2",' | sed -e 's/.*", *"//' -e 's/")\r\?$$$$//')
+SHELL_RES := $$(shell $$(TEST_HC) --info | grep '^ .("$2",' | tr -d '\r' | sed -e 's/.*", *"//' -e 's/")$$$$//')
 $1 := $$(strip \
 	  $$(if $$(SHELL_RES), \
          $$(if $$(subst YES,,$$(SHELL_RES)), \
