64bcc31d886edc1b4fbc00a20e269257bcc0d3d0
Ian Lynagh <igloo@earth.li>
2009-06-20 16:13:57 +0000

    Add a GHC.Debug module, with debugLn :: [Char] -> IO ()

---
+++ libraries/ghc-prim/GHC/Debug.hs
@@ -0,0 +1,31 @@
+
+module GHC.Debug (debugLn) where
+
+import GHC.Prim
+import GHC.Types
+import GHC.Unit
+
+debugLn :: [Char] -> IO ()
+debugLn xs = IO (\s0 ->
+                 -- Start with 1 so that we have space to put in a \0 at
+                 -- the end
+                 case len 1# xs of
+                 l ->
+                     case newByteArray# l s0 of
+                     (# s1, mba #) ->
+                         case write mba 0# xs s1 of
+                         s2 ->
+                             case c_debugLn mba of
+                             IO f -> f s2)
+    where len l [] = l
+          len l (_ : xs') = len (l +# 1#) xs'
+
+          write mba offset [] s = writeCharArray# mba offset '\0'# s
+          write mba offset (C# x : xs') s
+              = case writeCharArray# mba offset x s of
+                s' ->
+                    write mba (offset +# 1#) xs' s'
+
+foreign import ccall unsafe "debugLn"
+    c_debugLn :: MutableByteArray# RealWorld -> IO ()
+
---
+++ libraries/ghc-prim/cbits/debug.c
@@ -0,0 +1,6 @@
+
+#include <stdio.h>
+
+void debugLn(char *s) {
+    printf("%s\n", s);
+}
