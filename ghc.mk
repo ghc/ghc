1066870add4aa221eecfbe850019ed7778507108
Ian Lynagh <igloo@earth.li>
2009-07-05 17:01:48 +0000

    Remove the -Wwarn hack in the GHC build system

--- ghc.mk
+++ ghc.mk
@@ -2,8 +2,7 @@
 utils/haddock_USES_CABAL = YES
 utils/haddock_PACKAGE = haddock
 utils/haddock_CONFIGURE_OPTS = --flag in-ghc-tree
-# XXX This is a temporary hack:
-utils/haddock_HC_OPTS += -Wwarn -DNEW_GHC_LAYOUT
+utils/haddock_HC_OPTS += -DNEW_GHC_LAYOUT
 utils/haddock_dist_SHELL_WRAPPER = YES
 utils/haddock_dist_INSTALL_SHELL_WRAPPER = YES
 utils/haddock_dist_PROG = haddock$(exeext)
