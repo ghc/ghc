7f64e9c47f84f8d9fc19fcd31e2e8ad401142a54
Simon Marlow <marlowsd@gmail.com>
2009-07-06 09:20:31 +0000

    don't create inplace/bin/ghc-<version>

--- driver/ghc/ghc.mk
+++ driver/ghc/ghc.mk
@@ -15,6 +15,7 @@ ifeq "$(Windows)" "YES"
 driver/ghc_dist_C_SRCS  = ghc.c
 driver/ghc_dist_PROG    = ghc-$(ProjectVersion)
 driver/ghc_dist_INSTALL = YES
+driver/ghc_dist_INSTALL_INPLACE = NO
 
 $(eval $(call build-prog,driver/ghc,dist,0))
 
