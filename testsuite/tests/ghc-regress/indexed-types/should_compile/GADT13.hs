39428d8e11391cab5fc5d3e35c14d0ba33a5c1c9
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2009-07-10 03:55:36 +0000

    TFs: fixed should_compile/Simple8 & wibbles

--- testsuite/tests/ghc-regress/indexed-types/should_compile/GADT13.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/GADT13.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeFamilies, GADTs #-}
 
+module GADT13 where
+
 data family HiThere a :: *
 
 data instance HiThere () where
--- testsuite/tests/ghc-regress/indexed-types/should_compile/HO.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/HO.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, TypeOperators #-}
+{-# LANGUAGE TypeFamilies, TypeOperators, RankNTypes #-}
 
 module HO where 
 
