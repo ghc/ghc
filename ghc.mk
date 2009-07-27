dd849158c84941f5e3714dd4df24e467854f0d91
Simon Marlow <marlowsd@gmail.com>
2009-07-27 09:47:19 +0000

    remove a couple of ToDos

--- ghc.mk
+++ ghc.mk
@@ -29,9 +29,7 @@
 #     * docs/vh/Makefile
 #     * rts/dotnet/Makefile
 #     * utils/Makefile
-#   * GhcProfiled
 #   * optionally install stage3?
-#   * shared libraries, way dyn
 #   * add Makefiles for the rest of the utils/ programs that aren't built
 #     by default (need to exclude them from 'make all' too)
 #
