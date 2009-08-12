bf510aa86f3161c981b3432d43417874c38adf8f
Ian Lynagh <igloo@earth.li>
2009-08-12 18:57:43 +0000

    Add a test for #3303: multiline deprecated warnings

---
+++ testsuite/tests/ghc-regress/parser/should_compile/T3303.hs
@@ -0,0 +1,8 @@
+
+module T3303 where
+
+import T3303A
+
+bar :: Int
+bar = foo
+
---
+++ testsuite/tests/ghc-regress/parser/should_compile/T3303A.hs
@@ -0,0 +1,10 @@
+
+module T3303A where
+
+{-# DEPRECATED foo
+         ["This is a multi-line",
+          "deprecation message",
+          "for foo"] #-}
+foo :: Int
+foo = 4
+
