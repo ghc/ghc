0d127f8237536b04efec3b063df45b23bb4e4c14
David Waern <david.waern@gmail.com>
2009-08-16 01:43:04 +0000

    Add a test for cross-package documentation

---
+++ tests/tests/CrossPackageDocs.hs
@@ -0,0 +1,11 @@
+module CrossPackageDocs (map, Monad(..), runInteractiveProcess {- $ Bugs -}) where
+
+import System.Process
+
+-- $ Bugs:
+--
+--   * [] a
+--
+--   * No instances list
+--
+--   * No docs on function arguments
