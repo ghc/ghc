3adf1c560f467546a5b3c12f5a73dbcf2415bc91
simonpj <simonpj@microsoft.com>
2009-08-21 21:05:44 +0000

    Test Trac #3423

---
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/T3423.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TypeFamilies, UndecidableInstances, StandaloneDeriving #-}
+
+module T3423 where
+
+newtype Trie m k a = Trie (Maybe a, m (SubKey k) (Trie m k a))
+
+type family SubKey k
+type instance SubKey [k] = k
+
+deriving instance (Eq (m k (Trie m [k] a)), Eq a) 
+               => Eq (Trie m [k] a)
