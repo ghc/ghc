2c2668728f850e5969029f8a269079fa49ce881f
Simon Marlow <marlowsd@gmail.com>
2009-09-08 11:42:00 +0000

    fix warnings

--- compiler/rename/RnHsDoc.hs
+++ compiler/rename/RnHsDoc.hs
@@ -2,21 +2,9 @@
 module RnHsDoc ( rnHsDoc, rnLHsDoc, rnMbLHsDoc ) where
 
 import TcRnTypes
-import RnEnv       ( dataTcOccs, lookupGreRn_maybe )
 import HsSyn
-
-import RdrName     ( RdrName, gre_name )
-import Name        ( Name )
 import SrcLoc      ( Located(..) )
-import Outputable  ( ppr, defaultUserStyle )
 
-
-rnMbHsDoc :: Maybe HsDocString -> RnM (Maybe HsDocString)
-rnMbHsDoc mb_doc = case mb_doc of
-  Just doc -> do
-    doc' <- rnHsDoc doc
-    return (Just doc')
-  Nothing -> return Nothing
 
 rnMbLHsDoc :: Maybe LHsDocString -> RnM (Maybe LHsDocString)
 rnMbLHsDoc mb_doc = case mb_doc of
@@ -29,10 +17,6 @@ rnLHsDoc :: LHsDocString -> RnM LHsDocString
 rnLHsDoc (L pos doc) = do
   doc' <- rnHsDoc doc
   return (L pos doc')
-
-ids2string :: [RdrName] -> String
-ids2string []    = []
-ids2string (x:_) = show $ ppr x defaultUserStyle
 
 rnHsDoc :: HsDocString -> RnM HsDocString
 rnHsDoc (HsDocString s) = return (HsDocString s)
