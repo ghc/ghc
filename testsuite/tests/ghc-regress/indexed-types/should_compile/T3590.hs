c6528f563f44288c7df7188511a8892f051a75e6
simonpj <simonpj@microsoft.com>
2009-10-21 15:34:01 +0000

    Test Trac #3590

---
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/T3590.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+
+-- Trac #3590: a bug in typechecking of sections
+
+module T3590 where
+
+import Control.Monad.Reader ()
+import Control.Monad.Reader.Class (MonadReader(..))
+
+newtype ListT m a =
+  ListT { runListT :: m (Maybe (a, ListT m a)) }
+
+class Monad (ItemM l) => List l where
+  type ItemM l :: * -> *
+  joinL :: [ItemM l (l a) -> l a]
+
+instance Monad m => List (ListT m) where
+  type ItemM (ListT m) = m
+  joinL = [ ListT . (>>= runListT)	-- Right section
+          , ListT . (runListT <<=)      -- Left section
+          ]
+
+(<<=) :: Monad m => (a -> m b) -> m a -> m b
+(<<=) k m = m >>= k
+
