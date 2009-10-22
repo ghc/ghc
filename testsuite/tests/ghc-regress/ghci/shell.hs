f4cf5ca96742582e1f33d5da1c51b7f078c453ec
Ian Lynagh <igloo@earth.li>
2009-10-22 00:00:28 +0000

    More quoting fixes

--- testsuite/tests/ghc-regress/ghci/shell.hs
+++ testsuite/tests/ghc-regress/ghci/shell.hs
@@ -1,12 +1,9 @@
-{-# OPTIONS -cpp #-}
-
 -- Used to present a consistent shell view for :! commands in GHCi
--- scripts.  We're assuming that sh.exe is in the path and that it
+-- scripts.  We're assuming that sh is in the path and that it
 -- is a Bourne-compatible shell.
 
 import System.Cmd
-#ifdef mingw32_HOST_OS
-shell s = system ("sh.exe -c '" ++ s ++ "'")
-#else
-shell s = system s
-#endif
+import System.Exit
+
+shell :: String -> IO ExitCode
+shell s = rawSystem "sh" ["-c", s]
