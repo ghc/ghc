a359e3b5d282cb8a83d0439ed7fab0a52a627918
Simon Marlow <marlowsd@gmail.com>
2009-11-20 14:29:07 +0000

    make this test do what it was supposed to do

--- testsuite/tests/ghc-regress/ghci.debugger/scripts/break024.hs
+++ testsuite/tests/ghc-regress/ghci.debugger/scripts/break024.hs
@@ -1,4 +1,4 @@
 import Control.Exception as CE
 
 exception_uncaught = ioError (userError "error")
-exception_caught   = CE.try exception_uncaught :: IO (Either CE.ErrorCall ())
+exception_caught   = CE.try exception_uncaught :: IO (Either CE.IOException ())
