b26b9e5a4a5dd7e46282dcc8046c1012359014df
David Waern <david.waern@gmail.com>
2009-11-22 14:08:40 +0000

    Follow removal of GHC.MVar from base in CrossPackageDocs

--- tests/tests/CrossPackageDocs.hs
+++ tests/tests/CrossPackageDocs.hs
@@ -1,12 +1,3 @@
-module CrossPackageDocs (map, Monad(..), runInteractiveProcess, MVar(..), newEmptyMVar  {- $ Bugs -}) where
+module CrossPackageDocs (map, Monad(..), runInteractiveProcess) where
 
 import System.Process
-import GHC.MVar
-
--- $ Bugs:
---
---   * [] a
---
---   * No instances list
---
---   * No docs on function arguments
