9401f2e98d4ecd79e23b5e41649b098e93936eb2
David Waern <david.waern@gmail.com>
2010-01-22 22:57:03 +0000

    Create a test for function argument docs

---
+++ tests/tests/FunArgs.hs
@@ -0,0 +1,7 @@
+module FunArgs where
+
+f :: Ord a => Int -- ^ First argument
+  -> a            -- ^ Second argument
+  -> Bool         -- ^ Third argument
+  -> ()           -- ^ Result
+f = undefined
