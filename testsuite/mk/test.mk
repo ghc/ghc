192b2910c4be9bbff338c3bc6ea1e79d74c868ff
Ian Lynagh <igloo@earth.li>
2010-03-03 23:50:23 +0000

    Fix detection of whether we have profiling libs It broke when the installation path contained a space

--- testsuite/mk/test.mk
+++ testsuite/mk/test.mk
@@ -44,7 +44,8 @@ else
 RUNTEST_OPTS += -e ghc_with_native_codegen=0
 endif
 
-HAVE_PROFILING:=$(if $(wildcard $(shell "$(GHC_PKG)" field haskell98 library-dirs | sed 's/^[^:]*: *//')/libHShaskell98-*_p.a),YES,NO)
+HASKELL98_LIBDIR := $(shell "$(GHC_PKG)" field haskell98 library-dirs | sed 's/^[^:]*: *//')
+HAVE_PROFILING := $(shell if [ -f $(HASKELL98_LIBDIR)/libHShaskell98-*_p.a ]; then echo YES; else echo NO; fi)
 
 ifeq "$(HAVE_PROFILING)" "YES"
 RUNTEST_OPTS += -e ghc_with_profiling=1
