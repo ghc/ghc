21271c6ca1604b96c2818e81f196d8904363f47b
simonpj <simonpj@microsoft.com>
2010-03-17 12:37:57 +0000

    Add test for Trac #3920

---
+++ testsuite/tests/ghc-regress/th/T3920.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE EmptyDataDecls, TypeFamilies, TemplateHaskell #-}
+module Main where
+
+import Language.Haskell.TH
+
+type family S :: (* -> (* -> * -> *)) -> (* -> *) -> *
+
+test :: String
+test = $(do
+	test <- [d| 
+		type family T :: (* -> (* -> * -> *)) -> (* -> *) -> * |]
+        blah <- reify ''S
+	return (LitE (StringL (pprint test ++ "\n" ++ pprint blah))))
+
+main = putStrLn test
