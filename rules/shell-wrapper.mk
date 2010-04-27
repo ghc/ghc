1cfd9c70b82517183d86958ea331f60108baea7e
Ian Lynagh <igloo@earth.li>
2010-04-27 16:00:38 +0000

    Inplace programs depend on their shell wrappers

--- rules/shell-wrapper.mk
+++ rules/shell-wrapper.mk
@@ -33,7 +33,7 @@ ifneq "$$($1_$2_INSTALL_INPLACE)" "NO"
 all_$1_$2 : $$(INPLACE_BIN)/$$($1_$2_PROG)
 
 $$(INPLACE_BIN)/$$($1_$2_PROG): WRAPPER=$$@
-$$(INPLACE_BIN)/$$($1_$2_PROG): $$($1_$2_INPLACE)
+$$(INPLACE_BIN)/$$($1_$2_PROG): $$($1_$2_INPLACE) $$($1_$2_SHELL_WRAPPER_NAME)
 	"$$(RM)" $$(RM_OPTS)                              $$@
 	echo '#!$$(SHELL)'                             >> $$@
 	echo 'executablename="$$(TOP)/$$<"'            >> $$@
