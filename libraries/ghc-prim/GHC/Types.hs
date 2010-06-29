77f432c59fab42f73dac6a440f8cc84ce3b5ad33
Simon Marlow <marlowsd@gmail.com>
2010-06-29 13:39:16 +0000

    re-instate the documentation for 'Char', which got lost at some point

--- libraries/ghc-prim/GHC/Types.hs
+++ libraries/ghc-prim/GHC/Types.hs
@@ -26,6 +26,18 @@ infixr 5 :
 
 data [] a = [] | a : [a]
 
+{-| The character type 'Char' is an enumeration whose values represent
+Unicode (or equivalently ISO\/IEC 10646) characters
+(see <http://www.unicode.org/> for details).
+This set extends the ISO 8859-1 (Latin-1) character set
+(the first 256 charachers), which is itself an extension of the ASCII
+character set (the first 128 characters).
+A character literal in Haskell has type 'Char'.
+
+To convert a 'Char' to or from the corresponding 'Int' value defined
+by Unicode, use 'Prelude.toEnum' and 'Prelude.fromEnum' from the
+'Prelude.Enum' class respectively (or equivalently 'ord' and 'chr').
+-}
 data Char = C# Char#
 
 data Int = I# Int#
