f6b121abe0b310e3079c03b4634c6558116ff590
simonpj <simonpj@microsoft.com>
2010-07-01 15:08:27 +0000

    Add a RelaxedPolyRec test
    ..suggested by Haskell Cafe email (Job Vranish)

---
+++ testsuite/tests/ghc-regress/typecheck/should_compile/PolyRec.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE RelaxedPolyRec #-}
+
+-- An example of RelaxedPolyRec in action which came up
+-- on Haskell Cafe June 2010 (Job Vranish)
+
+module Foo where
+
+import Data.Maybe
+
+-- The fixed point datatype
+data Y f = Y (f (Y f))
+
+-- Silly dummy function
+maybeToInt :: Maybe a -> Int
+maybeToInt = length . maybeToList
+
+---------------------------
+-- f and g are mutually recursive
+-- Even though f has a totally monomorphic
+-- signature, g has a very polymorphic one
+
+f :: Y Maybe -> Int
+f (Y x) = g maybeToInt x
+
+-- With RelaxedPolyRec we can infer this type
+-- g :: Functor f => (f Int -> b) -> f (Y Maybe) -> b
+g h x = h $ fmap f x
+
+-- 'test' calls g at two different types, f=[] and f=Maybe
+test = (g (sum :: [Int] -> Int) [], 
+        g isJust Nothing)
