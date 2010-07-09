ecc60710f04cce34c121af1a17416c7140d364f2
Ian Lynagh <igloo@earth.li>
2010-07-09 22:45:34 +0000

    Move a bit of build system code

--- ghc.mk
+++ ghc.mk
@@ -591,18 +591,6 @@ ifneq "$(findstring $(phase),0 1 2)" ""
 ghc_stage1_DISABLE = YES
 endif
 
-ifneq "$(CLEANING)" "YES"
-ifeq "$(INTEGER_LIBRARY)" "integer-gmp"
-libraries/base_dist-install_CONFIGURE_OPTS += --flags=-integer-simple
-else
-    ifeq "$(INTEGER_LIBRARY)" "integer-simple"
-	libraries/base_dist-install_CONFIGURE_OPTS += --flags=integer-simple
-    else
-$(error Unknown integer library: $(INTEGER_LIBRARY))
-    endif
-endif
-endif
-
 ifneq "$(findstring $(phase),0 1 2 3)" ""
 # In phases 0-3, we disable stage2-3, the full libraries and haddock
 utils/haddock_dist_DISABLE = YES
@@ -618,6 +606,18 @@ ghc_stage2_DISABLE = YES
 ghc_stage3_DISABLE = YES
 $(foreach lib,$(PACKAGES) $(PACKAGES_STAGE2),$(eval \
   libraries/$(lib)_dist-install_DISABLE = YES))
+endif
+
+ifneq "$(CLEANING)" "YES"
+ifeq "$(INTEGER_LIBRARY)" "integer-gmp"
+libraries/base_dist-install_CONFIGURE_OPTS += --flags=-integer-simple
+else
+    ifeq "$(INTEGER_LIBRARY)" "integer-simple"
+	libraries/base_dist-install_CONFIGURE_OPTS += --flags=integer-simple
+    else
+$(error Unknown integer library: $(INTEGER_LIBRARY))
+    endif
+endif
 endif
 
 # These packages don't pass the Cabal checks because hs-source-dirs
