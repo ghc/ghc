28c2bbb03ff6144f3a09e5286c8c3ca6ad3689e8
Ian Lynagh <igloo@earth.li>
2010-07-29 01:25:07 +0000

    Disable symbol visibility pragmas for FreeBSD Do not use GCC pragmas for controlling visibility, because it causes "undefined reference" errors at link-time.  The true reasons are unknown, however FreeBSD 8.x includes GCC 4.2.1 in the base system, which might be buggy.

--- rts/BeginPrivate.h
+++ rts/BeginPrivate.h
@@ -1,3 +1,7 @@
-#if __GNUC__ >= 4
+/* We don't use symbol visibility pragmas on FreeBSD, because it causes
+   "undefined reference" errors at link-time.  The true reasons are
+   unknown, however FreeBSD 8.x includes GCC 4.2.1 in the base system,
+   which might be buggy. */
+#if __GNUC__ >= 4 && !defined(freebsd_HOST_OS)
 #pragma GCC visibility push(hidden)
 #endif
--- rts/EndPrivate.h
+++ rts/EndPrivate.h
@@ -1,3 +1,3 @@
-#if __GNUC__ >= 4
+#if __GNUC__ >= 4 && !defined(freebsd_HOST_OS)
 #pragma GCC visibility pop
 #endif
