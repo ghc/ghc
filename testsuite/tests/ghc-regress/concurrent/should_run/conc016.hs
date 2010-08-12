c24ca8cff9abff501b81ab424d4d0d61285c2436
Simon Marlow <marlowsd@gmail.com>
2010-08-12 11:24:25 +0000

    expect conc016(threaded2) to fail

--- testsuite/tests/ghc-regress/concurrent/should_run/conc016.hs
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc016.hs
@@ -3,6 +3,14 @@ import Control.Exception
 
 -- check that we can still kill a thread that is blocked on
 -- delivering an exception to us.
+
+-- NB. this test is delicate since 6.14, because throwTo is now always
+-- interruptible, so the main thread's killThread can be legitimately
+-- interrupted by the child thread's killThread, rather than the other
+-- way around.  This happens because the child thread is running on
+-- another processor, so the main thread's throwTo is blocked waiting
+-- for a response, and while waiting it is interruptible.
+
 main = do
   main_thread <- myThreadId
   m <- newEmptyMVar
@@ -10,9 +18,9 @@ main = do
 	    	 	 takeMVar m
 	    	 	 throwTo main_thread (ErrorCall "foo")
 	 		)
-  block (do
+  mask_ $ do
     putMVar m ()
     sum [1..10000] `seq` -- to be sure the other thread is now blocked
       killThread sub_thread
-   )
+
   putStrLn "ok"
