edf395e7f3be2a3a1d04c315922d5413c86b7b5b
simonpj <simonpj@microsoft.com>
2010-09-07 14:36:39 +0000

    Can't do type-function equalities under forall

--- testsuite/tests/ghc-regress/indexed-types/should_compile/T4120.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/T4120.hs
@@ -15,6 +15,10 @@ type instance Mutable Vector = MVector
 
 create :: (forall s. MVector s a) -> Int
 create = create1
+-- Here we get  Couldn't match expected type `forall s. MVector s a'
+--                          with actual type `forall s. Mutable Vector s a1'
+-- Reason: when unifying under a for-all we don't solve type 
+--         equalities.  Think more about this.
 
 create1 :: (forall s. Mutable Vector s a) -> Int
 create1 = error "urk"
