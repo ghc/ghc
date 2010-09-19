0cd2b1df1ba2f13054aaf528bbe7e631f41666dd
simonpj <simonpj@microsoft.com>
2010-09-19 15:36:05 +0000

    Add tests for Trac 4179, 4254

---
+++ testsuite/tests/ghc-regress/indexed-types/should_fail/T4254.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE TypeFamilies, FunctionalDependencies, RankNTypes, MultiParamTypeClasses #-}
+module T4254 where
+
+class FD a b | a -> b where 
+  op :: a -> b; 
+  op = undefined 
+
+instance FD Int Bool
+
+ok1   :: forall a b. (a~Int,FD a b) => a -> b
+ok1    = op
+-- Should be OK: op has the right type
+
+ok2   :: forall a b. (a~Int,FD a b,b~Bool) => a -> Bool
+ok2    = op
+-- Should be OK: needs the b~Bool
+
+fails :: forall a b. (a~Int,FD a b) => a -> Bool 
+fails  = op
+-- Should fail: no proof that b~Bool
