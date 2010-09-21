af36e0876c02fbd2c61aa47c0d06f9b48491461a
Ian Lynagh <igloo@earth.li>
2010-09-21 15:12:02 +0000

    Fix up __GLASGOW_HASKELL__ tests

--- src/Haddock/InterfaceFile.hs
+++ src/Haddock/InterfaceFile.hs
@@ -65,9 +65,9 @@ binaryInterfaceVersion :: Word16
 binaryInterfaceVersion = 15
 #elif __GLASGOW_HASKELL__ == 613
 binaryInterfaceVersion = 15
-#elif __GLASGOW_HASKELL__ == 614
+#elif __GLASGOW_HASKELL__ == 700
 binaryInterfaceVersion = 16
-#elif __GLASGOW_HASKELL__ == 615
+#elif __GLASGOW_HASKELL__ == 701
 binaryInterfaceVersion = 16
 #else
 #error Unknown GHC version
