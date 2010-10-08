811690f996f440c854d3d79714d62826d0f2710e
simonpj <simonpj@microsoft.com>
2010-10-08 15:25:17 +0000

    Test Trac #4358

---
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/T4358.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE TypeFamilies, Rank2Types, FlexibleContexts #-}
+
+module T4358 where
+
+type family T a
+
+t2 :: forall a. ((T a ~ a) => a) -> a
+t2 = t
+  
+t :: forall a. ((T a ~ a) => a) -> a
+t = undefined
