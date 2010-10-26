7fc16040453604a64fc1932acb38002cdf52911f
Ian Lynagh <igloo@earth.li>
2010-10-26 23:07:52 +0000

    Add a test for #4437 (check all the GHC extensions are registered with Cabal)

---
+++ testsuite/tests/ghc-regress/driver/T4437.hs
@@ -0,0 +1,21 @@
+
+module Main (main) where
+
+import Data.List
+import DynFlags
+import Language.Haskell.Extension
+
+main :: IO ()
+main = do let ghcExtensions = [ ext | (ext, _, _) <- xFlags ]
+              cabalExtensions = map show [ toEnum 0 :: KnownExtension .. ]
+              ghcOnlyExtensions = ghcExtensions \\ cabalExtensions
+              -- These are extensions which are deliberately not yet
+              -- registered with Cabal
+              expectedGhcOnlyExtensions
+                  = ["PArr",
+                     "AlternativeLayoutRule",
+                     "AlternativeLayoutRuleTransitional"]
+              unexpectedGhcOnlyExtension = ghcOnlyExtensions
+                                        \\ expectedGhcOnlyExtensions
+          mapM_ putStrLn unexpectedGhcOnlyExtension
+
