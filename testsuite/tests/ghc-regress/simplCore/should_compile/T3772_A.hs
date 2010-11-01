d7304d8f1cf869b411a095bf0e65f0b2fa8ed716
simonpj <simonpj@microsoft.com>
2010-11-01 16:51:49 +0000

    Comments only

--- testsuite/tests/ghc-regress/simplCore/should_compile/T3772_A.hs
+++ testsuite/tests/ghc-regress/simplCore/should_compile/T3772_A.hs
@@ -28,3 +28,21 @@ apply :: (C a, DeepSeq b) => Int -> (a -> b) -> ()
 apply n f = f (gen n) `deepSeq` ()
 
 
+{-
+$cdeepSeq :: DeepSeq a -> [a] -> b -> b
+-- User INLINE( 3 args )!
+$cdeepSeq a (d:DS a) b (x:[a]) (y:b) = ...
+
+$fDeepSeq[] :: DeepSeq a -> DeepSeq [a]
+-- DFun (with auto INLINE pragma)
+$fDeepSeq[] a d = $cdeepSeq a d |> blah
+
+$cp1 a d :: C a => DeepSep [a]
+-- We don't want to eta-expand this, lest
+-- $cdeepSeq gets inlined in it!
+$cp1 a d = $fDeepSep[] a (scsel a d)
+
+$fC[] :: C a => C [a]
+-- DFun
+$fC[] a d = MkC ($cp1 a d) ($cgen a d)
+-}
