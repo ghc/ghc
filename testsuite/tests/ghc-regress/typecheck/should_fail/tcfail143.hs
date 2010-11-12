42a4a85967281475879738805d87012c2c7f2b1f
simonpj <simonpj@microsoft.com>
2010-11-12 13:04:43 +0000

    Follow error message changes with new typechecker

--- testsuite/tests/ghc-regress/typecheck/should_fail/tcfail143.hs
+++ testsuite/tests/ghc-regress/typecheck/should_fail/tcfail143.hs
@@ -7,10 +7,11 @@ data Z		= Z
 data S a	= S a
 
 class MinMax a b c d | a b -> c d, a c d -> b, b c d -> a
-instance MinMax Z Z Z Z				
-instance MinMax a Z Z a	 -- L1: wrongly flagged as error src.
-instance MinMax Z b Z b				
+instance MinMax Z Z Z Z -- (a)
+instance MinMax a Z Z a -- (b)   -- L1: wrongly flagged as error src.
+instance MinMax Z b Z b -- (c)
 instance MinMax a b c d => MinMax (S a) (S b) (S c) (S d)
+                        -- (d)
 
 class Extend a b  where extend :: a -> b ->  b
 instance Extend Z b where  Z `extend` b = b
@@ -33,6 +34,16 @@ t2 = n1 `extend` n0 -- L3: uncommenting just this line produces
 		    --	error message rightly pointing at L2 and L3.
 
 
+{-  n0 :: Z;  n1 :: S Z
+
+Call of extend gives wanted: Extend (S Z) Z
+Use instance =>  MinMax (S Z) Z gamma Z
+FD on (b) => gamma ~ Z, Z ~ S Z
+             =>  MinMax (S Z) Z Z Z
+FD on (a), 3rd fundep => Z ~ S Z
+      (b) again (sadly)   Z ~ S Z
+
+-}
 {-
 
 Here's what is happening.
