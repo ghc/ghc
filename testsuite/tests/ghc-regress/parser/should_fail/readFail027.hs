fc8ecf824582da4ea5e61e87cc3cc0fa9f5456df
Ian Lynagh <igloo@earth.li>
2010-11-20 21:02:26 +0000

    de-tab readFail027, and remove trailing whitespace

--- testsuite/tests/ghc-regress/parser/should_fail/readFail027.hs
+++ testsuite/tests/ghc-regress/parser/should_fail/readFail027.hs
@@ -2,7 +2,7 @@ module ShouldFail where
 
 -- Erroneously allowed by GHC 6.2.x
 f x = case x of
-	 False -> do 
+         False -> do
     { return x; }
 -- this line should close the 'case' context and cause the 'do' to be empty.
 
