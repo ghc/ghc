2919d9a82165337c1434d3e73913bcb57d3798bf
Simon Marlow <marlowsd@gmail.com>
2010-12-01 14:41:35 +0000

    add test for #4808

---
+++ testsuite/tests/ghc-regress/lib/IO/4808.hs
@@ -0,0 +1,12 @@
+import System.IO
+import GHC.IO.Handle
+import GHC.IO.FD as FD
+
+main = do
+  (fd, _) <- FD.openFile "4808.hs" ReadWriteMode
+  hdl <- mkDuplexHandle fd "4808.hs" Nothing nativeNewlineMode
+  hClose hdl
+  (fd2, _) <- FD.openFile "4808.hs" ReadWriteMode
+  print (fdFD fd == fdFD fd2) -- should be True
+  hGetLine hdl >>= print -- should fail with an exception
+
