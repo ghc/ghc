ce286f4d578dc1da63e397a87f053cd5bcb21ad6
simonpj <simonpj@microsoft.com>
2010-12-15 17:27:01 +0000

    T3208b rightly rejects the program

--- testsuite/tests/ghc-regress/indexed-types/should_compile/T3208b.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_compile/T3208b.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeFamilies #-}
 
+-- This should fail
+
 module T3208b where
   
 class SUBST s where
@@ -10,7 +12,11 @@ class OBJECT o where
     apply :: (SUBST s, OTerm o ~ STerm s) => s -> o
 
 fce' :: (OTerm a ~ STerm a, OBJECT a, SUBST a) => a -> c
-fce' f = fce . apply $ f
+fce' f = fce (apply f)
+-- f :: a
+-- apply f :: (OBJECT a, SUBST a, OTerm o ~ STerm a) => o
+-- fce called with a=o, gives wanted (OTerm o ~ STerm o, OBJECT o, SUBST o)
+
 
 fce :: (OTerm a ~ STerm a, OBJECT a, SUBST a) => a -> c
 fce f = fce' f
