1f23f5d2588594616070486f35d8c16e384f8427
simonpj <simonpj@microsoft.com>
2010-12-24 11:02:04 +0000

    Comments only

--- testsuite/tests/ghc-regress/indexed-types/should_fail/T3330c.hs
+++ testsuite/tests/ghc-regress/indexed-types/should_fail/T3330c.hs
@@ -21,3 +21,38 @@ plug = plug' rep where
 
 plug' :: R f -> Der f x -> x -> f x
 plug' (RSum rf rg) (Inl df) x = Inl (plug rf df x)
+
+{-
+rf :: R f1, rg :: R g1
+Given by GADT match: f ~ f1 :+: g1
+
+Second arg has type (Der f x)
+    = (Der (f1:+:g1) x)
+    = (:+:) (Der f1) (Der g1) x
+Hence df :: Der f1 x
+
+Inl {f3,g3,x} (plug {f2,x1} rf df x)  gives rise to
+  result of Inl:   ((:+:) f3 g3 x ~ f x)
+  first arg (rf):  (R f1 ~ Der f2 x1)
+  second arg (df): (Der f1 x ~ x1)
+  result of plug:  (f2 x1 ~ x -> f3 x)
+
+  result of Inl: ((:+:) f3 g3 x ~ f x)
+       by given  ((:+:) f3 g3 x ~ (:+:) f1 g1 x)
+  hence need f3~f1, g3~g1
+
+So we are left with
+   first arg:      (R f1 ~ Der f2 x1)
+   second arg:     (Der f1 x ~ x1)
+   result:         (f2 x1  ~  (->) x (f3 x))
+
+Decompose result:
+          f2 ~ (->) x
+          x1 ~ f1 x
+Hence
+        first:  R f1 ~ Der ((->) x) (f1 x)
+  decompose  : R ~ Der ((->) x)
+               f1 ~ f1 x
+
+
+-}
