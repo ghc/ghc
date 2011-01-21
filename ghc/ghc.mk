d17afd1e4aa5c7a0992e5ac5b2cda325992706a8
Ian Lynagh <igloo@earth.li>
2011-01-21 23:05:52 +0000

    Manually control more of the Cabal flags for the compiler and ghc packages For some reason the Windows HEAD builder has started thinking the ghci flag should be on in stage 1. This should fix it, and generally make things a little more resilient.

--- ghc/ghc.mk
+++ ghc/ghc.mk
@@ -14,6 +14,14 @@
 ghc_USES_CABAL = YES
 ghc_PACKAGE = ghc-bin
 
+ghc_stage1_CONFIGURE_OPTS += --flags=stage1
+ghc_stage2_CONFIGURE_OPTS += --flags=stage2
+ghc_stage3_CONFIGURE_OPTS += --flags=stage3
+
+ifeq "$(GhcWithInterpreter)" "YES"
+ghc_stage2_CONFIGURE_OPTS += --flags=ghci
+endif
+
 ghc_stage1_MORE_HC_OPTS = $(GhcStage1HcOpts)
 ghc_stage2_MORE_HC_OPTS = $(GhcStage2HcOpts)
 ghc_stage3_MORE_HC_OPTS = $(GhcStage3HcOpts)
