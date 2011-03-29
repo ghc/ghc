35cf5aba99f5218fcb9415aa31fa92769d8fe586
Ian Lynagh <igloo@earth.li>
2011-03-29 17:53:56 +0000

    Test 4808: Follow change in GHC.IO.FD.openFile definition

--- testsuite/tests/ghc-regress/lib/IO/4808.hs
+++ testsuite/tests/ghc-regress/lib/IO/4808.hs
@@ -3,10 +3,10 @@ import GHC.IO.Handle
 import GHC.IO.FD as FD
 
 main = do
-  (fd, _) <- FD.openFile "4808.hs" ReadWriteMode
+  (fd, _) <- FD.openFile "4808.hs" ReadWriteMode False
   hdl <- mkDuplexHandle fd "4808.hs" Nothing nativeNewlineMode
   hClose hdl
-  (fd2, _) <- FD.openFile "4808.hs" ReadWriteMode
+  (fd2, _) <- FD.openFile "4808.hs" ReadWriteMode False
   print (fdFD fd == fdFD fd2) -- should be True
   hGetLine hdl >>= print -- should fail with an exception
 
