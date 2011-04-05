74040815b91987357f0e18778bf50ed730c839aa
Simon Marlow <marlowsd@gmail.com>
2011-04-05 13:34:12 +0100

    add some debugging traces

--- testsuite/tests/ghc-regress/rts/testblockalloc.c
+++ testsuite/tests/ghc-regress/rts/testblockalloc.c
@@ -1,5 +1,4 @@
 #include "Rts.h"
-#include "RtsFlags.h"
 
 #include <stdio.h>
 
@@ -9,7 +8,7 @@ extern void freeGroup_lock(bdescr *p);
 const int ARRSIZE  = 256;
 const int LOOPS    = 100;
 const int MAXALLOC = ((8 * 1024 * 1024) / BLOCK_SIZE - 1);
-//const int MAXALLOC = ((4 * 1024 * 1024) / BLOCK_SIZE - 1);
+//const int MAXALLOC = ((64 * 1024 * 1024) / BLOCK_SIZE - 1);
 const int SEED     = 0xf00f00;
 
 extern lnat mblocks_allocated;
@@ -24,27 +23,30 @@ int main (int argc, char *argv[])
 
     hs_init(&argc, &argv);
 
-    // repeatedly sweep though the array, allocating new random-sized
-    // objects and deallocating the old ones.
-    for (i=0; i < LOOPS; i++)
-    {
-        for (j=0; j < ARRSIZE; j++)
-        {
-            if (i > 0)
-            {
-                freeGroup_lock(a[j]);
-                DEBUG_ONLY(checkFreeListSanity());
-            }
-            a[j] = allocGroup_lock((rand() % MAXALLOC) + 1);
-            // allocating zero blocks isn't allowed
-            DEBUG_ONLY(checkFreeListSanity());
-        }
-    }
-    
-    for (j=0; j < ARRSIZE; j++)
-    {
-        freeGroup_lock(a[j]);
-    }
+   // repeatedly sweep though the array, allocating new random-sized
+   // objects and deallocating the old ones.
+   for (i=0; i < LOOPS; i++)
+   {
+       for (j=0; j < ARRSIZE; j++)
+       {
+           if (i > 0)
+           {
+               IF_DEBUG(block_alloc, debugBelch("A%d: freeing %p, %d blocks @ %p\n", j, a[j], a[j]->blocks, a[j]->start));
+               freeGroup_lock(a[j]);
+               DEBUG_ONLY(checkFreeListSanity());
+           }
+           b = (rand() % MAXALLOC) + 1;
+           a[j] = allocGroup_lock(b);
+           IF_DEBUG(block_alloc, debugBelch("A%d: allocated %p, %d blocks @ %p\n", j, a[j], b, a[j]->start));
+           // allocating zero blocks isn't allowed
+           DEBUG_ONLY(checkFreeListSanity());
+       }
+   }
+
+   for (j=0; j < ARRSIZE; j++)
+   {
+       freeGroup_lock(a[j]);
+   }
     
     // this time, sweep forwards allocating new blocks, and then
     // backwards deallocating them.
@@ -52,11 +54,14 @@ int main (int argc, char *argv[])
     {
         for (j=0; j < ARRSIZE; j++)
         {
-            a[j] = allocGroup_lock((rand() % MAXALLOC) + 1);
+            b = (rand() % MAXALLOC) + 1;
+            a[j] = allocGroup_lock(b);
+            IF_DEBUG(block_alloc, debugBelch("B%d,%d: allocated %p, %d blocks @ %p\n", i, j, a[j], b, a[j]->start));
             DEBUG_ONLY(checkFreeListSanity());
         }
         for (j=ARRSIZE-1; j >= 0; j--)
         {
+            IF_DEBUG(block_alloc, debugBelch("B%d,%d: freeing %p, %d blocks @ %p\n", i, j, a[j], a[j]->blocks, a[j]->start));
             freeGroup_lock(a[j]);
             DEBUG_ONLY(checkFreeListSanity());
         }
