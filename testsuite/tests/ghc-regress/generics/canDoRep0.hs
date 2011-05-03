92af81751072c7a18abf87173aa844432cbaa59f
Jose Pedro Magalhaes <jpm@cs.uu.nl>
2011-05-03 13:22:26 +0200

    More tests for the new generic deriving mechanism.

--- testsuite/tests/ghc-regress/generics/canDoRep0.hs
+++ testsuite/tests/ghc-regress/generics/canDoRep0.hs
@@ -12,3 +12,7 @@ data C = C0 | C1
 data D a = D0 | D1 { d11 :: a, d12 :: (D a) }
 
 data E a = E0 a (E a) (D a)
+
+-- We do not support datatype contexts, but this should still compile
+-- (Context will simply have no Representable0 instance)
+data (Show a) => Context a = Context a
