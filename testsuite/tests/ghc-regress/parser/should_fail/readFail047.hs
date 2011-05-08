412c310bbb6ef268101708db6d2bfb53fab83fdd
Ian Lynagh <igloo@earth.li>
2011-05-08 21:14:31 +0100

    Move tests out of h98; directory now removed

--- testsuite/tests/ghc-regress/parser/should_fail/readFail047.hs
+++ testsuite/tests/ghc-regress/parser/should_fail/readFail047.hs
@@ -1,8 +1,9 @@
 
+module ShouldFail where
+
 foo = let
     x = (1, 2
     y = 3
   in
   fst x + y
 
-main = print foo
--- testsuite/tests/h98/fields/empty.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-data T = T1 Int Int Int
-       | T2 Float Float Float
-
-f (T1 {}) = True
-f (T2 {}) = False
-
-main = print (f (T1 1 2 3))
