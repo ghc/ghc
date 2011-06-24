5505d8770e743d0630d05dd60e27feaa061c7a4c
Max Bolingbroke <batterseapower@hotmail.com>
2011-06-24 15:01:43 +0100

    Fix 3307 on Windows

--- testsuite/tests/ghc-regress/lib/IO/3307.hs
+++ testsuite/tests/ghc-regress/lib/IO/3307.hs
@@ -25,18 +25,19 @@ main = do
     readFile file >>= putStr
 
     -- 3) A file name occurring literally in the program
-    -- This will only work if we are in the UTF-8 locale since the file is created
-    -- on disk with a UTF-8 file name.
+    -- The file is created with a UTF-8 file name as well, so this will only work in Windows or a
+    -- UTF-8 locale, or this string will be encoded in some non-UTF-8 way and won't match.
     putStrLn "Test 3"
     let file = "chinese-file-小说"
     print $ map ord file
     readFile file >>= putStr
 
     -- 4) A file name arriving via another file.
-    -- In this case we have to override the default encoding
-    -- so we get surrogate bytes for non-decodable namse.
+    -- Again, the file is created with UTF-8 contents, so we read it in that encoding.
+    -- Once again, on non-Windows this may fail in a non-UTF-8 locale because we could encode the valid
+    -- filename string into a useless non-UTF-8 byte sequence.
     putStrLn "Test 4"
-    str <- readFileAs fileSystemEncoding "chinese-name"
+    str <- readFileAs utf8 "chinese-name"
     let file = dropTrailingSpace str
     print $ map ord file
     readFile file >>= putStr
