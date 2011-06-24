8a1d08338fb5d006cd8daa6606722636d0f76fed
Ian Lynagh <igloo@earth.li>
2011-06-24 01:49:45 +0100

    Tweak environment001

--- testsuite/tests/ghc-regress/lib/IO/environment001.hs
+++ testsuite/tests/ghc-regress/lib/IO/environment001.hs
@@ -5,12 +5,12 @@ main = do
     putStrLn var0
     -- The length proves that we actually decoded it properly, not just read it
     -- in as latin1 or something (#3308, #3307)
-    print (length var0)
-    
+    putStrLn ("Test 1: " ++ show (length var0))
+
     [arg0] <- getArgs
     putStrLn arg0
-    print (length arg0)
-    
+    putStrLn ("Test 2: " ++ show (length arg0))
+
     [arg1] <- withArgs ["你好!"] getArgs
     putStrLn arg1
-    print (length arg1)
+    putStrLn ("Test 3: " ++ show (length arg1))
