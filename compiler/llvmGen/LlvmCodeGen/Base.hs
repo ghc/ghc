ac0800b6bfdb7b1f3895fe33981e0bc853b0d409
David Terei <davidterei@gmail.com>
2011-06-27 23:57:42 -0700

    Fix LLVM backend in unregisterised build to use C calling convention.
    Patch based on one by Karel Gardas.

--- compiler/llvmGen/LlvmCodeGen/Base.hs
+++ compiler/llvmGen/LlvmCodeGen/Base.hs
@@ -29,6 +29,7 @@ import LlvmCodeGen.Regs
 
 import CLabel
 import CgUtils ( activeStgRegs )
+import Config
 import Constants
 import FastString
 import OldCmm
@@ -80,7 +81,8 @@ widthToLlvmInt w = LMInt $ widthInBits w
 
 -- | GHC Call Convention for LLVM
 llvmGhcCC :: LlvmCallConvention
-llvmGhcCC = CC_Ncc 10
+llvmGhcCC | cGhcUnregisterised == "NO" = CC_Ncc 10
+          | otherwise                  = CC_Ccc
 
 -- | Llvm Function type for Cmm function
 llvmFunTy :: LlvmType
