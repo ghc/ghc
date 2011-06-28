1053d725b3520457c81e8d5000ca9d573816f88e
Simon Marlow <marlowsd@gmail.com>
2011-06-28 08:51:37 +0100

    omit conc016(threaded2), it gives nondeterministic results

--- testsuite/tests/ghc-regress/concurrent/should_run/conc016.hs
+++ testsuite/tests/ghc-regress/concurrent/should_run/conc016.hs
@@ -4,12 +4,13 @@ import Control.Exception
 -- check that we can still kill a thread that is blocked on
 -- delivering an exception to us.
 
--- NB. this test is delicate since 6.14, because throwTo is now always
--- interruptible, so the main thread's killThread can be legitimately
--- interrupted by the child thread's killThread, rather than the other
--- way around.  This happens because the child thread is running on
--- another processor, so the main thread's throwTo is blocked waiting
--- for a response, and while waiting it is interruptible.
+-- NB. this test is non-deterministic in the threaded2 way since 6.14,
+-- because throwTo is now always interruptible, so the main thread's
+-- killThread can be legitimately interrupted by the child thread's
+-- killThread, rather than the other way around.  This happens because
+-- the child thread is running on another processor, so the main
+-- thread's throwTo is blocked waiting for a response, and while
+-- waiting it is interruptible.
 
 main = do
   main_thread <- myThreadId
