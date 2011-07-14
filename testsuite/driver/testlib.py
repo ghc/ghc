db53da38363588f4ab929d5bb00605b2c455d247
Ian Lynagh <igloo@earth.li>
2011-07-14 15:14:58 +0100

    Fix some (significant!) white space

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -1193,7 +1193,7 @@ def split_file(in_fn, delimiter, out1_fn, out2_fn):
     while (re.sub('^\s*','',line) != delimiter and line != ''):
         out1.write(line)
         line = infile.readline()
-    line = re.sub('\r', '', line)
+        line = re.sub('\r', '', line)
     out1.close()
 
     line = infile.readline()
