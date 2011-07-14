5cf87b43993f5fd2884755cd16268c2f40572026
Karel Gardas <karel.gardas@centrum.cz>
2011-07-14 22:21:09 +0200

    disable for now ARM specific target data layout and triple
    This patch disables ARM specific target data layout and triple.
    The reason for this is that LLVM asserts on some files if this
    is in use. The assert looks:
    Formal argument #8 has unhandled type i32UNREACHABLE executed at
    /llvm-ghc-arm/lib/CodeGen/CallingConvLower.cpp:81!

--- compiler/llvmGen/LlvmCodeGen/Ppr.hs
+++ compiler/llvmGen/LlvmCodeGen/Ppr.hs
@@ -51,12 +51,12 @@ moduleLayout =
     $+$ text "target triple = \"x86_64-linux-gnu\""
 #endif
 
-#elif defined (arm_TARGET_ARCH)
+-- #elif defined (arm_TARGET_ARCH)
 
-#if linux_TARGET_OS
-    text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32\""
-    $+$ text "target triple = \"arm-unknown-linux-gnueabi\""
-#endif
+-- #if linux_TARGET_OS
+--    text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32\""
+--    $+$ text "target triple = \"arm-unknown-linux-gnueabi\""
+-- #endif
 
 #else
     -- FIX: Other targets
