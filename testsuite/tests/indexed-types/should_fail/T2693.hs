07f62bf90c9c790fedda50f45ef80c0da49524b8
Simon Peyton Jones <simonpj@microsoft.com>
2011-08-16 11:45:03 +0100

    Add some new code to T2693

--- testsuite/tests/indexed-types/should_fail/T2693.hs
+++ testsuite/tests/indexed-types/should_fail/T2693.hs
@@ -4,8 +4,27 @@ module T2693 where
 
 type family TFn a :: *
 
+-- Boiled down version
 f :: Maybe ()
 f = do
   let Just x = undefined :: Maybe (TFn a)
   let n = fst x + fst x
   return ()
+
+f2 :: Maybe ()
+f2 = do
+  let x :: TFn a
+      x = undefined 
+  let n = fst x + snd x
+  return ()
+
+-- Close to the original report
+data PVR a = PVR {pvrX :: Int} deriving (Eq)
+
+g :: () -> Maybe (TFn a)
+g _ = undefined
+
+h :: Maybe ()
+h = do pvs <- mapM g undefined
+       let n = (map pvrX pvs) `min` (map pvrX pvs)
+       undefined
