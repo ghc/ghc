4a86a0bff7e8fb3e87708f29adf87bf566632861
Simon Marlow <marlowsd@gmail.com>
2011-08-23 16:08:43 +0100

    remove duplicate dump

--- compiler/codeGen/StgCmm.hs
+++ compiler/codeGen/StgCmm.hs
@@ -78,8 +78,6 @@ codeGen dflags this_mod data_tycons
                 -- initialisation routines; see Note
                 -- [pipeline-split-init].
 
-        ; dumpIfSet_dyn dflags Opt_D_dump_cmmz "New Cmm" (pprCmms (targetPlatform dflags) code_stuff)
-
         ; return code_stuff }
 
 
