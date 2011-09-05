a6b7bb8476f8877d9f9f7bdfacce390789a4ca4e
Simon Peyton Jones <simonpj@microsoft.com>
2011-09-05 17:28:46 +0100

    Wibbles for Trac #5455 test

--- testsuite/tests/deSugar/should_compile/T5455.hs
+++ testsuite/tests/deSugar/should_compile/T5455.hs
@@ -1,10 +1,13 @@
 {-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
 module T5455 where
 
--- We should get just one error message for each defn
+-- No error message for this one: 
+-- the pattern will never be demanded
 
 w :: String -> String
 w x = let (_:_) = x in "1"
 
+-- We should get just one error message here
+
 w2 :: String -> String
-w2 x2 = let (_a:_b) = x2 in "1"
+w2 x2 = let (a:as) = x2 in (a:a:as)
