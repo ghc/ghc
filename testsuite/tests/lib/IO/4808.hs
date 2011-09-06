12850c11733d0919f6df6c4ab2c6d40495bb7198
Ian Lynagh <igloo@earth.li>
2011-09-06 17:21:56 +0100

    Fix some more written-but-not-cleaned files

--- testsuite/tests/lib/IO/4808.hs
+++ testsuite/tests/lib/IO/4808.hs
@@ -3,10 +3,11 @@ import GHC.IO.Handle
 import GHC.IO.FD as FD
 
 main = do
-  (fd, _) <- FD.openFile "4808.hs" ReadWriteMode False
-  hdl <- mkDuplexHandle fd "4808.hs" Nothing nativeNewlineMode
+  writeFile "4808.test" "This is some test data"
+  (fd, _) <- FD.openFile "4808.test" ReadWriteMode False
+  hdl <- mkDuplexHandle fd "4808.test" Nothing nativeNewlineMode
   hClose hdl
-  (fd2, _) <- FD.openFile "4808.hs" ReadWriteMode False
+  (fd2, _) <- FD.openFile "4808.test" ReadWriteMode False
   print (fdFD fd == fdFD fd2) -- should be True
   hGetLine hdl >>= print -- should fail with an exception
 
