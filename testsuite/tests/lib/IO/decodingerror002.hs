767c1b7279c2568e0c6a539858b71fb32379830a
Max Bolingbroke <batterseapower@hotmail.com>
2011-09-24 22:01:06 +0900

    Attempt to fix decodingerror002 test for non-UTF8 locales

--- testsuite/tests/lib/IO/decodingerror002.hs
+++ testsuite/tests/lib/IO/decodingerror002.hs
@@ -3,7 +3,12 @@ import System.IO
 import System.IO.Error
 import GHC.IO.Handle (hSetEncoding)
 
-main = alltests "decodingerror002.in"
+main = do
+  -- Explicitly set stdout encoding so that the UTF8//ROUNDTRIP
+  -- test is always able to write the surrogate byte out without error.
+  enc <- mkTextEncoding "UTF8//ROUNDTRIP"
+  hSetEncoding stdout enc
+  alltests "decodingerror002.in"
 
 alltests file = mapM (test file)  ["UTF8",
                                    "UTF8//IGNORE",
