1e3708f48a42ad4e7e3d7df5ee38a7d6fd63e64f
Ian Lynagh <igloo@earth.li>
2011-09-30 20:27:50 +0100

    Add another case to cc015

--- testsuite/tests/ffi/should_compile/cc015.hs
+++ testsuite/tests/ffi/should_compile/cc015.hs
@@ -3,6 +3,8 @@
 
 module Cc015 where
 
+import Foreign.C
+
 type S a = a
 type IOS a = IO a
 
@@ -15,6 +17,7 @@ newtype G1F = G1F (F (S Int))
 newtype G2  = G2  Char
 newtype G3  = G3  (IO Int)
 newtype G4  = G4  G3
+newtype NIO a = NIO (IO a)
 
 -- Type synonyms should be transparent to the typechecker
 foreign import ccall f1      :: S Int -> IOS Int
@@ -28,4 +31,7 @@ foreign export ccall "g3" f3 :: G1 -> G2 -> G4
 -- And a combination
 foreign import ccall f4      :: G1F -> F Bool -> S G4
 foreign export ccall "g4" f4 :: G1F -> F Bool -> S G4
+-- And a newtyped IO
+foreign import ccall f5      :: NIO Int
+foreign export ccall "g5" f5 :: NIO Int
 
