18a64d159ed4fc49715d147aadc41c29c130e941
Simon Marlow <marlowsd@gmail.com>
2011-10-07 14:56:46 +0100

    make the test fail if the sleep doesn't get interrupted (#5471)

--- testsuite/tests/concurrent/should_run/foreignInterruptible.hs
+++ testsuite/tests/concurrent/should_run/foreignInterruptible.hs
@@ -20,10 +20,17 @@ main :: IO ()
 main = do
   newStablePtr stdout -- prevent stdout being finalized
   th <- newEmptyMVar
+
   tid <- forkIO $ do
      putStrLn "newThread started"
      (sleep 2 >> putStrLn "fail") `catch` (\ThreadKilled -> putStrLn "pass")
      putMVar th "child"
+
+  -- if the killThread below gets blocked for more than a second, then
+  -- this thread will kill the main thread and the test will fail.
+  main <- myThreadId
+  forkIO $ do threadDelay 1000000; throwTo main (ErrorCall "still waiting")
+
   yield
   threadDelay 500000
   killThread tid
