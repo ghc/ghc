1bbb89f3ab009367fcca84b73b351ddcf5be16a4
Simon Marlow <marlowsd@gmail.com>
2011-11-16 13:00:55 +0000

    Make the --fast option to validate faster, and add --normal
    The --fast option now disables the following:
      - dynamic libs
      - bindist and bindisttest
    
    Which knocks several minutes off validate for me, but it's still over
    30 minutes using 5 cores on 64-bit Linux.
    
    Usual caveats apply: if you're using --fast, then make sure you aren't
    doing anything that might destabilise dynamic libs or binary dists.

--- mk/validate-settings.mk
+++ mk/validate-settings.mk
@@ -27,7 +27,11 @@ GhcStage2HcOpts += -O -fwarn-tabs -dcore-lint
 # running of the tests, and faster building of the utils to be installed
 
 GhcLibHcOpts    += -O -dcore-lint
+ifeq "$(ValidateSpeed)" "FAST"
+GhcLibWays     := v
+else
 GhcLibWays     := $(filter v dyn,$(GhcLibWays))
+endif
 SplitObjs       = NO
 NoFibWays       =
 STRIP_CMD       = :
@@ -47,7 +51,7 @@ BUILD_DOCBOOK_PDF = NO
 ifeq "$(ValidateHpc)" "YES"
 GhcStage2HcOpts += -fhpc -hpcdir $(TOP)/testsuite/hpc_output/
 endif
-ifeq "$(ValidateSlow)" "YES"
+ifeq "$(ValidateSpeed)" "SLOW"
 GhcStage2HcOpts += -DDEBUG
 endif
 
