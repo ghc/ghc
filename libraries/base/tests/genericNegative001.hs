180933b3378da25f3ca2fe69d4a33828c06456f8
Ian Lynagh <igloo@earth.li>
2011-11-19 23:23:46 +0000

    Whitespace only in tests

--- libraries/base/tests/genericNegative001.hs
+++ libraries/base/tests/genericNegative001.hs
@@ -1,7 +1,7 @@
 -- Test for http://hackage.haskell.org/trac/ghc/ticket/2533
 import System.Environment
 import Data.List
-main = do 
+main = do
  (n:_) <- getArgs
  print (genericTake (read n) "none taken")
  print (genericDrop (read n) "none dropped")
