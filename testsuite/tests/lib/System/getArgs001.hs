0c2c601ed6a773a8375b1ba759391c60621d00ee
Ian Lynagh <igloo@earth.li>
2011-11-19 23:43:32 +0000

    Move lib/System tests to base/tests

--- testsuite/tests/lib/System/getArgs001.hs
+++
@@ -1,9 +0,0 @@
-import System.Environment (getProgName, getArgs)
-
-main = 
-    getProgName >>= \ argv0 ->
-    putStr argv0 >>
-    getArgs >>= \ argv ->
-    sequence (map (\ x -> putChar ' ' >> putStr x) argv) >>
-    putChar '\n'
-
