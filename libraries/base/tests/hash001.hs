56d233a1edc6e64e768b12696b9337e3d5cd59da
Ian Lynagh <igloo@earth.li>
2011-11-19 23:14:21 +0000

    Whitespace only in hash001 test

--- libraries/base/tests/hash001.hs
+++ libraries/base/tests/hash001.hs
@@ -1,3 +1,4 @@
+
 import Data.HashTable
 import qualified Data.HashTable as HT
 
@@ -5,10 +6,11 @@ test :: Int -> IO ()
 test n = do ht <- new (==) hashInt
             sequence_ [ insert ht key 0 | key <- [0..n]]
             sequence_ [ insert ht key 1 | key <- [0..n]]
-          
+
             let check key = do (Just val) <- HT.lookup ht key
-                               if val==1 then return () else putStrLn $ show key 
-          
+                               if val==1 then return ()
+                                         else putStrLn $ show key
+
             sequence_ [ check key | key <- [0..n]]
 
 main = test 2048
