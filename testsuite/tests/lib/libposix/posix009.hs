b65721412f00429c73d21f94a646cb0c7765da1f
Ian Lynagh <igloo@earth.li>
2011-11-20 14:11:53 +0000

    Fix and reenalbe posix009

--- testsuite/tests/lib/libposix/posix009.hs
+++ testsuite/tests/lib/libposix/posix009.hs
@@ -1,15 +1,15 @@
-import Posix
+import System.Posix.Signals
+import System.Posix.Unistd
 
-main =
-    putStr "Blocking real time alarms.\n" >>
-    blockSignals (addSignal realTimeAlarm emptySignalSet) >>
-    putStr "Scheduling an alarm in 2 seconds...\n" >>
-    scheduleAlarm 2 >>
-    putStr "Sleeping 5 seconds.\n" >>
-    sleep 5 >>
-    putStrLn "Woken up" >>
-    getPendingSignals >>= \ ints ->
-    putStr "Checking pending interrupts for RealTimeAlarm\n" >>
-    print (inSignalSet realTimeAlarm ints) >>
-    putChar '\n'
+main = do
+    putStrLn "Blocking real time alarms."
+    blockSignals (addSignal realTimeAlarm emptySignalSet)
+    putStrLn "Scheduling an alarm in 2 seconds..."
+    scheduleAlarm 2
+    putStrLn "Sleeping 5 seconds."
+    sleep 5
+    putStrLn "Woken up"
+    ints <- getPendingSignals
+    putStrLn "Checking pending interrupts for RealTimeAlarm"
+    print (inSignalSet realTimeAlarm ints)
 
