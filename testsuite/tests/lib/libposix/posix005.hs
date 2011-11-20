9a53d8f87da6e595e0b2d9553d960e247642ac9a
Ian Lynagh <igloo@earth.li>
2011-11-20 13:08:37 +0000

    Mostly fix posix005. Complete fix blocked on #5648.

--- testsuite/tests/lib/libposix/posix005.hs
+++ testsuite/tests/lib/libposix/posix005.hs
@@ -1,25 +1,21 @@
-import Posix
-import IO
-main =
-    hSetBuffering stdout NoBuffering >>
-    getEnvVar "TERM" >>= \ term ->
-    putStrLn term >>
-    setEnvironment [("one","1"),("two","2")] >>
-    getEnvironment >>= \ env ->
-    print env >>
-    setEnvVar "foo" "bar" >>
-    getEnvironment >>= \ env ->
-    print env >>
-    setEnvVar "foo" "baz" >>
-    getEnvironment >>= \ env ->
-    print env >>
-    setEnvVar "fu" "bar" >>
-    getEnvironment >>= \ env ->
-    print env >>
-    removeEnvVar "foo" >>
-    getEnvironment >>= \ env ->
-    print env >>
-    setEnvironment [] >>
-    getEnvironment >>= \ env ->
-    print env
+
+import System.IO
+import System.Posix.Env
+
+main = do
+    hSetBuffering stdout NoBuffering
+    term <- getEnvVar "TERM"
+    putStrLn term
+    setEnvironment [("one","1"),("two","2")]
+    getEnvironment >>= print
+    setEnv "foo" "bar" True
+    getEnvironment >>= print
+    setEnv "foo" "baz" True
+    getEnvironment >>= print
+    setEnv "fu" "bar" True
+    getEnvironment >>= print
+    unsetEnv "foo"
+    getEnvironment >>= print
+    setEnvironment []
+    getEnvironment >>= print
 
