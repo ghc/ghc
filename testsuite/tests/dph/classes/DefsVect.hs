8a7e20d37ca913912f872bd3f82e05b654d502e5
Manuel M T Chakravarty <chak@cse.unsw.edu.au>
2011-11-25 22:59:38 +1100

    Test vectorisation of type classes some more

--- testsuite/tests/dph/classes/DefsVect.hs
+++ testsuite/tests/dph/classes/DefsVect.hs
@@ -1,13 +1,23 @@
+{-# LANGUAGE ParallelArrays #-}
 {-# OPTIONS_GHC -fvectorise #-}
 
 module DefsVect where
 
+import Data.Array.Parallel
 import Data.Array.Parallel.Prelude.Bool
 import Data.Array.Parallel.Prelude.Int  (Int)
 
 
 {-# VECTORISE class Eq #-}
 {-# VECTORISE SCALAR instance Eq Int #-}
+
+-- {-# VECTORISE class Ord #-}
+-- {-# VECTORISE SCALAR instance Ord Int #-}
+
+-- {-# VECTORISE type Bool = Bool #-}
+-- {-# VECTORISE type Ordering #-}
+
+data MyBool = MyTrue | MyFalse
 
 class Eq a => Cmp a where
   cmp :: a -> a -> Bool
@@ -20,3 +30,24 @@ isEq x = x == x
 
 fiveEq :: Int -> Bool
 fiveEq x = isFive x && isEq x
+
+cmpArrs :: PArray Int -> PArray Int -> Bool
+{-# NOINLINE cmpArrs #-}
+cmpArrs v w = cmpArrs' (fromPArrayP v) (fromPArrayP w)
+
+cmpArrs' :: [:Int:] -> [:Int:] -> Bool
+cmpArrs' xs ys = andP [:x == y | x <- xs | y <- ys:]
+
+isFives :: PArray Int -> Bool
+{-# NOINLINE isFives #-}
+isFives xs = isFives' (fromPArrayP xs)
+
+isFives' :: [:Int:] -> Bool
+isFives' xs = andP (mapP isFive xs)
+
+isEqs :: PArray Int -> Bool
+{-# NOINLINE isEqs #-}
+isEqs xs = isEqs' (fromPArrayP xs)
+
+isEqs' :: [:Int:] -> Bool
+isEqs' xs = andP (mapP isEq xs)
--- testsuite/tests/dph/classes/Main.hs
+++ testsuite/tests/dph/classes/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
+import Data.Array.Parallel.PArray (PArray, fromList)
+
 import DefsVect
 
-main = print $ fiveEq 5
-
+main 
+  = let v      = fromList [1..10]
+        w      = fromList [11..20]
+  in
+  print $ [ fiveEq 5
+          , cmpArrs v w
+          , isFives (fromList $ replicate 10 5)
+          , isEqs v
+          ]
