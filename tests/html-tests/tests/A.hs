565ad5298d293f3e5c263898bada858b000f4ae3
David Waern <david.waern@gmail.com>
2011-11-26 19:56:21 +0100

    Update tests.

--- tests/html-tests/tests/A.hs
+++ tests/html-tests/tests/A.hs
@@ -1,2 +1,8 @@
 module A where
 data A = A
+
+other :: Int
+other = 2
+
+test2 :: Bool
+test2 = False
--- tests/html-tests/tests/B.hs
+++ tests/html-tests/tests/B.hs
@@ -1,2 +1,7 @@
-module B ( module A ) where
-import A
+module B ( module A, test ) where
+import A ( A(..), test2 )
+
+-- | This link shouldn't work: 'other'.
+--   These links should work: 'A.other', 'Data.List.sortBy', 'test2', 'A.test2', 'Data.Maybe.fromMaybe'.
+test :: Int
+test = 1
