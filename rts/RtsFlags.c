529b0ae11c0ef2c2f1ba37050d5d0a5469a24ed3
Edward Z. Yang <ezyang@mit.edu>
2011-12-01 01:27:12 -0500

    Add missing newline in RTS help output.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- rts/RtsFlags.c
+++ rts/RtsFlags.c
@@ -249,7 +249,7 @@ usage_text[] = {
 "  -I<sec>  Perform full GC after <sec> idle time (default: 0.3, 0 == off)",
 #endif
 "",
-"  -T         Collect GC statistics (useful for in-program statistics access)"
+"  -T         Collect GC statistics (useful for in-program statistics access)",
 "  -t[<file>] One-line GC statistics (if <file> omitted, uses stderr)",
 "  -s[<file>] Summary  GC statistics (if <file> omitted, uses stderr)",
 "  -S[<file>] Detailed GC statistics (if <file> omitted, uses stderr)",
