d6705e972b905f5034bdc3e0b2fca65f9be33b22
Iavor S. Diatchki <iavor.diatchki@gmail.com>
2011-12-18 18:34:38 -0800

    Remove tabs, so that I can push.

--- compiler/cmm/Cmm.hs
+++ compiler/cmm/Cmm.hs
@@ -1,14 +1,6 @@
 -- Cmm representations using Hoopl's Graph CmmNode e x.
 {-# LANGUAGE GADTs #-}
 {-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
-
-{-# OPTIONS -fno-warn-tabs #-}
--- The above warning supression flag is a temporary kludge.
--- While working on this module you are encouraged to remove it and
--- detab the module (please do the detabbing in a separate patch). See
---     http://hackage.haskell.org/trac/ghc/wiki/Commentary/CodingStyle#TabsvsSpaces
--- for details
-
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 #if __GLASGOW_HASKELL__ >= 703
 -- GHC 7.0.1 improved incomplete pattern warnings with GADTs
@@ -137,7 +129,7 @@ data ProfilingInfo
 -- we add a label for the table, and expect only the 'offset/length' form
 
 data C_SRT = NoC_SRT
-	   | C_SRT !CLabel !WordOff !StgHalfWord {-bitmap or escape-}
+           | C_SRT !CLabel !WordOff !StgHalfWord {-bitmap or escape-}
            deriving (Eq)
 
 needsSRT :: C_SRT -> Bool
