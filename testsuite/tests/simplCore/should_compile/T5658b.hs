e948d22b6daea720ab80c254df79f752a8c4e77d
Simon Peyton Jones <simonpj@microsoft.com>
2012-01-12 18:19:41 +0000

    Test Trac #5658b

---
+++ testsuite/tests/simplCore/should_compile/T5658b.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE MagicHash, BangPatterns #-}
+module T5658b where
+import GHC.Prim
+
+foo :: ByteArray# -> ByteArray# -> Int# -> Int# -> Bool
+foo xs ys m n = go 0# 0#
+  where
+    go i j = case i >=# m of
+      False -> let !x = indexIntArray# xs i in
+        case j >=# n of
+          False -> case x ==# indexIntArray# ys j of
+            False -> False
+            True  -> go (i +# 1#) (j +# 1#)
+          True -> False
+      True -> case j >=# n of
+        False -> let !y = indexIntArray# ys i in False
+        True -> True
