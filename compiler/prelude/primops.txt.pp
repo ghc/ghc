a30c2df58c319c99550c9c66827c1a984622866a
Simon Peyton Jones <simonpj@microsoft.com>
2012-01-13 15:48:23 +0000

    Comments only

--- compiler/prelude/primops.txt.pp
+++ compiler/prelude/primops.txt.pp
@@ -1481,7 +1481,10 @@ primop  RaiseOp "raise#" GenPrimOp
 -- one kind of bottom into another, as it is allowed to do in pure code.
 --
 -- But we *do* want to know that it returns bottom after 
--- being applied to two arguments
+-- being applied to two arguments, so that this function is strict in y
+--     f x y | x>0  	 = raiseIO blah
+--           | y>0  	 = return 1
+--           | otherwise = return 2 
 
 primop  RaiseIOOp "raiseIO#" GenPrimOp
    a -> State# RealWorld -> (# State# RealWorld, b #)
