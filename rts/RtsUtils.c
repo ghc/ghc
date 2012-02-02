bf456a09f9ef68436db48eb5ea25193d3b2f2ed5
Simon Marlow <marlowsd@gmail.com>
2012-02-02 10:28:34 +0000

    avoid 32-bit integer overflow (#5831)

--- rts/RtsUtils.c
+++ rts/RtsUtils.c
@@ -130,7 +130,7 @@ heapOverflow(void)
     {
         /* don't fflush(stdout); WORKAROUND bug in Linux glibc */
         OutOfHeapHook(0/*unknown request size*/,
-                      RtsFlags.GcFlags.maxHeapSize * BLOCK_SIZE);
+                      (lnat)RtsFlags.GcFlags.maxHeapSize * BLOCK_SIZE);
 
         heap_overflow = rtsTrue;
     }
