95fd4a1ae1511892a94cdbdecb2e5e630d75878b
Max Bolingbroke <batterseapower@hotmail.com>
2012-04-20 15:15:24 +0100

    Add FIXME reminding me to remove PrimOp

--- compiler/supercompile/Supercompile/Core/Syntax.hs
+++ compiler/supercompile/Supercompile/Core/Syntax.hs
@@ -128,7 +128,7 @@ data TermF ann = Var Id
                | TyApp (ann (TermF ann)) Type
                | CoApp (ann (TermF ann)) Coercion
                | App (ann (TermF ann)) Id
-               | PrimOp PrimOp [Type] [ann (TermF ann)]     -- FIXME: arguably we have just Vars as arguments for better Tag behaviour (otherwise improving the arguments is hidden by the Tag on the whole PrimOp stack frames)
+               | PrimOp PrimOp [Type] [ann (TermF ann)]     -- FIXME: arguably we have just Vars as arguments for better Tag behaviour (otherwise improving the arguments is hidden by the Tag on the whole PrimOp stack frames). FIXME: in fact, we need to change this because *NOT ALL PRIMOP ARGUMENTS ARE STRICT* (e.g. the lazy polymorphic arguments to newMutVar#, newArray#)
                | Case (ann (TermF ann)) Id Type [AltF ann]  -- NB: unlike GHC, for convenience we allow the list of alternatives to be empty
                | Let Id (ann (TermF ann)) (ann (TermF ann)) -- NB: might bind an unlifted thing, in which case evaluation changes. Unlike GHC, we do NOT assume the RHSes of unlifted bindings are ok-for-speculation.
                | LetRec [(Id, ann (TermF ann))] (ann (TermF ann))
