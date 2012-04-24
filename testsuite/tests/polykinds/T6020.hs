3b84b0a98a2c96726f921a1b10477bf4eee3a4f7
Simon Peyton Jones <simonpj@microsoft.com>
2012-04-24 09:30:35 +0100

    Test Trac #6020

---
+++ testsuite/tests/polykinds/T6020.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds, FunctionalDependencies, FlexibleInstances,
+             UndecidableInstances, PolyKinds, KindSignatures,
+             ConstraintKinds, FlexibleContexts #-}
+
+module T6020 where
+
+class Id (a :: k) (b :: k) | a -> b
+instance Id a a
+
+class Test (x :: a) (y :: a) | x -> y
+instance (Id x y, Id y z) => Test x z
+
+test :: Test True True => ()
+test = ()
