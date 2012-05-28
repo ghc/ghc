c38602fac5182f207a87177773fe2e6a4ca5a133
Ian Lynagh <igloo@earth.li>
2012-05-28 23:14:58 +0100

    Make traceBinIFaceReading use log_action
    It was printing directly to stdout

--- compiler/iface/BinIface.hs
+++ compiler/iface/BinIface.hs
@@ -85,7 +85,7 @@ readBinIface_ :: DynFlags -> CheckHiWay -> TraceBinIFaceReading -> FilePath
 readBinIface_ dflags checkHiWay traceBinIFaceReading hi_path ncu = do
     let printer :: SDoc -> IO ()
         printer = case traceBinIFaceReading of
-                      TraceBinIFaceReading -> \sd -> printSDoc sd defaultDumpStyle
+                      TraceBinIFaceReading -> \sd -> log_action dflags SevOutput noSrcSpan defaultDumpStyle sd
                       QuietBinIFaceReading -> \_ -> return ()
         wantedGot :: Outputable a => String -> a -> a -> IO ()
         wantedGot what wanted got =
