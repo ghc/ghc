aab1fe263304c63046f515201f2beed53a15fe68
Paolo Capriotti <p.capriotti@gmail.com>
2012-06-08 15:38:27 +0100

    Deprecate old -package-conf flags.

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -1785,8 +1785,12 @@ package_flags = [
   , Flag "user-package-db"       (NoArg (addPkgConfRef UserPkgConf))
 
     -- backwards compat with GHC<=7.4 :
-  , Flag "package-conf"          (HasArg (addPkgConfRef . PkgConfFile))
-  , Flag "no-user-package-conf"  (NoArg removeUserPkgConf)
+  , Flag "package-conf"          (HasArg $ \path -> do
+                                    addPkgConfRef (PkgConfFile path)
+                                    deprecate "Use -package-db instead")
+  , Flag "no-user-package-conf"  (NoArg $ do
+                                    removeUserPkgConf
+                                    deprecate "Use -no-user-package-db instead")
 
   , Flag "package-name"          (hasArg setPackageName)
   , Flag "package-id"            (HasArg exposePackageId)
