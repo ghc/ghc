ba5c1e9d7ee5bc6079b86fd39ef834081e77345d
Ian Lynagh <igloo@earth.li>
2012-06-08 20:39:29 +0100

    Add more cases to the integer constant folding test

--- testsuite/tests/lib/integer/integerConstantFolding.hs
+++ testsuite/tests/lib/integer/integerConstantFolding.hs
@@ -2,6 +2,7 @@
 module Main (main) where
 
 import Data.Bits
+import Data.Int
 import Data.Word
 
 main :: IO ()
@@ -49,12 +50,15 @@ main = do p "plusInteger"        plusInteger
           p "remInteger"        remInteger
           p "doubleFromInteger" doubleFromInteger
           p "floatFromInteger"  floatFromInteger
+          p "decodeIntegerDouble" decodeIntegerDouble
           p "encodeIntegerDouble" encodeIntegerDouble
           p "encodeIntegerFloat"  encodeIntegerFloat
           p "integerToWord"       integerToWord
           p "integerToInt"        integerToInt
           p "wordToInteger"       wordToInteger
           p "intToInteger"        intToInteger
+          p "word64ToInteger"     word64ToInteger
+          p "int64ToInteger"      int64ToInteger
 
     where p :: Show a => String -> a -> IO ()
           p str x = putStrLn (str ++ ": " ++ show x)
@@ -195,4 +199,13 @@ wordToInteger = toInteger (100075 :: Word) + 100076
 
 intToInteger :: Integer
 intToInteger = toInteger (100077 :: Int) + 100078
+
+word64ToInteger :: Integer
+word64ToInteger = toInteger (100079 :: Word64) + 100080
+
+int64ToInteger :: Integer
+int64ToInteger = toInteger (100081 :: Int64) + 100082
+
+decodeIntegerDouble :: (Integer, Int)
+decodeIntegerDouble = decodeFloat (100083 :: Double)
 
