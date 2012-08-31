23f4463f276fea717349b869da3e887f2e4e1bbd
Simon Peyton Jones <simonpj@microsoft.com>
2012-08-31 10:05:31 +0100

    Accept error message changes

--- testsuite/tests/indexed-types/should_compile/T3208b.hs
+++ testsuite/tests/indexed-types/should_compile/T3208b.hs
@@ -9,14 +9,25 @@ class SUBST s where
 
 class OBJECT o where
     type OTerm o
-    apply :: (SUBST s, OTerm o ~ STerm s) => s -> o
+    apply :: (SUBST a, OTerm o ~ STerm a) => a -> o
 
 fce' :: (OTerm a ~ STerm a, OBJECT a, SUBST a) => a -> c
 fce' f = fce (apply f)
 -- f :: a
--- apply f :: (OBJECT a, SUBST a, OTerm o ~ STerm a) => o
+-- apply f :: (SUBST a, OBJECT o, OTerm o ~ STerm a) => o
 -- fce called with a=o, gives wanted (OTerm o ~ STerm o, OBJECT o, SUBST o)
+-- o is a unif var.
 
-
-fce :: (OTerm a ~ STerm a, OBJECT a, SUBST a) => a -> c
+fce :: (OTerm o ~ STerm o, OBJECT o, SUBST o) => o -> c
 fce f = fce' f
+
+
+{-  (OTerm a ~ STerm a, OBJECT a, SUBST a)
+ => (SUBST a, OBJECT o, OTerm o ~ STerm a, OTerm o ~ STerm o, OBJECT o, SUBST o)
+    
+ =     OTerm o ~ u1
+       STerm o ~ u1
+       Sterm a ~ u1
+
+ =  (SUBST a, OBJECT o, STerm o ~ STerm a, OTerm o ~ STerm o, OBJECT o, SUBST o)
+-}
