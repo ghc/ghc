cca9cb117f795b2d0075c787b1129f65c4071238
Paolo Capriotti <p.capriotti@gmail.com>
2012-09-09 16:55:59 +0100

    Remove unnecessary hs_add_root call (#7031)

--- testsuite/tests/dynlibs/T4464B.c
+++ testsuite/tests/dynlibs/T4464B.c
@@ -1,8 +1,6 @@
 
 #include <Rts.h>
 
-extern void __stginit_T4464H(void);
-
 void HsStart(void) {
     int argc = 3;
     char* argv[] = {"ghcDll", "+RTS", "-H50M", NULL}; // argv must end with NULL
@@ -19,9 +17,6 @@ void HsStart(void) {
 #else
     hs_init(&argc, &args);
 #endif
-
-    // Tell Haskell about all root modules
-    hs_add_root(__stginit_T4464H);
 }
 
 void HsEnd(void) {
