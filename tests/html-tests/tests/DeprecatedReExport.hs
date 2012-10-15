3d109e446db11f06451a4d6c3e1159e238d4d2d9
Simon Hengel <sol@typeful.net>
2012-10-15 10:30:07 +0200

    Add description for DeprecatedReExport test

--- tests/html-tests/tests/DeprecatedReExport.hs
+++ tests/html-tests/tests/DeprecatedReExport.hs
@@ -1,3 +1,16 @@
-module DeprecatedReExport (foo) where
+-- |
+-- What is tested here:
+--
+-- * Deprecation messages are shown for re-exported items.
+--
+module DeprecatedReExport (
+-- * Re-exported from an other module
+  foo
+-- * Re-exported from an other package
+-- | Not yet working, see <http://trac.haskell.org/haddock/ticket/223>
+-- , isEmptyChan
+,
+) where
 
 import DeprecatedFunction
+import Control.Concurrent.Chan
