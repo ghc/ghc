3a3c6c7535c92f5c91c8ff9f5f88b824839ba542
Simon Hengel <sol@typeful.net>
2012-10-15 11:24:57 +0200

    Copy css, images, etc. on accept

--- html-test/accept.hs
+++ html-test/accept.hs
@@ -1,38 +1,39 @@
 import System.Cmd
 import System.Environment
 import System.FilePath
-import System.Exit
 import System.Directory
 import Data.List
-import Control.Monad
 import Control.Applicative
 
-
+main :: IO ()
 main = do
   args <- getArgs
   dir <- getCurrentDirectory
   contents <- filter (`notElem` ignore) <$> getDirectoryContents (dir </> "output")
-  if not $ null args
-    then
-      mapM_ copy [ "output" </> file  | file <- contents, ".html" `isSuffixOf` file, takeBaseName file `elem` args  ]
-    else
-      mapM_ copy [ "output" </> file | file <- contents, ".html" `isSuffixOf` file ]
+  if not $ null args then
+    mapM_ copy [ "output" </> file  | file <- contents, ".html" `isSuffixOf` file, takeBaseName file `elem` args  ]
+  else
+    mapM_ copy [ "output" </> file | file <- contents]
   where
     ignore = [
-        "doc-index.html"
+        "."
+      , ".."
+      , "doc-index.html"
       , "index-frames.html"
       , "index.html"
       ]
 
-
+copy :: FilePath -> IO ()
 copy file = do
   let new = "ref" </> takeFileName file
-  print file
-  print new
-  contents <- readFile file
-  writeFile new (stripLinks contents)
+  if ".html" `isSuffixOf` file then do
+    putStrLn (file ++ " -> " ++ new)
+    stripLinks <$> readFile file >>= writeFile new
+  else do
+    -- copy css, images, etc.
+    copyFile file new
 
-
+stripLinks :: String -> String
 stripLinks str =
   let prefix = "<a href=\"" in
   case stripPrefix prefix str of
