a299f108528ed4a8ba038a5c5e9a19768a0e44e0
Ian Lynagh <ian@well-typed.com>
2012-10-24 01:31:20 +0100

    Use Cabal to build mkUserGuidePart

--- utils/mkUserGuidePart/ghc.mk
+++ utils/mkUserGuidePart/ghc.mk
@@ -10,13 +10,10 @@
 #
 # -----------------------------------------------------------------------------
 
-utils/mkUserGuidePart_dist_MODULES         = Main
+utils/mkUserGuidePart_USES_CABAL           = YES
+utils/mkUserGuidePart_PACKAGE              = mkUserGuidePart
 utils/mkUserGuidePart_dist_PROG            = mkUserGuidePart$(exeext)
 utils/mkUserGuidePart_dist_INSTALL_INPLACE = YES
 utils/mkUserGuidePart_dist_PROGRAM_WAY     = v
-utils/mkUserGuidePart_HC_OPTS              = -package ghc
-
-utils/mkUserGuidePart/dist/build/Main.o: $(ALL_STAGE1_LIBS)
-utils/mkUserGuidePart/dist/build/Main.o: $(compiler_stage2_v_LIB)
 
 $(eval $(call build-prog,utils/mkUserGuidePart,dist,1))
