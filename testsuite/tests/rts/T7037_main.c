188b56251284352d2a2da90d5fab31276a834ff7
Ian Lynagh <ian@well-typed.com>
2013-01-25 23:35:28 +0000

    Make T7037 work on both Windows and other platforms

--- testsuite/tests/rts/T7037_main.c
+++ testsuite/tests/rts/T7037_main.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    const char *args[2] = {"T7037", NULL};
+#ifdef __MINGW32__
+    const
+#endif
+    char * args[2] = {"T7037", NULL};
     execv("./T7037", args);
 }
