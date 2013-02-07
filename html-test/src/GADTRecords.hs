3148ce0e6a0386a2569fa8b1d199e15828a90f2f
Kazu Yamamoto <kazu@iij.ad.jp>
2013-02-07 17:45:10 +0900

    Using new syntax in html-test/src/GADTRecords.hs.

--- html-test/src/GADTRecords.hs
+++ html-test/src/GADTRecords.hs
@@ -5,8 +5,7 @@ module GADTRecords (H1(..)) where
 data H1 a b where
   C1 :: H1 a b
   C2 :: Ord a => [a] -> H1 a a
-  C3 { field :: Int -- ^ hello docs
-     } :: H1 Int Int
-  C4 { field2 :: a -- ^ hello2 docs
-     } :: H1 Int a
-
+  C3 :: { field :: Int -- ^ hello docs
+        } -> H1 Int Int
+  C4 :: { field2 :: a -- ^ hello2 docs
+        } -> H1 Int a
