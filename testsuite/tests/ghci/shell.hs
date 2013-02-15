d447f1d23ba69965f16bad373a79bea21bdbbc56
Ian Lynagh <ian@well-typed.com>
2013-02-15 22:34:35 +0000

    Don't use deprecated System.Cmd

--- testsuite/tests/ghci/shell.hs
+++ testsuite/tests/ghci/shell.hs
@@ -2,8 +2,8 @@
 -- scripts.  We're assuming that sh is in the path and that it
 -- is a Bourne-compatible shell.
 
-import System.Cmd
 import System.Exit
+import System.Process (rawSystem)
 
 shell :: String -> IO ExitCode
 shell s = rawSystem "sh" ["-c", s]
