f2a8479da2c984a6d2aa44a02c1817e064d3e7e1
Simon Marlow <marlowsd@gmail.com>
2013-02-20 09:25:43 +0000

    small tidyup

--- testsuite/tests/concurrent/should_run/conc069.hs
+++ testsuite/tests/concurrent/should_run/conc069.hs
@@ -6,11 +6,11 @@ main = do
   m <- newEmptyMVar
   forkIO (do stat; putMVar m ())
   takeMVar m
-  mask $ \_ -> forkIO (do stat; putMVar m ())
+  mask_ $ forkIO (do stat; putMVar m ())
   takeMVar m
   forkOS (do stat; putMVar m ())
   takeMVar m
-  mask $ \_ -> forkOS (do stat; putMVar m ())
+  mask_ $ forkOS (do stat; putMVar m ())
   takeMVar m
 
 stat = do
