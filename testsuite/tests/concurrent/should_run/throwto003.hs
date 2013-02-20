d1d45e89cfa8df13caa045e153cdf733e217884c
Simon Marlow <marlowsd@gmail.com>
2013-02-20 09:26:27 +0000

    The "unblock" was important here
    Because catch implicitly masks the exception handler.

--- testsuite/tests/concurrent/should_run/throwto003.hs
+++ testsuite/tests/concurrent/should_run/throwto003.hs
@@ -5,12 +5,12 @@ import Control.Monad
 
 main = do
   m <- newMVar 1
-  t1 <- forkIO $ thread m
+  t1 <- mask $ \restore -> forkIO $ thread restore m
   t2 <- forkIO $ forever $ killThread t1
   threadDelay 1000000
   takeMVar m
 
-thread m = run
+thread restore m = run
   where 
-    run = (forever $ modifyMVar_ m $ \v -> if v `mod` 2 == 1 then return (v*2) else return (v-1))
+    run = (restore $ forever $ modifyMVar_ m $ \v -> if v `mod` 2 == 1 then return (v*2) else return (v-1))
              `catch` \(e::SomeException) -> run
