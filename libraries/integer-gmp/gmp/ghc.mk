8ff29424bb3b9488fcf25544325e5430a0d55d16
Ian Lynagh <ian@well-typed.com>
2013-03-01 23:24:44 +0000

    Follow changes in GHC's build system

--- libraries/integer-gmp/gmp/ghc.mk
+++ libraries/integer-gmp/gmp/ghc.mk
@@ -33,7 +33,7 @@ clean_gmp:
 	$(call removeTrees,libraries/integer-gmp/gmp/gmpbuild)
 endif
 
-ifeq "$(Windows)" "YES"
+ifeq "$(Windows_Host)" "YES"
 # Apparently building on Windows fails when there is a system gmp
 # available, so we never try to use the system gmp on Windows
 libraries/integer-gmp_CONFIGURE_OPTS += --configure-option=--with-intree-gmp
