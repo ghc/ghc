ddb4879ca33064d68e43b273d42c1624769c0cbe
Ian Lynagh <ian@well-typed.com>
2013-03-03 21:09:58 +0000

    Remove the _INSTALL_IN support in the build system
    It doesn't seem to do anything that _INSTALL and _INSTALL_INPLACE
    can't do.

--- rules/build-perl.mk
+++ rules/build-perl.mk
@@ -54,16 +54,6 @@ $$($1_$2_INPLACE): $1/$2/$$($1_$2_PROG) | $$$$(dir $$$$@)/.
 	"$$(CP)" $$< $$@
 	$$(EXECUTABLE_FILE) $$@
 
-ifneq "$$($1_$2_INSTALL_IN)" ""
-BINDIST_PERL_SOURCES += $1/$2/$$($1_$2_PROG).prl
-
-install: install_$1_$2
-
-.PHONY: install_$1_$2
-install_$1_$2: $1/$2/$$($1_$2_PROG)
-	$$(call INSTALL_DIR,"$$($1_$2_INSTALL_IN)")
-	$$(call INSTALL_SCRIPT,$$(INSTALL_OPTS),$$<,"$$($1_$2_INSTALL_IN)")
-endif
 endif
 
 $(call profEnd, build-perl($1,$2))
