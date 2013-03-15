c536db7318ad58d5ea7a522c49a8f932792ff60c
Simon Peyton Jones <simonpj@microsoft.com>
2013-03-15 15:53:34 +0000

    Initial test for Trac #4175

---
+++ testsuite/tests/ghci/scripts/T4175.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE TypeFamilies #-}
+module T4175 where
+
+type family A a b
+type instance A Int Int = ()
+type instance A (Maybe a) a = a
+
+data family B a
+data instance B () = MkB
+
+class C a where
+    type D a b
+
+instance C Int where
+    type D Int () = String
+
+instance C () where
+    type D () () = Bool
+
+type family E a
+
+type instance where
+    E ()  = Bool
+    E Int = String
