622ae3c18cf6537ebda4fcc7ade77e2dfafea16b
KC Sivaramakrishnan <chandras@cs.purdue.edu>
2013-05-12 16:47:36 -0400

    Added debug messages to tease out the unsafe use of withArrayLen. Added comment.

--- tests/Benchmarks/ChameneosRedux/chameneos-redux-lwc.hs
+++ tests/Benchmarks/ChameneosRedux/chameneos-redux-lwc.hs
@@ -5,7 +5,13 @@
    Modified by Péter Diviánszky, 19 May 2010
    Modified by Louis Wasserman, 14 June 2010
 
-   Should be compiled with -O2 -threaded -fvia-c -optc-O3 and run with +RTS -N<number of cores>.
+	 Should be compiled with -O2 -threaded -fvia-c -optc-O3 and run with +RTS
+	 -N<number of cores>.
+
+	 XXX KC: The user of withArrayLen is unsafe. We obtain pointers to
+	 addresses inside the array but not the byte array itself. This is a
+	 recipie for disaster. See
+	 http://hackage.haskell.org/trac/ghc/ticket/7012. Solution?
    -}
 
 import LwConc.Substrate
@@ -30,42 +36,61 @@ instance Show Color where
   show Y = "yellow"
   show R = "red"
   show B = "blue"
+  show (C v) = error ("show: impossible " ++ show v)
 
 complement :: Color -> Color -> Color
 complement !a !b = case a of
     B -> case b of R -> Y; B -> B; _ -> R
     R -> case b of B -> Y; R -> R; _ -> B
     Y -> case b of B -> R; Y -> Y; _ -> B
-    _ -> error "complement: impossible"
+    C v -> error ("complement: impossible " ++ show v)
 
 type Chameneous = Ptr Color
 data MP = Nobody !Int | Somebody !Int !Chameneous !(MVar Chameneous)
 
 arrive :: MVar MP -> MVar (Int, Int) -> Chameneous -> IO ()
 arrive !mpv !finish !ch = do
+    sc <- getSContIO
     !waker <- newEmptyMVar
     !hole1 <- newIORef undefined
     !hole2 <- newIORef undefined
     !tk <- atomically $ newResumeToken
     let inc x = (fromEnum (ch == x) +)
         go !t !b = do
+            -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
             w <- takeMVarWithHole mpv hole1 tk
+            -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
             case w of
                 Nobody 0 -> do
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     putMVar mpv w tk
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     putMVar finish (t, b) tk
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
+                    return ()
                 Nobody q -> do
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     putMVar mpv (Somebody q ch waker) tk
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     ch' <- takeMVarWithHole waker hole2 tk
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     go (t+1) $ inc ch' b
                 Somebody q ch' waker' -> do
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     c  <- peek ch
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     c' <- peek ch'
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     let !c'' = complement c c'
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     poke ch  c''
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     poke ch' c''
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     let !q' = q-1
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     putMVar waker' ch tk
+                    -- peek ch >>= debugPrint . (\s -> show sc ++ " " ++ show s ++ " " ++ show ch)
                     putMVar mpv (Nobody q') tk
                     go (t+1) $ inc ch' b
     go 0 0
@@ -95,11 +120,11 @@ initSched = do
   replicateM_ (n-1) newCapability
 
 main = do
+    initSched
     putStrLn . map toLower . unlines $
         [unwords [show a, "+", show b, "->", show $ complement a b]
             | a <- [B..Y], b <- [B..Y]]
 
     n <- readIO . head =<< getArgs
-    initSched
-    actions <- zipWithM (run n) [0..] [[B..Y],[B,R,Y,R,Y,B,R,Y,R,B]]
+    actions <- zipWithM (run n) [0..] [[B..Y], [B,R,Y,R,Y,B,R,Y,R,B]]
     sequence_ actions
