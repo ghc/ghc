f9763a53465232a1b0ec4668cbf9ef36dc812922
Simon Peyton Jones <simonpj@microsoft.com>
2013-05-15 10:19:21 +0100

    Test Trac #7910

---
+++ testsuite/tests/th/T7910.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TemplateHaskell #-}
+module Main where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Syntax
+
+class C a
+instance C Int
+
+type D a = C a
+
+main = print $(
+  do isCInst <- isInstance ''C [ConT ''Int]
+     isDInst <- isInstance ''D [ConT ''Int]
+     lift (isCInst,isDInst))
