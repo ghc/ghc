e20dfbcdaaa4c735d5adc235442716fc9597e4fd
Ian Lynagh <ian@well-typed.com>
2013-05-19 15:41:03 +0100

    Expose __word_encode{Float,Double}; fixes integer-simple build

--- rts/StgPrimFloat.h
+++ rts/StgPrimFloat.h
@@ -14,10 +14,8 @@
 /* grimy low-level support functions defined in StgPrimFloat.c */
 void      __decodeDouble_2Int (I_ *man_sign, W_ *man_high, W_ *man_low, I_ *exp, StgDouble dbl);
 void      __decodeFloat_Int (I_ *man, I_ *exp, StgFloat flt);
-StgDouble __word_encodeDouble (W_ j, I_ e);
-StgFloat  __word_encodeFloat (W_ j, I_ e);
 
-// __int_encodeDouble and __int_encodeFloat are public, declared in 
+// __{int,word}_encode{Float,Double} are public, declared in 
 // includes/rts/PrimFloat.h.
 
 #include "EndPrivate.h"
