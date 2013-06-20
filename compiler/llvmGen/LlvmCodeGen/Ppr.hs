6ca7525b1004ab081c5708007d58175867434a84
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-06-19 20:22:45 -0500

    Support QNXNTO for arm under LLVM
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- compiler/llvmGen/LlvmCodeGen/Ppr.hs
+++ compiler/llvmGen/LlvmCodeGen/Ppr.hs
@@ -61,6 +61,9 @@ moduleLayout = sdocWithPlatform $ \platform ->
     Platform { platformArch = ArchARM {}, platformOS = OSAndroid } ->
         text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32\""
         $+$ text "target triple = \"arm-unknown-linux-androideabi\""
+    Platform { platformArch = ArchARM {}, platformOS = OSQNXNTO } ->
+        text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32\""
+        $+$ text "target triple = \"arm-unknown-nto-qnx8.0.0eabi\""
     Platform { platformArch = ArchARM {}, platformOS = OSiOS } ->
         text "target datalayout = \"e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32\""
         $+$ text "target triple = \"arm-apple-darwin10\""
