6170f97e3d8c54953b7cf49297374566875f4397
Niklas Hambüchen <mail@nh2.me>
2013-08-22 11:18:25 +0900

    Fingerprint: Add Show instance for easier debugging
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- libraries/base/GHC/Fingerprint/Type.hs
+++ libraries/base/GHC/Fingerprint/Type.hs
@@ -12,10 +12,21 @@
 module GHC.Fingerprint.Type (Fingerprint(..)) where
 
 import GHC.Base
+import GHC.List (length, replicate)
+import GHC.Num
+import GHC.Show
 import GHC.Word
+import Numeric (showHex)
 
 -- Using 128-bit MD5 fingerprints for now.
 
 data Fingerprint = Fingerprint {-# UNPACK #-} !Word64 {-# UNPACK #-} !Word64
   deriving (Eq, Ord)
 
+instance Show Fingerprint where
+  show (Fingerprint w1 w2) = hex16 w1 ++ hex16 w2
+    where
+      -- | Formats a 64 bit number as 16 digits hex.
+      hex16 :: Word64 -> String
+      hex16 i = let hex = showHex i ""
+                 in replicate (16 - length hex) '0' ++ hex
