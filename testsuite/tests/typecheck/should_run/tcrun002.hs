9cd6e6e16cdd9ca371dae4650f79920ed0394ae9
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2013-08-24 16:56:14 +0200

    Change deprecated _scc_ to {-# SCC #-} (#8170)

--- testsuite/tests/typecheck/should_run/tcrun002.hs
+++ testsuite/tests/typecheck/should_run/tcrun002.hs
@@ -9,8 +9,8 @@ import GHC.IO
 
 sfoldl :: (a -> Int -> a) -> a -> [Int] -> a
 sfoldl f z [] = z
-sfoldl f z (x:xs) = _scc_ "sfoldl1" (sfoldl f fzx (fzx `seq` xs))
-                  where fzx = _scc_ "fzx" (f z x)
+sfoldl f z (x:xs) = {-# SCC "sfoldl1" #-} (sfoldl f fzx (fzx `seq` xs))
+                  where fzx = {-# SCC "fzx" #-} (f z x)
 
 
 main = IO (\s -> case print (sfoldl (+) (0::Int) [1..200000]) of { IO a -> a s })
