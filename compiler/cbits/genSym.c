1f5338e7b71f56c6d2451b9b23608c851bf90c8a
Patrick Palka <patrick@parcs.ath.cx>
2013-09-04 14:49:51 -0400

    Fix bootstrapping of GHC with earlier versions
    We can no longer use atomic_inc() in the stage1 compiler because its
    prototype was recently changed.
    
    Since the stage1 compiler is always single-threaded, only use
    atomic_inc() when THREADED_RTS is defined.

--- compiler/cbits/genSym.c
+++ compiler/cbits/genSym.c
@@ -4,10 +4,14 @@
 static HsInt GenSymCounter = 0;
 
 HsInt genSym(void) {
+#if defined(THREADED_RTS)
     if (n_capabilities == 1) {
         return GenSymCounter++;
     } else {
         return atomic_inc((StgWord *)&GenSymCounter, 1);
     }
+#else
+    return GenSymCounter++;
+#endif
 }
 
