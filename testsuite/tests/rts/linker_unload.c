35db0b3d112cf2d169bfa24eac3bd16507cd59c1
Edward Z. Yang <ezyang@mit.edu>
2013-09-13 22:45:02 -0700

    Fix egregious pathchar business in linker_unload.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- testsuite/tests/rts/linker_unload.c
+++ testsuite/tests/rts/linker_unload.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
 #include "Rts.h"
 
 #define ITERATIONS 10000
+
+#if defined(mingw32_HOST_OS)
+#define OBJPATH L"Test.o"
+#else
 #define OBJPATH "Test.o"
+#endif
 
 typedef int testfun(int);
 
-#define BASE "/home/simon/code-all/work/ghc-validate/libraries/base/dist-install/build/libHSbase-4.7.0.0.a"
-#define GHCPRIM "/home/simon/code-all/work/ghc-validate/libraries/ghc-prim/dist-install/build/libHSghc-prim-0.3.1.0.a"
-
-void loadPkg(char *path)
+void loadPkg(pathchar *path)
 {
     int r;
 
@@ -30,7 +34,22 @@ int main (int argc, char *argv[])
     initLinker();
 
     for (i=1; i < argc; i++) {
+#if defined(mingw32_HOST_OS)
+        size_t len = mbstowcs(NULL, argv[i], 0) + 1;
+        if (len == -1) {
+            errorBelch("invalid multibyte sequence in argument %d: %s", i, argv[i]);
+            exit(1);
+        }
+        wchar_t *buf = (wchar_t*)_alloca(len * sizeof(wchar_t));
+        size_t len2 = mbstowcs(buf, argv[i], len);
+        if (len != len2 + 1) {
+            errorBelch("something fishy is going on in argument %d: %s", i, argv[i]);
+            exit(1);
+        }
+        loadPkg(buf);
+#else
         loadPkg(argv[i]);
+#endif
     }
 
     for (i=0; i < ITERATIONS; i++) {
