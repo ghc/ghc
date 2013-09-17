7b12be799595cf9aaf27ae755c15aafbd96bfdd1
Edward Z. Yang <ezyang@mit.edu>
2013-09-17 00:02:45 -0700

    Finish up asm test #5435 for Mac OS X
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- testsuite/tests/rts/T5435_asm.c
+++ testsuite/tests/rts/T5435_asm.c
@@ -4,10 +4,12 @@
 //      (1) if we segfault, we'll see the output
 //      (2) we get ordered correctly with Haskell output, which uses
 //          different buffers
-static void initArray1(void) { printf("initArray1\n"); fflush(stdout); }
-static void initArray2(void) { printf("initArray2\n"); fflush(stdout); }
-static void ctors1(void)     { printf("ctors1\n");     fflush(stdout); }
-static void ctors2(void)     { printf("ctors2\n");     fflush(stdout); }
+static void initArray1(void)    { printf("initArray1\n");   fflush(stdout); }
+static void initArray2(void)    { printf("initArray2\n");   fflush(stdout); }
+static void ctors1(void)        { printf("ctors1\n");       fflush(stdout); }
+static void ctors2(void)        { printf("ctors2\n");       fflush(stdout); }
+static void modInitFunc1(void)  { printf("modInitFunc1\n"); fflush(stdout); }
+static void modInitFunc2(void)  { printf("modInitFunc2\n"); fflush(stdout); }
 
 #if defined(cygwin32_HOST_OS) || defined(mingw32_HOST_OS)
 
@@ -19,7 +21,15 @@ static void (*ctors[2])(void) __attribute__((
 
 #elif defined(darwin_HOST_OS)
 
-#error "Not implemented yet!"
+static void (*mod_init_func[2])(void) __attribute__((
+            // Mac OS X sections are in two parts: the segment name and
+            // the section name.  The third part is the flag which says
+            // that this section is a list of module initialization
+            // functions.
+            section("__DATA,__mod_init_func,mod_init_funcs"),
+            used,
+            aligned(sizeof(void*))))
+    = {modInitFunc1, modInitFunc2};
 
 #else /* ELF */
 
