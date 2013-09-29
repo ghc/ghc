5fa0c79414a72e98fac829ac21f11bbfa074205a
Reid Barton <rwbarton@gmail.com>
2013-09-29 16:36:54 -0400

    Update compareAndSwap for new comparison primops

--- testsuite/tests/concurrent/should_run/compareAndSwap.hs
+++ testsuite/tests/concurrent/should_run/compareAndSwap.hs
@@ -19,7 +19,7 @@ import Control.Monad
 casArrayST :: MutableArray s a -> Int -> a -> a -> ST s (Bool, a)
 casArrayST (MutableArray arr#) (I# i#) old new = ST$ \s1# ->
   case casArray# arr# i# old new s1# of 
-    (# s2#, x#, res #) -> (# s2#, (x# ==# 0#, res) #)
+    (# s2#, x#, res #) -> (# s2#, (isTrue# (x# ==# 0#), res) #)
 
 casSTRef :: STRef s a -- ^ The 'STRef' containing a value 'current'
          -> a -- ^ The 'old' value to compare
@@ -30,7 +30,7 @@ casSTRef (STRef var#) old new = ST $ \s1# ->
    -- Zero means the CAS worked, one that it didn't.
    -- We flip that here:
     case casMutVar# var# old new s1# of
-      (# s2#, x#, res #) -> (# s2#, (x# ==# 0#, res) #)
+      (# s2#, x#, res #) -> (# s2#, (isTrue# (x# ==# 0#), res) #)
 
 -- | Performs a machine-level compare and swap operation on an
 -- 'IORef'. Returns a tuple containing a 'Bool' which is 'True' when a
