b84fbabb0cad88a5b1ae62a53e04075383bc11f4
Patrick Palka <patrick@parcs.ath.cx>
2013-09-29 17:11:51 -0400

    Initialize gc_thread.mut_spin to the 'acquired' state

--- rts/sm/GC.c
+++ rts/sm/GC.c
@@ -801,6 +801,7 @@ new_gc_thread (nat n, gc_thread *t)
     initSpinLock(&t->gc_spin);
     initSpinLock(&t->mut_spin);
     ACQUIRE_SPIN_LOCK(&t->gc_spin);
+    ACQUIRE_SPIN_LOCK(&t->mut_spin);
     t->wakeup = GC_THREAD_INACTIVE;  // starts true, so we can wait for the
                           // thread to start up, see wakeup_gc_threads
 #endif
