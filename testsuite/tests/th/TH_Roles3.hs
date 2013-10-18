169a18845fa823b37ea5cb0ad68aa8c8285e76bd
Joachim Breitner <mail@joachim-breitner.de>
2013-10-18 21:42:16 +0200

    Flush stdout after printing in runIO
    as the docs of runIO tell me to.

--- testsuite/tests/th/TH_Roles3.hs
+++ testsuite/tests/th/TH_Roles3.hs
@@ -3,8 +3,9 @@
 module Roles3 where
 
 import Language.Haskell.TH
+import System.IO
 
 $( do { decls <- [d| data Foo a (b :: k) c (d :: k)
                      type role Foo _ _ representational nominal |]
-      ; runIO $ putStrLn (pprint decls)
+      ; runIO $ putStrLn (pprint decls) >> hFlush stdout
       ; return decls })
--- testsuite/tests/th/TH_Roles4.hs
+++ testsuite/tests/th/TH_Roles4.hs
@@ -3,9 +3,10 @@
 module Roles4 where
 
 import Language.Haskell.TH
+import System.IO
 
 data Sticky a b = MkSticky (a b)
 
 $( do roles <- reifyRoles (mkName "Sticky")
-      runIO $ putStrLn (show roles)
+      runIO $ putStrLn (show roles) >> hFlush stdout
       return [] )
