a2ff078d25e09dea6c86419b04036f6de72739f3
Simon Peyton Jones <simonpj@microsoft.com>
2013-11-05 13:53:03 +0000

    Error messsage wibbles

--- testsuite/tests/indexed-types/should_fail/T3330a.hs
+++ testsuite/tests/indexed-types/should_fail/T3330a.hs
@@ -18,6 +18,27 @@ type family PF (phi :: * -> *) :: (* -> *) -> * -> *
 children :: s ix -> (PF s) r ix -> [AnyF s]
 children p x = execWriter (hmapM p collect x)
 
+{-
+0 from instantiating hmap
+2 from instantiating collect
+
+  (forall ixx. (phi0 ixx -> r0 ixx -> m0 (r'0 ixx) ~ s ix))
+  phi0 ix0 ~ s2 ix2 -> r2 ix2 -> Writer [AnyF s2] (r2 ix2)
+  f0 r0 ix0 ~ PF s r ix
+  m0 (f0 r'0 ix0) ~ Writer [AnyF s] a0
+
+Hence ix0 := ix
+      r0  := r
+      f0  := PF s
+      phi0 := s2 ix2
+      m0 := Writer [AnyF s]
+      a0 : = f0 r'0 ix0
+
+  (forall ixx. (s2 ix2 ixx -> r ixx -> Writer [AnyF s] (r'0 ixx) ~ s ix))
+  s2 ix2 ix0 ~ s2 ix2 -> r2 ix2 -> Writer [AnyF s2] (r2 ix2)
+
+-}
+
 collect :: HFunctor (PF s) => s ix -> r ix -> Writer [AnyF s] (r ix)
 collect = error "collect"
 
