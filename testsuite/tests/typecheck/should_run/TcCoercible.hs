483ff505b7e1616680b8e8605292ae160a6ff77d
Joachim Breitner <mail@joachim-breitner.de>
2013-11-19 11:48:59 +0000

    Add examples from #8541 to testsuite

--- testsuite/tests/typecheck/should_run/TcCoercible.hs
+++ testsuite/tests/typecheck/should_run/TcCoercible.hs
@@ -11,6 +11,10 @@ newtype Baz = Baz Bar deriving Show
 type role Map nominal representational
 data Map a b = Map a b deriving Show
 
+-- Higher kinded coercions (#8541)
+newtype List a = List [a] deriving Show
+data T f = T (f Int)
+
 main = do
     print (coerce $ one                       :: Age)
     print (coerce $ Age 1                     :: Int)
@@ -25,7 +29,13 @@ main = do
     print (coerce $ Just one                  :: First Int)
     print (coerce $ (mempty :: Last Age)      :: Last Int)
 
+    printT (coerce $ (T (Left (Age 1)) :: T (Either Age))  :: T (Either Int))
+    printT (coerce $ (T (Left one)     :: T (Either Int))  :: T (Either Age))
+    printT (coerce $ (T [one]          :: T [])            :: T List)
+    printT (coerce $ (T (List [one])   :: T List)          :: T [])
+
   where one = 1 :: Int
+        printT (T x) = print x
 
 
 
