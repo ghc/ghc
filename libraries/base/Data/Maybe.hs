cfe79fd38c143c0203c0acb711234d4025fe1789
Takano Akio <aljee@hyper.cx>
2013-11-20 16:30:31 +0900

    Add fusion RULES for mapMaybe

--- libraries/base/Data/Maybe.hs
+++ libraries/base/Data/Maybe.hs
@@ -126,4 +126,16 @@ mapMaybe f (x:xs) =
  case f x of
   Nothing -> rs
   Just r  -> r:rs
+{-# NOINLINE [1] mapMaybe #-}
 
+{-# RULES
+"mapMaybe"     [~1] forall f xs. mapMaybe f xs
+                    = build (\c n -> foldr (mapMaybeFB c f) n xs)
+"mapMaybeList" [1]  forall f. foldr (mapMaybeFB (:) f) [] = mapMaybe f
+  #-}
+
+{-# NOINLINE [0] mapMaybeFB #-}
+mapMaybeFB :: (b -> r -> r) -> (a -> Maybe b) -> a -> r -> r
+mapMaybeFB cons f x next = case f x of
+  Nothing -> next
+  Just r -> cons r next
