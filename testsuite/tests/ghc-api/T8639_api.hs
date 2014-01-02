40db99a7c485aa58f9fc3a209fced8720cf5c3e5
Simon Peyton Jones <simonpj@microsoft.com>
2014-01-02 15:18:10 +0000

    Test Trac #8639

---
+++ testsuite/tests/ghc-api/T8639_api.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import GHC
+import GhcMonad
+import Outputable
+import System.IO
+import System.Environment( getArgs )
+
+main
+ = do { [libdir] <- getArgs
+      ; runGhc (Just libdir) $ do
+           flags <- getSessionDynFlags
+           setSessionDynFlags (flags{ hscTarget = HscInterpreted, ghcLink = LinkInMemory})
+           target <- guessTarget "T8639_api_a.hs" Nothing
+           setTargets [target]
+           load LoadAllTargets
+           imps <- mapM parseImportDecl ["import Prelude", "import T8639_api_a"]
+           setContext (map IIDecl imps)
+
+           -- With the next line, you get an "Not in scope" exception.
+           -- If you comment out this runStmt, it runs without error and prints the  type.
+           runStmt "putStrLn (show 3)" RunToCompletion
+
+           ty <- exprType "T8639_api_a.it"
+           liftIO (putStrLn (showPpr flags ty))
+       ; hFlush stdout }
