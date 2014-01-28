ghc/ghc

commit 4ade9627608ea0a88450506222bb9afbbcff4294

Abort when binutils ld is used with dynamic linking on ARM
The binutils linker on ARM emits unnecessary R_ARM_COPY relocations
which breaks tables-next-to-code in dynamically linked modules. This
check should be more selective but there is currently no released
version where this bug is fixed.  See
https://sourceware.org/bugzilla/show_bug.cgi?id=16177 and
https://ghc.haskell.org/trac/ghc/ticket/4210#comment:29 for details.

Signed-off-by: Austin Seipp <austin@well-typed.com>

compiler/main/GHC.hs
@@ -295,6 +295,7 @@ import Annotations
 import Module
 import UniqFM
 import Panic
+import Platform
 import Bag              ( unitBag )
 import ErrUtils
 import MonadUtils
@@ -450,11 +451,41 @@ initGhcMonad mb_top_dir
                    ; initStaticOpts
                    ; mySettings <- initSysTools mb_top_dir
                    ; dflags <- initDynFlags (defaultDynFlags mySettings)
+                   ; checkBrokenTablesNextToCode dflags
                    ; setUnsafeGlobalDynFlags dflags
                       -- c.f. DynFlags.parseDynamicFlagsFull, which
                       -- creates DynFlags and sets the UnsafeGlobalDynFlags
                    ; newHscEnv dflags }
        ; setSession env }
+
+-- | The binutils linker on ARM emits unnecessary R_ARM_COPY relocations which
+-- breaks tables-next-to-code in dynamically linked modules. This
+-- check should be more selective but there is currently no released
+-- version where this bug is fixed.
+-- See https://sourceware.org/bugzilla/show_bug.cgi?id=16177 and
+-- https://ghc.haskell.org/trac/ghc/ticket/4210#comment:29
+checkBrokenTablesNextToCode :: MonadIO m => DynFlags -> m ()
+checkBrokenTablesNextToCode dflags
+  = do { broken <- checkBrokenTablesNextToCode' dflags
+       ; when broken
+         $ do { liftIO $ throwIO $ mkApiErr dflags 
+                  (text "Tables-next-to-code not supported on ARM using binutils ld (https://sourceware.org/bugzilla/show_bug.cgi?id=16177)")
+              ; fail "unsupported linker"
+              }
+       }
+
+checkBrokenTablesNextToCode' :: MonadIO m => DynFlags -> m Bool
+checkBrokenTablesNextToCode' dflags
+  | not (isARM arch)              = return False
+  | WayDyn `notElem` ways dflags  = return False
+  | not (tablesNextToCode dflags) = return False
+  | otherwise                     = do
+    linkerInfo <- liftIO $ getLinkerInfo dflags
+    case linkerInfo of
+      GnuLD _  -> return True
+      _        -> return False
+  where platform = targetPlatform dflags
+        arch = platformArch platform
 
 
 -- %************************************************************************

docs/users_guide/7.8.1-notes.xml
@@ -365,6 +365,12 @@
                 (e.g. GHCi) support for architectures without support in
                 GHC's own runtime linker (e.g. ARM).
            </para>
+           <para>
+                Note: Tables-next-to-code is disabled when building on
+                ARM with binutil's ld due to a
+                <ulink url="https://sourceware.org/bugzilla/show_bug.cgi?id=16177">
+                bug</ulink> in ld.
+           </para>
        </listitem>
    </itemizedlist>
   </sect2>

commit 874124d71fb8af9c42fabd1b08e0e66545a02ff2

Fix ./validate failure due to unused result.
Fallout from 4ade9627608ea0a88450506222bb9afbbcff4294

Signed-off-by: Austin Seipp <austin@well-typed.com>

compiler/main/GHC.hs
@@ -468,11 +468,14 @@ checkBrokenTablesNextToCode :: MonadIO m => DynFlags -> m ()
 checkBrokenTablesNextToCode dflags
   = do { broken <- checkBrokenTablesNextToCode' dflags
        ; when broken
-         $ do { liftIO $ throwIO $ mkApiErr dflags 
-                  (text "Tables-next-to-code not supported on ARM using binutils ld (https://sourceware.org/bugzilla/show_bug.cgi?id=16177)")
+         $ do { _ <- liftIO $ throwIO $ mkApiErr dflags invalidLdErr
               ; fail "unsupported linker"
               }
        }
+  where
+    invalidLdErr = text "Tables-next-to-code not supported on ARM" <+>
+                   text "when using binutils ld (please see:" <+>
+                   text "https://sourceware.org/bugzilla/show_bug.cgi?id=16177)"
 
 checkBrokenTablesNextToCode' :: MonadIO m => DynFlags -> m Bool
 checkBrokenTablesNextToCode' dflags