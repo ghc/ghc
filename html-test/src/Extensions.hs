0f67305a819203f8d9c5379c123b68d40f564982
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-01-29 21:41:58 +0000

    Update tests
    This updates tests due to Haddock Trac haskell/haddock#271 fix and due to removal of
    TypeHoles as an extension from GHC.

--- html-test/src/Extensions.hs
+++ html-test/src/Extensions.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Haskell2010, TypeHoles, MonomorphismRestriction #-}
+{-# LANGUAGE Haskell2010, ExplicitForAll, MonomorphismRestriction #-}
 {-# OPTIONS_HADDOCK show-extensions #-}
 module Extensions where
 
