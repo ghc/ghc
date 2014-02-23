b59e3227f82982ef842264fdee6e10bfcb8f8cac
Niklas Haas <git@nand.wakku.to>
2014-02-23 16:11:22 +0100

    Add RankNTypes test case to ImplicitParams.hs
    This test actually tests what haskell/haddock#260 originally reported - I omitted the
    RankNTypes scenario from the original fix because I realized it's not
    relevant to the underlying issue and indeed, this renders as intended
    now. Still good to have more tests.

--- html-test/src/ImplicitParams.hs
+++ html-test/src/ImplicitParams.hs
@@ -1,10 +1,13 @@
-{-# LANGUAGE ImplicitParams #-}
+{-# LANGUAGE ImplicitParams, RankNTypes #-}
 module ImplicitParams where
 
-data X
+data X = X
 
 c :: (?x :: X) => X
 c = ?x
 
 d :: (?x :: X, ?y :: X) => (X, X)
 d = (?x, ?y)
+
+f :: ((?x :: X) => a) -> a
+f a = let ?x = X in a
