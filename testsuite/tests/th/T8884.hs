a78d60201e126f2533752041538a67385b2a081d
Richard Eisenberg <eir@cis.upenn.edu>
2014-03-13 15:48:56 -0400

    Fix #8884.
    There were two unrelated errors fixed here:
     1) Make sure that only the *result kind* is reified when reifying
        a type family. Previously, the whole kind was reified, which
        defies the TH spec.
    
     2) Omit kind patterns in equations.
    
    (cherry picked from commit 8c5ea91d68cdc79b413e05f7dacfd052f5de8c64)
    
    Conflicts:
    	testsuite/tests/th/all.T

---
+++ testsuite/tests/th/T8884.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell, TypeFamilies, PolyKinds #-}
+
+module T8884 where
+
+import Language.Haskell.TH
+
+type family Foo a where
+  Foo x = x
+
+type family Baz (a :: k)
+type instance Baz x = x
+
+$( do FamilyI foo@(ClosedTypeFamilyD _ tvbs1 m_kind1 eqns1) [] <- reify ''Foo
+      FamilyI baz@(FamilyD TypeFam _ tvbs2 m_kind2)
+              [inst@(TySynInstD _ eqn2)] <- reify ''Baz
+      runIO $ putStrLn $ pprint foo
+      runIO $ putStrLn $ pprint baz
+      runIO $ putStrLn $ pprint inst
+      return [ ClosedTypeFamilyD (mkName "Foo'") tvbs1 m_kind1 eqns1
+             , FamilyD TypeFam (mkName "Baz'") tvbs2 m_kind2
+             , TySynInstD (mkName "Baz'") eqn2 ] )
