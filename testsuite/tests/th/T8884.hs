7f5d1f52f84cf18a53d6f256ab828df6c005163a
Joachim Breitner <mail@joachim-breitner.de>
2014-03-21 13:46:15 +0100

    Flush after TH in #8884 test case
    (I recall that this was needed in some cases in the past, and might fix
    the validate error on travis.)
    
    (cherry picked from commit df409de9550dc8a07e010964a54112266d809341)

--- testsuite/tests/th/T8884.hs
+++ testsuite/tests/th/T8884.hs
@@ -3,6 +3,7 @@
 module T8884 where
 
 import Language.Haskell.TH
+import System.IO
 
 type family Foo a where
   Foo x = x
@@ -16,6 +17,7 @@ $( do FamilyI foo@(ClosedTypeFamilyD _ tvbs1 m_kind1 eqns1) [] <- reify ''Foo
       runIO $ putStrLn $ pprint foo
       runIO $ putStrLn $ pprint baz
       runIO $ putStrLn $ pprint inst
+      runIO $ hFlush stdout
       return [ ClosedTypeFamilyD (mkName "Foo'") tvbs1 m_kind1 eqns1
              , FamilyD TypeFam (mkName "Baz'") tvbs2 m_kind2
              , TySynInstD (mkName "Baz'") eqn2 ] )
