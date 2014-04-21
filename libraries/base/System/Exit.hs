bcd989d13072a189b49d9393b0d4b1bbaede9d36
Herbert Valerio Riedel <hvr@gnu.org>
2014-04-21 20:29:15 +0200

    Generalise type of recently added System.Exit.die
    This is a follow-up to 77ea2eb0ab36d1a (re #9016) which added `die` with a
    return type of `IO ()` even though all other functions in System.Exit have
    the more general return type `IO a`. It is assumed this was an oversight in
    the original proposal.
    
    Acked-by: Edward Kmett <ekmett@gmail.com>
    Signed-off-by: Herbert Valerio Riedel <hvr@gnu.org>

--- libraries/base/System/Exit.hs
+++ libraries/base/System/Exit.hs
@@ -79,5 +79,5 @@ exitSuccess = exitWith ExitSuccess
 -- | Write given error message to `stderr` and terminate with `exitFailure`.
 --
 -- /Since: 4.7.1.0/
-die :: String -> IO ()
+die :: String -> IO a
 die err = hPutStrLn stderr err >> exitFailure
