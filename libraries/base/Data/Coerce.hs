1946922c61df427e59f8a00572fd4dd6501abd98
Joachim Breitner <mail@joachim-breitner.de>
2014-06-09 20:55:22 +0200

    Make Ptr's parameter phantom
    and implement castPtr with coerce, which gives
        12% less allocation in reverse-complem
        7.3% less allocation in fasta.
        Binary sizes fell 0.1%.
    as reported and discussed in #9163.

--- libraries/base/Data/Coerce.hs
+++ libraries/base/Data/Coerce.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE Unsafe #-}
-{-# LANGUAGE AutoDeriveTypeable #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 -----------------------------------------------------------------------------
 -- |
