426f2acaef9e9117c136126daa0923089260160a
Austin Seipp <austin@well-typed.com>
2014-07-21 10:29:04 -0500

    rts: delint/detab/dewhitespace GetEnv.c
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- rts/posix/GetEnv.c
+++ rts/posix/GetEnv.c
@@ -32,7 +32,7 @@ static char** get_environ(void) { return environ; }
 void getProgEnvv(int *out_envc, char **out_envv[]) {
     int envc;
     char **environ = get_environ();
-    
+
     for (envc = 0; environ[envc] != NULL; envc++) {};
 
     *out_envc = envc;
