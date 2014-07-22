d76535983f4dd6fe3f239c6a5b3439440de86ea2
Austin Seipp <austin@well-typed.com>
2014-07-21 20:36:05 -0500

    rts: delint/detab/dewhitespace StgRun.h
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- rts/StgRun.h
+++ rts/StgRun.h
@@ -3,7 +3,7 @@
  * (c) The GHC Team, 1998-2004
  *
  * Tiny assembler 'layer' between the C and STG worlds.
- * 
+ *
  ---------------------------------------------------------------------------- */
 
 #ifndef STGRUN_H
