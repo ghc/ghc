e6e45a1c497eae37fbc5daf5e201fe97181e840c
Richard Eisenberg <eir@cis.upenn.edu>
2014-08-07 09:20:41 -0400

    Test #9404 (typecheck/should_compile/T9404 and T9404b)

---
+++ testsuite/tests/typecheck/should_compile/T9404b.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE RankNTypes, TypeFamilies #-}
+
+module T9404b where
+
+type family ListTF x where
+  ListTF x = [x]
+
+bar :: (forall x. ListTF x -> Int) -> ()
+bar _ = ()
+
+myconst :: ((forall r. ListTF r -> Int) -> ()) -> x -> (forall r. ListTF r -> Int) -> ()
+myconst x _ = x
+
+foo = (bar `myconst` ()) $ length
+foo2 = (myconst bar ()) $ length
