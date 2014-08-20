f2864e9621b7f35b6524545799da429a6b422143
Austin Seipp <austin@well-typed.com>
2014-08-20 12:29:22 -0500

    rts: detabify/dewhitespace Disassembler.c
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- rts/Disassembler.c
+++ rts/Disassembler.c
@@ -66,7 +66,7 @@ disInstr ( StgBCO *bco, int pc )
 
    switch (instr & 0xff) {
       case bci_BRK_FUN:
-         debugBelch ("BRK_FUN  " );  printPtr( ptrs[instrs[pc]] ); 
+         debugBelch ("BRK_FUN  " );  printPtr( ptrs[instrs[pc]] );
          debugBelch (" %d ", instrs[pc+1]); printPtr( ptrs[instrs[pc+2]] ); debugBelch("\n" );
          pc += 3;
          break;
@@ -75,7 +75,7 @@ disInstr ( StgBCO *bco, int pc )
                          instrs[pc], (signed int)instrs[pc+1]);
          pc += 2; break;
       case bci_CCALL:
-         debugBelch("CCALL    marshaller at 0x%" FMT_Word "\n", 
+         debugBelch("CCALL    marshaller at 0x%" FMT_Word "\n",
                          literals[instrs[pc]] );
          pc += 1; break;
      case bci_STKCHECK:  {
@@ -83,15 +83,15 @@ disInstr ( StgBCO *bco, int pc )
          debugBelch("STKCHECK %" FMT_Word "\n", (W_)stk_words_reqd );
          break;
      }
-      case bci_PUSH_L: 
+      case bci_PUSH_L:
          debugBelch("PUSH_L   %d\n", instrs[pc] );
          pc += 1; break;
       case bci_PUSH_LL:
-         debugBelch("PUSH_LL  %d %d\n", instrs[pc], instrs[pc+1] ); 
+         debugBelch("PUSH_LL  %d %d\n", instrs[pc], instrs[pc+1] );
          pc += 2; break;
       case bci_PUSH_LLL:
-         debugBelch("PUSH_LLL %d %d %d\n", instrs[pc], instrs[pc+1], 
-                                                            instrs[pc+2] ); 
+         debugBelch("PUSH_LLL %d %d %d\n", instrs[pc], instrs[pc+1],
+                                                            instrs[pc+2] );
          pc += 3; break;
       case bci_PUSH_G:
          debugBelch("PUSH_G   " ); printPtr( ptrs[instrs[pc]] );
@@ -129,44 +129,44 @@ disInstr ( StgBCO *bco, int pc )
 
       case bci_PUSH_UBX:
          debugBelch("PUSH_UBX ");
-         for (i = 0; i < instrs[pc+1]; i++) 
+         for (i = 0; i < instrs[pc+1]; i++)
             debugBelch("0x%" FMT_Word " ", literals[i + instrs[pc]] );
          debugBelch("\n");
          pc += 2; break;
       case bci_PUSH_APPLY_N:
-	  debugBelch("PUSH_APPLY_N\n");
-	  break;
+          debugBelch("PUSH_APPLY_N\n");
+          break;
       case bci_PUSH_APPLY_V:
-	  debugBelch("PUSH_APPLY_V\n");
-	  break;
+          debugBelch("PUSH_APPLY_V\n");
+          break;
       case bci_PUSH_APPLY_F:
-	  debugBelch("PUSH_APPLY_F\n");
-	  break;
+          debugBelch("PUSH_APPLY_F\n");
+          break;
       case bci_PUSH_APPLY_D:
-	  debugBelch("PUSH_APPLY_D\n");
-	  break;
+          debugBelch("PUSH_APPLY_D\n");
+          break;
       case bci_PUSH_APPLY_L:
-	  debugBelch("PUSH_APPLY_L\n");
-	  break;
+          debugBelch("PUSH_APPLY_L\n");
+          break;
       case bci_PUSH_APPLY_P:
-	  debugBelch("PUSH_APPLY_P\n");
-	  break;
+          debugBelch("PUSH_APPLY_P\n");
+          break;
       case bci_PUSH_APPLY_PP:
-	  debugBelch("PUSH_APPLY_PP\n");
-	  break;
+          debugBelch("PUSH_APPLY_PP\n");
+          break;
       case bci_PUSH_APPLY_PPP:
-	  debugBelch("PUSH_APPLY_PPP\n");
-	  break;
+          debugBelch("PUSH_APPLY_PPP\n");
+          break;
       case bci_PUSH_APPLY_PPPP:
-	  debugBelch("PUSH_APPLY_PPPP\n");
-	  break;
+          debugBelch("PUSH_APPLY_PPPP\n");
+          break;
       case bci_PUSH_APPLY_PPPPP:
-	  debugBelch("PUSH_APPLY_PPPPP\n");
-	  break;
+          debugBelch("PUSH_APPLY_PPPPP\n");
+          break;
       case bci_PUSH_APPLY_PPPPPP:
-	  debugBelch("PUSH_APPLY_PPPPPP\n");
-	  break;
-      case bci_SLIDE: 
+          debugBelch("PUSH_APPLY_PPPPPP\n");
+          break;
+      case bci_SLIDE:
          debugBelch("SLIDE     %d down by %d\n", instrs[pc], instrs[pc+1] );
          pc += 2; break;
       case bci_ALLOC_AP:
@@ -177,14 +177,14 @@ disInstr ( StgBCO *bco, int pc )
          pc += 1; break;
       case bci_ALLOC_PAP:
          debugBelch("ALLOC_PAP %d arity, %d words\n",
-		 instrs[pc], instrs[pc+1] );
+                 instrs[pc], instrs[pc+1] );
          pc += 2; break;
       case bci_MKAP:
-         debugBelch("MKAP      %d words, %d stkoff\n", instrs[pc+1], 
+         debugBelch("MKAP      %d words, %d stkoff\n", instrs[pc+1],
                                                            instrs[pc] );
          pc += 2; break;
       case bci_MKPAP:
-         debugBelch("MKPAP     %d words, %d stkoff\n", instrs[pc+1], 
+         debugBelch("MKPAP     %d words, %d stkoff\n", instrs[pc+1],
                                                            instrs[pc] );
          pc += 2; break;
       case bci_UNPACK:
@@ -233,7 +233,7 @@ disInstr ( StgBCO *bco, int pc )
          debugBelch("TESTEQ_P  %d, fail to %d\n", instrs[pc],
                                                       instrs[pc+1]);
          pc += 2; break;
-      case bci_CASEFAIL: 
+      case bci_CASEFAIL:
          debugBelch("CASEFAIL\n" );
          break;
       case bci_JMP:
@@ -246,25 +246,25 @@ disInstr ( StgBCO *bco, int pc )
 
       case bci_RETURN:
          debugBelch("RETURN\n" );
-	 break;
+         break;
       case bci_RETURN_P:
          debugBelch("RETURN_P\n" );
-	 break;
+         break;
       case bci_RETURN_N:
          debugBelch("RETURN_N\n" );
-	 break;
+         break;
       case bci_RETURN_F:
          debugBelch("RETURN_F\n" );
-	 break;
+         break;
       case bci_RETURN_D:
          debugBelch("RETURN_D\n" );
-	 break;
+         break;
       case bci_RETURN_L:
          debugBelch("RETURN_L\n" );
-	 break;
+         break;
       case bci_RETURN_V:
          debugBelch("RETURN_V\n" );
-	 break;
+         break;
 
       default:
          barf("disInstr: unknown opcode %u", (unsigned int) instr);
@@ -275,8 +275,8 @@ disInstr ( StgBCO *bco, int pc )
 
 /* Something of a kludge .. how do we know where the end of the insn
    array is, since it isn't recorded anywhere?  Answer: the first
-   short is the number of bytecodes which follow it.  
-   See ByteCodeGen.linkBCO.insns_arr for construction ...  
+   short is the number of bytecodes which follow it.
+   See ByteCodeGen.linkBCO.insns_arr for construction ...
 */
 void disassemble( StgBCO *bco )
 {
@@ -297,7 +297,7 @@ void disassemble( StgBCO *bco )
    j = 16;
    for (i = 0; i < nbcs; i++) {
       debugBelch("%3d ", (int)instrs[i] );
-      j--; 
+      j--;
       if (j == 0) { j = 16; debugBelch("\n   "); };
    }
    debugBelch("\n");
@@ -306,7 +306,7 @@ void disassemble( StgBCO *bco )
    j = 8;
    for (i = 0; i < ptrs->ptrs; i++) {
       debugBelch("%8p ", ptrs->payload[i] );
-      j--; 
+      j--;
       if (j == 0) { j = 8; debugBelch("\n   "); };
    }
    debugBelch("\n");
