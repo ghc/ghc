15df6d98afb8c3813013c5b97efffe0ba8020d32
Gabor Greif <ggreif@gmail.com>
2014-08-20 19:39:56 +0200

    Comment why the include is necessary

--- rts/Capability.c
+++ rts/Capability.c
@@ -26,7 +26,7 @@
 #include "sm/GC.h" // for gcWorkerThread()
 #include "STM.h"
 #include "RtsUtils.h"
-#include "rts/IOManager.h"
+#include "rts/IOManager.h" // for setIOManagerControlFd()
 
 #include <string.h>
 
