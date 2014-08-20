584d459a2fa1e836ace6e8c5f77debe894d9b549
Austin Seipp <austin@well-typed.com>
2014-08-20 12:26:37 -0500

    rts: detabify/dewhitespace StgPrimFloat.h
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- rts/StgPrimFloat.h
+++ rts/StgPrimFloat.h
@@ -15,7 +15,7 @@
 void      __decodeDouble_2Int (I_ *man_sign, W_ *man_high, W_ *man_low, I_ *exp, StgDouble dbl);
 void      __decodeFloat_Int (I_ *man, I_ *exp, StgFloat flt);
 
-// __{int,word}_encode{Float,Double} are public, declared in 
+// __{int,word}_encode{Float,Double} are public, declared in
 // includes/rts/PrimFloat.h.
 
 #include "EndPrivate.h"
