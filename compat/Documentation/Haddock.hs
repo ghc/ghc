309a94ce1761fd3d240c1533fde383bcdeed3f0c
Simon Hengel <sol@typeful.net>
2014-08-22 15:18:06 +0800

    Add deprecated compatibility module

---
+++ compat/Documentation/Haddock.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE PackageImports #-}
+module Documentation.Haddock {-# DEPRECATED "Use @Documentation.Haddock@ from package <http://hackage.haskell.org/package/haddock-api haddock-api> instead!" #-} (
+  module Documentation.Haddock
+) where
+import "haddock-api" Documentation.Haddock
