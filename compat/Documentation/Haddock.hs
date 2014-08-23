85817dc48494f2cc71d3551c3e7c3678c859ee63
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-08-23 10:10:48 +0100

    Remove compat stuff

--- compat/Documentation/Haddock.hs
+++
@@ -1,5 +0,0 @@
-{-# LANGUAGE PackageImports #-}
-module Documentation.Haddock {-# DEPRECATED "Use @Documentation.Haddock@ from package <http://hackage.haskell.org/package/haddock-api haddock-api> instead!" #-} (
-  module Documentation.Haddock
-) where
-import "haddock-api" Documentation.Haddock
