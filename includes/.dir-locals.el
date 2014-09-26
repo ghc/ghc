23bb90460d7c963ee617d250fa0a33c6ac7bbc53
Simon Marlow <marlowsd@gmail.com>
2014-09-26 20:16:37 +0100

    Add emacs indentation/line-length settings

---
+++ includes/.dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c-mode
+  (fill-column . 80))
+ (emacs-lisp-mode
+  (buffer-file-coding-system . utf-8-unix)
+  (c-basic-offset . 4)
+  (indent-tabs-mode)))
+
+
+
