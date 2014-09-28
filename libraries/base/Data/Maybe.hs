21f9bc434c12e928005d59c494e4f48c242b0613
Wieland Hoffmann <themineo@gmail.com>
2014-09-28 19:22:37 +0200

    mapMaybe: Typo in the comment (#9644)
    Signed-off-by: Austin Seipp <austin@well-typed.com>

--- libraries/base/Data/Maybe.hs
+++ libraries/base/Data/Maybe.hs
@@ -87,7 +87,7 @@ catMaybes ls = [x | Just x <- ls]
 -- | The 'mapMaybe' function is a version of 'map' which can throw
 -- out elements.  In particular, the functional argument returns
 -- something of type @'Maybe' b@.  If this is 'Nothing', no element
--- is added on to the result list.  If it just @'Just' b@, then @b@ is
+-- is added on to the result list.  If it is @'Just' b@, then @b@ is
 -- included in the result list.
 mapMaybe          :: (a -> Maybe b) -> [a] -> [b]
 mapMaybe _ []     = []
