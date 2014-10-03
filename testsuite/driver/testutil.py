542b9c3589e803c101398621e516a7bba424a90e
Krzysztof Gogolewski <krz.gogolewski@gmail.com>
2014-10-03 19:18:38 +0200

    Revert "Basic Python 3 support for testsuite driver (Trac #9184)"
    This reverts commit 084d241b316bfa12e41fc34cae993ca276bf0730.
    
    This is a possible culprit of Windows breakage reported at ghc-devs.

--- testsuite/driver/testutil.py
+++ testsuite/driver/testutil.py
@@ -1,5 +1,39 @@
 # -----------------------------------------------------------------------------
 # Utils
+
+def id(a):
+    return a
+
+def eq(x):
+    return lambda y,z=x: y == z
+
+def neq(x):
+    return lambda y,z=x: y != z
+
+def append(x,y):
+    return x + y
+
+def concat(xs):
+    return reduce(append,xs,[])
+
+def chop(s):
+    if s[len(s)-1:] == '\n':
+        return s[:len(s)-1]
+    else:
+        return s
+    
+def all(p,xs):
+    for x in xs:
+        if not p(x):
+            return False
+    return True
+
+def elem(xs):
+    return lambda x: x in xs
+
+def notElem(xs):
+    return lambda x: x not in xs
+
 def version_to_ints(v):
     return [ int(x) for x in v.split('.') ]
 
