f02c915c1583e40d614dfd97047d5117a9b8a9a5
Simon Peyton Jones <simonpj@microsoft.com>
2014-10-31 12:28:42 +0000

    Make this test a bit simpler
    There were two unrelated functions, and the `-ddump-rule-firings` output
    was coming in a non-deterministic order as a result. So now there is just
    one function.

--- testsuite/tests/simplCore/should_compile/T6056.hs
+++ testsuite/tests/simplCore/should_compile/T6056.hs
@@ -1,8 +1,6 @@
 module T6056 where
 import T6056a
 
-foo1 :: Int -> (Maybe Int, [Int])
-foo1 x = smallerAndRest x [x]
+foo :: Int -> (Maybe Int, [Int])
+foo x = smallerAndRest x [x]
 
-foo2 :: Integer -> (Maybe Integer, [Integer])
-foo2 x = smallerAndRest x [x]
