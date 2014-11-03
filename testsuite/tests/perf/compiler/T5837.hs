37785df6febb50350bdc7967e7361eb68ee24425
Simon Peyton Jones <simonpj@microsoft.com>
2014-11-03 10:37:59 +0000

    Compiler performance is much worse in for loopy givens
    This is a deliberate choice, to simplify code, invariants, and I think
    performance in typical cases.  The "loopy givens" case is situations like
    
       [G]  a ~ TF (a, Int)
    
    where TF is a type function with TF (a,b) = (TF a, TF b).
    
    See Note [An alternative story for the inert substitution] in TcFlatten.

--- testsuite/tests/perf/compiler/T5837.hs
+++ testsuite/tests/perf/compiler/T5837.hs
@@ -10,14 +10,44 @@ t = undefined
 
 {-
 
-    [G] a ~ TF (a,Int)
+         [G] a ~ TF (a,Int)               -- a = a_am1
 -->
-    TF (a,Int) ~ fsk
-    fsk ~ a
+         [G] TF (a,Int) ~ fsk             -- fsk = fsk_am8
+inert    [G] fsk ~ a
+
 --->
-    fsk ~ (TF a, TF Int)
-    fsk ~ a
+    [G] fsk ~ (TF a, TF Int)
+inert    [G] fsk ~ a
+
 --->
     a ~ (TF a, TF Int)
+inert    [G] fsk ~ a
+
+---> (attempting to flatten (TF a) so that it does not mention a
+         TF a ~ fsk2
+inert    a ~ (fsk2, TF Int)
+inert    fsk ~ (fsk2, TF Int)
+
+---> (substitute for a)
+         TF (fsk2, TF Int) ~ fsk2
+inert    a ~ (fsk2, TF Int)
+inert    fsk ~ (fsk2, TF Int)
+
+---> (top-level reduction, re-orient)
+         fsk2 ~ (TF fsk2, TF Int)
+inert    a ~ (fsk2, TF Int)
+inert    fsk ~ (fsk2, TF Int)
+
+---> (attempt to flatten (TF fsk2) to get rid of fsk2
+         TF fsk2 ~ fsk3
+         fsk2 ~ (fsk3, TF Int)
+inert    a   ~ (fsk2, TF Int)
+inert    fsk ~ (fsk2, TF Int)
+
+--->
+         TF fsk2 ~ fsk3
+inert    fsk2 ~ (fsk3, TF Int)
+inert    a   ~ ((fsk3, TF Int), TF Int)
+inert    fsk ~ ((fsk3, TF Int), TF Int)
 
 -}
