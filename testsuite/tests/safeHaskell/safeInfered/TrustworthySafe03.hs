185482524d4fa5954243476de1452219fb077228
Herbert Valerio Riedel <hvr@gnu.org>
2014-11-15 11:18:12 +0100

    Workaround 452d6aa95b7 breaking TrustworthySafe03
    This is only a temporary kludge until the issue workarounded by 452d6aa95b7
    gets properly fixed

--- testsuite/tests/safeHaskell/safeInfered/TrustworthySafe03.hs
+++ testsuite/tests/safeHaskell/safeInfered/TrustworthySafe03.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE Trustworthy #-}
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fwarn-trustworthy-safe #-} -- temp broken by 452d6aa95
 
 -- | This module is marked trustworthy but should be inferable as -XSafe.
 -- Warning enabled through `-W`.
