df1307f079ae69dcd735e0973de987b141d509da
Joachim Breitner <mail@joachim-breitner.de>
2014-12-09 18:18:11 -0600

    Link pre-ARMv6 spinlocks into all RTS variants
    Summary:
    For compatibility with ARM machines from pre v6, the RTS provides
    implementations of certain atomic operations. Previously, these
    were only included in the threaded RTS.
    
    But ghc (the library) contains the code in compiler/cbits/genSym.c, which
    uses these operations if there is more than one capability. But there is only
    one libHSghc, so the linker wants to resolve these symbols in every case.
    
    By providing these operations in all RTSs, the linker is happy. The only
    downside is a small amount of dead code in the non-threaded RTS on old ARM
    machines.
    
    Test Plan: It helped here.
    
    Reviewers: bgamari, austin
    
    Reviewed By: bgamari, austin
    
    Subscribers: carter, thomie
    
    Differential Revision: https://phabricator.haskell.org/D564
    
    GHC Trac Issues: #8951

--- rts/OldARMAtomic.c
+++ rts/OldARMAtomic.c
@@ -5,6 +5,12 @@
  * Inefficient but necessary atomic locks used for implementing atomic
  * operations on ARM architectures pre-ARMv6.
  *
+ * These operations are not only referenced in the threaded RTS, but also in
+ * ghc (the library), via the operations in compiler/cbits/genSym.c.
+ * They are not actually called in a non-threaded environment, but we still
+ * need them in every RTS to make the linker happy, hence no
+ * #if defined(THREADED_RTS) here. See #8951.
+ *
  * -------------------------------------------------------------------------- */
 
 #include "PosixSource.h"
@@ -13,8 +19,6 @@
 #if defined(HAVE_SCHED_H)
 #include <sched.h>
 #endif
-
-#if defined(THREADED_RTS)
 
 #if arm_HOST_ARCH && defined(arm_HOST_ARCH_PRE_ARMv6)
 
@@ -51,5 +55,3 @@ void arm_atomic_spin_unlock()
 }
 
 #endif  /* arm_HOST_ARCH && defined(arm_HOST_ARCH_PRE_ARMv6) */
-
-#endif  /* defined(THREADED_RTS) */
