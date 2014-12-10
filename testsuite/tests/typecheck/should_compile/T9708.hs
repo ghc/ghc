37b3646c9da4da62ae95aa3a9152335e485b261e
Simon Peyton Jones <simonpj@microsoft.com>
2014-12-10 14:35:54 +0000

    Testsuite wibbles from constraint-solver improvements

--- testsuite/tests/typecheck/should_compile/T9708.hs
+++ testsuite/tests/typecheck/should_compile/T9708.hs
@@ -6,5 +6,8 @@ import Data.Proxy
 
 type family SomeFun (n :: Nat)
 
+-- See the Trac ticket; whether this suceeds or fails is distintly random
+-- Currently it succeeds
+
 ti7 :: (x <= y, y <= x) => Proxy (SomeFun x) -> Proxy y -> ()
 ti7 _ _ = ()
