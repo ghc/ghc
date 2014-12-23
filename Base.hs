4e03b1c45d2be172e017d3ec5d8dfff5ab8354d9
Andrey Mokhov <andrey.mokhov@ncl.ac.uk>
2014-12-23 17:44:51 +0000

    Add Base.hs (basic datatypes and imports for the build system).

---
+++ Base.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+module Base (
+    module Development.Shake,
+    module Development.Shake.FilePath,
+    module Control.Applicative,
+    module Data.Monoid,
+    Stage (..),
+    Args, arg, Condition,
+    joinArgs, joinArgsWithSpaces,
+    filterOut,
+    ) where
+
+import Development.Shake hiding ((*>))
+import Development.Shake.FilePath
+import Control.Applicative
+import Data.Monoid
+import Data.List
+
+data Stage = Stage0 | Stage1 | Stage2 | Stage3 deriving (Eq, Enum)
+
+type Args = Action [String]
+
+type Condition = Action Bool
+
+instance Monoid a => Monoid (Action a) where
+    mempty = return mempty
+    mappend p q = mappend <$> p <*> q
+
+arg :: [String] -> Args
+arg = return
+
+intercalateArgs :: String -> Args -> Args
+intercalateArgs s args = do
+    as <- args
+    return [intercalate s as]
+
+joinArgsWithSpaces :: Args -> Args
+joinArgsWithSpaces = intercalateArgs " "
+
+joinArgs :: Args -> Args
+joinArgs = intercalateArgs ""
+
+filterOut :: Args -> [String] -> Args
+filterOut args list = filter (`notElem` list) <$> args
