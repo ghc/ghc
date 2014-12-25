23c7701c1fab401a45f707c9abac101c6be9ce56
Andrey Mokhov <andrey.mokhov@gmail.com>
2014-12-25 18:13:12 +0000

    Fix paths to source files.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @mkdir _shake 2> nul
-@ghc --make Main.hs -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
+@ghc --make src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
