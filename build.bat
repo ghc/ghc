7eb2d388f236b8759046b1d58e89cbf9088e4940
Andrey Mokhov <andrey.mokhov@gmail.com>
2014-12-29 21:43:26 +0000

    Add -fwarn-tabs and -fwarn-unused-imports.

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
 @mkdir _shake 2> nul
-@ghc --make src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
+@ghc --make -fwarn-tabs -fwarn-unused-imports src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build 
+@_shake\build --lint --directory ".." %*
