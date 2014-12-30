4198a65a93270c5be30cf99acecc922bd4a4712b
Andrey Mokhov <andrey.mokhov@gmail.com>
2014-12-30 03:53:34 +0000

    Bring && back.

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 @mkdir _shake 2> nul
-@ghc --make -fwarn-tabs -fwarn-unused-imports src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build 
-@_shake\build --lint --directory ".." %*
+@ghc --make -fwarn-tabs -fwarn-unused-imports src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
