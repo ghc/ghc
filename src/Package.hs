eeea3ed76e1886c34234a4efde6f3c6dc296c2d4
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-01-05 00:57:26 +0000

    Add directions to Package submodules.

--- src/Package.hs
+++ src/Package.hs
@@ -1,16 +1,15 @@
-{-# LANGUAGE NoImplicitPrelude #-}
-module Package (
-    packageRules
-    ) where
+module Package (packageRules) where
 
 import Package.Base
 import Package.Data
 import Package.Dependencies
 
 -- These are the packages we build
+-- See Package.Base for definitions of basic types
 packages :: [Package]
 packages = [libraryPackage "deepseq" Stage1 defaultSettings]
 
+-- Rule buildXY is defined in module X.Y
 buildPackage :: Package -> TodoItem -> Rules ()
 buildPackage pkg todoItem = do
     buildPackageData         pkg todoItem
@@ -18,8 +17,7 @@ buildPackage pkg todoItem = do
 
 packageRules :: Rules ()
 packageRules = do
-
-    want ["libraries/deepseq/dist-install/build/deepseq.m"]
+    want ["libraries/deepseq/dist-install/build/deepseq.m"] -- TODO: control targets from commang line arguments
     forM_ packages $ \pkg -> do
         forM_ (pkgTodo pkg) $ \todoItem -> do
             buildPackage pkg todoItem
