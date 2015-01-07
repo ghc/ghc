7792b9a32bbc24e3d3a0fb04f534e7c293ac9ea2
Andrey Mokhov <andrey.mokhov@ncl.ac.uk>
2015-01-07 16:30:30 +0000

    Add instance ShowAction PackageData.

--- src/Oracles/PackageData.hs
+++ src/Oracles/PackageData.hs
@@ -2,7 +2,7 @@
 
 module Oracles.PackageData (
     PackageDataPair (..),
-    packagaDataOption, PackageDataKey (..)
+    PackageData (..)
     ) where
 
 import Development.Shake.Classes
@@ -12,26 +12,27 @@ import Util
 newtype PackageDataPair = PackageDataPair (FilePath, String)
                         deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
 
-packagaDataOptionWithDefault :: FilePath -> String -> Action String -> Action String
-packagaDataOptionWithDefault file key defaultAction = do
+packagaDataWithDefault :: FilePath -> String -> Action String -> Action String
+packagaDataWithDefault file key defaultAction = do
     maybeValue <- askOracle $ PackageDataPair (file, key) 
     case maybeValue of
         Just value -> return value
         Nothing    -> defaultAction
 
-data PackageDataKey = Modules | SrcDirs | PackageKey | IncludeDirs | Deps | DepKeys
-                    deriving Show
+data PackageData = Modules FilePath | SrcDirs FilePath | PackageKey FilePath 
+                 | IncludeDirs FilePath | Deps FilePath | DepKeys FilePath
+                 deriving Show
 
-packagaDataOption :: FilePath -> PackageDataKey -> Action String
-packagaDataOption file key = do
-    let (keyName, ifEmpty) = case key of
-           Modules     -> ("MODULES"     , "" )
-           SrcDirs     -> ("HS_SRC_DIRS" , ".")
-           PackageKey  -> ("PACKAGE_KEY" , "" )
-           IncludeDirs -> ("INCLUDE_DIRS", ".")
-           Deps        -> ("DEPS"        , "" )
-           DepKeys     -> ("DEP_KEYS"    , "" )
-        keyFullName = replaceSeparators '_' $ takeDirectory file ++ "_" ++ keyName
-    res <- packagaDataOptionWithDefault file keyFullName $
-        error $ "\nCannot find key '" ++ keyName ++ "' in " ++ file ++ "."
-    return $ if res == "" then ifEmpty else res
+instance ShowAction PackageData where
+    showAction key = do
+        let (keyName, file, ifEmpty) = case key of
+               Modules     file -> ("MODULES"     , file, "" )
+               SrcDirs     file -> ("HS_SRC_DIRS" , file, ".")
+               PackageKey  file -> ("PACKAGE_KEY" , file, "" )
+               IncludeDirs file -> ("INCLUDE_DIRS", file, ".")
+               Deps        file -> ("DEPS"        , file, "" )
+               DepKeys     file -> ("DEP_KEYS"    , file, "" )
+            keyFullName = replaceSeparators '_' $ takeDirectory file ++ "_" ++ keyName
+        res <- packagaDataWithDefault file keyFullName $
+            error $ "\nCannot find key '" ++ keyName ++ "' in " ++ file ++ "."
+        return $ words $ if res == "" then ifEmpty else res
