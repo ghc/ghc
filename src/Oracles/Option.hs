817ed0570d9b58d3a3220fadcc704cfa7913d90f
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-01-11 21:50:41 +0000

    Fit lines into 80 characters.

--- src/Oracles/Option.hs
+++ src/Oracles/Option.hs
@@ -6,9 +6,17 @@ module Oracles.Option (
 import Base
 import Oracles.Base
 
-data Option = TargetOS | TargetArch | TargetPlatformFull
-            | ConfCcArgs Stage | ConfGccLinkerArgs Stage | ConfLdLinkerArgs Stage | ConfCppArgs Stage
-            | IconvIncludeDirs | IconvLibDirs | GmpIncludeDirs | GmpLibDirs
+data Option = TargetOS
+            | TargetArch
+            | TargetPlatformFull
+            | ConfCcArgs Stage
+            | ConfGccLinkerArgs Stage
+            | ConfLdLinkerArgs Stage
+            | ConfCppArgs Stage
+            | IconvIncludeDirs
+            | IconvLibDirs
+            | GmpIncludeDirs
+            | GmpLibDirs
             | SrcHcOpts
             | HostOsCpp
 
@@ -17,10 +25,10 @@ instance ShowArgs Option where
         TargetOS                -> "target-os"
         TargetArch              -> "target-arch"
         TargetPlatformFull      -> "target-platform-full"
-        ConfCcArgs        stage -> "conf-cc-args-stage-"         ++ (show . fromEnum) stage
-        ConfCppArgs       stage -> "conf-cpp-args-stage-"        ++ (show . fromEnum) stage
-        ConfGccLinkerArgs stage -> "conf-gcc-linker-args-stage-" ++ (show . fromEnum) stage
-        ConfLdLinkerArgs  stage -> "conf-ld-linker-args-stage-"  ++ (show . fromEnum) stage
+        ConfCcArgs        stage -> "conf-cc-args-stage-"         ++ show stage
+        ConfCppArgs       stage -> "conf-cpp-args-stage-"        ++ show stage
+        ConfGccLinkerArgs stage -> "conf-gcc-linker-args-stage-" ++ show stage
+        ConfLdLinkerArgs  stage -> "conf-ld-linker-args-stage-"  ++ show stage
         IconvIncludeDirs        -> "iconv-include-dirs"
         IconvLibDirs            -> "iconv-lib-dirs"
         GmpIncludeDirs          -> "gmp-include-dirs"
@@ -33,14 +41,20 @@ ghcWithInterpreter = do
     [os]   <- showArgs TargetOS
     [arch] <- showArgs TargetArch
     return $
-        os `elem` ["mingw32", "cygwin32", "linux", "solaris2", "freebsd", "dragonfly", "netbsd", "openbsd", "darwin", "kfreebsdgnu"]
+        os `elem` [ "mingw32", "cygwin32", "linux", "solaris2"
+                  , "freebsd", "dragonfly", "netbsd", "openbsd"
+                  , "darwin", "kfreebsdgnu"]
         &&
         arch `elem` ["i386", "x86_64", "powerpc", "sparc", "sparc64", "arm"]
 
+-- TODO: i386-unknown-solaris2 should be in the list if 
+-- @SOLARIS_BROKEN_SHLD@ == YES
 platformSupportsSharedLibs :: Condition
 platformSupportsSharedLibs = do
     [platform] <- showArgs TargetPlatformFull
-    return $ platform `notElem` [ "powerpc-unknown-linux", "x86_64-unknown-mingw32", "i386-unknown-mingw32" ] -- TODO: i386-unknown-solaris2?
+    return $ platform `notElem` [ "powerpc-unknown-linux"
+                                , "x86_64-unknown-mingw32"
+                                , "i386-unknown-mingw32" ]
 
 windowsHost :: Condition
 windowsHost = do
