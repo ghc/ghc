cd02d00639738e151e77288de9d116e286cb83c1
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-01-13 06:43:02 +0000

    Add bin-package-db package.

--- src/Package.hs
+++ src/Package.hs
@@ -10,8 +10,9 @@ import Package.Dependencies
 
 -- These are the packages we build:
 packages :: [Package]
-packages = [libraryPackage "array"   Stage1 defaultSettings,
-            libraryPackage "deepseq" Stage1 defaultSettings]
+packages = [libraryPackage "array"          Stage1 defaultSettings,
+            libraryPackage "deepseq"        Stage1 defaultSettings,
+            libraryPackage "bin-package-db" Stage1 defaultSettings]
 
 -- Rule buildPackageX is defined in module Package.X
 buildPackage :: Package -> TodoItem -> Rules ()
@@ -28,7 +29,10 @@ packageRules = do
          , "libraries/deepseq/dist-install/build/HSdeeps_FT5iVCELxOr62eHY0nbvnU.o"
          , "libraries/array/dist-install/build/libHSarray_3w0nMK0JfaFJPpLFn2yWAJ.a"
          , "libraries/array/dist-install/build/libHSarray_3w0nMK0JfaFJPpLFn2yWAJ.p_a"
-         , "libraries/array/dist-install/build/HSarray_3w0nMK0JfaFJPpLFn2yWAJ.o" ]
+         , "libraries/array/dist-install/build/HSarray_3w0nMK0JfaFJPpLFn2yWAJ.o"
+         , "libraries/bin-package-db/dist-install/build/libHSbinpa_9qPPbdABQ6HK3eua2jBtib.a"
+         , "libraries/bin-package-db/dist-install/build/libHSbinpa_9qPPbdABQ6HK3eua2jBtib.p_a"
+         , "libraries/bin-package-db/dist-install/build/HSbinpa_9qPPbdABQ6HK3eua2jBtib.o" ]
     forM_ packages $ \pkg -> do
         forM_ (pkgTodo pkg) $ \todoItem -> do
             buildPackage pkg todoItem
