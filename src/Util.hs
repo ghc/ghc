797df55a99ffbe2fe94bae5dc202444b294ae2d0
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-01-15 02:02:28 +0000

    Add chunksOfSize helper function.

--- src/Util.hs
+++ src/Util.hs
@@ -1,13 +1,14 @@
 module Util (
     module Data.Char,
-    replaceIf, replaceEq, replaceSeparators
+    replaceIf, replaceEq, replaceSeparators,
+    chunksOfSize
     ) where
 
 import Base
 import Data.Char
 
 replaceIf :: (a -> Bool) -> a -> [a] -> [a]
-replaceIf p to = map (\from -> if p from then to else from) 
+replaceIf p to = map (\from -> if p from then to else from)
 
 replaceEq :: Eq a => a -> a -> [a] -> [a]
 replaceEq from = replaceIf (== from)
@@ -15,3 +16,17 @@ replaceEq from = replaceIf (== from)
 replaceSeparators :: Char -> String -> String
 replaceSeparators = replaceIf isPathSeparator
 
+-- (chunksOfSize size ss) splits a list of strings 'ss' into chunks not
+-- exceeding the given 'size'.
+chunksOfSize :: Int -> [String] -> [[String]]
+chunksOfSize _    [] = []
+chunksOfSize size ss = reverse chunk : chunksOfSize size rest
+  where
+    (chunk, rest) = go [] 0 ss
+    go chunk _         []     = (chunk, [])
+    go chunk chunkSize (s:ss) = let newSize = chunkSize + length s
+                                    (newChunk, rest) = go (s:chunk) newSize ss
+                                in
+                                if newSize > size
+                                then (chunk   , s:ss)
+                                else (newChunk, rest)
