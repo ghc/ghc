528b503ad7beacad9d35de99cc27cb6074794b71
Austin Seipp <austin@well-typed.com>
2015-02-23 05:48:15 -0600

    Fix build bogons due to missing separator
    Signed-off-by: Austin Seipp <austin@well-typed.com>
    (cherry picked from commit 9004f0d267613b9989fc2f3313c0bd64936103cd)

--- ghc.mk
+++ ghc.mk
@@ -1108,7 +1108,7 @@ SRC_DIST_GHC_DIRS = mk rules docs distrib bindisttest libffi includes \
     utils docs rts compiler ghc driver libraries libffi-tarballs
 SRC_DIST_GHC_FILES += \
     configure.ac config.guess config.sub configure \
-    aclocal.m4 README.md ANNOUNCE HACKING.md INSTALL.md LICENSE Makefile
+    aclocal.m4 README.md ANNOUNCE HACKING.md INSTALL.md LICENSE Makefile \
     install-sh settings.in VERSION GIT_COMMIT_ID \
     boot packages ghc.mk MAKEHELP.md
 
