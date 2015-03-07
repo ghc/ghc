e341dccf354e03dadbb5bf39814241ea90168c05
Herbert Valerio Riedel <hvr@gnu.org>
2015-03-07 11:08:01 +0100

    Drop redundant LANGUAGE pragmas
    Due to refactoring & cleanups those pragmas have become redundant by now
    
    (cherry picked from commit 1965202febe27949a027dea90c0f0040fd9236e7)

--- libraries/base/Prelude.hs
+++ libraries/base/Prelude.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE CPP, NoImplicitPrelude, BangPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 -----------------------------------------------------------------------------
 -- |
