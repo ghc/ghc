3389562148153186261cb4d9771bbb7b5172b615
Joachim Breitner <mail@joachim-breitner.de>
2015-04-15 13:32:32 +0200

    Improve Call Arity performance
    This improves how the Call Arity deals with "boring" variables. Boring
    variables are those where it does not bother to include in the analysis
    result, so whenever something is looked up in the analysis result, we
    have to make a conservative assumption about them.
    
    Previously, we extended the result with such conservative information
    about them, to keep the code uniform, but that could blow up the amount
    of data passed around, even if only temporarily, and slowed things down.
    
    We now pass around an explicit list (well, set) of variable that are
    boring and take that into account whenever we use the result. Not as
    pretty, but noticably faster.
    
    (cherry picked from commit a9ca67f6bfb45d13944ba15452d3af613ec84d8b)

--- testsuite/tests/callarity/unittest/CallArity1.hs
+++ testsuite/tests/callarity/unittest/CallArity1.hs
@@ -145,6 +145,11 @@ exprs =
         Let (Rec [ (x, Var d `mkApps` [go `mkLApps` [1,2]])
                  , (go, mkLams [x] $ mkACase (mkLams [z] $ Var x) (Var go `mkVarApps` [x]) ) ]) $
             Var go `mkApps` [mkLit 0, go `mkLApps` [0,1]]
+  , ("a thunk (non-function-type), in mutual recursion, causes many calls (d 1 would be bad)",) $
+    mkLet d (f `mkLApps` [0]) $
+        Let (Rec [ (x, Var go `mkApps` [go `mkLApps` [1,2], go `mkLApps` [1,2]])
+                 , (go, mkLams [x] $ mkACase (Var d) (Var go `mkVarApps` [x]) ) ]) $
+            Var go `mkApps` [mkLit 0, go `mkLApps` [0,1]]
   , ("a thunk (function type), in mutual recursion, still calls once (d 1 would be good)",) $
     mkLet d (f `mkLApps` [0]) $
         Let (Rec [ (n, Var go `mkApps` [d `mkLApps` [1]])
