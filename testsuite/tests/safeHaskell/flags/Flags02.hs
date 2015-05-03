f7dfcef47af9bf33837e0af3d391c45d12f17677
Erik de Castro Lopo <erikd@mega-nerd.com>
2015-05-04 08:19:04 +1000

    Fix safeHaskell test for llvm backend
    Test was failing (could not execute: pgmlc) for arm (which uses
    the llvm backend) due to the `-pgmlc pgmlc` in OPTIONS_GHC. It
    was also failing on amd64 in the same way when `-fllvm` was
    added to the command line. Its safe to remove because the
    compiler should already know which llvm tool to use.
    
    Test Plan: validate
    
    Reviewers: dterei, austin
    
    Subscribers: bgamari, thomie
    
    Differential Revision: https://phabricator.haskell.org/D874

--- testsuite/tests/safeHaskell/flags/Flags02.hs
+++ testsuite/tests/safeHaskell/flags/Flags02.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -pgmlc pgmlc, -pgmdll pgmdll, -I., -L., -Uggg, -Dggg, -with-rtsopts full #-}
+{-# OPTIONS_GHC -pgmdll pgmdll, -I., -L., -Uggg, -Dggg, -with-rtsopts full #-}
 
 -- | These are all flags that should be allowed
 module Flags02 where
