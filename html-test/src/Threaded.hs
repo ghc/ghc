10a9bb76d449ecd48a4e4949914f623f34c5570a
Emanuel Borsboom <manny@fpcomplete.com>
2015-05-23 04:56:18 -0700

    Build executable with '-threaded' (fixes haskell/haddock#399)

---
+++ html-test/src/Threaded.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+-- | Ensures haddock built with @-threaded@.
+module Threaded where
+
+import Threaded_TH
+
+-- | @$(forkTH)@ fails at compile time if haddock isn't using the
+-- threaded RTS.
+f = $(forkTH)
