29548785e7977fa3def01f94bf3307986b0ee3da
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-06-04 19:59:27 +0200

    Implement function for tagging parsed chunks with source spans.

--- haddock-api/src/Haddock/Backends/Hyperlinker/Parser.hs
+++ haddock-api/src/Haddock/Backends/Hyperlinker/Parser.hs
@@ -30,7 +30,14 @@ chunk :: String -> [String]
 chunk = undefined
 
 tag :: [String] -> [(Span, String)]
-tag = undefined
+tag =
+    reverse . snd . foldl aux (Position 1 1, [])
+  where
+    aux (pos, cs) c =
+        let pos' = if c == "\n"
+                   then pos { posRow = posRow pos + 1, posCol = 1 }
+                   else pos { posCol = posCol pos + length c }
+        in (pos', (Span pos pos', c):cs)
 
 tokenize :: [(Span, String)] -> [Token]
 tokenize = undefined
