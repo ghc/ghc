7e149bc28cdee0b0faf9ac85414eb06dd3ebd0ee
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-06-06 19:36:53 +0200

    Implement utility method for extracting variable identifiers from AST.

--- haddock-api/src/Haddock/Backends/Hyperlinker/Ast.hs
+++ haddock-api/src/Haddock/Backends/Hyperlinker/Ast.hs
@@ -1,6 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Haddock.Backends.Hyperlinker.Ast where
 
 import qualified GHC
+import Data.Data
+import Control.Applicative
 
 import Haddock.Backends.Hyperlinker.Parser
 
@@ -18,3 +22,15 @@ enrich src =
 
 lookupName :: GHC.RenamedSource -> Span -> Maybe GHC.Name
 lookupName = undefined
+
+everything :: (r -> r -> r) -> (forall a. Data a => a -> r)
+           -> (forall a. Data a => a -> r)
+everything k f x = foldl k (f x) (gmapQ (everything k f) x)
+
+variables :: GHC.RenamedSource -> [(GHC.SrcSpan, GHC.Name)]
+variables =
+    everything (<|>) var
+  where
+    var term = case cast term of
+        (Just (GHC.L sspan (GHC.HsVar sid))) -> pure (sspan, sid)
+        _ -> empty
