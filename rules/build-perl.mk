ghc/ghc

commit 5dd02864a844bcf6fe0018755ff261affdef3fea

Build: remove more unnecessary CLEANING/=YES checks
All these checks that CLEANING/=YES are no longer needed, because
nowadays $1_$2_PROGNAME is always set explicitly, and $1_$2_PROG isn't.

They were once introduce to allow `make clean` before `./configure`. I
checked, and it still works.

Remove the checks to make the build system a tiny bit shorter, and to
no longer wonder why they are there.

Differential Revision: https://phabricator.haskell.org/D941

rules/build-perl.mk
@@ -24,13 +24,11 @@ $(call profStart, build-perl($1,$2))
 # $1 = dir
 # $2 = distdir
 
-ifneq "$$(CLEANING)" "YES"
 ifeq "$$($1_$2_PROGNAME)" ""
 $$(error $1_$2_PROGNAME is not set)
 endif
 ifneq "$$($1_$2_PROG)" ""
 $$(error $1_$2_PROG is set)
-endif
 $1_$2_PROG = $$($1_$2_PROGNAME)
 
 ifneq "$$($$($1_$2_PROG)_INPLACE)" ""

rules/build-prog.mk
@@ -22,13 +22,11 @@ $(call profStart, build-prog($1,$2,$3))
 # $2 = distdir
 # $3 = GHC stage to use (0 == bootstrapping compiler)
 
-ifneq "$$(CLEANING)" "YES"
 ifeq "$$($1_$2_PROGNAME)" ""
 $$(error $1_$2_PROGNAME is not set)
 endif
 ifneq "$$($1_$2_PROG)" ""
 $$(error $1_$2_PROG is set)
-endif
 endif
 $1_$2_PROG = $$($1_$2_PROGNAME)$$(exeext$3)
 
@@ -91,10 +89,8 @@ else
 $1_$2_INPLACE =
 endif
 else
-ifneq "$$(CLEANING)" "YES"
 ifneq "$$($$($1_$2_PROGNAME)_INPLACE)" ""
 $$(error $$($1_$2_PROGNAME)_INPLACE defined twice)
-endif
 endif
 ifeq "$$($1_$2_TOPDIR)" "YES"
 $$($1_$2_PROGNAME)_INPLACE = $$(INPLACE_TOPDIR)/$$($1_$2_PROG)

commit 89223ce1340654455a9f3aa9cbf25f30884227fd

Fix the build when SplitObjs=YES
The default (perf) build, which sets SplitObjs=YES, was broken with
commit 5dd02864a844bcf6fe0018755ff261affdef3fea.

I accidently removed the wrong `endif`. This should fix it.

rules/build-perl.mk
@@ -29,6 +29,7 @@ $$(error $1_$2_PROGNAME is not set)
 endif
 ifneq "$$($1_$2_PROG)" ""
 $$(error $1_$2_PROG is set)
+endif
 $1_$2_PROG = $$($1_$2_PROGNAME)
 
 ifneq "$$($$($1_$2_PROG)_INPLACE)" ""
@@ -38,7 +39,6 @@ ifeq "$$($1_$2_TOPDIR)" "YES"
 $$($1_$2_PROG)_INPLACE = $$(INPLACE_TOPDIR)/$$($1_$2_PROG)
 else
 $$($1_$2_PROG)_INPLACE = $$(INPLACE_BIN)/$$($1_$2_PROG)
-endif
 endif
 
 $1_$2_INPLACE = $$($$($1_$2_PROG)_INPLACE)