a73449e0e4991c3647694c41420995bdd8b5b939
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-07-01 18:33:44 +0200

    Create test case for hyperlinking @-patterns.

--- hypsrc-test/src/Constructors.hs
+++ hypsrc-test/src/Constructors.hs
@@ -25,3 +25,11 @@ unnorf :: Norf -> [Foo]
 unnorf (Norf (Bar, xs, Bar)) = xs
 unnorf (Norf (Baz, xs, Baz)) = reverse xs
 unnorf _ = undefined
+
+
+unnorf' :: Norf -> Int
+unnorf' x@(Norf (f1@(Quux _ n), _, f2@(Quux f3 _))) =
+    x' + n * unfoo f1 + aux f3
+  where
+    aux fx = unfoo f2 * unfoo fx * unfoo f3
+    x' = sum . map unfoo . unnorf $ x
