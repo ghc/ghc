094c9e1ca01c88675c987380097a962700ce4358
Adam Gundry <adam@well-typed.com>
2015-07-07 16:13:00 +0100

    Extra tests for odd corner cases

---
+++ testsuite/tests/overloadedrecflds/should_fail/overloadedrecfldsfail08.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE AllowDuplicateRecordFields, TypeFamilies #-}
+
+data family F a
+data instance F Int  = MkFInt  { x :: Int }
+data instance F Bool = MkFBool { y :: Bool }
+
+-- No data type has both these fields, but they belong to the same
+-- lexical parent (F).  This used to confuse AllowDuplicateRecordFields.
+foo e = e { x = 3, y = True }
+
+main = return ()
