e98f470842aea35c5d040b079439d26829dfbf59
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-07-15 18:25:36 +0200

    Create convenience functions for type specialization module.

--- haddock-api/src/Haddock/Backends/Xhtml/Specialize.hs
+++ haddock-api/src/Haddock/Backends/Xhtml/Specialize.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE FlexibleContexts #-}
 
 
-module Haddock.Backends.Xhtml.Specialize (specialize) where
+module Haddock.Backends.Xhtml.Specialize
+    ( specialize, specialize'
+    , specializeTyVarBndrs
+    ) where
 
 
 import Haddock.Syb
@@ -14,8 +17,26 @@ import Data.Data
 specialize :: (Eq name, Typeable name)
            => Data a
            => name -> HsType name -> a -> a
-specialize name details = everywhere (mkT $ specialize' name details)
+specialize name details = everywhere (mkT $ specializeStep name details)
 
-specialize' :: Eq name => name -> HsType name -> HsType name -> HsType name
-specialize' name details (HsTyVar name') | name == name' = details
-specialize' _ _ typ = typ
+
+specialize' :: (Eq name, Typeable name)
+            => Data a
+            => [(name, HsType name)] -> a -> a
+specialize' = flip $ foldr (uncurry specialize)
+
+
+specializeStep :: Eq name => name -> HsType name -> HsType name -> HsType name
+specializeStep name details (HsTyVar name') | name == name' = details
+specializeStep _ _ typ = typ
+
+
+specializeTyVarBndrs :: (Eq name, Typeable name, DataId name)
+                     => LHsTyVarBndrs name -> [HsType name]
+                     -> HsType name -> HsType name
+specializeTyVarBndrs bndrs typs =
+    specialize' $ zip bndrs' typs
+  where
+    bndrs' = map (bname . unLoc) . hsq_tvs $ bndrs
+    bname (UserTyVar name) = name
+    bname (KindedTyVar (L _ name) _) = name
