6a4801645de86bd79c78dee58d630376d5cac4b7
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-07-20 18:16:26 +0200

    Add some test cases for type renamer.

--- html-test/src/Instances.hs
+++ html-test/src/Instances.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 
 
 module Instances where
@@ -34,3 +35,20 @@ instance Bar Maybe [a]
 instance Bar [] (a, a)
 instance Foo f => Bar (Either a) (f a)
 instance Foo ((,,) a b) => Bar ((,,) a b) (a, b, a)
+
+
+class Baz a where
+
+	baz :: a -> (forall a. a -> a) -> (b, forall c. c -> a) -> (b, c)
+	baz' :: b -> (forall b. b -> a) -> (forall b. b -> a) -> [(b, a)]
+	baz'' :: b -> (forall b. (forall b. b -> a) -> c) -> (forall c. c -> b)
+
+	baz = undefined
+	baz' = undefined
+	baz'' = undefined
+
+
+instance Baz (a -> b)
+instance Baz [c]
+instance Baz (a, b, c)
+instance Baz (a, [b], b, a)
