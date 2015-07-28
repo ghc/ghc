2e33b9c87615d3cc26ad188d5ca565e8a981208e
Simon Peyton Jones <simonpj@microsoft.com>
2015-07-28 22:24:46 +0100

    Modify spec002 to be less trivial
    As it stood the main function in the test always returned
    bottom, and GHC noticed that and therefore made no attempt
    to optimise it, which rather negated the test.
    
    This change makes it non-vacuous.

--- testsuite/tests/simplCore/should_compile/spec002.hs
+++ testsuite/tests/simplCore/should_compile/spec002.hs
@@ -12,6 +12,7 @@ laLayout :: Int -> [Int] -> [Token] -> [Token]
 
 laLayout l (s:ss) (t1@(l1, n1, w1, c1) :
                    t2@(l2, n2, w2, c2) : ts)
-   | n1 < s    = laLayout l2 (n2:s:ss) ts 
+   | n1 < s    = laLayout l2 (n2:s:ss) ts
+   | n1 == s   = []   -- Otherwise laLayout is bottom!
    | otherwise = laLayout l ss (t1:t2:ts)
 
