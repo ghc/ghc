d9b618ff78e13f5fe7ae53e4363c658eb805785f
Gabor Greif <ggreif@gmail.com>
2015-08-02 19:26:04 +0200

    Typo in comment

--- compiler/types/FamInstEnv.hs
+++ compiler/types/FamInstEnv.hs
@@ -274,7 +274,7 @@ Type families are reduced during type inference, but not data families;
 the user explains when to use a data family instance by using contructors
 and pattern matching.
 
-Neverthless it is still useful to have data families in the FamInstEnv:
+Nevertheless it is still useful to have data families in the FamInstEnv:
 
  - For finding overlaps and conflicts
 
