e4c35cd229069cb9a40e59de7bc42e8ad5cab27d
Richard Eisenberg <eir@cis.upenn.edu>
2015-08-05 11:10:46 -0400

    Remove the abominable matchExpectedFunTys_Args

--- compiler/typecheck/TcHsType.hs-boot
+++
@@ -1,8 +0,0 @@
-module TcHsType where
-
-import HsTypes   ( LHsType )
-import Name      ( Name )
-import TcType    ( Kind, Type )
-import TcRnMonad ( TcM )
-
-tcHsTypeApp :: (LHsType Name, [Name]) -> Kind -> TcM Type
--- compiler/typecheck/TcUnify.hs
+++ compiler/typecheck/TcUnify.hs
@@ -27,7 +27,7 @@ module TcUnify (
   matchExpectedPArrTy,
   matchExpectedTyConApp,
   matchExpectedAppTy,
-  matchExpectedFunTys, matchExpectedFunTys_Args,
+  matchExpectedFunTys,
   matchExpectedFunKind,
   newOverloadedLit,
   wrapFunResCoercion
@@ -35,8 +35,6 @@ module TcUnify (
   ) where
 
 #include "HsVersions.h"
-
-import {-# SOURCE #-} TcHsType ( tcHsTypeApp )
 
 import HsSyn
 import TypeRep
@@ -57,7 +55,7 @@ import VarSet
 import ErrUtils
 import DynFlags
 import BasicTypes
-import Maybes ( isJust, expectJust )
+import Maybes ( isJust )
 import Util
 import Outputable
 import FastString
@@ -165,36 +163,12 @@ ignoring the quantification. In App, we want to instantiate.
 
 -}
 
--- | Wrapper for 'match_fun_tys', to be used unless you have visible
--- type arguments to pass in.
 matchExpectedFunTys :: ExpOrAct
                     -> SDoc   -- See Note [Herald for matchExpectedFunTys]
                     -> Arity
                     -> TcSigmaType
                     -> TcM (HsWrapper, [TcSigmaType], TcSigmaType)
-matchExpectedFunTys ea herald n orig_ty
-  = match_fun_tys ea herald (panic "matchExpectedFunTys")
-                  (nOfThem n Nothing) orig_ty
-
--- | Wrapper for 'match_fun_tys', to be used when you do have visible
--- type arugments to pass in.
-matchExpectedFunTys_Args :: CtOrigin
-                         -> SDoc  -- See Note [Herald for matchExpectedFunTys]
-                         -> LHsExpr Name  -- the function (for errors)
-                         -> [LHsExpr Name]
-                         -> TcSigmaType
-                         -> TcM (HsWrapper, [TcSigmaType], TcSigmaType)
-matchExpectedFunTys_Args orig herald orig_fun args orig_ty
-  = match_fun_tys (Actual orig) herald orig_fun (map Just args) orig_ty
-
-match_fun_tys
-  :: ExpOrAct
-  -> SDoc          -- See Note [Herald for matchExpectedFunTys]
-  -> LHsExpr Name  -- the function expression; only used when there are type args
-  -> [Maybe (LHsExpr Name)]
-  -> TcSigmaType
-  -> TcM (HsWrapper, [TcSigmaType], TcSigmaType)
-
+matchExpectedFunTys ea herald arity orig_ty = go arity id orig_ty
 -- If    matchExpectFunTys n ty = (wrap, [t1,..,tn], ty_r)
 -- then  wrap : ty "->" (t1 -> ... -> tn -> ty_r)
 --
@@ -204,69 +178,46 @@ match_fun_tys
 --              (forall a. ty) -> other
 -- If allocated (fresh-meta-var1 -> fresh-meta-var2) and unified, we'd
 -- hide the forall inside a meta-variable
-
-match_fun_tys ea herald orig_fun orig_args orig_ty = go orig_args id orig_ty
   where
     -- If     go n ty = (co, [t1,..,tn], ty_r)
     -- then   Actual:   wrap : ty "->" (t1 -> .. -> tn -> ty_r)
     --        Expected: wrap : (t1 -> .. -> tn -> ty_r) "->" ty
 
-    go :: [Maybe (LHsExpr Name)]
+    go :: Arity
        -> (TcSigmaType -> TcSigmaType)
             -- this goes from the "remainder type" to the full type
        -> TcSigmaType   -- the remainder of the type as we're processing
        -> TcM (HsWrapper, [TcSigmaType], TcSigmaType)
-    go [] _ ty = return (idHsWrapper, [], ty)
+    go 0 _ ty = return (idHsWrapper, [], ty)
 
-    go (arg:args) mk_full_ty ty
-      | Just (Just hs_ty_arg) <- fmap isLHsTypeExpr_maybe arg
-      = do { let origin = case ea of Expected    -> panic "match_fun_tys"
-                                     Actual orig -> orig
-           ; (wrap1, upsilon_ty) <- topInstantiateInferred origin ty
-               -- wrap1 :: ty "->" upsilon_ty
-           ; case tcSplitForAllTy_maybe upsilon_ty of
-               Just (tv, inner_ty) ->
-                 ASSERT( isSpecifiedTyVar tv )
-                 do { let kind = tyVarKind tv
-                    ; ty_arg <- tcHsTypeApp hs_ty_arg kind
-                    ; let insted_ty = substTyWith [tv] [ty_arg] inner_ty
-                    ; (inner_wrap, arg_tys, res_ty)
-                        <- go args mk_full_ty insted_ty
-                        -- inner_wrap :: insted_ty "->" arg_tys -> res_ty
-                    ; let inst_wrap = mkWpTyApps [ty_arg]
-                        -- inst_wrap :: upsilon_ty "->" insted_ty
-                    ; return ( inner_wrap <.> inst_wrap <.> wrap1
-                             , arg_tys, res_ty ) }
-               Nothing -> ty_app_err upsilon_ty (fst hs_ty_arg) }
-
-    go args mk_full_ty ty
+    go n mk_full_ty ty
       | not (null tvs && null theta)
       = do { (wrap, (arg_tys, res_ty)) <- exposeRhoType ea ty $ \rho ->
-             do { (inner_wrap, arg_tys, res_ty) <- go args mk_full_ty rho
+             do { (inner_wrap, arg_tys, res_ty) <- go n mk_full_ty rho
                 ; return (inner_wrap, (arg_tys, res_ty)) }
            ; return (wrap, arg_tys, res_ty) }
       where
         (tvs, theta, _) = tcSplitSigmaTy ty
 
-    go args mk_full_ty ty
-      | Just ty' <- tcView ty = go args mk_full_ty ty'
+    go n mk_full_ty ty
+      | Just ty' <- tcView ty = go n mk_full_ty ty'
 
-    go (_arg:args) mk_full_ty (FunTy arg_ty res_ty)
+    go n mk_full_ty (FunTy arg_ty res_ty)
       | not (isPredTy arg_ty)
       = do { let mk_full_ty' res_ty' = mk_full_ty (mkFunTy arg_ty res_ty')
-           ; (wrap_res, tys, ty_r) <- go args mk_full_ty' res_ty
+           ; (wrap_res, tys, ty_r) <- go (n-1) mk_full_ty' res_ty
            ; let rhs_ty = case ea of
                    Expected -> res_ty
                    Actual _ -> mkFunTys tys ty_r
            ; return ( mkWpFun idHsWrapper wrap_res arg_ty rhs_ty
                     , arg_ty:tys, ty_r ) }
 
-    go args mk_full_ty ty@(TyVarTy tv)
+    go n mk_full_ty ty@(TyVarTy tv)
       | ASSERT( isTcTyVar tv) isMetaTyVar tv
       = do { cts <- readMetaTyVar tv
            ; case cts of
-               Indirect ty' -> go args mk_full_ty ty'
-               Flexi        -> defer args ty (isReturnTyVar tv) }
+               Indirect ty' -> go n mk_full_ty ty'
+               Flexi        -> defer n ty (isReturnTyVar tv) }
 
        -- In all other cases we bale out into ordinary unification
        -- However unlike the meta-tyvar case, we are sure that the
@@ -283,16 +234,15 @@ match_fun_tys ea herald orig_fun orig_args orig_ty = go orig_args id orig_ty
        --
        -- But in that case we add specialized type into error context
        -- anyway, because it may be useful. See also Trac #9605.
-    go args mk_full_ty ty = addErrCtxtM (mk_ctxt (mk_full_ty ty)) $
-                            defer args ty False
+    go n mk_full_ty ty = addErrCtxtM (mk_ctxt (mk_full_ty ty)) $
+                         defer n ty False
 
     ------------
     -- If we decide that a ReturnTv (see Note [ReturnTv] in TcType) should
     -- really be a function type, then we need to allow the argument and
     -- result types also to be ReturnTvs.
-    defer args fun_ty is_return
-      = do { check_for_type_args args  -- fails if there are any type args
-           ; arg_tys <- mapM new_ty_var_ty (map (const openTypeKind) args)
+    defer n fun_ty is_return
+      = do { arg_tys <- replicateM n (new_ty_var_ty openTypeKind)
                         -- See Note [Foralls to left of arrow]
            ; res_ty  <- new_ty_var_ty openTypeKind
            ; let unif_fun_ty = mkFunTys arg_tys res_ty
@@ -303,16 +253,6 @@ match_fun_tys ea herald orig_fun orig_args orig_ty = go orig_args id orig_ty
                       | otherwise = newFlexiTyVarTy
 
     ------------
-    check_for_type_args args
-      = when (any (maybe False isLHsTypeExpr) args) $
-        failWith $
-        vcat [ text "Cannot apply" <+> quotes (ppr orig_fun) <+>
-                 text "to arguments"
-             , nest 4 (sep (map (pprParendExpr .
-                                 expectJust "check_for_type_args") orig_args))
-             , hang (text "I do not know enough about the function's type")
-                  2 (ppr orig_ty) ]
-
     mk_ctxt :: TcSigmaType -> TidyEnv -> TcM (TidyEnv, MsgDoc)
     mk_ctxt full_ty env
       = do { (env', ty) <- zonkTidyTcType env full_ty
@@ -321,7 +261,6 @@ match_fun_tys ea herald orig_fun orig_args orig_ty = go orig_args id orig_ty
                  (env'', full_ty') = tidyOpenType env' full_ty
            ; return (env'', mk_msg full_ty' ty n_actual) }
 
-    arity = length orig_args
     mk_msg full_ty ty n_args
       = herald <+> speakNOf arity (text "argument") <> comma $$
         if n_args == arity
@@ -331,12 +270,6 @@ match_fun_tys ea herald orig_fun orig_args orig_ty = go orig_args id orig_ty
           else sep [ptext (sLit "but its type") <+> quotes (pprType ty),
                     if n_args == 0 then ptext (sLit "has none")
                     else ptext (sLit "has only") <+> speakN n_args]
-
-    ty_app_err ty arg
-      = do { (_, ty) <- zonkTidyTcType emptyTidyEnv ty
-           ; failWith $
-               text "Cannot not apply expression of type" <+> quotes (ppr ty) $$
-               text "to a visible type argument" <+> quotes (ppr arg) }
 
 {-
 Note [Foralls to left of arrow]
