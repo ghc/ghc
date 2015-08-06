89f8e7c6db4bdaafad7f2835e042bd326ce24381
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-08-06 21:17:10 +0200

    Extend advanced types test case with other examples.

--- html-test/src/AdvanceTypes.hs
+++ html-test/src/AdvanceTypes.hs
@@ -4,6 +4,20 @@
 {-# LANGUAGE TypeOperators #-}
 module AdvanceTypes where
 
+
+data RevList a = RNil | RevList a :> a
+
+
 data Pattern :: [*] -> * where
   Nil :: Pattern '[]
   Cons :: Maybe h -> Pattern t -> Pattern (h ': t)
+
+
+-- Unlike (:), (:>) does not have to be quoted on type level.
+data RevPattern :: RevList * -> * where
+    RevNil :: RevPattern RNil
+    RevCons :: Maybe h -> RevPattern t -> RevPattern (t :> h)
+
+
+data Tuple :: (*, *) -> * where
+    Tuple :: a -> b -> Tuple '(a, b)
