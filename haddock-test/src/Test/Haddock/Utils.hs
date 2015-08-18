8545715e1a7486075dd266d3f0f1fafa6e255e01
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-08-18 18:06:59 +0200

    Create utility function for recursive obtaining directory contents.

--- haddock-test/src/Test/Haddock/Utils.hs
+++ haddock-test/src/Test/Haddock/Utils.hs
@@ -1,8 +1,33 @@
 module Test.Haddock.Utils where
 
 
+import Control.Monad
+
 import Data.Maybe
+
+import System.Directory
+import System.FilePath
 
 
 mlast :: [a] -> Maybe a
 mlast = listToMaybe . reverse
+
+
+partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a], [a])
+partitionM _ [] = pure ([], [])
+partitionM p (x:xs) = do
+    (ss, fs) <- partitionM p xs
+    b <- p x
+    pure $ if b then (x:ss, fs) else (ss, x:fs)
+
+
+getDirectoryTree :: FilePath -> IO [FilePath]
+getDirectoryTree path = do
+    (dirs, files) <- partitionM isDirectory =<< contents
+    subfiles <- fmap concat . forM dirs $ \dir ->
+        map (dir </>) <$> getDirectoryTree (path </> dir)
+    pure $ files ++ subfiles
+  where
+    contents = filter realEntry <$> getDirectoryContents path
+    isDirectory entry = doesDirectoryExist $ path </> entry
+    realEntry entry = not $ entry == "." || entry == ".."
