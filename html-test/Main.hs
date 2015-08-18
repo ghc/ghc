41b3d93d1fa22246d70a6424b547edcf1509192f
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-08-18 21:07:06 +0200

    Adapt HTML test runner to use new ignoring functionality.

--- html-test/Main.hs
+++ html-test/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE CPP #-}
 
 
+import Data.Char
+
 import System.Environment
 import System.FilePath
 
@@ -17,7 +19,9 @@ checkConfig = CheckConfig
 
 
 dirConfig :: DirConfig
-dirConfig = defaultDirConfig $ takeDirectory __FILE__
+dirConfig = (defaultDirConfig $ takeDirectory __FILE__)
+    { dcfgCheckIgnore = checkIgnore
+    }
 
 
 main :: IO ()
@@ -36,3 +40,8 @@ stripIfRequired mdl =
 -- | List of modules in which we don't 'stripLinks'
 preserveLinksModules :: [String]
 preserveLinksModules = ["Bug253"]
+
+
+checkIgnore :: FilePath -> Bool
+checkIgnore file@(c:_) | takeExtension file == ".html" && isUpper c = False
+checkIgnore _ = True
