b22134f944c66beb038c0a6fb12a164b61e5630a
Łukasz Hanuszczak <lukasz.hanuszczak@gmail.com>
2015-08-18 21:57:54 +0200

    Specify ignored files for hyperlinker source test runner.

--- hypsrc-test/Main.hs
+++ hypsrc-test/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP #-}
 
 
+import Data.Char
 import Data.List
 
 import System.Environment
@@ -23,8 +24,20 @@ checkConfig = CheckConfig
 
 
 dirConfig :: DirConfig
-dirConfig = defaultDirConfig $ takeDirectory __FILE__
+dirConfig = (defaultDirConfig $ takeDirectory __FILE__)
+    { dcfgCheckIgnore = checkIgnore
+    }
 
 
 main :: IO ()
 main = runAndCheck =<< parseArgs checkConfig dirConfig =<< getArgs
+
+
+checkIgnore :: FilePath -> Bool
+checkIgnore file
+    | and . map ($ file) $ [isHtmlFile, isSourceFile, isModuleFile] = False
+  where
+    isHtmlFile = (== ".html") . takeExtension
+    isSourceFile = (== "src") . takeDirectory
+    isModuleFile = isUpper . head . takeBaseName
+checkIgnore _ = True
