134cac9ab3a450be48cadce39a0faac00be227fb
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-08-22 17:59:54 +0100

    Use -Wall when compiling the build system.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @mkdir _shake 2> nul
-@ghc --make -fwarn-tabs -fwarn-unused-imports src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
+@ghc --make -Wall src/Main.hs -isrc -rtsopts -with-rtsopts=-I0 -outputdir=_shake -o _shake/build && _shake\build --lint --directory ".." %*
