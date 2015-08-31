10a07753ff4ac0c1285454567c926d580d0f0470
Bartosz Nitka <niteria@gmail.com>
2015-08-31 16:10:34 +0100

    Anchor type family instances deterministically
    Summary:
    This is very similar to D1073. It makes type family instances to be
    attached to a binding with a least `OccName`, therefore not depending on `Unique` ordering.
    
    Test Plan:
    * this makes `Language.Haskell.Exts.SrcLoc` deterministic
    * ./validate
    
    Reviewers: simonmar, austin, bgamari, simonpj
    
    Reviewed By: simonpj
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1192
    
    GHC Trac Issues: #4012

--- compiler/iface/MkIface.hs
+++ compiler/iface/MkIface.hs
@@ -1853,13 +1853,8 @@ famInstToIfaceFamInst (FamInst { fi_axiom    = axiom,
 
     orph | is_local fam_decl
          = NotOrphan (nameOccName fam_decl)
-
-         | not (isEmptyNameSet lhs_names)
-         = NotOrphan (nameOccName (head (nameSetElems lhs_names)))
-
-
          | otherwise
-         = IsOrphan
+         = chooseOrphanAnchor $ nameSetElems lhs_names
 
 --------------------------
 toIfaceLetBndr :: Id -> IfaceLetBndr
