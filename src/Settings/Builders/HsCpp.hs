555265ce0686be733a2a3f66abbab1bc8771d237
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-09-24 23:45:01 +0100

    Fix HsCpp argument list.

--- src/Settings/Builders/HsCpp.hs
+++ src/Settings/Builders/HsCpp.hs
@@ -1,6 +1,7 @@
 module Settings.Builders.HsCpp (hsCppArgs) where
 
 import Expression
+import Oracles
 import Predicates (builder)
 import Settings.Builders.GhcCabal
 
@@ -9,9 +10,11 @@ hsCppArgs :: Args
 hsCppArgs = builder HsCpp ? do
     stage <- getStage
     src   <- getSource
-    mconcat [ arg "-P"
+    args  <- getSettingList HsCppArgs
+    mconcat [ append args
+            , arg "-P"
             , cppArgs
-            , arg $ "-Icompiler/stage" ++ show stage
+            , arg $ "-Icompiler/stage" ++ show (succ stage)
             , arg "-x"
             , arg "c"
             , arg src ]
