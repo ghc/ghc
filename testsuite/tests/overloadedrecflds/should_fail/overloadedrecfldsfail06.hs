be204f0af9ef400d358eac77408aebfff4d44a3f
Adam Gundry <adam@well-typed.com>
2015-10-06 16:42:39 +0100

    Resurrect a failing report-unused-imports test

--- testsuite/tests/overloadedrecflds/should_fail/overloadedrecfldsfail06.hs
+++ testsuite/tests/overloadedrecflds/should_fail/overloadedrecfldsfail06.hs
@@ -4,12 +4,15 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# OPTIONS_GHC -Werror -fwarn-unused-imports #-}
 
-import OverloadedRecFldsFail06_A (U(MkU, x, y), V(MkV, MkV2, x, y), Unused(unused), u, getX, getY)
+import OverloadedRecFldsFail06_A (U(x, y), V(MkV, MkV2, x, y), Unused(unused), u, getY)
+import qualified OverloadedRecFldsFail06_A as M (U(x))
+import qualified OverloadedRecFldsFail06_A as N (V(x, y))
+import qualified OverloadedRecFldsFail06_A as P (U(x), V(x))
 
 v = MkV2 True
 
--- Check that this counts a use of U(x) and V(y) but not U(y) or V(x)
-main = do print (getX u)
-          print (getY v)
-          print ((\ MkU{x=x} -> x) u)
+-- Check that this counts a use of U(x) and V(y) but not U(y) or V(x)...
+main = do print (u { x = True } :: U)
           print ((\ MkV2{y=y} -> y) v)
+          print (N.x v)
+          print (getY (v { P.x = 3 }))
