65630525db41e31cc309a59c363aca0869a63760
Ben Gamari <ben@smart-cactus.org>
2015-10-22 18:04:36 +0200

    InteractiveUI: Fix previous commit
    Some day I'll learn to validate before pushing.

--- ghc/InteractiveUI.hs
+++ ghc/InteractiveUI.hs
@@ -614,7 +614,7 @@ checkPerms name =
       -- #8248: Improving warning to include a possible fix.
       putStrLn $ "*** WARNING: " ++ name ++
                  " is writable by someone else, IGNORING!" ++
-                 "\nSuggested fix: execute 'chmod go-w " ++ file ++ "'"
+                 "\nSuggested fix: execute 'chmod go-w " ++ name ++ "'"
     return ok
 #endif
 
