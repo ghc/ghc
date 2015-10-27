d1d8704cb3d003315177fad1394fce49f98fb1a2
Siddhanathan Shanmugam <siddhanathan@gmail.com>
2015-10-27 15:08:41 +0100

    Use correct documentation flag for freverse-errors
    Reviewers: austin, thomie, bgamari
    
    Reviewed By: bgamari
    
    Differential Revision: https://phabricator.haskell.org/D1376
    
    GHC Trac Issues: #10848

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -2402,9 +2402,9 @@ dynamic_flags = [
                                      deprecate "Use -fno-force-recomp instead"))
   , defGhcFlag "no-recomp" (NoArg (do setGeneralFlag Opt_ForceRecomp
                                       deprecate "Use -fforce-recomp instead"))
-  , defGhcFlag "freverse-errors"
+  , defFlag "freverse-errors"
       (noArg (\d -> d {reverseErrors = True} ))
-  , defGhcFlag "fno-reverse-errors"
+  , defFlag "fno-reverse-errors"
       (noArg (\d -> d {reverseErrors = False} ))
 
         ------ HsCpp opts ---------------------------------------------------
