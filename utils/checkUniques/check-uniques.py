e506f02dd2ff75857e975170eb1988b3c89ff190
Ben Gamari <bgamari.foss@gmail.com>
2015-11-24 12:45:27 +0100

    Rewrite checkUniques and incorporate into validate
    This should catch duplicate uniques in the future before Bad Things
    happen.
    
    Test Plan: Introduce a duplicate unique and validate
    
    Reviewers: austin, hvr, thomie
    
    Reviewed By: hvr, thomie
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1521

---
+++ utils/checkUniques/check-uniques.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import os.path
+import sys
+import re
+import glob
+from collections import defaultdict
+
+# keyed on unique type, values are lists of (unique, name) pairs
+def find_uniques(source_files):
+    uniques = defaultdict(lambda: defaultdict(lambda: set()))
+    unique_re = re.compile(r"([\w\d]+)\s*=\s*mk([\w\d']+)Unique\s+(\d+)")
+    for f in source_files:
+        ms = unique_re.findall(open(f).read())
+        for m in ms:
+            name = m[0]
+            _type = m[1]
+            n = int(m[2])
+            uniques[_type][n].add(name)
+
+    return uniques
+
+def print_all(uniques):
+    for _type, uniqs in uniques.items():
+        print('{_type} uniques'.format(**locals()))
+        for n,names in uniqs.items():
+            all_names = ', '.join(names)
+            print('  {n} = {all_names}'.format(**locals()))
+
+def find_conflicts(uniques):
+    return [ (uniqueType, number, names)
+             for uniqueType, uniqs in uniques.items()
+             for number, names in uniqs.items()
+             if len(names) > 1
+           ]
+
+top_dir = sys.argv[1]
+uniques = find_uniques(glob.glob(os.path.join(top_dir, 'compiler', 'prelude', '*.hs')))
+#print_all(uniques)
+conflicts = find_conflicts(uniques)
+if len(conflicts) > 0:
+    print("Error: check-uniques: Found Unique conflict")
+    print()
+    for (ty, n, names) in conflicts:
+        print('    %s unique %d conflict: %s' % (ty, n, ', '.join(names)))
+    print()
+    sys.exit(1)
