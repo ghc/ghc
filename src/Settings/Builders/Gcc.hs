098c9ece49564542cc180a6cba06577695243c7e
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-12-11 00:24:01 +0000

    Pass __GLASGOW_HASKELL__ to gcc when compiling directory.c

--- src/Settings/Builders/Gcc.hs
+++ src/Settings/Builders/Gcc.hs
@@ -1,14 +1,21 @@
 module Settings.Builders.Gcc (gccArgs, gccMArgs) where
 
 import Expression
+import GHC
 import Oracles
-import Predicates (stagedBuilder)
+import Predicates (package, stagedBuilder)
 import Settings
 
+-- TODO: I had to define symbol __GLASGOW_HASKELL__ as otherwise directory.c is
+-- effectively empty. I presume it was expected that GHC will be used for
+-- compiling all C files, but I don't know why. It seems that directory.c is the
+-- only file which requires special treatment when using GCC.
 gccArgs :: Args
-gccArgs = stagedBuilder Gcc ? mconcat [ commonGccArgs
-                                      , arg "-c", arg =<< getInput
-                                      , arg "-o", arg =<< getOutput ]
+gccArgs = stagedBuilder Gcc ?
+    mconcat [ commonGccArgs
+            , package directory ? arg "-D__GLASGOW_HASKELL__"
+            , arg "-c", arg =<< getInput
+            , arg "-o", arg =<< getOutput ]
 
 -- TODO: handle custom $1_$2_MKDEPENDC_OPTS and
 gccMArgs :: Args
