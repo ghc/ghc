5e7de75760b6b44eb7d9caaae3dc805c44bb6604
Andrey Mokhov <andrey.mokhov@gmail.com>
2015-12-16 22:49:27 +0000

    No need to modify configure.ac any more. See https://phabricator.haskell.org/D1638

--- src/Rules/Config.hs
+++ src/Rules/Config.hs
@@ -2,11 +2,6 @@ module Rules.Config (configRules) where
 
 import Base
 
--- We add the following line to 'configure.ac' in order to produce configuration
--- file "system.config" from "system.config.in" by running 'configure' script.
-configCommand :: String
-configCommand = "AC_CONFIG_FILES([" ++ configPath ++ "system.config])"
-
 configRules :: Rules ()
 configRules = do
     configPath -/- "system.config" %> \_ -> do
@@ -14,16 +9,6 @@ configRules = do
         putBuild "Running configure..."
         cmd "bash configure" -- TODO: get rid of 'bash'
 
-    -- TODO: this rule won't rebuild if configure.ac is changed. Worth fixing?
     "configure" %> \_ -> do
-        -- Make sure 'configure.ac' script contains a line with configCommand
-        script <- fmap lines . liftIO $ readFile "configure.ac"
-        when (configCommand `notElem` script) $ do
-            putBuild $ "Adding '" ++ configCommand ++ "' to configure.ac..."
-            let (before, rest) = break ("AC_CONFIG_FILES" `isPrefixOf`) script
-            when (null rest) $ do
-                putError "No AC_CONFIG_FILES command in configure.ac!"
-            let newScript = unlines $ before ++ [configCommand] ++ rest
-            length newScript `seq` liftIO (writeFile "configure.ac" newScript)
         putBuild "Running autoconf..."
         cmd "bash autoconf" -- TODO: get rid of 'bash'
