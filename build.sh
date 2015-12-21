828bc3a27a26a02e1bad68ef0c25d7d3a1ce64fd
David Luposchainsky <dluposchainsky@gmail.com>
2015-12-21 13:46:03 +0100

    Avoid common shell scripting pitfalls
    - Get bash from $PATH instead of reading it from /bin/bash (useful for e.g. NixOS)
    - set -euo pipefail: "strict bash mode"
    - Quote all paths to be whitespace compatible
    - GHC uses --make mode automatically

--- build.sh
+++ build.sh
@@ -1,6 +1,22 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
 
-root=`dirname $0`
-mkdir -p $root/.shake
-ghc --make -Wall $root/src/Main.hs -i$root/src -rtsopts -with-rtsopts=-I0 -outputdir=$root/.shake -o $root/.shake/build
-$root/.shake/build --lint --directory $root/.. $@
+set -euo pipefail
+
+root="$(dirname "$0")"
+
+mkdir -p "$root/.shake"
+
+ghc                           \
+    "$root/src/Main.hs"       \
+    -Wall                     \
+    -i"$root/src"             \
+    -rtsopts                  \
+    -with-rtsopts=-I0         \
+    -outputdir="$root/.shake" \
+    -o "$root/.shake/build"
+
+"$root/.shake/build"       \
+    --lint                 \
+    --directory "$root/.." \
+    --colour               \
+    "$@"
