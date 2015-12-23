2581084196407d8438714e3d3468babdf235c8d8
Ben Gamari <ben@smart-cactus.org>
2015-12-24 00:30:48 +0100

    OrphanInstances: Accept test output

--- html-test/src/OrphanInstances.hs
+++ html-test/src/OrphanInstances.hs
@@ -3,5 +3,6 @@ module OrphanInstances where
 import OrphanInstancesType
 import OrphanInstancesClass
 
-instance AClass AnotherType where
+-- | This is an orphan instance.
+instance AClass AType where
   aClass (AType n) = n
