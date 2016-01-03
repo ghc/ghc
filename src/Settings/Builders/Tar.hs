fd3a1f89719fb551ed3f6579ef978ab304abe66c
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-01-03 18:16:59 +0000

    Add support for both *.gz and *.bz2 archives, see #79.

--- src/Settings/Builders/Tar.hs
+++ src/Settings/Builders/Tar.hs
@@ -1,10 +1,14 @@
 module Settings.Builders.Tar (tarBuilderArgs) where
 
+import Base
 import Expression
 import Predicates (builder)
 
 tarBuilderArgs :: Args
 tarBuilderArgs = builder Tar ? do
-    mconcat [ arg "-xzf"
-            , arg =<< getInput
+    input <- getInput
+    mconcat [ arg "-xf"
+            , ("*.gz"  ?== input) ? arg "--gzip"
+            , ("*.bz2" ?== input) ? arg "--bzip2"
+            , arg input
             , arg "-C", arg =<< getOutput ]
