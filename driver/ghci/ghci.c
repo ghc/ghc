595eb24f2ffb06dfac6ad619bb3abe75b59bbf44
RyanGlScott <ryan.gl.scott@gmail.com>
2016-01-06 23:10:54 +0100

    Fall back on ghc-stage2 when using Windows' GHCi driver
    Reviewers: austin, hvr, bgamari, thomie
    
    Reviewed By: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1721
    
    (cherry picked from commit f01eb5410d7980567bf2731b9cded9a4894a3ef5)

--- driver/ghci/ghci.c
+++ driver/ghci/ghci.c
@@ -3,6 +3,12 @@
 #include "getLocation.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+BOOL fileExists(const char *path) {
+    const DWORD r = GetFileAttributesA(path);
+    return r != INVALID_FILE_ATTRIBUTES && !(r & FILE_ATTRIBUTE_DIRECTORY);
+}
 
 int main(int argc, char** argv) {
     char *binDir;
@@ -18,6 +24,13 @@ int main(int argc, char** argv) {
     binDir = getExecutablePath();
     exePath = mkString("%s/ghc.exe", binDir);
     preArgv[0] = "--interactive";
+
+    /* If ghc.exe can't be found, we assume that we're building ghc from
+     * source, in which case we fall back on ghc-stage2.
+     */
+    if (!fileExists(exePath)) {
+        exePath = mkString("%s/ghc-stage2.exe", binDir);
+    }
 
     run(exePath, 1, preArgv, argc - 1, argv + 1);
 }
