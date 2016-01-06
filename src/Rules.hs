20381e58208ca2693f034566e450f5b0b28674b4
David Luposchainsky <dluposchainsky@gmail.com>
2016-01-06 14:49:58 +0100

    Split up definitions in Rules.hs

--- src/Rules.hs
+++ src/Rules.hs
@@ -1,6 +1,8 @@
 module Rules (generateTargets, packageRules) where
 
 import Base
+import Data.Foldable
+import Data.Traversable
 import Expression
 import GHC
 import Rules.Generate
@@ -8,26 +10,31 @@ import Rules.Package
 import Rules.Resources
 import Settings
 
+allStages :: [Stage]
+allStages = [Stage0 ..]
+
 -- TODO: not all program targets should be needed explicitly
--- | generateTargets needs top-level build targets
+-- | 'need' all top-level build targets
 generateTargets :: Rules ()
 generateTargets = action $ do
-    targets <- fmap concat . forM [Stage0 ..] $ \stage -> do
-        pkgs <- interpretWithStage stage getPackages
-        let libPkgs = filter isLibrary pkgs \\ [rts, libffi]
-        libTargets <- fmap concat . forM libPkgs $ \pkg -> do
-            let target = PartialTarget stage pkg
-            needHaddock <- interpretPartial target buildHaddock
-            return [ pkgHaddockFile pkg | needHaddock && stage == Stage1 ]
-        let programTargets = [ prog | Just prog <- programPath stage <$> pkgs ]
-        return $ libTargets ++ programTargets
-
+    targets <- fmap concat (traverse targetsForStage allStages)
     rtsLib <- pkgLibraryFile Stage1 rts "rts" vanilla
     need $ targets ++ installTargets ++ [ rtsLib ]
+
+targetsForStage :: Stage -> Action [String]
+targetsForStage stage = do
+    pkgs <- interpretWithStage stage getPackages
+    let libPkgs = filter isLibrary pkgs \\ [rts, libffi]
+    libTargets <- fmap concat . for libPkgs $ \pkg -> do
+        let target = PartialTarget stage pkg
+        needHaddock <- interpretPartial target buildHaddock
+        return [ pkgHaddockFile pkg | needHaddock && stage == Stage1 ]
+    let programTargets = [ prog | Just prog <- programPath stage <$> pkgs ]
+    return (libTargets ++ programTargets)
 
 packageRules :: Rules ()
 packageRules = do
     resources <- resourceRules
-    forM_ [Stage0 ..] $ \stage ->
-        forM_ knownPackages $ \pkg ->
+    for allStages $ \stage ->
+        for_ knownPackages $ \pkg ->
             buildPackage resources $ PartialTarget stage pkg
