ghc/ghc

commit ce1f1607ed7f8fedd2f63c8610cafefd59baaf32

Make GHCi & TH work when the compiler is built with -prof
Summary:
Amazingly, there were zero changes to the byte code generator and very
few changes to the interpreter - mainly because we've used good
abstractions that hide the differences between profiling and
non-profiling.  So that bit was pleasantly straightforward, but there
were a pile of other wibbles to get the whole test suite through.

Note that a compiler built with -prof is now like one built with
-dynamic, in that to use TH you have to build the code the same way.
For dynamic, we automatically enable -dynamic-too when TH is required,
but we don't have anything equivalent for profiling, so you have to
explicitly use -prof when building code that uses TH with a profiled
compiler.  For this reason Cabal won't work with TH.  We don't expect
to ship a profiled compiler, so I think that's OK.

Test Plan: validate with GhcProfiled=YES in validate.mk

Reviewers: goldfire, bgamari, rwbarton, austin, hvr, erikd, ezyang

Reviewed By: ezyang

Subscribers: thomie

Differential Revision: https://phabricator.haskell.org/D1407

GHC Trac Issues: #4837, #545

rts/ProfHeap.c
@@ -311,7 +311,7 @@ nextEra( void )
 FILE *hp_file;
 static char *hp_filename;
 
-void initProfiling1 (void)
+void initProfiling (void)
 {
 }
 

testsuite/config/ghc
@@ -179,7 +179,6 @@ def get_compiler_info():
 
     if re.match(".*_p(_.*|$)", rtsInfoDict["RTS way"]):
         config.compiler_profiled = True
-        config.run_ways = [x for x in config.run_ways if x != 'ghci']
     else:
         config.compiler_profiled = False
 
@@ -204,6 +203,11 @@ def get_compiler_info():
         config.ghci_way_flags   = "-dynamic"
         config.ghc_th_way       = "dyn"
         config.ghc_plugin_way   = "dyn"
+    elif config.compiler_profiled:
+        config.ghc_th_way_flags = "-prof"
+        config.ghci_way_flags   = "-prof"
+        config.ghc_th_way       = "prof"
+        config.ghc_plugin_way   = "prof"
     else:
         config.ghc_th_way_flags = "-static"
         config.ghci_way_flags   = "-static"

testsuite/tests/annotations/should_compile/all.T
@@ -1,6 +1,3 @@
-
-setTestOpts(when(compiler_profiled(), skip))
-
 # Annotations, like Template Haskell, require runtime evaluation.  In
 # order for this to work with profiling, we would have to build the
 # program twice and use -osuf p_o (see the TH_splitE5_prof test).  For

testsuite/tests/annotations/should_compile/th/all.T
@@ -1,5 +1,3 @@
-setTestOpts(when(compiler_profiled(), skip))
-
 # Annotations and Template Haskell, require runtime evaluation.  In
 # order for this to work with profiling, we would have to build the
 # program twice and use -osuf p_o (see the TH_splitE5_prof test).  For

testsuite/tests/annotations/should_run/all.T
@@ -1,4 +1,3 @@
-setTestOpts(when(compiler_profiled(), skip))
 # These tests are very slow due to their use of package GHC
 setTestOpts(when(fast(), skip))
 

testsuite/tests/cabal/cabal04/all.T
@@ -8,7 +8,7 @@ if config.have_profiling:
 else:
     prof = '--disable-library-profiling'
 
-if config.have_shared_libs:
+if not config.compiler_profiled and config.have_shared_libs:
     dyn = '--enable-shared'
 else:
     dyn = '--disable-shared'

testsuite/tests/ghc-e/should_fail/all.T
@@ -1,5 +1,3 @@
-setTestOpts(when(compiler_profiled(), skip))
-
 test('T7962', [exit_code(2), req_interp, ignore_output], run_command,
      ['$MAKE --no-print-directory -s T7962'])
 

testsuite/tests/ghc-e/should_run/all.T
@@ -1,6 +1,4 @@
 
-setTestOpts(when(compiler_profiled(), skip))
-
 test('ghc-e001', req_interp, run_command, ['$MAKE --no-print-directory -s ghc-e001'])
 test('ghc-e002', req_interp, run_command, ['$MAKE --no-print-directory -s ghc-e002'])
 test('ghc-e003', req_interp, run_command, ['$MAKE --no-print-directory -s ghc-e003'])

commit c33e7c2b1a62f340432c752fb37ca1374e3e982a

Fix +RTS -h when compiling without -prof
Summary:
Was broken by ce1f1607ed7f8fedd2f63c8610cafefd59baaf32.  I've added a
test so that hopefully it won't break again.

Test Plan: validate & new test case

Reviewers: bgamari, austin, erikd

Subscribers: thomie

Differential Revision: https://phabricator.haskell.org/D1746

GHC Trac Issues: #11304

rts/ProfHeap.c
@@ -311,15 +311,11 @@ nextEra( void )
 FILE *hp_file;
 static char *hp_filename;
 
-void initProfiling (void)
-{
-}
-
 void freeProfiling (void)
 {
 }
 
-void initProfiling2 (void)
+void initProfiling (void)
 {
     char *prog;
 

testsuite/config/ghc
@@ -23,7 +23,7 @@ config.compile_ways       = ['normal', 'hpc']
 config.run_ways           = ['normal', 'hpc']
 
 # ways that are not enabled by default, but can always be invoked explicitly
-config.other_ways         = ['prof',
+config.other_ways         = ['prof', 'prof_h',
                              'prof_hc_hb','prof_hb',
                              'prof_hd','prof_hy','prof_hr',
                              'threaded1_ls', 'threaded2_hT',
@@ -99,6 +99,7 @@ config.way_flags = lambda name : {
     'threaded2'    : ['-O', '-threaded', '-eventlog'],
     'threaded2_hT' : ['-O', '-threaded'],
     'hpc'          : ['-O', '-fhpc', '-hpcdir', '.hpc.' + name ],
+    'prof_h'       : [],
     'prof_hc_hb'   : ['-O', '-prof', '-static', '-auto-all'],
     'prof_hb'      : ['-O', '-prof', '-static', '-auto-all'],
     'prof_hd'      : ['-O', '-prof', '-static', '-auto-all'],
@@ -114,7 +115,7 @@ config.way_flags = lambda name : {
     'ghci-ext'         : ['--interactive', '-v0', '-ignore-dot-ghci', '-fexternal-interpreter', '+RTS', '-I0.1', '-RTS'],
    }
 
-config.way_rts_flags = { 
+config.way_rts_flags = {
     'normal'       : [],
     'g1'           : ['-G1'],
     'optasm'       : [],
@@ -130,6 +131,7 @@ config.way_rts_flags = {
     'threaded2'    : ['-N2 -ls'],
     'threaded2_hT' : ['-N2', '-hT'],
     'hpc'          : [],
+    'prof_h'       : ['-h'], # works without -prof
     'prof_hc_hb'   : ['-hc -hbvoid'],
     'prof_hb'      : ['-hb'],
     'prof_hd'      : ['-hd'],
@@ -216,4 +218,3 @@ def get_compiler_info():
         config.ghci_way_flags   = "-static"
         config.ghc_th_way       = "normal"
         config.ghc_plugin_way   = "normal"
-

testsuite/tests/profiling/should_run/all.T
@@ -4,9 +4,16 @@ extra_prof_ways = ['prof', 'prof_hc_hb', 'prof_hb', 'prof_hd', 'prof_hy', 'prof_
 
 test('heapprof001',
      [only_ways(prof_ways),
-      extra_ways(extra_prof_ways),
-      req_profiling,
+      when(have_profiling(), extra_ways(extra_prof_ways)),
       extra_run_opts('7')],
+     compile_and_run, [''])
+
+test('heapprof002',
+     [ pre_cmd('cp heapprof001.hs heapprof002.hs')
+     , extra_clean(['heapprof002.hs'])
+     , extra_ways(['prof_h'])
+     , extra_run_opts('7')
+     ],
      compile_and_run, [''])
 
 test('T2592',

testsuite/tests/profiling/should_run/heapprof002.stdout
@@ -1,0 +1,7 @@
+a <= 
+a <= 
+a <= 
+a <= 
+a <= 
+a <= 
+a <= 