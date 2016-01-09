8efb43dd384111a0f724924507aac59a655b382d
Neil Mitchell <ndmitchell@gmail.com>
2016-01-09 21:33:02 +0000

    Don't conflict with parallel in the latest Shake, provides a warning free way to be forward and backward compatible with Shake (albeit not pretty)

--- src/Way.hs
+++ src/Way.hs
@@ -1,7 +1,7 @@
 module Way (
     WayUnit (..), Way, wayUnit,
 
-    vanilla, profiling, logging, parallel, granSim,
+    vanilla, profiling, logging, Way.parallel, granSim,
     threaded, threadedProfiling, threadedLogging,
     debug, debugProfiling, threadedDebug, threadedDebugProfiling,
     dynamic, profilingDynamic, threadedProfilingDynamic,
