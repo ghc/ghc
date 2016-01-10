1d18a749606d4c093b00e8f4823b89597dae4c9a
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-01-10 12:11:11 +0000

    Fix include paths.
    See #145.

--- src/Settings/Packages/Ghc.hs
+++ src/Settings/Packages/Ghc.hs
@@ -1,16 +1,16 @@
 module Settings.Packages.Ghc (ghcPackageArgs) where
 
-import Base
 import Expression
 import GHC (ghc, compiler)
 import Oracles.Config.Setting
 import Predicates (builder, builderGhc, package, notStage0)
+import Settings.TargetDirectory
 
 ghcPackageArgs :: Args
 ghcPackageArgs = package ghc ? do
     stage <- getStage
     mconcat [ builderGhc ? mconcat
-              [ arg ("-I" ++ pkgPath compiler -/- stageString stage)
+              [ arg $ "-I" ++ targetPath stage compiler
               , arg "-no-hs-main" ]
 
             , builder GhcCabal ?
