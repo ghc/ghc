6e1511f9d48bb5198ab6feb4f9a2e6039df5b67f
Neil Mitchell <ndmitchell@gmail.com>
2016-01-10 20:34:32 +0000

    Remove an unnecessary hiding after disabling name shadow warning

--- src/Rules/Library.hs
+++ src/Rules/Library.hs
@@ -2,7 +2,7 @@ module Rules.Library (buildPackageLibrary, cSources, hSources) where
 
 import Data.Char
 
-import Base hiding (splitPath, split)
+import Base hiding (splitPath)
 import Expression
 import GHC
 import Oracles
