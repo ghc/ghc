3b1dae2267241fa6a959e5d9785e20f712c11af0
Simon Peyton Jones <simonpj@microsoft.com>
2016-01-18 10:43:47 +0000

    Fix typecheck of default associated type decls
    This bug was thrown up by Trac #11361, but I found that the
    problem was deeper: GHC was allowing
    
      class C a where
        type F (a :: k) :: *
        type F (x :: *) = x    -- Not right!
    
    (Which is now indexed-types/should_compile/T11361a.)
    
    Anyway the fix is relatively simple; use tcMatchTys in
    tcDefaultAssocDecl.
    
    Merge to 8.0 branch.
    
    (cherry picked from commit cb24e684759f3d181a104cde76f0f95da896a7ef)

---
+++ testsuite/tests/indexed-types/should_compile/T11361.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+  -- this is needed because |FamHelper a x| /< |Fam a x|
+{-# OPTIONS_GHC -dinitial-unique=16777000 -dunique-increment=-1 #-}
+  -- This is what made GHC crash before
+
+module T11361 where
+
+class Cls a where
+    type Fam a b :: *
+    -- Multiple defaults!
+    type Fam a x = FamHelper a x
+
+type family FamHelper a x
+type instance FamHelper a Bool = Maybe a
+type instance FamHelper a Int  = (String, a)
+
+instance Cls Int where
+    -- Gets type family from default
+
+inc :: (Fam a Bool ~ Maybe Int, Fam a Int ~ (String, Int)) => a -> Fam a Bool -> Fam a Int -> Fam a Bool
+inc _proxy (Just x) (_, y) = Just (x + y + 1)
+inc _proxy Nothing  (_, y) = Just y
+
+foo :: Maybe Int -> (String, Int) -> Maybe Int
+foo = inc (undefined :: Int)
