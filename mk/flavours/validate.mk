66fa0ed7087588b9c1577f44836f712f093089c1
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-02-07 13:14:44 -0500

    validate: enable -DDEBUG in stage 1 by default
    Since the whole point of validation is to test the compiler, assertions
    should be enabled at least for some part of the build. Previously
    assertions were only enabled (1) in stage 2 compiler (2) when "slow"
    setting is used. With this patch we enable assertions in stage 1
    compiler in all settings, to test them on (1) the compiler itself (2)
    the libraries, even with the "fast" setting.
    
    This will make "fast" setting slower, but the difference should be quite
    modest - I didn't realize a significant difference in validation times.
    
    Reviewers: bgamari, austin, thomie
    
    Reviewed By: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1890

--- mk/flavours/validate.mk
+++ mk/flavours/validate.mk
@@ -1,6 +1,6 @@
 SRC_HC_OPTS        = -O0 -H64m
 SRC_HC_OPTS_STAGE1 = -fllvm-fill-undef-with-garbage   # See Trac 11487
-GhcStage1HcOpts    = -O
+GhcStage1HcOpts    = -O -DDEBUG
 GhcStage2HcOpts    = -O -dcore-lint
 GhcLibHcOpts       = -O -dcore-lint
 BUILD_PROF_LIBS    = NO
