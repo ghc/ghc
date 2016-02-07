e1b6c5688198e78d8c1a6261479f69efdc640e1f
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-02-07 02:34:27 +0000

    Fix Haddock documentation.
    Fix #98.

--- src/Rules/Documentation.hs
+++ src/Rules/Documentation.hs
@@ -8,6 +8,9 @@ import Rules.Actions
 import Rules.Resources
 import Settings
 
+haddockHtmlLib :: FilePath
+haddockHtmlLib = "inplace/lib/html/haddock-util.js"
+
 -- Note: this build rule creates plenty of files, not just the .haddock one.
 -- All of them go into the 'doc' subdirectory. Pedantically tracking all built
 -- files in the Shake databases seems fragile and unnecesarry.
@@ -22,9 +25,10 @@ buildPackageDocumentation _ target @ (PartialTarget stage pkg) =
             let haddocks = [ pkgHaddockFile depPkg
                            | Just depPkg <- map findKnownPackage deps
                            , depPkg /= rts ]
-            need $ srcs ++ haddocks
+            need $ srcs ++ haddocks ++ [haddockHtmlLib]
 
             -- HsColour sources
+            -- TODO: what is the output of GhcCabalHsColour?
             whenM (specified HsColour) $ do
                 pkgConf <- pkgConfFile stage pkg
                 need [ cabalFile, pkgConf ] -- TODO: check if need pkgConf
@@ -33,6 +37,11 @@ buildPackageDocumentation _ target @ (PartialTarget stage pkg) =
             -- Build Haddock documentation
             let haddockWay = if dynamicGhcPrograms then dynamic else vanilla
             build $ fullTargetWithWay target Haddock haddockWay srcs [file]
+
+        when (pkg == haddock) $ haddockHtmlLib %> \_ -> do
+            let dir = takeDirectory haddockHtmlLib
+            liftIO $ removeFiles dir ["//*"]
+            copyDirectory "utils/haddock/haddock-api/resources/html" dir
 
 -- # Make the haddocking depend on the library .a file, to ensure
 -- # that we wait until the library is fully built before we haddock it
