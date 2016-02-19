a8653c84a6322d10c646b05ea5406a23a4b7ffbb
Thomas Miedema <thomasmiedema@gmail.com>
2016-02-19 18:58:03 +0100

    Docs: no space in `-i⟨dir1⟩:⟨dir2⟩` [skip ci]

--- utils/mkUserGuidePart/Options/FindingImports.hs
+++ utils/mkUserGuidePart/Options/FindingImports.hs
@@ -4,7 +4,7 @@ import Types
 
 findingImportsOptions :: [Flag]
 findingImportsOptions =
-  [ flag { flagName = "-i ⟨dir1⟩:⟨dir2⟩:..."
+  [ flag { flagName = "-i⟨dir1⟩:⟨dir2⟩:..."
          , flagDescription = "add ⟨dir⟩, ⟨dir2⟩, etc. to import path"
          , flagType = DynamicSettableFlag
          }
