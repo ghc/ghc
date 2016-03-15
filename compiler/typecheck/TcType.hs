19be5385e2875578c3a7d1154238580f0ef3c754
Richard Eisenberg <eir@cis.upenn.edu>
2016-03-15 11:37:00 -0400

    Remove redundant anonymiseTyBinders (#11648)
    This was necessary in an earlier version of the patch for #11648,
    but not in the final version. I forgot to remove it.

--- compiler/typecheck/TcType.hs
+++ compiler/typecheck/TcType.hs
@@ -87,7 +87,6 @@ module TcType (
   orphNamesOfTypes, orphNamesOfCoCon,
   getDFunTyKey,
   evVarPred_maybe, evVarPred,
-  anonymiseTyBinders,
 
   ---------------------------------
   -- Predicate types
@@ -223,7 +222,6 @@ import qualified GHC.LanguageExtensions as LangExt
 import Data.IORef
 import Control.Monad (liftM, ap)
 import Data.Functor.Identity
-import Data.List ( mapAccumR )
 
 {-
 ************************************************************************
@@ -2360,28 +2358,3 @@ sizeType = go
 
 sizeTypes :: [Type] -> TypeSize
 sizeTypes tys = sum (map sizeType tys)
-
-{-
-************************************************************************
-*                                                                      *
-       Binders
-*                                                                      *
-************************************************************************
--}
-
--- | Given a list of binders and a type they bind in, turn any
--- superfluous Named binders into Anon ones.
-anonymiseTyBinders :: [TyBinder] -> Type -> [TyBinder]
-anonymiseTyBinders binders res_ty = binders'
-  where
-    (_, binders') = mapAccumR go (tyCoVarsOfTypeAcc res_ty) binders
-
-    go :: FV -> TyBinder -> (FV, TyBinder)
-    go fv (Named tv Visible)
-      | not (tv `elemVarSet` runFVSet fv)
-      = ( (tv `FV.delFV` fv) `unionFV` tyCoVarsOfTypeAcc kind
-        , Anon kind )
-      where
-        kind = tyVarKind tv
-
-    go fv binder = (tyCoVarsBndrAcc binder fv, binder)
