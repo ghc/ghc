8dff21104b6a56e458943f25f3013fb2b0be0a0f
Erik de Castro Lopo <erikd@mega-nerd.com>
2016-03-16 20:19:28 +1100

    Add test for #9646
    Test Plan: Test that it passes git HEAD and fails with GHC 7.8.
    
    Reviewers: bgamari, hvr, austin, goldfire, thomie
    
    Differential Revision: https://phabricator.haskell.org/D2009
    
    GHC Trac Issues: #9646
    
    (cherry picked from commit 220a0b934c71a8844a14dd8cd67fa0e23f807182)

---
+++ testsuite/tests/simplCore/T9646/cbits/primitive-memops.h
@@ -0,0 +1,10 @@
+#ifndef haskell_primitive_memops_h
+#define haskell_primitive_memops_h
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <HsFFI.h>
+
+void hsprimitive_memset_Word (HsWord *, ptrdiff_t, size_t, HsWord);
+
+#endif
