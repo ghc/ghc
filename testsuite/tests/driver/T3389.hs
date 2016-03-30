b6be8a106c6c6fb477832220b606f19a5943f083
Thomas Miedema <thomasmiedema@gmail.com>
2016-03-30 21:43:51 +0200

    Don't require -hide-all-packages for MIN_VERSION_* macros
    Define MIN_VERSION_pkgname and VERSION_pkgname macros for all exposed
    packages, without requiring -hide-all-packages.
    
    See #10970 comment 7-10 for discussion.
    
    Reviewers: duncan, ezyang, bgamari, austin
    
    Reviewed By: ezyang
    
    Subscribers: hvr, rwbarton
    
    Differential Revision: https://phabricator.haskell.org/D1869
    
    GHC Trac Issues: #10970
    
    (cherry picked from commit e9c2555ac666912f7dff56448ced4bfa06d14e76)

--- testsuite/tests/driver/T3389.hs
+++ testsuite/tests/driver/T3389.hs
@@ -6,7 +6,7 @@
 
 main :: IO ()
 main = putStrLn [A,
--- /*
+-- CPP treats this as the beginning of a multiline C-style comment: /usr/local/*
                  'b',
--- */
+-- And */ close it. By passing `-optP -C` we tell CPP not to delete it.
                  'c']
