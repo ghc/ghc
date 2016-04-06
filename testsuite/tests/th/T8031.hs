dd99f2ece1bd139be02beddc6dc672862ee5ae34
Richard Eisenberg <eir@cis.upenn.edu>
2016-04-06 16:37:22 +0200

    Fix #11797.
    DsMeta curiously omitted quantified tyvars in certain circumstances.
    This patch means it doesn't.
    
    Test case: th/T11797

--- testsuite/tests/th/T8031.hs
+++ testsuite/tests/th/T8031.hs
@@ -1,13 +1,14 @@
-{-# LANGUAGE TemplateHaskell, RankNTypes, DataKinds, TypeOperators, PolyKinds,
+{-# LANGUAGE TemplateHaskell, RankNTypes, TypeOperators, TypeInType,
              GADTs #-}
 
 module T8031 where
 
 import Data.Proxy
+import Data.Kind
 
 data SList :: [k] -> * where
   SCons :: Proxy h -> Proxy t -> SList (h ': t)
-  
+
 $( [d| foo :: forall (a :: k). Proxy a
            -> forall (b :: [k]). Proxy b
            -> SList (a ': b)
