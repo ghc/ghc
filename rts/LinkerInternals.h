4f6960bff673525ba399f769e1e585c0475c8ca9
Tamar Christina <tamar@zhox.com>
2016-04-17 13:03:17 +0200

    Add Windows import library support to the Runtime Linker
    Summary:
    Import libraries are files ending in `.dll.a` and `.lib` depending on which
    compiler creates them (GCC, vs MSVC).
    
    Import Libraries are standard `archive` files that contain object files.
    These object files can have two different formats:
    
    1) The normal COFF Object format for object files
        (contains all ascii data and very little program code, so do not
         try to execute.)
    2) "short import" format which just contains a symbol name and
       the dll in which the symbol can be found.
    
    Import Libraries are useful for two things:
    
    1) Allowing applications that don't support dynamic linking to
       link against the import lib (non-short format) which then
       makes calls into the DLL by loading it at runtime.
    
    2) Allow linking of mutually recursive dlls. if `A.DLL` requires
       `B.DLL` and vice versa, import libs can be used to break the cycle
       as they can be created from the expected exports of the DLLs.
    
    A side effect of having these two capabilities is that Import libs are often
    used to hide specific versions of DLLs behind a non-versioned import lib.
    
    e.g. GCC_S.a (non-conventional import lib) will point to the correct
    `libGCC` DLL. With this support Windows Haskell files can now just link
    to `-lGCC_S` and not have to worry about what the actual name of libGCC is.
    
    Also third party libraries such as `icuuc` use import libs to forward to
    versioned DLLs. e.g. `icuuc.lib` points to `icuuc51.dll` etc.
    
    Test Plan:
    ./validate
    
    Two new tests added T11072gcc T11072msvc
    
    Two binary files have been added to the test folder because the "short"
    import library format doesn't seem to be creatable via `dlltool`
    and requires Microsoft's `lib.exe`.
    
    Reviewers: bgamari, RyanGlScott, erikd, goldfire, austin, hvr
    
    Reviewed By: RyanGlScott, erikd
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D1696
    
    GHC Trac Issues: #11072
    
    (cherry picked from commit 97f2b16483aae28dc8fd60b6d2e1e283618f2390)

--- rts/LinkerInternals.h
+++ rts/LinkerInternals.h
@@ -16,7 +16,8 @@ typedef enum {
     OBJECT_LOADED,
     OBJECT_NEEDED,
     OBJECT_RESOLVED,
-    OBJECT_UNLOADED
+    OBJECT_UNLOADED,
+    OBJECT_DONT_RESOLVE
 } OStatus;
 
 /* Indication of section kinds for loaded objects.  Needed by
@@ -172,6 +173,11 @@ typedef struct _ObjectCode {
 
     ForeignExportStablePtr *stable_ptrs;
 
+    /* Indicates whether if the .o file comes from
+       an import library. In which case we shouldn't
+       execute code from it. */
+    HsBool isImportLib;
+
 } ObjectCode;
 
 #define OC_INFORMATIVE_FILENAME(OC)             \
@@ -191,5 +197,122 @@ extern Mutex linker_unloaded_mutex;
 void exitLinker( void );
 
 void freeObjectCode (ObjectCode *oc);
+
+#if defined(mingw32_HOST_OS)
+
+typedef unsigned char          UChar;
+typedef unsigned short         UInt16;
+typedef short                  Int16;
+typedef unsigned int           UInt32;
+typedef          int           Int32;
+typedef unsigned long long int UInt64;
+
+
+typedef
+struct {
+    UInt16 Machine;
+    UInt16 NumberOfSections;
+    UInt32 TimeDateStamp;
+    UInt32 PointerToSymbolTable;
+    UInt32 NumberOfSymbols;
+    UInt16 SizeOfOptionalHeader;
+    UInt16 Characteristics;
+}
+COFF_header;
+
+#define sizeof_COFF_header 20
+
+/* Section 7.1 PE Specification */
+typedef
+struct {
+    UInt16 Sig1;
+    UInt16 Sig2;
+    UInt16 Version;
+    UInt16 Machine;
+    UInt32 TimeDateStamp;
+    UInt32 SizeOfData;
+    UInt16 Ordinal;
+    UInt16 Type_NameType_Reserved;
+}
+COFF_import_header;
+
+#define sizeof_COFF_import_Header 20
+
+typedef
+struct {
+    UChar  Name[8];
+    UInt32 VirtualSize;
+    UInt32 VirtualAddress;
+    UInt32 SizeOfRawData;
+    UInt32 PointerToRawData;
+    UInt32 PointerToRelocations;
+    UInt32 PointerToLinenumbers;
+    UInt16 NumberOfRelocations;
+    UInt16 NumberOfLineNumbers;
+    UInt32 Characteristics;
+}
+COFF_section;
+
+#define sizeof_COFF_section 40
+
+
+typedef
+struct {
+    UChar  Name[8];
+    UInt32 Value;
+    Int16  SectionNumber;
+    UInt16 Type;
+    UChar  StorageClass;
+    UChar  NumberOfAuxSymbols;
+}
+COFF_symbol;
+
+#define sizeof_COFF_symbol 18
+
+
+typedef
+struct {
+    UInt32 VirtualAddress;
+    UInt32 SymbolTableIndex;
+    UInt16 Type;
+}
+COFF_reloc;
+
+#define sizeof_COFF_reloc 10
+
+/* From PE spec doc, section 3.3.2 */
+/* Note use of MYIMAGE_* since IMAGE_* are already defined in
+windows.h -- for the same purpose, but I want to know what I'm
+getting, here. */
+#define MYIMAGE_FILE_RELOCS_STRIPPED        0x0001
+#define MYIMAGE_FILE_EXECUTABLE_IMAGE       0x0002
+#define MYIMAGE_FILE_DLL                    0x2000
+#define MYIMAGE_FILE_SYSTEM                 0x1000
+#define MYIMAGE_FILE_BYTES_REVERSED_HI      0x8000
+#define MYIMAGE_FILE_BYTES_REVERSED_LO      0x0080
+#define MYIMAGE_FILE_32BIT_MACHINE          0x0100
+
+/* From PE spec doc, section 5.4.2 and 5.4.4 */
+#define MYIMAGE_SYM_CLASS_EXTERNAL          2
+#define MYIMAGE_SYM_CLASS_STATIC            3
+#define MYIMAGE_SYM_UNDEFINED               0
+#define MYIMAGE_SYM_CLASS_SECTION           104
+#define MYIMAGE_SYM_CLASS_WEAK_EXTERNAL     105
+
+/* From PE spec doc, section 3.1 */
+#define MYIMAGE_SCN_CNT_CODE                0x00000020
+#define MYIMAGE_SCN_CNT_INITIALIZED_DATA    0x00000040
+#define MYIMAGE_SCN_CNT_UNINITIALIZED_DATA  0x00000080
+#define MYIMAGE_SCN_LNK_COMDAT              0x00001000
+#define MYIMAGE_SCN_LNK_NRELOC_OVFL         0x01000000
+#define MYIMAGE_SCN_LNK_REMOVE              0x00000800
+#define MYIMAGE_SCN_MEM_DISCARDABLE         0x02000000
+
+/* From PE spec doc, section 5.2.1 */
+#define MYIMAGE_REL_I386_DIR32              0x0006
+#define MYIMAGE_REL_I386_DIR32NB            0x0007
+#define MYIMAGE_REL_I386_REL32              0x0014
+
+#endif /* OBJFORMAT_PEi386 */
 
 #endif /* LINKERINTERNALS_H */
