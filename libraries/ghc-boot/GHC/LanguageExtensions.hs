eed820b672e6c3d23106cd151b1e31ce29326e32
Ben Gamari <bgamari.foss@gmail.com>
2016-05-16 15:21:34 +0200

    Move Extension type to ghc-boot-th
    This creates a new package, `ghc-boot-th`, to contain the `Extension`
    type, which now lives in `GHC.LanguageExtension.Type`. This ensures that
    the transitive dependency set of the `template-haskell` package remains
    minimal.
    
    The `GHC.LanguageExtensions.Type` module is also re-exported by
    `ghc-boot`, which provides an orphan `binary` instance as well.
    
    Test Plan: Validate
    
    Reviewers: goldfire, thomie, hvr, austin
    
    Reviewed By: thomie
    
    Subscribers: RyanGlScott, thomie, erikd, ezyang
    
    Differential Revision: https://phabricator.haskell.org/D2224

--- libraries/ghc-boot/GHC/LanguageExtensions.hs
+++ libraries/ghc-boot/GHC/LanguageExtensions.hs
@@ -1,130 +1,17 @@
------------------------------------------------------------------------------
--- |
--- Module      :  GHC.LanguageExtensions
--- Copyright   :  (c) The GHC Team
---
--- Maintainer  :  ghc-devs@haskell.org
--- Portability :  portable
---
--- A data type defining the language extensions supported by GHC.
---
-{-# LANGUAGE DeriveGeneric #-}
-module GHC.LanguageExtensions ( Extension(..) ) where
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
-import GHC.Generics
+-- | This module re-exports the 'Extension' type along with an orphan 'Binary'
+-- instance for it.
+--
+-- Note that the @ghc-boot@ package has a large set of dependencies; for this
+-- reason the 'Extension' type itself is defined in the
+-- "GHC.LanguageExtensions.Type" module provided by the @ghc-boot-th@ package,
+-- which has no dependencies outside of @base@. For this reason
+-- @template-haskell@ depends upon @ghc-boot-th@, not @ghc-boot@.
+--
+module GHC.LanguageExtensions ( module GHC.LanguageExtensions.Type ) where
+
 import Data.Binary
-
--- | The language extensions known to GHC.
-data Extension
--- See Note [Updating flag description in the User's Guide] in DynFlags
-   = Cpp
-   | OverlappingInstances
-   | UndecidableInstances
-   | IncoherentInstances
-   | UndecidableSuperClasses
-   | MonomorphismRestriction
-   | MonoPatBinds
-   | MonoLocalBinds
-   | RelaxedPolyRec           -- Deprecated
-   | ExtendedDefaultRules     -- Use GHC's extended rules for defaulting
-   | ForeignFunctionInterface
-   | UnliftedFFITypes
-   | InterruptibleFFI
-   | CApiFFI
-   | GHCForeignImportPrim
-   | JavaScriptFFI
-   | ParallelArrays           -- Syntactic support for parallel arrays
-   | Arrows                   -- Arrow-notation syntax
-   | TemplateHaskell
-   | TemplateHaskellQuotes    -- subset of TH supported by stage1, no splice
-   | QuasiQuotes
-   | ImplicitParams
-   | ImplicitPrelude
-   | ScopedTypeVariables
-   | AllowAmbiguousTypes
-   | UnboxedTuples
-   | BangPatterns
-   | TypeFamilies
-   | TypeFamilyDependencies
-   | TypeInType
-   | OverloadedStrings
-   | OverloadedLists
-   | NumDecimals
-   | DisambiguateRecordFields
-   | RecordWildCards
-   | RecordPuns
-   | ViewPatterns
-   | GADTs
-   | GADTSyntax
-   | NPlusKPatterns
-   | DoAndIfThenElse
-   | RebindableSyntax
-   | ConstraintKinds
-   | PolyKinds                -- Kind polymorphism
-   | DataKinds                -- Datatype promotion
-   | InstanceSigs
-   | ApplicativeDo
-
-   | StandaloneDeriving
-   | DeriveDataTypeable
-   | AutoDeriveTypeable       -- Automatic derivation of Typeable
-   | DeriveFunctor
-   | DeriveTraversable
-   | DeriveFoldable
-   | DeriveGeneric            -- Allow deriving Generic/1
-   | DefaultSignatures        -- Allow extra signatures for defmeths
-   | DeriveAnyClass           -- Allow deriving any class
-   | DeriveLift               -- Allow deriving Lift
-
-   | TypeSynonymInstances
-   | FlexibleContexts
-   | FlexibleInstances
-   | ConstrainedClassMethods
-   | MultiParamTypeClasses
-   | NullaryTypeClasses
-   | FunctionalDependencies
-   | UnicodeSyntax
-   | ExistentialQuantification
-   | MagicHash
-   | EmptyDataDecls
-   | KindSignatures
-   | RoleAnnotations
-   | ParallelListComp
-   | TransformListComp
-   | MonadComprehensions
-   | GeneralizedNewtypeDeriving
-   | RecursiveDo
-   | PostfixOperators
-   | TupleSections
-   | PatternGuards
-   | LiberalTypeSynonyms
-   | RankNTypes
-   | ImpredicativeTypes
-   | TypeOperators
-   | ExplicitNamespaces
-   | PackageImports
-   | ExplicitForAll
-   | AlternativeLayoutRule
-   | AlternativeLayoutRuleTransitional
-   | DatatypeContexts
-   | NondecreasingIndentation
-   | RelaxedLayout
-   | TraditionalRecordSyntax
-   | LambdaCase
-   | MultiWayIf
-   | BinaryLiterals
-   | NegativeLiterals
-   | DuplicateRecordFields
-   | OverloadedLabels
-   | EmptyCase
-   | PatternSynonyms
-   | PartialTypeSignatures
-   | NamedWildCards
-   | StaticPointers
-   | TypeApplications
-   | Strict
-   | StrictData
-   | MonadFailDesugaring
-   deriving (Eq, Enum, Show, Generic)
+import GHC.LanguageExtensions.Type
 
 instance Binary Extension
