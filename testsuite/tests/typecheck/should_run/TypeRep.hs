2e6433af56a97ad9c501648afa5e1d8c6c17f82a
Ben Gamari <bgamari.foss@gmail.com>
2016-05-19 21:05:22 +0200

    testsuite: Add a TypeRep test
    Test Plan: Validate
    
    Reviewers: goldfire, austin
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D2239
    
    GHC Trac Issues: #12082, #11120

---
+++ testsuite/tests/typecheck/should_run/TypeRep.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeInType #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeOperators #-}
+
+import Data.Typeable
+import Data.Kind
+import GHC.Exts
+
+-- Test that Typeable works for various wired-in types.
+-- See, for instance, #11120.
+
+rep :: forall a. Typeable a => TypeRep
+rep = typeRep (Proxy :: Proxy a)
+
+main :: IO ()
+main = do
+  -- the basics
+  print $ rep @String
+  print $ rep @Char
+  print $ rep @Int
+  print $ rep @Word
+  print $ rep @Double
+  print $ rep @(IO ())
+  print $ rep @IO
+  print $ rep @"hi"
+  print $ rep @(Char, Int, String)
+  print $ rep @Bool
+  print $ rep @Ordering
+  print $ rep @(Int -> Int)
+
+  -- Various instantiations of a kind-polymorphic type
+  print $ rep @(Proxy (Eq Int))
+  print $ rep @(Proxy (Int, Int))
+  print $ rep @(Proxy "hello world")
+  print $ rep @(Proxy 1)
+  print $ rep @(Proxy [1,2,3])
+  print $ rep @(Proxy 'EQ)
+  print $ rep @(Proxy TYPE)
+  print $ rep @(Proxy (TYPE 'PtrRepLifted))
+  print $ rep @(Proxy *)
+  print $ rep @(Proxy ★)
+  print $ rep @(Proxy 'PtrRepLifted)
+
+  -- Something lifted and primitive
+  print $ rep @RealWorld
