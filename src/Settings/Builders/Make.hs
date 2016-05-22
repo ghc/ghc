3de1a5a65b7d97635ccd9a14601113b0802cd8f7
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-05-22 01:22:19 +0100

    Run Make builder with -jN using N = shakeThreads

--- src/Settings/Builders/Make.hs
+++ src/Settings/Builders/Make.hs
@@ -1,10 +1,14 @@
 module Settings.Builders.Make (makeBuilderArgs) where
 
+import Base
 import Predicate
 import Settings
 
 makeBuilderArgs :: Args
-makeBuilderArgs = mconcat
-    [ builder (Make gmpBuildPath     ) ? arg "MAKEFLAGS="
-    , builder (Make libffiBuildPath  ) ? append ["MAKEFLAGS=", "install"]
-    , builder (Make "testsuite/tests") ? arg "fast" ]
+makeBuilderArgs = do
+    threads <- shakeThreads <$> lift getShakeOptions
+    let j = "-j" ++ show threads
+    mconcat
+        [ builder (Make gmpBuildPath     ) ? append ["MAKEFLAGS=" ++ j]
+        , builder (Make libffiBuildPath  ) ? append ["MAKEFLAGS=" ++ j, "install"]
+        , builder (Make "testsuite/tests") ? arg "fast" ]
