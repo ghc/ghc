66d70fd68b4b52f0e6751679f5702964c122f767
Matthew Pickering <matthewtpickering@gmail.com>
2016-05-22 11:52:26 +0100

    Allow unlifted types in pattern synonym result type
    Fixes #12094
    
    Test Plan: ./validate
    
    Reviewers: austin, bgamari
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D2255
    
    GHC Trac Issues: #12094
    
    (cherry picked from commit 8c9b8a31dd9e085704ecac3361a64f196a0bc09d)

--- compiler/typecheck/TcPatSyn.hs
+++ compiler/typecheck/TcPatSyn.hs
@@ -14,7 +14,7 @@ module TcPatSyn ( tcPatSynSig, tcInferPatSynDecl, tcCheckPatSynDecl
 import HsSyn
 import TcPat
 import TcHsType( tcImplicitTKBndrs, tcExplicitTKBndrs
-               , tcHsContext, tcHsLiftedType, tcHsOpenType, kindGeneralize )
+               , tcHsContext, tcHsOpenType, kindGeneralize )
 import TcRnMonad
 import TcEnv
 import TcMType
@@ -117,7 +117,9 @@ tcPatSynSig name sig_ty
               do { req     <- tcHsContext hs_req
                  ; prov    <- tcHsContext hs_prov
                  ; arg_tys <- mapM tcHsOpenType (hs_arg_tys :: [LHsType Name])
-                 ; body_ty <- tcHsLiftedType hs_body_ty
+                 -- A (literal) pattern can be unlifted;
+                 -- -- e.g. pattern Zero <- 0#   (Trac #12094)
+                 ; body_ty <- tcHsOpenType hs_body_ty
                  ; let bound_tvs
                          = unionVarSets [ allBoundVariabless req
                                         , allBoundVariabless prov
