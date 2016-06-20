6d6834a6abebaff0d4aa60e615a027d68ed964d9
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-06-20 03:08:45 +0100

    Fix Quick flavour
    See #259, #268.

--- src/Settings/Flavours/Quick.hs
+++ src/Settings/Flavours/Quick.hs
@@ -1,9 +1,17 @@
 module Settings.Flavours.Quick (quickFlavourArgs, quickFlavourWays) where
 
+import Context
+import GHC
 import Predicate
 
+optimise :: Context -> Bool
+optimise Context {..} = stage == Stage0 && package `elem` [compiler, ghc]
+                     || stage == Stage1 && isLibrary package
+
 quickFlavourArgs :: Args
-quickFlavourArgs = builder Ghc ? arg "-O0"
+quickFlavourArgs = builder Ghc ? do
+    context <- getContext
+    if optimise context then arg "-O" else arg "-O0"
 
 quickFlavourWays :: Ways
 quickFlavourWays = remove [profiling]
