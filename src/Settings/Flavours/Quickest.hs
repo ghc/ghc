0955d43bbbde1ee3ab71d3a01bf3d2f6e53afbe9
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-06-21 20:10:31 +0100

    Don't optimise GHC in Quickest flavour
    See #259, #268.

--- src/Settings/Flavours/Quickest.hs
+++ src/Settings/Flavours/Quickest.hs
@@ -1,16 +1,9 @@
 module Settings.Flavours.Quickest (quickestFlavourArgs, quickestFlavourWays) where
 
-import Context
-import GHC
 import Predicate
 
-optimise :: Context -> Bool
-optimise Context {..} = stage == Stage0 && package `elem` [compiler, ghc]
-
 quickestFlavourArgs :: Args
-quickestFlavourArgs = builder Ghc ? do
-    context <- getContext
-    if optimise context then arg "-O" else arg "-O0"
+quickestFlavourArgs = builder Ghc ? arg "-O0"
 
 quickestFlavourWays :: Ways
 quickestFlavourWays = remove [profiling]
