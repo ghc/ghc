b819d4464ee835fef21a4dd403dc18c14db27426
Markus Rothe <markusr815@gmail.com>
2016-06-26 11:28:36 +0200

    rules/sphinx.mk: stop xelatex on error
    This avoids the interactive prompt of xelatex on error.
    
    (cherry picked from commit 6ba4197e006b6d6bc2657141d4c10c91bd806cb3)

--- rules/sphinx.mk
+++ rules/sphinx.mk
@@ -58,12 +58,12 @@ pdf : pdf_$1
 ifneq "$$(BINDIST)" "YES"
 $1/$2.pdf: $1/conf.py $$($1_RST_SOURCES)
 	$(SPHINXBUILD) -b latex -d $1/.doctrees-pdf $(SPHINXOPTS) $1 $1/build-pdf/$2
-	cd $1/build-pdf/$2 ; xelatex $2.tex
-	cd $1/build-pdf/$2 ; xelatex $2.tex
-	cd $1/build-pdf/$2 ; xelatex $2.tex
+	cd $1/build-pdf/$2 ; xelatex -halt-on-error $2.tex
+	cd $1/build-pdf/$2 ; xelatex -halt-on-error $2.tex
+	cd $1/build-pdf/$2 ; xelatex -halt-on-error $2.tex
 	cd $1/build-pdf/$2 ; makeindex $2.idx
-	cd $1/build-pdf/$2 ; xelatex $2.tex
-	cd $1/build-pdf/$2 ; xelatex $2.tex
+	cd $1/build-pdf/$2 ; xelatex -halt-on-error $2.tex
+	cd $1/build-pdf/$2 ; xelatex -halt-on-error $2.tex
 	cp $1/build-pdf/$2/$2.pdf $1/$2.pdf
 endif
 
