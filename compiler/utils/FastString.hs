91fd87e2384091f1872f91953e83b299d8e1478f
Ben Gamari <ben@smart-cactus.org>
2016-07-04 20:33:57 -0400

    FastString: Reduce allocations of concatFS
    Instead of unpacking and then repacking we simply concatenate all of the
    individual ByteStrings.

--- compiler/utils/FastString.hs
+++ compiler/utils/FastString.hs
@@ -512,7 +512,7 @@ appendFS fs1 fs2 = mkFastStringByteString
                              (fastStringToByteString fs2)
 
 concatFS :: [FastString] -> FastString
-concatFS ls = mkFastString (Prelude.concat (map unpackFS ls)) -- ToDo: do better
+concatFS = mkFastStringByteString . BS.concat . map fs_bs
 
 headFS :: FastString -> Char
 headFS (FastString _ 0 _ _) = panic "headFS: Empty FastString"
