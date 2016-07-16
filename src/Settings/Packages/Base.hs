116e64d5596233dcacff48ce7e5e0531f730e6bd
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-07-17 00:52:00 +0100

    Attempt to fix the 'unknown symbol stat' issue on Travis Linux
    See #259.

--- src/Settings/Packages/Base.hs
+++ src/Settings/Packages/Base.hs
@@ -6,5 +6,6 @@ import Predicate
 import UserSettings
 
 basePackageArgs :: Args
-basePackageArgs = package base ?
-    builder GhcCabal ? arg ("--flags=" ++ takeFileName (pkgPath integerLibrary))
+basePackageArgs = package base ? mconcat
+    [ builder GhcCabal ? arg ("--flags=" ++ takeFileName (pkgPath integerLibrary))
+    , builder Cc ? arg "-O2" ] -- Fix the 'unknown symbol stat' issue, see #259.
