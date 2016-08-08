4d9c22d01561bda90632d9a470fd2a2bb167658a
Ryan Scott <ryan.gl.scott@gmail.com>
2016-08-08 12:59:17 -0400

    Fix typo in Data.Bitraversable Haddocks
    [ci skip]

--- libraries/base/Data/Bitraversable.hs
+++ libraries/base/Data/Bitraversable.hs
@@ -40,7 +40,7 @@ import GHC.Generics (K1(..))
 -- element on which an action can be performed, 'Bitraversable' data structures
 -- have two such varieties of elements.
 --
--- A definition of 'traverse' must satisfy the following laws:
+-- A definition of 'bitraverse' must satisfy the following laws:
 --
 -- [/naturality/]
 --   @'bitraverse' (t . f) (t . g) ≡ t . 'bitraverse' f g@
