f937d806ea8397132405eeede17f2662d8b0c85f
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-09-01 17:58:44 +0100

    Exit the build script if Hadrian cannot be built

--- build.stack.bat
+++ build.stack.bat
@@ -1,8 +1,9 @@
 @rem Change the current directory to the one containing this script
 @cd %~dp0
 
-@rem Build Hadrian and dependencies
+@rem Build Hadrian and dependencies and exit the script if the build failed
 @stack build
+@if %errorlevel% neq 0 exit /B %errorlevel%
 
 @rem Run Hadrian in GHC top directory forwarding additional user arguments
 @stack exec hadrian -- --lint --directory ".." %*
