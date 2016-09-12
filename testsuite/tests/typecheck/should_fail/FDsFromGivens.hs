5eeabe250a1de456f70af07bd3f507a32cb8e10e
Simon Peyton Jones <simonpj@microsoft.com>
2016-09-12 22:21:15 +0100

    Test wibbles for commit 03541cba
    I must have failed to validate properly, sorry.
    These testsuite wibbles belong with
    
      commit 03541cbae50f0d1cdf99120ab88698f29a278159
      Author: Simon Peyton Jones <simonpj@microsoft.com>
      Date:   Fri Sep 9 17:42:42 2016 +0100
    
          Be less picky about reporing inaccessible code

--- testsuite/tests/typecheck/should_fail/FDsFromGivens.hs
+++ testsuite/tests/typecheck/should_fail/FDsFromGivens.hs
@@ -6,7 +6,10 @@ class C a b | a -> b where
    cop :: a -> b -> ()
 
 {- Failing, as it righteously should! It's inaccessible code -}
--- But (c.f. test T5236) we no longer reject this (see Trac #12466)
+-- But (c.f. test T5236) we no longer reject this in the
+-- typechecker (see Trac #12466)
+-- Instead we get a redundant pattern-match warning,
+-- in the post-typechecking pattern-match checks
 g1 :: (C Char [a], C Char Bool) => a -> ()
 g1 x = ()
 
