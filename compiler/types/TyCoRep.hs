28a00eafd7abc474e6e5c3d92d3c0ec561906c0b
Gabor Greif <ggreif@gmail.com>
2016-09-30 16:37:53 +0200

    Correct spelling in note references

--- compiler/types/TyCoRep.hs
+++ compiler/types/TyCoRep.hs
@@ -2954,7 +2954,7 @@ pprTcApp_help to_type p pp tc tys dflags style
     pp_tc        = ppr tc
     tys_wo_kinds = suppressInvisibles to_type dflags tc tys
 
-    -- See Note [Printing eqauality constraints]
+    -- See Note [Printing equality constraints]
     mb_saturated_equality
       | hetero_eq_tc
       , [k1, k2, t1, t2] <- tys
@@ -2965,13 +2965,13 @@ pprTcApp_help to_type p pp tc tys dflags style
       | otherwise
       = Nothing
 
-    -- See Note [Printing eqauality constraints]
+    -- See Note [Printing equality constraints]
     homo_eq_tc   =  tc `hasKey` eqTyConKey             -- ~
     hetero_eq_tc =  tc `hasKey` eqPrimTyConKey         -- ~#
                  || tc `hasKey` eqReprPrimTyConKey     -- ~R#
                  || tc `hasKey` heqTyConKey            -- ~~
 
-    -- See Note [Printing eqauality constraints]
+    -- See Note [Printing equality constraints]
     print_equality (ki1, ki2, ty1, ty2)
       | print_eqs
       = ppr_infix_eq pp_tc
