dbfe8adf5a6a2512797df9be3301b60f93de3cf1
Simon Peyton Jones <simonpj@microsoft.com>
2016-09-30 16:15:28 +0100

    Less voluminous trace

--- compiler/typecheck/TcSMonad.hs
+++ compiler/typecheck/TcSMonad.hs
@@ -3061,13 +3061,13 @@ matchFam tycon args = wrapTcS $ matchFamTcM tycon args
 matchFamTcM :: TyCon -> [Type] -> TcM (Maybe (Coercion, TcType))
 -- Given (F tys) return (ty, co), where co :: F tys ~ ty
 matchFamTcM tycon args
-  = do { fam_envs@(_,lcl) <- FamInst.tcGetFamInstEnvs
+  = do { fam_envs@(_,_lcl) <- FamInst.tcGetFamInstEnvs
        ; let match_fam_result
               = reduceTyFamApp_maybe fam_envs Nominal tycon args
        ; TcM.traceTc "matchFamTcM" $
          vcat [ text "Matching:" <+> ppr (mkTyConApp tycon args)
-              , ppr_res match_fam_result
-              , text "Lcl fam env:" <+> ppr lcl ]
+              , ppr_res match_fam_result ]
+--              , text "Lcl fam env:" <+> ppr _lcl ]
        ; return match_fam_result }
   where
     ppr_res Nothing        = text "Match failed"
