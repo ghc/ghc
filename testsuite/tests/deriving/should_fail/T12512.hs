15df51703a45880a794e8d0f6327c57e368d3cb4
Ben Gamari <ben@smart-cactus.org>
2016-10-01 23:19:09 -0400

    Fix T12512

--- testsuite/tests/deriving/should_fail/T12512.hs
+++ testsuite/tests/deriving/should_fail/T12512.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE UnboxedSums #-}
 {-# LANGUAGE UnboxedTuples #-}
 module T12512 where
 
@@ -9,6 +8,3 @@ import GHC.Exts
 
 class Wat1 (a :: TYPE 'UnboxedTupleRep)
 deriving instance Wat1 (# a, b #)
-
-class Wat2 (a :: TYPE 'UnboxedSumRep)
-deriving instance Wat2 (# a | b #)
