703429d917a4349d0a7ec8809dfb41c8b3433937
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-10-05 17:36:32 +0100

    Limit Make's thread
    See #289.

--- src/Settings/Builders/Make.hs
+++ src/Settings/Builders/Make.hs
@@ -7,7 +7,7 @@ import Settings.Paths
 makeBuilderArgs :: Args
 makeBuilderArgs = do
     threads <- shakeThreads <$> lift getShakeOptions
-    let t = show threads
+    let t = show $ max 4 (threads - 2) -- Don't use all Shake's threads
     mconcat
         [ builder (Make gmpBuildPath     ) ? append ["MAKEFLAGS=-j" ++ t]
         , builder (Make libffiBuildPath  ) ? append ["MAKEFLAGS=-j" ++ t, "install"]
