ghc/ghc

commit 8c6a3d68c0301bb985aa2a462936bbcf7584ae9c

Add missing Semigroup instances for Monoidal datatypes in base
Summary:
There are currently three datatypes that are exposed in `base` that have
`Monoid` instances, but no `Semigroup` instances:

* `IO`
* `Event` (from `GHC.Event`)
* `Lifetime` (from `GHC.Event`)

(There is also `EventLifetime` in `GHC.Event.Internal`, but it is not exported
directly, so I didn't bother with it.)

Adding the `Semigroup` instances for these types directly in the modules in
which they're defined resulted in some horrific import cycles, so I opted to
take the easy approach of defining all of these instances in `Data.Semigroup`.
(When `Semigroup` becomes a superclass of `Monoid`, these instances will have
to be moved somehow.)

Fixes #12464.

Test Plan: It compiles

Reviewers: hvr, ekmett, austin, bgamari

Reviewed By: ekmett

Subscribers: thomie

Differential Revision: https://phabricator.haskell.org/D2588

GHC Trac Issues: #12464

libraries/base/Data/Semigroup.hs
@@ -83,6 +83,7 @@ import           Data.Monoid         (All (..), Any (..), Dual (..), Endo (..),
 import           Data.Monoid         (Alt (..))
 import qualified Data.Monoid         as Monoid
 import           Data.Void
+import           GHC.Event           (Event, Lifetime (..))
 import           GHC.Generics
 
 infixr 6 <>
@@ -705,3 +706,17 @@ instance Semigroup (Proxy s) where
   _ <> _ = Proxy
   sconcat _ = Proxy
   stimes _ _ = Proxy
+
+-- | @since 4.10.0.0
+instance Semigroup a => Semigroup (IO a) where
+    (<>) = liftA2 (<>)
+
+-- | @since 4.10.0.0
+instance Semigroup Event where
+    (<>) = mappend
+    stimes = stimesMonoid
+
+-- | @since 4.10.0.0
+instance Semigroup Lifetime where
+    (<>) = mappend
+    stimes = stimesMonoid

commit e39589e2e4f788565c4a7f02cb85802214a95757

Fix Windows build following D2588
Commit 8c6a3d68c0301bb985aa2a462936bbcf7584ae9c inadvertently broke the build
on Windows. This restores Windows compatibility.

libraries/base/Data/Semigroup.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                        #-}
 {-# LANGUAGE DefaultSignatures          #-}
 {-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE DeriveGeneric              #-}
@@ -83,7 +84,9 @@ import           Data.Monoid         (All (..), Any (..), Dual (..), Endo (..),
 import           Data.Monoid         (Alt (..))
 import qualified Data.Monoid         as Monoid
 import           Data.Void
-import           GHC.Event           (Event, Lifetime (..))
+#ifndef mingw32_HOST_OS
+import           GHC.Event           (Event, Lifetime)
+#endif
 import           GHC.Generics
 
 infixr 6 <>
@@ -711,6 +714,7 @@ instance Semigroup (Proxy s) where
 instance Semigroup a => Semigroup (IO a) where
     (<>) = liftA2 (<>)
 
+#ifndef mingw32_HOST_OS
 -- | @since 4.10.0.0
 instance Semigroup Event where
     (<>) = mappend
@@ -720,3 +724,4 @@ instance Semigroup Event where
 instance Semigroup Lifetime where
     (<>) = mappend
     stimes = stimesMonoid
+#endif