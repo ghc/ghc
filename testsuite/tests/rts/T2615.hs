46f5f0284aa2cbe2e57716e1a4df5cb15037082f
Simon Marlow <marlowsd@gmail.com>
2016-10-18 15:18:28 +0100

    fixup! Add option to not retain CAFs to the linker API

--- testsuite/tests/rts/T2615.hs
+++ testsuite/tests/rts/T2615.hs
@@ -3,7 +3,7 @@ import GHCi.ObjLink
 library_name = "libfoo_script_T2615.so" -- this is really a linker script
 
 main = do
-  initObjLinker
+  initObjLinker RetainCAFs
   result <- loadDLL library_name
   case result of
     Nothing -> putStrLn (library_name ++ " loaded successfully")
