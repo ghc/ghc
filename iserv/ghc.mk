fa8940e80138fed457e8903d59e2cd011741fa29
Karel Gardas <karel.gardas@centrum.cz>
2016-10-18 21:47:35 +0200

    fix build failure on Solaris caused by usage of --export-dynamic
    Summary:
    This patch fixes build failure on Solaris which is caused
    by usage --export-dynamic linker parameter. This parameter is not
    supported by Solaris linker. The param itself was added by D2590
    
    Reviewers: bgamari, simonmar, austin
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D2606

--- iserv/ghc.mk
+++ iserv/ghc.mk
@@ -29,9 +29,13 @@ iserv_stage2_dyn_MORE_HC_OPTS += -threaded
 # of overhead to startup and increases the binary sizes) but if you
 # need it there's no alternative.
 ifeq "$(TargetElf)" "YES"
+ifneq "$(TargetOS_CPP)" "solaris2"
+# The Solaris linker does not support --export-dynamic option. It also
+# does not need it since it exports all dynamic symbols by default
 iserv_stage2_MORE_HC_OPTS += -optl-Wl,--export-dynamic
 iserv_stage2_p_MORE_HC_OPTS += -optl-Wl,--export-dynamic
 iserv_stage2_dyn_MORE_HC_OPTS += -optl-Wl,--export-dynamic
+endif
 endif
 
 # Override the default way, because we want a specific version of this
