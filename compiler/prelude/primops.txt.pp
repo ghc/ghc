5ebcb3acaae63886d55f379d7e48437ac264a169
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-10-28 15:36:55 -0400

    Document unpackClosure# primop

--- compiler/prelude/primops.txt.pp
+++ compiler/prelude/primops.txt.pp
@@ -2631,6 +2631,11 @@ primop  NewBCOOp "newBCO#" GenPrimOp
 
 primop  UnpackClosureOp "unpackClosure#" GenPrimOp
    a -> (# Addr#, Array# b, ByteArray# #)
+   { {\tt unpackClosure\# closure} copies non-pointers and pointers in the
+     payload of the given closure into two new arrays, and returns a pointer to
+     the first word of the closure's info table, a pointer array for the
+     pointers in the payload, and a non-pointer array for the non-pointers in
+     the payload. }
    with
    out_of_line = True
 
