cb582b6476f64c1c86d89dce4d78462581fd2a6f
Shea Levy <shea@shealevy.com>
2016-12-13 14:53:50 -0500

    Don't have CPP macros expanding to 'defined'.
    Reviewers: austin, simonmar, erikd, bgamari
    
    Reviewed By: erikd, bgamari
    
    Subscribers: angerman, thomie
    
    Differential Revision: https://phabricator.haskell.org/D2823

--- includes/rts/storage/ClosureMacros.h
+++ includes/rts/storage/ClosureMacros.h
@@ -520,8 +520,17 @@ INLINE_HEADER StgWord8 *mutArrPtrsCard (StgMutArrPtrs *a, W_ n)
 
    -------------------------------------------------------------------------- */
 
-#define ZERO_SLOP_FOR_LDV_PROF     (defined(PROFILING))
-#define ZERO_SLOP_FOR_SANITY_CHECK (defined(DEBUG) && !defined(THREADED_RTS))
+#if defined(PROFILING)
+#define ZERO_SLOP_FOR_LDV_PROF 1
+#else
+#define ZERO_SLOP_FOR_LDV_PROF 0
+#endif
+
+#if defined(DEBUG) && !defined(THREADED_RTS)
+#define ZERO_SLOP_FOR_SANITY_CHECK 1
+#else
+#define ZERO_SLOP_FOR_SANITY_CHECK 0
+#endif
 
 #if ZERO_SLOP_FOR_LDV_PROF || ZERO_SLOP_FOR_SANITY_CHECK
 #define OVERWRITING_CLOSURE(c) overwritingClosure(c)
