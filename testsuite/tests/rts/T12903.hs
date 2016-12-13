aa123f445338c2980fcee87a09c01d14a83bf409
Alexander Vershilov <alexander.vershilov@gmail.com>
2016-12-13 14:54:36 -0500

    Fix testcase T12903 on OS X
    Old test used timeouts that leads to the various sporadic errors.
    Tet was rewritten to not use timeouts.
    
    Reviewers: austin, erikd, simonmar, bgamari
    
    Reviewed By: simonmar
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D2827
    
    GHC Trac Issues: #12956

--- testsuite/tests/rts/T12903.hs
+++ testsuite/tests/rts/T12903.hs
@@ -1,10 +1,21 @@
 import Control.Concurrent
 import Control.Exception
+import System.IO
 import System.Posix
+import System.Posix.IO
 
 main = do
+  (pout1, pin1) <- createPipe
+  (pout2, _) <- createPipe
   pid <- forkProcess $ do
-           handle (\UserInterrupt{} -> putStrLn "caught")
-                  $ threadDelay 2000000
+           hdl <- fdToHandle pin1
+           hSetBuffering hdl LineBuffering
+           handle (\UserInterrupt{} -> hPutStrLn hdl "caught")
+                  $ do hPutStrLn hdl "registered"
+                       hdl2 <- fdToHandle pout2
+                       putStrLn =<< hGetLine hdl2
+  hdl <- fdToHandle pout1
+  hSetBuffering hdl LineBuffering
+  "registered" <- hGetLine hdl
   signalProcess sigINT pid
-  threadDelay 2000000
+  putStrLn =<< hGetLine hdl
