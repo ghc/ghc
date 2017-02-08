015e97afd43a1b1762e1d6eef5d904c904e82965
Matthew Pickering <matthewtpickering@gmail.com>
2017-02-07 21:39:02 -0500

    Pass -v0 to ghc-pkg to reduce noise in build ouput
    By default ghc-pkg is quite noisy as it can't find interface files.
    
    Test Plan:
    Look at the build log stderr and see if there are lots of "cannot find
    any of" errors.
    
    Reviewers: austin, bgamari
    
    Reviewed By: bgamari
    
    Subscribers: thomie, snowleopard
    
    Differential Revision: https://phabricator.haskell.org/D3103

--- rules/build-package-data.mk
+++ rules/build-package-data.mk
@@ -129,7 +129,7 @@ endif
 endif
 	"$$(ghc-cabal_INPLACE)" configure $1 $2 "$$($1_$2_dll0_MODULES)" --with-ghc="$$($1_$2_HC_CONFIG)" --with-ghc-pkg="$$($1_$2_GHC_PKG)" $$($1_CONFIGURE_OPTS) $$($1_$2_CONFIGURE_OPTS)
 ifeq "$$($1_$2_PROG)" ""
-	$$(call cmd,$1_$2_GHC_PKG) update --force $$($1_$2_GHC_PKG_OPTS) $1/$2/inplace-pkg-config
+	$$(call cmd,$1_$2_GHC_PKG) update -v0 --force $$($1_$2_GHC_PKG_OPTS) $1/$2/inplace-pkg-config
 endif
 endif # NO_GENERATED_MAKEFILE_RULES
 endif # BINDIST
