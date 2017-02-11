203ecbd0adb3ad38132e51e72b77fc658a9c228f
Reid Barton <rwbarton@gmail.com>
2017-02-11 16:48:19 -0500

    Build stage1 compiler with -O2

--- mk/custom-settings.mk
+++ mk/custom-settings.mk
@@ -6,6 +6,7 @@ include mk/flavours/validate.mk
 -include mk/validate.mk
 else
 -include $(firstword $(wildcard mk/$(TargetPlatformFull)-build.mk) mk/build.mk)
+GhcStage1HcOpts = -O2
 endif
 
 ifeq "$(BINDIST)" "YES"
