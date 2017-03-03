10d28d0ababe52a705c10d450869d7a61063c4ae
Ben Gamari <bgamari.foss@gmail.com>
2017-03-03 15:49:14 -0500

    testsuite: Add test for floating-point abs (numrun015)
    Test Plan: Validate
    
    Reviewers: idontgetoutmuch, austin
    
    Subscribers: thomie
    
    Differential Revision: https://phabricator.haskell.org/D3273

---
+++ testsuite/tests/numeric/should_run/numrun015.hs
@@ -0,0 +1,20 @@
+-- Test that floating-point abs works correctly
+
+absF :: Float -> Float
+absF = abs
+
+absD :: Double -> Double
+absD = abs
+
+main :: IO ()
+main = do
+    print $ absF (1 / 0)
+    print $ absD (1 / 0)
+    print $ absF 1
+    print $ absD 1
+    print $ absF (-1)
+    print $ absD (-1)
+    print $ absF (-1 / 0)
+    print $ absD (-1 / 0)
+    print $ absF (1 / 0)
+    print $ absD (1 / 0)
