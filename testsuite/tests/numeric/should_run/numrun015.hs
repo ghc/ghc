0fd8340517a7a582bc5adb15771d82a69e3e4348
Ben Gamari <ben@smart-cactus.org>
2017-03-03 17:45:04 -0500

    testsuite: Fix double test of +Infinity
    The second set was supposed to be a NaN.

--- testsuite/tests/numeric/should_run/numrun015.hs
+++ testsuite/tests/numeric/should_run/numrun015.hs
@@ -16,6 +16,6 @@ main = do
     print $ absD (-1)
     print $ absF (-1 / 0)
     print $ absD (-1 / 0)
-    print $ absF (1 / 0)
-    print $ absD (1 / 0)
+    print $ absF (0 / 0)
+    print $ absD (0 / 0)
     print $ absD $ sqrt (-1)
