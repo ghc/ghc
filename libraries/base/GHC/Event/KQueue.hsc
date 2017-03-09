ghc/ghc

commit ecb880caea441b6dd7f75a555546e55abe11c233

base: kevent.filter is signed
I checked both the FreeBSD and Darwin manpages; it's signed in both cases.
This was producing validation failures on OS X due to the new literal
range-check.

libraries/base/GHC/Event/KQueue.hsc
@@ -186,10 +186,10 @@ newtype Flag = Flag Word16
  , flagOneshot = EV_ONESHOT
  }
 
-#if SIZEOF_KEV_FILTER == 4 /*kevent.filter: uint32_t or uint16_t. */
-newtype Filter = Filter Word32
+#if SIZEOF_KEV_FILTER == 4 /*kevent.filter: int32_t or int16_t. */
+newtype Filter = Filter Int32
 #else
-newtype Filter = Filter Word16
+newtype Filter = Filter Int16
 #endif
     deriving (Bits, FiniteBits, Eq, Num, Show, Storable)
 

commit de62f587463f6377df1e69e11504578833dfe653

base: Import Data.Int in KQueue
This was broken in the ecb880caea441b6dd7f75a555546e55abe11c233.

libraries/base/GHC/Event/KQueue.hsc
@@ -28,6 +28,7 @@ available = False
 
 import Data.Bits (Bits(..), FiniteBits(..))
 import Data.Word (Word16, Word32)
+import Data.Int  (Int16)
 import Foreign.C.Error (throwErrnoIfMinus1, eINTR, eINVAL,
                         eNOTSUP, getErrno, throwErrno)
 import Foreign.C.Types