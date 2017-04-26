b888972c5de36295d6be78743c24b2875a1a6d0d
Alex Biehl <alexbiehl@gmail.com>
2017-04-26 07:49:10 +0200

    Cherry-picked remaining commits from haddock-2.17.4-release (#603)
    * Release haddock/haddock-api 2.17.4 and haddock-library 1.4.3
    
    * Set version bounds for haddock-library
    
    NB: This allows GHC 8.2.1's base
    
    * Set version bounds for haddock & haddock-api
    
    The version bounds support GHC 8.2
    
    * Merge (temporary) v2.17.3 branch into v2.17
    
    This allows us to delete the v2.17.3 branch
    
    * Fixup changelog
    
    * Pin down haddock-api to a single version
    
    as otherwise `haddock`'s package version has no proper meaning
    
    * fix source-repo spec for haddock-api

--- haddock-api/src/Haddock/Types.hs
+++ haddock-api/src/Haddock/Types.hs
@@ -460,8 +460,8 @@ instance (NFData a, NFData mod)
     DocExamples a             -> a `deepseq` ()
     DocHeader a               -> a `deepseq` ()
 
-#if __GLASGOW_HASKELL__ < 801
--- These were added to GHC itself in 8.2.1
+#if !MIN_VERSION_ghc(8,0,2)
+-- These were added to GHC itself in 8.0.2
 instance NFData Name where rnf x = seq x ()
 instance NFData OccName where rnf x = seq x ()
 instance NFData ModuleName where rnf x = seq x ()
