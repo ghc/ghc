614117eb0028ba1fded9fdd40ae7f10fae9a8f6b
Jared Weakly <jweakly@pdx.edu>
2017-07-20 17:30:21 -0700

    Added initial metric comparison tooling

--- testsuite/driver/runtests.py
+++ testsuite/driver/runtests.py
@@ -329,14 +329,11 @@ else:
 
     summary(t, sys.stdout, config.no_print_summary)
 
-    # This here is loading up all of the git notes into memory.
-    # It's most likely in the wrong spot and I haven't fully fleshed out
-    # where exactly I'm putting this and how I'm refactoring the performance
-    # test running logic.
-    # Currently this is useful for debugging, at least.
+    # Write our accumulated metrics into the git notes for this commit.
     if config.use_git_notes:
             note = subprocess.check_output(["git","notes","--ref=perf","append","-m", "\n".join(config.accumulate_metrics)])
-            parse_git_notes('perf') # Should this be hardcoded? Most likely not...
+            # v-- This is in a nonsensical area. It should be happening before all of the tests are even run.
+            # parse_git_notes('perf') # Should it even be happening in the test-driver logic anymore?
 
     # This here is loading up all of the git notes into memory.
     # It's most likely in the wrong spot and I haven't fully fleshed out
