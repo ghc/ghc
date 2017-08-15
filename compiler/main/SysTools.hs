21bd9b2f3f48af453348f40a740ee8f4c26fdace
Gleb Popov <6yearold@gmail.com>
2017-08-14 20:56:44 -0400

    Recognize FreeBSD compiler as Clang.
    Reviewers: austin, bgamari
    
    Reviewed By: bgamari
    
    Subscribers: rwbarton, thomie
    
    Differential Revision: https://phabricator.haskell.org/D3840

--- compiler/main/SysTools.hs
+++ compiler/main/SysTools.hs
@@ -868,6 +868,9 @@ getCompilerInfo' dflags = do
         -- Regular clang
         | any ("clang version" `isInfixOf`) stde =
           return Clang
+        -- FreeBSD clang
+        | any ("FreeBSD clang version" `isInfixOf`) stde =
+          return Clang
         -- XCode 5.1 clang
         | any ("Apple LLVM version 5.1" `isPrefixOf`) stde =
           return AppleClang51
