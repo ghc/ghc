0a891c8c448a0e70abb075702857d743674ad1ad
Gleb Popov <6yearold@gmail.com>
2017-08-14 20:54:36 -0400

    Properly handle dlerror() message on FreeBSD when linking linker scripts
    Test Plan: `GHCi.loadDll "/usr/lib/libc++.so` now works on FreeBSD.
    
    Reviewers: austin, bgamari, erikd, simonmar
    
    Reviewed By: simonmar
    
    Subscribers: rwbarton, thomie
    
    Differential Revision: https://phabricator.haskell.org/D3841

--- rts/Linker.c
+++ rts/Linker.c
@@ -482,7 +482,7 @@ initLinker_ (int retain_cafs)
 #   endif /* RTLD_DEFAULT */
 
     compileResult = regcomp(&re_invalid,
-           "(([^ \t()])+\\.so([^ \t:()])*):([ \t])*(invalid ELF header|file too short)",
+           "(([^ \t()])+\\.so([^ \t:()])*):([ \t])*(invalid ELF header|file too short|invalid file format)",
            REG_EXTENDED);
     if (compileResult != 0) {
         barf("Compiling re_invalid failed");
