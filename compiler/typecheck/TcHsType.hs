1b55c6ac6aba7cd8014111dec6271eae1b89773c
Mark Barbone <mark.l.barbone@gmail.com>
2017-09-05 19:53:05 -0400

    Added checks disallowing linear arrows in kinds

--- compiler/typecheck/TcHsType.hs
+++ compiler/typecheck/TcHsType.hs
@@ -548,8 +548,11 @@ tc_hs_type _ ty@(HsSpliceTy {}) _exp_kind
   = failWithTc (text "Unexpected type splice:" <+> ppr ty)
 
 ---------- Functions and applications
-tc_hs_type mode (HsFunTy ty1 weight ty2) exp_kind
-  = tc_fun_type mode weight ty1 ty2 exp_kind
+tc_hs_type mode ty@(HsFunTy ty1 weight ty2) exp_kind
+  | mode_level mode == KindLevel && not (weight == Omega)
+    = failWithTc (text "Linear arrows disallowed in kinds:" <+> ppr ty)
+  | otherwise
+    = tc_fun_type mode weight ty1 ty2 exp_kind
 
 tc_hs_type mode (HsOpTy ty1 (L _ op) ty2) exp_kind
   | op `hasKey` funTyConKey
