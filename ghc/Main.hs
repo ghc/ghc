53efa284f6bb5b5835d76faeb54c9ae74b267296
Ben Gamari <ben@smart-cactus.org>
2017-09-12 06:45:44 -0400

    Various cleanups

--- ghc/Main.hs
+++ ghc/Main.hs
@@ -73,8 +73,6 @@ import Control.Monad
 import Data.Char
 import Data.List
 import Data.Maybe
-import qualified Data.Text as DontUseText
-import qualified Data.Text.Prettyprint.Doc as DontUsePrettyPrint
 
 -----------------------------------------------------------------------------
 -- ToDo:
