5a8b8434bd79e5597a66a2779012082e369b340b
Herbert Valerio Riedel <hvr@gnu.org>
2017-09-22 10:35:02 +0200

    Remove 'stm' from EXTRA_PACKAGES set
    This effectively broke `make sdist`; the surprising thing is that
    ./validate didn't catch this (and thus the buildbots didn't either).
    
    Also, I would have expected `EXTRA_PACKAGES` to be populated by the
    data in `./packages` which already encodes that information...
    
    This is a follow-up to 02ff70563e490d2a7f3141eab7229803c523da57

--- ghc.mk
+++ ghc.mk
@@ -1235,7 +1235,7 @@ GIT_COMMIT_ID:
 sdist-ghc-prep-tree : VERSION GIT_COMMIT_ID
 
 # Extra packages which shouldn't be in the source distribution: see #8801
-EXTRA_PACKAGES=parallel stm random primitive vector dph
+EXTRA_PACKAGES=parallel random primitive vector dph
 
 .PHONY: sdist-ghc-prep-tree
 sdist-ghc-prep-tree :
