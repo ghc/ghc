8c23b54f0e51b7fb028e46a52d4329235d2013ca
Ben Gamari <bgamari.foss@gmail.com>
2017-09-26 14:32:02 -0400

    Rules: Show the binder type in the unbound template binder error
    Reviewers: austin
    
    Subscribers: rwbarton, thomie
    
    Differential Revision: https://phabricator.haskell.org/D4013

--- compiler/specialise/Rules.hs
+++ compiler/specialise/Rules.hs
@@ -583,7 +583,7 @@ matchN (in_scope, id_unf) rule_name tmpl_vars tmpl_es target_es
             = env
 
     unbound var = pprPanic "Template variable unbound in rewrite rule" $
-                  vcat [ text "Variable:" <+> ppr var
+                  vcat [ text "Variable:" <+> ppr var <+> dcolon <+> ppr (varType var)
                        , text "Rule" <+> pprRuleName rule_name
                        , text "Rule bndrs:" <+> ppr tmpl_vars
                        , text "LHS args:" <+> ppr tmpl_es
