d86ab8df2f720d1c311ba0aaf438b550e109c76a
Ben Gamari <ben@smart-cactus.org>
2017-10-24 12:19:08 -0400

    base: Make Foreign.Marshal.Alloc.allocBytes[Aligned] NOINLINE
    As noted in #14346, touch# may be optimized away when the simplifier can see
    that the continuation passed to allocaBytes will not return. Marking CPS-style
    functions with NOINLINE ensures that the simplier can't draw any unsound
    conclusions.
    
    Ultimately the right solution here will be to do away with touch# and instead
    introduce a scoped primitive as is suggested in #14375.
    
    Note: This was present in 8.2 but was never merged to 8.4 in hopes that
    we would have #14375 implemented in time. This meant that the issue
    regressed again in 8.4. Thankfully we caught it in time to fix it for
    8.6.
    
    (cherry picked from commit 404bf05ed3193e918875cd2f6c95ae0da5989be2)
    (cherry picked from commit 56590db07a776ce81eb89d4a4d86bd0f953fb44e)

--- libraries/base/Foreign/Marshal/Alloc.hs
+++ libraries/base/Foreign/Marshal/Alloc.hs
@@ -136,6 +136,19 @@ alloca  = doAlloca undefined
 -- away with touch# in favor of a primitive that will capture the scoping left
 -- implicit in the case of touch#.
 
+-- Note [NOINLINE for touch#]
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~
+-- Both allocaBytes and allocaBytesAligned use the touch#, which is notoriously
+-- fragile in the presence of simplification (see #14346). In particular, the
+-- simplifier may drop the continuation containing the touch# if it can prove
+-- that the action passed to allocaBytes will not return. The hack introduced to
+-- fix this for 8.2.2 is to mark allocaBytes as NOINLINE, ensuring that the
+-- simplifier can't see the divergence.
+--
+-- These can be removed once #14375 is fixed, which suggests that we instead do
+-- away with touch# in favor of a primitive that will capture the scoping left
+-- implicit in the case of touch#.
+
 -- |@'allocaBytes' n f@ executes the computation @f@, passing as argument
 -- a pointer to a temporarily allocated block of memory of @n@ bytes.
 -- The block of memory is sufficiently aligned for any of the basic
