ed18f47f931361a9adbb109085c6feb432ec41aa
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2017-11-07 10:52:38 +0000

    Factor out builds into steps. Address ghc/ghc#83 comments.
    This should greatly improve log output.

--- .circleci/prepare-system.sh
+++ .circleci/prepare-system.sh
@@ -7,6 +7,18 @@ fail() {
   exit 1
 }
 
+echo 'BUILD_SPHINX_HTML = NO' > mk/validate.mk
+echo 'BUILD_SPHINX_PDF = NO' >> mk/validate.mk
+
+cat > mk/build.mk <<EOF
+V=1
+HADDOCK_DOCS=YES
+LATEX_DOCS=YES
+HSCOLOUR_SRCS=YES
+BUILD_DOCBOOK_HTML=YES
+BeConservative=YES
+EOF
+
 case "$(uname)" in
   Linux)
     if [[ -n ${TARGET:-} ]]; then
@@ -14,17 +26,23 @@ case "$(uname)" in
         # cross-compiling to FreeBSD
         add-apt-repository -y ppa:hvr/ghc
         apt-get update -qq
-        apt-get install -qy ghc-8.0.2 cabal-install  alex happy ncurses-dev git openssh-client make automake autoconf gcc perl python3 texinfo xz-utils
+        apt-get install -qy ghc-8.0.2 cabal-install-1.24 alex happy \
+                            ncurses-dev git make automake autoconf gcc perl \
+                            python3 texinfo xz-utils
         cabal update
         cabal install --reinstall hscolour
         ln -s $HOME/.cabal/bin/HsColour /usr/local/bin/HsColour
+
+        echo 'HADDOCK_DOCS = NO' >> mk/build.mk
+        echo 'WERROR=' >> mk/build.mk
+        # https://circleci.com/docs/2.0/env-vars/#interpolating-environment-variables-to-set-other-environment-variables
+        echo 'export PATH=/opt/ghc/bin:$PATH' >> $BASH_ENV
       else
         fail "TARGET=$target not supported"
       fi
     else
       # assuming Ubuntu
-      apt-get update -qq
-      apt-get install -qy git openssh-client make automake autoconf gcc perl python3 texinfo xz-utils
+      apt-get install -qy git make automake autoconf gcc perl python3 texinfo xz-utils
       cabal update
       cabal install --reinstall hscolour
     fi
