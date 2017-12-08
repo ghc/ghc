7733e44dd4ba7e7a0a9f3456e6ddc32decbcf5a3
Ben Gamari <ben@smart-cactus.org>
2017-12-08 12:42:35 -0500

    Rip out hadrian subtree
    Sadly subtrees haven't worked quite as well as we would have liked for
    developers. See Hadrian #440.

--- hadrian/src/Settings/Packages/Ghci.hs
+++
@@ -1,6 +0,0 @@
-module Settings.Packages.Ghci (ghciPackageArgs) where
-
-import Expression
-
-ghciPackageArgs :: Args
-ghciPackageArgs = package ghci ? notStage0 ? builder GhcCabal ? arg "--flags=ghci"
--- hadrian/src/Settings/Packages/Haskeline.hs
+++
@@ -1,8 +0,0 @@
-module Settings.Packages.Haskeline (haskelinePackageArgs) where
-
-import Expression
-import Oracles.Flag (crossCompiling)
-
-haskelinePackageArgs :: Args
-haskelinePackageArgs =
-    package haskeline ? builder GhcCabal ? crossCompiling ? arg "-f-terminfo"
