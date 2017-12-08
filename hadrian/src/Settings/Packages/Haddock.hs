351c460c6df24d8c0a490eea7e19867d2f1784fd
Ben Gamari <ben@smart-cactus.org>
2017-12-08 12:42:35 -0500

    Rip out hadrian subtree
    Sadly subtrees haven't worked quite as well as we would have liked for
    developers. See Hadrian #440.

--- hadrian/src/Settings/Packages/Haddock.hs
+++
@@ -1,7 +0,0 @@
-module Settings.Packages.Haddock (haddockPackageArgs) where
-
-import Expression
-
-haddockPackageArgs :: Args
-haddockPackageArgs = package haddock ?
-    builder GhcCabal ? pure ["--flag", "in-ghc-tree"]
