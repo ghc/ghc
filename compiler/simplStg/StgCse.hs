ae2f1782d16c89acd8ebb711d923f5ece6d65a1f
Gabor Greif <ggreif@gmail.com>
2017-12-27 20:56:51 +0100

    this is not needed any more
    should fix Travis

--- compiler/simplStg/StgCse.hs
+++ compiler/simplStg/StgCse.hs
@@ -350,7 +350,6 @@ stgCseExpr env (StgConApp dataCon args tys)
     = StgConApp dataCon args' tys
   where args' = substArgs env args
         dc = Lax dataCon
-        u = getUnique (getName dc)
 
 -- Let bindings
 -- The binding might be removed due to CSE (we do not want trivial bindings on
