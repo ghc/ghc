585b34dcee92a30e2bc81632f9ab9d190abdaa31
Gabor Greif <ggreif@gmail.com>
2018-01-02 19:50:47 +0100

    WIP: check isEvaldUnfolding
    unfortunately it does not cover the "wild_*" variables :-(

--- compiler/GHC/StgToCmm/Closure.hs
+++ compiler/GHC/StgToCmm/Closure.hs
@@ -67,6 +67,7 @@ module GHC.StgToCmm.Closure (
 import GhcPrelude
 
 import StgSyn
+import CoreSyn (isEvaldUnfolding)
 import SMRep
 import Cmm
 import PprCmmExpr() -- For Outputable instances
@@ -617,6 +618,10 @@ getCallMethod dflags name id (LFThunk _ _ updatable std_form_info is_fun)
 
 getCallMethod _ _name _ (LFUnknown True) _n_arg _v_args _cg_locs _self_loop_info
   = SlowCall -- might be a function
+
+getCallMethod _ _name id (LFUnknown False) 0 _v_args _cg_loc _self_loop_info
+  | isEvaldUnfolding (idUnfolding id)
+  = pprTrace "getCallMethod" (ppr id) ReturnIt -- seems to come from case, must be (tagged) WHNF already
 
 getCallMethod _ name _ (LFUnknown False) 0 _v_args _cg_loc _self_loop_info
   | occNameString (nameOccName name) == "wild" -- TODO: make this robust
