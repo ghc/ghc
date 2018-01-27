d77c2214db9a9167db34fdfb42ef7cd6b5f59c85
Gabor Greif <ggreif@gmail.com>
2018-01-27 20:16:52 +0100

    WIP: exclude casts for now

--- compiler/codeGen/StgCmmClosure.hs
+++ compiler/codeGen/StgCmmClosure.hs
@@ -68,7 +68,7 @@ module StgCmmClosure (
 
 import GhcPrelude
 
-import CoreSyn( isValueUnfolding, maybeUnfoldingTemplate )
+import CoreSyn( isValueUnfolding, maybeUnfoldingTemplate, Expr(Cast) )
 import CoreOpt( exprIsSatConApp_maybe )
 import StgSyn
 import SMRep
@@ -332,6 +332,9 @@ mkLFImported id
   | isValueUnfolding unf
   , Just expr <- maybeUnfoldingTemplate unf
   , Just con <- exprIsSatConApp_maybe expr
+  , let casted (Cast _ _) = pprTrace "mkLFImported" (ppr unf <+> ppr expr <+> ppr con) True
+        casted _ = False
+  , not $ casted expr
   = LFCon con
 
   | arity > 0
