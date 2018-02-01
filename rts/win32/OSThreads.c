382c12d18f3d64e01502a5c8bbc64d4aa4842e3e
Ben Gamari <bgamari.foss@gmail.com>
2018-01-31 21:37:21 -0500

    rts: Ensure that forkOS releases Task on termination
    Test Plan: validate
    
    Reviewers: simonmar, erikd
    
    Reviewed By: simonmar
    
    Subscribers: rwbarton, thomie, carter
    
    GHC Trac Issues: #14725
    
    Differential Revision: https://phabricator.haskell.org/D4346

--- rts/win32/OSThreads.c
+++ rts/win32/OSThreads.c
@@ -236,6 +236,7 @@ forkOS_createThreadWrapper ( void * entry )
     cap = rts_lock();
     rts_evalStableIO(&cap, (HsStablePtr) entry, NULL);
     rts_unlock(cap);
+    rts_done();
     return 0;
 }
 
