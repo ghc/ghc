37bd95fee83b50c2567914a6e6c2a4cc73803f30
Matthew Pickering <matthewtpickering@gmail.com>
2018-02-28 18:09:34 +0000

    Pin nixpkgs and all-cabal-hashes in shell.nix (#511)
    * Pin shell.nix
    
    Also adds a new option to build `cabal-install` HEAD which is
    sometimes useful.
    
    * Bump QuickCheck bound
    
    * Remove cabal-install which snuck in

--- shell.nix
+++ shell.nix
@@ -2,9 +2,26 @@
 # by only invoking hadrian.
 
 
-{ nixpkgs ? import <nixpkgs> {} }:
+{ _nixpkgs ? import <nixpkgs> {} }:
 
 let
+
+  nixpkgs = import (_nixpkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "e7a327da5cffdf5e77e1924906a4f0983591bd3e";
+    sha256 = "1xzil4mayhggg2miwspbk12nihlszg0y4n6i4qacrxql5n75f0hr";
+  }){ overlays = [cabalHashes]; };
+
+
+
+  cabalHashes = sel: super: {
+    all-cabal-hashes = super.fetchurl {
+      url    = "https://github.com/commercialhaskell/all-cabal-hashes/archive/b2b93ae610f5f1b51d22b191f972dc3dec8f94c6.tar.gz";
+      sha256 = "0bffclpqbw62xff36qlzxghr042mhv0m06k5ml4298w6fv7ly1xw";
+    };
+  };
+
   haskellPackages = nixpkgs.haskell.packages.ghc822;
 
   removeBuild = path: type:
@@ -28,7 +45,10 @@ let
         localPackage = name: path: self.callCabal2nix name (filterSrc path) {};
       in {
         hadrian = localPackage "hadrian" ./. ;
-        shake = self.callHackage "shake" "0.16" {};
+        happy = nixpkgs.haskell.lib.dontCheck (super.happy);
+        shake = self.callHackage "shake" "0.16.2" {};
+        extra = self.callHackage "extra" "1.6.4" {};
+        QuickCheck = self.callHackage "QuickCheck" "2.10" {};
         Cabal = localPackage "Cabal" ./../libraries/Cabal/Cabal ;
         filepath = localPackage "filepath" ./../libraries/filepath ;
         text = localPackage "text" ./../libraries/text  ;
@@ -39,14 +59,28 @@ let
         directory = localPackage "directory" ./../libraries/directory ;
       }; };
 
+  cabalPackages = nixpkgs.haskell.packages.ghc822.override {
+    overrides = self: super: let
+        localPackage = name: path: self.callCabal2nix name (filterSrc path) {};
+      in {
+        Cabal = localPackage "Cabal" ./../../cabal/Cabal ;
+        cabal-install = self.callPackage ./../../cabal/cabal-install.nix {};
+      }; };
+
+
 in
   nixpkgs.lib.overrideDerivation nixpkgs.haskell.packages.ghcHEAD.ghc
     (drv: {
       name = "ghc-dev";
-      nativeBuildInputs = drv.nativeBuildInputs ++ 
+      nativeBuildInputs = drv.nativeBuildInputs ++
         [ hadrianPackages.hadrian
           nixpkgs.arcanist
           nixpkgs.python3Packages.sphinx
+          nixpkgs.texlive.combined.scheme-basic
+          (nixpkgs.haskell.packages.ghc822.ghcWithPackages
+            (ps: [ps.html ps.regex-compat ps.dump-core]))
+
+          #cabalPackages.cabal-install
         ];
     })
 
