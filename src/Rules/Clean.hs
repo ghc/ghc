47678ea67682ce49b37d5b3109a5b42030c09fc3
Tamar Christina <Mistuke@users.noreply.github.com>
2018-03-20 08:51:53 +0000

    Add clean routines for fs (#533)

--- src/Rules/Clean.hs
+++ src/Rules/Clean.hs
@@ -18,6 +18,19 @@ cleanSourceTree = do
     removeDirectory inplaceBinPath
     removeDirectory inplaceLibPath
     removeDirectory "sdistprep"
+    cleanFsUtils
+
+-- Clean all temporary fs files copied by configure into the source folder
+cleanFsUtils :: Action ()
+cleanFsUtils = do
+    let dirs = [ "utils/lndir/"
+               , "utils/unlit/"
+               , "rts/"
+               , "libraries/base/include/"
+               , "libraries/base/cbits/"
+               ]
+    liftIO $ forM_ dirs (flip removeFiles ["fs.*"])
+
 
 cleanRules :: Rules ()
 cleanRules = "clean" ~> clean
