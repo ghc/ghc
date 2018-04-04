8a968b561e74a1ea99cd8df92e4b0a6684798917
Matthew Pickering <matthew.pickering@tweag.io>
2018-04-04 10:31:38 +0000

    Add CSE test
    This program causes a core lint error after the CSE pass runs because
    the two definitions are CSEd but the types are different.

---
+++ linear-tests/CSETest.hs
@@ -0,0 +1,13 @@
+{- This test makes sure that if two expressions with conflicting types are
+   CSEd then appropiate things happen. -}
+module CSETest where
+
+minimal :: a ⊸ a
+minimal x = x
+
+maximal :: a -> a
+maximal x = x
+
+
+
+
