81e7980a015795209f8edbf298761df9c749a4b3
Edward Z. Yang <ezyang@fb.com>
2018-04-07 13:32:31 -0400

    Minor typofix in LoadArchive.c
    Test Plan: none
    
    Reviewers: bgamari, erikd, simonmar
    
    Reviewed By: bgamari
    
    Subscribers: thomie, carter
    
    Differential Revision: https://phabricator.haskell.org/D4551

--- rts/linker/LoadArchive.c
+++ rts/linker/LoadArchive.c
@@ -261,7 +261,7 @@ static HsInt loadArchive_ (pathchar *path)
     int misalignment = 0;
 
     DEBUG_LOG("start\n");
-    DEBUG_LOG("Loading archive `%" PATH_FMT" '\n", path);
+    DEBUG_LOG("Loading archive `%" PATH_FMT "'\n", path);
 
     /* Check that we haven't already loaded this archive.
        Ignore requests to load multiple times */
