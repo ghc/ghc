5bd2b34e6903aeef09162c61e107a775a2e0968a
Matthew Pickering <matthew.pickering@tweag.io>
2018-04-13 13:49:24 +0000

    Also include error message in file

--- linear-tests/Op.hs
+++ linear-tests/Op.hs
@@ -16,3 +16,21 @@ instance Or (->) where
 
 
 foo = or Just (\x -> Just x)
+
+{-
+[1 of 1] Compiling Dollar           ( linear-tests/Op.hs, linear-tests/Op.o )
+
+linear-tests/Op.hs:18:16: error:
+    • Couldn't match expected type ‘a ⊸ Maybe a’
+                  with actual type ‘a0 -> Maybe a0’
+    • The lambda expression ‘\ x -> Just x’ has one argument,
+      its type is ‘p0 a b0’,
+      it is specialized to ‘a ⊸ Maybe a’
+      In the second argument of ‘or’, namely ‘(\ x -> Just x)’
+      In the expression: or Just (\ x -> Just x)
+    • Relevant bindings include
+        foo :: a ⊸ Maybe a (bound at linear-tests/Op.hs:18:1)
+   |
+18 | foo = or Just (\x -> Just x)
+   |                ^^^^^^^^^^^^
+-}
