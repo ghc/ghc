091f7d022fec21149e027f731a94fe98e1d2f03b
Matthew Pickering <matthew.pickering@tweag.io>
2018-04-13 11:11:41 +0000

    Reduce RankN test further

--- linear-tests/RankN.hs
+++ linear-tests/RankN.hs
@@ -3,22 +3,17 @@
 module RankN where
 {-
 inplace/bin/ghc-stage1 -O2 -dcore-lint
-
-$ is not given a linear polymorphic type and so core lint complains
 -}
 
 import GHC.Base
 
 class Data a where
-  gunfold :: (forall b r. Data b => c (b -> r) -> c r)
+  gunfold :: (forall b r.  c (b -> r) -> c r)
           -> (forall r. r -> c r)
           -> c a
 
 foo = 1
 {-# NOINLINE foo #-}
 
-instance Data a => Data [a] where
-  gunfold k z = case foo of
-                    1 -> z []
-                    2 -> k (k (z (:)))
-                    _ -> errorWithoutStackTrace "Data.Data.gunfold(List)"
+instance Data [a] where
+  gunfold k z = k (k (z (:)))
