1e8c8cdf405d689dc5ec2f42b5d6af843999400f
Matthew Pickering <matthew.pickering@tweag.io>
2018-04-13 11:40:23 +0000

    Simplify RankN example some more

--- linear-tests/RankN.hs
+++ linear-tests/RankN.hs
@@ -6,7 +6,7 @@ inplace/bin/ghc-stage1 -O2 -dcore-lint
 -}
 
 import GHC.Base
-
+{-
 class Data a where
   gunfold :: (forall b r.  c (b -> r) -> c r)
           -> (forall r. r -> c r)
@@ -17,3 +17,22 @@ foo = 1
 
 instance Data [a] where
   gunfold k z = k (k (z (:)))
+-}
+
+data Identity a = Identity a
+
+qux :: Identity (Int ⊸ Int)
+qux = Identity (\x -> x)
+
+app :: Identity (a -> b) -> Identity a -> Identity b
+app (Identity f) (Identity a) = Identity (f a)
+
+example = app qux (Identity 5)
+
+unqux :: Int ⊸ Int
+unqux = \x -> x
+
+unapp :: (a -> b) -> a -> b
+unapp f a = f a
+
+example1 = unapp unqux 5
