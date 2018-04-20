85ba5bf862f69fbd32f56675256da06158392975
Andrew Martin <andrew.thaddeus@gmail.com>
2018-04-20 13:36:23 -0400

    improve the documentation of indexArray# even more

--- compiler/prelude/primops.txt.pp
+++ compiler/prelude/primops.txt.pp
@@ -818,14 +818,13 @@ primop  SizeofMutableArrayOp "sizeofMutableArray#" GenPrimOp
 
 primop  IndexArrayOp "indexArray#" GenPrimOp
    Array# a -> Int# -> (# a #)
-   {Read from specified index of immutable array. Result is packaged into
-    an unboxed unary tuple; the result itself is not yet evaluated. Pattern
-    matching on the tuple forces the indexing of the array
-    to happen but does not evaluate the element itself. Doing this
-    pattern match immidiately can be useful for (1) preventing
-    additional thunks from building up on the heap and (2) eliminating
-    references to the argument array, allowing it to be garbage
-    collected more promptly.}
+   {Read from specified index of immutable array. Result is packaged
+    into an unboxed unary tuple; the result itself is not yet
+    evaluated. Pattern matching on the tuple forces the indexing of the
+    array to happen but does not evaluate the element itself. Evaluating
+    the thunk prevents additional thunks from building up on the
+    heap. Avoiding these thunks, in turn, reduces references to the
+    argument array, allowing it to be garbage collected more promptly.}
    with
    can_fail         = True
 
