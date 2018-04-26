bf50ef6f5f4cd745412e17261fc61a509eb24147
Matthew Pickering <matthew.pickering@tweag.io>
2018-04-26 12:01:40 +0000

    Remove linear variable printing from lambda

--- compiler/coreSyn/PprCore.hs
+++ compiler/coreSyn/PprCore.hs
@@ -395,7 +395,9 @@ pprTypedLamBinder bind_site debug_on var
       | isTyVar var  -> parens (pprKindedTyVarBndr var)
 
       | otherwise    -> parens (hang (pprIdBndr var)
-                                   2 (vcat [ dcolon <> brackets (ppr (idWeight var)) <+> pprType (idType var)
+                                   2 (vcat [  dcolon
+                                                -- <> brackets (ppr (idWeight var))
+                                                <+> pprType (idType var)
                                            , pp_unf]))
   where
     suppress_sigs = gopt Opt_SuppressTypeSignatures
