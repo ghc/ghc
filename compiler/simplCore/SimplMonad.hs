b3c4cca9bcb2164f5c624d750a249afa30bb6c30
Matthew Pickering <matthew.pickering@tweag.io>
2018-05-17 11:59:55 +0000

    Scale lets as they pass through case contexts
    See #78

--- compiler/simplCore/SimplMonad.hs
+++ compiler/simplCore/SimplMonad.hs
@@ -16,14 +16,17 @@ module SimplMonad (
         -- Counting
         SimplCount, tick, freeTick, checkedTick,
         getSimplCount, zeroSimplCount, pprSimplCount,
-        plusSimplCount, isZeroSimplCount
+        plusSimplCount, isZeroSimplCount,
+
+        -- Scaling
+        reportScaling, readScaling
     ) where
 
 import GhcPrelude
 
 import Var              ( Var, isTyVar, mkLocalVar )
 import Name             ( mkSystemVarName )
-import Id               ( Id, mkSysLocalOrCoVar )
+import Id               ( Id, mkSysLocalOrCoVar, idWeight )
 import IdInfo           ( IdDetails(..), vanillaIdInfo, setArityInfo )
 import Type             ( Type, mkLamTypes, Rig(..))
 import FamInstEnv       ( FamInstEnv )
@@ -55,7 +58,7 @@ newtype SimplM result
                 -> UniqSupply   -- We thread the unique supply because
                                 -- constantly splitting it is rather expensive
                 -> SimplCount
-                -> IO (result, UniqSupply, SimplCount)}
+                -> IO (result, [Id], UniqSupply, SimplCount)}
   -- we only need IO here for dump output
 
 data SimplTopEnv
@@ -72,7 +75,7 @@ initSmpl :: DynFlags -> RuleEnv -> (FamInstEnv, FamInstEnv)
          -> IO (a, SimplCount)
 
 initSmpl dflags rules fam_envs us size m
-  = do (result, _, count) <- unSM m env us (zeroSimplCount dflags)
+  = do (result, _, _, count) <- unSM m env us (zeroSimplCount dflags)
        return (result, count)
   where
     env = STE { st_flags = dflags, st_rules = rules
@@ -117,20 +120,23 @@ instance Monad SimplM where
    (>>=)  = thenSmpl
 
 returnSmpl :: a -> SimplM a
-returnSmpl e = SM (\_st_env us sc -> return (e, us, sc))
+returnSmpl e = SM (\_st_env us sc -> return (e, [], us, sc))
 
 thenSmpl  :: SimplM a -> (a -> SimplM b) -> SimplM b
 thenSmpl_ :: SimplM a -> SimplM b -> SimplM b
 
 thenSmpl m k
   = SM $ \st_env us0 sc0 -> do
-      (m_result, us1, sc1) <- unSM m st_env us0 sc0
-      unSM (k m_result) st_env us1 sc1
+      (m_result, w1, us1, sc1) <- unSM m st_env us0 sc0
+      (r, w2, us2, sc2) <- unSM (k m_result) st_env us1 sc1
+      return (r, w1 ++ w2, us2, sc2)
+
 
 thenSmpl_ m k
   = SM $ \st_env us0 sc0 -> do
-      (_, us1, sc1) <- unSM m st_env us0 sc0
-      unSM k st_env us1 sc1
+      (_, w1, us1, sc1) <- unSM m st_env us0 sc0
+      (r, w2, us2, sc2) <- unSM k st_env us1 sc1
+      return (r, w1 ++ w2, us2, sc2)
 
 -- TODO: this specializing is not allowed
 -- {-# SPECIALIZE mapM         :: (a -> SimplM b) -> [a] -> SimplM [b] #-}
@@ -155,29 +161,29 @@ traceSmpl herald doc
 instance MonadUnique SimplM where
     getUniqueSupplyM
        = SM (\_st_env us sc -> case splitUniqSupply us of
-                                (us1, us2) -> return (us1, us2, sc))
+                                (us1, us2) -> return (us1, [], us2, sc))
 
     getUniqueM
        = SM (\_st_env us sc -> case takeUniqFromSupply us of
-                                (u, us') -> return (u, us', sc))
+                                (u, us') -> return (u, [], us', sc))
 
     getUniquesM
         = SM (\_st_env us sc -> case splitUniqSupply us of
-                                (us1, us2) -> return (uniqsFromSupply us1, us2, sc))
+                                (us1, us2) -> return (uniqsFromSupply us1, [], us2, sc))
 
 instance HasDynFlags SimplM where
-    getDynFlags = SM (\st_env us sc -> return (st_flags st_env, us, sc))
+    getDynFlags = SM (\st_env us sc -> return (st_flags st_env, [], us, sc))
 
 instance MonadIO SimplM where
     liftIO m = SM $ \_ us sc -> do
       x <- m
-      return (x, us, sc)
+      return (x, [], us, sc)
 
 getSimplRules :: SimplM RuleEnv
-getSimplRules = SM (\st_env us sc -> return (st_rules st_env, us, sc))
+getSimplRules = SM (\st_env us sc -> return (st_rules st_env, [], us, sc))
 
 getFamEnvs :: SimplM (FamInstEnv, FamInstEnv)
-getFamEnvs = SM (\st_env us sc -> return (st_fams st_env, us, sc))
+getFamEnvs = SM (\st_env us sc -> return (st_fams st_env, [], us, sc))
 
 newId :: FastString -> Rig -> Type -> SimplM Id
 newId fs w ty = do uniq <- getUniqueM
@@ -206,11 +212,11 @@ newJoinId bndrs body_ty
 -}
 
 getSimplCount :: SimplM SimplCount
-getSimplCount = SM (\_st_env us sc -> return (sc, us, sc))
+getSimplCount = SM (\_st_env us sc -> return (sc, [], us, sc))
 
 tick :: Tick -> SimplM ()
 tick t = SM (\st_env us sc -> let sc' = doSimplTick (st_flags st_env) t sc
-                              in sc' `seq` return ((), us, sc'))
+                              in sc' `seq` return ((),[], us, sc'))
 
 checkedTick :: Tick -> SimplM ()
 -- Try to take a tick, but fail if too many
@@ -220,7 +226,7 @@ checkedTick t
            then throwGhcExceptionIO $
                   PprProgramError "Simplifier ticks exhausted" (msg sc)
            else let sc' = doSimplTick (st_flags st_env) t sc
-                in sc' `seq` return ((), us, sc'))
+                in sc' `seq` return ((), [], us, sc'))
   where
     msg sc = vcat
       [ text "When trying" <+> ppr t
@@ -249,4 +255,72 @@ freeTick :: Tick -> SimplM ()
 -- used to decide when nothing further has happened
 freeTick t
    = SM (\_st_env us sc -> let sc' = doFreeSimplTick t sc
-                           in sc' `seq` return ((), us, sc'))
+                           in sc' `seq` return ((), [], us, sc'))
+
+{-
+************************************************************************
+*                                                                      *
+Weights
+*                                                                      *
+************************************************************************
+-}
+
+{- Note [Scaling lets]
+The simplifier creates let bindings under certain circumstances which
+are then inserted later. These are returned in `SimplFloats`.
+
+However, we have to be somewhat careful here when it comes to linearity
+as if we create a floating binding x in the scrutinee position.
+
+case_w (let x[1] = "Foo" in Qux x) of
+  Qux _ -> "Bar"
+
+then the let will end up outside the `case` if we perform KnownBranch or
+the case of case optimisation.
+
+let x[1] = "Foo"
+in "Bar"
+
+So we get a linearity failure as the one usage of x is eliminated.
+However, because the ambient context is an Omega context, we know that
+we will use the scrutinee Omega times and hence all bindings inside it
+Omega times as well. The failure was that we created a [1] binding
+whilst inside this context and it then escaped without being scaled.
+
+We also have to be careful as if we have a [1] case
+
+case_1 (let x[1] = "Foo" in Qux x) of
+  Qux x -> x
+
+then the binding maintains the correct linearity once it is floated rom
+the case and KnownBranch is performed.
+
+let x[1] = "Foo"
+in x
+
+The difficulty mainly comes from that we only discover this context
+at a later point once we have rebuilt the contituation. So, whilst rebuilding
+a continuation we keep track of how many case-of-case like opportunities
+take place and hence how much we have to scale lets floated from the scrutinee.
+This is achieved by adding a Writer like effect to the SimplM data type.
+It seems to work in practice, at least for T12944 which originally highlighted
+this problem.
+
+Why do we do this scaling afterwards rather than when the binding is
+created? It is possible the binding comes from a point deep inside the
+expression. It wasn't clear to me that we know enough about the context
+at the point we make the binding due to the SimplCont type. It might
+be thread this information through to get it right at definition site.
+For now, I leave warnings and this message to my future self.
+-}
+
+readScaling :: SimplM a -> SimplM (a, Rig)
+readScaling (SM k) = SM (\st_env us sc -> do
+                            (r, w, u, s) <- k st_env us sc
+                            pprTrace "readScaling" (ppr w) (return ())
+                            return ((r, foldr (*) One (map idWeight w)), w, u, s))
+
+reportScaling :: Id -> SimplM ()
+reportScaling r =
+  pprTrace "reporting scaling" (ppr r <+> ppr (idWeight r))
+    $ SM (\_st_env us sc -> return ((), [r], us, sc))
