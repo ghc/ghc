3837187e57bfcc9c00a717eb70cb3e9271525047
Alp Mestanogullari <alpmestan@gmail.com>
2018-05-18 11:58:12 +0200

    remove (unnecessary) recursive dependency involving ghcPath (#606)

--- src/Settings/Builders/Make.hs
+++ src/Settings/Builders/Make.hs
@@ -1,6 +1,5 @@
 module Settings.Builders.Make (makeBuilderArgs) where
 
-import Builder
 import Rules.Gmp
 import Rules.Libffi
 import Settings.Builders.Common
@@ -10,16 +9,9 @@ makeBuilderArgs = do
     threads    <- shakeThreads <$> expr getShakeOptions
     gmpPath    <- expr gmpBuildPath
     libffiPath <- expr libffiBuildPath
-    ghcPath    <- expr $
-      (-/-) <$> topDirectory <*> builderPath (Ghc CompileHs Stage2)
-    perlPath   <- expr $ builderPath Perl
     let t = show $ max 4 (threads - 2) -- Don't use all Shake's threads
     mconcat
         [ builder (Make gmpPath          ) ? pure ["MAKEFLAGS=-j" ++ t]
         , builder (Make libffiPath       ) ? pure ["MAKEFLAGS=-j" ++ t, "install"]
         , builder (Make "testsuite/tests") ? pure ["THREADS=" ++ t, "fast"]
-        , builder (Make "nofib"          ) ? pure
-            [ "WithNofibHc=" ++ ghcPath
-            , "PERL=" ++ perlPath
-            ]
         ]
