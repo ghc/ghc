92db10bc061e0054d0a7504de420b5ad7f72a0a0
Ben Gamari <ben@smart-cactus.org>
2018-08-21 12:48:34 -0400

    testsuite: Deduplicate source in wcompat-warnings test

---
+++ testsuite/tests/wcompat-warnings/Template.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE NoMonadFailDesugaring #-}
+
+module WCompatWarningsOnOff where
+
+import qualified Data.Semigroup as Semi
+
+monadFail :: Monad m => m a
+monadFail = do
+    Just _ <- undefined
+    undefined
+
+(<>) = undefined -- Semigroup warnings
+
+-- -fwarn-noncanonical-monoid-instances
+newtype S = S Int
+
+instance Semi.Semigroup S where
+  (<>) = mappend
+
+instance Monoid S where
+  S a `mappend` S b = S (a+b)
+  mempty = S 0
+
