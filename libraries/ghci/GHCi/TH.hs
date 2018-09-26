a04ecd7ba8c7f012369eeb5864b813a130e043e3
Simon Marlow <marlowsd@gmail.com>
2018-09-26 15:32:29 -0500

    Fix for recover with -fexternal-interpreter (#15418)
    Summary:
    When using -fexternal-interpreter, recover was not treating a Q
    compuation that simply registered an error with addErrTc as failing.
    
    Test Plan:
    New unit tests:
    * T15418 is the repro from in the ticket
    * TH_recover_warns is a new test to ensure that we're keeping warnings when
      the body of recover succeeds.
    
    Reviewers: bgamari, RyanGlScott, angerman, goldfire, erikd
    
    Subscribers: rwbarton, carter
    
    GHC Trac Issues: #15418
    
    Differential Revision: https://phabricator.haskell.org/D5185
    
    (cherry picked from commit d00c308633fe7d216d31a1087e00e63532d87d6d)

--- libraries/ghci/GHCi/TH.hs
+++ libraries/ghci/GHCi/TH.hs
@@ -105,6 +105,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as LB
 import Data.Data
 import Data.Dynamic
+import Data.Either
 import Data.IORef
 import Data.Map (Map)
 import qualified Data.Map as M
@@ -169,13 +170,13 @@ instance TH.Quasi GHCiQ where
   qReport isError msg = ghcCmd (Report isError msg)
 
   -- See Note [TH recover with -fexternal-interpreter] in TcSplice
-  qRecover (GHCiQ h) (GHCiQ a) = GHCiQ $ \s -> (do
+  qRecover (GHCiQ h) a = GHCiQ $ \s -> mask $ \unmask -> do
     remoteTHCall (qsPipe s) StartRecover
-    (r, s') <- a s
-    remoteTHCall (qsPipe s) (EndRecover False)
-    return (r,s'))
-      `catch`
-       \GHCiQException{} -> remoteTHCall (qsPipe s) (EndRecover True) >> h s
+    e <- try $ unmask $ runGHCiQ (a <* ghcCmd FailIfErrs) s
+    remoteTHCall (qsPipe s) (EndRecover (isLeft e))
+    case e of
+      Left GHCiQException{} -> h s
+      Right r -> return r
   qLookupName isType occ = ghcCmd (LookupName isType occ)
   qReify name = ghcCmd (Reify name)
   qReifyFixity name = ghcCmd (ReifyFixity name)
