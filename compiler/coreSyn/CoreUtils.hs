4111540127ae059774b2b7e69d7eddee01a17e81
Simon Peyton Jones <simonpj@microsoft.com>
2018-10-16 12:00:32 +0100

    Comments and white space

--- compiler/coreSyn/CoreUtils.hs
+++ compiler/coreSyn/CoreUtils.hs
@@ -1503,7 +1503,6 @@ expr_ok primop_ok (Lam b e)
                  | isTyVar b = expr_ok primop_ok  e
                  | otherwise = True
 
-
 -- Tick annotations that *tick* cannot be speculated, because these
 -- are meant to identify whether or not (and how often) the particular
 -- source expression was evaluated at runtime.
@@ -1613,7 +1612,7 @@ isDivOp _                = False
 exprOkForSpeculation accepts very special case expressions.
 Reason: (a ==# b) is ok-for-speculation, but the litEq rules
 in PrelRules convert it (a ==# 3#) to
-   case a of { DEAFULT -> 0#; 3# -> 1# }
+   case a of { DEFAULT -> 0#; 3# -> 1# }
 for excellent reasons described in
   PrelRules Note [The litEq rule: converting equality to case].
 So, annoyingly, we want that case expression to be
@@ -1685,7 +1684,7 @@ In earlier GHCs, we got this:
           0 -> 0 }
 
 Before join-points etc we could only get rid of two cases (which are
-redundant) by recognising that th e(case <# ds 5 of { ... }) is
+redundant) by recognising that the (case <# ds 5 of { ... }) is
 ok-for-speculation, even though it has /lifted/ type.  But now join
 points do the job nicely.
 ------- End of historical note ------------
