5c8ea5054f4b71b22aa31000a16dd9a98a782b65
Julian Leviston <julian@leviston.net>
2018-11-15 22:00:53 +1100

    Fix a large rationals bug (#15646)

--- compiler/basicTypes/Literal.hs
+++ compiler/basicTypes/Literal.hs
@@ -218,8 +218,13 @@ instance Binary Literal where
              put_ bh i
              put_ bh e
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
+=======
+=======
+
+>>>>>>> Fix a large rationals bug (#15646)
 >>>>>>> Fix a large rationals bug (#15646)
     put_ bh (LitRubbish)      = do putByte bh 8
     get bh = do
