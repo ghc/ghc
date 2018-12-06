637f7e025f73a28aa6ffb5011010a0d9e8e30fad
Alec Theriault <alec.theriault@gmail.com>
2018-12-05 20:46:21 -0800

    Hadrian: support in-tree GMP
    Summary:
    This adds top-level configure flags '--with-intree-gmp' and
    '--with-framework-preferred', both of which are especially relevant
    on MacOS.
    
    Besides gaining two new flags, Hadrian also had to be taught what to
    do with the 'framework' in .cabal files.
    
    Test Plan:
    ./boot && ./configure --with-intree-gmp && ./hadrian/build.sh
    ./boot && ./configure --with-gmp-framework-preferred && ./hadrian/build.sh # on macos
    
    Reviewers: carter, snowleopard, alpmestan, hvr, goldfire, bgamari
    
    Subscribers: rwbarton, erikd
    
    GHC Trac Issues: #16001
    
    Differential Revision: https://phabricator.haskell.org/D5417

--- hadrian/src/Settings/Builders/Ghc.hs
+++ hadrian/src/Settings/Builders/Ghc.hs
@@ -45,6 +45,7 @@ ghcLinkArgs = builder (Ghc LinkHs) ? do
     pkg     <- getPackage
     libs    <- getContextData extraLibs
     libDirs <- getContextData extraLibDirs
+    fmwks   <- getContextData frameworks
     dynamic <- requiresDynamic
     darwin  <- expr osxHost
 
@@ -71,6 +72,7 @@ ghcLinkArgs = builder (Ghc LinkHs) ? do
             , not (nonHsMainPackage pkg) ? arg "-rtsopts"
             , pure [ "-l" ++ lib    | lib <-    libs    ]
             , pure [ "-L" ++ libDir | libDir <- libDirs ]
+            , darwin ? pure (concat [ ["-framework", fmwk] | fmwk <- fmwks ])
             ]
 
 findHsDependencies :: Args
