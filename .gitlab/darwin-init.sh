4ae6aa398d05f764563e95d77a5e3e9961fb73ac
Ben Gamari <ben@smart-cactus.org>
2018-12-21 23:23:56 -0500

    gitlab-ci: Fix Darwin build
    Previously the toolchain directory was wiped away by git clean.

--- .gitlab/darwin-init.sh
+++ .gitlab/darwin-init.sh
@@ -12,7 +12,9 @@ fi
 if [ ! -e $toolchain/bin/ghc ]; then
     mkdir -p tmp
     cd tmp
-    curl https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz | tar -xJ
+    ghc_tarball="https://downloads.haskell.org/~ghc/$GHC_VERSION/ghc-$GHC_VERSION-x86_64-apple-darwin.tar.xz"
+    echo "Fetching GHC from $ghc_tarball"
+    curl $ghc_tarball | tar -xJ
     cd ghc-$GHC_VERSION
     ./configure --prefix=$toolchain
     make install
@@ -22,6 +24,7 @@ fi
 
 if [ ! -e $toolchain/bin/cabal ]; then
     cabal_tarball="https://downloads.haskell.org/~cabal/cabal-install-latest/cabal-install-2.4.1.0-x86_64-apple-darwin-sierra.tar.xz"
+    echo "Fetching cabal-install from $cabal_tarball"
     curl $cabal_tarball | tar -xz
     mv cabal $toolchain/bin
 fi
