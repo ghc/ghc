58698098cf64905741251df8b76eac5bc9b5aebc
Ben Gamari <ben@smart-cactus.org>
2018-12-23 12:33:12 -0500

    ghc-compact: Use Char instead of Integer in compact_share test
    The representation of Integer depends upon the choice of integer backend whereas
    Char should be consistent.

--- libraries/ghc-compact/tests/compact_share.hs
+++ libraries/ghc-compact/tests/compact_share.hs
@@ -1,9 +1,10 @@
+import Data.Char
 import GHC.Compact
 import qualified Data.Map as Map
 
 main = do
-  let m1 = Map.fromList [(x,show x) | x <- [1..(10000::Integer)]]
-      m2 = Map.fromList [(x,y) | x <- [1..(10000::Integer)],
+  let m1 = Map.fromList [(x,show x) | x <- ['a'..chr 10000]]
+      m2 = Map.fromList [(x,y) | x <- ['a'..chr 10000],
                                  Just y <- [Map.lookup x m1]]
   c <- compact (m1,m2)
   print (length (show (getCompact c)))
