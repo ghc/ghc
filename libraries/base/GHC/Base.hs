ea192a0932c6a95d7854e86f743c11249af23ded
Andrew Martin <andrew.thaddeus@gmail.com>
2019-01-04 12:01:25 -0500

    base: Add documentation that liftA2 used to not be a typeclass method

--- libraries/base/GHC/Base.hs
+++ libraries/base/GHC/Base.hs
@@ -543,6 +543,9 @@ class Functor f => Applicative f where
     -- efficient than the default one. In particular, if 'fmap' is an
     -- expensive operation, it is likely better to use 'liftA2' than to
     -- 'fmap' over the structure and then use '<*>'.
+    --
+    -- This became a typeclass method in 4.10.0.0. Prior to that, it was
+    -- a function defined in terms of '<*>' and 'fmap'.
     liftA2 :: (a -> b -> c) -> f a -> f b -> f c
     liftA2 f x = (<*>) (fmap f x)
 
