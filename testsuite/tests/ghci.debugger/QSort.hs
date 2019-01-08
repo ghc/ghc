d2eb344a38f6c727040ba76e584aeaca8e8aefb3
Alec Theriault <alec.theriault@gmail.com>
2019-01-08 10:50:30 -0800

    Fix tests for `integer-simple`
    A bunch of tests for `integer-simple` were now broken for a foolish reason:
    unlike the `integer-gmp` case, there is no CorePrep optimization for turning
    small integers directly into applications of `S#`.
    
    Rather than port this optimization to `integer-simple` (which would involve
    moving a bunch of `integer-simple` names into `PrelNames`), I switched
    as many tests as possible to use `Int`.
    
    The printing of `Integer` is already tested in `print037`.

--- testsuite/tests/ghci.debugger/QSort.hs
+++ testsuite/tests/ghci.debugger/QSort.hs
@@ -5,7 +5,7 @@ qsort [] = []
 qsort (a:as) = (qsort left) ++ [a] ++ (qsort right)
  where (left,right) = (filter (<=a) as, filter (>a) as)
 
-run = qsort [8, 4, 0, 3, 1, 23, 11, 18]
+run = qsort [8 :: Int, 4, 0, 3, 1, 23, 11, 18]
 
 -- > run
 -- [0,1,3,4,8,11,18,23]
