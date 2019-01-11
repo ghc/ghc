404f1ca71f5c2f99b0e5078b3222706dc059c6c5
David Eichmann <EichmannD@gmail.com>
2019-01-11 14:35:20 -0700

    mkdir .ssh earlier

--- .gitlab/push-test-metrics.sh
+++ .gitlab/push-test-metrics.sh
@@ -30,12 +30,12 @@ if [ "$TEST_ENV" = "" ]; then
 fi
 
 # Add gitlab as a known host.
+mkdir -p ~/.ssh
 echo "|1|+AUrMGS1elvPeLNt+NHGa5+c6pU=|4XvfRsQftO1OgZD4c0JJ7oNaii8= ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDXilA5l4kOZPx0nM6xDATF+t4fS6te0eYPDwBI/jLWD9cJVtCnsrwMl5ar+/NfmcD0jnCYztUiVHuXyTaWPJYSQpwltfpTeqpo9/z/0MxkPtSl1uMP2cLbDiqA01OWveChktOXwU6hRQ+7MmO+dNRS/iXrRmYrGv/p1W811QgLBLS9fefEdF25n+0dP71L7Ov7riOawlDmd0C11FraE/R8HX6gs6lbXta1kisdxGyKojYSiCtobUaJxRoatMfUP0a9rwTAyl8tf56LgB+igjMky879VAbL7eQ/AmfHYPrSGJ/YlWP6Jj23Dnos5nOVlWL/rVTs9Y/NakLpPwMs75KTC0Pd74hdf2e3folDdAi2kLrQgO2SI6so7rOYZ+mFkCM751QdDVy4DzjmDvSgSIVf9SV7RQf7e7unE7pSZ/ILupZqz9KhR1MOwVO+ePa5qJMNSdC204PIsRWkIO5KP0QLl507NI9Ri84+aODoHD7gDIWNhU08J2P8/E6r0wcC8uWaxh+HaOjI9BkHjqRYsrgfn54BAuO9kw1cDvyi3c8n7VFlNtvQP15lANwim3gr9upV+r95KEPJCgZMYWJBDPIVtp4GdYxCfXxWj5oMXbA5pf0tNixwNJjAsY7I6RN2htHbuySH36JybOZk+gCj6mQkxpCT/tKaUn14hBJWLq7Q+Q==" >> ~/.ssh/known_hosts
 echo "|1|JZkdAPJmpX6SzGeqhmQLfMWLGQA=|4vTELroOlbFxbCr0WX+PK9EcpD0= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJknufU+I6A5Nm58lmse4/o11Ai2UzYbYe7782J1+kRk" >> ~/.ssh/known_hosts
 
 # Setup ssh keys.
 eval `ssh-agent`
-mkdir -p ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDoMpqMpHea4BphxjEn7khvW4S64FaWqnSTSAHXTX0CWUS3d0WhvT0GZRjiZ8ULJUCbKXHLhjNYo2b9do2Cd+pCeeVc2GPxajLxyJT5bvSouLhFV5LpMqeZz49A90Xbeux61RuVlxCRDGHy6lCw8c8BmWLJcLXdSflM1YbHcDZyg5LGAdBrG4dmDlfWqDAC381T3mFSTy9O60qNAMHjVb2CwmekhkKeN5q53sHV0kb5KryDTSHDprd8tcSMHKbcXsyFbtc2JSy8vYeBRqfCXluIJGMv1YvT0+bdgGF+aIrnqMmqWYnvoNfQxc98c0tDry8pfTNS4SzTeEzNaq0hJcHf david@david-OctaneV" > ~/.ssh/perf_rsa.pub
 touch ~/.ssh/perf_rsa
 chmod 0600 ~/.ssh/perf_rsa
