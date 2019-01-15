7d354e5fd240ff870cfb01a4e73bd5220fab3df0
Sylvain Henry <sylvain@haskus.fr>
2019-01-15 13:14:45 +0100

    Add support for ASM foreign files (.s) in TH (#16180)

--- libraries/ghc-boot-th/GHC/ForeignSrcLang/Type.hs
+++ libraries/ghc-boot-th/GHC/ForeignSrcLang/Type.hs
@@ -6,6 +6,12 @@ module GHC.ForeignSrcLang.Type
 import Prelude -- See note [Why do we import Prelude here?]
 import GHC.Generics (Generic)
 
+-- | Foreign formats supported by GHC via TH
 data ForeignSrcLang
-  = LangC | LangCxx | LangObjc | LangObjcxx | RawObject
+  = LangC      -- ^ C
+  | LangCxx    -- ^ C++
+  | LangObjc   -- ^ Objective C
+  | LangObjcxx -- ^ Objective C++
+  | LangAsm    -- ^ Assembly language (.s)
+  | RawObject  -- ^ Object (.o)
   deriving (Eq, Show, Generic)
--- libraries/template-haskell/Language/Haskell/TH/Syntax.hs
+++ libraries/template-haskell/Language/Haskell/TH/Syntax.hs
@@ -501,11 +501,12 @@ addForeignFile = addForeignSource
 addForeignSource :: ForeignSrcLang -> String -> Q ()
 addForeignSource lang src = do
   let suffix = case lang of
-                 LangC -> "c"
-                 LangCxx -> "cpp"
-                 LangObjc -> "m"
+                 LangC      -> "c"
+                 LangCxx    -> "cpp"
+                 LangObjc   -> "m"
                  LangObjcxx -> "mm"
-                 RawObject -> "a"
+                 LangAsm    -> "s"
+                 RawObject  -> "a"
   path <- addTempFile suffix
   runIO $ writeFile path src
   addForeignFilePath lang path
