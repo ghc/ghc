0e4d37c48910b1f7afae6818b595f5945f8229ef
Sylvain Henry <sylvain@haskus.fr>
2019-01-16 13:17:35 +0100

    NCG: fast compilation of very large strings (#16190)
    This patch adds an optimization into the NCG: for large strings
    (threshold configurable via -fbinary-blob-threshold=XXX flag), instead
    of printing `.asciz "..."` in the generated ASM source, we print
    `.incbin "tmpXXX.dat"` and we dump the contents of the string into a
    temporary "tmpXXX.dat" file.
    
    See the note for more details.

--- compiler/nativeGen/Dwarf/Types.hs
+++ compiler/nativeGen/Dwarf/Types.hs
@@ -27,24 +27,21 @@ import GhcPrelude
 import Debug
 import CLabel
 import CmmExpr         ( GlobalReg(..) )
-import Encoding
 import FastString
 import Outputable
 import Platform
 import Unique
 import Reg
 import SrcLoc
-import Util
+import PprBase
 
 import Dwarf.Constants
 
-import qualified Data.ByteString as BS
 import qualified Control.Monad.Trans.State.Strict as S
 import Control.Monad (zipWithM, join)
 import Data.Bits
 import qualified Data.Map as Map
 import Data.Word
-import Data.Char
 
 import CodeGen.Platform
 
@@ -580,25 +577,7 @@ pprString' str = text "\t.asciz \"" <> str <> char '"'
 
 -- | Generate a string constant. We take care to escape the string.
 pprString :: String -> SDoc
-pprString str
-  = pprString' $ hcat $ map escapeChar $
-    if str `lengthIs` utf8EncodedLength str
-    then str
-    else map (chr . fromIntegral) $ BS.unpack $ bytesFS $ mkFastString str
-
--- | Escape a single non-unicode character
-escapeChar :: Char -> SDoc
-escapeChar '\\' = text "\\\\"
-escapeChar '\"' = text "\\\""
-escapeChar '\n' = text "\\n"
-escapeChar c
-  | isAscii c && isPrint c && c /= '?' -- prevents trigraph warnings
-  = char c
-  | otherwise
-  = char '\\' <> char (intToDigit (ch `div` 64)) <>
-                 char (intToDigit ((ch `div` 8) `mod` 8)) <>
-                 char (intToDigit (ch `mod` 8))
-  where ch = ord c
+pprString str = pprBytes $ bytesFS $ mkFastString str
 
 -- | Generate an offset into another section. This is tricky because
 -- this is handled differently depending on platform: Mac Os expects
