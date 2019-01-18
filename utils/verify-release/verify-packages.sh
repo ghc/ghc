17cfc485be15d6670c8fac297d61f5a48829c417
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-18 21:56:48 +0800

    Improve the verify script

--- utils/verify-release/verify-packages.sh
+++ utils/verify-release/verify-packages.sh
@@ -5,7 +5,7 @@
 rm -fR ghc-packages hackage-packages package-diffs
 mkdir -p ghc-packages hackage-packages package-diffs
 # We'll skip Cabal and tarballs, while looking only at packages in libraries
-# the we reference as gitmodules.
+# that we reference as gitmodules.
 for lib in $(git submodule status|grep libraries|grep -v tarballs|grep -v Cabal|awk -F\  '{ print $2 }'); do
     (cd $lib && cabal new-sdist -o ../../ghc-packages);
 done
@@ -13,10 +13,8 @@ done
 for pkg in $(cd ghc-packages && ls *.tar.gz); do
     PKG=${pkg%%.tar.gz}
     (cd hackage-packages && cabal get --pristine ${PKG})
-    (cd ghc-packages && tar xzf $pkg)
-    diff -ur hackage-packages/${PKG} ghc-packages/${PKG} \
-	| sed "s/hackage-packages\/${PKG}/hackage-package-${PKG}/"g \
-	| sed "s/ghc-packages\/${PKG}/ghc-package-${PKG}/"g \
-	      > package-diffs/${PKG}.patch
+    (cd hackage-packages/${PKG} && git init && git add . && git commit -q -m "hackage packages" && rm -fR *)
+    (cd hackage-packages && tar xzf ../ghc-packages/$pkg)
+    (cd hackage-packages/${PKG} && git -c core.fileMode=false diff > ../$PKG.patch)
 done
-ls -lah package-diffs
+find hackage-packages -name "*.patch" -not -empty -type f -exec false {} +
