e7369f6bf0351ab9fe0752afe27e71f6e691d694
Joachim Breitner <mail@joachim-breitner.de>
2019-01-20 19:25:26 -0500

    Make tablesNextToCode a proper dynamic flag (#15548)
    Summary:
    There is no more use of the TABLES_NEXT_TO_CODE CPP macro in
    `compiler/`. The default value of `tablesNextToCode` is calculated as
    before, but now users of the GHCI API can modify this flag.
    
    Reviewers:
    
    Subscribers: TerrorJack, rwbarton, carter
    
    GHC Trac Issues: #15548
    
    Differential Revision: https://phabricator.haskell.org/D5082

--- hadrian/src/Settings/Packages.hs
+++ hadrian/src/Settings/Packages.hs
@@ -58,10 +58,6 @@ packageArgs = do
             , (any (wayUnit Threaded) rtsWays) ?
               notStage0 ? arg "--ghc-option=-optc-DTHREADED_RTS"
             , ghcWithInterpreter ?
-              ghcEnableTablesNextToCode ?
-              notM (flag GhcUnregisterised) ?
-              notStage0 ? arg "--ghc-option=-DGHCI_TABLES_NEXT_TO_CODE"
-            , ghcWithInterpreter ?
               ghciWithDebugger <$> flavour ?
               notStage0 ? arg "--ghc-option=-DDEBUGGER"
             , ghcProfiled <$> flavour ?
