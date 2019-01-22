abecc2c5f426912899e7a4da7c3445b1d9cf68fa
Alan Zimmerman <alan.zimm@gmail.com>
2019-01-22 23:29:25 +0200

    check-api-annotations checks for annotation preceding its span
    For an API annotation to be useful, it must not occur before the span
    it is enclosed in.
    
    So, for check-api-annotation output, a line such as
    
    ((Test16212.hs:3:22-36,AnnOpenP), [Test16212.hs:3:21]),
    
    should be flagged as an error, as the AnnOpenP location of 3:21
    precedes its enclosing span of 3:22-26.
    
    This patch does this.
    
    Closes #16217

---
+++ testsuite/tests/ghc-api/annotations/Test16212.hs
@@ -0,0 +1,11 @@
+module Test16212 where
+
+class LiftingMonad  (trans :: MTrans) where
+  proof :: Monad m :- Monad (trans m)
+
+class LiftingMonad2  ((trans :: MTrans)) where
+  proof :: Monad m :- Monad (trans m)
+
+data Nat (t :: NatKind) where
+    ZeroNat :: Nat Zero
+    SuccNat :: Nat t -> Nat (Succ t)
