97e2657f5bd557844826e1ce6e438943b6ac7aa0
Ben Gamari <ben@smart-cactus.org>
2019-01-22 19:05:45 -0500

    testsuite: Fix T16180 on Darwin
    As mentioned in #16218, the CPP macro likely isn't defined when we run the
    assembler.

--- testsuite/tests/th/T16180.hs
+++ testsuite/tests/th/T16180.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
 
 module Main where
 
@@ -8,15 +9,18 @@ import Foreign.C.String
 $(do
    -- some architectures require a "_" symbol prefix...
    -- GHC defines a LEADING_UNDERSCORE CPP constant to indicate this.
-   addForeignSource LangAsm
-      "#if defined(LEADING_UNDERSCORE)\n\
-      \.global \"_mydata\"\n\
-      \_mydata:\n\
-      \#else\n\
-      \.global \"mydata\"\n\
-      \mydata:\n\
-      \#endif\n\
-      \.ascii \"Hello world\\0\"\n"
+   let asm = unlines
+           [ "#include \"ghcconfig.h\""
+           , "#if LEADING_UNDERSCORE"
+           , ".global \"_mydata\""
+           , "_mydata:"
+           , "#else"
+           , ".global \"mydata\""
+           , "mydata:"
+           , "#endif"
+           , ".ascii \"Hello world\\0\""
+           ]
+   addForeignSource LangAsm asm
    return [])
 
 foreign import ccall "&mydata" mystring :: CString
