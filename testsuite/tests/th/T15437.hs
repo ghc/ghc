7f630f0dd34e2d8df86b57675dc8f69625969412
Matthew Pickering <matthewtpickering@gmail.com>
2019-01-24 17:32:53 +0000

    Make test better

--- testsuite/tests/th/T15437.hs
+++ testsuite/tests/th/T15437.hs
@@ -1,8 +1,19 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ImpredicativeTypes #-}
+{-# LANGUAGE KindSignatures #-}
 module T15437 where
 
 import T15437A
 
 f :: Int
 f = $$(foo @Int)
+
+g :: Int
+g = $$(foo2 @Int)
+
+h :: Int
+h = $$(foo3 @Int)
+
+i :: Int
+i = $$(foo @(forall (a :: *) . a -> a))
