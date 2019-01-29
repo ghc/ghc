135bb0b0a531b9f6201fdc80ddc2dd896d7a580f
Sylvain Henry <sylvain@haskus.fr>
2019-01-29 02:14:12 +0100

    Fix test for T16180 on Darwin (fix #16128)

--- testsuite/tests/th/T16180.hs
+++ testsuite/tests/th/T16180.hs
@@ -4,19 +4,18 @@ module Main where
 
 import Language.Haskell.TH.Syntax
 import Foreign.C.String
+import Config -- from "ghc" package
 
 $(do
-   -- some architectures require a "_" symbol prefix...
-   -- GHC defines a LEADING_UNDERSCORE CPP constant to indicate this.
-   addForeignSource LangAsm
-      "#if defined(LEADING_UNDERSCORE)\n\
-      \.global \"_mydata\"\n\
-      \_mydata:\n\
-      \#else\n\
-      \.global \"mydata\"\n\
-      \mydata:\n\
-      \#endif\n\
-      \.ascii \"Hello world\\0\"\n"
+   -- some targets (e.g. Darwin) require a "_" symbol prefix...
+   addForeignSource LangAsm (if cLeadingUnderscore == "YES"
+      then ".global \"_mydata\"\n\
+           \_mydata:\n\
+           \.ascii \"Hello world\\0\"\n"
+      else ".global \"mydata\"\n\
+           \mydata:\n\
+           \.ascii \"Hello world\\0\"\n"
+      )
    return [])
 
 foreign import ccall "&mydata" mystring :: CString
