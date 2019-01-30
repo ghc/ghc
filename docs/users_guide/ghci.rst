d83f32f942cd628ea4ed3673e50389f4ba0da112
Moritz Angermann <moritz.angermann@gmail.com>
2019-01-30 09:47:20 +0800

    Cleanup iserv/iserv-proxy
    This adds trace messages that include the processes name and as such
    make debugging and following the communication easier.
    
    It also adds a note regarding the fwd*Call proxy-communication logic
    between the proxy and the slave.
    
    The proxy will now also poll for 60s to wait for the remote iserv
    to come up. (Alternatively you can start the remote process
    beforehand; and just have iserv-proxy connect to it)

--- docs/users_guide/ghci.rst
+++ docs/users_guide/ghci.rst
@@ -3287,6 +3287,38 @@ dynamically-linked) from GHC itself.  So for example:
 This feature is experimental in GHC 8.0.x, but it may become the
 default in future releases.
 
+.. _external-interpreter-proxy:
+
+Running the interpreter on a different host
+-------------------------------------------
+
+When using the flag :ghc-flag:`-fexternal-interpreter` GHC will
+spawn and communicate with the separate process using pipes.  There
+are scenarios (e.g. when cross compiling) where it is favourable to
+have the communication happen over the network. GHC provides two
+utilities for this, which can be found in the ``utils`` directory.
+
+- ``remote-iserv`` needs to be built with the cross compiler to be
+  executed on the remote host. Or in the case of using it on the
+  same host the stage2 compiler will do as well.
+
+- ``iserv-proxy`` needs to be built on the build machine by the
+  build compiler.
+
+After starting ``remote-iserv ⟨tmp_dir⟩ ⟨port⟩`` on the target and
+providing it with a temporary folder (where it will copy the
+necessary libraries to load to) and port it will listen for
+the proxy to connect.
+
+Providing :ghc-flag:`-pgmi /path/to/iserv-proxy`, :ghc-flag:`-pgmo ⟨option⟩`
+and :ghc-flag:`-pgmo ⟨port⟩` in addition to :ghc-flag:`-fexternal-interpreter`
+will then make ghc go through the proxy instead.
+
+There are some limitations when using this. File and process IO
+will be executed on the target. As such packages like git-embed,
+file-embed and others might not behave as expected if the target
+and host do not share the same filesystem.
+
 .. _ghci-faq:
 
 FAQ and Things To Watch Out For
--- libraries/libiserv/src/Lib.hs
+++ libraries/libiserv/src/Lib.hs
@@ -10,16 +10,24 @@ import Control.Exception
 import Control.Monad
 import Data.Binary
 
+import Text.Printf
+import System.Environment (getProgName)
+
 type MessageHook = Msg -> IO Msg
+
+trace :: String -> IO ()
+trace s = getProgName >>= \name -> printf "[%20s] %s\n" name s
 
 serv :: Bool -> MessageHook -> Pipe -> (forall a .IO a -> IO a) -> IO ()
 serv verbose hook pipe restore = loop
  where
   loop = do
+    when verbose $ trace "reading pipe..."
     Msg msg <- readPipe pipe getMessage >>= hook
+
     discardCtrlC
 
-    when verbose $ putStrLn ("iserv: " ++ show msg)
+    when verbose $ trace ("msg: " ++ (show msg))
     case msg of
       Shutdown -> return ()
       RunTH st q ty loc -> wrapRunTH $ runTH pipe st q ty loc
@@ -28,7 +36,7 @@ serv verbose hook pipe restore = loop
 
   reply :: forall a. (Binary a, Show a) => a -> IO ()
   reply r = do
-    when verbose $ putStrLn ("iserv: return: " ++ show r)
+    when verbose $ trace ("writing pipe: " ++ show r)
     writePipe pipe (put r)
     loop
 
@@ -38,23 +46,29 @@ serv verbose hook pipe restore = loop
   -- Note [Remote Template Haskell] in libraries/ghci/GHCi/TH.hs.
   wrapRunTH :: forall a. (Binary a, Show a) => IO a -> IO ()
   wrapRunTH io = do
+    when verbose $ trace "wrapRunTH..."
     r <- try io
+    when verbose $ trace "wrapRunTH done."
+    when verbose $ trace "writing RunTHDone."
     writePipe pipe (putTHMessage RunTHDone)
     case r of
       Left e
-        | Just (GHCiQException _ err) <- fromException e  ->
+        | Just (GHCiQException _ err) <- fromException e  -> do
+           when verbose $ trace ("QFail " ++ show err)
            reply (QFail err :: QResult a)
         | otherwise -> do
            str <- showException e
+           when verbose $ trace ("QException " ++ str)
            reply (QException str :: QResult a)
       Right a -> do
-        when verbose $ putStrLn "iserv: QDone"
+        when verbose $ trace "QDone"
         reply (QDone a)
 
   -- carefully when showing an exception, there might be other exceptions
   -- lurking inside it.  If so, we return the inner exception instead.
   showException :: SomeException -> IO String
   showException e0 = do
+     when verbose $ trace "showException"
      r <- try $ evaluate (force (show (e0::SomeException)))
      case r of
        Left e -> showException e
@@ -64,6 +78,7 @@ serv verbose hook pipe restore = loop
   -- interpreted code.  GHC will also get the ^C, and either ignore it
   -- (if this is GHCi), or tell us to quit with a Shutdown message.
   discardCtrlC = do
+    when verbose $ trace "discardCtrlC"
     r <- try $ restore $ return ()
     case r of
       Left UserInterrupt -> return () >> discardCtrlC
