c5aa4d9598e637d26d34f4f64080d6d3af5db428
Ben Gamari <ben@well-typed.com>
2019-02-05 11:51:14 -0500

    rts: Implement concurrent collection in the nonmoving collector
    This extends the non-moving collector to allow concurrent collection.
    
    The full design of the collector implemented here is described in detail
    in a technical note
    
        B. Gamari. "A Concurrent Garbage Collector For the Glasgow Haskell
        Compiler" (2018)
    
    This extension involves the introduction of a capability-local
    remembered set, known as the /update remembered set/, which tracks
    objects which may no longer be visible to the collector due to mutation.
    To maintain this remembered set we introduce a write barrier on
    mutations which is enabled while a concurrent mark is underway.
    
    The update remembered set representation is similar to that of the
    nonmoving mark queue, being a chunked array of `MarkEntry`s. Each
    `Capability` maintains a single accumulator chunk, which it flushed
    when it (a) fills it, or (b) when the nonmoving collector enters its
    post-mark synchronization phase.
    
    Co-Authored-by: Ömer Sinan Ağacan <omer@well-typed.com>

---
+++ includes/rts/NonMoving.h
@@ -0,0 +1,25 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 2018-2019
+ *
+ * Non-moving garbage collector
+ *
+ * Do not #include this file directly: #include "Rts.h" instead.
+ *
+ * To understand the structure of the RTS headers, see the wiki:
+ *   http://ghc.haskell.org/trac/ghc/wiki/Commentary/SourceTree/Includes
+ *
+ * -------------------------------------------------------------------------- */
+
+#pragma once
+
+/* This is called by the code generator */
+extern DLL_IMPORT_RTS
+void updateRemembSetPushClosure_(StgRegTable *reg,
+                               StgClosure *p,
+                               StgClosure **origin);
+
+
+void updateRemembSetPushClosure(Capability *cap,
+                              StgClosure *p,
+                              StgClosure **origin);
