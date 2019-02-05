a99e4b0ea696ecc4493a926b45e9f08ca3eae459
Ben Gamari <ben@well-typed.com>
2019-02-05 11:52:13 -0500

    rts: Introduce non-moving heap census
    This introduces a simple census of the non-moving heap (not to be
    confused with the heap census used by the heap profiler). This
    collects basic heap usage information (number of allocated and free
    blocks) which is useful when characterising fragmentation of the
    nonmoving heap.

---
+++ rts/sm/NonMovingCensus.h
@@ -0,0 +1,11 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 1998-2018
+ *
+ * Non-moving garbage collector and allocator: Accounting census
+ *
+ * ---------------------------------------------------------------------------*/
+
+#pragma once
+
+void nonmovingPrintAllocatorCensus(void);
