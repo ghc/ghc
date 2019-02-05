68c28082a398d6fc59ac318106c8239254730bae
Ben Gamari <ben@well-typed.com>
2019-02-05 11:51:14 -0500

    rts: Implement concurrent collection in the nonmoving collector
    This extends the non-moving collector to allow concurrent collection.
    
    The full design of the collector implemented here is described in detail
    in a technical note
    
        B. Gamari. "A Concurrent Garbage Collector For the Glasgow Haskell
        Compiler" (2018)
    
    This extension involves the introduction of a capability-local
    remembered set, known as the /update remembered set/, which tracks
    objects which may no longer be visible to the collector due to mutation.
    To maintain this remembered set we introduce a write barrier on
    mutations which is enabled while a concurrent mark is underway.
    
    The update remembered set representation is similar to that of the
    nonmoving mark queue, being a chunked array of `MarkEntry`s. Each
    `Capability` maintains a single accumulator chunk, which it flushed
    when it (a) fills it, or (b) when the nonmoving collector enters its
    post-mark synchronization phase.
    
    Co-Authored-by: Ömer Sinan Ağacan <omer@well-typed.com>

--- rts/Threads.c
+++ rts/Threads.c
@@ -709,9 +709,19 @@ threadStackUnderflow (Capability *cap, StgTSO *tso)
             barf("threadStackUnderflow: not enough space for return values");
         }
 
-        new_stack->sp -= retvals;
+        if (nonmoving_write_barrier_enabled) {
+            // ensure that values that we copy into the new stack are marked
+            // for the nonmoving collector. Note that these values won't
+            // necessarily form a full closure so we need to handle them
+            // specially.
+            for (unsigned int i = 0; i < retvals; i++) {
+                updateRemembSetPushClosure(cap,
+                                         (StgClosure *) old_stack->sp[i],
+                                         NULL);
+            }
+        }
 
-        memcpy(/* dest */ new_stack->sp,
+        memcpy(/* dest */ new_stack->sp - retvals,
                /* src  */ old_stack->sp,
                /* size */ retvals * sizeof(W_));
     }
@@ -723,8 +733,12 @@ threadStackUnderflow (Capability *cap, StgTSO *tso)
     // restore the stack parameters, and update tot_stack_size
     tso->tot_stack_size -= old_stack->stack_size;
 
-    // we're about to run it, better mark it dirty
+    // we're about to run it, better mark it dirty.
+    //
+    // N.B. the nonmoving collector may mark the stack, meaning that sp must
+    // point at a valid stack frame.
     dirty_STACK(cap, new_stack);
+    new_stack->sp -= retvals;
 
     return retvals;
 }
@@ -755,7 +769,7 @@ loop:
     if (q == (StgMVarTSOQueue*)&stg_END_TSO_QUEUE_closure) {
         /* No further takes, the MVar is now full. */
         if (info == &stg_MVAR_CLEAN_info) {
-            dirty_MVAR(&cap->r, (StgClosure*)mvar);
+            dirty_MVAR(&cap->r, (StgClosure*)mvar, mvar->value);
         }
 
         mvar->value = value;
