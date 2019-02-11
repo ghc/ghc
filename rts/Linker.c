5182b67d1058e10ccc53c63705a9c27af0f76dca
Artem Pyanykh <artempyanykh@gmail.com>
2019-02-11 14:05:14 +0300

    Adjust section placement and relocation logic for Mach-O
    1. Place each section on a separate page to ensure required
    alignment (wastes lots ot space, needs to be improved).
    2. Unwire relocation logic from macho sections (the most fiddly part
    is adjusting internal relocations).
    
    Other todos:
    0. Add a test for section alignment.
    1. Investigate 32bit relocations!
    2. Fix memory leak in ZEROPAGE section allocation.
    3. Fix creating redundant jump islands for GOT.
    4. Investigate more compact section placement.

--- rts/Linker.c
+++ rts/Linker.c
@@ -1421,6 +1421,8 @@ preloadObjectFile (pathchar *path)
 
 #endif /* RTS_LINKER_USE_MMAP */
 
+   IF_DEBUG(linker, debugBelch("loadObj: preloaded image at %p\n", (void *) image));
+
    oc = mkOc(path, image, fileSize, true, NULL, misalignment);
 
 #if defined(OBJFORMAT_MACHO)
@@ -1442,7 +1444,7 @@ preloadObjectFile (pathchar *path)
 static HsInt loadObj_ (pathchar *path)
 {
    ObjectCode* oc;
-   IF_DEBUG(linker, debugBelch("loadObj %" PATH_FMT "\n", path));
+   IF_DEBUG(linker, debugBelch("loadObj: %" PATH_FMT "\n", path));
 
    /* debugBelch("loadObj %s\n", path ); */
 
@@ -1612,6 +1614,8 @@ int ocTryLoad (ObjectCode* oc) {
     if (!r) { return r; }
 
     // run init/init_array/ctors/mod_init_func
+
+    IF_DEBUG(linker, debugBelch("ocTryLoad: ocRunInit pre-start\n"));
 
     loading_obj = oc; // tells foreignExportStablePtr what to do
 #if defined(OBJFORMAT_ELF)
@@ -1816,8 +1820,8 @@ void freeProddableBlocks (ObjectCode *oc)
  */
 void
 addSection (Section *s, SectionKind kind, SectionAlloc alloc,
-            void* start, StgWord size, StgWord mapped_offset,
-            void* mapped_start, StgWord mapped_size)
+            void* start, StgWord size,
+            StgWord mapped_offset, void* mapped_start, StgWord mapped_size)
 {
    s->start        = start;     /* actual start of section in memory */
    s->size         = size;      /* actual size of section in memory */
--- rts/linker/MachO.h
+++ rts/linker/MachO.h
@@ -6,14 +6,15 @@
 
 #include "MachOTypes.h"
 
-void ocInit_MachO          ( ObjectCode* oc );
-void ocDeinit_MachO        ( ObjectCode* oc );
-int ocVerifyImage_MachO    ( ObjectCode* oc );
-int ocGetNames_MachO       ( ObjectCode* oc );
-int ocResolve_MachO        ( ObjectCode* oc );
-int ocRunInit_MachO        ( ObjectCode* oc );
-int machoGetMisalignment( FILE * );
-int ocAllocateExtras_MachO ( ObjectCode* oc );
+void   ocInit_MachO                 ( ObjectCode* oc );
+void   ocDeinit_MachO               ( ObjectCode* oc );
+int    ocVerifyImage_MachO          ( ObjectCode* oc );
+size_t ocGetRequiredSpace_MachO     ( ObjectCode* oc);
+int    ocGetNames_MachO             ( ObjectCode* oc );
+int    ocResolve_MachO              ( ObjectCode* oc );
+int    ocRunInit_MachO              ( ObjectCode* oc );
+int    machoGetMisalignment         ( FILE * );
+int    ocAllocateSymbolExtras_MachO ( ObjectCode* oc );
 
 #if defined(powerpc_HOST_ARCH)
 void machoInitSymbolsWithoutUnderscore( void );
