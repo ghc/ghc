3582ead9bf1be50949cb789f7a614c409dc23fba
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-02-19 12:41:17 +0300

    Disable auto-enabling compaction for oldest gen when non-moving GC is enabled
    Fixes #162

--- rts/sm/GC.c
+++ rts/sm/GC.c
@@ -1715,10 +1715,12 @@ resize_generations (void)
 
         // Auto-enable compaction when the residency reaches a
         // certain percentage of the maximum heap size (default: 30%).
-        if (RtsFlags.GcFlags.compact ||
-            (max > 0 &&
-             oldest_gen->n_blocks >
-             (RtsFlags.GcFlags.compactThreshold * max) / 100)) {
+        // Except when non-moving GC is enabled.
+        if (!RtsFlags.GcFlags.useNonmoving &&
+            (RtsFlags.GcFlags.compact ||
+             (max > 0 &&
+              oldest_gen->n_blocks >
+              (RtsFlags.GcFlags.compactThreshold * max) / 100))) {
             oldest_gen->mark = 1;
             oldest_gen->compact = 1;
 //        debugBelch("compaction: on\n", live);
