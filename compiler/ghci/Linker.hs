01472e31a2111aac9b5e15c49921f852a41308e9
Julian Leviston <julian@leviston.net>
2019-03-02 10:22:47 +1100

    Factor out DynLinker and several of its dependencies into LinkerTypes
    Extract ClosureEnv
    
    Resolve further extracted types
    
    Add the LinkerTypes module to exported modules in cabal file

--- compiler/ghci/Linker.hs
+++ compiler/ghci/Linker.hs
@@ -39,6 +39,7 @@ import Name
 import NameEnv
 import Module
 import ListSetOps
+import LinkerTypes (DynLinker(..), LinkerUnitId, PersistentLinkerState(..))
 import DynFlags
 import BasicTypes
 import Outputable
@@ -97,9 +98,6 @@ The PersistentLinkerState maps Names to actual closures (for
 interpreted code only), for use during linking.
 -}
 
-newtype DynLinker =
-  DynLinker { dl_mpls :: IORef (MVar (Maybe PersistentLinkerState)) }
-
 uninitializedLinker :: IO DynLinker
 uninitializedLinker =
   newMVar Nothing >>= newIORef >>= (pure . DynLinker)
@@ -123,35 +121,6 @@ readPLS dl = readIORef (dl_mpls dl)
 modifyMbPLS_
   :: DynLinker -> (Maybe PersistentLinkerState -> IO (Maybe PersistentLinkerState)) -> IO ()
 modifyMbPLS_ dl f = readIORef (dl_mpls dl) >>= flip modifyMVar_ f
-
-data PersistentLinkerState
-   = PersistentLinkerState {
-
-        -- Current global mapping from Names to their true values
-        closure_env :: ClosureEnv,
-
-        -- The current global mapping from RdrNames of DataCons to
-        -- info table addresses.
-        -- When a new Unlinked is linked into the running image, or an existing
-        -- module in the image is replaced, the itbl_env must be updated
-        -- appropriately.
-        itbl_env    :: !ItblEnv,
-
-        -- The currently loaded interpreted modules (home package)
-        bcos_loaded :: ![Linkable],
-
-        -- And the currently-loaded compiled modules (home package)
-        objs_loaded :: ![Linkable],
-
-        -- The currently-loaded packages; always object code
-        -- Held, as usual, in dependency order; though I am not sure if
-        -- that is really important
-        pkgs_loaded :: ![LinkerUnitId],
-
-        -- we need to remember the name of previous temporary DLL/.so
-        -- libraries so we can link them (see #10322)
-        temp_sos :: ![(FilePath, String)] }
-
 
 emptyPLS :: DynFlags -> PersistentLinkerState
 emptyPLS _ = PersistentLinkerState {
@@ -1215,9 +1184,6 @@ showLS (Archive nm)   = "(static archive) " ++ nm
 showLS (DLL nm)       = "(dynamic) " ++ nm
 showLS (DLLPath nm)   = "(dynamic) " ++ nm
 showLS (Framework nm) = "(framework) " ++ nm
-
--- TODO: Make this type more precise
-type LinkerUnitId = InstalledUnitId
 
 -- | Link exactly the specified packages, and their dependents (unless of
 -- course they are already linked).  The dependents are linked
