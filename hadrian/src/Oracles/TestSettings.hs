4ebcee67dc36b566d356907b55ee14f7720ad7bd
Alp Mestanogullari <alpmestan@gmail.com>
2019-03-05 13:22:46 +0100

    Hadrian: various improvements around the 'test' rule
    - introduce a -k/--keep-test-files flag to prevent cleanup
    - add -dstg-lint to the options that are always passed to tests
    - infer library ways from the compiler to be tested instead of getting them
      from the flavour (like make)
    - likewise for figuring out whether the compiler to be tested is "debugged"
    - specify config.exeext
    - correctly specify config.in_tree_compiler, instead of always passing True
    - fix formatting of how we pass a few test options
    - add (potential) extensions to check-* program names
    - build check-* programs with the compiler to be tested
    - set TEST_HC_OPTS_INTERACTIVE and PYTHON env vars when running tests

--- hadrian/src/Oracles/TestSettings.hs
+++ hadrian/src/Oracles/TestSettings.hs
@@ -2,10 +2,16 @@
 -- | compiler. We need to search this file for required keys and setting
 -- | required for testsuite e.g. WORDSIZE, HOSTOS etc.
 
-module Oracles.TestSettings (TestSetting (..), testSetting, testRTSSettings) where
+module Oracles.TestSettings
+  ( TestSetting (..), testSetting, testRTSSettings
+  , getCompilerPath, getBinaryDirectory
+  ) where
 
 import Base
 import Hadrian.Oracles.TextFile
+import Oracles.Setting (topDirectory, setting, Setting(..))
+import Settings (programContext)
+import Packages
 
 testConfigFile :: Action FilePath
 testConfigFile = buildRoot <&> (-/- "test/ghcconfig")
@@ -67,3 +73,23 @@ testRTSSettings :: Action [String]
 testRTSSettings = do
     file <- testConfigFile
     words <$> lookupValueOrError file "GhcRTSWays"
+
+-- | Directory to look for binaries.
+--   We assume that required programs are present in the same binary directory
+--   in which ghc is stored and that they have their conventional name.
+getBinaryDirectory :: String -> Action FilePath
+getBinaryDirectory "stage0" = takeDirectory <$> setting SystemGhc
+getBinaryDirectory "stage1" = liftM2 (-/-) topDirectory (stageBinPath Stage0)
+getBinaryDirectory "stage2" = liftM2 (-/-) topDirectory (stageBinPath Stage1)
+getBinaryDirectory compiler = pure $ takeDirectory compiler
+
+-- | Get the path to the given @--test-compiler@.
+getCompilerPath :: String -> Action FilePath
+getCompilerPath "stage0" = setting SystemGhc
+getCompilerPath "stage1" = liftM2 (-/-) topDirectory (fullPath Stage0 ghc)
+getCompilerPath "stage2" = liftM2 (-/-) topDirectory (fullPath Stage1 ghc)
+getCompilerPath compiler = pure compiler
+
+-- | Get the full path to the given program.
+fullPath :: Stage -> Package -> Action FilePath
+fullPath stage pkg = programPath =<< programContext stage pkg
