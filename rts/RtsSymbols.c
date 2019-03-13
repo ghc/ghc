b8d20fe34f79c3599ff094eb36a1519c9712a7fb
Ben Gamari <ben@smart-cactus.org>
2019-03-13 19:42:47 -0400

    ghc-heap: Introduce closureSize
    This function allows the user to compute the (non-transitive) size of a
    heap object in words. The "closure" in the name is admittedly confusing
    but we are stuck with this nomenclature at this point.

--- rts/RtsSymbols.c
+++ rts/RtsSymbols.c
@@ -631,6 +631,7 @@
       SymI_HasProto(initLinker)                                         \
       SymI_HasProto(initLinker_)                                        \
       SymI_HasProto(stg_unpackClosurezh)                                \
+      SymI_HasProto(stg_closureSizzezh)                                 \
       SymI_HasProto(stg_getApStackValzh)                                \
       SymI_HasProto(stg_getSparkzh)                                     \
       SymI_HasProto(stg_numSparkszh)                                    \
