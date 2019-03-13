929047fc24bb316550dee8207243deac8eb71a53
Ben Gamari <ben@smart-cactus.org>
2019-03-13 19:42:47 -0400

    ghc-heap: Introduce closureSize
    This function allows the user to compute the (non-transitive) size of a
    heap object in words. The "closure" in the name is admittedly confusing
    but we are stuck with this nomenclature at this point.

---
+++ libraries/ghc-heap/tests/closure_size.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Control.Monad
+import Type.Reflection
+
+import GHC.Exts.Heap.Closures
+
+assertSize :: forall a. Typeable a => a -> Int -> IO ()
+assertSize !x expected = do
+  let !size = closureSize (asBox x)
+  when (size /= expected) $ do
+    putStrLn $ "closureSize ("++show (typeRep @a)++") == "++show size++", expected "++show expected
+{-# NOINLINE assertSize #-}
+
+pap :: Int -> Char -> Int
+pap x _ = x
+{-# NOINLINE pap #-}
+
+main :: IO ()
+main = do
+  assertSize 'a' 2
+  assertSize (Just ()) 2
+  assertSize (Nothing :: Maybe ()) 1
+  assertSize ((1,2) :: (Int,Int)) 3
+  assertSize ((1,2,3) :: (Int,Int,Int)) 4
+  assertSize (id :: Int -> Int) 1
+  assertSize (fst :: (Int,Int) -> Int) 1
+  assertSize (pap 1) 2
+
