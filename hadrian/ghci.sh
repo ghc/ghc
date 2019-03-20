8e07368faf5090a5ac803f0e4fa3a5a2a4aba05c
Matthew Pickering <matthewtpickering@gmail.com>
2019-03-20 14:12:53 +0000

    Refactor ./hadrian/ghci.sh for better error messages
    By separating these two lines, if the first command fails then `ghci` is
    not loaded. Before it would still load ghci but display lots of errors
    about not being able to find modules.

--- hadrian/ghci.sh
+++ hadrian/ghci.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ghci $(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@") -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs
+set -e
+
+GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
+ghci $GHC_FLAGS -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs
