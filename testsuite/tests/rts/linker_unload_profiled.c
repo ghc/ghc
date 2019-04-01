50f717e086a82a0821d31bbba2036342ab9f84e5
Andrew Farmer <anfarmer@fb.com>
2019-04-01 14:25:53 -0700

    Add failing test for unloading profiled objects
    Summary:
    
    A modern redo of the attempt in D2068. Made easier by D3963.
    
    * Enhances existing linker_unload test to check object status
    before/after the crucial steps. Test still passes.
    
    * Adds a similiar linker_unload_profiled test that is expected to fail. It does.
    
    Test Plan:
    
    Existing test still passes, new test fails as expected.
    
    Reviewers: bgamari smarlow bnitka
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- testsuite/tests/rts/linker_unload_profiled.c
+++ testsuite/tests/rts/linker_unload_profiled.c
@@ -4,7 +4,7 @@
 #include "Rts.h"
 #include <string.h>
 
-#define ITERATIONS 1000
+#define ITERATIONS 100
 
 #if defined(mingw32_HOST_OS)
 #define OBJPATH L"Test.o"
@@ -78,40 +78,6 @@ int main (int argc, char *argv[])
             exit(1);
         }
 
-        printf("%d ", i);
-        fflush(stdout);
-    }
-
-    for (i=0; i < ITERATIONS; i++) {
-        r = loadObj(OBJPATH);
-        if (!r) {
-            errorBelch("loadObj(%s) failed", OBJPATH);
-            exit(1);
-        }
-        r = resolveObjs();
-        if (!r) {
-            errorBelch("resolveObjs failed");
-            exit(1);
-        }
-#if LEADING_UNDERSCORE
-        f = lookupSymbol("_f");
-#else
-        f = lookupSymbol("f");
-#endif
-        if (!f) {
-            errorBelch("lookupSymbol failed");
-            exit(1);
-        }
-        r = f(3);
-        if (r != 4) {
-            errorBelch("call failed; %d", r);
-            exit(1);
-        }
-        // check that we can purge first, then unload
-        purgeObj(OBJPATH);
-        performMajorGC();
-        unloadObj(OBJPATH);
-        performMajorGC();
         printf("%d ", i);
         fflush(stdout);
     }
