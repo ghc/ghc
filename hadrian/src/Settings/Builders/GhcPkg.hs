5276f13edfffc7e6cc56a41ea2428274f68ff8f5
John Ericson <git@JohnEricson.me>
2019-04-07 22:33:21 -0400

    Add extra flag to ghc-pkg and use it

--- hadrian/src/Settings/Builders/GhcPkg.hs
+++ hadrian/src/Settings/Builders/GhcPkg.hs
@@ -8,17 +8,34 @@ ghcPkgBuilderArgs = mconcat
         verbosity <- expr getVerbosity
         stage     <- getStage
         pkgDb     <- expr $ packageDbPath stage
-        mconcat [ arg "--global-package-db"
-                , arg pkgDb
-                , arg "register"
-                , verbosity < Chatty ? arg "-v0"
-                ]
+        targetArch <- getSetting TargetArch
+        targetOS  <- getSetting TargetOs
+        version <- getSetting ProjectVersion
+        mconcat
+          [ arg "--global-package-db"
+          , arg pkgDb
+          , -- this flag is new in this version
+            notStage0 ? mconcat
+              [ arg "--default-user-package-db-subdir"
+              , arg (intercalate "-" [targetArch, targetOS, version])
+              ]
+          , arg "register"
+          , verbosity < Chatty ? arg "-v0"
+          ]
     , builder (GhcPkg Unregister) ? do
         verbosity <- expr getVerbosity
         stage     <- getStage
         pkgDb     <- expr $ packageDbPath stage
+        targetArch <- getSetting TargetArch
+        targetOS  <- getSetting TargetOs
+        version <- getSetting ProjectVersion
         mconcat [ arg "--global-package-db"
                 , arg pkgDb
+                , -- this flag is new in this version
+                  notStage0 ? mconcat
+                    [ arg "--default-user-package-db-subdir"
+                    , arg (intercalate "-" [targetArch, targetOS, version])
+                    ]
                 , arg "unregister"
                 , arg "--force"
                 , verbosity < Chatty ? arg "-v0"
@@ -29,10 +46,23 @@ ghcPkgBuilderArgs = mconcat
         config    <- expr $ pkgInplaceConfig context
         stage     <- getStage
         pkgDb     <- expr $ packageDbPath stage
-        mconcat [ notStage0 ? arg "--global-package-db"
-                , notStage0 ? arg pkgDb
-                , arg "update"
-                , arg "--force"
-                , verbosity < Chatty ? arg "-v0"
-                , bootPackageDatabaseArgs
-                , arg config ] ]
+        targetArch <- getSetting TargetArch
+        targetOS  <- getSetting TargetOs
+        version <- getSetting ProjectVersion
+        mconcat
+          [ -- The wrapper scripts provide this for stage0, but the freshly
+            -- built tools are unwrapped for the later stages.
+            notStage0 ? mconcat
+              [ arg "--global-package-db"
+              , arg pkgDb
+              , -- Additionally, this flag is new in this version
+                arg "--default-user-package-db-subdir"
+              , arg (intercalate "-" [targetArch, targetOS, version])
+              ]
+          , arg "update"
+          , arg "--force"
+          , verbosity < Chatty ? arg "-v0"
+          , bootPackageDatabaseArgs
+          , arg config
+          ]
+    ]
