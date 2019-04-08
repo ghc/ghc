1d0106d922324f849c648db79c4e4b0ce18798d4
John Ericson <git@JohnEricson.me>
2019-04-07 22:33:21 -0400

    Remove most uses of TARGET platform macros
    These prevent multi-target builds.

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -1497,9 +1497,11 @@ versionedAppDir dflags = do
 -- constructing platform-version-dependent files that need to co-exist.
 --
 versionedFilePath :: DynFlags -> FilePath
-versionedFilePath dflags =     TARGET_ARCH
-                        ++ '-':TARGET_OS
-                        ++ '-':projectVersion dflags
+versionedFilePath dflags = intercalate "-"
+  [ stringEncodeArch $ platformArch $ targetPlatform dflags
+  , stringEncodeOS $ platformOS $ targetPlatform dflags
+  , projectVersion dflags
+  ]
   -- NB: This functionality is reimplemented in Cabal, so if you
   -- change it, be sure to update Cabal.
 
@@ -5493,7 +5495,7 @@ addIncludePath p =
 addFrameworkPath p =
   upd (\s -> s{frameworkPaths = frameworkPaths s ++ splitPathList p})
 
-#if !defined(mingw32_TARGET_OS)
+#if !defined(mingw32_HOST_OS)
 split_marker :: Char
 split_marker = ':'   -- not configurable (ToDo)
 #endif
@@ -5505,7 +5507,7 @@ splitPathList s = filter notNull (splitUp s)
                 -- cause confusion when they are translated into -I options
                 -- for passing to gcc.
   where
-#if !defined(mingw32_TARGET_OS)
+#if !defined(mingw32_HOST_OS)
     splitUp xs = split split_marker xs
 #else
      -- Windows: 'hybrid' support for DOS-style paths in directory lists.
