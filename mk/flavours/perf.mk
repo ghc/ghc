76f744a5b8ef170f9d2106cbd5d13a97991d9cee
Takenobu Tani <takenobu.hs@gmail.com>
2019-04-14 21:31:05 +0900

    Add `-haddock` to prepare-system.sh and .gitlab-ci.yml
    To cover ci conditions from ghc8.6 to 8.9, I add `-haddock` option
    to `.circleci/prepare-system.sh` and .gitlab-ci.yml.
    after including `mk/flavours/*`.

--- mk/flavours/perf.mk
+++ mk/flavours/perf.mk
@@ -1,7 +1,7 @@
 SRC_HC_OPTS        = -O -H64m
 GhcStage1HcOpts    = -O2
 GhcStage2HcOpts    = -O2
-GhcLibHcOpts       = -O2 -haddock
+GhcLibHcOpts       = -O2
 BUILD_PROF_LIBS    = YES
 #HADDOCK_DOCS
 #BUILD_SPHINX_HTML
