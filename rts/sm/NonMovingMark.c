d15ac82dcaf1994a3027596e4c8c48fd300bb62d
Ben Gamari <ben@smart-cactus.org>
2019-04-14 12:26:57 -0400

    NonMoving: Allocate mark queues in larger block groups

--- rts/sm/NonMovingMark.c
+++ rts/sm/NonMovingMark.c
@@ -361,7 +361,7 @@ push (MarkQueue *q, const MarkQueueEnt *ent)
         } else {
             // allocate a fresh block.
             ACQUIRE_SM_LOCK;
-            bdescr *bd = allocGroup(1);
+            bdescr *bd = allocGroup(MARK_QUEUE_BLOCKS);
             bd->link = q->blocks;
             q->blocks = bd;
             q->top = (MarkQueueBlock *) bd->start;
@@ -392,7 +392,7 @@ markQueuePushClosureGC (MarkQueue *q, StgClosure *p)
         // Yes, this block is full.
         // allocate a fresh block.
         ACQUIRE_SPIN_LOCK(&gc_alloc_block_sync);
-        bdescr *bd = allocGroup(1);
+        bdescr *bd = allocGroup(MARK_QUEUE_BLOCKS);
         bd->link = q->blocks;
         q->blocks = bd;
         q->top = (MarkQueueBlock *) bd->start;
@@ -742,7 +742,7 @@ again:
 /* Must hold sm_mutex. */
 static void init_mark_queue_ (MarkQueue *queue)
 {
-    bdescr *bd = allocGroup(1);
+    bdescr *bd = allocGroup(MARK_QUEUE_BLOCKS);
     queue->blocks = bd;
     queue->top = (MarkQueueBlock *) bd->start;
     queue->top->head = 0;
