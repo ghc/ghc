feb4ac19d5d7a71e4f71d6db79b8603c955978f4
John Ericson <John.Ericson@Obsidian.Systems>
2019-05-07 19:09:25 -0400

    Dont refer to `cLeadingUnderscore` in test
    Can't use this config entry because it's about to go away

--- testsuite/tests/th/T16180.hs
+++ testsuite/tests/th/T16180.hs
@@ -1,21 +1,27 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Language.Haskell.TH.Syntax
 import Foreign.C.String
-import Config -- from "ghc" package
 
 $(do
-   -- some targets (e.g. Darwin) require a "_" symbol prefix...
-   addForeignSource LangAsm (if cLeadingUnderscore == "YES"
-      then ".global \"_mydata\"\n\
-           \_mydata:\n\
-           \.ascii \"Hello world\\0\"\n"
-      else ".global \"mydata\"\n\
-           \mydata:\n\
-           \.ascii \"Hello world\\0\"\n"
-      )
+   -- some targets (e.g. Darwin) require a "_" symbol prefix. Ideally we
+   -- could reuse FP_LEADING_UNDERSCORE from aclocal.m4 for this
+   -- somehow, but this hopefully is OK for now.
+   addForeignSource LangAsm $ unlines
+#if defined(darwin_HOST_OS) || (defined(mingw32_HOST_OS) && WORD_SIZE_IN_BITS == 32)
+      [ ".global \"_mydata\""
+      , "_mydata:"
+      , ".ascii \"Hello world\\0\""
+      ]
+#else
+      [ ".global \"mydata\""
+      , "mydata:"
+      , ".ascii \"Hello world\\0\""
+      ]
+#endif
    return [])
 
 foreign import ccall "&mydata" mystring :: CString
