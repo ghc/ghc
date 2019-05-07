1e84cb65f1ea272c72cc274eb6fe4a120b3d462a
John Ericson <John.Ericson@Obsidian.Systems>
2019-05-07 19:09:25 -0400

    Can't use this config entry because it's about to go away
    Ideally we could reuse the aclocal.m4 for this somehow, but this
    hopefully is OK for now.

--- testsuite/tests/th/T16180.hs
+++ testsuite/tests/th/T16180.hs
@@ -1,21 +1,24 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Language.Haskell.TH.Syntax
 import Foreign.C.String
-import Config -- from "ghc" package
 
 $(do
    -- some targets (e.g. Darwin) require a "_" symbol prefix...
-   addForeignSource LangAsm (if cLeadingUnderscore == "YES"
-      then ".global \"_mydata\"\n\
-           \_mydata:\n\
-           \.ascii \"Hello world\\0\"\n"
-      else ".global \"mydata\"\n\
-           \mydata:\n\
-           \.ascii \"Hello world\\0\"\n"
-      )
+#if defined(darwin_HOST_OS)
+   addForeignSource LangAsm
+      ".global \"_mydata\"\n\
+      \_mydata:\n\
+      \.ascii \"Hello world\\0\"\n"
+#else
+   addForeignSource LangAsm
+      ".global \"mydata\"\n\
+      \mydata:\n\
+      \.ascii \"Hello world\\0\"\n"
+#endif
    return [])
 
 foreign import ccall "&mydata" mystring :: CString
