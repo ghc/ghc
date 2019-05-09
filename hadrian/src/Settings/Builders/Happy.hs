684dc290563769d456b6f1c772673d64307ab072
Vladislav Zavialov <vlad.z.4096@gmail.com>
2019-05-09 11:01:17 +0300

    Restore the --coerce option in 'happy' configuration
    happy-1.19.10 has been released with a fix for --coerce in the presence
    of higher rank types. This should result in about 10% performance
    improvement in the parser.

--- hadrian/src/Settings/Builders/Happy.hs
+++ hadrian/src/Settings/Builders/Happy.hs
@@ -3,7 +3,7 @@ module Settings.Builders.Happy (happyBuilderArgs) where
 import Settings.Builders.Common
 
 happyBuilderArgs :: Args
-happyBuilderArgs = builder Happy ? mconcat [ arg "-ag" -- TODO (int-index): restore the -c option when happy/pull/134 is merged.
+happyBuilderArgs = builder Happy ? mconcat [ arg "-agc"
                                            , arg "--strict"
                                            , arg =<< getInput
                                            , arg "-o", arg =<< getOutput ]
