fc83fb40e23f5f5e0fc72c721dad026506a67718
Ryan Scott <ryan.gl.scott@gmail.com>
2019-05-14 15:04:02 -0400

    Some forall-related cleanup in deriving code
    * Tweak the parser to allow `deriving` clauses to mention explicit
      `forall`s or kind signatures without gratuitous parentheses.
      (This fixes #14332 as a consequence.)
    * Allow Haddock comments on `deriving` clauses with explicit
      `forall`s. This requires corresponding changes in Haddock.

--- testsuite/tests/haddock/should_compile_flag_haddock/T11768.hs
+++ testsuite/tests/haddock/should_compile_flag_haddock/T11768.hs
@@ -1,5 +1,11 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module T11768 where
+
+class C a b
 
 data Foo = Foo
   deriving Eq -- ^ Documenting a single type
@@ -8,6 +14,7 @@ data Bar = Bar
   deriving ( Eq -- ^ Documenting one of multiple types
            , Ord
            )
+  deriving anyclass ( forall a. C a {- ^ Documenting forall type -} )
 
 -- | Documenting a standalone deriving instance
 deriving instance Read Bar
