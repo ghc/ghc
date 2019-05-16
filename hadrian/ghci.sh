2e508375ad56887c8859b4de80ff43e827ac6a8f
Matthew Pickering <matthewtpickering@gmail.com>
2019-05-16 17:09:50 +0100

    cm

--- hadrian/ghci.sh
+++ hadrian/ghci.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
-ghci $GHC_FLAGS -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs
+GHC=/home/matt/ghc/m559b/stage1/bin/ghc
+GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh"  tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
+echo $GHC_FLAGS
+/home/matt/ghc/m559b/stage1/bin/ghc --interactive -O0 $GHC_FLAGS -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs
