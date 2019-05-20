18f3a3408464c6bbdeb4390dd25af9187a09d438
Matthew Pickering <matthewtpickering@gmail.com>
2019-05-20 17:13:23 +0100

    Working

--- hadrian/ghci.sh
+++ hadrian/ghci.sh
@@ -6,4 +6,4 @@ GHC=/home/matt/ghc/m559b/stage1/bin/ghc
 export GHC_LOADED_INTO_GHCI=1
 GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh"  tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
 echo $GHC_FLAGS
-/home/matt/ghc/m559b/stage1/bin/ghc --interactive -fexternal-interpreter -O0 $GHC_FLAGS -fwrite-interface -hidir=.hadrian_ghci/interface -O0 -DGHC_LOADED_INTO_GHCI -Lcompiler/parser ghc/Main.hs
+_GHC_TOP_DIR=/home/matt/ghc/m559b/stage1/lib /home/matt/ghc/m559b/stage1/bin/ghc --interactive -fexternal-interpreter -O0 $GHC_FLAGS -fwrite-interface -hidir=.hadrian_ghci/interface -O0 -DGHC_LOADED_INTO_GHCI -dynamic ghc/Main.hs
