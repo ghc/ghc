de7f902e3eb3872bd743925a7573e4728f4fd4a5
Ben Gamari <ben@smart-cactus.org>
2019-05-20 22:12:51 -0400

    Revert "XXX: Disable selector optimization and shortcutting"
    This reverts commit e1df1b1e24c66fc9e391dfa275ec2e0332b698ac.

--- rts/sm/Evac.c
+++ rts/sm/Evac.c
@@ -816,9 +816,6 @@ loop:
       StgClosure *r;
       const StgInfoTable *i;
       r = ((StgInd*)q)->indirectee;
-      // XXX: disable shortcutting
-      copy(p,info,q,sizeofW(StgInd),gen_no);
-      return;
       if (GET_CLOSURE_TAG(r) == 0) {
           i = r->header.info;
           if (IS_FORWARDING_PTR(i)) {
@@ -873,8 +870,6 @@ loop:
       return;
 
   case IND:
-    // XXX: disable shortcutting
-    copy(p,info,q,sizeofW(StgInd),gen_no);
     // follow chains of indirections, don't evacuate them
     q = ((StgInd*)q)->indirectee;
     *p = q;
@@ -1242,9 +1237,6 @@ selector_loop:
         // leaks by evaluating this selector thunk anyhow.
         goto bale_out;
     }
-
-    // XXX: Disable selector optimization
-    goto bale_out;
 
     info = INFO_PTR_TO_STRUCT(info);
     switch (info->type) {
