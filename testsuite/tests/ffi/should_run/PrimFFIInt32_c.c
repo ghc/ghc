06982b6cc886d65aa325475ddfb4ad38c69b2d96
John Ericson <git@JohnEricson.me>
2019-06-03 23:47:10 -0400

    Make primops for `{Int,Word}32#`
    Progress towards #19026.
    
    The type was added before, but not its primops. We follow the
    conventions in 36fcf9edee31513db2ddbf716ee0aa79766cbe69 and
    2c959a1894311e59cd2fd469c1967491c1e488f3 for names and testing.
    
    Along with the previous 8- and 16-bit primops, this will allow us to
    avoid many conversions for 8-, 16-, and 32-bit sized numeric types.
    
    Co-authored-by: Sylvain Henry <hsyl20@gmail.com>

---
+++ testsuite/tests/ffi/should_run/PrimFFIInt32_c.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+
+int32_t add_all_int32(
+        int32_t a, int32_t b, int32_t c, int32_t d, int32_t e,
+        int32_t f, int32_t g, int32_t h, int32_t i, int32_t j) {
+    return a + b + c + d + e + f + g + h + i + j;
+}
--- testsuite/tests/primops/should_run/ShowPrim.hs
+++ testsuite/tests/primops/should_run/ShowPrim.hs
@@ -10,13 +10,20 @@ data Test1 = Test1 Int8# Word8#
 data Test2 = Test2 Int16# Word16#
     deriving (Show)
 
+data Test3 = Test3 Int32# Word32#
+    deriving (Show)
+
 test1 :: Test1
 test1 = Test1 (narrowInt8# 1#) (narrowWord8# 2##)
 
 test2 :: Test2
 test2 = Test2 (narrowInt16# 1#) (narrowWord16# 2##)
 
+test3 :: Test3
+test3 = Test3 (narrowInt32# 1#) (narrowWord32# 2##)
+
 main :: IO ()
 main = do
   print test1
   print test2
+  print test3
