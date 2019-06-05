89a7e35dd13c3d2a3adba439daca91497e16e24d
Simon Jakobi <simon.jakobi@gmail.com>
2019-06-05 15:07:12 +0200

    Mark Prelude "not-home" for haddock
    Links to any exports that are also exposed from other, more specific
    modules will now point to those instead.
    
    References to exports like `Enum` that aren't exposed from other
    `base` modules still point at `Prelude`, so nothing seems broken.
    
    See #3870.

--- libraries/base/Prelude.hs
+++ libraries/base/Prelude.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_HADDOCK not-home #-}
 
 -----------------------------------------------------------------------------
 -- |
