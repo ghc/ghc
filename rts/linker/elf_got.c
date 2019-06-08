ghc/ghc

commit 67c422ca0e7b94e021430e3dfc9b19f3de21ed16

rts/linker/{SymbolExtras,elf_got}.c: map code as read-only
protect mmaped addresses from writes after being initially manipulated

Test Plan: ./validate

Reviewers: bgamari, erikd, simonmar

Reviewed By: bgamari

Subscribers: angerman, carlostome, rwbarton, thomie, carter

GHC Trac Issues: #14069

Differential Revision: https://phabricator.haskell.org/D4817

rts/linker/elf_got.c
@@ -62,6 +62,9 @@ makeGot(ObjectCode * oc) {
                     symTab->symbols[i].got_addr
                             = (uint8_t *)oc->info->got_start
                               + (slot++ * sizeof(void*));
+        if(mprotect(mem, oc->info->got_size, PROT_READ) != 0) {
+            sysErrorBelch("unable to protect memory");
+        }
     }
     return EXIT_SUCCESS;
 }

rts/linker/SymbolExtras.c
@@ -51,8 +51,9 @@ int ocAllocateSymbolExtras( ObjectCode* oc, int count, int first )
       n = roundUpToPage(oc->fileSize);
 
       /* Keep image and symbol_extras contiguous */
-      void *new = mmapForLinker(n + (sizeof(SymbolExtra) * count),
-                                MAP_ANONYMOUS, -1, 0);
+
+      size_t allocated_size = n + (sizeof(SymbolExtra) * count);
+      void *new = mmapForLinker(allocated_size, MAP_ANONYMOUS, -1, 0);
       if (new) {
           memcpy(new, oc->image, oc->fileSize);
           if (oc->imageMapped) {
@@ -62,6 +63,9 @@ int ocAllocateSymbolExtras( ObjectCode* oc, int count, int first )
           oc->imageMapped = true;
           oc->fileSize = n + (sizeof(SymbolExtra) * count);
           oc->symbol_extras = (SymbolExtra *) (oc->image + n);
+          if(mprotect(new, allocated_size, PROT_READ | PROT_EXEC) != 0) {
+              sysErrorBelch("unable to protect memory");
+          }
       }
       else {
           oc->symbol_extras = NULL;

commit 653ad809d5669e6ffe29083463ed6d2e98ada351

rts/linker: Only mprotect GOT after it is filled
This fixes a regression, introduced by 67c422ca, where we mprotect'd the
global offset table (GOT) region to PROT_READ before we had finished
filling it, resulting in a linker crash.

Fixes #16779.

rts/linker/elf_got.c
@@ -64,8 +64,6 @@ makeGot(ObjectCode * oc) {
                     symTab->symbols[i].got_addr
                             = (uint8_t *)oc->info->got_start
                               + (slot++ * sizeof(void*));
-        if(mprotect(mem, oc->info->got_size, PROT_READ) != 0) {
-            sysErrorBelch("unable to protect memory");
         }
     }
     return EXIT_SUCCESS;
@@ -114,6 +112,11 @@ fillGot(ObjectCode * oc) {
                 *(void**)symbol->got_addr = symbol->addr;
             }
         }
+    }
+
+    // We are done initializing the GOT; freeze it.
+    if(mprotect(mem, oc->info->got_size, PROT_READ) != 0) {
+        sysErrorBelch("unable to protect memory");
     }
     return EXIT_SUCCESS;
 }