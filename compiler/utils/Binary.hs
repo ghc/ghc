993f05cc9186e4c64b8f372aca2b3ea8b882d675
John Ericson <git@JohnEricson.me>
2019-06-10 09:32:50 -0400

    Create {Int,Word}32Rep
    This prepares the way for making Int32# and Word32# the actual size they
    claim to be.

--- compiler/utils/Binary.hs
+++ compiler/utils/Binary.hs
@@ -651,6 +651,8 @@ instance Binary RuntimeRep where
     put_ bh Word8Rep        = putByte bh 13
     put_ bh Int16Rep        = putByte bh 14
     put_ bh Word16Rep       = putByte bh 15
+    put_ bh Int32Rep        = putByte bh 16
+    put_ bh Word32Rep       = putByte bh 17
 #endif
 
     get bh = do
@@ -673,6 +675,8 @@ instance Binary RuntimeRep where
           13 -> pure Word8Rep
           14 -> pure Int16Rep
           15 -> pure Word16Rep
+          16 -> pure Int32Rep
+          17 -> pure Word32Rep
 #endif
           _  -> fail "Binary.putRuntimeRep: invalid tag"
 
