d32a697da0445cdda59ac72fa23fb54b8952475f
Adam Gundry <adam@well-typed.com>
2019-06-14 20:10:02 +0100

    Update HasField tests
    Adapt overloadedrecflds tests and T17355 to new definition of HasField
    Extend hasfieldrun01 test with partial record field test
    Update hasfieldfail02 test to check unlifted type case
    Accept changed T14189 output due to FieldLabel additional field
    Adjust expected output from dynamic-paper
    Add hasfieldrun03 test for example from user's guide
    
     Metric Increase:
         T12227
         T12707
         T13056
         T15630
         T18304
         T9233
         T9675

--- testsuite/tests/typecheck/should_fail/T17355.hs
+++ testsuite/tests/typecheck/should_fail/T17355.hs
@@ -8,4 +8,4 @@ data Foo = Foo { poly :: forall a. a -> a }
 
 instance Generic (forall a . a)
 instance HasField "myPoly" Foo (forall a. a -> a) where
-    getField (Foo x) = x
+    hasField (Foo x) = (undefined, x)
