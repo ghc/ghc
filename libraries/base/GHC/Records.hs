342e178d9384971a2f4aabb44351228bdfd98d57
Adam Gundry <adam@well-typed.com>
2019-06-14 20:09:43 +0100

    Update GHC.Records to use hasField instead of getField

--- libraries/base/GHC/Records.hs
+++ libraries/base/GHC/Records.hs
@@ -1,31 +1,37 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  GHC.Records
--- Copyright   :  (c) Adam Gundry 2015-2016
+-- Copyright   :  (c) Adam Gundry 2015-2020
 -- License     :  see libraries/base/LICENSE
 --
 -- Maintainer  :  cvs-ghc@haskell.org
 -- Stability   :  internal
 -- Portability :  non-portable (GHC extensions)
 --
--- This module defines the 'HasField' class used by the
--- @OverloadedRecordFields@ extension.  See the
--- <https://gitlab.haskell.org/ghc/ghc/wikis/records/overloaded-record-fields
--- wiki page> for more details.
+-- This module defines the 'HasField' class used to provide polymorphism over
+-- record fields.  See the
+-- [wiki page](https://gitlab.haskell.org/ghc/ghc/wikis/records/overloaded-record-fields)
+-- for more details.
 --
 -----------------------------------------------------------------------------
 
 module GHC.Records
        ( HasField(..)
+       , getField
+       , setField
        ) where
 
--- | Constraint representing the fact that the field @x@ belongs to
--- the record type @r@ and has field type @a@.  This will be solved
--- automatically, but manual instances may be provided as well.
+-- | Constraint representing the fact that the field @x@ can be get and set on
+--   the record type @r@ and has field type @a@.  This constraint will be solved
+--   automatically, but manual instances may be provided as well.
+--
+-- @since 4.10.0.0
 
 --   HasField :: forall {k}. k -> * -> * -> Constraint
 --   getField :: forall {k} (x::k) r a. HasField x r a => r -> a
@@ -34,5 +40,28 @@ module GHC.Records
 --     say     getField @"foo"
 --     not     getField @Symbol @"foo"
 class HasField x r a | x r -> a where
-  -- | Selector function to extract the field from the record.
-  getField :: r -> a
+
+  -- | Function to get and set a field in a record.
+  --
+  --   The function should satisfy the invariant:
+  --
+  -- > uncurry ($) (hasField @x r) == r
+  --
+  -- @since 4.15.0.0
+  hasField :: r -> (a -> r, a)
+
+
+-- | Selector function to extract the field from the record.
+--
+-- @since 4.10.0.0
+--
+getField :: forall x r a . HasField x r a => r -> a
+getField = snd . hasField @x
+
+
+-- | Update function to set a new value of the field on the record.
+--
+-- @since 4.15.0.0
+--
+setField :: forall x r a . HasField x r a => r -> a -> r
+setField = fst . hasField @x
