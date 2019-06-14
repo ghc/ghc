c9f97a196288e864837c0e292f2e9964e2c3ef9f
Adam Gundry <adam@well-typed.com>
2019-06-14 20:10:02 +0100

    Update HasField tests
    Adapt overloadedrecflds tests and T17355 to new definition of HasField
    Extend hasfieldrun01 test with partial record field test
    Update hasfieldfail02 test to check unlifted type case
    Accept changed T14189 output due to FieldLabel additional field
    Adjust expected output from dynamic-paper
    
     Metric Increase:
         T12227
         T12707
         T13056
         T15630
         T18304
         T9233
         T9675

--- testsuite/tests/overloadedrecflds/should_fail/hasfieldfail02.hs
+++ testsuite/tests/overloadedrecflds/should_fail/hasfieldfail02.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE DataKinds, ExistentialQuantification, MagicHash, RankNTypes,
              TypeApplications #-}
 
-import GHC.Records (HasField(..))
+import GHC.Prim (Int#)
+import GHC.Records (HasField, getField)
 
 data T = MkT { foo :: forall a . a -> a }
 data U = forall b . MkU { bar :: b }
+data V = MkV { baz :: Int# }
 
 -- This should fail because foo is higher-rank.
 x = getField @"foo" (MkT id)
@@ -12,5 +14,8 @@ x = getField @"foo" (MkT id)
 -- This should fail because bar is a naughty record selector (it
 -- involves an existential).
 y = getField @"bar" (MkU True)
+
+-- This should fail because baz is not of kind Type.
+z = getField @"baz" (MkV 3#)
 
 main = return ()
