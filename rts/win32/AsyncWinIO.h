64d8f2fe2d27743e2986d2176b1aa934e5484d7a
Tamar Christina <tamar@zhox.com>
2019-06-16 21:32:25 +0100

    winio: core non-threaded I/O manager

---
+++ rts/win32/AsyncWinIO.h
@@ -0,0 +1,26 @@
+/* AsyncIO.h
+ *
+ * Integrating Win32 asynchronous IOCP with the GHC RTS.
+ *
+ * (c) Tamar Christina, 2018
+ *
+ * NOTE: This is the WinIO manager, only used for --io-manager=native.
+ *       For the MIO manager see AsyncIO.h.
+ */
+
+#pragma once
+
+#include "Rts.h"
+#include <stdbool.h>
+#include <windows.h>
+
+extern bool startupAsyncWinIO(void);
+extern void shutdownAsyncWinIO(bool wait_threads);
+extern void awaitAsyncRequests(bool wait);
+extern void registerNewIOCPHandle (HANDLE port);
+extern void registerAlertableWait (HANDLE port, DWORD mssec, uint64_t num_req);
+
+extern OVERLAPPED_ENTRY* getOverlappedEntries (uint32_t *num);
+extern void servicedIOEntries (uint64_t remaining);
+extern void completeSynchronousRequest (void);
+
