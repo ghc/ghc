4c5ad18bad897fe062ea4e6f209f510b7be38ad7
Andreas Klebinger <klebinger.andreas@gmx.at>
2019-06-17 15:40:14 +0200

    Make sure mkSplitUniqSupply stores the precomputed mask only.
    mkSplitUniqSupply was lazy on the boxed char.
    
    This caused a bunch of issues:
    * The closure captured the boxed Char
    * The mask was recomputed on every split of the supply.
    * It also caused the allocation of MkSplitSupply to happen in it's own
    (allocated) closure. The reason of which I did not further investigate.
    
    We know force the computation of the mask inside mkSplitUniqSupply.
    * This way the mask is computed at most once per UniqSupply creation.
    * It allows ww to kick in, causing the closure to retain the unboxed
    value.
    
    Requesting Uniques in a loop is now faster by about 20%.
    
    I did not check the impact on the overall compiler, but I added a test
    to avoid regressions.

---
+++ testsuite/tests/perf/should_run/UniqLoop.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
+
+module Main where
+
+import UniqSupply
+import Unique
+
+-- Generate a lot of uniques
+main = do
+    us <- mkSplitUniqSupply 'v'
+    seq (churn us 10000000) (return ())
+
+churn :: UniqSupply -> Int -> Int
+churn !us 0 = getKey $ uniqFromSupply us
+churn us n =
+  let (!x,!us') = takeUniqFromSupply us
+  in churn us' (n-1)
