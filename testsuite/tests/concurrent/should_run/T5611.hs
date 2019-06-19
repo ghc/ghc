aeb511a5a26813ee0831acbf27a6351ee2316cb7
Ben Gamari <ben@smart-cactus.org>
2019-06-19 11:58:42 -0400

    testsuite: Use safe FFI call in T5611
    The original issue, #5611, was concerned with safe calls. However, the
    test inexplicably used an unsafe call. Fix this.

--- testsuite/tests/concurrent/should_run/T5611.hs
+++ testsuite/tests/concurrent/should_run/T5611.hs
@@ -4,12 +4,12 @@ import Control.Concurrent
 import Foreign.C
 import System.IO
 
-#ifdef mingw32_HOST_OS
+#if defined(mingw32_HOST_OS)
 sleep n = sleepBlock (n*1000)
-foreign import stdcall unsafe "Sleep" sleepBlock :: Int -> IO ()
+foreign import stdcall safe "Sleep" sleepBlock :: Int -> IO ()
 #else
 sleep n = sleepBlock n
-foreign import ccall unsafe "sleep" sleepBlock :: Int -> IO ()
+foreign import ccall safe "sleep" sleepBlock :: Int -> IO ()
 #endif
 
 main :: IO ()
