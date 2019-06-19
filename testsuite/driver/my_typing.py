fab0397afc10ea63851a026301d1f60edb8ae751
Ben Gamari <ben@smart-cactus.org>
2019-06-19 13:25:07 -0400

    testsuite: A major revamp of the driver
    This tries to put the testsuite driver into a slightly more maintainable
    condition:
    
    * Add type annotations where easily done
    * Use pathlib.Path instead of str paths
    * Make it pass the mypy typechecker

---
+++ testsuite/driver/my_typing.py
@@ -0,0 +1,43 @@
+"""
+This module provides some type definitions and backwards compatibility shims
+for use in the testsuite driver.
+
+The testsuite driver can be typechecked using mypy [1].
+
+
+[1] http://mypy-lang.org/
+"""
+
+try:
+    from typing import *
+    import typing
+except:
+    # The backwards compatibility stubs must live in another module lest
+    # mypy complains.
+    from typing_stubs import * # type: ignore
+
+
+####################################################
+# Backwards compatibility shims
+#
+# N.B. mypy appears to typecheck as though the "then" clause of if structures
+# is taken. We exploit this below.
+
+# TextIO is missing on some older Pythons.
+if 'TextIO' in globals():
+    TextIO = typing.TextIO
+else:
+    TextIO = None # type: ignore
+
+
+####################################################
+# Testsuite-specific types
+
+WayName = NewType("WayName", str)
+TestName = NewType("TestName", str)
+OutputNormalizer = Callable[[str], str]
+IssueNumber = NewType("IssueNumber", int)
+
+# Used by perf_notes
+GitHash = NewType("GitHash", str)
+GitRef = NewType("GitRef", str)
