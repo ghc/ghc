46163af1e49b5850065d66333d0c75d65a391fab
Ben Gamari <ben@smart-cactus.org>
2019-06-21 20:42:07 -0400

    Provide dummy definitions

--- testsuite/driver/my_typing.py
+++ testsuite/driver/my_typing.py
@@ -1,18 +1,8 @@
 try:
     from typing import *
-
-    WayName = NewType("WayName", str)
-    TestName = NewType("TestName", str)
-    OutputNormalizer = Callable[[str], str]
 except:
-    pass
-    #WayName = str
-    #TestName = str
+    from typing_stubs import * # type: ignore
 
-    #class Dummy:
-    #    def __index__(self, ty):
-    #        return None
-
-    #List = Dummy() # type: ignore
-    #Tuple = Dummy() # type: ignore
-
+WayName = NewType("WayName", str)
+TestName = NewType("TestName", str)
+OutputNormalizer = Callable[[str], str]
