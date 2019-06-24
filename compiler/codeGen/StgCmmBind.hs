e7f95f00a68dcb4783403849af4e7966244600ca
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-06-24 14:24:58 +0300

    Simplify link_caf and mkForeignLabel functions

--- compiler/codeGen/StgCmmBind.hs
+++ compiler/codeGen/StgCmmBind.hs
@@ -664,7 +664,7 @@ setupUpdate closure_info node body
 
         ; if closureUpdReqd closure_info
           then do       -- Blackhole the (updatable) CAF:
-                { upd_closure <- link_caf node True
+                { upd_closure <- link_caf node
                 ; pushUpdateFrame mkBHUpdInfoLabel upd_closure body }
           else do {tickyUpdateFrameOmitted; body}
     }
@@ -704,11 +704,10 @@ emitUpdateFrame dflags frame lbl updatee = do
 -- See Note [CAF management] in rts/sm/Storage.c
 
 link_caf :: LocalReg           -- pointer to the closure
-         -> Bool               -- True <=> updatable, False <=> single-entry
          -> FCode CmmExpr      -- Returns amode for closure to be updated
 -- This function returns the address of the black hole, so it can be
 -- updated with the new value when available.
-link_caf node _is_upd = do
+link_caf node = do
   { dflags <- getDynFlags
         -- Call the RTS function newCAF, returning the newly-allocated
         -- blackhole indirection closure
