102518e216d6a768298acac29d3558e94ba0400b
Ben Gamari <ben@smart-cactus.org>
2019-06-26 13:41:24 -0400

    hadrian: Warn user if hadrian build fails due to lack of threaded RTS
    See #16873.

--- hadrian/build.cabal.sh
+++ hadrian/build.cabal.sh
@@ -21,13 +21,27 @@ fi
 CABVERSTR=$("$CABAL" --numeric-version)
 CABVER=( ${CABVERSTR//./ } )
 
+build_failed() {
+    ( ghc --info | grep -s '("Support SMP","YES")' ) \
+      || cat <<EOF
+Your compiler does not support the threaded runtime system.
+Please disable the \`threaded\` Cabal flag in project.cabal.local
+by running:
+
+    echo -e "package hadrian\n  flags: -threaded" >> project.cabal.local
+
+EOF
+    exit 1
+}
+
 if [ "${CABVER[0]}" -gt 2 -o "${CABVER[0]}" -eq 2 -a "${CABVER[1]}" -ge 2 ];
 then
     "$CABAL" --project-file="$PROJ" new-build $CABFLAGS -j exe:hadrian
     # use new-exec instead of new-run to make sure that the build-tools (alex & happy) are in PATH
     "$CABAL" --project-file="$PROJ" new-exec  $CABFLAGS    hadrian -- \
         --directory "$PWD" \
-        "$@"
+        "$@" \
+        || build_failed
 else
     echo "Cabal version is too old; you need at least cabal-install 2.2"
     exit 2
