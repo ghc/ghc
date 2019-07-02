fd55afb3cfe45723242debb0c75f8ed650b8e186
Andrew Martin <andrew.thaddeus@gmail.com>
2019-07-02 11:11:08 -0400

    [skip ci] beef up tests

--- testsuite/tests/primops/should_run/BoundsCheckA.hs
+++ testsuite/tests/primops/should_run/BoundsCheckA.hs
@@ -5,6 +5,7 @@
 
 import GHC.Exts
 import GHC.IO (IO(..))
+import GHC.Word (Word16(W16#))
 import Control.Exception (SomeException,toException,try)
 import GHC.IO.Exception (BoundsCheckException(BoundsCheckException))
 import System.IO.Error (userError)
@@ -13,29 +14,52 @@ import Foreign.Storable (sizeOf)
 data IntArray = IntArray ByteArray#
 data MutableIntArray = MutableIntArray (MutableByteArray# RealWorld)
 
+data Word16Array = Word16Array ByteArray#
+data MutableWord16Array = MutableWord16Array (MutableByteArray# RealWorld)
+
 main :: IO ()
 main = do
-  arr <- newIntArray 5
-  writeIntArray arr 0 47
-  writeIntArray arr 1 49
-  writeIntArray arr 2 51
-  writeIntArray arr 3 53
-  writeIntArray arr 4 55
-  demandException =<< try (readIntArray arr (-1))
-  demandException =<< try (readIntArray arr 5)
-  demandValue 47 =<< readIntArray arr 0
-  demandValue 55 =<< readIntArray arr 4
-  demandException =<< try (writeIntArray arr 5 2467)
-  demandException =<< try (writeIntArray arr (-1) 1958)
+  arrA <- newIntArray 5
+  writeIntArray arrA 0 47
+  writeIntArray arrA 1 49
+  writeIntArray arrA 2 51
+  writeIntArray arrA 3 53
+  writeIntArray arrA 4 55
+  demandException =<< try (readIntArray arrA (-1))
+  demandException =<< try (readIntArray arrA 5)
+  demandInt 47 =<< readIntArray arrA 0
+  demandInt 55 =<< readIntArray arrA 4
+  demandException =<< try (writeIntArray arrA 5 2467)
+  demandException =<< try (writeIntArray arrA (-1) 1958)
+  arrB <- newWord16Array 9
+  writeWord16Array arrB 0 103
+  writeWord16Array arrB 1 107
+  writeWord16Array arrB 2 111
+  writeWord16Array arrB 3 115
+  writeWord16Array arrB 4 119
+  writeWord16Array arrB 5 123
+  writeWord16Array arrB 6 127
+  writeWord16Array arrB 7 131
+  writeWord16Array arrB 8 135
+  demandException =<< try (readWord16Array arrB (-1))
+  demandException =<< try (readWord16Array arrB 9)
+  demandWord16 103 =<< readWord16Array arrB 0
+  demandWord16 135 =<< readWord16Array arrB 8
+  
 
 demandException :: Either BoundsCheckException a -> IO ()
 demandException (Left BoundsCheckException) = pure ()
 demandException (Right _) = fail "expected exception"
 
-demandValue :: Int -> Int -> IO ()
-demandValue expected actual = if expected == actual
+demandInt :: Int -> Int -> IO ()
+demandInt expected actual = if expected == actual
   then pure ()
-  else fail $ "expected " ++ show expected ++ " but got " ++ show actual
+  else fail $ "expected int " ++ show expected ++ " but got " ++ show actual
+
+demandWord16 :: Word16 -> Word16 -> IO ()
+demandWord16 expected actual = if expected == actual
+  then pure ()
+  else fail $ "expected word16 " ++ show expected ++ " but got " ++ show actual
 
 sizeOfInt :: Int
 sizeOfInt = sizeOf @Int undefined
@@ -43,7 +67,6 @@ sizeOfInt = sizeOf @Int undefined
 unInt :: Int -> Int#
 unInt (I# i) = i
 
--- This implementation is only correct on a 64-bit platform
 newIntArray :: Int -> IO MutableIntArray
 newIntArray (I# n#)
   = IO (\s# ->
@@ -51,16 +74,30 @@ newIntArray (I# n#)
         (# s'#, arr# #) -> (# s'#, MutableIntArray arr# #)
     )
 
+newWord16Array :: Int -> IO MutableWord16Array
+newWord16Array (I# n#)
+  = IO (\s# ->
+      case newByteArray# (n# *# 2#) s# of
+        (# s'#, arr# #) -> (# s'#, MutableWord16Array arr# #)
+    )
+
 -- | Read a primitive value from the primitive array.
 indexIntArray :: IntArray -> Int -> Int
 indexIntArray (IntArray arr#) (I# i#) = I# (indexIntArray# arr# i#)
+
+indexWord16Array :: Word16Array -> Int -> Word16
+indexWord16Array (Word16Array arr#) (I# i#) = W16# (indexWord16Array# arr# i#)
 
 readIntArray :: MutableIntArray -> Int -> IO Int
 readIntArray (MutableIntArray arr#) (I# i#) = IO $ \s0 ->
   case readIntArray# arr# i# s0 of
     (# s1, r #) -> (# s1, I# r #)
 
--- | Write an element to the given index.
+readWord16Array :: MutableWord16Array -> Int -> IO Word16
+readWord16Array (MutableWord16Array arr#) (I# i#) = IO $ \s0 ->
+  case readWord16Array# arr# i# s0 of
+    (# s1, r #) -> (# s1, W16# r #)
+
 writeIntArray ::
      MutableIntArray -- ^ array
   -> Int -- ^ index
@@ -70,9 +107,21 @@ writeIntArray (MutableIntArray arr#) (I# i#) (I# x) = IO $ \s0 ->
   case writeIntArray# arr# i# x s0 of
     s1 -> (# s1, () #)
 
--- | Convert a mutable byte array to an immutable one without copying. The
--- array should not be modified after the conversion.
+writeWord16Array ::
+     MutableWord16Array -- ^ array
+  -> Int -- ^ index
+  -> Word16 -- ^ element
+  -> IO ()
+writeWord16Array (MutableWord16Array arr#) (I# i#) (W16# x) = IO $ \s0 ->
+  case writeWord16Array# arr# i# x s0 of
+    s1 -> (# s1, () #)
+
 unsafeFreezeIntArray :: MutableIntArray -> IO IntArray
 unsafeFreezeIntArray (MutableIntArray arr#) = IO $ \s# ->
+  case unsafeFreezeByteArray# arr# s# of
+    (# s'#, arr'# #) -> (# s'#, IntArray arr'# #)
+
+unsafeFreezeWord16Array :: MutableIntArray -> IO IntArray
+unsafeFreezeWord16Array (MutableIntArray arr#) = IO $ \s# ->
   case unsafeFreezeByteArray# arr# s# of
     (# s'#, arr'# #) -> (# s'#, IntArray arr'# #)
