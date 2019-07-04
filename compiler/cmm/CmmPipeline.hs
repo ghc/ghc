71e8af7477c026f13ee378bb821cf15d673a0b53
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-07-04 12:24:09 +0300

    Return new CAFs in after all the optimizations (in doSRTs)
    [skip ci]

--- compiler/cmm/CmmPipeline.hs
+++ compiler/cmm/CmmPipeline.hs
@@ -50,10 +50,11 @@ cmmPipeline hsc_env srtInfo prog = withTiming (return dflags) (text "Cmm pipelin
      -- pprTrace "cmmPipeline" (text "CAF infos:" <+> ppr caf_infos) (return ())
      let srtInfo1 = srtInfo0{ cafInfos = caf_infos }
 
-     (srtInfo2, cmms) <- {-# SCC "doSRTs" #-} doSRTs dflags srtInfo1 tops
+     (srtInfo2, cmms, new_cafs) <- {-# SCC "doSRTs" #-} doSRTs dflags srtInfo1 tops
      dumpWith dflags Opt_D_dump_cmm_cps "Post CPS Cmm" (ppr cmms)
 
-     return (srtInfo2, cmms)
+     pprTrace "doSRTs" (text "new_cafs:" <+> ppr new_cafs) $
+       return (srtInfo2, cmms)
 
   where forceRes (info, group) =
           info `seq` foldr (\decl r -> decl `seq` r) () group
