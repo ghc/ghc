ff4d44dfa7f4dbdb91b0fa60cb13882578f79b99
Daniel Gröber <dxld@darkboxed.org>
2019-07-04 04:48:07 +0200

    rts: RetainerProfile.c: Re-enable and fix warnings
    Turns out some genius disabled warnings for RetainerProfile.c in the build
    system. That would have been good to know about five silent type mismatch
    crashes ago.. :)

--- rts/TraverseHeap.h
+++ rts/TraverseHeap.h
@@ -95,7 +95,7 @@ typedef struct traverseState_ {
  * once you can simply return 'first_visit'.
  */
 typedef bool (*visitClosure_cb) (
-    const StgClosure *c,
+    StgClosure *c,
     const StgClosure *cp,
     const stackData data,
     const bool first_visit,
