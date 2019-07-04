c4f820a8a4c386fef22169eda9cf854d886f266f
Ben Gamari <ben@smart-cactus.org>
2019-07-04 16:05:15 -0400

    typecheck: Check for non-sensical mkCoherence*Co applications
    The mkCoherenceLeftCo and mkCoherenceRightCo helpers expect a
    precondition on their arguments. Namely, in the case of
    
        mkCoherenceLeftCo r ty1 co co2
    
    it holds that
    
        co2 :: ty1 ~r ty2
    
    Check this.

--- compiler/GHC/Core/Coercion.hs
+++ compiler/GHC/Core/Coercion.hs
@@ -1146,6 +1146,9 @@ mkGReflLeftCo r ty co
 -- is a GRefl coercion.
 mkCoherenceLeftCo :: Role -> Type -> CoercionN -> Coercion -> Coercion
 mkCoherenceLeftCo r ty co co2
+  | debugIsOn
+  , Pair ty' _ <- coercionKind co2
+  , not $ ty `eqType` ty' = pprPanic "mkCoherenceLeftCo" (ppr ty $$ ppr co $$ ppr co2)
   | isGReflCo co = co2
   | otherwise = (mkSymCo $ GRefl r ty (MCo co)) `mkTransCo` co2
 
@@ -1155,6 +1158,9 @@ mkCoherenceLeftCo r ty co co2
 -- is a GRefl coercion.
 mkCoherenceRightCo :: Role -> Type -> CoercionN -> Coercion -> Coercion
 mkCoherenceRightCo r ty co co2
+  | debugIsOn
+  , Pair _ ty' <- coercionKind co2
+  , not $ ty `eqType` ty' = pprPanic "mkCoherenceLeftCo" (ppr ty $$ ppr co $$ ppr co2)
   | isGReflCo co = co2
   | otherwise = co2 `mkTransCo` GRefl r ty (MCo co)
 
