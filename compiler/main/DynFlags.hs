8908422d5f452030480daf82d77ce20073768a52
Ben Gamari <ben@smart-cactus.org>
2019-07-05 11:31:32 -0400

    Fix formatting of --info's "Debug on" field
    As noted in #16914, the value `True` was used instead of `YES` here, in
    contrast to the other boolean fields emitted by `--info`. This confused
    the testsuite driver and broke the `ghc_debugged` testsuite predicate.

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -5700,7 +5700,7 @@ compilerInfo dflags
        ("GHC Dynamic",                 showBool dynamicGhc),
        -- Whether or not GHC was compiled using -prof
        ("GHC Profiled",                showBool rtsIsProfiled),
-       ("Debug on",                    show debugIsOn),
+       ("Debug on",                    showBool debugIsOn),
        ("LibDir",                      topDir dflags),
        -- The path of the global package database used by GHC
        ("Global Package DB",           systemPackageConfig dflags)
