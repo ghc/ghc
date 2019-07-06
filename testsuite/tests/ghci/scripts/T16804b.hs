8baf7ec7a33ea16aca6f2bbf1dea3ddc8102cfe7
Eric Wolf <ericwolf42@gmail.com>
2019-07-06 17:36:24 +0200

    Add testcase T16804 for #16804
    slightly larger testcase for :type-at and :uses
    so we can see changes, if #16804 is done.

---
+++ testsuite/tests/ghci/scripts/T16804b.hs
@@ -0,0 +1,11 @@
+module T16804b where
+
+import T16804a
+
+printStuff :: IO ()
+printStuff = do
+  print (testFunction A A)
+  print (testFunction2 True)
+  print (testFunction2 False)
+  print niceValue
+  print niceValue2
