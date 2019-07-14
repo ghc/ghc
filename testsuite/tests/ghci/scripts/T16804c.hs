d8cb2737f18f1e548c222ae3e0d9cc3574b4db0d
Eric Wolf <ericwolf42@gmail.com>
2019-07-14 09:24:14 +0200

    Add another small test step
    Use the same identifier name in different
    scopes and see, if ':uses' handles that.

---
+++ testsuite/tests/ghci/scripts/T16804c.hs
@@ -0,0 +1,12 @@
+module T16804c where
+
+import T16804a
+
+a :: Test
+a = A
+
+localScope :: Test -> Bool
+localScope arg = testFunction a arg
+
+localScope2 :: Test -> Bool
+localScope2 a = testFunction B a
