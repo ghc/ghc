2ffb0d40eea4a9db428a7fb874618986ae067d44
Baldur Blöndal <baldurpet@gmail.com>
2019-07-17 00:53:34 +0100

    Kind of Category. Exposition for Control.Category, explain a category-theoretic definition of isomorphisms.

--- libraries/base/Control/Category.hs
+++ libraries/base/Control/Category.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE Trustworthy #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wno-inline-rule-shadowing #-}
     -- The RULES for the methods of class Category may never fire
     -- e.g. identity/left, identity/right, association;  see #10528
@@ -28,17 +30,38 @@ import Data.Coerce (coerce)
 infixr 9 .
 infixr 1 >>>, <<<
 
--- | A class for categories. Instances should satisfy the laws
+-- | A class for categories describing composition of typed
+-- arrows. Has kind
+--
+-- @
+-- Category :: (ob -> ob -> Type) -> Constraint
+-- @
+--
+-- Category has been polykinded since base 4.7.0.0 (GHC 7.8.1, Apr
+-- 2014) before which instances had kind @Type -> Type -> Type@.
+--
+-- Instances should satisfy the laws
 --
 -- [Right identity] @f '.' 'id'  =  f@
 -- [Left identity]  @'id' '.' f  =  f@
 -- [Associativity]  @f '.' (g '.' h)  =  (f '.' g) '.' h@
 --
+-- It lays the groundwork for abstract definitions that are 'Category'
+-- polymorphic and apply to any instance. The central notion of an
+-- /isomorphism/ which is an arrow @f :: cat a b@ that has an inverse
+-- @inv :: cat b a@
+--
+-- * @inv '.' f = 'id' \@a@
+-- * @f '.' inv = 'id' \@b@
+--
+-- and we say that @a@ and @b@ are isomorphic.
+--
+-- /See also/: 'Control.Arrow.Arrow', @Profunctor@.
 class Category cat where
-    -- | the identity morphism
+    -- | The identity arrow.
     id :: cat a a
 
-    -- | morphism composition
+    -- | A composition of two arrows.
     (.) :: cat b c -> cat a b -> cat a c
 
 {-# RULES
@@ -52,22 +75,34 @@ class Category cat where
 
 -- | @since 3.0
 instance Category (->) where
-    id = GHC.Base.id
-    (.) = (GHC.Base..)
+  id :: a -> a
+  id = GHC.Base.id
+
+  (.) :: (b -> c) -> (a -> b) -> (a -> c)
+  (.) = (GHC.Base..)
 
 -- | @since 4.7.0.0
 instance Category (:~:) where
-  id          = Refl
+  id :: a :~: a
+  id = Refl
+
+  (.) :: (b :~: c) -> (a :~: b) -> (a :~: c)
   Refl . Refl = Refl
 
 -- | @since 4.10.0.0
 instance Category (:~~:) where
-  id            = HRefl
+  id :: a :~~:a
+  id = HRefl
+
+  (.) :: (b :~~: c) -> (a :~~: b) -> (a :~~: c)
   HRefl . HRefl = HRefl
 
 -- | @since 4.7.0.0
 instance Category Coercion where
+  id :: Coercion a a
   id = Coercion
+
+  (.) :: Coercion b c -> Coercion a b -> Coercion a c
   (.) Coercion = coerce
 
 -- | Right-to-left composition
