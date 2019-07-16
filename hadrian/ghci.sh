b7937199ea0c6746d7aabc5da9921169b8342681
Sebastian Graf <sgraf1337@gmail.com>
2019-07-16 16:05:39 +0100

    Make GHC-in-GHCi work on Windows
    By not building anything in the dynamic way on Windows, where we don't
    have a working story for DLLs yet.
    
    Also the ghcid command needs to call bash on the hadrian/ghci.sh script
    explicitly as the path gets interpreted differently otherwise.

--- hadrian/ghci.sh
+++ hadrian/ghci.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
-GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 . "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
+GHC_FLAGS=$(TERM=dumb CABFLAGS=-v0 "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")
 ghci $GHC_FLAGS -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs
