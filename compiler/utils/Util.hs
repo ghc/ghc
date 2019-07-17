fee821351500e5e9cb953b89f59b3e939ff227f4
Eric Wolf <ericwolf42@gmail.com>
2019-07-17 07:31:46 +0200

    GHCi: Reimplement +c using the same machinery as .hie files
    see #16804
    
    also adds the hidden command :dump-hie to ghci, to look at the
    hie file produced by :set +c

--- compiler/utils/Util.hs
+++ compiler/utils/Util.hs
@@ -124,6 +124,10 @@ module Util (
         -- * Utils for flags
         OverridingBool(..),
         overrideWith,
+
+        -- * Utils for timestamps of dependent things
+        DependencyState(..),
+        dependencyState,
     ) where
 
 #include "HsVersions.h"
@@ -1468,3 +1472,18 @@ overrideWith :: Bool -> OverridingBool -> Bool
 overrideWith b Auto   = b
 overrideWith _ Always = True
 overrideWith _ Never  = False
+
+data DependencyState
+  = DSMissing
+  -- ^ the dependee is missing
+  | DSOlder
+  -- ^ the dependee is older
+  | DSUpToDate
+  -- ^ the dependee is up to date
+   deriving Eq
+
+dependencyState :: UTCTime -> Maybe UTCTime -> DependencyState
+dependencyState _ Nothing = DSMissing
+dependencyState depender (Just dependee)
+  | dependee < depender = DSOlder
+  | otherwise = DSUpToDate
