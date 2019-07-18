e10889453bd13ac98021bbbcee98cca706b29d36
Alejandro Serrano <trupill@gmail.com>
2019-07-18 16:08:56 +0200

    Use guardedness in function type subsumption

--- compiler/typecheck/TcUnify.hs
+++ compiler/typecheck/TcUnify.hs
@@ -759,9 +759,66 @@ tc_sub_type_ds eq_orig inst_orig ctxt ty_actual ty_expected
     -- caused #12616 because (also bizarrely) 'deriving' code had
     -- -XImpredicativeTypes on.  I deleted the entire case.
 
+    go ty_a ty_e
+      | (act_args@(_ : _), _) <- tcSplitFunTys ty_a
+      , (exp_args@(_ : _), _) <- tcSplitFunTys ty_e
+      = do { let args_to_consider = min (length act_args) (length exp_args)
+                 Right (n_act_args, act_res) = tcSplitFunTysN args_to_consider ty_a
+                 Right (n_exp_args, exp_res) = tcSplitFunTysN args_to_consider ty_e
+             -- See Note [Co/contra-variance of subsumption checking]
+           ; res_wrap <- tc_sub_type_ds eq_orig inst_orig  ctxt act_res exp_res
+             -- res_wrap :: act-res ~> exp_res
+           ; handle_arguments (zip n_act_args n_exp_args) exp_res res_wrap
+           }
+      where
+
+        handle_arguments :: [(TcSigmaType, TcSigmaType)]  -- pairs of arguments
+                         -> TcSigmaType -> HsWrapper      -- info about result
+                         -> TcM HsWrapper
+        -- GenSigCtxt: See Note [Setting the argument context]
+        -- in each case, arg_wrap :: exp_arg ~> act_arg
+        handle_arguments [] _ _
+          = pprPanic "tc_sub_type_ds" (ppr ty_a <+> ppr ty_e)
+        handle_arguments [(last_act_arg, last_exp_arg)] exp_res res_wrap
+          = do { last_arg_wrap <- tc_sub_tc_type eq_orig (given_orig last_exp_arg)
+                                                 GenSigCtxt last_exp_arg last_act_arg
+               ; return (mkWpFun last_arg_wrap res_wrap last_exp_arg exp_res doc)
+               }
+        handle_arguments ((act_arg, exp_arg) : more_args) exp_res res_wrap
+          | is_guarded_type act_arg
+          , is_guarded_type exp_arg
+          = do {  -- Both guarded, do it now
+               ; arg_wrap <- tc_sub_tc_type eq_orig (given_orig exp_arg)
+                                            GenSigCtxt exp_arg act_arg
+               ; more_wrap <- handle_arguments more_args exp_res res_wrap
+               ; return (mkWpFun arg_wrap more_wrap exp_arg more_arrow doc)
+               }
+          | otherwise
+          = do {  -- Some is not guarded guarded, do the rest first
+               ; more_wrap <- handle_arguments more_args exp_res res_wrap
+               ; act_arg' <- zonkTcType act_arg
+               ; exp_arg' <- zonkTcType exp_arg
+               ; arg_wrap <- tc_sub_tc_type eq_orig (given_orig exp_arg')
+                                            GenSigCtxt exp_arg' act_arg'
+               ; return (mkWpFun arg_wrap more_wrap exp_arg' more_arrow doc)
+               }
+          where
+            more_arrow = mkVisFunTys (map snd more_args) exp_res
+
+        is_guarded_type ty
+          | Just (tc, _) <- tcSplitTyConApp_maybe ty
+          = not (isFunTyCon tc) && isInjectiveTyCon tc Nominal
+          | otherwise
+          = False
+          
+        given_orig exp_arg = GivenOrigin (SigSkol GenSigCtxt exp_arg [])
+        doc = text "When checking that" <+> quotes (ppr ty_actual) <+>
+              text "is more polymorphic than" <+> quotes (ppr ty_expected)
+
+    {-
     go (FunTy { ft_af = VisArg, ft_arg = act_arg, ft_res = act_res })
        (FunTy { ft_af = VisArg, ft_arg = exp_arg, ft_res = exp_res })
-      = -- See Note [Co/contra-variance of subsumption checking]
+      = 
         do { res_wrap <- tc_sub_type_ds eq_orig inst_orig  ctxt       act_res exp_res
            ; arg_wrap <- tc_sub_tc_type eq_orig given_orig GenSigCtxt exp_arg act_arg
                          -- GenSigCtxt: See Note [Setting the argument context]
@@ -772,6 +829,7 @@ tc_sub_type_ds eq_orig inst_orig ctxt ty_actual ty_expected
         given_orig = GivenOrigin (SigSkol GenSigCtxt exp_arg [])
         doc = text "When checking that" <+> quotes (ppr ty_actual) <+>
               text "is more polymorphic than" <+> quotes (ppr ty_expected)
+    -}
 
     go ty_a ty_e
       | let (tvs, theta, _) = tcSplitSigmaTy ty_a
