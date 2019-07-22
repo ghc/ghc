2bc1cebd2b0455a397ab44c0d05764d27efe5047
Sebastian Graf <sgraf1337@gmail.com>
2019-07-22 13:35:38 +0000

    Remove unused import

--- compiler/coreSyn/MkCore.hs
+++ compiler/coreSyn/MkCore.hs
@@ -72,7 +72,6 @@ import TysPrim
 import DataCon          ( DataCon, dataConWorkId )
 import IdInfo
 import Demand
-import Cpr
 import Name      hiding ( varName )
 import Outputable
 import FastString
