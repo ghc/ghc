11ce59424ce9240968545466b9652b6d6bf253c2
Richard Eisenberg <rae@richarde.dev>
2019-07-23 15:39:06 -0400

    Note [Don't flatten tuples from HsSyn] in MkCore
    Previously, we would sometimes flatten 1-tuples and sometimes
    not. This didn't cause damage because there is no way to
    generate HsSyn with 1-tuples. But, with the upcoming fix to #16881,
    there will be. Without this patch, obscure lint errors would
    have resulted.
    
    No test case, as there is not yet a way to tickle this.

--- compiler/typecheck/TcPat.hs
+++ compiler/typecheck/TcPat.hs
@@ -446,6 +446,8 @@ tc_pat penv (ListPat (Just e) pats) pat_ty thing_inside
 tc_pat penv (TuplePat _ pats boxity) pat_ty thing_inside
   = do  { let arity = length pats
               tc = tupleTyCon boxity arity
+              -- NB: tupleTyCon does not flatten 1-tuples
+              -- See Note [Don't flatten tuples from HsSyn] in MkCore
         ; (coi, arg_tys) <- matchExpectedPatTy (matchExpectedTyConApp tc)
                                                penv pat_ty
                      -- Unboxed tuples have RuntimeRep vars, which we discard:
