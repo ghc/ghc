7776b41b0aa5ba4df09bcab3f81ac36e626e7dd6
Ben Gamari <ben@smart-cactus.org>
2019-07-25 18:35:39 -0400

    Rewrite FV

--- compiler/utils/FV.hs
+++ compiler/utils/FV.hs
@@ -1,201 +1,200 @@
-{-
-(c) Bartosz Nitka, Facebook 2015
+{-# OPTIONS_GHC -ddump-to-file -ddump-simpl #-}
 
-Utilities for efficiently and deterministically computing free variables.
-
--}
-
-{-# LANGUAGE BangPatterns #-}
-
-module FV (
-        -- * Deterministic free vars computations
-        FV, InterestingVarFun,
-
-        -- * Running the computations
-        fvVarListVarSet, fvVarList, fvVarSet, fvDVarSet,
-
-        -- ** Manipulating those computations
-        unitFV,
-        emptyFV,
-        mkFVs,
-        unionFV,
-        unionsFV,
-        delFV,
-        delFVs,
-        filterFV,
-        mapUnionFV,
-    ) where
+module FV2 where
 
 import GhcPrelude
 
 import Var
 import VarSet
 
--- | Predicate on possible free variables: returns @True@ iff the variable is
--- interesting
+class Monoid m => FVM m where
+  coholeFV  :: CoercionHole -> m
+  tyvarFV   :: TyVar -> m
+  covarFV   :: CoVar -> m
+  tycoVarsFV :: DVarSet -> m
+  bindTyvar :: TyVar -> m -> m
+
+typeFVs :: FVM m => Type -> m
+typeFVs (TyVarTy v) = tyvarFV v
+typeFVs (TyConApp _ tys)   = foldMap typeFVs tys
+typeFVs (LitTy {})         = mempty
+typeFVs (AppTy fun arg)    = typeFVs fun <> typeFVs arg
+typeFVs (FunTy _ arg res)  = typeFVs arg <> typeFVs res
+typeFVs (ForAllTy (Bndr tv _) ty) = typeFVs (varType tv) <> bindTyvar tv (typeFVs ty)
+typeFVs (CastTy ty co)     = typeFVsty <> coFVs co
+typeFVs (CoercionTy co)    = ty_co_vars_of_co co is acc
+{-# SPECIALISE typeFVs :: Type -> FV #-}
+{-# SPECIALISE typeFVs :: Type -> NonDetFV #-}
+
+typesFVs :: FVM m => [Type] -> m
+typesFVs = foldMap typeFVs
+{-# SPECIALISE typesFVs :: [Type] -> FV #-}
+{-# SPECIALISE typesFVs :: [Type] -> NonDetFV #-}
+
+coFVs :: FVM m => Coercion -> m
+coFVs (Refl ty) = typeFVs ty
+coFVs (GRefl _ ty mco) = typeFVs ty <> mcoFVs mco
+coFVs (TyConAppCo_ _ cos) = foldMap coFVs cos
+coFVs (AppCo co arg) = coFVs co <> coFVs arg
+coFVs (ForAllCo tv kind_co co) = coFVs kind_co <> bindTyvar tv (coFVs co)
+coFVs (FunCo _ co1 co2) = coFVs co1 <> coFVs co2
+coFVs (HoleCo hole) = coholeFV hole
+coFVs (AxiomInstCo _ _ cos) = foldMap coFVs cos
+coFVs (UnivCo p _ t1 t2) = provFVs p <> typeFVs t1 <> typeFVs t2
+coFVs (SymCo co) = coFVs co
+coFVs (TransCo co1 co2) = coFVs co1 <> coFVs co2
+coFVs (NthCo _ _ co) = coFVs co
+coFVs (LRCo _ co) = coFVs co
+coFVs (InstCo co arg) = coFVs co <> coFVs arg
+coFVs (KindCo co) = coFVs co
+coFVs (SubCo co) = coFVs co
+coFVs (AxiomRuleCo _ cos) = foldMap coFVs co
+{-# SPECIALISE coFVs :: Coercion -> FV #-}
+{-# SPECIALISE coFVs :: Coercion -> NonDetFV #-}
+
+cosFVs :: FVM m => [Coercion] -> m
+cosFVs = foldMap coFVs
+{-# SPECIALISE cosFVs :: [Coercion] -> FV #-}
+{-# SPECIALISE cosFVs :: [Coercion] -> NonDetFV #-}
+
+mcoFVs :: FVM m => MCoercion -> m
+mcoFVs MRefl = mempty
+mcoFVs (MCo co) = coFVs co
+{-# SPECIALISE mcoFVs :: MCoercion -> FV #-}
+{-# SPECIALISE mcoFVs :: MCoercion -> NonDetFV #-}
+
+provFVs :: FVM m => UnivCoProvenance -> m
+provFVs (PhantomProv co)    = coFVs co
+provFVs (ProofIrrelProv co) = coFVs co
+provFVs UnsafeCoerceProv    = mempty
+provFVs (PluginProv _)      = mempty
+provFVs (ZappedProv fvs)    = tycoVarsFV fvs
+provFVs (TcZappedProv fvs coholes)
+                            = foldMap (tyvarFV . coHoleCoVar) coholes <> tyscoVarsFV fvs
+{-# SPECIALISE provFVs :: UnivCoProvenance -> FV #-}
+{-# SPECIALISE provFVs :: UnivCoProvenance -> NonDetFV #-}
+
+
+newtype AnyFVs = AnyFVs (VarSet -> Bool)
+
+instance Monoid AnyFVs where
+  mempty = AnyFVs $ const False
+  {-# INLINE mempty #-}
+
+instance Semigroup AnyFVs where
+  AnyFVs f <> AnyFVs g = AnyFVs $ \in_scope -> f in_scope || g in_scope
+  {-# INLINE (<>) #-}
+
+instance FVM AnyFVs where
+  coholeFV hole = mempty
+  unitFV v = AnyFVs $ \in_scope -> not (v `elemVarSet` in_scope)
+  tycovarsFV fvs = AnyFVs $ \in_scope -> not $ nullVarSet $ dVarSetToVarSet fvs `minusVarSet` in_scope
+  bindTyVar tv (AnyFVs f) = AnyFVs $ \in_scope -> f (extendVarSet tv in_scope)
+
+  {-# INLINE coholeFV #-}
+  {-# INLINE tyvarFV #-}
+  {-# INLINE covarFV #-}
+  {-# INLINE bindTyvar #-}
+
+runAnyFVs :: AnyFVs -> Bool
+runAnyFVs (AnyFVs f) = f emptyVarSet
+
+
+newtype NonDetFV = NonDetFV { runNonDetFV :: TyCoVarSet -> TyCoVarSet -> TyCoVarSet }
+
+instance Monoid NonDetFV where
+  mempty = NonDetFV $ \_ acc -> acc
+  {-# INLINE mempty #-}
+
+instance Semigroup NonDetFV where
+  NonDetFV f <> NonDetFV g = NonDetFV $ \is acc -> g is (f is acc)
+  {-# INLINE (<>) #-}
+
+addNDFV :: Var -> NonDetFV
+addNDFV v = NonDetFV $ \is acc ->
+    if | v `elemVarSet` is  -> acc
+       | v `elemVarSet` acc -> acc
+       | otherwise       acc -> runNonDetFV (typeFVs (varType v)) emptyVarSet (extendVarSet acc v)
+
+instance FVM NonDetFV where
+  coholeFV hole = covarFV $ coHoleCoVar hole
+  tyvarFV tv = addNDFV tv
+  covarFV cv = addNDFV cv
+  tycovarsFV fvs = NonDetFV $ \is acc -> acc `unionVarSet` (dVarSetToVarSet fvs `minusVarSet` is)
+  bindTyVar tv (NonDetFV f) = NonDetFV $ \is acc -> f (extendVarSet tv is) acc
+
+  {-# INLINE coholeFV #-}
+  {-# INLINE tyvarFV #-}
+  {-# INLINE covarFV #-}
+  {-# INLINE bindTyvar #-}
+
 type InterestingVarFun = Var -> Bool
 
--- Note [Deterministic FV]
--- ~~~~~~~~~~~~~~~~~~~~~~~
--- When computing free variables, the order in which you get them affects
--- the results of floating and specialization. If you use UniqFM to collect
--- them and then turn that into a list, you get them in nondeterministic
--- order as described in Note [Deterministic UniqFM] in UniqDFM.
+newtype FV = FV { runFV :: InterestingVarFun -> TyCoVarSet -> ([Var], VarSet) -> ([Var], VarSet) }
 
--- A naive algorithm for free variables relies on merging sets of variables.
--- Merging costs O(n+m) for UniqFM and for UniqDFM there's an additional log
--- factor. It's cheaper to incrementally add to a list and use a set to check
--- for duplicates.
-type FV = InterestingVarFun
-             -- Used for filtering sets as we build them
-          -> VarSet
-             -- Locally bound variables
-          -> ([Var], VarSet)
-             -- List to preserve ordering and set to check for membership,
-             -- so that the list doesn't have duplicates
-             -- For explanation of why using `VarSet` is not deterministic see
-             -- Note [Deterministic UniqFM] in UniqDFM.
-          -> ([Var], VarSet)
+instance Monoid FV where
+  mempty = FV $ \_ acc -> acc
+  {-# INLINE mempty #-}
 
--- Note [FV naming conventions]
--- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
--- To get the performance and determinism that FV provides, FV computations
--- need to built up from smaller FV computations and then evaluated with
--- one of `fvVarList`, `fvDVarSet`, `fvVarListVarSet`. That means the functions
--- returning FV need to be exported.
---
--- The conventions are:
---
--- a) non-deterministic functions:
---   * a function that returns VarSet
---       e.g. `tyVarsOfType`
--- b) deterministic functions:
---   * a worker that returns FV
---       e.g. `tyFVsOfType`
---   * a function that returns [Var]
---       e.g. `tyVarsOfTypeList`
---   * a function that returns DVarSet
---       e.g. `tyVarsOfTypeDSet`
---
--- Where tyVarsOfType, tyVarsOfTypeList, tyVarsOfTypeDSet are implemented
--- in terms of the worker evaluated with fvVarSet, fvVarList, fvDVarSet
--- respectively.
+instance Semigroup FV where
+  FV f <> FV g = FV $ \fv_cand in_scope acc -> f fv_cand in_scope $! g fv_cand in_scope $! acc
+  {-# INLINE (<>) #-}
 
--- | Run a free variable computation, returning a list of distinct free
--- variables in deterministic order and a non-deterministic set containing
--- those variables.
+instance FVM FV where
+  coholeFV hole = covarFV $ coHoleCoVar hole
+  tyvarFV tv = addFV tv
+  covarFV cv = addFV cv
+  tycoVarsFV fvs = foldMap tyvar (dvarSetElems fvs) -- can we do better than this?
+  bindTyVar tv (FV f) = FV $ \fv_cand in_scope acc ->
+    f fv_cand (extendVarSet tv in_scope) acc ``
+
+  {-# INLINE coholeFV #-}
+  {-# INLINE tyvarFV #-}
+  {-# INLINE covarFV #-}
+  {-# INLINE bindTyvar #-}
+
+addFV :: Var -> FV
+addFV v = FV $ \fv_cand in_scope acc@(have, haveSet) ->
+    if | v `elemVarSet` in_scope  -> acc
+       | v `elemVarSet` haveSet   -> acc
+       | fv_cand var              -> (var: have, extendVarSet haveSet var)
+       | otherwise                -> acc
+{-# INLINE addFV #-}
+
 fvVarListVarSet :: FV ->  ([Var], VarSet)
-fvVarListVarSet fv = fv (const True) emptyVarSet ([], emptyVarSet)
+fvVarListVarSet (FV fv) = fv (const True) emptyVarSet ([], emptyVarSet)
 
--- | Run a free variable computation, returning a list of distinct free
--- variables in deterministic order.
 fvVarList :: FV -> [Var]
 fvVarList = fst . fvVarListVarSet
 
--- | Run a free variable computation, returning a deterministic set of free
--- variables. Note that this is just a wrapper around the version that
--- returns a deterministic list. If you need a list you should use
--- `fvVarList`.
 fvDVarSet :: FV -> DVarSet
-fvDVarSet = mkDVarSet . fst . fvVarListVarSet
+fvDVarSet (FV fv) = mkDVarSet $ fst $ fvVarListVarSet fv
 
--- | Run a free variable computation, returning a non-deterministic set of
--- free variables. Don't use if the set will be later converted to a list
--- and the order of that list will impact the generated code.
 fvVarSet :: FV -> VarSet
 fvVarSet = snd . fvVarListVarSet
 
--- Note [FV eta expansion]
--- ~~~~~~~~~~~~~~~~~~~~~~~
--- Let's consider an eta-reduced implementation of freeVarsOf using FV:
---
--- freeVarsOf (App a b) = freeVarsOf a `unionFV` freeVarsOf b
---
--- If GHC doesn't eta-expand it, after inlining unionFV we end up with
---
--- freeVarsOf = \x ->
---   case x of
---     App a b -> \fv_cand in_scope acc ->
---       freeVarsOf a fv_cand in_scope $! freeVarsOf b fv_cand in_scope $! acc
---
--- which has to create a thunk, resulting in more allocations.
---
--- On the other hand if it is eta-expanded:
---
--- freeVarsOf (App a b) fv_cand in_scope acc =
---   (freeVarsOf a `unionFV` freeVarsOf b) fv_cand in_scope acc
---
--- after inlining unionFV we have:
---
--- freeVarsOf = \x fv_cand in_scope acc ->
---   case x of
---     App a b ->
---       freeVarsOf a fv_cand in_scope $! freeVarsOf b fv_cand in_scope $! acc
---
--- which saves allocations.
---
--- GHC when presented with knowledge about all the call sites, correctly
--- eta-expands in this case. Unfortunately due to the fact that freeVarsOf gets
--- exported to be composed with other functions, GHC doesn't have that
--- information and has to be more conservative here.
---
--- Hence functions that get exported and return FV need to be manually
--- eta-expanded. See also #11146.
+-- | Filter a free variable computation.
+filterFV :: InterestingVarFun -> FV -> FV
+filterFV fv_cand2 (FV fv) = FV $ \fv_cand1 in_scope acc ->
+  fv (\v -> fv_cand1 v && fv_cand2 v) in_scope acc
+{-# INLINE filterFV #-}
 
--- | Add a variable - when free, to the returned free variables.
--- Ignores duplicates and respects the filtering function.
 unitFV :: Id -> FV
-unitFV var fv_cand in_scope acc@(have, haveSet)
-  | var `elemVarSet` in_scope = acc
-  | var `elemVarSet` haveSet = acc
-  | fv_cand var = (var:have, extendVarSet haveSet var)
-  | otherwise = acc
+unitFV = tyvarFV
 {-# INLINE unitFV #-}
 
 -- | Return no free variables.
 emptyFV :: FV
-emptyFV _ _ acc = acc
+emptyFV = mempty
 {-# INLINE emptyFV #-}
-
--- | Union two free variable computations.
-unionFV :: FV -> FV -> FV
-unionFV fv1 fv2 fv_cand in_scope acc =
-  fv1 fv_cand in_scope $! fv2 fv_cand in_scope $! acc
-{-# INLINE unionFV #-}
 
 -- | Mark the variable as not free by putting it in scope.
 delFV :: Var -> FV -> FV
-delFV var fv fv_cand !in_scope acc =
-  fv fv_cand (extendVarSet in_scope var) acc
+delFV = bindTyVar
 {-# INLINE delFV #-}
 
 -- | Mark many free variables as not free.
 delFVs :: VarSet -> FV -> FV
-delFVs vars fv fv_cand !in_scope acc =
+delFVs vars (FV fv) = FV $ \fv_cand !in_scope acc ->
   fv fv_cand (in_scope `unionVarSet` vars) acc
 {-# INLINE delFVs #-}
-
--- | Filter a free variable computation.
-filterFV :: InterestingVarFun -> FV -> FV
-filterFV fv_cand2 fv fv_cand1 in_scope acc =
-  fv (\v -> fv_cand1 v && fv_cand2 v) in_scope acc
-{-# INLINE filterFV #-}
-
--- | Map a free variable computation over a list and union the results.
-mapUnionFV :: (a -> FV) -> [a] -> FV
-mapUnionFV _f [] _fv_cand _in_scope acc = acc
-mapUnionFV f (a:as) fv_cand in_scope acc =
-  mapUnionFV f as fv_cand in_scope $! f a fv_cand in_scope $! acc
-{-# INLINABLE mapUnionFV #-}
-
--- | Union many free variable computations.
-unionsFV :: [FV] -> FV
-unionsFV fvs fv_cand in_scope acc = mapUnionFV id fvs fv_cand in_scope acc
-{-# INLINE unionsFV #-}
-
--- | Add multiple variables - when free, to the returned free variables.
--- Ignores duplicates and respects the filtering function.
-mkFVs :: [Var] -> FV
-mkFVs vars fv_cand in_scope acc =
-  mapUnionFV unitFV vars fv_cand in_scope acc
-{-# INLINE mkFVs #-}
