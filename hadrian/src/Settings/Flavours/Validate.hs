ff09df321eced1299a886c820b4a90a09f21676f
Ben Gamari <ben@smart-cactus.org>
2019-07-26 16:49:36 -0400

    hadrian: Add `validate` and `slow validate` flavours

---
+++ hadrian/src/Settings/Flavours/Validate.hs
@@ -0,0 +1,38 @@
+module Settings.Flavours.Validate (validateFlavour, slowValidateFlavour) where
+
+import Expression
+import Flavour
+import {-# SOURCE #-} Settings.Default
+
+-- Please update doc/flavours.md when changing this file.
+validateFlavour :: Flavour
+validateFlavour = werror $ defaultFlavour
+    { name = "validate"
+    , args = defaultBuilderArgs <> validateArgs <> defaultPackageArgs
+    , libraryWays = pure [vanilla]
+    , rtsWays = pure [vanilla, threaded] }
+
+validateArgs :: Args
+validateArgs = sourceArgs SourceArgs
+    { hsDefault  = mconcat [ stage0 ? arg "-O0 -H64m"
+                             -- See #11487
+                           , notStage0 ? arg "-fllvm-fill-undef_with-garbage"
+                           ]
+    , hsLibrary  = pure ["-O", "-dcore-lint", "-dno-debug-output"]
+    , hsCompiler = mconcat [ stage0 ? arg "-O2 -DDEBUG"
+                           , notStage0 ? arg "-O -dcore-lint -dno-debug-output"
+                           ]
+    , hsGhc      = pure ["-O"] }
+
+slowValidateFlavour :: Flavour
+slowValidateFlavour = werror $ defaultFlavour
+    { name = "slow-validate"
+    , args = defaultBuilderArgs <> slowValidateArgs <> defaultPackageArgs
+    , libraryWays = pure [vanilla]
+    , rtsWays = pure [vanilla, threaded, profiling] }
+
+slowValidateArgs :: Args
+slowValidateArgs =
+  mconcat [ validateArgs
+          , notStage0 ? arg "-DDEBUG"
+          ]
