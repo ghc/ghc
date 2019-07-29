d99fe6050cb4c6c5ead24cbe4ed76cb0933db7a4
Ben Gamari <ben@smart-cactus.org>
2019-07-29 19:34:57 -0400

    Refactor, document, and optimize LLVM configuration loading
    As described in the new Note [LLVM Configuration] in SysTools, we now
    load llvm-targets and llvm-passes lazily to avoid the overhead of doing
    so when -fllvm isn't used (also known as "the common case").
    
    Noticed in #17003.

--- compiler/llvmGen/LlvmCodeGen.hs
+++ compiler/llvmGen/LlvmCodeGen.hs
@@ -90,12 +90,18 @@ llvmCodeGen' cmm_stream
   where
     header :: SDoc
     header = sdocWithDynFlags $ \dflags ->
-      let target = platformMisc_llvmTarget $ platformMisc dflags
-          layout = case lookup target (llvmTargets dflags) of
-            Just (LlvmTarget dl _ _) -> dl
-            Nothing -> error $ "Failed to lookup the datalayout for " ++ target ++ "; available targets: " ++ show (map fst $ llvmTargets dflags)
-      in     text ("target datalayout = \"" ++ layout ++ "\"")
-         $+$ text ("target triple = \"" ++ target ++ "\"")
+          text ("target datalayout = \"" ++ getDataLayout dflags target ++ "\"")
+      $+$ text ("target triple = \"" ++ target ++ "\"")
+      where target = platformMisc_llvmTarget $ platformMisc dflags
+
+    getDataLayout :: DynFlags -> String -> String
+    getDataLayout dflags target =
+      case lookup target (llvmTargets $ llvmConfig dflags) of
+        Just (LlvmTarget {lDataLayout=dl}) -> dl
+        Nothing -> pprPanic "Failed to lookup LLVM data layout" $
+                   text "Target:" <+> target $$
+                   hang (text "Available targets:") 4
+                        (vsep $ map (text . fst) $ llvmTargets $ llvmConfig dflags)
 
 llvmGroupLlvmGens :: RawCmmGroup -> LlvmM ()
 llvmGroupLlvmGens cmm = do
