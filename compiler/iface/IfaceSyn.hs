a75ffb96d86bebd7c2b581e25c228d52f503f1a0
Andreas Klebinger <klebinger.andreas@gmx.at>
2019-07-30 15:31:25 +0200

    Expose codegens view of exported ids in interfaces.
    The code generator collectes certain information about
    t push gitlab -f
    all ids inside LambdaFormInformation.
    
    This includes but is not limited to weither an id is:
    * A constructor and which.
    * A thunk
    * A function and it's arity.
    
    This has a few effects:
    * We can avoid redundant slow calls
      by always knowing the arity of ids.
    * We can avoid entering constructors since we can always infer
      their tag.
    
    This can have quite an extreme effect on thight loops, for example
    the CS benchmark showed 80% less allocations and 70% less
    instructions executed. Since it is just a tight loop which happens
    to benefit from this change.
    
    In general the effects are smaller, averaging a reduction of ~1%
    of instructions executed for nofib.
    
    Implementation wise we collect the information during the execution
    of codeGen and push back interface file generation when required in
    order to include the generated information.
    
    Compile time performance wise there is a non-trivial cost for single
    files with many dependencies (+3-5% allocations).
    However when using make mode this cost becomes insignificant if multiple
    modules are compiled at the same time as repeated inputs do not cause
    extra work. In practice this means when compiling something with 10+
    modules the difference in allocation is less than 0.5%.
    
    This implements #17004

--- compiler/iface/IfaceSyn.hs
+++ compiler/iface/IfaceSyn.hs
@@ -4,6 +4,7 @@
 -}
 
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module IfaceSyn (
         module IfaceType,
@@ -22,6 +23,9 @@ module IfaceSyn (
         IfaceTyConParent(..),
         IfaceCompleteMatch(..),
 
+        -- * CodeGen Information
+        IfLFInfo(..), IfStandardFormInfo(..),
+
         -- * Binding names
         IfaceTopBndr,
         putIfaceTopBndr, getIfaceTopBndr,
@@ -36,7 +40,11 @@ module IfaceSyn (
         -- Pretty printing
         pprIfaceExpr,
         pprIfaceDecl,
-        AltPpr(..), ShowSub(..), ShowHowMuch(..), showToIface, showToHeader
+        AltPpr(..), ShowSub(..), ShowHowMuch(..), showToIface, showToHeader,
+
+        -- Here to make it useable for the Outputable instance.
+        tcStandardFormInfo
+
     ) where
 
 #include "HsVersions.h"
@@ -69,9 +77,12 @@ import TyCon ( Role (..), Injectivity(..), tyConBndrVisArgFlag )
 import Util( dropList, filterByList, notNull, unzipWith )
 import DataCon (SrcStrictness(..), SrcUnpackedness(..))
 import Lexeme (isLexSym)
+import CgTypes
 
 import Control.Monad
 import System.IO.Unsafe
+import Data.Bits
+import Data.Word
 
 infixl 3 &&&
 
@@ -520,6 +531,79 @@ data IfaceLetBndr = IfLetBndr IfLclName IfaceType IfaceIdInfo IfaceJoinInfo
 
 data IfaceJoinInfo = IfaceNotJoinPoint
                    | IfaceJoinPoint JoinArity
+
+{-
+************************************************************************
+*                                                                      *
+                CodeGen Information
+*                                                                      *
+************************************************************************
+-}
+
+{- Note [Exported lambda form info]
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+We only export a subset of the full information, in particular:
+* LNE/Unknown is never exported, the former is not exported and the
+  later not possible as we know the form of all internal binds.
+* Not top level flag, all exported bindings are naturally top level binds.
+
+We do export:
+* RepArity: Allows us to directly jump into exactly saturated functions and
+  tag references to functions.
+* no_fvs: Used by nodeMustPointToIt. Might allow us to avoid assigning R1,
+* same for StandardFormInfo,
+* maybeFunction might allow us to avoid a slowcall in favour of jumping to
+  entry code.
+* The constructor (by name). Used to tag references.
+
+TODO:
+* It's not clear if it's better to rederive LFUnlifted from the type or
+  to simply cache it in the interface file.
+* Similar but slightly more complex for unary data cons, as we also have
+  to ensure there is an unfolding.
+-}
+
+newtype IfStandardFormInfo = IfStandardFormInfo Word16
+
+tcStandardFormInfo :: IfStandardFormInfo -> StandardFormInfo
+tcStandardFormInfo (IfStandardFormInfo w)
+  | testBit w 0 = NonStandardThunk
+  | otherwise = con field
+  where
+    field = fromIntegral (w `unsafeShiftR` 2)
+    con
+      | testBit w 1 = ApThunk
+      | otherwise = SelectorThunk
+
+instance Binary IfStandardFormInfo where
+    put_ bh (IfStandardFormInfo w) = put_ bh (w :: Word16)
+    get bh = IfStandardFormInfo <$> (get bh :: IO Word16)
+
+data IfLFInfo =
+    -- | We encode the fields bitwise:
+    --    bit 0: OneShotInfo
+    --    bit 1: no fvs
+    --    bit 2-15 arity
+    ILFReEntrant
+        OneShotInfo
+        RepArity
+        Bool -- True <=> no free fvs
+
+  -- | We encode some fields bitwise:
+    --    bit 0: no fvs
+    --    bit 1: updateable
+    --    bit 2: might be function
+  | ILFThunk             -- Thunk (zero arity)
+        (Bool,Bool,Bool) -- fvs, updtable, maybe a function
+        !IfStandardFormInfo
+
+
+  | ILFCon               -- A saturated constructor application
+        !Name            -- The constructor Name
+
+  | ILFUnlifted          -- A value of unboxed type;
+                        -- always a value, needs evaluation
 
 {-
 Note [Empty case alternatives]
@@ -1356,6 +1440,17 @@ instance Outputable IfaceUnfolding where
                                         pprParendIfaceExpr e]
   ppr (IfDFunUnfold bs es) = hang (text "DFun:" <+> sep (map ppr bs) <> dot)
                                 2 (sep (map pprParendIfaceExpr es))
+
+
+instance Outputable IfLFInfo where
+    ppr (ILFReEntrant oneshot rep fvs_flag) =
+        text "LFReEntrant" <+> ppr (oneshot, rep, fvs_flag)
+    ppr (ILFThunk fields sfi) =
+        text "LFThunk" <+> ppr fields <+> ppr (tcStandardFormInfo sfi)
+    ppr (ILFCon con) = text "LFCon" <> brackets (ppr con)
+    ppr (ILFUnlifted) = text "LFUnlifted"
+
+
 
 {-
 ************************************************************************
@@ -2360,3 +2455,30 @@ instance Binary IfaceTyConParent where
 instance Binary IfaceCompleteMatch where
   put_ bh (IfaceCompleteMatch cs ts) = put_ bh cs >> put_ bh ts
   get bh = IfaceCompleteMatch <$> get bh <*> get bh
+
+
+instance Binary IfLFInfo where
+    -- TODO: We could pack the bytes somewhat
+
+    put_ bh (ILFReEntrant oneshot rep fvs_flag) =
+        putByte bh 0 >>
+        put_ bh oneshot >>
+        put_ bh rep >>
+        put_ bh fvs_flag
+    put_ bh (ILFThunk encoded_flds sfi) = do
+        putByte bh 1
+        put_ bh (encoded_flds :: (Bool, Bool, Bool))
+        put_ bh sfi
+    put_ bh (ILFCon conName) =
+        putByte bh 2 >>
+        put_ bh conName
+    put_ bh (ILFUnlifted) =
+        putByte bh 3
+    get bh = do
+        tag <- getByte bh
+        case tag of
+            0 -> pure ILFReEntrant <*> get bh <*> get bh <*> get bh
+            1 -> pure ILFThunk <*> (get bh :: IO (Bool, Bool, Bool)) <*> (get bh :: IO IfStandardFormInfo)
+            2 -> pure ILFCon <*> get bh
+            3 -> pure ILFUnlifted
+            _ -> panic "Invalid byte"
