59505ae54f5a03284d2038487593670319e3abd7
Alejandro Serrano <trupill@gmail.com>
2019-08-06 14:31:44 +0200

    zonk everything after quick looking, fix checking for polytype in `uQuickLookTryFillPoly`

--- compiler/typecheck/TcMType.hs
+++ compiler/typecheck/TcMType.hs
@@ -66,7 +66,7 @@ module TcMType (
     zonkTcTyVar, zonkTcTyVars,
   zonkTcTyVarToTyVar, zonkTyVarTyVarPairs,
   zonkTyCoVarsAndFV, zonkTcTypeAndFV,
-  zonkTyCoVarsAndFVList,
+  zonkTyCoVarsAndFVList, zonkExpType,
   candidateQTyVarsOfType,  candidateQTyVarsOfKind,
   candidateQTyVarsOfTypes, candidateQTyVarsOfKinds,
   CandidatesQTvs(..), delCandidates, candidateKindVars,
@@ -1859,6 +1859,10 @@ zonkTyCoVarKind tv = do { kind' <- zonkTcType (tyVarKind tv)
 
 zonkTcTypes :: [TcType] -> TcM [TcType]
 zonkTcTypes tys = mapM zonkTcType tys
+
+zonkExpType :: ExpType -> TcM ExpType
+zonkExpType (Infer v) = return (Infer v)
+zonkExpType (Check t) = Check <$> zonkTcType t
 
 {-
 ************************************************************************
