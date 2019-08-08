3742b16955d38e6be0bfbf927836616318831d9f
Andreas Klebinger <klebinger.andreas@gmx.at>
2019-08-08 11:29:00 +0200

    All unknown

--- compiler/iface/TcIface.hs
+++ compiler/iface/TcIface.hs
@@ -1868,5 +1868,6 @@ tcLFInfo (ILFThunk (fvs_flag, upd_flag, fun_flag) sfi) = do
 tcLFInfo (ILFUnlifted) = pure $ LFUnlifted
 tcLFInfo (ILFCon conName) =
     forkM (text "Loading LFCon constructor:" <+> ppr conName) $ do
-      pprTraceM "Loading constructor:" (ppr conName)
-      LFCon <$> (tcIfaceDataCon conName)
+      pure $! LFUnknown True
+    --   pprTraceM "Loading constructor:" (ppr conName)
+    --   LFCon <$> (tcIfaceDataCon conName)
