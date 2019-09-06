7829b98e74c40c6d0733ece0b34198739f85ade2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-09-06 16:33:19 +0300

    Refactor bad coercion checking in a few places
    We do bad coercion checking in a few places in the compiler, but they
    all checked it differently:
    
    - CoreToStg.coreToStgArgs:
    
      Disallowed lifted-to-unlifted, disallowed changing prim reps even when
      the sizes are the same.
    
    - StgCmmExpr.cgCase:
    
      Checked primRepSlot equality. This disallowed Int to Int64 coercions
      on 64-bit systems (and Int to Int32 on 32-bit) even though those are
      fine.
    
    - CoreLint:
    
      Only place where we do this right. Full rules are explained in Note
      [Bad unsafe coercion].
    
    This patch implements the check explained in Note [Bad unsafe coercion]
    in CoreLint and uses it in CoreToStg.coreToStgArgs and
    StgCmmExpr.cgCase.
    
    This fixes #16952 and unblocks !1381 (which fixes #16893).
    
    This is the most conservative and correct change I came up with that
    fixes #16952.
    
    One remaining problem with coercion checking is that it's currently done
    in seemingly random places. What's special about CoreToStg.coreToStgArgs
    and StgCmmExpr.cgCase? My guess is that adding assertions to those
    places caught bugs before so we left assertions in those places. I think
    we should remove these assertions and do coercion checking in CoreLint
    and StgLint only (#17041).

--- compiler/types/TyCon.hs
+++ compiler/types/TyCon.hs
@@ -121,6 +121,8 @@ module TyCon(
         primRepSizeB,
         primElemRepSizeB,
         primRepIsFloat,
+        primRepsCompatible,
+        primRepCompatible,
 
         -- * Recursion breaking
         RecTcChecker, initRecTc, defaultRecTcMaxBound,
@@ -1422,7 +1424,7 @@ data PrimRep
   | FloatRep
   | DoubleRep
   | VecRep Int PrimElemRep  -- ^ A vector
-  deriving( Eq, Show )
+  deriving( Show )
 
 data PrimElemRep
   = Int8ElemRep
@@ -1451,6 +1453,21 @@ isGcPtrRep :: PrimRep -> Bool
 isGcPtrRep LiftedRep   = True
 isGcPtrRep UnliftedRep = True
 isGcPtrRep _           = False
+
+-- See Note [bad unsafe coercion] in CoreLint
+primRepCompatible :: DynFlags -> PrimRep -> PrimRep -> Bool
+primRepCompatible dflags rep1 rep2 =
+    (isUnboxed rep1 == isUnboxed rep2) &&
+    (primRepSizeB dflags rep1 == primRepSizeB dflags rep2) &&
+    (primRepIsFloat rep1 == primRepIsFloat rep2)
+  where
+    isUnboxed = not . isGcPtrRep
+
+-- See Note [bad unsafe coercion] in CoreLint
+primRepsCompatible :: DynFlags -> [PrimRep] -> [PrimRep] -> Bool
+primRepsCompatible dflags reps1 reps2 =
+    length reps1 == length reps2 &&
+    and (zipWith (primRepCompatible dflags) reps1 reps2)
 
 -- | The size of a 'PrimRep' in bytes.
 --
