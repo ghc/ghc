6cb09f8c7a31e48ea0d01f10171be895c38c5e08
Ben Gamari <ben@smart-cactus.org>
2019-09-19 10:27:06 -0400

    Define STAGE macro in platform.h

--- hadrian/src/Rules/Generate.hs
+++ hadrian/src/Rules/Generate.hs
@@ -237,6 +237,7 @@ generateGhcPlatformH = do
     return . unlines $
         [ "#if !defined(__GHCPLATFORM_H__)"
         , "#define __GHCPLATFORM_H__"
+        , "#define STAGE " ++ show (fromEnum stage + 1)
         , ""
         , "#define BuildPlatform_TYPE  " ++ cppify hostPlatform
         , "#define HostPlatform_TYPE   " ++ cppify targetPlatform
@@ -412,6 +413,7 @@ generateGhcBootPlatformH = do
     return $ unlines
         [ "#if !defined(__PLATFORM_H__)"
         , "#define __PLATFORM_H__"
+        , "#define STAGE " ++ show (fromEnum stage)
         , ""
         , "#define BuildPlatform_NAME  " ++ show buildPlatform
         , "#define HostPlatform_NAME   " ++ show hostPlatform
