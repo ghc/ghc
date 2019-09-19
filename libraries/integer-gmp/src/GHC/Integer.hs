c830ff219d213cd17419da8dd3fac834a3a1f9ee
Sylvain Henry <sylvain@haskus.fr>
2019-09-19 16:26:52 +0200

    ghc-bignum library
    ghc-bignum is a newer package that aims to replace the legacy
    integer-simple and integer-gmp packages.
    
    * it supports several backends. In particular GMP is still supported and
      most of the code from integer-gmp has been merged in the "gmp"
      backend.
    
    * the pure Haskell "native" backend is new and is much faster than the
      previous pure Haskell implementation provided by integer-simple
    
    * new backends are easier to write because they only have to provide a
      few well defined functions. All the other code is common to all
      backends. In particular they all share the efficient small/big number
      distinction previously used only in integer-gmp.
    
    * backends can all be tested against the "native" backend with a simple
      Cabal flag. Backends are only allowed to differ in performance, their
      results should be the same.
    
    * Add `integer-gmp` compat package: provide some pattern synonyms and
      function aliases for those in `ghc-bignum`. It is intended to avoid
      breaking packages that depend on `integer-gmp` internals.
    
    Update submodules: text, bytestring
    
    Metric Decrease:
        Conversions
        ManyAlternatives
        ManyConstructors
        Naperian
        T10359
        T10547
        T10678
        T12150
        T12227
        T12234
        T12425
        T13035
        T13719
        T14936
        T1969
        T4801
        T4830
        T5237
        T5549
        T5837
        T8766
        T9020
        parsing001
        space_leak_001
        T16190
    
    On ARM and i386, T17499 regresses (+6% > 5%).
    On x86_64 unregistered, T13701 sometimes regresses (+2.2% > 2%).
    
    Metric Increase:
        T17499
        T13701

--- libraries/integer-gmp/src/GHC/Integer.hs
+++ libraries/integer-gmp/src/GHC/Integer.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE MagicHash #-}
+{-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 #include "MachDeps.h"
@@ -24,7 +25,7 @@ module GHC.Integer (
     Integer,
 
     -- * Construct 'Integer's
-    mkInteger, smallInteger, wordToInteger,
+    smallInteger, wordToInteger,
 #if WORD_SIZE_IN_BITS < 64
     word64ToInteger, int64ToInteger,
 #endif
@@ -70,6 +71,212 @@ module GHC.Integer (
     hashInteger,
     ) where
 
-import GHC.Integer.Type
+import GHC.Num.Integer hiding (integerToWord,integerToInt)
+import GHC.Prim
+import GHC.Types
 
-default ()
+{-# DEPRECATED smallInteger "Use integerFromInt# instead" #-}
+smallInteger :: Int# -> Integer
+smallInteger = integerFromInt#
+
+{-# DEPRECATED integerToInt "Use integerToInt# instead" #-}
+integerToInt :: Integer -> Int#
+integerToInt = integerToInt#
+
+{-# DEPRECATED wordToInteger "Use integerFromWord# instead" #-}
+wordToInteger :: Word# -> Integer
+wordToInteger = integerFromWord#
+
+{-# DEPRECATED integerToWord "Use integerToWord# instead" #-}
+integerToWord :: Integer -> Word#
+integerToWord = integerToWord#
+
+
+
+#if WORD_SIZE_IN_BITS < 64
+
+{-# DEPRECATED word64ToInteger "Use integerFromWord64# instead" #-}
+word64ToInteger :: Word64# -> Integer
+word64ToInteger = integerFromWord64#
+
+{-# DEPRECATED integerToWord64 "Use integerToWord64# instead" #-}
+integerToWord64 :: Integer -> Word64#
+integerToWord64 = integerToWord64#
+
+{-# DEPRECATED int64ToInteger "Use integerFromInt64# instead" #-}
+int64ToInteger :: Int64# -> Integer
+int64ToInteger = integerFromInt64#
+
+{-# DEPRECATED integerToInt64 "Use integerToInt64# instead" #-}
+integerToInt64 :: Integer -> Int64#
+integerToInt64 = integerToInt64#
+
+#endif
+
+
+{-# DEPRECATED encodeFloatInteger "Use integerEncodeFloat# instead" #-}
+encodeFloatInteger :: Integer -> Int# -> Float#
+encodeFloatInteger = integerEncodeFloat#
+
+{-# DEPRECATED floatFromInteger "Use integerToFloat# instead" #-}
+floatFromInteger :: Integer -> Float#
+floatFromInteger = integerToFloat#
+
+{-# DEPRECATED encodeDoubleInteger "Use integerEncodeDouble# instead" #-}
+encodeDoubleInteger :: Integer -> Int# -> Double#
+encodeDoubleInteger = integerEncodeDouble#
+
+{-# DEPRECATED doubleFromInteger "Use integerToDouble# instead" #-}
+doubleFromInteger :: Integer -> Double#
+doubleFromInteger = integerToDouble#
+
+{-# DEPRECATED decodeDoubleInteger "Use integerDecodeDouble# instead" #-}
+decodeDoubleInteger :: Double# -> (# Integer, Int# #)
+decodeDoubleInteger = integerDecodeDouble#
+
+
+
+{-# DEPRECATED plusInteger "Use integerAdd instead" #-}
+plusInteger :: Integer -> Integer -> Integer
+plusInteger = integerAdd
+
+{-# DEPRECATED minusInteger "Use integerSub instead" #-}
+minusInteger :: Integer -> Integer -> Integer
+minusInteger = integerSub
+
+{-# DEPRECATED timesInteger "Use integerMul instead" #-}
+timesInteger :: Integer -> Integer -> Integer
+timesInteger = integerMul
+
+{-# DEPRECATED negateInteger "Use integerNegate instead" #-}
+negateInteger :: Integer -> Integer
+negateInteger = integerNegate
+
+{-# DEPRECATED absInteger "Use integerAbs instead" #-}
+absInteger :: Integer -> Integer
+absInteger = integerAbs
+
+{-# DEPRECATED signumInteger "Use integerSignum instead" #-}
+signumInteger :: Integer -> Integer
+signumInteger = integerSignum
+
+{-# DEPRECATED divModInteger "Use integerDivMod# instead" #-}
+divModInteger :: Integer -> Integer -> (# Integer, Integer #)
+divModInteger = integerDivMod#
+
+{-# DEPRECATED divInteger "Use integerDiv instead" #-}
+divInteger :: Integer -> Integer -> Integer
+divInteger = integerDiv
+
+{-# DEPRECATED modInteger "Use integerMod instead" #-}
+modInteger :: Integer -> Integer -> Integer
+modInteger = integerMod
+
+{-# DEPRECATED quotRemInteger "Use integerQuotRem# instead" #-}
+quotRemInteger :: Integer -> Integer -> (# Integer, Integer #)
+quotRemInteger = integerQuotRem#
+
+{-# DEPRECATED quotInteger "Use integerQuot instead" #-}
+quotInteger :: Integer -> Integer -> Integer
+quotInteger = integerQuot
+
+{-# DEPRECATED remInteger "Use integerRem instead" #-}
+remInteger :: Integer -> Integer -> Integer
+remInteger = integerRem
+
+
+{-# DEPRECATED eqInteger "Use integerEq instead" #-}
+eqInteger :: Integer -> Integer -> Bool
+eqInteger = integerEq
+
+{-# DEPRECATED neqInteger "Use integerNe instead" #-}
+neqInteger :: Integer -> Integer -> Bool
+neqInteger = integerNe
+
+{-# DEPRECATED leInteger "Use integerLe instead" #-}
+leInteger :: Integer -> Integer -> Bool
+leInteger = integerLe
+
+{-# DEPRECATED gtInteger "Use integerGt instead" #-}
+gtInteger :: Integer -> Integer -> Bool
+gtInteger = integerGt
+
+{-# DEPRECATED ltInteger "Use integerLt instead" #-}
+ltInteger :: Integer -> Integer -> Bool
+ltInteger = integerLt
+
+{-# DEPRECATED geInteger "Use integerGe instead" #-}
+geInteger :: Integer -> Integer -> Bool
+geInteger = integerGe
+
+{-# DEPRECATED compareInteger "Use integerCompare instead" #-}
+compareInteger :: Integer -> Integer -> Ordering
+compareInteger = integerCompare
+
+
+
+{-# DEPRECATED eqInteger# "Use integerEq# instead" #-}
+eqInteger# :: Integer -> Integer -> Int#
+eqInteger# = integerEq#
+
+{-# DEPRECATED neqInteger# "Use integerNe# instead" #-}
+neqInteger# :: Integer -> Integer -> Int#
+neqInteger# = integerNe#
+
+{-# DEPRECATED leInteger# "Use integerLe# instead" #-}
+leInteger# :: Integer -> Integer -> Int#
+leInteger# = integerLe#
+
+{-# DEPRECATED gtInteger# "Use integerGt# instead" #-}
+gtInteger# :: Integer -> Integer -> Int#
+gtInteger# = integerGt#
+
+{-# DEPRECATED ltInteger# "Use integerLt# instead" #-}
+ltInteger# :: Integer -> Integer -> Int#
+ltInteger# = integerLt#
+
+{-# DEPRECATED geInteger# "Use integerGe# instead" #-}
+geInteger# :: Integer -> Integer -> Int#
+geInteger# = integerGe#
+
+
+{-# DEPRECATED andInteger "Use integerAnd instead" #-}
+andInteger :: Integer -> Integer -> Integer
+andInteger = integerAnd
+
+{-# DEPRECATED orInteger "Use integerOr instead" #-}
+orInteger :: Integer -> Integer -> Integer
+orInteger = integerOr
+
+{-# DEPRECATED xorInteger "Use integerXor instead" #-}
+xorInteger :: Integer -> Integer -> Integer
+xorInteger = integerXor
+
+{-# DEPRECATED complementInteger "Use integerComplement instead" #-}
+complementInteger :: Integer -> Integer
+complementInteger = integerComplement
+
+{-# DEPRECATED shiftLInteger "Use integerShiftL# instead" #-}
+shiftLInteger :: Integer -> Int# -> Integer
+shiftLInteger n i = integerShiftL# n (int2Word# i)
+
+{-# DEPRECATED shiftRInteger "Use integerShiftR# instead" #-}
+shiftRInteger :: Integer -> Int# -> Integer
+shiftRInteger n i = integerShiftR# n (int2Word# i)
+
+{-# DEPRECATED testBitInteger "Use integerTestBit# instead" #-}
+testBitInteger :: Integer -> Int# -> Bool
+testBitInteger n i = isTrue# (integerTestBit# n (int2Word# i))
+
+{-# DEPRECATED hashInteger "Use integerToInt# instead" #-}
+hashInteger :: Integer -> Int#
+hashInteger = integerToInt#
+
+{-# DEPRECATED bitInteger "Use integerBit# instead" #-}
+bitInteger :: Int# -> Integer
+bitInteger i = integerBit# (int2Word# i)
+
+{-# DEPRECATED popCountInteger "Use integerPopCount# instead" #-}
+popCountInteger :: Integer -> Int#
+popCountInteger = integerPopCount#
+
