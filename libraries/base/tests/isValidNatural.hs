6bd40cf944281db1b5fcb48c653f084b2c341319
Sylvain Henry <sylvain@haskus.fr>
2019-09-19 16:26:52 +0200

    Implement ghc-bignum
    Metric Decrease:
        T12150
        T10359
        T10678
        T12234
        T1969
        parsing001
        T9020
        T10547
        T13719
        space_leak_001
        T4801
        T12227
        ManyAlternatives
        ManyConstructors
        T4830
        T5237
        Conversions
        T5549
        T14936
        T8766

--- libraries/base/tests/isValidNatural.hs
+++ libraries/base/tests/isValidNatural.hs
@@ -1,9 +1,10 @@
 {-# language MagicHash #-}
 
-import GHC.Integer.GMP.Internals
-import GHC.Natural
+import GHC.Num.Natural
+import GHC.Num.BigNat
+import GHC.Exts
 
-main = print $ map isValidNatural [0, 1, maxWord, maxWord + 1, invalid]
+main = print $ map naturalCheck [0, 1, maxWord, maxWord + 1, invalid]
   where
     maxWord = fromIntegral (maxBound :: Word)
-    invalid = NatJ# oneBigNat -- 1 would fit into the NatS# constructor.
+    invalid = NB (bigNatOne void#) -- 1 would fit into the NS constructor.
