2654d10ff4215cc76ec5c68612ffd60b6f60406f
Gabor Greif <ggreif@gmail.com>
2019-09-21 23:07:35 +0200

    add test for exhibiting the double switch scheme

---
+++ testsuite/tests/codeGen/should_compile/T14373c.hs
@@ -0,0 +1,9 @@
+data BigFam = A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P
+
+{-# NOINLINE mixedSwitch #-}
+mixedSwitch A = True
+mixedSwitch B = False
+mixedSwitch C = False
+mixedSwitch P = True
+
+main = print $ mixedSwitch B
