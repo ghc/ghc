39f8872013b85ca2a100508eaee9f55ec448d376
Ben Gamari <ben@smart-cactus.org>
2019-09-23 12:11:14 -0400

    testsuite: Make ExplicitForAllRules1 more robust
    Previously the test relied on `id` not inlining. Fix this.

--- testsuite/tests/rename/should_compile/ExplicitForAllRules1.hs
+++ testsuite/tests/rename/should_compile/ExplicitForAllRules1.hs
@@ -14,20 +14,24 @@ import Data.Kind
 
 {-# RULES
 "example"  forall a b. forall. map @a @b f = f
-"example2" forall a. forall (x :: a). id x = x
+"example2" forall a. forall (x :: a). id' x = x
   #-}
 
 {-# NOINLINE f #-}
 f :: a -> b
 f = undefined
 
+id' :: a -> a
+id' x = x
+{-# NOINLINE id' #-}
+
 -- More tests
 
 {-# RULES
 "example3" forall (a :: Type -> Type) (b :: a Int) c. forall x y. g @(Proxy b) @(Proxy c) x y = ()
-"example4" forall (a :: Bool) (b :: Proxy a). forall x. g @(Proxy b) @() x = id @()
-"example5" forall (a :: Type). forall. h @a = id @a
-"example5" forall k (c :: k). forall (x :: Proxy c). id @(Proxy c) x = x
+"example4" forall (a :: Bool) (b :: Proxy a). forall x. g @(Proxy b) @() x = id' @()
+"example5" forall (a :: Type). forall. h @a = id' @a
+"example5" forall k (c :: k). forall (x :: Proxy c). id' @(Proxy c) x = x
   #-}
 
 {-# NOINLINE g #-}
@@ -42,4 +46,4 @@ h x = x
 {-# RULES "example6" forall a forall. g a forall = () #-}
 
 -- Should generate a warning
-{-# RULES "example7" forall a b. forall (x :: a). id x = x #-}
+{-# RULES "example7" forall a b. forall (x :: a). id' x = x #-}
