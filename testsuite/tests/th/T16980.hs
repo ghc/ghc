3288d8971ec743cecf94e39c0a248ed72a097555
Richard Eisenberg <rae@richarde.dev>
2019-09-26 14:31:30 +0100

    Solve constraints from top-level groups sooner
    Previously, all constraints from all top-level groups (as
    separated by top-level splices) were lumped together and solved
    at the end. This could leak metavariables to TH, though, and
    that's bad. This patch solves each group's constraints before
    running the next group's splice.
    
    Fixes #16980
    
    Test cases: th/T16980{,a}

---
+++ testsuite/tests/th/T16980.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module T16980 where
+
+import Language.Haskell.TH
+import Language.Haskell.TH.Ppr
+
+aNumber = 5
+
+do VarI name1 t1 _ <- reify 'aNumber
+   runIO . print $ ppr_sig name1 t1
+   runIO . print =<< reifyType 'aNumber
+   return []
