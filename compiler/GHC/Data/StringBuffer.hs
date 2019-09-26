acf744d7775635d6a502dcc92ed059b59a96ba9f
Daniel Gröber <dxld@darkboxed.org>
2019-09-26 20:57:11 +0200

    Use ShortByteString for FastString
    There are multiple reasons we want this:
    
    - Fewer allocations: ByteString has 3 fields, ShortByteString just has one.
    - ByteString memory is pinned:
      - This can cause fragmentation issues (see for example #13110) but also
      - makes using FastStrings in compact regions impossible.
    
    Metric Decrease:
        T5837
        T12150
        T12234
        T12425

--- compiler/GHC/Data/StringBuffer.hs
+++ compiler/GHC/Data/StringBuffer.hs
@@ -200,7 +200,7 @@ nextChar (StringBuffer buf len (I# cur#)) =
   -- Getting our fingers dirty a little here, but this is performance-critical
   inlinePerformIO $ do
     withForeignPtr buf $ \(Ptr a#) -> do
-        case utf8DecodeChar# (a# `plusAddr#` cur#) of
+        case utf8DecodeCharAddr# (a# `plusAddr#` cur#) of
           (# c#, nBytes# #) ->
              let cur' = I# (cur# +# nBytes#) in
              return (C# c#, StringBuffer buf len cur')
