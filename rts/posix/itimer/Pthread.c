0be2cb75899832ff832475ee55cb24ba72a3771b
Ben Gamari <ben@smart-cactus.org>
2019-09-27 01:51:39 +0000

    rts: Annotate benign race in ticker shutdown logic
    We are guaranteed to see the change in the `stopped` flag at some point
    so the race is harmless.

--- rts/posix/itimer/Pthread.c
+++ rts/posix/itimer/Pthread.c
@@ -119,6 +119,9 @@ static void *itimer_thread_func(void *_handle_tick)
     }
 #endif
 
+    // Benign race: If we don't see that exited was set in one iteration we will
+    // see it next time.
+    TSAN_ANNOTATE_BENIGN_RACE(&exited, "itimer_thread_func");
     while (!exited) {
         if (USE_TIMERFD_FOR_ITIMER) {
             if (read(timerfd, &nticks, sizeof(nticks)) != sizeof(nticks)) {
@@ -133,6 +136,7 @@ static void *itimer_thread_func(void *_handle_tick)
         }
 
         // first try a cheap test
+        TSAN_ANNOTATE_BENIGN_RACE(&stopped, "itimer_thread_func");
         if (stopped) {
             OS_ACQUIRE_LOCK(&mutex);
             // should we really stop?
