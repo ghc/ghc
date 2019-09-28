267ce63c73eb00a98fa73554da012e5fdaf2f056
Ben Gamari <ben@smart-cactus.org>
2019-09-28 17:36:00 +0000

    GC

--- includes/rts/storage/GC.h
+++ includes/rts/storage/GC.h
@@ -242,7 +242,7 @@ extern bool keepCAFs;
 
 INLINE_HEADER void initBdescr(bdescr *bd, generation *gen, generation *dest)
 {
-    bd->gen     = gen;
-    bd->gen_no  = gen->no;
-    bd->dest_no = dest->no;
+    RELAXED_STORE(&bd->gen, gen);
+    RELAXED_STORE(&bd->gen_no, gen->no);
+    RELAXED_STORE(&bd->dest_no, dest->no);
 }
