ec46c83de8b35c84146dcc4b80891aa408ef2787
Ben Gamari <ben@smart-cactus.org>
2019-10-02 23:29:02 -0400

    rts: Fix CNF dirtying logic
    Previously due to a silly implementation bug CNFs would never have their
    dirty flag set, resulting in their being added again and again to the
    `mut_list`. Fix this.
    
    Fixes #17297.
    
    (cherry picked from commit ba27fe740d4b141d2a2fa209f98a0946cb80f379)

--- rts/sm/CNF.c
+++ rts/sm/CNF.c
@@ -542,9 +542,9 @@ insertCompactHash (Capability *cap,
                    StgClosure *p, StgClosure *to)
 {
     insertHashTable(str->hash, (StgWord)p, (const void*)to);
-    const StgInfoTable *strinfo = str->header.info;
-    if (strinfo == &stg_COMPACT_NFDATA_CLEAN_info) {
-        strinfo = &stg_COMPACT_NFDATA_DIRTY_info;
+    const StgInfoTable **strinfo = &str->header.info;
+    if (*strinfo == &stg_COMPACT_NFDATA_CLEAN_info) {
+        *strinfo = &stg_COMPACT_NFDATA_DIRTY_info;
         recordClosureMutated(cap, (StgClosure*)str);
     }
 }
