0c6a6966a88bd7296c649ff9028846d0874b33aa
Ryan Scott <ryan.gl.scott@gmail.com>
2019-10-03 18:40:30 -0400

    Reify oversaturated data family instances correctly (#17296)
    `TcSplice` was not properly handling oversaturated data family
    instances, such as the example in #17296, as it dropped arguments due
    to carelessly zipping data family instance arguments with
    `tyConTyVars`. For data families, the number of `tyConTyVars` can
    sometimes be less than the number of arguments it can accept in a
    data family instance due to the fact that data family instances can
    be oversaturated.
    
    To account for this, `TcSplice.mkIsPolyTvs` has now been renamed to
    `tyConArgsPolyKinded` and now factors in `tyConResKind` in addition
    to `tyConTyVars`. I've also added
    `Note [Reified instances and explicit kind signatures]` which
    explains the various subtleties in play here.
    
    Fixes #17296.

--- compiler/types/Type.hs
+++ compiler/types/Type.hs
@@ -1775,7 +1775,6 @@ tyCoBinderVar_maybe (Named tv) = Just $ binderVar tv
 tyCoBinderVar_maybe _          = Nothing
 
 tyCoBinderType :: TyCoBinder -> Type
--- Barely used
 tyCoBinderType (Named tvb) = binderType tvb
 tyCoBinderType (Anon _ ty) = ty
 
