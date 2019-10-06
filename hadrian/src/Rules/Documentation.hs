c80b0a6c1e64c94ac1c7a2d7a970319b745f0ae7
Ben Gamari <ben@smart-cactus.org>
2019-10-06 14:47:47 -0400

    gitlab-ci: Check coverage of GHC flags in users guide
    This ensures that all GHC flags are documented during the documentation
    build.
    
    Fixes #17315.

--- hadrian/src/Rules/Documentation.hs
+++ hadrian/src/Rules/Documentation.hs
@@ -16,6 +16,7 @@ import Context
 import Expression (getContextData, interpretInContext, (?), package)
 import Flavour
 import Oracles.ModuleFiles
+import Oracles.Setting (topDirectory)
 import Packages
 import Settings
 import Target
@@ -111,6 +112,11 @@ documentationRules = do
 
         need $ map (root -/-) targets
 
+        when (SphinxPDFs `Set.member` doctargets)
+          $ checkUserGuideFlags $ pdfRoot -/- "users_guide" -/- "ghc-flags.txt"
+        when (SphinxHTML `Set.member` doctargets)
+          $ checkUserGuideFlags $ htmlRoot -/- "users_guide" -/- "ghc-flags.txt"
+
     where archiveTarget "libraries"   = Haddocks
           archiveTarget _             = SphinxHTML
 
@@ -123,6 +129,17 @@ checkSphinxWarnings out = do
     log <- liftIO $ readFile (out -/- ".log")
     when ("reference target not found" `isInfixOf` log)
       $ fail "Undefined reference targets found in Sphinx log."
+
+-- | Check that all GHC flags are documented in the users guide.
+checkUserGuideFlags :: FilePath -> Action ()
+checkUserGuideFlags documentedFlagList = do
+    scriptPath <- (</> "docs/user_guide/compare-flags.py") <$> topDirectory
+    ghcPath <- (</>) <$> topDirectory <*> programPath (vanillaContext Stage1 ghc)
+    runBuilder Python
+      [ scriptPath
+      , "--doc-flags", documentedFlagList
+      , "--ghc", ghcPath
+      ] [documentedFlagList] []
 
 
 ------------------------------------- HTML -------------------------------------
