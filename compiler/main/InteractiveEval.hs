c5cfebae19b99e7bb8293f585a82d47fb476ed97
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-10-10 13:28:21 +0300

    Fix warnings

--- compiler/main/InteractiveEval.hs
+++ compiler/main/InteractiveEval.hs
@@ -103,7 +103,6 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import StringBuffer (stringToStringBuffer)
 import Control.Monad
-import GHC.Exts
 import Data.Array
 import Exception
 import Unsafe.Coerce
