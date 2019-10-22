2d12d59ec989d0f174f5f15e0a92cb1a9cc017e4
Ben Gamari <ben@smart-cactus.org>
2019-10-22 00:00:20 -0400

    Drop duplicate -optl's from GHC invocations
    Previously the make build system would pass things like
    `-optl-optl-Wl,-x -optl-optl-Wl,noexecstack` to GHC. This would
    naturally result in mass confusion as GHC would pass `-optl-Wl,-x` to
    GCC. GCC would in turn interpret this as `-o ptl-Wl,-x`, setting the
    output pass of the invocation.
    
    The problem that `-optl` was added to the command-line in two places in
    the build system. Fix this.
    
    Fixes #17385.

--- rules/distdir-way-opts.mk
+++ rules/distdir-way-opts.mk
@@ -186,8 +186,8 @@ $1_$2_$3_ALL_LD_OPTS = \
  $$($1_$2_$3_LD_OPTS) \
  $$($1_$2_EXTRA_LD_OPTS) \
  $$(EXTRA_LD_OPTS) \
- $$(foreach o,$$(EXTRA_LD_LINKER_OPTS),-optl-Wl$$(comma)$$o) \
- $$(foreach o,$$(CONF_LD_LINKER_OPTS_STAGE$4),-optl-Wl$$(comma)$$o)
+ $$(foreach o,$$(EXTRA_LD_LINKER_OPTS),-Wl$$(comma)$$o) \
+ $$(foreach o,$$(CONF_LD_LINKER_OPTS_STAGE$4),-Wl$$(comma)$$o)
 
 # Options for passing to GHC when we use it for linking
 $1_$2_$3_GHC_LD_OPTS = \
