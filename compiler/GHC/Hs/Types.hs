2828b4c08db217cda8d0ff10935eef8053173195
Ryan Scott <ryan.gl.scott@gmail.com>
2019-10-24 13:52:36 -0400

    Clean up TH's treatment of unary tuples (or, #16881 part two)
    !1906 left some loose ends in regards to Template Haskell's treatment
    of unary tuples. This patch ends to tie up those loose ends:
    
    * In addition to having `TupleT 1` produce unary tuples, `TupE [exp]`
      and `TupP [pat]` also now produce unary tuples.
    * I have added various special cases in GHC's pretty-printers to
      ensure that explicit 1-tuples are printed using the `Unit` type.
      See `testsuite/tests/th/T17380`.
    * The GHC 8.10.1 release notes entry has been tidied up a little.
    
    Fixes #16881. Fixes #17371. Fixes #17380.

--- compiler/GHC/Hs/Types.hs
+++ compiler/GHC/Hs/Types.hs
@@ -85,6 +85,7 @@ import RdrName ( RdrName )
 import DataCon( HsSrcBang(..), HsImplBang(..),
                 SrcStrictness(..), SrcUnpackedness(..) )
 import TysPrim( funTyConName )
+import TysWiredIn( mkTupleStr )
 import Type
 import GHC.Hs.Doc
 import BasicTypes
@@ -1600,7 +1601,12 @@ ppr_mono_ty (HsTyVar _ prom (L _ name))
   | isPromoted prom = quote (pprPrefixOcc name)
   | otherwise       = pprPrefixOcc name
 ppr_mono_ty (HsFunTy _ ty1 ty2)   = ppr_fun_ty ty1 ty2
-ppr_mono_ty (HsTupleTy _ con tys) = tupleParens std_con (pprWithCommas ppr tys)
+ppr_mono_ty (HsTupleTy _ con tys)
+  | [ty] <- tys
+  , BoxedTuple <- std_con
+  = sep [text (mkTupleStr Boxed 1), ppr_mono_lty ty]
+  | otherwise
+  = tupleParens std_con (pprWithCommas ppr tys)
   where std_con = case con of
                     HsUnboxedTuple -> UnboxedTuple
                     _              -> BoxedTuple
