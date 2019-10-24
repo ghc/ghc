6158a2e97b1c4484ce12a0a78ae0399ebb104edf
Ryan Scott <ryan.gl.scott@gmail.com>
2019-10-24 13:52:36 -0400

    Clean up TH's treatment of unary tuples (or, #16881 part two)
    !1906 left some loose ends in regards to Template Haskell's treatment
    of unary tuples. This patch ends to tie up those loose ends:
    
    * In addition to having `TupleT 1` produce unary tuples, `TupE [exp]`
      and `TupP [pat]` also now produce unary tuples.
    * I have added various special cases in GHC's pretty-printers to
      ensure that explicit 1-tuples are printed using the `Unit` type.
      See `testsuite/tests/th/T17380`.
    * The GHC 8.10.1 release notes entry has been tidied up a little.
    
    Fixes #16881. Fixes #17371. Fixes #17380.

--- compiler/GHC/Hs/Expr.hs
+++ compiler/GHC/Hs/Expr.hs
@@ -13,6 +13,7 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- | Abstract Haskell syntax for expressions.
 module GHC.Hs.Expr where
@@ -43,6 +44,7 @@ import Util
 import Outputable
 import FastString
 import Type
+import TysWiredIn (mkTupleStr)
 import TcType (TcType)
 import {-# SOURCE #-} TcRnTypes (TcLclEnv)
 
@@ -908,6 +910,12 @@ ppr_expr (SectionR _ op expr)
     pp_infixly v = sep [v, pp_expr]
 
 ppr_expr (ExplicitTuple _ exprs boxity)
+    -- Special-case unary boxed tuples so that they are pretty-printed as
+    -- `Unit x`, not `(x)`
+  | [dL -> L _ (Present _ expr)] <- exprs
+  , Boxed <- boxity
+  = hsep [text (mkTupleStr Boxed 1), ppr expr]
+  | otherwise
   = tupleParens (boxityTupleSort boxity) (fcat (ppr_tup_args $ map unLoc exprs))
   where
     ppr_tup_args []               = []
