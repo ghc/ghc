d7ba8def3e1ee74c45ef5d359a3df2cacf39c1d5
Sebastian Graf <sgraf1337@gmail.com>
2019-11-03 19:42:52 +0000

    Implement the UnliftedDatatypes extension
    GHC Proposal: 0265-unlifted-datatypes.rst
    Discussion: https://github.com/ghc-proposals/ghc-proposals/pull/265
    Issues: None yet. TODO?
    Implementation Details: Note [Implementation of UnliftedDatatypes]
    
    This patch introduces the -XUnliftedDatatypes extension. When this extension is
    enabled, GHC relaxes the restrictions around what result kinds are allowed in
    data declarations. This allows data types for which an unlifted or
    levity-polymorphic result kind is inferred.
    
    The most significant changes are in `GHC.Tc.TyCl`, where
    `Note [Implementation of UnliftedDatatypes]` describes the details of the
    implementation.

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -3567,6 +3567,7 @@ xFlagsDeps = [
   flagSpec "UndecidableSuperClasses"          LangExt.UndecidableSuperClasses,
   flagSpec "UnicodeSyntax"                    LangExt.UnicodeSyntax,
   flagSpec "UnliftedFFITypes"                 LangExt.UnliftedFFITypes,
+  flagSpec "UnliftedDatatypes"                LangExt.UnliftedDatatypes,
   flagSpec "UnliftedNewtypes"                 LangExt.UnliftedNewtypes,
   flagSpec "ViewPatterns"                     LangExt.ViewPatterns
   ]
@@ -3749,6 +3750,11 @@ impliedXFlags
 
     , (LangExt.TemplateHaskell, turnOn, LangExt.TemplateHaskellQuotes)
     , (LangExt.Strict, turnOn, LangExt.StrictData)
+
+    -- What writing @data T :: GHC.Types.UnliftedType where T :: T@ needs
+    , (LangExt.UnliftedDatatypes, turnOn, LangExt.GADTSyntax)
+    , (LangExt.UnliftedDatatypes, turnOn, LangExt.DataKinds)
+    , (LangExt.UnliftedDatatypes, turnOn, LangExt.KindSignatures)
   ]
 
 -- Note [When is StarIsType enabled]
