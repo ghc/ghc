f299ed573c8b560b8ef7e10ba16d598a1a479b41
Simon Peyton Jones <simonpj@microsoft.com>
2019-11-08 17:49:35 +0000

    Re-engineer the binder-swap transformation
    The binder-swap transformation is implemented by the occurrence
    analyser -- see Note [Binder swap] in OccurAnal. However it had
    a very nasty corner in it, for the case where the case scrutinee
    was a GlobalId.  This led to trouble and hacks, and ultimately
    to #16296.
    
    This patch re-engineers how the occurrence analyser implements
    the binder-swap, by actually carrying out a substitution rather
    than by adding a let-binding.  It's all described in
    Note [The binder-swap substitution].
    
    I did a few other things along the way
    
    * Fix a bug in StgCse, which could allow a loop breaker to be CSE'd
      away.  See Note [Care with loop breakers] in StgCse.  I think it can
      only show up if occurrence analyser sets up bad loop breakers, but
      still.
    
    * Better commenting in SimplUtils.prepareAlts
    
    * A little refactoring in CoreUnfold; nothing significant
      e.g. rename CoreUnfold.mkTopUnfolding to mkFinalUnfolding
    
    * Renamed CoreSyn.isFragileUnfolding to hasCoreUnfolding
    
    * Move mkRuleInfo to CoreFVs
    
    There's a 4.6% metric decrease here:
    
    Metric Decrease:
        T9961

--- testsuite/tests/simplCore/should_compile/T7360.hs
+++ testsuite/tests/simplCore/should_compile/T7360.hs
@@ -6,7 +6,7 @@ module T7360 where
 import GHC.List as L
 
 data Foo = Foo1 | Foo2 | Foo3 !Int
-    
+
 fun1 :: Foo -> ()
 {-# NOINLINE fun1 #-}
 fun1 x = case x of
@@ -14,7 +14,7 @@ fun1 x = case x of
                Foo2 -> ()
                Foo3 {} -> ()
 
-fun2 x = (fun1 Foo1,  -- Keep -ddump-simpl output 
+fun2 x = (fun1 Foo1,  -- Keep -ddump-simpl output
                       -- in a predictable order
          case x of
           [] -> L.length x
