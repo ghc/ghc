337b2c49d07eb11c2e6c380efeb4cdbe2f40c116
Gabor Greif <ggreif@gmail.com>
2019-11-08 14:31:50 +0100

    heed Matt's hunch

--- compiler/GHC/StgToCmm/Expr.hs
+++ compiler/GHC/StgToCmm/Expr.hs
@@ -638,7 +638,9 @@ cgAlts gc_plan bndr (AlgAlt tycon) alts
               ptag_expr = cmmConstrTag1 dflags (CmmReg bndr_reg)
               branches' = first succ <$> branches
               maxpt = mAX_PTR_TAG dflags
-              (via_ptr, via_info) = partition ((< maxpt) . fst) branches'
+              (via_ptr, via_info) = if all ((< maxpt) . fst) branches'
+                                    then (branches', [])
+                                    else partition ((< maxpt) . fst) branches'
               small = isSmallFamily dflags fam_sz
 
                 -- Is the constructor tag in the node reg?
