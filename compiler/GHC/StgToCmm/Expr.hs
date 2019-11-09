849c625259c70150126539bff2a99df08b044e1e
Gabor Greif <ggreif@gmail.com>
2019-11-09 23:29:56 +0100

    use better partition

--- compiler/GHC/StgToCmm/Expr.hs
+++ compiler/GHC/StgToCmm/Expr.hs
@@ -51,7 +51,6 @@ import Outputable
 
 import Control.Monad ( unless, void )
 import Control.Arrow ( first )
-import Data.List     ( partition )
 
 ------------------------------------------------------------------------
 --              cgExpr: the main function
@@ -602,6 +601,15 @@ chooseReturnBndrs bndr PolyAlt _alts
 chooseReturnBndrs _ _ _ = panic "chooseReturnBndrs"
                              -- MultiValAlt has only one alternative
 
+-- space conscious partition
+partition_ :: (a -> Bool) -> [a] -> ([a], [a])
+partition_ _ [] = ([], [])
+partition_ p l@(a : as)
+    | (y, n) <- partition_ p as =
+                if p a
+                then (if null n then l else a : y, n)
+                else (y, if null y then l else a : n)
+
 -------------------------------------
 cgAlts :: (GcPlan,ReturnKind) -> NonVoid Id -> AltType -> [CgStgAlt]
        -> FCode ReturnKind
@@ -651,7 +659,7 @@ cgAlts gc_plan bndr (AlgAlt tycon) alts
            else -- No, the get exact tag from info table when mAX_PTR_TAG
                 -- See Note [Double switching for big families]
               do
-                let (via_ptr, via_info) = partition low_tag branches'
+                let (via_ptr, via_info) = partition_ low_tag branches'
                     untagged_ptr = cmmUntag dflags (CmmReg bndr_reg)
                     itag_expr = getConstrTag dflags untagged_ptr
                     info0 = first pred <$> via_info
