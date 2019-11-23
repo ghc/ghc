36550b69370c5123bdbdd96b4ac7a601f4fb9202
Tamar Christina <tamar@zhox.com>
2019-11-23 00:03:19 +0000

    winio: Implement new tempfile routines for winio

--- libraries/base/System/IO.hs
+++ libraries/base/System/IO.hs
@@ -232,6 +232,12 @@ import Foreign.C.String
 import Foreign.Ptr
 import Foreign.Marshal.Alloc
 import Foreign.Storable
+import GHC.IO.SubSystem
+import GHC.IO.Windows.Handle (openFileAsTemp)
+import GHC.IO.Handle.Windows (mkHandleFromHANDLE)
+import GHC.IO.Device as IODevice
+import GHC.Real (fromIntegral)
+import Foreign.Marshal.Utils (new)
 #endif
 import Foreign.C.Types
 import System.Posix.Internals
@@ -530,13 +536,29 @@ openTempFile' loc tmp_dir template binary mode
          -- beginning with '.' as the second component.
          _                      -> errorWithoutStackTrace "bug in System.IO.openTempFile"
 #if defined(mingw32_HOST_OS)
-    findTempName = do
+    findTempName = findTempNamePosix <!> findTempNameWinIO
+
+    findTempNameWinIO = do
+      let label = if null prefix then "ghc" else prefix
+      withCWString tmp_dir $ \c_tmp_dir ->
+        withCWString label $ \c_template ->
+          withCWString suffix $ \c_suffix -> do
+            c_ptr <- new nullPtr
+            res <- c_createUUIDTempFileErrNo c_tmp_dir c_template c_suffix
+                                             c_ptr
+            if not res
+               then do errno <- getErrno
+                       ioError (errnoToIOError loc errno Nothing (Just tmp_dir))
+               else do c_p <- peek c_ptr
+                       filename <- peekCWString c_p
+                       free c_p
+                       handleResultsWinIO filename ((fromIntegral mode .&. o_EXCL) == o_EXCL)
+
+    findTempNamePosix = do
       let label = if null prefix then "ghc" else prefix
       withCWString tmp_dir $ \c_tmp_dir ->
         withCWString label $ \c_template ->
           withCWString suffix $ \c_suffix ->
-            -- FIXME: revisit this when new I/O manager in place and use a UUID
-            --       based one when we are no longer MAX_PATH bound.
             allocaBytes (sizeOf (undefined :: CWchar) * 260) $ \c_str -> do
             res <- c_getTempFileNameErrorNo c_tmp_dir c_template c_suffix 0
                                             c_str
@@ -544,9 +566,9 @@ openTempFile' loc tmp_dir template binary mode
                then do errno <- getErrno
                        ioError (errnoToIOError loc errno Nothing (Just tmp_dir))
                else do filename <- peekCWString c_str
-                       handleResults filename
+                       handleResultsPosix filename
 
-    handleResults filename = do
+    handleResultsPosix filename = do
       let oflags1 = rw_flags .|. o_EXCL
           binary_flags
               | binary    = o_BINARY
@@ -567,8 +589,20 @@ openTempFile' loc tmp_dir template binary mode
 
              return (filename, h)
 
+    handleResultsWinIO filename excl = do
+      (hwnd, hwnd_type) <- openFileAsTemp filename True excl
+      mb_codec <- if binary then return Nothing else fmap Just getLocaleEncoding
+
+      -- then use it to make a Handle
+      h <- mkHandleFromHANDLE hwnd hwnd_type filename ReadWriteMode mb_codec
+                `onException` IODevice.close hwnd
+      return (filename, h)
+
 foreign import ccall "getTempFileNameErrorNo" c_getTempFileNameErrorNo
   :: CWString -> CWString -> CWString -> CUInt -> Ptr CWchar -> IO Bool
+
+foreign import ccall "__createUUIDTempFileErrNo" c_createUUIDTempFileErrNo
+  :: CWString -> CWString -> CWString -> Ptr CWString -> IO Bool
 
 pathSeparator :: String -> Bool
 pathSeparator template = any (\x-> x == '/' || x == '\\') template
