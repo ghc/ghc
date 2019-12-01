7d55449863ca52c1819e47c921aa441c71601bc9
Ben Gamari <ben@smart-cactus.org>
2019-12-01 11:52:33 -0500

    rts/SpinLock: Move to proper atomics
    This is fairly straightforward; we just needed to use relaxed operations
    for the PROF_SPIN counters and a release store instead of a write
    barrier.

--- includes/rts/SpinLock.h
+++ includes/rts/SpinLock.h
@@ -47,10 +47,10 @@ INLINE_HEADER void ACQUIRE_SPIN_LOCK(SpinLock * p)
         for (i = 0; i < SPIN_COUNT; i++) {
             r = cas((StgVolatilePtr)&(p->lock), 1, 0);
             if (r != 0) return;
-            p->spin++;
+            IF_PROF_SPIN(__atomic_fetch_add(&p->spin, 1, __ATOMIC_RELAXED));
             busy_wait_nop();
         }
-        p->yield++;
+        IF_PROF_SPIN(__atomic_fetch_add(&p->yield, 1, __ATOMIC_RELAXED));
         yieldThread();
     } while (1);
 }
@@ -58,17 +58,15 @@ INLINE_HEADER void ACQUIRE_SPIN_LOCK(SpinLock * p)
 // release spin lock
 INLINE_HEADER void RELEASE_SPIN_LOCK(SpinLock * p)
 {
-    write_barrier();
-    p->lock = 1;
+    RELEASE_STORE(&p->lock, 1);
 }
 
 // initialise spin lock
 INLINE_HEADER void initSpinLock(SpinLock * p)
 {
-    write_barrier();
-    p->lock = 1;
-    p->spin = 0;
-    p->yield = 0;
+    IF_PROF_SPIN(p->spin = 0);
+    IF_PROF_SPIN(p->yield = 0);
+    RELEASE_STORE(&p->lock, 1);
 }
 
 #else
