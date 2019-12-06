06fe0b0ef49386d2635a5d22d182881fd486c02a
Ben Gamari <ben@smart-cactus.org>
2019-12-06 14:13:31 -0500

    gitlab-ci: Move changelog linting logic to shell script
    Allowing it to be easily used locally.

--- .gitlab/linters/check-changelogs.sh
+++ .gitlab/linters/check-changelogs.sh
@@ -9,6 +9,8 @@ COLOR_NONE="\e[0m"
 grep TBA libraries/*/changelog.md && (
     echo -e "${COLOR_RED}Error: Found \"TBA\"s in changelogs.${COLOR_NONE}"
     exit 1
+) || (
+    echo -e "${COLOR_GREEN}changelogs look okay.${COLOR_NONE}"
+    exit 0
 )
 
-echo -e "${COLOR_GREEN}changelogs look okay.${COLOR_NONE}"
