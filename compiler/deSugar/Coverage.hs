28130f382cdf965fdcfeae2a106211d31485da57
Cale Gibbard <cgibbard@gmail.com>
2019-12-10 13:52:51 -0500

    Change the fail operator argument of BindStmt to be a Maybe
    Don't use noSyntaxExpr for it. There is no good way to defensively case
    on that, nor is it clear one ought to do so.

--- compiler/deSugar/Coverage.hs
+++ compiler/deSugar/Coverage.hs
@@ -707,7 +707,7 @@ addTickStmt _isGuard (BindStmt x pat e bind fail) = do
                 (addTickLPat pat)
                 (addTickLHsExprRHS e)
                 (addTickSyntaxExpr hpcSrcSpan bind)
-                (addTickSyntaxExpr hpcSrcSpan fail)
+                (mapM (addTickSyntaxExpr hpcSrcSpan) fail)
 addTickStmt isGuard (BodyStmt x e bind' guard') = do
         liftM3 (BodyStmt x)
                 (addTick isGuard e)
@@ -763,7 +763,7 @@ addTickApplicativeArg isGuard (op, arg) =
       <$> addTickLPat pat
       <*> addTickLHsExpr expr
       <*> pure isBody
-      <*> addTickSyntaxExpr hpcSrcSpan fail
+      <*> mapM (addTickSyntaxExpr hpcSrcSpan) fail
   addTickArg (ApplicativeArgMany x stmts ret pat) =
     (ApplicativeArgMany x)
       <$> addTickLStmts isGuard stmts
