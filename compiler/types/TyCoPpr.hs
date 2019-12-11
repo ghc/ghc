6aa93760c60ef8f5e34adcab3ea28b3b1305f40b
Richard Eisenberg <rae@richarde.dev>
2019-12-11 15:58:56 +0000

    Don't zap to Any; error instead
    This changes GHC's treatment of so-called Naughty Quantification
    Candidates to issue errors, instead of zapping to Any.
    
    Close #16775.
    
    No new test cases, because existing ones cover this well.

--- compiler/types/TyCoPpr.hs
+++ compiler/types/TyCoPpr.hs
@@ -5,7 +5,7 @@ module TyCoPpr
         PprPrec(..), topPrec, sigPrec, opPrec, funPrec, appPrec, maybeParen,
 
         -- * Pretty-printing types
-        pprType, pprParendType, pprPrecType, pprPrecTypeX,
+        pprType, pprParendType, pprTidiedType, pprPrecType, pprPrecTypeX,
         pprTypeApp, pprTCvBndr, pprTCvBndrs,
         pprSigmaType,
         pprTheta, pprParendTheta, pprForAll, pprUserForAll,
@@ -79,9 +79,12 @@ See Note [Precedence in types] in BasicTypes.
 -- See Note [Pretty printing via IfaceSyn] in PprTyThing
 --------------------------------------------------------
 
-pprType, pprParendType :: Type -> SDoc
+pprType, pprParendType, pprTidiedType :: Type -> SDoc
 pprType       = pprPrecType topPrec
 pprParendType = pprPrecType appPrec
+
+-- already pre-tidied
+pprTidiedType = pprIfaceType . toIfaceTypeX emptyVarSet
 
 pprPrecType :: PprPrec -> Type -> SDoc
 pprPrecType = pprPrecTypeX emptyTidyEnv
