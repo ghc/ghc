3705b319ba934d2e48f9242af12b066df6b0f943
Ben Gamari <ben@smart-cactus.org>
2019-12-27 12:38:27 -0500

    codeGen: Produce local symbols for module-internal functions
    It turns out that some important native debugging/profiling tools (e.g.
    perf) rely only on symbol tables for function name resolution (as
    opposed to using DWARF DIEs). However, previously GHC would emit
    temporary symbols (e.g. `.La42b`) to identify module-internal
    entities. Such symbols are dropped during linking and therefore not
    visible to runtime tools (in addition to having rather un-helpful unique
    names). For instance, `perf report` would often end up attributing all
    cost to the libc `frame_dummy` symbol since Haskell code was no covered
    by any proper symbol (see #17605).
    
    We now rather follow the model of C compilers and emit
    descriptively-named local symbols for module internal things. Since this
    will increase object file size this behavior can be disabled with the
    `-fno-expose-all-symbols` flag.
    
    With this `perf record` can finally be used against Haskell executables.
    Even more, with `-g3` `perf annotate` provides inline source code.

--- compiler/nativeGen/X86/Ppr.hs
+++ compiler/nativeGen/X86/Ppr.hs
@@ -39,6 +39,9 @@ import BasicTypes       (Alignment, mkAlignment, alignmentBytes)
 import DynFlags
 import Cmm              hiding (topInfoTable)
 import BlockId
+import Module           ( Module )
+import Name             ( isInternalName )
+import OccName          ( occNameFS, occName )
 import CLabel
 import Unique           ( pprUniqueAlways )
 import GHC.Platform
@@ -73,18 +76,18 @@ pprProcAlignment :: SDoc
 pprProcAlignment = sdocWithDynFlags $ \dflags ->
   (maybe empty (pprAlign . mkAlignment) (cmmProcAlignment dflags))
 
-pprNatCmmDecl :: NatCmmDecl (Alignment, CmmStatics) Instr -> SDoc
-pprNatCmmDecl (CmmData section dats) =
+pprNatCmmDecl :: Module -> NatCmmDecl (Alignment, CmmStatics) Instr -> SDoc
+pprNatCmmDecl _this_mod (CmmData section dats) =
   pprSectionAlign section $$ pprDatas dats
 
-pprNatCmmDecl proc@(CmmProc top_info lbl _ (ListGraph blocks)) =
-  sdocWithDynFlags $ \dflags ->
+pprNatCmmDecl this_mod proc@(CmmProc top_info lbl _ (ListGraph blocks)) =
   pprProcAlignment $$
   case topInfoTable proc of
     Nothing ->
         -- special case for code without info table:
         pprSectionAlign (Section Text lbl) $$
         pprProcAlignment $$
+        pprProcLabel this_mod lbl $$
         pprLabel lbl $$ -- blocks guaranteed not null, so label needed
         vcat (map (pprBasicBlock top_info) blocks) $$
         (if debugLevel dflags > 0
@@ -95,6 +98,7 @@ pprNatCmmDecl proc@(CmmProc top_info lbl _ (ListGraph blocks)) =
       sdocWithPlatform $ \platform ->
       pprSectionAlign (Section Text info_lbl) $$
       pprProcAlignment $$
+      pprProcLabel this_mod lbl $$
       (if platformHasSubsectionsViaSymbols platform
           then ppr (mkDeadStripPreventer info_lbl) <> char ':'
           else empty) $$
@@ -109,6 +113,17 @@ pprNatCmmDecl proc@(CmmProc top_info lbl _ (ListGraph blocks)) =
             <+> ppr (mkDeadStripPreventer info_lbl)
        else empty) $$
       pprSizeDecl info_lbl
+
+pprProcLabel :: Module -> CLabel -> SDoc
+pprProcLabel this_mod lbl = sdocWithDynFlags $ \dflags ->
+    if gopt Opt_ExposeAllSymbols dflags then doc else empty
+  where
+    doc
+      | Just nm <- hasHaskellName lbl
+      , isInternalName nm
+      = char '_' <> ppr this_mod <> char '_' <> ztext (zEncodeFS (occNameFS (occName nm))) <> char ':'
+      | otherwise
+      = empty
 
 -- | Output the ELF .size directive.
 pprSizeDecl :: CLabel -> SDoc
