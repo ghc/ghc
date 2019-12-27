3f1177575162132345c3cbd7ced45c1df263b0f0
Vladislav Zavialov <vlad.z.4096@gmail.com>
2019-12-27 20:25:58 +0300

    Simplify Parser.y

--- compiler/parser/HaddockUtils.hs
+++ compiler/parser/HaddockUtils.hs
@@ -6,15 +6,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ApplicativeDo #-}
 
-module HaddockUtils
-  ( addFieldDoc,
-    addFieldDocs,
-    addConDoc,
-    addConDocs,
-    addConDocFirst,
-
-    addModuleHaddock,
-  ) where
+module HaddockUtils (addModuleHaddock) where
 
 import GhcPrelude
 
@@ -579,28 +571,3 @@ mkLHsDocTy t doc =
 
 mkLHsDocTyMaybe :: LHsType GhcPs -> Maybe LHsDocString -> LHsType GhcPs
 mkLHsDocTyMaybe t = maybe t (mkLHsDocTy t)
-
--- -----------------------------------------------------------------------------
--- Adding documentation to record fields (used in parsing).
-
-addFieldDoc :: LConDeclField a -> Maybe LHsDocString -> LConDeclField a
-addFieldDoc (L l fld) doc
-  = L l (fld { cd_fld_doc = cd_fld_doc fld `mplus` doc })
-
-addFieldDocs :: [LConDeclField a] -> Maybe LHsDocString -> [LConDeclField a]
-addFieldDocs [] _ = []
-addFieldDocs (x:xs) doc = addFieldDoc x doc : xs
-
-
-addConDoc :: LConDecl a -> Maybe LHsDocString -> LConDecl a
-addConDoc decl    Nothing = decl
-addConDoc (L p c) doc     = L p ( c { con_doc = con_doc c `mplus` doc } )
-
-addConDocs :: [LConDecl a] -> Maybe LHsDocString -> [LConDecl a]
-addConDocs [] _ = []
-addConDocs [x] doc = [addConDoc x doc]
-addConDocs (x:xs) doc = x : addConDocs xs doc
-
-addConDocFirst :: [LConDecl a] -> Maybe LHsDocString -> [LConDecl a]
-addConDocFirst [] _ = []
-addConDocFirst (x:xs) doc = addConDoc x doc : xs
