67e076c4974880d6c835f0883af3e7f6a8dbf046
Ben Gamari <ben@smart-cactus.org>
2019-12-29 14:18:47 -0500

    Enable -Wstar-is-type in -Wall
    As noted in [proposal 0143][proposal] this is supposed to happen in
    8.12.
    
    Also fix an incorrect claim in the users guide that -Wstar-is-type is
    enabled by default.
    
    [proposal]: https://github.com/ghc-proposals/ghc-proposals/blob/master/proposals/0143-remove-star-kind.rst

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -4912,7 +4912,8 @@ minusWallOpts
         Opt_WarnUntickedPromotedConstructors,
         Opt_WarnMissingPatternSynonymSignatures,
         Opt_WarnUnusedRecordWildcards,
-        Opt_WarnRedundantRecordWildcards
+        Opt_WarnRedundantRecordWildcards,
+        Opt_WarnStarIsType
       ]
 
 -- | Things you get with -Weverything, i.e. *all* known warnings flags
