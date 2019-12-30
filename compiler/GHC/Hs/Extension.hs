c5ec996583373025488c090fb2c89f7bda38c1cb
Ben Gamari <ben@smart-cactus.org>
2019-12-29 21:11:28 -0500

    GHC.Hs.Extension: Use Type instead of *

--- compiler/GHC/Hs/Extension.hs
+++ compiler/GHC/Hs/Extension.hs
@@ -169,7 +169,7 @@ noExtCon x = case x of {}
 -- | GHC's L prefixed variants wrap their vanilla variant in this type family,
 -- to add 'SrcLoc' info via 'Located'. Other passes than 'GhcPass' not
 -- interested in location information can define this instance as @f p@.
-type family XRec p (f :: * -> *) = r | r -> p f
+type family XRec p (f :: Type -> Type) = r | r -> p f
 type instance XRec (GhcPass p) f = Located (f (GhcPass p))
 
 {-
