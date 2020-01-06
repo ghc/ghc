f3d7fdb3437a6161cf7ff1f30cd0a6563bf07db0
Ben Gamari <ben@smart-cactus.org>
2020-01-06 12:28:15 -0500

    llvmGen: Fix typo in readnone attribute

--- compiler/llvmGen/Llvm/Types.hs
+++ compiler/llvmGen/Llvm/Types.hs
@@ -566,7 +566,7 @@ instance Outputable LlvmFuncAttr where
   ppr OptSize            = text "optsize"
   ppr NoReturn           = text "noreturn"
   ppr NoUnwind           = text "nounwind"
-  ppr ReadNone           = text "readnon"
+  ppr ReadNone           = text "readnone"
   ppr ReadOnly           = text "readonly"
   ppr Ssp                = text "ssp"
   ppr SspReq             = text "ssqreq"
