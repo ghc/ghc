6eb566a0cf43aefaf451f5885ca9b56686840a23
Xavier Denis <xldenis@gmail.com>
2020-01-15 14:37:54 +0100

    Add ghc-in-ghci for stack based builds

---
+++ hadrian/ghci.stack.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+set -e
+
+GHC_FLAGS="$GHC_FLAGS $(TERM=dumb CABFLAGS=-v0 "hadrian/build.stack.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")"
+stack exec -- ghci $GHC_FLAGS "$@" -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs +RTS -A128m
