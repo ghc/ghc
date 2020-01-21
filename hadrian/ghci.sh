b1a32170a1b8e7a31c2dd28eb1b02f1375ea3998
Xavier Denis <xldenis@gmail.com>
2020-01-21 09:06:49 +0100

    Create ghci.cabal.sh

--- hadrian/ghci.sh
+++ hadrian/ghci.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-set -e
-
-GHC_FLAGS="$GHC_FLAGS $(TERM=dumb CABFLAGS=-v0 "hadrian/build.cabal.sh" tool-args -q --build-root=.hadrian_ghci --flavour=ghc-in-ghci "$@")"
-ghci $GHC_FLAGS "$@" -fno-code -fwrite-interface -hidir=.hadrian_ghci/interface -O0 ghc/Main.hs +RTS -A128m
+# By default on Linux/MacOS we build Hadrian using Cabal
+(. "hadrian/ghci.cabal.sh" "$@")
