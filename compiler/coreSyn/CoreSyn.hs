d48b98f13c2780707418d9ec16d84bdada83c279
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2020-01-24 15:52:12 +0100

    Fix an outdated note link
    This link appears to have been forgotten in
    0dad81ca5fd1f63bf8a3b6ad09787559e8bd05c0 .

--- compiler/coreSyn/CoreSyn.hs
+++ compiler/coreSyn/CoreSyn.hs
@@ -539,7 +539,7 @@ substitutions until the next run of the simplifier.
 
 Note [CoreSyn case invariants]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-See #case_invariants#
+See Note [Case expression invariants]
 
 Note [Levity polymorphism invariants]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
