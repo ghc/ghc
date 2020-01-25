35a4fdb9cb91a46df7d43778655383bd5e737574
John Ericson <git@JohnEricson.me>
2020-01-25 15:46:07 -0500

    Trees That Grow refactor for `ConPat` and `CoPat`
    - `ConPat{In,Out}` -> `ConPat`
    
    - `CoPat` -> `XPat (CoPat ..)`
    
    Note that `GHC.HS.*` still uses `HsWrap`, but only when `p ~ GhcTc`.
    After this change, moving the type family instances out of `GHC.HS.*` is
    sufficient to break the cycle.
    
    Add XCollectPat class to decide how binders are collected from XXPat based on the pass.
    
    Previously we did this with IsPass, but that doesn't work for Haddock's
    DocNameI, and the constraint doesn't express what actual distinction is being
    made. Perhaps a class for collecting binders more generally is in order, but we
    haven't attempted this yet.
    
    Pure refactor of code around ConPat
    
     - InPat/OutPat synonyms removed
    
     - rename several identifiers
    
     - redundant constraints removed
    
     - move extension field in ConPat to be first
    
     - make ConPat use record syntax more consistently
    
    Fix T6145 (ConPatIn became ConPat)
    
    Add comments from SPJ.
    
    Add comment about haddock's use of CollectPass.
    
    Updates haddock submodule.

--- compiler/GHC/HsToCore/Quote.hs
+++ compiler/GHC/HsToCore/Quote.hs
@@ -1914,7 +1914,7 @@ repP (TuplePat _ ps boxed)
   | otherwise           = do { qs <- repLPs ps; repPunboxedTup qs }
 repP (SumPat _ p alt arity) = do { p1 <- repLP p
                                  ; repPunboxedSum p1 alt arity }
-repP (ConPatIn dc details)
+repP (ConPat NoExtField dc details)
  = do { con_str <- lookupLOcc dc
       ; case details of
          PrefixCon ps -> do { qs <- repLPs ps; repPcon con_str qs }
