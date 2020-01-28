03d69e4bb6b92ccd8238bebd9cff68da23741f49
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-01-28 20:53:04 +0100

    Enable strict dicts by default at -O2.
    In the common case this is a straight performance win
    at a compile time cost so we enable it at -O2.
    
    In rare cases it can lead to compile time regressions
    because of changed inlining behaviour. Which can very
    rarely also affect runtime performance.
    
    Increasing the inlining threshold can help to avoid this
    which is documented in the user guide.
    
    In terms of measured results this reduced instructions executed
    for nofib by 1%.
    However for some cases (e.g. Cabal) enabling this
    by default increases compile time by 2-3% so we enable it only
    at -O2 where it's clear that a user is willing to trade compile
    time for runtime.
    
    Most of the testsuite runs without -O2 so there are few
    perf changes.
    
    Increases:
    T12545/T18698: We perform more WW work because dicts are now treated strict.
    T9198: Also some more work because functions are now subject to W/W
    
    Decreases:
    T14697: Compiling empty modules. Probably because of changes inside ghc.
    T9203:  I can't reproduce this improvement locally. Might be spurious.
    
    -------------------------
    Metric Decrease:
         T12227
         T14697
         T9203
    Metric Increase:
         T9198
         T12545
         T18698a
         T18698b
    -------------------------

--- testsuite/tests/warnings/should_compile/T16282/T16282.hs
+++ testsuite/tests/warnings/should_compile/T16282/T16282.hs
@@ -1,4 +1,15 @@
-import Data.Map
+{-# OPTIONS_GHC -Wall-missed-specialisations -O2 #-}
+
+import T16282A
+
+import Data.Map (Map)
+-- The purpose of this test is simple to trigger
+-- a missed specialization and check if GHC puts
+-- out a warning.
+-- It used to fail to specialize on the show instance
+-- for Data.Map, now that we enable -fdicts-strict by default
+-- the worker for these no longer take a dictionary (having been
+-- WWed). So instead we force it to fail to specialize on myMapM_
 
 -- If someone improves the specializer so that
 -- GHC no longer misses the specialization below,
@@ -10,5 +21,5 @@ import Data.Map
 -- the compiler may spit out the "specialization" spelling.
 main :: IO ()
 main = do
-  let m = [] :: [Map String Bool]
-  mapM_ print m
+  let m = [] :: [MyMap Double]
+  myMapM_ print m
