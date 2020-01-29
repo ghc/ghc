5a5fc83d71d8234c02d4cd04e5b6f1d7fe93ebbf
Gert-Jan <gertjan.bottu@kuleuven.be>
2020-01-29 09:42:02 +0000

    test updates (WIP)

--- testsuite/tests/th/TH_Roles1.hs
+++ testsuite/tests/th/TH_Roles1.hs
@@ -4,6 +4,6 @@ module TH_Roles1 where
 
 import Language.Haskell.TH
 
-$( return [ DataD [] (mkName "T") [PlainTV (mkName "a")] Nothing [] []
+$( return [ DataD [] (mkName "T") [PlainTV (mkName "a") ()] Nothing [] []
           , RoleAnnotD (mkName "T") [RepresentationalR] ] )
 
