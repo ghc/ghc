68ccdb7fe8477b2edf8ea94397bfd87921350ad0
John Ericson <git@JohnEricson.me>
2020-02-03 00:18:03 -0500

    Begin to split compiler frontend and middle in driver.
    The compiler "frontend" is taken to be the parsing, renaming, and type
    checking steps, while the middle is the desugaring and simplifying
    steps.
    
    The original motivation came from #14095, where we want to frontend vs
    middle work for smaller and more paralell rebuilds.
    
    As, if not more, important, is the need to explore how to untangle the
    deriver for sake of maintainability, not new features. I don't know the
    destination. Here is a few points of exploration:
    
    - I do hope separating things into smaller piece that compose, rather
      than big piece that at highly conditional ("do this for GHCi, this for
      backpck, this for hs-boot", this for one-shot, etc.).
    
    - !1823 might have a regression in terms of dependencies from modules on
      instantiations, which could be solved by tracking resolved `Module`s
      rather than syntactic `(Maybe FastString {-# library name #-},
      ModuleName)` pairs. But `ModSummary` is used in many different ways
      which makes this an unclear refactor, even allowing we parameterized
      `ModSummary` or make more types.
    
    - !935, which I earlier began but is also part to a GSOC proposal, also
      benefits from a `Unit`-aware --make, and likewise could benefit from
      the clearer use of `ModSummary` and other data types.
    
    None of these are solved/answered by this, but I believe it gets us
    closer.

--- compiler/GHC/Unit/Module/Status.hs
+++ compiler/GHC/Unit/Module/Status.hs
@@ -1,7 +1,7 @@
 module GHC.Unit.Module.Status
-   ( HscStatus (..)
-   )
-where
+  ( HscFrontendStatus'(..)
+  , HscMiddleStatus(..)
+  ) where
 
 import GHC.Prelude
 
@@ -9,21 +9,46 @@ import GHC.Unit
 import GHC.Unit.Module.ModGuts
 import GHC.Unit.Module.ModIface
 import GHC.Unit.Module.ModDetails
+import GHC.Unit.Module.ModSummary
 
 import GHC.Utils.Fingerprint
 
--- | Status of a module compilation to machine code
-data HscStatus
-    -- | Nothing to do.
-    = HscNotGeneratingCode ModIface ModDetails
-    -- | Nothing to do because code already exists.
-    | HscUpToDate ModIface ModDetails
-    -- | Update boot file result.
-    | HscUpdateBoot ModIface ModDetails
-    -- | Generate signature file (backpack)
-    | HscUpdateSig ModIface ModDetails
+-- | Status of a compilation to an "tc interface", i.e. enough information to
+-- begin type checking the next module
+
+-- TODO Type parameter until we sort imports out
+data HscFrontendStatus' tcGblEnv
+
+    -- | Nothing to do because interface already exists.
+    = HscFrontendStatus_UpToDate ModIface ModDetails
+
+    -- | Something changed. For regular module recompile this module at least
+    -- through desugaring. For hs-boot or hsig, update stub objects.
+    | HscFrontendStatus_Recomp
+        { hsc_frontend_status_simple_mod_iface :: ModIface
+        , hsc_frontend_status_simple_mod_details :: ModDetails
+        , hsc_frontend_status_mod_sumary :: ModSummary
+        , hsc_frontend_status_tc_result :: tcGblEnv
+        , hsc_frontend_status_mb_old_hash_ :: Maybe Fingerprint
+        }
+
+-- | Status of a compilation from type checked code to hard executable code. Not
+-- coincidentally, this is the slow parts of compilation.
+data HscMiddleStatus
+
+    -- | Nothing to do. Already have frontend's mod iface and details.
+    = HscMiddleStatus_NotGeneratingCode
+
+    --- TODO add this back for fast path distinct from the above. E.g. we could
+    --- have already typed checked a module but changed the optimization
+    --- settings so the two conceptual interface files are not valid or invalid
+    --- in lock step.
+
+    --- -- | Nothing to do because code already exists.
+    --- | HscMiddleStatus_UpToDate ModIface ModDetails
+
     -- | Recompile this module.
-    | HscRecomp
+    | HscMiddleStatus_Recomp
         { hscs_guts           :: CgGuts
           -- ^ Information for the code generator.
         , hscs_mod_location   :: !ModLocation
