7afdc9e870e69bb0f3ea34857da9da3fc92961a8
Philipp Krüger <philipp.krueger1@gmail.com>
2020-02-07 12:32:40 +0100

    Convert more `Located *` to `XRec pass *`

--- compiler/deSugar/ExtractDocs.hs
+++ compiler/deSugar/ExtractDocs.hs
@@ -115,8 +115,9 @@ user-written. This lets us relate Names (from ClsInsts) to comments
 (associated with InstDecls and DerivDecls).
 -}
 
-getMainDeclBinder :: XRec pass (Pat pass) ~ Located (Pat pass) =>
-                     HsDecl pass -> [IdP pass]
+getMainDeclBinder :: ( XRec pass (Pat pass) ~ Located (Pat pass)
+                     , XRec pass (IdP pass) ~ Located (IdP pass)
+                     ) => HsDecl pass -> [IdP pass]
 getMainDeclBinder (TyClD _ d) = [tcdName d]
 getMainDeclBinder (ValD _ d) =
   case collectHsBindBinders d of
@@ -127,7 +128,9 @@ getMainDeclBinder (ForD _ (ForeignImport _ name _ _)) = [unLoc name]
 getMainDeclBinder (ForD _ (ForeignExport _ _ _ _)) = []
 getMainDeclBinder _ = []
 
-sigNameNoLoc :: Sig pass -> [IdP pass]
+
+sigNameNoLoc :: ( XRec pass (IdP pass) ~ Located (IdP pass) 
+                ) => Sig pass -> [IdP pass]
 sigNameNoLoc (TypeSig    _   ns _)         = map unLoc ns
 sigNameNoLoc (ClassOpSig _ _ ns _)         = map unLoc ns
 sigNameNoLoc (PatSynSig  _   ns _)         = map unLoc ns
@@ -338,7 +341,9 @@ filterDecls = filter (isHandled . unLoc . fst)
 
 
 -- | Go through all class declarations and filter their sub-declarations
-filterClasses :: XRec p (HsDecl p) ~ Located (HsDecl p) =>
+filterClasses :: ( XRec p (HsDecl p) ~ Located (HsDecl p) 
+                 , XRec p (Sig p) ~ Located (Sig p)
+                 ) =>
                  [(LHsDecl p, doc)] -> [(LHsDecl p, doc)]
 filterClasses = map (first (mapLoc filterClass))
   where
