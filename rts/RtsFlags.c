32ffcec19d530ed81445291b4141e8cff3e63f4e
Daniel Gröber <dxld@darkboxed.org>
2020-02-09 05:41:27 +0100

    rts: ProfHeap: Add support for filtering by user-specified roots
    This allows a regular heap profile to be filtered by reachability from
    user-specified roots, making use-cases such as wanting to know which
    closure type is using the most memory in a specific field of a datatype
    rather than across the entire heap possible.

--- rts/RtsFlags.c
+++ rts/RtsFlags.c
@@ -376,6 +376,7 @@ usage_text[] = {
 "    -hy<typ>...  closures with specified type descriptions",
 "    -hr<cc>...   closures with specified retainers",
 "    -hb<bio>...  closures with specified biographies (lag,drag,void,use)",
+"    -hO          closures reachable from root objects selected by program",
 "",
 "  -R<size>       Set the maximum retainer set size (default: 8)",
 "",
@@ -2128,6 +2129,7 @@ static bool read_heap_profiling_flag(const char *arg)
     case 'b':
     case 'T':
     case 'o':
+    case 'O':
         if (arg[2] != '\0' && arg[3] != '\0') {
             {
                 const char *left  = strchr(arg, '{');
@@ -2176,6 +2178,9 @@ static bool read_heap_profiling_flag(const char *arg)
                     free(selector);
                 }
             }
+            break;
+        } else if (arg[2] == 'O') { // root select
+            RtsFlags.ProfFlags.rootSelector = true;
             break;
         }
 
