2a50e0908795a97fc8cdabe366ce12f79f4ef5c8
Daniel Gröber <dxld@darkboxed.org>
2020-02-11 06:59:55 +0100

    rts: RootProfile: Move setRootProfPtrs wrapper to base:GHC.Profiling
    To get the test working again it was necessary to adapt the setRootProfPtrs
    wrapper slightly. I'm not sure why moving the code to base made the test
    fail but the new version is better anyways.
    
    Before we would call `evaluate` on the passed closure to get rid of (among
    other things) the thunk unsafeCoerce introduces. Obviously this 1) affects
    program behaviour and 2) decreases the accuracy of the measurment. Turns
    out that by using unsafeCoerce# we don't have to apply this hack.
    
    To ensure that we're not adding any overhead that's being hidden by
    optimization we also disable optimization for `-ho` tests. In order to get
    the same test output as before we implement simple.go using unboxed types
    by hand.

---
+++ includes/rts/RootProfile.h
@@ -0,0 +1,18 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 2019
+ *
+ * Root profiling API
+ *
+ * Do not #include this file directly: #include "Rts.h" instead.
+ *
+ * To understand the structure of the RTS headers, see the wiki:
+ *   https://gitlab.haskell.org/ghc/ghc/wikis/commentary/source-tree/includes
+ *
+ * -------------------------------------------------------------------------- */
+
+#pragma once
+
+#if defined(PROFILING)
+StgWord setRootProfPtrs(StgWord n, HsStablePtr *sps, const char** descs);
+#endif
