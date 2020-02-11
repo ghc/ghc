f3317911fe0f99b55d68ca7dac861782f39bfe8f
Sylvain Henry <sylvain@haskus.fr>
2020-02-11 09:41:44 +0100

    Update compiler
    Thanks to ghc-bignum, the compiler can be simplified:
    
    * Types and constructors of Integer and Natural can be wired-in. It
      means that we don't have to query them from interfaces. It also means
      that numeric literals don't have to carry their type with them.
    
    * The same code is used whatever ghc-bignum backend is enabled. In
      particular, conversion of bignum literals into final Core expressions
      is now much more straightforward. Bignum closure inspection too.
    
    * GHC itself doesn't depend on any integer-* package anymore
    
    * The `integerLibrary` setting is gone.

--- compiler/GHC/Unit/State.hs
+++ compiler/GHC/Unit/State.hs
@@ -859,26 +859,12 @@ sortByPreference prec_map = sortBy (flip (compareByPreference prec_map))
 -- Pursuant to #12518, we could change this policy to, for example, remove
 -- the version preference, meaning that we would always prefer the packages
 -- in later package database.
---
--- Instead, we use that preference based policy only when one of the packages
--- is integer-gmp and the other is integer-simple.
--- This currently only happens when we're looking up which concrete
--- package to use in place of @integer-wired-in@ and that two different
--- package databases supply a different integer library. For more about
--- the fake @integer-wired-in@ package, see Note [The integer library]
--- in the @GHC.Builtin.Names@ module.
 compareByPreference
     :: PackagePrecedenceIndex
     -> UnitInfo
     -> UnitInfo
     -> Ordering
 compareByPreference prec_map pkg pkg'
-  | Just prec  <- Map.lookup (unitId pkg)  prec_map
-  , Just prec' <- Map.lookup (unitId pkg') prec_map
-  , differentIntegerPkgs pkg pkg'
-  = compare prec prec'
-
-  | otherwise
   = case comparing unitPackageVersion pkg pkg' of
         GT -> GT
         EQ | Just prec  <- Map.lookup (unitId pkg)  prec_map
@@ -889,12 +875,6 @@ compareByPreference prec_map pkg pkg'
            | otherwise
            -> EQ
         LT -> LT
-
-  where isIntegerPkg p = unitPackageNameString p `elem`
-          ["integer-simple", "integer-gmp"]
-        differentIntegerPkgs p p' =
-          isIntegerPkg p && isIntegerPkg p' &&
-          (unitPackageName p /= unitPackageName p')
 
 comparing :: Ord a => (t -> a) -> t -> t -> Ordering
 comparing f a b = f a `compare` f b
@@ -963,10 +943,6 @@ findWiredInPackages dflags prec_map pkgs vis_map = do
   -- in Note [Wired-in units] in GHC.Unit.Module
   let
         matches :: UnitInfo -> WiredInUnitId -> Bool
-        pc `matches` pid
-            -- See Note [The integer library] in GHC.Builtin.Names
-            | pid == unitString integerUnitId
-            = unitPackageNameString pc `elem` ["integer-gmp", "integer-simple"]
         pc `matches` pid = unitPackageNameString pc == pid
 
         -- find which package corresponds to each wired-in package
