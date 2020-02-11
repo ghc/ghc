33757c03c020701444e78c939993d7cc4b6cd4de
Sylvain Henry <sylvain@haskus.fr>
2020-02-11 09:41:44 +0100

    Update compiler
    Thanks to ghc-bignum, the compiler can be simplified:
    
    * Types and constructors of Integer and Natural can be wired-in. It
      means that we don't have to query them from interfaces. It also means
      that numeric literals don't have to carry their type with them.
    
    * The same code is used whatever ghc-bignum backend is enabled. In
      particular, conversion of bignum literals into final Core expressions
      is now much more straightforward. Bignum closure inspection too.
    
    * GHC itself doesn't depend on any integer-* package anymore
    
    * The `integerLibrary` setting is gone.

--- compiler/main/DynFlags.hs
+++ compiler/main/DynFlags.hs
@@ -133,8 +133,6 @@ module DynFlags (
         sOpt_i,
         sExtraGccViaCFlags,
         sTargetPlatformString,
-        sIntegerLibrary,
-        sIntegerLibraryType,
         sGhcWithInterpreter,
         sGhcWithNativeCodeGen,
         sGhcWithSMP,
@@ -145,7 +143,6 @@ module DynFlags (
         sGhcThreaded,
         sGhcDebugged,
         sGhcRtsWithLibdw,
-        IntegerLibrary(..),
         GhcNameVersion(..),
         FileSettings(..),
         PlatformMisc(..),
@@ -975,9 +972,6 @@ data DynFlags = DynFlags {
   platformConstants :: PlatformConstants,
   rawSettings       :: [(String, String)],
 
-  integerLibrary        :: IntegerLibrary,
-    -- ^ IntegerGMP or IntegerSimple. Set at configure time, but may be overridden
-    --   by GHC-API users. See Note [The integer library] in PrelNames
   llvmConfig            :: LlvmConfig,
     -- ^ N.B. It's important that this field is lazy since we load the LLVM
     -- configuration lazily. See Note [LLVM Configuration] in SysTools.
@@ -1955,7 +1949,6 @@ defaultDynFlags mySettings llvmConfig =
         ghcMode                 = CompManager,
         ghcLink                 = LinkBinary,
         hscTarget               = defaultHscTarget (sTargetPlatform mySettings) (sPlatformMisc mySettings),
-        integerLibrary          = sIntegerLibraryType mySettings,
         verbosity               = 0,
         optLevel                = 0,
         debugLevel              = 0,
