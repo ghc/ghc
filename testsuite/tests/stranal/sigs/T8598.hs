4351330c84c2d4a8f60351b0b758d9bccec79cf3
Sylvain Henry <sylvain@haskus.fr>
2020-02-11 09:18:09 +0100

    Update testsuite
    * support detection of slow ghc-bignum backend (to replace the detection
      of integer-simple use). There are still some test cases that the
      native backend doesn't handle efficiently enough.
    
    * remove tests for GMP only functions that have been removed from
      ghc-bignum
    
    * fix test results showing dependent packages (e.g. integer-gmp) or
      showing suggested instances
    
    * fix test using Integer/Natural API or showing internal names

--- testsuite/tests/stranal/sigs/T8598.hs
+++ testsuite/tests/stranal/sigs/T8598.hs
@@ -3,14 +3,14 @@
 module T8598(fun) where
 
 import GHC.Float (Double(..))
-import GHC.Integer (decodeDoubleInteger, encodeDoubleInteger)
+import GHC.Num.Integer (integerDecodeDouble#, integerEncodeDouble#)
 
 -- Float.scaleFloat for Doubles, slightly simplified
 fun :: Double -> Double
 fun x | isFix           = x
       | otherwise       = case x of
-          (D# x#) -> case decodeDoubleInteger x# of
-            (# i, j #) -> D# (encodeDoubleInteger i j)
+          (D# x#) -> case integerDecodeDouble# x# of
+            (# i, j #) -> D# (integerEncodeDouble# i j)
   where
   isFix = isDoubleFinite x == 0
 
