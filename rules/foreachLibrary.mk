c5dca1ce2bca56b20374b721e274031e103cf635
Sylvain Henry <sylvain@haskus.fr>
2020-02-11 09:56:43 +0100

    Update `make` based build system
    * replace integer-* package selection with ghc-bignum backend selection

--- rules/foreachLibrary.mk
+++ rules/foreachLibrary.mk
@@ -32,7 +32,7 @@
 #  - ghc-boot-th
 #  - ghc-prim
 #  - integer-gmp
-#  - integer-simple
+#  - ghc-bignum
 #  - template-haskell
 
 define foreachLibrary
@@ -44,7 +44,7 @@ $$(foreach hashline,libraries/ghc-boot-th#-#no-remote-repo#no-vcs           \
                     libraries/base#-#no-remote-repo#no-vcs                  \
                     libraries/ghc-prim#-#no-remote-repo#no-vcs              \
                     libraries/integer-gmp#-#no-remote-repo#no-vcs           \
-                    libraries/integer-simple#-#no-remote-repo#no-vcs        \
+                    libraries/ghc-bignum#-#no-remote-repo#no-vcs            \
                     libraries/template-haskell#-#no-remote-repo#no-vcs      \
                     $$(shell grep '^libraries/' packages | sed 's/  */#/g'),\
     $$(eval FEL_line    := $$(subst #,$$(space),$$(hashline)))              \
