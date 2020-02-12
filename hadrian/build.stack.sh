d4333929018e7b1e23a1f05ec12ca5ba184656ff
Sylvain Henry <sylvain@haskus.fr>
2020-02-12 11:24:36 +0100

    Ensure that Hadrian is built correctly before using it
    When Hadrian failed to build, the script would pick a previously built
    Hadrian (if available) instead of failing.

--- hadrian/build.stack.sh
+++ hadrian/build.stack.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Make sure that the script exits if Hadrian fails to build
+set -euo pipefail
+
 # Make sure Hadrian is up-to-date
 cd hadrian
 stack build --no-library-profiling ${HADRIAN_NIX:+--nix}
