7550417ac866e562bb015149d8f9a6b8c97b5f84
Sylvain Henry <sylvain@haskus.fr>
2020-02-13 11:03:07 +0100

    Hadrian: drop Sphinx flag checking for PDF documentation (#17825)
    It seems that Sphinx produces the ghc-flags.txt in
    doc/users_guide/_build rather than pdfRoot. We could copy ghc-flags.txt
    into pdfRoot (like happens naturally in the HTML case) but the benefit
    is pretty small. Let's just only check the HTML case.

--- hadrian/src/Rules/Documentation.hs
+++ hadrian/src/Rules/Documentation.hs
@@ -111,8 +111,6 @@ documentationRules = do
 
         need $ map (root -/-) targets
 
-        when (SphinxPDFs `Set.member` doctargets)
-          $ checkUserGuideFlags $ pdfRoot -/- "users_guide" -/- "ghc-flags.txt"
         when (SphinxHTML `Set.member` doctargets)
           $ checkUserGuideFlags $ root -/- htmlRoot -/- "users_guide" -/- "ghc-flags.txt"
 
