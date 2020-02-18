ghc/ghc

commit 6e2d9ee25bce06ae51d2f1cf8df4f7422106a383

Module hierarchy: Cmm (cf #13009)

ghc.mk
@@ -1217,8 +1217,8 @@ sdist-ghc-prep-tree :
 
 # Add files generated by alex and happy.
 # These rules depend on sdist-ghc-prep-tree.
-$(eval $(call sdist-ghc-file,compiler,stage2,cmm,CmmLex,x))
-$(eval $(call sdist-ghc-file,compiler,stage2,cmm,CmmParse,y))
+$(eval $(call sdist-ghc-file,compiler,stage2,GHC,Cmm,Lexer,x))
+$(eval $(call sdist-ghc-file,compiler,stage2,GHC,Cmm,Parser,y))
 $(eval $(call sdist-ghc-file,compiler,stage2,parser,Lexer,x))
 $(eval $(call sdist-ghc-file,compiler,stage2,parser,Parser,y))
 $(eval $(call sdist-ghc-file,utils/hpc,dist-install,,HpcParser,y))

commit d04ae2b473979f86f71e1cd2b3f3efb3a03d720f

Fix source distribution generation
Fixes #17848, a regression introduced by 6e2d9ee2.

ghc.mk
@@ -1217,8 +1217,8 @@ sdist-ghc-prep-tree :
 
 # Add files generated by alex and happy.
 # These rules depend on sdist-ghc-prep-tree.
-$(eval $(call sdist-ghc-file,compiler,stage2,GHC,Cmm,Lexer,x))
-$(eval $(call sdist-ghc-file,compiler,stage2,GHC,Cmm,Parser,y))
+$(eval $(call sdist-ghc-file,compiler,stage2,GHC/Cmm,Lexer,x))
+$(eval $(call sdist-ghc-file,compiler,stage2,GHC/Cmm,Parser,y))
 $(eval $(call sdist-ghc-file,compiler,stage2,parser,Lexer,x))
 $(eval $(call sdist-ghc-file,compiler,stage2,parser,Parser,y))
 $(eval $(call sdist-ghc-file,utils/hpc,dist-install,,HpcParser,y))