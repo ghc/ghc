217546a798e1eb5a931a677771bbd54bb0e4b5ee
Ben Gamari <ben@smart-cactus.org>
2020-02-23 15:46:19 -0500

    testsuite: Flush stdout buffers in InitEventLogging
    Otherwise we are sensitive to libc's buffering strategy.
    Similar to the issue fixed in 543dfaab166c81f46ac4af76918ce32190aaab22.

--- testsuite/tests/rts/InitEventLogging.hs
+++ testsuite/tests/rts/InitEventLogging.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
+import System.IO
+
 -- Test that the startEventLog interface works as expected.
 main :: IO ()
 main = do
   putStrLn "Starting eventlog..."
+  hFlush stdout
   c_init_eventlog
-  putStrLn "done"
 
 foreign import ccall unsafe "init_eventlog"
   c_init_eventlog :: IO ()
--- testsuite/tests/rts/InitEventLogging_c.c
+++ testsuite/tests/rts/InitEventLogging_c.c
@@ -3,19 +3,23 @@
 
 void test_init(void) {
   printf("init\n");
+  fflush(stdout);
 }
 
 bool test_write(void *eventlog, size_t eventlog_size) {
   printf("write\n");
+  fflush(stdout);
   return true;
 }
 
 void test_flush(void) {
   printf("flush\n");
+  fflush(stdout);
 }
 
 void test_stop(void) {
   printf("stop\n");
+  fflush(stdout);
 }
 
 const EventLogWriter writer = {
