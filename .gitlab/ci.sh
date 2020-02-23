f0179e333b49d2a8a22ee174324fa7117391be2d
Ben Gamari <ben@smart-cactus.org>
2020-02-23 16:18:35 -0500

    gitlab-ci: Add run_hadrian subcommand
    I've ruined two trees already by failing to pass --flavour to hadrian.
    Let's factor this out so it can be reused during troubleshooting.

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -378,11 +378,7 @@ function build_hadrian() {
     fail "FLAVOUR not set"
   fi
 
-  run hadrian/build.cabal.sh \
-    --flavour="$FLAVOUR" \
-    -j"$cores" \
-    $HADRIAN_ARGS \
-    binary-dist
+  run_hadrian binary-dist
 
   mv _build/bindist/ghc*.tar.xz ghc.tar.xz
 }
@@ -393,10 +389,7 @@ function test_hadrian() {
   run "$MAKE" install
   cd ../../../
 
-  run hadrian/build.cabal.sh \
-    --flavour="$FLAVOUR" \
-    -j"$cores" \
-    $HADRIAN_ARGS \
+  run_hadrian \
     test \
     --summary-junit=./junit.xml \
     --test-compiler="$TOP"/_build/install/bin/ghc
@@ -406,6 +399,14 @@ function clean() {
   rm -R tmp
   run "$MAKE" --quiet clean || true
   run rm -Rf _build
+}
+
+function run_hadrian() {
+  run hadrian/build.cabal.sh \
+    --flavour="$FLAVOUR" \
+    -j"$cores" \
+    $HADRIAN_ARGS \
+    $@
 }
 
 # A convenience function to allow debugging in the CI environment.
@@ -445,6 +446,7 @@ case $1 in
   test_make) fetch_perf_notes; test_make; push_perf_notes ;;
   build_hadrian) build_hadrian ;;
   test_hadrian) fetch_perf_notes; test_hadrian; push_perf_notes ;;
+  run_hadrian) run_hadrian $@ ;;
   clean) clean ;;
   shell) shell $@ ;;
   *) fail "unknown mode $1" ;;
