40f89b46fb2e254c69eb7c9b817c930fa2727807
Ilias Tsitsimpis <iliastsi@debian.org>
2020-03-01 16:25:13 +0200

    Do not define hs_atomic{read,write}64() on non-64bit
    Do not define hs_atomicread64() and hs_atomicwrite64() on machines where
    WORD_SIZE_IN_BITS is less than 64, just like we do with the rest of the atomic
    functions which work on 64-bit values.
    
    Without this, compilation fails on MIPSel and PowerPC with the following error:
    
    /usr/bin/ld: /<<PKGBUILDDIR>>/libraries/ghc-prim/dist-install/build/libHSghc-prim-0.5.3_p.a(atomic.p_o): in function `hs_atomicread64':
    atomic.c:(.text.hs_atomicread64+0x8): undefined reference to `__sync_add_and_fetch_8'
    /usr/bin/ld: /<<PKGBUILDDIR>>/libraries/ghc-prim/dist-install/build/libHSghc-prim-0.5.3_p.a(atomic.p_o): in function `hs_atomicwrite64':
    atomic.c:(.text.hs_atomicwrite64+0x38): undefined reference to `__sync_bool_compare_and_swap_8'
    
    Fixes #17886.

--- libraries/ghc-prim/cbits/atomic.c
+++ libraries/ghc-prim/cbits/atomic.c
@@ -361,6 +361,7 @@ hs_atomicread32(StgWord x)
 #endif
 }
 
+#if WORD_SIZE_IN_BITS == 64
 extern StgWord64 hs_atomicread64(StgWord x);
 StgWord64
 hs_atomicread64(StgWord x)
@@ -371,6 +372,7 @@ hs_atomicread64(StgWord x)
   return __sync_add_and_fetch((StgWord64 *) x, 0);
 #endif
 }
+#endif
 
 // AtomicWriteByteArrayOp_Int
 // Implies a full memory barrier (see compiler/prelude/primops.txt.pp)
@@ -409,6 +411,7 @@ hs_atomicwrite32(StgWord x, StgWord val)
 #endif
 }
 
+#if WORD_SIZE_IN_BITS == 64
 extern void hs_atomicwrite64(StgWord x, StgWord64 val);
 void
 hs_atomicwrite64(StgWord x, StgWord64 val)
@@ -419,4 +422,6 @@ hs_atomicwrite64(StgWord x, StgWord64 val)
   while (!__sync_bool_compare_and_swap((StgWord64 *) x, *(StgWord64 *) x, (StgWord64) val));
 #endif
 }
+#endif
+
 #endif
