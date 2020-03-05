414d1857396b3f30fc1391a7549e01058084725b
Simon Peyton Jones <simonpj@microsoft.com>
2020-03-05 12:06:46 +0000

    Pretty printing improvements

--- compiler/GHC/Hs/Pat.hs
+++ compiler/GHC/Hs/Pat.hs
@@ -526,10 +526,11 @@ pprPat (NPat _ l (Just _) _)    = char '-' <> ppr l
 pprPat (NPlusKPat _ n k _ _ _)  = hcat [ppr n, char '+', ppr k]
 pprPat (SplicePat _ splice)     = pprSplice splice
 pprPat (CoPat _ co pat _)       = pprIfTc @p $
-                                  pprHsWrapper co $ \parens
-                                              -> if parens
-                                                 then pprParendPat appPrec pat
-                                                 else pprPat pat
+                                  sdocWithDynFlags $ \ dflags ->
+                                  if gopt Opt_PrintTypecheckerElaboration dflags
+                                  then hang (text "CoPat" <+> parens (ppr co))
+                                          2 (pprParendPat appPrec pat)
+                                  else pprPat pat
 pprPat (SigPat _ pat ty)        = ppr pat <+> dcolon <+> ppr_ty
   where ppr_ty = case ghcPass @p of
                    GhcPs -> ppr ty
