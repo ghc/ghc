62de0ab410d7e0f44ad70776be06b459cf9cb3db
Sylvain Henry <sylvain@haskus.fr>
2020-03-16 16:46:39 +0100

    CmmToAsm DynFlags refactoring (#17957)
    * Remove `DynFlags` parameter from `isDynLinkName`: `isDynLinkName` used
      to test the global `ExternalDynamicRefs` flag. Now we test it outside of
      `isDynLinkName`
    
    * Add new fields into `NCGConfig`: current unit id, sse/bmi versions,
      externalDynamicRefs, etc.
    
    * Replace many uses of `DynFlags` by `NCGConfig`
    
    * Moved `BMI/SSE` datatypes into `GHC.Platform`

--- compiler/GHC/CmmToAsm/Config.hs
+++ compiler/GHC/CmmToAsm/Config.hs
@@ -9,21 +9,28 @@ where
 import GhcPrelude
 import GHC.Platform
 import GHC.Cmm.Type (Width(..))
+import GHC.Types.Module
 
 -- | Native code generator configuration
 data NCGConfig = NCGConfig
-   { ncgPlatform            :: !Platform    -- ^ Target platform
-   , ncgProcAlignment       :: !(Maybe Int) -- ^ Mandatory proc alignment
-   , ncgDebugLevel          :: !Int         -- ^ Debug level
-   , ncgExternalDynamicRefs :: !Bool        -- ^ Generate code to link against dynamic libraries
-   , ncgPIC                 :: !Bool        -- ^ Enable Position-Independent Code
-   , ncgSplitSections       :: !Bool        -- ^ Split sections
-   , ncgSpillPreallocSize   :: !Int         -- ^ Size in bytes of the pre-allocated spill space on the C stack
-   , ncgRegsIterative       :: !Bool
-   , ncgAsmLinting          :: !Bool        -- ^ Perform ASM linting pass
-   , ncgDumpRegAllocStages  :: !Bool
-   , ncgDumpAsmStats        :: !Bool
-   , ncgDumpAsmConflicts    :: !Bool
+   { ncgPlatform              :: !Platform        -- ^ Target platform
+   , ncgUnitId                :: UnitId           -- ^ Target unit ID
+   , ncgProcAlignment         :: !(Maybe Int)     -- ^ Mandatory proc alignment
+   , ncgDebugLevel            :: !Int             -- ^ Debug level
+   , ncgExternalDynamicRefs   :: !Bool            -- ^ Generate code to link against dynamic libraries
+   , ncgPIC                   :: !Bool            -- ^ Enable Position-Independent Code
+   , ncgInlineThresholdMemcpy :: !Word            -- ^ If inlining `memcpy` produces less than this threshold (in pseudo-instruction unit), do it
+   , ncgInlineThresholdMemset :: !Word            -- ^ Ditto for `memset`
+   , ncgSplitSections         :: !Bool            -- ^ Split sections
+   , ncgSpillPreallocSize     :: !Int             -- ^ Size in bytes of the pre-allocated spill space on the C stack
+   , ncgRegsIterative         :: !Bool
+   , ncgAsmLinting            :: !Bool            -- ^ Perform ASM linting pass
+   , ncgDoConstantFolding     :: !Bool            -- ^ Perform CMM constant folding
+   , ncgSseVersion            :: Maybe SseVersion -- ^ (x86) SSE instructions
+   , ncgBmiVersion            :: Maybe BmiVersion -- ^ (x86) BMI instructions
+   , ncgDumpRegAllocStages    :: !Bool
+   , ncgDumpAsmStats          :: !Bool
+   , ncgDumpAsmConflicts      :: !Bool
    }
 
 -- | Return Word size
