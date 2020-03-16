c205336861f6f33e3a7311ed5512822c9664fdd4
Sylvain Henry <sylvain@haskus.fr>
2020-03-16 15:13:05 +0100

    GHC.Runtime: avoid DynFlags

--- compiler/typecheck/TcRnTypes.hs
+++ compiler/typecheck/TcRnTypes.hs
@@ -70,6 +70,7 @@ module TcRnTypes(
         TcId, TcIdSet,
         NameShape(..),
         removeBindingShadowing,
+        getPlatform,
 
         -- Constraint solver plugins
         TcPlugin(..), TcPluginResult(..), TcPluginSolver,
@@ -84,6 +85,7 @@ module TcRnTypes(
 #include "HsVersions.h"
 
 import GhcPrelude
+import GHC.Platform
 
 import GHC.Hs
 import GHC.Driver.Types
@@ -902,6 +904,11 @@ removeBindingShadowing bindings = reverse $ fst $ foldl
         then (bindingAcc, seenNames)              -- skip it
         else (binding:bindingAcc, extendOccSet seenNames (occName binding)))
     ([], emptyOccSet) bindings
+
+
+-- | Get target platform
+getPlatform :: TcM Platform
+getPlatform = targetPlatform <$> getDynFlags
 
 ---------------------------
 -- Template Haskell stages and levels
