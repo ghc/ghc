195cfaf69d5b0aa68cf0eb9fd6182a099a204fd1
Sylvain Henry <sylvain@haskus.fr>
2020-03-17 15:32:32 +0100

    Refactor PprDebug handling
    If `-dppr-debug` is set, then PprUser and PprDump styles are silently
    replaced with PprDebug style. This was done in `mkUserStyle` and
    `mkDumpStyle` smart constructors. As a consequence they needed a
    DynFlags parameter.
    
    Now we keep the original PprUser and PprDump styles until they are used
    to create an `SDocContext`. I.e. the substitution is only performed in
    `initSDocContext`.

--- compiler/GHC/Iface/Load.hs
+++ compiler/GHC/Iface/Load.hs
@@ -989,7 +989,6 @@ readIface :: Module -> FilePath
 readIface wanted_mod file_path
   = do  { res <- tryMostM $
                  readBinIface CheckHiWay QuietBinIFaceReading file_path
-        ; dflags <- getDynFlags
         ; case res of
             Right iface
                 -- NB: This check is NOT just a sanity check, it is
@@ -1000,7 +999,7 @@ readIface wanted_mod file_path
                 | otherwise     -> return (Failed err)
                 where
                   actual_mod = mi_module iface
-                  err = hiModuleNameMismatchWarn dflags wanted_mod actual_mod
+                  err = hiModuleNameMismatchWarn wanted_mod actual_mod
 
             Left exn    -> return (Failed (text (showException exn)))
     }
@@ -1115,7 +1114,7 @@ showIface hsc_env filename = do
                                    neverQualifyModules
                                    neverQualifyPackages
    putLogMsg dflags NoReason SevDump noSrcSpan
-      (mkDumpStyle dflags print_unqual) (pprModIface iface)
+      (mkDumpStyle print_unqual) (pprModIface iface)
 
 -- Show a ModIface but don't display details; suitable for ModIfaces stored in
 -- the EPT.
@@ -1261,8 +1260,8 @@ badIfaceFile file err
   = vcat [text "Bad interface file:" <+> text file,
           nest 4 err]
 
-hiModuleNameMismatchWarn :: DynFlags -> Module -> Module -> MsgDoc
-hiModuleNameMismatchWarn dflags requested_mod read_mod
+hiModuleNameMismatchWarn :: Module -> Module -> MsgDoc
+hiModuleNameMismatchWarn requested_mod read_mod
  | moduleUnitId requested_mod == moduleUnitId read_mod =
     sep [text "Interface file contains module" <+> quotes (ppr read_mod) <> comma,
          text "but we were expecting module" <+> quotes (ppr requested_mod),
@@ -1273,7 +1272,7 @@ hiModuleNameMismatchWarn dflags requested_mod read_mod
  | otherwise =
   -- ToDo: This will fail to have enough qualification when the package IDs
   -- are the same
-  withPprStyle (mkUserStyle dflags alwaysQualify AllTheWay) $
+  withPprStyle (mkUserStyle alwaysQualify AllTheWay) $
     -- we want the Modules below to be qualified with package names,
     -- so reset the PrintUnqualified setting.
     hsep [ text "Something is amiss; requested module "
