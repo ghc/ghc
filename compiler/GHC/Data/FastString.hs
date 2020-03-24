cbae7c5306e15595f6d818b349a80a003371bdc9
Matthew Pickering <matthewtpickering@gmail.com>
2020-03-24 19:59:53 +0000

    Use ShortByteString for FastZString
    We need something compactable here, as a FastString ends up getting
    compacted.

--- compiler/GHC/Data/FastString.hs
+++ compiler/GHC/Data/FastString.hs
@@ -129,11 +129,11 @@ import Data.ByteString (ByteString)
 import Data.ByteString.Short (ShortByteString)
 import qualified Data.ByteString          as BS
 import qualified Data.ByteString.Char8    as BSC
-import qualified Data.ByteString.Unsafe   as BS
 import qualified Data.ByteString.Short    as SBS
 #if !MIN_VERSION_bytestring(0,11,0)
 import qualified Data.ByteString.Short.Internal as SBS
 #endif
+import Data.ByteString.Internal (c2w)
 import Foreign.C
 import System.IO
 import Data.Data
@@ -164,7 +164,7 @@ fastStringToShortByteString :: FastString -> ShortByteString
 fastStringToShortByteString = fs_sbs
 
 fastZStringToByteString :: FastZString -> ByteString
-fastZStringToByteString (FastZString bs) = bs
+fastZStringToByteString (FastZString bs) = SBS.fromShort bs
 
 -- This will drop information if any character > '\xFF'
 unsafeMkByteString :: String -> ByteString
@@ -175,23 +175,30 @@ hashFastString fs = hashStr $ fs_sbs fs
 
 -- -----------------------------------------------------------------------------
 
-newtype FastZString = FastZString ByteString
+newtype FastZString = FastZString ShortByteString
   deriving NFData
 
 hPutFZS :: Handle -> FastZString -> IO ()
-hPutFZS handle (FastZString bs) = BS.hPut handle bs
+hPutFZS handle (FastZString bs) = BS.hPut handle (SBS.fromShort bs)
 
 zString :: FastZString -> String
 zString (FastZString bs) =
-    inlinePerformIO $ BS.unsafeUseAsCStringLen bs peekCAStringLen
+    inlinePerformIO $ useAsCStringLen bs peekCAStringLen
 
 lengthFZS :: FastZString -> Int
-lengthFZS (FastZString bs) = BS.length bs
+lengthFZS (FastZString bs) = SBS.length bs
 
 mkFastZStringString :: String -> FastZString
-mkFastZStringString str = FastZString (BSC.pack str)
+mkFastZStringString str = FastZString (SBS.pack (map c2w str))
 
 -- -----------------------------------------------------------------------------
+--
+useAsCStringLen :: ShortByteString -> (CStringLen -> IO a) -> IO a
+useAsCStringLen bs action =
+  allocaBytes l $ \buf -> do
+      SBS.copyToPtr bs 0 buf (fromIntegral l)
+      action (buf, l)
+  where l = SBS.length bs
 
 {-| A 'FastString' is a UTF-8 encoded string together with a unique ID. All
 'FastString's are stored in a global hashtable to support fast O(1)
