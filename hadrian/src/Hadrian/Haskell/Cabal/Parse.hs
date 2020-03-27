27740f24cb70fc14b00c1212c06642a144a6117d
Ryan Scott <ryan.gl.scott@gmail.com>
2020-03-27 14:06:39 -0400

    Make Hadrian build with Cabal-3.2
    GHC 8.10 ships with `Cabal-3.2.0.0`, so it would be convenient to
    make Hadrian supporting building against 3.2.* instead of having to
    rebuild the entirety of `Cabal-3.0.0.0`. There is one API change in
    `Cabal-3.2.*` that affects Hadrian: the `synopsis` and `description`
    functions now return `ShortText` instead of `String`. Since Hadrian
    manipulates these `String`s in various places, I found that the
    simplest fix was to use CPP to convert `ShortText` to `String`s
    where appropriate.

--- hadrian/src/Hadrian/Haskell/Cabal/Parse.hs
+++ hadrian/src/Hadrian/Haskell/Cabal/Parse.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module     : Hadrian.Haskell.Cabal.Parse
@@ -38,6 +39,9 @@ import qualified Distribution.Text                             as C
 import qualified Distribution.Types.LocalBuildInfo             as C
 import qualified Distribution.Types.CondTree                   as C
 import qualified Distribution.Types.MungedPackageId            as C
+#if MIN_VERSION_Cabal(3,2,0)
+import qualified Distribution.Utils.ShortText                  as C
+#endif
 import qualified Distribution.Verbosity                        as C
 import Hadrian.Expression
 import Hadrian.Haskell.Cabal
@@ -69,7 +73,10 @@ parsePackageData pkg = do
         sorted  = sort [ C.unPackageName p | C.Dependency p _ _ <- allDeps ]
         deps    = nubOrd sorted \\ [name]
         depPkgs = catMaybes $ map findPackageByName deps
-    return $ PackageData name version (C.synopsis pd) (C.description pd) depPkgs gpd
+    return $ PackageData name version
+                         (shortTextToString (C.synopsis pd))
+                         (shortTextToString (C.description pd))
+                         depPkgs gpd
   where
     -- Collect an overapproximation of dependencies by ignoring conditionals
     collectDeps :: Maybe (C.CondTree v [C.Dependency] a) -> [C.Dependency]
@@ -77,6 +84,14 @@ parsePackageData pkg = do
     collectDeps (Just (C.CondNode _ deps ifs)) = deps ++ concatMap f ifs
       where
         f (C.CondBranch _ t mt) = collectDeps (Just t) ++ collectDeps mt
+
+#if MIN_VERSION_Cabal(3,2,0)
+    shortTextToString :: C.ShortText -> String
+    shortTextToString = C.fromShortText
+#else
+    shortTextToString :: String -> String
+    shortTextToString = id
+#endif
 
 -- | Parse the package identifier from a Cabal file.
 parseCabalPkgId :: FilePath -> IO String
