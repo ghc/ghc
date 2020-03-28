eea1305090aa8154e843569186f22ff679058b63
Sebastian Graf <sebastian.graf@kit.edu>
2020-03-28 13:46:58 +0100

    Change forcesRealWorld to work like the old IO hack

--- compiler/GHC/Core/Op/DmdAnal.hs
+++ compiler/GHC/Core/Op/DmdAnal.hs
@@ -27,6 +27,7 @@ import Data.List        ( mapAccumL )
 import GHC.Core.DataCon
 import GHC.Types.Id
 import GHC.Types.Id.Info
+import TysWiredIn
 import GHC.Core.Utils
 import GHC.Core.TyCon
 import GHC.Core.Type
@@ -350,20 +351,26 @@ forcesRealWorld :: FamInstEnvs -> Type -> Bool
 forcesRealWorld fam_envs = go initRecTc
   where
     go :: RecTcChecker -> Type -> Bool
-    go rec_tc ty
-      -- Found it!
-      | ty `eqType`  realWorldStatePrimTy
-      = True
-      -- search depth-first
+    go _rec_tc ty
       | Just DataConAppContext{ dcac_dc = dc, dcac_arg_tys = field_tys }
           <- deepSplitProductType_maybe fam_envs ty
-      -- don't check the same TyCon twice
-      , Just rec_tc' <- checkRecTc rec_tc (dataConTyCon dc)
-      = any (strict_field_forces rec_tc') field_tys
+      , dc == tupleDataCon Unboxed 2
+      , ((first_ty,_):_) <- field_tys
+      , first_ty `eqType` realWorldStatePrimTy
+      = True
+      -- -- Found it!
+      -- | ty `eqType`  realWorldStatePrimTy
+      -- = True
+      -- -- search depth-first
+      -- | Just DataConAppContext{ dcac_dc = dc, dcac_arg_tys = field_tys }
+      --     <- deepSplitProductType_maybe fam_envs ty
+      -- -- don't check the same TyCon twice
+      -- , Just rec_tc' <- checkRecTc rec_tc (dataConTyCon dc)
+      -- = any (strict_field_forces rec_tc') field_tys
       | otherwise
       = False
 
-    strict_field_forces rec_tc (field_ty, str_mark) =
+    _strict_field_forces rec_tc (field_ty, str_mark) =
       (isMarkedStrict str_mark || isLiftedType_maybe field_ty == Just False)
         && go rec_tc field_ty
 
