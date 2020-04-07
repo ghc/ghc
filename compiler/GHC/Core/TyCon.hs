bd87df8927eff4829d6f963d01b1f8165b1921d0
Ben Gamari <ben@smart-cactus.org>
2020-04-07 16:59:00 -0400

    Inline expandSynTyCon_maybe
    This is necessary to avoid some needless allocation since we currently
    lack nested CPR on sums.
    
    Metric Decrease:
       T12227
       T12545
       T12707
       T14683
       T3064
       T5631
       T5642
       T9020
       T9872a

--- compiler/GHC/Core/TyCon.hs
+++ compiler/GHC/Core/TyCon.hs
@@ -2318,6 +2318,9 @@ expandSynTyCon_maybe tc tys
               LT -> Nothing
    | otherwise
    = Nothing
+{-# INLINE expandSynTyCon_maybe #-}
+-- Inline to avoid allocation of tuples due to lack of nested CPR on sums.
+-- Particularly relevant to coreView and tcView, which are hammered.
 
 ----------------
 
