723062edf6191084a99787d3f235183cf6b7d051
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-04-07 12:06:32 +0300

    testsuite: Move no_lint to the top level, tweak hie002
    - We don't want to benchmark linting so disable lints in hie002 perf
      test
    
    - Move no_lint to the top-level to be able to use it in tests other than
      those in `testsuite/tests/perf/compiler`.
    
    - Filter out -dstg-lint in no_lint.
    
    - hie002 allocation numbers on 32-bit are unstable, so skip it on 32-bit
    
    Metric Decrease:
        hie002
        ManyConstructors
        T12150
        T12234
        T13035
        T1969
        T4801
        T9233
        T9961

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -116,6 +116,12 @@ def expect_fail( name, opts ):
     # future.
     opts.expect = 'fail';
 
+def no_lint( name, opts ):
+   """Disable Core, STG and Cmm lints. Useful when testing compiler perf."""
+   opts.compiler_always_flags = \
+       [opt for opt in opts.compiler_always_flags \
+            if opt not in ['-dcore-lint', '-dstg-lint', '-dcmm-lint']]
+
 def reqlib( lib ):
     return lambda name, opts, l=lib: _reqlib (name, opts, l )
 
