fd02ceed5661e1c2fa85ec5c663697a3b0cf3e04
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-04-10 13:02:38 +0200

    winio: Fix threadDelay maxBound invocations.
    Instead of letting the ns timer overflow now clamp it at
    (maxBound :: Word64) ns. That still gives a few hundred
    years.

--- libraries/base/GHC/Event/Windows/Thread.hs
+++ libraries/base/GHC/Event/Windows/Thread.hs
@@ -26,19 +26,13 @@ threadDelay :: Int -> IO ()
 threadDelay usecs = mask_ $ do
     m <- newEmptyIOPort
     mgr <- getSystemManager
-    reg <- registerTimeout mgr secs $ writeIOPort m () >> return ()
+    reg <- registerTimeout mgr usecs $ writeIOPort m () >> return ()
     readIOPort m `onException` unregisterTimeout mgr reg
-  where
-    secs = microsecondsToSeconds usecs
 
 registerDelay :: Int -> IO (TVar Bool)
 registerDelay usecs = do
     t <- newTVarIO False
     mgr <- getSystemManager
-    _ <- registerTimeout mgr secs $ atomically $ writeTVar t True
+    _ <- registerTimeout mgr usecs $ atomically $ writeTVar t True
     return t
-  where
-    secs = microsecondsToSeconds usecs
 
-microsecondsToSeconds :: Int -> Seconds
-microsecondsToSeconds us = fromIntegral us / 1000000.0
