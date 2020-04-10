b64e1682a6fde0e9f53b277f9b6b6b859bd08000
Daniel Gröber <dxld@darkboxed.org>
2020-04-10 04:10:36 +0200

    Always zero shrunk mutable array slop when profiling
    When shrinking arrays in the profiling way we currently don't always zero
    the leftover slop. This means we can't traverse such closures in the heap
    profiler. The old Note [zeroing slop] and #8402 have some rationale for why
    this is so but I belive the reasoning doesn't apply to mutable
    closures. There users already have to ensure multiple threads don't step on
    each other's toes so zeroing should be safe.

--- includes/Cmm.h
+++ includes/Cmm.h
@@ -626,11 +626,15 @@
 #if defined(PROFILING) || (!defined(THREADED_RTS) && defined(DEBUG))
 #define OVERWRITING_CLOSURE_SIZE(c, size) foreign "C" overwritingClosureSize(c "ptr", size)
 #define OVERWRITING_CLOSURE(c) foreign "C" overwritingClosure(c "ptr")
-#define OVERWRITING_CLOSURE_OFS(c, off) foreign "C" overwritingClosureOfs(c "ptr", off)
 #else
 #define OVERWRITING_CLOSURE_SIZE(c, size) /* nothing */
 #define OVERWRITING_CLOSURE(c) /* nothing */
-#define OVERWRITING_CLOSURE_OFS(c, off) /* nothing */
+#endif
+
+#if defined(PROFILING)
+#define OVERWRITING_MUTABLE_CLOSURE(c, off) foreign "C" overwritingMutableClosureOfs(c "ptr", off)
+#else
+#define OVERWRITING_MUTABLE_CLOSURE(c, off) /* nothing */
 #endif
 
 // Memory barriers.
