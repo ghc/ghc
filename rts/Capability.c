47508fa96aa5175d6ceab00d3fb73af91840a104
Ben Gamari <ben@smart-cactus.org>
2020-04-11 15:54:17 -0400

    rts: Flush eventlog buffers from flushEventLog
    As noted in #18043, flushTrace failed flush anything beyond the writer.
    This means that a significant amount of data sitting in capability-local
    event buffers may never get flushed, despite the users' pleads for us to
    flush.
    
    Fix this by making flushEventLog flush all of the event buffers before
    flushing the writer.
    
    Fixes #18043.

--- rts/Capability.c
+++ rts/Capability.c
@@ -23,6 +23,7 @@
 #include "Schedule.h"
 #include "Sparks.h"
 #include "Trace.h"
+#include "eventlog/EventLog.h" // for flushLocalEventsBuf
 #include "sm/GC.h" // for gcWorkerThread()
 #include "STM.h"
 #include "RtsUtils.h"
@@ -883,6 +884,10 @@ yieldCapability (Capability** pCap, Task *task, bool gcAllowed)
 
             case SYNC_FLUSH_UPD_REM_SET:
                 debugTrace(DEBUG_nonmoving_gc, "Flushing update remembered set blocks...");
+                break;
+
+            case SYNC_FLUSH_EVENT_LOG:
+                flushLocalEventsBuf(cap);
                 break;
 
             default:
