e00f4b97a34c63365cbc582d8aed03d9276e90e5
Daniel Gröber <dxld@darkboxed.org>
2020-04-13 05:40:31 +0200

    rts: TraverseHeap: Improve test precision

---
+++ testsuite/tests/profiling/should_run/TestHeap.h
@@ -0,0 +1,61 @@
+#pragma once
+
+static StgClosure *c10, *c11, *c20;
+
+static synthHeap initializeTestHeap(void)
+{
+    synthHeap sh = allocSynthHeap();
+
+/*
+  1.0) Just a simple case to start with.
+
+   1
+  /
+  0---2---3
+  \
+   4
+*/
+    c10 = node3(&sh, 1000,
+                node0(&sh, 1001),
+                node1(&sh, 1002,
+                      node0(&sh, 1003)),
+                node0(&sh, 1004));
+
+/*
+  1.1) Now with a merge
+
+   1
+  /` \,
+  0--->2--->3
+  \,
+   4
+*/
+    StgClosure *c1102;
+    c11 = node3(&sh, 1100,
+                node1(&sh, 1101,
+                      c1102 = node1(&sh, 1102,
+                                    node0(&sh, 1103))),
+                c1102,
+                node0(&sh, 1104));
+
+/*
+  2.0) This tests the chain optimization.
+
+   1     5
+  /     /
+  0-2-3-4-6
+  \     \
+   8     7
+*/
+    c20 = node3(&sh, 2000,
+                node0(&sh, 2001),
+                node1(&sh, 2002,
+                      node1(&sh, 2003,
+                            node3(&sh, 2004,
+                                  node0(&sh, 2005),
+                                  node0(&sh, 2006),
+                                  node0(&sh, 2007)))),
+                node0(&sh, 2008));
+
+    return sh;
+}
