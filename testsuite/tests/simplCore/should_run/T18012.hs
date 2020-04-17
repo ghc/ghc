8a6ca45ae9ca25e40e566d5b63283309f2bb1ea9
Alexis King <lexi.lambda@gmail.com>
2020-04-17 16:43:49 -0500

    Mark DataCon wrappers CONLIKE
    Now that DataCon wrappers don’t inline until phase 0 (see commit
    b78cc64e923716ac0512c299f42d4d0012306c05), it’s important that
    case-of-known-constructor and RULE matching be able to see saturated
    applications of DataCon wrappers in unfoldings. Making them conlike is a
    natural way to do it, since they are, in fact, precisely the sort of
    thing the CONLIKE pragma exists to solve.
    
    Fixes #18012.
    
    This also bumps the version of the parsec submodule to incorporate a
    patch that avoids a metric increase on the haddock perf tests. The
    increase was not really a flaw in this patch, as parsec was implicitly
    relying on inlining heuristics. The patch to parsec just adds some
    INLINABLE pragmas, and we get a nice performance bump out of it (well
    beyond the performance we lost from this patch).
    
    Metric Decrease:
        T12234
        WWRec
        haddock.Cabal
        haddock.base
        haddock.compiler

---
+++ testsuite/tests/simplCore/should_run/T18012.hs
@@ -0,0 +1,41 @@
+module Main (main) where
+
+{- This program is designed to check that case-of-known-constructor
+fires even if an application of a DataCon wrapper is floated out:
+
+  * The early FloatOut pass will float `D False` out of `g`, since
+    it’s a constant, non-trivial expression.
+
+  * But since `D` is strict, the floated-out expression will actually
+    be `$WD False`.
+
+  * In simplifier phase 2, `f` will be inlined into `g`, leading to a
+    case expression that scrutinizes the floated-out binding.
+
+  * If case-of-known-constructor fires, we’ll end up with `notRule
+    False`, the RULE will fire, and we get True.
+
+  * If it doesn’t fire at phase 2, it will fire later at phase 0 when
+    we inline the DataCon wrapper. But now the RULE is inactive, so
+    we’ll end up with False instead.
+
+We want case-of-known-constructor to fire early, so we want the output
+to be True. See #18012 for more details. -}
+
+main :: IO ()
+main = print (g ())
+
+data T = D !Bool
+
+notRule :: Bool -> Bool
+notRule x = x
+{-# INLINE [0] notRule #-}
+{-# RULES "notRule/False" [~0] notRule False = True #-}
+
+f :: T -> () -> Bool
+f (D a) () = notRule a
+{-# INLINE [100] f #-} -- so it isn’t inlined before FloatOut
+
+g :: () -> Bool
+g x = f (D False) x
+{-# NOINLINE g #-}
