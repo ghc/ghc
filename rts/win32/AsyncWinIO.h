b96d7599c698c95ec9b34dbdcc8168fec8cd7195
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-04-20 16:21:27 +0200

    winio: nontreaded: Create io processing threads in main thread.
    We now set a flag in the IO thread. The scheduler when looking for work
    will check the flag and create/queue threads accordingly.
    
    We used to create these in the IO thread. This improved performance
    but caused frequent segfaults. Thread creation/allocation is only safe to
    do if nothing currently accesses the storeagemanager. However without
    locks in the non-threaded runtime this can't be guaranteed.
    
    This shouldn't change performance all too much.
    
    In the past we had:
    * IO: Create/Queue thread.
    * Scheduler: Runs a few times. Eventually picks up IO processing thread.
    
    Now it's:
    * IO: Set flag to queue thread.
    * Scheduler: Pick up flag, if set create/queue thread. Eventually picks up IO processing thread.

--- rts/win32/AsyncWinIO.h
+++ rts/win32/AsyncWinIO.h
@@ -23,4 +23,9 @@ extern void registerAlertableWait (HANDLE port, DWORD mssec, uint64_t num_req);
 extern OVERLAPPED_ENTRY* getOverlappedEntries (uint32_t *num);
 extern void servicedIOEntries (uint64_t remaining);
 extern void completeSynchronousRequest (void);
+<<<<<<< HEAD
+
+=======
+extern bool queueIOThread(void);
+>>>>>>> f1207f6124... winio: nontreaded: Create io processing threads in main thread.
 
