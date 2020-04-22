442932e14794bb8f498727a4378c001c411a7638
Sebastian Graf <sebastian.graf@kit.edu>
2020-04-22 22:18:18 +0200

    stuff

--- compiler/GHC/Types/Id/Make.hs
+++ compiler/GHC/Types/Id/Make.hs
@@ -1217,8 +1217,8 @@ mkPrimOpId prim_op
     id   = mkGlobalId (PrimOpId prim_op) name ty info
 
     -- PrimOps don't ever construct a product, but we want to preserve bottoms
-    cpr | isBotDiv (snd (splitStrictSig strict_sig)) = botCpr
-        | otherwise                                  = topCpr
+    cpr | isBotDiv (snd (splitStrictSig strict_sig)) = initRecFunCpr
+        | otherwise                                  = whnfTermCpr
 
     info = noCafIdInfo
            `setRuleInfo`           mkRuleInfo (maybeToList $ primOpRules name prim_op)
