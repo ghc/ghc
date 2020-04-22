7bfe9ac514e18c0b0e24ff55230fe98ec9db894c
Ben Gamari <ben@smart-cactus.org>
2020-04-22 14:21:01 -0400

    rts: Enable tracing of nonmoving heap census with -ln
    Previously this was not easily available to the user. Fix this.
    Non-moving collection lifecycle events are now reported with -lg.

--- rts/sm/NonMoving.c
+++ rts/sm/NonMoving.c
@@ -1125,6 +1125,10 @@ static void nonmovingMark_(MarkQueue *mark_queue, StgWeak **dead_weaks, StgTSO *
     if (RtsFlags.DebugFlags.nonmoving_gc)
         nonmovingPrintAllocatorCensus();
 #endif
+#if defined(TRACING)
+    if (RtsFlags.TraceFlags.nonmoving_gc)
+        nonmovingTraceAllocatorCensus();
+#endif
 
     // TODO: Remainder of things done by GarbageCollect (update stats)
 
