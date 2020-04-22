567acb8907ea5e7107cb5ac4b92c7b8695d3b634
Sylvain Henry <sylvain@haskus.fr>
2020-04-22 19:13:30 +0200

    Workaround #17791 with weak symbols

---
+++ libraries/ghc-bignum/cbits/weak_symbols.c
@@ -0,0 +1,4 @@
+#include <stddef.h>
+
+void * base_ControlziExceptionziBase_patError_closure __attribute__((weak)) = NULL;
+void * base_ControlziExceptionziBase_absentSumFieldError_closure __attribute__((weak)) = NULL;
