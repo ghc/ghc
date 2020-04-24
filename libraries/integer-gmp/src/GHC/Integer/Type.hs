80beb8923bf86367e3f83f59b8a9482ba9ee3a5b
Sebastian Graf <sebastian.graf@kit.edu>
2020-04-24 11:21:03 +0200

    Inline `decodeDoubleInteger` and constant-fold `decodeDouble_Int64#` instead
    Currently, `decodeDoubleInteger` is known-key so that it can be
    recognised in constant folding. But that is very brittle and doesn't
    survive worker/wrapper, which we even do for
    `NOINLINE`/`CONSTANT_FOLDED` things since #13143.
    Also it is a trade-off: The implementation of `decodeDoubleInteger`
    allocates an `Integer` box that never cancels aways if we don't inline
    it.
    
    Hence we recognise the `decodeDouble_Int64#` primop instead in constant
    folding, so that we can inline `decodeDoubleInteger`.
    
    You may wonder how this affects performance of code using
    `integer-simple`; Apparently, according to @hsyl20 this is not a concern
    since we will hopefully land !2231 soon.
    
    Fixes #18092.

--- libraries/integer-gmp/src/GHC/Integer/Type.hs
+++ libraries/integer-gmp/src/GHC/Integer/Type.hs
@@ -1618,7 +1618,6 @@ foreign import ccall unsafe "integer_gmp_invert"
 -- Conversions to/from floating point
 
 decodeDoubleInteger :: Double# -> (# Integer, Int# #)
--- decodeDoubleInteger 0.0## = (# S# 0#, 0# #)
 #if WORD_SIZE_IN_BITS == 64
 decodeDoubleInteger x = case decodeDouble_Int64# x of
                           (# m#, e# #) -> (# S# m#, e# #)
@@ -1626,7 +1625,7 @@ decodeDoubleInteger x = case decodeDouble_Int64# x of
 decodeDoubleInteger x = case decodeDouble_Int64# x of
                           (# m#, e# #) -> (# int64ToInteger m#, e# #)
 #endif
-{-# CONSTANT_FOLDED decodeDoubleInteger #-}
+{-# INLINE decodeDoubleInteger #-}
 
 -- provided by GHC's RTS
 foreign import ccall unsafe "__int_encodeDouble"
