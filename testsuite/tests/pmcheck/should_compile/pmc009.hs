94a6f5a032b6995d524e2e4563e375f2b19de32c
Sylvain Henry <sylvain@haskus.fr>
2020-04-28 22:03:21 +0200

    Fix some stuff
    Update haddock submodule

--- testsuite/tests/pmcheck/should_compile/pmc009.hs
+++ testsuite/tests/pmcheck/should_compile/pmc009.hs
@@ -2,7 +2,7 @@ module HsUtils where
 import GHC.Hs.Binds
 import GHC.Types.SrcLoc
 
-addPatSynSelector:: LHsBind p -> [a]
+addPatSynSelector:: GenLocated l (HsBindLR idL idR) -> [a]
 addPatSynSelector bind
   | PatSynBind _ _ <- unLoc bind
   = []
