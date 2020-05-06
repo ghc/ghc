3dd22e4c4b53598b84d721a8c6cf7ff856ee461e
Travis Whitaker <pi.boy.travis@gmail.com>
2020-05-06 04:14:47 +0000

    WIP: Build a threaded stage 1 if the bootstrapping GHC supports it.

--- ghc/ghc.mk
+++ ghc/ghc.mk
@@ -66,8 +66,13 @@ else
 ghc_stage2_CONFIGURE_OPTS += -f-threaded
 ghc_stage3_CONFIGURE_OPTS += -f-threaded
 endif
-# Stage-0 compiler isn't guaranteed to have a threaded RTS.
+
+# If stage 0 supplies a threaded RTS, we can use it for stage 1.
+ifeq "$(GhcThreadedRts)" "YES"
+ghc_stage1_MORE_HC_OPTS += -threaded
+else
 ghc_stage1_CONFIGURE_OPTS += -f-threaded
+endif
 
 ifeq "$(GhcProfiled)" "YES"
 ghc_stage2_PROGRAM_WAY = p
