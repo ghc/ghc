7846cbf58384f4fb79e77a154ef4cffbb9274329
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-05-06 13:39:47 +0200

    winio: Refactor non-threaded runner thread and scheduler interface.
    Only use a single communication point (registerAlertableWait) to inform
    the C side aobut both timeouts to use as well as outstanding requests.
    
    Also queue a haskell processing thread after each return from alertable
    waits. This way there is no risk of us missing a timer event.

--- rts/win32/AsyncWinIO.h
+++ rts/win32/AsyncWinIO.h
@@ -18,14 +18,14 @@ extern bool startupAsyncWinIO(void);
 extern void shutdownAsyncWinIO(bool wait_threads);
 extern void awaitAsyncRequests(bool wait);
 extern void registerNewIOCPHandle (HANDLE port);
-extern void registerAlertableWait (HANDLE port, DWORD mssec, uint64_t num_req);
+extern void registerAlertableWait (bool has_timeout, DWORD mssec, uint64_t num_req, bool service_pending);
 
 extern OVERLAPPED_ENTRY* getOverlappedEntries (uint32_t *num);
-extern void servicedIOEntries (uint64_t remaining);
 extern void completeSynchronousRequest (void);
 <<<<<<< HEAD
 
 =======
 extern bool queueIOThread(void);
 >>>>>>> f1207f6124... winio: nontreaded: Create io processing threads in main thread.
+
 
