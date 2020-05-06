67738db10010fd28a8e997b5c8f83ea591b88a0e
Travis Whitaker <pi.boy.travis@gmail.com>
2020-05-06 04:14:47 +0000

    Build a threaded stage 1 if the bootstrapping GHC supports it.

--- ghc/ghc.mk
+++ ghc/ghc.mk
@@ -66,8 +66,15 @@ else
 ghc_stage2_CONFIGURE_OPTS += -f-threaded
 ghc_stage3_CONFIGURE_OPTS += -f-threaded
 endif
-# Stage-0 compiler isn't guaranteed to have a threaded RTS.
+
+# If stage 0 supplies a threaded RTS, we can use it for stage 1.
+# See Note [Linking ghc-bin against threaded stage0 RTS] in
+# hadrian/src/Settings/Packages.hs for details.
+ifeq "$(GhcThreadedRts)" "YES"
+ghc_stage1_MORE_HC_OPTS += -threaded
+else
 ghc_stage1_CONFIGURE_OPTS += -f-threaded
+endif
 
 ifeq "$(GhcProfiled)" "YES"
 ghc_stage2_PROGRAM_WAY = p
--- hadrian/src/Oracles/Flag.hs
+++ hadrian/src/Oracles/Flag.hs
@@ -24,25 +24,27 @@ data Flag = ArSupportsAtFile
           | WithLibnuma
           | HaveLibMingwEx
           | UseSystemFfi
+          | BootstrapThreadedRts
 
 -- Note, if a flag is set to empty string we treat it as set to NO. This seems
 -- fragile, but some flags do behave like this.
 flag :: Flag -> Action Bool
 flag f = do
     let key = case f of
-            ArSupportsAtFile   -> "ar-supports-at-file"
-            CrossCompiling     -> "cross-compiling"
-            CcLlvmBackend      -> "cc-llvm-backend"
-            GhcUnregisterised  -> "ghc-unregisterised"
-            TablesNextToCode   -> "tables-next-to-code"
-            GmpInTree          -> "intree-gmp"
-            GmpFrameworkPref   -> "gmp-framework-preferred"
-            LeadingUnderscore  -> "leading-underscore"
-            SolarisBrokenShld  -> "solaris-broken-shld"
-            WithLibdw          -> "with-libdw"
-            WithLibnuma        -> "with-libnuma"
-            HaveLibMingwEx     -> "have-lib-mingw-ex"
-            UseSystemFfi       -> "use-system-ffi"
+            ArSupportsAtFile     -> "ar-supports-at-file"
+            CrossCompiling       -> "cross-compiling"
+            CcLlvmBackend        -> "cc-llvm-backend"
+            GhcUnregisterised    -> "ghc-unregisterised"
+            TablesNextToCode     -> "tables-next-to-code"
+            GmpInTree            -> "intree-gmp"
+            GmpFrameworkPref     -> "gmp-framework-preferred"
+            LeadingUnderscore    -> "leading-underscore"
+            SolarisBrokenShld    -> "solaris-broken-shld"
+            WithLibdw            -> "with-libdw"
+            WithLibnuma          -> "with-libnuma"
+            HaveLibMingwEx       -> "have-lib-mingw-ex"
+            UseSystemFfi         -> "use-system-ffi"
+            BootstrapThreadedRts -> "bootstrap-threaded-rts"
     value <- lookupValueOrError configFile key
     when (value `notElem` ["YES", "NO", ""]) . error $ "Configuration flag "
         ++ quote (key ++ " = " ++ value) ++ " cannot be parsed."
