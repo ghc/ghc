2c05fca6cd33d42af53c8657892301b0052d4a7b
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-05-14 11:39:20 +0900

    base: Add Generic instances to various datatypes under GHC.*
    * GHC.Fingerprint.Types: Fingerprint
    * GHC.RTS.Flags: GiveGCStats, GCFlags, ConcFlags, DebugFlags, CCFlags, DoHeapProfile, ProfFlags, DoTrace, TraceFlags, TickyFlags, ParFlags and RTSFlags
    * GHC.Stats: RTSStats and GCStats
    * GHC.ByteOrder: ByteOrder
    * GHC.Unicode: GeneralCategory
    * GHC.Stack.Types: SrcLoc

--- libraries/base/GHC/Generics.hs
+++ libraries/base/GHC/Generics.hs
@@ -746,6 +746,8 @@ import GHC.Classes ( Eq(..), Ord(..) )
 import GHC.Enum    ( Bounded, Enum )
 import GHC.Read    ( Read(..) )
 import GHC.Show    ( Show(..), showString )
+import GHC.Stack.Types ( SrcLoc )
+import GHC.Unicode ( GeneralCategory )
 
 -- Needed for metadata
 import Data.Proxy   ( Proxy(..) )
@@ -1477,6 +1479,11 @@ deriving instance Generic ((,,,,,,) a b c d e f g)
 -- | @since 4.12.0.0
 deriving instance Generic (Down a)
 
+-- | @since 4.15.0.0
+deriving instance Generic SrcLoc
+
+-- | @since 4.15.0.0
+deriving instance Generic GeneralCategory
 
 -- | @since 4.6.0.0
 deriving instance Generic1 []
