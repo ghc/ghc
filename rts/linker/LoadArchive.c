146ef07ed078355f5ad0ad6c815a35f0ccf85a5d
Moritz Angermann <moritz.angermann@gmail.com>
2020-05-14 08:17:59 +0800

    Load .lo as well.
    Some archives contain so called linker objects, with the affectionate
    .lo suffic.  For example the musl libc.a will come in that form.  We
    still want to load those objects, hence we should not discard them and
    look for .lo as well.  Ultimately we might want to fix this proerly by
    looking at the file magic.
    
    (cherry picked from commit 3fd12af1eaafe304e5916bc1fcfdf31709d360b8)

--- rts/linker/LoadArchive.c
+++ rts/linker/LoadArchive.c
@@ -461,6 +461,7 @@ static HsInt loadArchive_ (pathchar *path)
         /* TODO: Stop relying on file extensions to determine input formats.
                  Instead try to match file headers. See Trac #13103.  */
         isObject = (thisFileNameSize >= 2 && strncmp(fileName + thisFileNameSize - 2, ".o"  , 2) == 0)
+                || (thisFileNameSize >= 3 && strncmp(fileName + thisFileNameSize - 3, ".lo" , 3) == 0)
                 || (thisFileNameSize >= 4 && strncmp(fileName + thisFileNameSize - 4, ".p_o", 4) == 0)
                 || (thisFileNameSize >= 4 && strncmp(fileName + thisFileNameSize - 4, ".obj", 4) == 0);
 
