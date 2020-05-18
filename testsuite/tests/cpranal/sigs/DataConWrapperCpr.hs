e3436ac58e5ef10e2790a07f39812c8aca3fa59d
Sebastian Graf <sebastian.graf@kit.edu>
2020-05-18 13:32:05 +0200

    Testsuite changes

--- testsuite/tests/cpranal/sigs/DataConWrapperCpr.hs
+++
@@ -1,8 +0,0 @@
-{-# OPTIONS_GHC -O2 -fforce-recomp #-}
-module DataConWrapperCpr where
-
-data Foo = Foo !Int
-
--- Should have the CPR property! Hence the wrapper $WFoo must have it.
-foo :: Int -> Foo
-foo = Foo
