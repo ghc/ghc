4c4312edd988b8aeeb65e095056743c30678df15
Ben Gamari <ben@smart-cactus.org>
2020-05-20 14:47:23 -0400

    Coverage: Drop redundant ad-hoc boot module check
    To determine whether the module is a boot module
    Coverage.addTicksToBinds was checking for a `boot` suffix in the module
    source filename. This is quite ad-hoc and shouldn't be necessary; the
    callsite in `deSugar` already checks that the module isn't a boot
    module.

--- compiler/GHC/HsToCore/Coverage.hs
+++ compiler/GHC/HsToCore/Coverage.hs
@@ -78,8 +78,7 @@ addTicksToBinds
 addTicksToBinds hsc_env mod mod_loc exports tyCons binds
   | let dflags = hsc_dflags hsc_env
         passes = coveragePasses dflags, not (null passes),
-    Just orig_file <- ml_hs_file mod_loc,
-    not ("boot" `isSuffixOf` orig_file) = do
+    Just orig_file <- ml_hs_file mod_loc = do
 
      let  orig_file2 = guessSourceFile binds orig_file
 
