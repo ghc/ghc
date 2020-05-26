85e64a5f7549542e99fe55c1d9f31071de866872
Matthew Pickering <matthewtpickering@gmail.com>
2020-05-26 13:31:13 +0100

    Use a newtype `Code` for the return type of typed quotations (Proposal #195)
    There are three problems with the current API:
    
    1. It is hard to properly write instances for ``Quote m => m (TExp a)`` as the type is the composition
       of two type constructors. Doing so in your program involves making your own newtype and
       doing a lot of wrapping/unwrapping.
    
       For example, if I want to create a language which I can either run immediately or
       generate code from I could write the following with the new API. ::
    
          class Lang r where
            _int :: Int -> r Int
            _if  :: r Bool -> r a -> r a -> r a
    
          instance Lang Identity where
            _int = Identity
            _if (Identity b) (Identity t) (Identity f) = Identity (if b then t else f)
    
          instance Quote m => Lang (Code m) where
            _int = liftTyped
            _if cb ct cf = [|| if $$cb then $$ct else $$cf ||]
    
    2. When doing code generation it is common to want to store code fragments in
       a map. When doing typed code generation, these code fragments contain a
       type index so it is desirable to store them in one of the parameterised
       map data types such as ``DMap`` from ``dependent-map`` or ``MapF`` from
       ``parameterized-utils``.
    
       ::
    
          compiler :: Env -> AST a -> Code Q a
    
          data AST a where ...
          data Ident a = ...
    
          type Env = MapF Ident (Code Q)
    
          newtype Code m a = Code (m (TExp a))
    
       In this example, the ``MapF`` maps an ``Ident String`` directly to a ``Code Q String``.
       Using one of these map types currently requires creating your own newtype and constantly
       wrapping every quotation and unwrapping it when using a splice. Achievable, but
       it creates even more syntactic noise than normal metaprogramming.
    
    3. ``m (TExp a)`` is ugly to read and write, understanding ``Code m a`` is
       easier. This is a weak reason but one everyone
       can surely agree with.
    
    Updates text submodule.

--- compiler/GHC/Tc/Gen/Splice.hs
+++ compiler/GHC/Tc/Gen/Splice.hs
@@ -196,7 +196,7 @@ tcTypedBracket rn_expr brack@(TExpBr _ expr) res_ty
        ; let rep = getRuntimeRep expr_ty
        ; meta_ty <- tcTExpTy m_var expr_ty
        ; ps' <- readMutVar ps_ref
-       ; texpco <- tcLookupId unsafeTExpCoerceName
+       ; texpco <- tcLookupId unsafeCodeCoerceName
        ; tcWrapResultO (Shouldn'tHappenOrigin "TExpBr")
                        rn_expr
                        (unLoc (mkHsApp (mkLHsWrap (applyQuoteWrapper wrapper)
@@ -302,9 +302,9 @@ tcPendingSplice m_var (PendingRnSplice flavour splice_name expr)
 tcTExpTy :: TcType -> TcType -> TcM TcType
 tcTExpTy m_ty exp_ty
   = do { unless (isTauTy exp_ty) $ addErr (err_msg exp_ty)
-       ; texp <- tcLookupTyCon tExpTyConName
+       ; codeCon <- tcLookupTyCon codeTyConName
        ; let rep = getRuntimeRep exp_ty
-       ; return (mkAppTy m_ty (mkTyConApp texp [rep, exp_ty])) }
+       ; return (mkTyConApp codeCon [rep, m_ty, exp_ty]) }
   where
     err_msg ty
       = vcat [ text "Illegal polytype:" <+> ppr ty
@@ -619,10 +619,10 @@ tcNestedSplice pop_stage (TcPending ps_var lie_var q@(QuoteWrapper _ m_var)) spl
        ; expr' <- setStage pop_stage $
                   setConstraintVar lie_var $
                   tcLExpr expr (mkCheckExpType meta_exp_ty)
-       ; untypeq <- tcLookupId unTypeQName
+       ; untypeCode <- tcLookupId unTypeCodeName
        ; let expr'' = mkHsApp
                         (mkLHsWrap (applyQuoteWrapper q)
-                          (nlHsTyApp untypeq [rep, res_ty])) expr'
+                          (nlHsTyApp untypeCode [rep, res_ty])) expr'
        ; ps <- readMutVar ps_var
        ; writeMutVar ps_var (PendingTcSplice splice_name expr'' : ps)
 
