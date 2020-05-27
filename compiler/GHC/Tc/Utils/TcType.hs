af98d24e979d6349cd2e24d3850d685df06d215c
Daniel Rogozin <daniel.rogozin@serokell.io>
2020-05-27 13:35:24 +0300

    the char kind: type families, functionality, and submodule updates
    Co-authored-by: Rinat Stryungis <rinat.stryungis@serokell.io>
    
    Metric Decrease:
        T5205
    
    Metric Increase:
        Naperian
        T13035

--- compiler/GHC/Tc/Utils/TcType.hs
+++ compiler/GHC/Tc/Utils/TcType.hs
@@ -1166,6 +1166,7 @@ getDFunTyKey t@(CoercionTy _)        = pprPanic "getDFunTyKey" (ppr t)
 getDFunTyLitKey :: TyLit -> OccName
 getDFunTyLitKey (NumTyLit n) = mkOccName Name.varName (show n)
 getDFunTyLitKey (StrTyLit n) = mkOccName Name.varName (show n)  -- hm
+getDFunTyLitKey (CharTyLit n) = mkOccName Name.varName (show n)
 
 {- *********************************************************************
 *                                                                      *
