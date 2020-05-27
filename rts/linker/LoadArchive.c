07b2185689562132d76c773c04b160fe7dfdfd10
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-05-27 13:13:49 +0300

    WIP implementing new plan

--- rts/linker/LoadArchive.c
+++ rts/linker/LoadArchive.c
@@ -536,8 +536,10 @@ static HsInt loadArchive_ (pathchar *path)
                 fclose(f);
                 return 0;
             } else {
+                /* TODO(osa): Update this code
                 oc->next = objects;
                 objects = oc;
+                */
             }
         }
         else if (isGnuIndex) {
