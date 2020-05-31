09596c07a5bac49982b0980d1a4aebaa36b02fcb
Zubin Duggal <zubin@cmi.ac.in>
2020-06-01 00:12:33 +0530

    Simplify contexts

--- compiler/GHC/Parser/PostProcess/Haddock.hs
+++ compiler/GHC/Parser/PostProcess/Haddock.hs
@@ -14,29 +14,24 @@ import Control.Monad
 -- -----------------------------------------------------------------------------
 -- Adding documentation to record fields (used in parsing).
 
-addFieldDoc :: XRec a (ConDeclField a) ~ Located (ConDeclField a)
-            => LConDeclField a -> Maybe LHsDocString -> LConDeclField a
+addFieldDoc :: Located (ConDeclField a) -> Maybe LHsDocString -> Located (ConDeclField a)
 addFieldDoc (L l fld) doc
   = L l (fld { cd_fld_doc = cd_fld_doc fld `mplus` doc })
 
-addFieldDocs :: XRec a (ConDeclField a) ~ Located (ConDeclField a)
-             => [LConDeclField a] -> Maybe LHsDocString -> [LConDeclField a]
+addFieldDocs :: [Located (ConDeclField a)] -> Maybe LHsDocString -> [Located (ConDeclField a)]
 addFieldDocs [] _ = []
 addFieldDocs (x:xs) doc = addFieldDoc x doc : xs
 
 
-addConDoc :: XRec pass (ConDecl pass) ~ Located (ConDecl pass) =>
-             LConDecl pass -> Maybe LHsDocString -> LConDecl pass
+addConDoc :: Located (ConDecl pass) -> Maybe LHsDocString -> Located (ConDecl pass)
 addConDoc decl    Nothing = decl
 addConDoc (L p c) doc     = L p ( c { con_doc = con_doc c `mplus` doc } )
 
-addConDocs :: XRec pass (ConDecl pass) ~ Located (ConDecl pass) =>
-              [LConDecl pass] -> Maybe LHsDocString -> [LConDecl pass]
+addConDocs :: [Located (ConDecl pass)] -> Maybe LHsDocString -> [Located (ConDecl pass)]
 addConDocs [] _ = []
 addConDocs [x] doc = [addConDoc x doc]
 addConDocs (x:xs) doc = x : addConDocs xs doc
 
-addConDocFirst :: XRec pass (ConDecl pass) ~ Located (ConDecl pass) =>
-                  [LConDecl pass] -> Maybe LHsDocString -> [LConDecl pass]
+addConDocFirst :: [Located (ConDecl pass)] -> Maybe LHsDocString -> [Located (ConDecl pass)]
 addConDocFirst [] _ = []
 addConDocFirst (x:xs) doc = addConDoc x doc : xs
