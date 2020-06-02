4b19d7587eb1cf592e07ae46541fbea494ee8c74
fendor <power.walross@gmail.com>
2020-06-02 16:09:20 +0200

    Adapt testcase to changed API

--- testsuite/tests/ghc-api/annotations/t11430.hs
+++ testsuite/tests/ghc-api/annotations/t11430.hs
@@ -35,6 +35,7 @@ testOneFile libdir fileName = do
                         setSessionDynFlags dflags
                         let mn =mkModuleName fileName
                         addTarget Target { targetId = TargetModule mn
+                                         , targetPackage = homeUnitId dflags
                                          , targetAllowObjCode = True
                                          , targetContents = Nothing }
                         load LoadAllTargets
