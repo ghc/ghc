8099f1a42116a6fe8da02e9d06a7ae40331110be
Brian Jaress <bjaress@gmail.com>
2020-06-07 13:11:44 -0700

    alpine-static-flavour -- started static flavour

---
+++ hadrian/src/Settings/Flavours/Static.hs
@@ -0,0 +1,18 @@
+module Settings.Flavours.Static (staticFlavour) where
+
+import Expression
+import Flavour
+import {-# SOURCE #-} Settings.Default
+
+-- Please update doc/flavours.md when changing this file.
+staticFlavour :: Flavour
+staticFlavour = defaultFlavour
+    { name = "broken_static"
+    , args = defaultBuilderArgs <> performanceArgs <> defaultPackageArgs }
+
+performanceArgs :: Args
+performanceArgs = sourceArgs SourceArgs
+    { hsDefault  = pure ["-O", "-H64m"]
+    , hsLibrary  = notStage0 ? arg "-O2"
+    , hsCompiler = pure ["-O2"]
+    , hsGhc      = mconcat [stage0 ? arg "-O", notStage0 ? arg "-O2"] }
