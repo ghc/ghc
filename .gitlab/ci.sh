7ba7d889e309c9b1be98b5036d3538d0337d3849
Brian Jaress <bjaress@gmail.com>
2020-06-07 13:11:44 -0700

    alpine-static-flavour -- created static flavour

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -464,6 +464,19 @@ function test_hadrian() {
     return
   fi
 
+  if [[ "${FLAVOUR}" = *static* ]]
+  then
+    for binary in _build/stage1/bin/*
+    do
+      if ldd "${binary}" &> /dev/null
+      then
+        echo "${binary} is not static!"
+        ldd "${binary}"
+        exit 1
+      fi
+    done
+  fi
+
   cd _build/bindist/ghc-*/
   run ./configure --prefix="$TOP"/_build/install
   run "$MAKE" install
