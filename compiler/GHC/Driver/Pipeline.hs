0e3fc9504e10858616b3d12279ec38a6ab6a04a6
fendor <power.walross@gmail.com>
2020-06-08 12:54:42 +0200

    Revert changes to compiler/GHC/Driver/Finder.hs

--- compiler/GHC/Driver/Pipeline.hs
+++ compiler/GHC/Driver/Pipeline.hs
@@ -1113,8 +1113,8 @@ runPhase (RealPhase (Hsc src_flavour)) input_fn dflags0
         PipeState{hsc_env=hsc_env'} <- getPipeState
 
   -- Tell the finder cache about this module
-        mod <- liftIO $ addHomeModuleToFinder hsc_env' mod_name location $
-            hsc_currentPackage hsc_env'
+        mod <- liftIO $ addHomeModuleToFinder hsc_env' mod_name location
+          --  $ hsc_currentPackage hsc_env'
 
   -- Make the ModSummary to hand to hscMain
         let
