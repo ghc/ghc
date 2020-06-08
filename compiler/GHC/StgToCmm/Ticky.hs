75819b0d465e802cab116902dd583ac8469b6a99
Sylvain Henry <sylvain@haskus.fr>
2020-06-08 18:33:41 +0200

    CmmToC: don't add extern decl to parsed Cmm data
    Previously, if a .cmm file *not in the RTS* contained something like:
    
    ```cmm
    section "rodata" { msg : bits8[] "Test\n"; }
    ```
    
    It would get compiled by CmmToC into:
    
    ```c
    ERW_(msg);
    const char msg[] = "Test\012";
    ```
    
    and fail with:
    
    ```
    /tmp/ghc32129_0/ghc_4.hc:5:12: error:
         error: conflicting types for \u2018msg\u2019
         const char msg[] = "Test\012";
                    ^~~
    
    In file included from /tmp/ghc32129_0/ghc_4.hc:3:0: error:
    
    /tmp/ghc32129_0/ghc_4.hc:4:6: error:
         note: previous declaration of \u2018msg\u2019 was here
         ERW_(msg);
              ^
    
    /builds/hsyl20/ghc/_build/install/lib/ghc-8.11.0.20200605/lib/../lib/x86_64-linux-ghc-8.11.0.20200605/rts-1.0/include/Stg.h:253:46: error:
         note: in definition of macro \u2018ERW_\u2019
         #define ERW_(X)   extern       StgWordArray (X)
                                                      ^
    ```
    
    See the rationale for this on https://gitlab.haskell.org/ghc/ghc/-/wikis/commentary/compiler/backends/ppr-c#prototypes
    
    Now we don't generate these extern declarations (ERW_, etc.) for
    top-level data. It shouldn't change anything for the RTS (the only place
    we use .cmm files) as it is already special cased in
    `GHC.Cmm.CLabel.needsCDecl`. And hand-written Cmm can use explicit
    extern declarations when needed.
    
    Note that it allows `cgrun069` test to pass with CmmToC (cf #15467).

--- compiler/GHC/StgToCmm/Ticky.hs
+++ compiler/GHC/StgToCmm/Ticky.hs
@@ -115,7 +115,6 @@ import GHC.Cmm.Utils
 import GHC.Cmm.CLabel
 import GHC.Runtime.Heap.Layout
 
-import GHC.Unit
 import GHC.Types.Name
 import GHC.Types.Id
 import GHC.Types.Basic
@@ -356,7 +355,7 @@ registerTickyCtr ctr_lbl = do
         , mkStore (CmmLit (cmmLabelOffB ctr_lbl
                                 (oFFSET_StgEntCounter_registeredp dflags)))
                    (mkIntExpr platform 1) ]
-    ticky_entry_ctrs = mkLblExpr (mkCmmDataLabel rtsUnitId (fsLit "ticky_entry_ctrs"))
+    ticky_entry_ctrs = mkLblExpr (mkRtsCmmDataLabel (fsLit "ticky_entry_ctrs"))
   emit =<< mkCmmIfThen test (catAGraphs register_stmts)
 
 tickyReturnOldCon, tickyReturnNewCon :: RepArity -> FCode ()
@@ -498,12 +497,12 @@ tickyAllocHeap genuine hp
                      bytes,
             -- Bump the global allocation total ALLOC_HEAP_tot
             addToMemLbl (bWord platform)
-                        (mkCmmDataLabel rtsUnitId (fsLit "ALLOC_HEAP_tot"))
+                        (mkRtsCmmDataLabel (fsLit "ALLOC_HEAP_tot"))
                         bytes,
             -- Bump the global allocation counter ALLOC_HEAP_ctr
             if not genuine then mkNop
             else addToMemLbl (bWord platform)
-                             (mkCmmDataLabel rtsUnitId (fsLit "ALLOC_HEAP_ctr"))
+                             (mkRtsCmmDataLabel (fsLit "ALLOC_HEAP_ctr"))
                              1
             ]}
 
@@ -567,13 +566,13 @@ ifTickyDynThunk :: FCode () -> FCode ()
 ifTickyDynThunk code = tickyDynThunkIsOn >>= \b -> when b code
 
 bumpTickyCounter :: FastString -> FCode ()
-bumpTickyCounter lbl = bumpTickyLbl (mkCmmDataLabel rtsUnitId lbl)
+bumpTickyCounter lbl = bumpTickyLbl (mkRtsCmmDataLabel lbl)
 
 bumpTickyCounterBy :: FastString -> Int -> FCode ()
-bumpTickyCounterBy lbl = bumpTickyLblBy (mkCmmDataLabel rtsUnitId lbl)
+bumpTickyCounterBy lbl = bumpTickyLblBy (mkRtsCmmDataLabel lbl)
 
 bumpTickyCounterByE :: FastString -> CmmExpr -> FCode ()
-bumpTickyCounterByE lbl = bumpTickyLblByE (mkCmmDataLabel rtsUnitId lbl)
+bumpTickyCounterByE lbl = bumpTickyLblByE (mkRtsCmmDataLabel lbl)
 
 bumpTickyEntryCount :: CLabel -> FCode ()
 bumpTickyEntryCount lbl = do
@@ -615,7 +614,7 @@ bumpHistogram lbl n = do
     emit (addToMem (bWord platform)
            (cmmIndexExpr platform
                 (wordWidth platform)
-                (CmmLit (CmmLabel (mkCmmDataLabel rtsUnitId lbl)))
+                (CmmLit (CmmLabel (mkRtsCmmDataLabel lbl)))
                 (CmmLit (CmmInt (fromIntegral offset) (wordWidth platform))))
            1)
 
