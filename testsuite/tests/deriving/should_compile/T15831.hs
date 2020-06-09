71006532abb88a53df7c7e0b3a5e2c8af99a48d1
Ryan Scott <ryan.gl.scott@gmail.com>
2020-06-09 18:13:35 -0400

    Reject nested foralls/contexts in instance types more consistently
    GHC is very wishy-washy about rejecting instance declarations with
    nested `forall`s or contexts that are surrounded by outermost
    parentheses. This can even lead to some strange interactions with
    `ScopedTypeVariables`, as demonstrated in #18240. This patch makes
    GHC more consistently reject instance types with nested
    `forall`s/contexts so as to prevent these strange interactions.
    
    On the implementation side, this patch tweaks `splitLHsInstDeclTy`
    and `getLHsInstDeclHead` to not look through parentheses, which can
    be semantically significant. I've added a
    `Note [No nested foralls or contexts in instance types]` in
    `GHC.Hs.Type` to explain why. This also introduces a
    `no_nested_foralls_contexts_err` function in `GHC.Rename.HsType` to
    catch nested `forall`s/contexts in instance types. This function is
    now used in `rnClsInstDecl` (for ordinary instance declarations) and
    `rnSrcDerivDecl` (for standalone `deriving` declarations), the latter
    of which fixes #18271.
    
    On the documentation side, this adds a new
    "Formal syntax for instance declaration types" section to the GHC
    User's Guide that presents a BNF-style grammar for what is and isn't
    allowed in instance types.
    
    Fixes #18240. Fixes #18271.

--- testsuite/tests/deriving/should_compile/T15831.hs
+++ testsuite/tests/deriving/should_compile/T15831.hs
@@ -13,21 +13,21 @@ newtype Age = MkAge Int
   deriving Ord
     via Const Int (Any :: k)
   deriving Read
-    via (forall k. Const Int (Any :: k))
+    via forall k. Const Int (Any :: k)
   deriving Show
     via Const Int a
   deriving Enum
     via Const Int (a :: k)
   deriving Bounded
-    via (forall a. Const Int a)
+    via forall a. Const Int a
   deriving Num
-    via (forall k (a :: k). Const Int a)
+    via forall k (a :: k). Const Int a
 
 newtype Age2 = MkAge2 Int
-deriving via                     Const Int Any         instance Eq      Age2
-deriving via                     Const Int (Any :: k)  instance Ord     Age2
-deriving via (forall k.          Const Int (Any :: k)) instance Read    Age2
-deriving via                     Const Int a           instance Show    Age2
-deriving via                     Const Int (a :: k)    instance Enum    Age2
-deriving via (forall a.          Const Int a)          instance Bounded Age2
-deriving via (forall k (a :: k). Const Int a)          instance Num     Age2
+deriving via                    Const Int Any        instance Eq      Age2
+deriving via                    Const Int (Any :: k) instance Ord     Age2
+deriving via forall k.          Const Int (Any :: k) instance Read    Age2
+deriving via                    Const Int a          instance Show    Age2
+deriving via                    Const Int (a :: k)   instance Enum    Age2
+deriving via forall a.          Const Int a          instance Bounded Age2
+deriving via forall k (a :: k). Const Int a          instance Num     Age2
