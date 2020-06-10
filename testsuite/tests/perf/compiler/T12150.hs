d1accf9aa3bff16bddb61ba556ee7b007d00ff17
Ben Gamari <ben@smart-cactus.org>
2020-06-10 13:28:55 -0400

    testsuite: Increase size of T12150
    As noted in #18319, this test was previously very fragile. Increase its
    size to make it more likely that its fails with its newly-increased
    acceptance threshold.
    
    Metric Increase:
        T12150

--- testsuite/tests/perf/compiler/T12150.hs
+++ testsuite/tests/perf/compiler/T12150.hs
@@ -8,11 +8,421 @@ data Result a = Success a | Error String
 
    ghc-7.10.3 -O :  0.3s
    ghc-8.0.1 -O  :  1.8s
+
+   Increased to 450 guards in June 2020, along with increasing size of
+   acceptance threshold. 0.4s compile time
 -}
 
 instance Functor Result where
     {-# INLINE fmap #-}
     fmap | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+         | bool = f
+
+         | bool = f
          | bool = f
          | bool = f
          | bool = f
