01c2b7c10a7a0fcb81819df844974380729ef5ae
Simon Peyton Jones <simonpj@microsoft.com>
2020-06-13 00:23:16 +0100

    Improve handling of data type return kinds
    Following a long conversation with Richard, this patch tidies up the
    handling of return kinds for data/newtype declarations (vanilla,
    family, and instance).
    
    I have substantially edited the Notes in TyCl, so they would
    bear careful reading.
    
    Fixes #18300.

--- compiler/GHC/Tc/Instance/Family.hs
+++ compiler/GHC/Tc/Instance/Family.hs
@@ -162,34 +162,27 @@ addressed yet.
 newFamInst :: FamFlavor -> CoAxiom Unbranched -> TcM FamInst
 -- Freshen the type variables of the FamInst branches
 newFamInst flavor axiom@(CoAxiom { co_ax_tc = fam_tc })
-  = ASSERT2( tyCoVarsOfTypes lhs `subVarSet` tcv_set, text "lhs" <+> pp_ax )
-    ASSERT2( lhs_kind `eqType` rhs_kind, text "kind" <+> pp_ax $$ ppr lhs_kind $$ ppr rhs_kind )
-    -- We used to have an assertion that the tyvars of the RHS were bound
-    -- by tcv_set, but in error situations like  F Int = a that isn't
-    -- true; a later check in checkValidFamInst rejects it
-    do { (subst, tvs') <- freshenTyVarBndrs tvs
-       ; (subst, cvs') <- freshenCoVarBndrsX subst cvs
-       ; dflags <- getDynFlags
-       ; let lhs'     = substTys subst lhs
-             rhs'     = substTy  subst rhs
-             tcvs'    = tvs' ++ cvs'
+  = do { -- Use lintAxiom to check that the axiom is well formed
+         dflags <- getDynFlags
        ; ifErrsM (return ()) $ -- Don't lint when there are errors, because
                                -- errors might mean TcTyCons.
                                -- See Note [Recover from validity error] in GHC.Tc.TyCl
          when (gopt Opt_DoCoreLinting dflags) $
-           -- Check that the types involved in this instance are well formed.
-           -- Do /not/ expand type synonyms, for the reasons discussed in
-           -- Note [Linting type synonym applications].
-           case lintTypes dflags tcvs' (rhs':lhs') of
+         case lintAxiom dflags axiom of
              Nothing       -> pure ()
              Just fail_msg -> pprPanic "Core Lint error in newFamInst" $
                               vcat [ fail_msg
                                    , ppr fam_tc
-                                   , ppr subst
-                                   , ppr tvs'
-                                   , ppr cvs'
-                                   , ppr lhs'
-                                   , ppr rhs' ]
+                                   , ppr tvs
+                                   , ppr cvs
+                                   , ppr lhs
+                                   , ppr rhs ]
+
+       -- Freshen the type variables
+       ; (subst, tvs') <- freshenTyVarBndrs tvs
+       ; (subst, cvs') <- freshenCoVarBndrsX subst cvs
+       ; let lhs'     = substTys subst lhs
+             rhs'     = substTy  subst rhs
        ; return (FamInst { fi_fam      = tyConName fam_tc
                          , fi_flavor   = flavor
                          , fi_tcs      = roughMatchTcs lhs
