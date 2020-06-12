6ce4b0b25649cf2f331fc2d283313db0854ffae5
Cale Gibbard <cgibbard@gmail.com>
2020-06-12 09:39:29 -0400

    Update error messages, and fix a couple of failing test cases. There's still an unexpected pass (ghci014) which I don't understand why it should fail, and some curious differences in UnsatFun.stderr.
    Update some test output

--- testsuite/tests/ghc-api/downsweep/OldModLocation.hs
+++ testsuite/tests/ghc-api/downsweep/OldModLocation.hs
@@ -54,7 +54,7 @@ main = do
     -- using the 'location' parameter we'd end up using the old location of
     -- the "B" module in this test. Make sure that doesn't happen.
 
-    hPrint stderr $ sort (map (ml_hs_file . ms_location) (rights emss))
+    hPrint stderr $ sort (map (ml_hs_file . ms_location) (map fst (rights emss)))
 
 writeMod :: [String] -> IO ()
 writeMod src@(head -> stripPrefix "module " -> Just (takeWhile (/=' ') -> mod))
