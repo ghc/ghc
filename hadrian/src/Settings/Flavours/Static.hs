3209fc958489d80f4451ba1590d933094606f97a
Brian Jaress <bjaress@gmail.com>
2020-06-13 12:19:30 -0700

    alpine-static-flavour -- using static flags

--- hadrian/src/Settings/Flavours/Static.hs
+++ hadrian/src/Settings/Flavours/Static.hs
@@ -4,10 +4,6 @@ import Expression
 import Flavour
 import {-# SOURCE #-} Settings.Default
 
-{-
-    TODO use these flags:
-    -static -optl -static -optP -static
--}
 -- Please update doc/flavours.md when changing this file.
 staticFlavour :: Flavour
 staticFlavour = defaultFlavour
@@ -19,4 +15,10 @@ staticArgs = sourceArgs SourceArgs
     { hsDefault  = pure ["-O", "-H64m"]
     , hsLibrary  = notStage0 ? arg "-O2"
     , hsCompiler = pure ["-O2"]
-    , hsGhc      = mconcat [stage0 ? arg "-O", notStage0 ? arg "-O2"] }
+    , hsGhc      = mconcat
+        [ stage0 ? arg "-O"
+        , notStage0 ? arg "-O2"
+        , "-static"
+        , "-optl", "-static"
+        , "-optP", "-static"
+        ] }
