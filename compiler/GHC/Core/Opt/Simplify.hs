7376e6be91b441f3a4937093be6b4a739412ab5e
Simon Peyton Jones <simonpj@microsoft.com>
2020-06-18 15:40:25 +0100

    Use dumpStyle when printing inlinings
    This just makes debug-printing consistent,
    and more informative.

--- compiler/GHC/Core/Opt/Simplify.hs
+++ compiler/GHC/Core/Opt/Simplify.hs
@@ -1932,7 +1932,7 @@ completeCall env var cont
 
     log_inlining doc
       = liftIO $ dumpAction dflags
-           (mkUserStyle alwaysQualify AllTheWay)
+           (mkDumpStyle alwaysQualify)
            (dumpOptionsFromFlag Opt_D_dump_inlinings)
            "" FormatText doc
 
