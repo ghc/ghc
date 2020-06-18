54112d5b9debf5d053379b8241899a1540585f13
fendor <power.walross@gmail.com>
2020-06-18 10:27:32 +0200

    Add a unit to Targets in testsuite

--- testsuite/tests/ghc-api/annotations-literals/parsed.hs
+++ testsuite/tests/ghc-api/annotations-literals/parsed.hs
@@ -9,7 +9,6 @@ import Data.List (intercalate)
 import System.IO
 import GHC
 import GHC.Driver.Session
-import GHC.Driver.Types
 import GHC.Utils.Monad
 import GHC.Utils.Outputable
 import GHC.Data.Bag (filterBag,isEmptyBag)
@@ -28,9 +27,8 @@ testOneFile libdir fileName = do
         dflags <- getSessionDynFlags
         setSessionDynFlags dflags
         let mn =mkModuleName fileName
-        unitId <- hsc_currentPackage <$> getSession
         addTarget Target { targetId = TargetModule mn
-                         , targetPackage = unitId
+                         , targetPackage = homeUnitId dflags
                          , targetAllowObjCode = True
                          , targetContents = Nothing }
         load LoadAllTargets
