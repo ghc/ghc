8f758e86ac4a365401db010961597183cd36794e
fendor <power.walross@gmail.com>
2020-06-21 12:45:02 +0200

    Rename functions to use "unit"

--- compiler/GHC/Driver/Pipeline.hs
+++ compiler/GHC/Driver/Pipeline.hs
@@ -1110,7 +1110,7 @@ runPhase (RealPhase (Hsc src_flavour)) input_fn dflags0
 
   -- Tell the finder cache about this module
         mod <- liftIO $ addHomeModuleToFinder hsc_env' mod_name location
-                        $ hsc_currentPackage hsc_env'
+                        $ hsc_currentUnit hsc_env'
 
   -- Make the ModSummary to hand to hscMain
         let
