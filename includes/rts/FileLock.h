c1577409dcaad7944157164b1fb516f105606a60
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-06-22 13:48:52 +0200

    winio: add a note about file locking in the RTS.

--- includes/rts/FileLock.h
+++ includes/rts/FileLock.h
@@ -11,6 +11,22 @@
  *
  * ---------------------------------------------------------------------------*/
 
+/* Note [RTS File locking]
+ * ~~~~~~~~~~~~~~~~~~~~~~~
+ *
+ * The Haskell report dictates certain file locking behaviour.
+ * This is specified in the Haskell98 report under: 21.2.3  File locking
+ *
+ * GHC does not rely on the platform it's on to implement this.
+ * Instead we keep track of locked files in a data structure in
+ * the RTS. This file provides the interface to this data structure.
+ *
+ * In the base librarie we then use this interface to "lock" files.
+ * This means it's very much still possible for users outside of the
+ * rts/base library to open the files in question even if they are
+ * locked.
+ * */
+
 #pragma once
 
 #include "Stg.h"
