ded58a037774147073366ff2dfdc8965e02a96e5
Tamar Christina <tamar@zhox.com>
2020-06-24 22:25:41 +0100

    winio: update lockfile signature and remove mistaken symbol in rts.

--- includes/rts/FileLock.h
+++ includes/rts/FileLock.h
@@ -33,5 +33,5 @@
 
 /* No valid FD would be negative, so use a word instead of int so the value
    is compatible with a Windows handle.  */
-int  lockFile(StgWord id, StgWord64 dev, StgWord64 ino, int for_writing);
-int  unlockFile(StgWord id);
+int  lockFile(StgWord64 id, StgWord64 dev, StgWord64 ino, int for_writing);
+int  unlockFile(StgWord64 id);
