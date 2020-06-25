66e153f85a492168dad9c5b9424ba8142e2b9edc
Sven Tennie <sven.tennie@gmail.com>
2020-06-25 20:24:34 +0200

    Add test list_threads_and_misc_roots (#18405)
    It uses rts_listThreads() and rts_listMiscRoots().

---
+++ libraries/ghc-heap/tests/list_threads_and_misc_roots_c.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Rts.h"
+#include "RtsAPI.h"
+#include "list_threads_and_misc_roots_c.h"
+
+int tsoCount = 0;
+StgTSO** tsos;
+
+int miscRootsCount = 0;
+StgClosure** miscRoots;
+
+void collectTSOsCallback(void *user, StgTSO* tso){
+    tsoCount++;
+    tsos = realloc(tsos, sizeof(StgTSO*) * tsoCount);
+    tsos[tsoCount - 1] = tso;
+}
+
+void collectMiscRootsCallback(void *user, StgClosure* closure){
+    miscRootsCount++;
+    miscRoots = realloc(miscRoots, sizeof(StgClosure*) * miscRootsCount);
+    miscRoots[miscRootsCount - 1] = closure;
+}
+
+void* listThreads_thread(void* unused){
+    RtsPaused paused = rts_pause();
+    rts_listThreads(&collectTSOsCallback, NULL);
+    rts_listMiscRoots(&collectMiscRootsCallback, NULL);
+    rts_unpause(paused);
+
+    return NULL;
+}
+
+void listThreadsAndMiscRoots(void){
+    pthread_t threadId;
+    pthread_create(&threadId, NULL, &listThreads_thread, NULL);
+    pthread_join(threadId, NULL);
+}
+
+int getTSOCount(void){
+    return tsoCount;
+}
+
+StgTSO** getTSOs(void){
+    return tsos;
+}
+
+int getMiscRootsCount(void){
+    return miscRootsCount;
+}
+
+StgClosure** getMiscRoots(void){
+    return miscRoots;
+}
