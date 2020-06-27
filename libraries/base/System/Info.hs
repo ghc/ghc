127ee5cb2d3e35bfc2b8b24f07ec63fdc501fcad
Hécate <hecate+gitlab@glitchbra.in>
2020-06-27 23:04:19 +0200

    Add most common return values for `os` and `arch`

--- libraries/base/System/Info.hs
+++ libraries/base/System/Info.hs
@@ -11,9 +11,11 @@
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- Information about the characteristics of the host 
+-- Information about the characteristics of the host
 -- system lucky enough to run your program.
 --
+-- For a comprehensive listing of supported platforms, please refer to
+-- https://gitlab.haskell.org/ghc/ghc/-/wikis/platforms
 -----------------------------------------------------------------------------
 
 module System.Info
@@ -28,6 +30,10 @@ import Data.Version
 
 -- | The version of 'compilerName' with which the program was compiled
 -- or is being interpreted.
+-- 
+-- ==== __Example__
+-- > ghci> compilerVersion
+-- > Version {versionBranch = [8,8], versionTags = []}
 compilerVersion :: Version
 compilerVersion = Version [major, minor] []
   where (major, minor) = compilerVersionRaw `divMod` 100
@@ -35,15 +41,52 @@ compilerVersion = Version [major, minor] []
 #include "ghcplatform.h"
 
 -- | The operating system on which the program is running.
+-- Common values include:
+--
+--     * "darwin" — macOS
+--     * "freebsd"
+--     * "linux"
+--     * "linux-android"
+--     * "mingw32" — Windows
+--     * "netbsd"
+--     * "openbsd"
 os :: String
 os = HOST_OS
 
 -- | The machine architecture on which the program is running.
+-- Common values include:
+--
+--    * "aarch64"
+--    * "alpha"
+--    * "arm"
+--    * "hppa"
+--    * "hppa1_1"
+--    * "i386"
+--    * "ia64"
+--    * "m68k"
+--    * "mips"
+--    * "mipseb"
+--    * "mipsel"
+--    * "nios2"
+--    * "powerpc"
+--    * "powerpc64"
+--    * "powerpc64le"
+--    * "riscv32"
+--    * "riscv64"
+--    * "rs6000"
+--    * "s390"
+--    * "s390x"
+--    * "sh4"
+--    * "sparc"
+--    * "sparc64"
+--    * "vax"
+--    * "x86_64"
 arch :: String
 arch = HOST_ARCH
 
 -- | The Haskell implementation with which the program was compiled
 -- or is being interpreted.
+-- On the GHC platform, the value is "ghc".
 compilerName :: String
 compilerName = "ghc"
 
