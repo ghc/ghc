cdd81859f9e172e9114e0a037b675ea972ef80f9
Sylvain Henry <sylvain@haskus.fr>
2020-06-29 22:36:40 +0200

    Replace HscTarget with Backend
    They both have the same role and Backend name is more explicit.
    
    Metric Decrease:
        T3064
    
    Update Haddock submodule

--- testsuite/tests/ghc-api/apirecomp001/myghc.hs
+++ testsuite/tests/ghc-api/apirecomp001/myghc.hs
@@ -11,6 +11,7 @@ module Main where
 
 import GHC
 import GHC.Driver.Session
+import GHC.Driver.Backend
 import GHC.Utils.Monad ( MonadIO(..) )
 import GHC.Types.Basic ( failed )
 import GHC.Data.Bag        ( bagToList )
@@ -24,8 +25,8 @@ main = do
     dflags0 <- getSessionDynFlags
     (dflags, _, _) <- parseDynamicFlags dflags0
       (map (mkGeneralLocated "on the commandline") args)
-    setSessionDynFlags $ dflags { hscTarget = HscNothing
-                                , ghcLink  = LinkInMemory
+    setSessionDynFlags $ dflags { backend   = NoBackend
+                                , ghcLink   = LinkInMemory
                                 , verbosity = 0 -- silence please
                                 }
     root_mod <- guessTarget "A.hs" Nothing
@@ -35,7 +36,7 @@ main = do
     prn "target nothing: ok"
 
     dflags <- getSessionDynFlags
-    setSessionDynFlags $ dflags { hscTarget = HscInterpreted }
+    setSessionDynFlags $ dflags { backend = Interpreter }
     ok <- load LoadAllTargets
     when (failed ok) $ error "Couldn't load A.hs in interpreted mode"
     prn "target interpreted: ok"
