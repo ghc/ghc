062ccb46170aa1cc6ac0a2d018165696b5daa169
Sylvain Henry <sylvain@haskus.fr>
2020-06-29 19:58:56 +0200

    Replace ghcWithNativeCodeGen with a proper Backend datatype
    * Represent backends with a `Backend` datatype in GHC.Driver.Backend
    
    * Don't detect the default backend to use for the target platform at
      compile time in Hadrian/make but at runtime. It makes "Settings"
      simpler and it is a step toward making GHC multi-target.

---
+++ compiler/GHC/Driver/Backend.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE MultiWayIf #-}
+
+-- | Code generation backends
+module GHC.Driver.Backend
+   ( Backend (..)
+   , platformDefaultBackend
+   , platformNcgSupported
+   )
+where
+
+import GHC.Prelude
+import GHC.Platform
+
+-- | Backend
+data Backend
+   = NCG           -- ^ Native code generator backend
+   | LLVM          -- ^ LLVM backend
+   | ViaC          -- ^ Via-C backend
+   | Interpreter   -- ^ Interpreter
+   deriving (Eq,Ord,Show,Read)
+
+-- | Default backend to use for the given platform.
+platformDefaultBackend :: Platform -> Backend
+platformDefaultBackend platform = if
+      | platformUnregisterised platform -> ViaC
+      | platformNcgSupported platform
+        && ncgValidOs                   -> NCG
+      | otherwise                       -> LLVM
+   where
+      ncgValidOs = case platformOS platform of
+         OSAIX    -> False -- we want to use LLVM by default on AIX
+         OSDarwin -> False -- and IOS/Darwin
+         _        -> True
+
+
+-- | Is the platform supported by the Native Code Generator?
+platformNcgSupported :: Platform -> Bool
+platformNcgSupported platform = if
+      | platformUnregisterised platform -> False
+        -- NCG doesn't support unregisterised ABI
+      | ncgValidArch                    -> True
+      | otherwise                       -> False
+   where
+      ncgValidArch = case platformArch platform of
+         ArchX86     -> True
+         ArchX86_64  -> True
+         ArchPPC     -> True
+         ArchSPARC   -> True
+         _           -> False
