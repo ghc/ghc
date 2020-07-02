471fccd5268d651068eae9279373b97f3850f7fe
Wander Hillen <wjw.hillen@gmail.com>
2020-07-02 12:44:32 +0200

    Implement I/O event manager based on io_uring
    This adds an I/O event manager backend based on the io_uring interface
    available in modern Linux kernels. The relevant code is in
    /libraries/base/GHC/Event/IoUring.hsc. It also contains the following
    changes to support the new backend:
    
     * Port over the io_uring bindings from @bgamari into base.
     * Add a new HAVE_IO_URING check similar to HAVE_EPOLL, HAVE_KQUEUE etc.
       All changes are no-opped if this flag is not set.
    
    See also: #18390

--- rts/RtsFlags.c
+++ rts/RtsFlags.c
@@ -503,7 +503,6 @@ usage_text[] = {
 "            The number of worker threads to use in the native I/O manager to",
 "            handle completion events. (defualt: num cores)",
 #endif
-"  -e<n>     Maximum number of outstanding local sparks (default: 4096)",
 #endif
 #if defined(x86_64_HOST_ARCH)
 #if !DEFAULT_LINKER_ALWAYS_PIC
