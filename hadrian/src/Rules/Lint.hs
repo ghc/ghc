265a353140ad1bd9f3a44aa62cbb63be3e050ab4
Hécate <hecate+gitlab@glitchbra.in>
2020-07-06 01:32:15 +0200

    Add a Lint hadrian rule and an .hlint.yaml file in base/

---
+++ hadrian/src/Rules/Lint.hs
@@ -0,0 +1,20 @@
+module Rules.Lint
+  ( lint
+  ) where
+
+import Base
+import System.Process (system)
+
+lintRules :: Rules ()
+lintRules = "lint" ~> lint
+
+lint :: Action ()
+lint = do
+    putBuild "| Running the linter…"
+    lintBase
+    putSuccess "| Done."
+
+lintBase :: Action ()
+lintBase = do 
+    _ <- liftIO system "hlint -j --cpp-define x86_64_HOST_ARCH --cpp-include=./libraries/base/include/ --cpp-include=includes --cpp-include=./_build/stage1/lib/ -h libraries/base/.hlint.yaml libraries/base"
+    pure ()
