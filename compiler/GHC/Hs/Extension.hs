54f64e1ad8e9e2554c6a9e58e01f2eb12e7c9450
Alan Zimmerman <alan.zimm@gmail.com>
2020-07-06 22:57:54 +0100

    Proof of Concept implementation of in-tree API Annotations
    This MR introduces a possible machinery to introduce API Annotations
    into the TTG extension points.

--- compiler/GHC/Hs/Extension.hs
+++ compiler/GHC/Hs/Extension.hs
@@ -28,9 +28,10 @@ import Language.Haskell.Syntax.Extension
 import GHC.Types.Name
 import GHC.Types.Name.Reader
 import GHC.Types.Var
-import GHC.Utils.Outputable
-import GHC.Types.SrcLoc (Located, unLoc, noLoc)
+import GHC.Utils.Outputable hiding ((<>))
+import GHC.Types.SrcLoc (GenLocated(..), unLoc)
 import GHC.Utils.Panic
+import GHC.Parser.Annotation
 
 {-
 Note [IsPass]
@@ -67,7 +68,7 @@ Type. We never build an HsType GhcTc. Why do this? Because we need to be
 able to compare type-checked types for equality, and we don't want to do
 this with HsType.
 
-This causes wrinkles within the AST, where we normally thing that the whole
+This causes wrinkles within the AST, where we normally think that the whole
 AST travels through the GhcPs --> GhcRn --> GhcTc pipeline as one. So we
 have the NoGhcTc type family, which just replaces GhcTc with GhcRn, so that
 user-written types can be preserved (as HsType GhcRn) even in e.g. HsExpr GhcTc.
@@ -94,14 +95,22 @@ saying that NoGhcTcPass is idempotent.
 
 -}
 
-type instance XRec (GhcPass p) a = Located a
+type instance XRec (GhcPass p) a = GenLocated (Anno a) a
+
+type instance Anno RdrName = SrcSpanAnnName
+type instance Anno Name    = SrcSpanAnnName
+type instance Anno Id      = SrcSpanAnnName
+
+type IsSrcSpanAnn p a = ( Anno (IdGhcP p) ~ SrcSpanAnn' (ApiAnn' a),
+                          IsPass p)
 
 instance UnXRec (GhcPass p) where
   unXRec = unLoc
 instance MapXRec (GhcPass p) where
   mapXRec = fmap
-instance WrapXRec (GhcPass p) where
-  wrapXRec = noLoc
+
+-- instance WrapXRec (GhcPass p) a where
+--   wrapXRec = noLocA
 
 {-
 Note [NoExtCon and strict fields]
@@ -203,6 +212,9 @@ type family NoGhcTcPass (p :: Pass) :: Pass where
 type OutputableBndrId pass =
   ( OutputableBndr (IdGhcP pass)
   , OutputableBndr (IdGhcP (NoGhcTcPass pass))
+  -- AZ: suspect the next two are not necessary
+  , Outputable (GenLocated (Anno (IdGhcP pass)) (IdGhcP pass))
+  , Outputable (GenLocated (Anno (IdGhcP (NoGhcTcPass pass))) (IdGhcP (NoGhcTcPass pass)))
   , IsPass pass
   )
 
--- testsuite/tests/printer/Ppr011.hs
+++ testsuite/tests/printer/Ppr011.hs
@@ -8,8 +8,8 @@ data Foo = A
          | C
 
 --         | data_or_newtype capi_ctype tycl_hdr constrs deriving
-data {-# Ctype "Foo" "bar" #-} F1 = F1
-data {-# Ctype       "baz" #-} Eq a =>  F2 a = F2 a
+data {-# Ctype  "Foo"   "bar" #-}  F1             = F1
+data {-# Ctype          "baz" #-}  Eq  a =>  F2 a = F2 a
 
 data (Eq a,Ord a) => F3 a = F3 Int a
 
@@ -18,10 +18,11 @@ data F4 a = forall x y. (Eq x,Eq y) => F4 a x y
 
 
 data G1 a :: * where
-  G1A,  G1B :: Int -> G1 a
-  G1C :: Double -> G1 a
+  G1A,  G1B  ::  Int  ->  G1  a
+  G1C  ::  G1 a ->  G1 a
+  G1D  ::  G1 a -> (Int -> G1 a)
 
-data G2 a :: * where
+data G2 a ::  * where
   G2A :: { g2a :: a, g2b :: Int } -> G2 a
   G2C :: Double -> G2 a
 
@@ -32,3 +33,13 @@ data (Eq a,Ord a) => G3 a = G3
   , g3B :: Bool
   , g3a :: a
   } deriving (Eq,Ord)
+
+data G4 a :: * where
+  G4A,  G4B  ::  Int  ->  G4  a
+  G4C  :: {- A -} G4 {- B -}a {- C -} -> {- D -} G4 {- E -}a
+  G4D  ::  {- A -}G4 {- B -}a {- C -} -> {- D -}( {- E -}Int{- F -} -> {- G -}G4 {- H -}a {- I -})
+
+ff x =
+  case  x  of
+    1 -> True
+    _ -> False
