2defec93e0468e0154cae55228edd6a4ed05488b
Alan Zimmerman <alan.zimm@gmail.com>
2020-07-06 22:57:54 +0100

    Proof of Concept implementation of in-tree API Annotations
    This MR introduces a possible machinery to introduce API Annotations
    into the TTG extension points.

---
+++ utils/check-exact/cases/AddHiding1.expected.hs
@@ -0,0 +1,8 @@
+module AddHiding1 where
+
+import Data.Maybe hiding (n1,n2)
+
+import Data.Maybe hiding (n1,n2)
+
+f = 1
+
---
+++ utils/check-exact/cases/LocalDecls2.expected.hs
@@ -0,0 +1,8 @@
+module LocalDecls2 where
+
+foo a = bar a
+  where
+    nn :: Int
+    nn = 2
+
+
