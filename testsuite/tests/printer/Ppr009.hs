494f55ec7ce17302c345e961d3a0b14da80ad72e
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2020-07-11 03:02:09 +0200

    Remove {-# CORE #-} pragma (part of #18048)
    This pragma has no effect since 2011.
    It was introduced for External Core, which no longer exists.
    
    Updates haddock submodule.

--- testsuite/tests/printer/Ppr009.hs
+++ testsuite/tests/printer/Ppr009.hs
@@ -3,7 +3,6 @@ module Ppr009 where
 
 {-# INLINE strictStream #-}
 strictStream (Bitstream l v)
-    = {-# CORE "Strict Bitstream stream" #-}
-      S.concatMap stream (GV.stream v)
+    = S.concatMap stream (GV.stream v)
       `S.sized`
       Exact l
