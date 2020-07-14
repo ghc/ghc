watashi/fbghc

Always retain bindings for interactive module
Summary: Retaining all bindings from interpreted modules consumes a lot of memory and we have seen this causing problems on some of our machines with less memory. This adds a flag to only retain modules for explicitly added targets. This set of modules is presumably much smaller than the full graph dependencies and should allow us to save a lot of memory.

Test Plan:
 - Built and test memory usage of ghci with `-fminimal-bindings-retention`

Reviewers: watashi, smarlow, #sigma

Reviewed By: smarlow

Subscribers: smarlow

compiler/deSugar/Desugar.hs
@@ -296,7 +296,6 @@ deSugar hsc_env
                      (const ()) $
      do { -- Desugar the program
         ; let export_set = availsToNameSet exports
-              target     = hscTarget dflags
               hpcInfo    = emptyHpcInfo other_hpc_info
 
         ; (binds_cvr, ds_hpc_info, modBreaks)
@@ -334,7 +333,7 @@ deSugar hsc_env
      do {       -- Add export flags to bindings
           keep_alive <- readIORef keep_var
         ; let (rules_for_locals, rules_for_imps) = partition isLocalRule all_rules
-              final_prs = addExportFlagsAndRules target export_set keep_alive
+              final_prs = addExportFlagsAndRules hsc_env mod export_set keep_alive
                                                  rules_for_locals (fromOL all_prs)
 
               final_pgm = combineEvBinds ds_ev_binds final_prs
@@ -462,9 +461,9 @@ deSugarExpr hsc_env tc_expr
 -}
 
 addExportFlagsAndRules
-    :: HscTarget -> NameSet -> NameSet -> [CoreRule]
+    :: HscEnv -> Module -> NameSet -> NameSet -> [CoreRule]
     -> [(Id, t)] -> [(Id, t)]
-addExportFlagsAndRules target exports keep_alive rules prs
+addExportFlagsAndRules hsc_env mod exports keep_alive rules prs
   = mapFst add_one prs
   where
     add_one bndr = add_rules name (add_export name bndr)
@@ -500,8 +499,8 @@ addExportFlagsAndRules target exports keep_alive rules prs
         -- isExternalName separates the user-defined top-level names from those
         -- introduced by the type checker.
     is_exported :: Name -> Bool
-    is_exported | targetRetainsAllBindings target = isExternalName
-                | otherwise                       = (`elemNameSet` exports)
+    is_exported | moduleRetainsAllBindings hsc_env mod = isExternalName
+                | otherwise                            = (`elemNameSet` exports)
 
 {-
 Note [Adding export flags]

ghc/ghc

(cherry picked from commit cac16469f09675acefe0b93d7be0f5c3e1a5d8ba)

compiler/deSugar/Desugar.hs
@@ -119,7 +119,6 @@ deSugar hsc_env
                      (const ()) $
      do { -- Desugar the program
         ; let export_set = availsToNameSet exports
-              target     = hscTarget dflags
               hpcInfo    = emptyHpcInfo other_hpc_info
 
         ; (binds_cvr, ds_hpc_info, modBreaks)
@@ -148,8 +147,9 @@ deSugar hsc_env
      do {       -- Add export flags to bindings
           keep_alive <- readIORef keep_var
         ; let (rules_for_locals, rules_for_imps) = partition isLocalRule all_rules
-              final_prs = addExportFlagsAndRules target export_set keep_alive
-                                                 rules_for_locals (fromOL all_prs)
+              final_prs = addExportFlagsAndRules hsc_env export_set keep_alive
+                                                 mod rules_for_locals
+                                                 (fromOL all_prs)
 
               final_pgm = combineEvBinds ds_ev_binds final_prs
         -- Notice that we put the whole lot in a big Rec, even the foreign binds
@@ -283,9 +283,9 @@ deSugarExpr hsc_env tc_expr = do {
 -}
 
 addExportFlagsAndRules
-    :: HscTarget -> NameSet -> NameSet -> [CoreRule]
+    :: HscEnv -> NameSet -> NameSet -> Module -> [CoreRule]
     -> [(Id, t)] -> [(Id, t)]
-addExportFlagsAndRules target exports keep_alive rules prs
+addExportFlagsAndRules hsc_env exports keep_alive mod rules prs
   = mapFst add_one prs
   where
     add_one bndr = add_rules name (add_export name bndr)
@@ -321,8 +321,8 @@ addExportFlagsAndRules target exports keep_alive rules prs
         -- isExternalName separates the user-defined top-level names from those
         -- introduced by the type checker.
     is_exported :: Name -> Bool
-    is_exported | targetRetainsAllBindings target = isExternalName
-                | otherwise                       = (`elemNameSet` exports)
+    is_exported | moduleRetainsAllBindings hsc_env mod = isExternalName
+                | otherwise                            = (`elemNameSet` exports)
 
 {-
 Note [Adding export flags]