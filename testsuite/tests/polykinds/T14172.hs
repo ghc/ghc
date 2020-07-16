00080feb9d17967ed1a67f632b490e7b2e1308de
Simon Peyton Jones <simonpj@microsoft.com>
2020-07-16 14:35:42 +0100

    Improve kind generalisation, error messages
    This patch does two things:
    
    * It refactors GHC.Tc.Errors a bit.  In debugging Quick Look I was
      forced to look in detail at error messages, and ended up doing a bit
      of refactoring, esp in mkTyVarEqErr'.  It's still quite a mess, but
      a bit better, I think.
    
    * It makes a significant improvement to the kind checking of type and
      class declarations. Specifically, we now ensure that if kind
      checking fails with an unsolved constraint, all the skolems are in
      scope.  That wasn't the case before, which led to some obscure error
      messages; and occasional failures with "no skolem info" (eg #16245).
    
    Both of these, and the main Quick Look patch itself, affect a /lot/ of
    error messages, as you can see from the number of files changed.  I've
    checked them all; I think they are as good or better than before.
    
    Smaller things
    
    * I documented the various instances of VarBndr better.
      See Note [The VarBndr tyep and its uses] in GHC.Types.Var
    
    * Renamed GHC.Tc.Solver.simpl_top to simplifyTopWanteds
    
    * A bit of refactoring in bindExplicitTKTele, to avoid the
      footwork with Either.  Simpler now.
    
    * Move promoteTyVar from GHC.Tc.Solver to GHC.Tc.Utils.TcMType
    
    Fixes #16245 (comment 211369), memorialised as
      typecheck/polykinds/T16245a
    Also fixes the three bugs in #18640

--- testsuite/tests/polykinds/T14172.hs
+++ testsuite/tests/polykinds/T14172.hs
@@ -5,3 +5,8 @@ import T14172a
 
 traverseCompose :: (a -> f b) -> g a -> f (h _)
 traverseCompose = _Wrapping Compose . traverse
+
+-- traverse :: (Traversable t, Applicative f) => (a -> f b) -> t a -> f (t b)
+-- (.) :: (y->z) -> (x->y) -> (x -> z)
+-- x := a -> f b
+-- z := g a -> f (h a1)
