e158f6f7176a096d104c90c1e07b58271e38c67b
Brian Jaress <bjaress@gmail.com>
2020-07-26 10:21:41 -0700

    alpine-static-flavour -- getting perf args from perf flavor

--- hadrian/src/Settings/Flavours/Static.hs
+++ hadrian/src/Settings/Flavours/Static.hs
@@ -4,11 +4,13 @@ import Expression
 import Flavour
 import {-# SOURCE #-} Settings.Default
 
+import Settings.Flavours.Performance (performanceArgs)
+
 -- Please update doc/flavours.md when changing this file.
 staticFlavour :: Flavour
 staticFlavour = defaultFlavour
     { name = "static"
-    , args = defaultBuilderArgs <> perfArgs <> defaultPackageArgs <> staticExec
+    , args = defaultBuilderArgs <> performanceArgs <> defaultPackageArgs <> staticExec
     , dynamicGhcPrograms = return False }
 
 
@@ -18,11 +20,3 @@ staticExec = not . wayUnit Dynamic <$> getWay ? mconcat
     , builder (Ghc CompileCWithGhc) ? pure [ "-fPIC", "-optc", "-static"]
     , builder (Ghc LinkHs) ? pure [ "-optl", "-static" ]
     ]
-
--- Same as in the performance flavour
-perfArgs :: Args
-perfArgs = sourceArgs SourceArgs
-    { hsDefault  = pure ["-O", "-H64m"]
-    , hsLibrary  = notStage0 ? arg "-O2"
-    , hsCompiler = pure ["-O2"]
-    , hsGhc      = mconcat [stage0 ? arg "-O", notStage0 ? arg "-O2"] }
