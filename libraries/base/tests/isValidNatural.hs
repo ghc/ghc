4d069cf6e506e4a5de343b5d59ec9f29cc1ab3a4
Sylvain Henry <sylvain@haskus.fr>
2020-07-30 17:30:09 +0200

    Bignum: add backward compat integer-gmp functions
    Also enhance bigNatCheck# and isValidNatural test

--- libraries/base/tests/isValidNatural.hs
+++ libraries/base/tests/isValidNatural.hs
@@ -3,8 +3,16 @@
 import GHC.Num.Natural
 import GHC.Num.BigNat
 import GHC.Exts
+import GHC.IO
 
-main = print $ map naturalCheck [0, 1, maxWord, maxWord + 1, invalid]
-  where
-    maxWord = fromIntegral (maxBound :: Word)
-    invalid = NB (bigNatOne# (# #)) -- 1 would fit into the NS constructor.
+main = do
+   let
+      maxWord = fromIntegral (maxBound :: Word)
+      invalid = NB (bigNatOne# (# #)) -- 1 would fit into the NS constructor.
+
+   -- byteArray whose size is not a multiple of Word size
+   invalid2 <- IO $ \s -> case newByteArray# 27# s of
+                           (# s', mba #) -> case unsafeFreezeByteArray# mba s' of
+                              (# s'', ba #) -> (# s'', NB ba #)
+
+   print $ map naturalCheck [0, 1, maxWord, maxWord + 1, invalid, invalid2]
