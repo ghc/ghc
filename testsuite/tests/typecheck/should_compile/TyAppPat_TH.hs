6202aa0cf8723ed4dedf6a32b12ddfedd3cc4a50
Cale Gibbard <cgibbard@gmail.com>
2020-07-30 12:57:30 -0400

    Some Template Haskell support for type applications in patterns.

--- testsuite/tests/typecheck/should_compile/TyAppPat_TH.hs
+++ testsuite/tests/typecheck/should_compile/TyAppPat_TH.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
+
+import Language.Haskell.TH
 
 apat :: Q Pat
 apat = [p| Just @[a] xs |]
