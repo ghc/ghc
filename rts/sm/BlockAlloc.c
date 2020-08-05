6b3044a038a94e323478eac4278632cd84f94274
David Eichmann <EichmannD@gmail.com>
2020-08-05 11:39:17 +0100

    Add code comments to allocator code

--- rts/sm/BlockAlloc.c
+++ rts/sm/BlockAlloc.c
@@ -549,6 +549,8 @@ allocGroupOnNode (uint32_t node, W_ n)
         ln++;
     }
 
+    // If no free blocks exist then allocate a new megablock and keep just a
+    // chunk of it.
     if (ln == NUM_FREE_LISTS) {
 #if 0  /* useful for debugging fragmentation */
         if ((W_)mblocks_allocated * BLOCKS_PER_MBLOCK * BLOCK_SIZE_W
@@ -1020,6 +1022,9 @@ freeGroup(bdescr *p)
   }
 
   // coalesce backwards
+  // Note that p is not a megablock/megagroup, so there are still live blocks on
+  // this megablock. Hence we can't coalesce backwards past the first block
+  // descriptor of this megablock.
   if (p != FIRST_BDESCR(MBLOCK_ROUND_DOWN(p)))
   {
       bdescr *prev;
