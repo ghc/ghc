4cfa7bb70f3cb62461021b9257a18e8fefa685b7
Sven Tennie <sven.tennie@gmail.com>
2020-08-09 17:24:05 +0200

    Add assertions to prof_info test (#18405)

--- libraries/ghc-heap/tests/prof_info.hs
+++ libraries/ghc-heap/tests/prof_info.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE ForeignFunctionInterface, MagicHash, CPP, BangPatterns #-}
 
+import Prelude
 import Foreign
 import Foreign.C.Types
 import GHC.Exts.Heap
 import GHC.Exts
+import Data.Functor
 
 import GHC.Word
+import Data.List (find)
 
 #include "ghcconfig.h"
 #include "rts/Constants.h"
@@ -18,6 +21,12 @@ foreign import ccall unsafe "create_tso.h create_tso"
 -- (which is a primop that expects a pointer to a closure).
 data FoolStgTSO
 
+createTSOClosure :: IO (GenClosure Box)
+createTSOClosure = do
+    ptr <- {-# SCC "MyCostCentre" #-} c_create_tso
+    let wPtr = unpackWord# ptr
+    getClosureData ((unsafeCoerce# wPtr) :: FoolStgTSO)
+
 -- We can make some assumptions about the - otherwise dynamic - properties of
 -- StgTSO and StgStack, because a new, non-running TSO is created with
 -- create_tso() (create_tso.c).create_tso
@@ -25,14 +34,31 @@ main :: IO ()
 main = do
     tso <- createTSOClosure
 
--- TODO: remove print, add assertion
-    print $ "tso : "++ show tso
+    let costCentre = prof tso >>= cccs <&> ccs_cc
 
-createTSOClosure :: IO (GenClosure Box)
-createTSOClosure = do
-    ptr <- c_create_tso
-    let wPtr = unpackWord# ptr
-    getClosureData ((unsafeCoerce# wPtr) :: FoolStgTSO)
+    case costCentre of
+        Nothing -> error $ "No CostCentre found in TSO: " ++ show tso
+        Just _ -> case findMyCostCentre (linkedCostCentres costCentre) of
+                    Just myCostCentre -> do
+                        assertEqual (cc_ccID myCostCentre) 1
+                        assertEqual (cc_label myCostCentre) "MyCostCentre"
+                        assertEqual (cc_module myCostCentre) "Main"
+                        assertEqual (cc_srcloc myCostCentre) (Just "prof_info.hs:26:39-50")
+                        assertEqual (cc_mem_alloc myCostCentre) 0
+                        assertEqual (cc_time_ticks myCostCentre) 0
+                        assertEqual (cc_is_caf myCostCentre) False
+                        assertEqual (cc_link myCostCentre) Nothing
+                    Nothing -> error "MyCostCentre not found!"
 
 unpackWord# :: Word -> Word#
 unpackWord# (W# w#) = w#
+
+linkedCostCentres :: Maybe CostCentre -> [CostCentre]
+linkedCostCentres Nothing = []
+linkedCostCentres (Just cc) = cc : linkedCostCentres (cc_link cc)
+
+findMyCostCentre:: [CostCentre] -> Maybe CostCentre
+findMyCostCentre ccs = find (\cc -> cc_label cc == "MyCostCentre") ccs
+
+assertEqual :: (Eq a, Show a) => a -> a -> IO ()
+assertEqual x y = if x == y then return () else error $ "assertEqual: " ++ show x ++ " /= " ++ show y
