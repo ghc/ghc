cf97889a38edc3314a7b61e6e0b6e6d0f434c8a2
Hécate <hecate+gitlab@glitchbra.in>
2020-08-11 19:57:05 +0200

    Re-add BangPatterns to CodePage.hs

--- libraries/base/GHC/IO/Encoding/CodePage.hs
+++ libraries/base/GHC/IO/Encoding/CodePage.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE NondecreasingIndentation #-}
 {-# LANGUAGE Trustworthy #-}
