4df1a44df87a653cd6061a0e918d266740658a78
Sven Tennie <sven.tennie@gmail.com>
2020-08-15 14:37:33 +0200

    END_TSO_QUEUE is not a closure type on it's own
    Indeed it's a CONSTR_NOCAF.

--- libraries/ghc-heap/cbits/utils.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "Rts.h"
-
-bool isEndTsoQueue(StgTSO* tso){
-    errorBelch("tso: %p", tso);
-    errorBelch("END_TSO_QUEUE: %p", END_TSO_QUEUE);
-    return tso == END_TSO_QUEUE;
-}
