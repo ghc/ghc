ff6ff6bcda1bad57453fd6ecaeb86630e6dec851
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-08-15 17:47:26 +0900

    problematic example

--- testsuite/tests/rename/should_compile/NoFieldSelectors.hs
+++ testsuite/tests/rename/should_compile/NoFieldSelectors.hs
@@ -8,7 +8,7 @@ where
 import Prelude
 
 data Foo = Foo { foo :: Int, bar :: String }
--- data Bar = Bar { foo :: Int, baz :: String }
+data Bar = Bar { barX :: Int, barY :: String }
 
 foo = 3 -- should not conflict
 fooX = foo + 1
@@ -22,3 +22,13 @@ foo1 = Foo 3 "bar"
 foo2 = Foo { foo = 3, bar = "bar" } -- disambiguate foo
 foo3 = foo1 { foo = 4 } -- update
 foo4 = foo1 { bar = "baz" } -- bar is unambiguous
+
+barY' :: Int
+barY' = barY
+{-
+NoFieldSelectors.hs:27:9: error:
+    • Couldn't match expected type ‘Int’
+                  with actual type ‘Bar -> String’
+    • In the expression: barY
+      In an equation for ‘barY'’: barY' = barY
+-}
