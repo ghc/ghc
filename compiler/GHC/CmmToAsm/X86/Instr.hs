61a9ca9ff9278fa03b92b324cf62085cfb6dc93d
Moritz Angermann <moritz.angermann@gmail.com>
2020-08-18 02:29:12 +0000

    Revert the Spill/Reload fix; undo :got: loads.
    This breaks dynamic, however we can build a working
    stage2 compiler with the following mk/build.mk
    
    BuildFlavour = quick
    
    ifneq "$(BuildFlavour)" ""
    include mk/flavours/$(BuildFlavour).mk
    endif
    
    STRIP_CMD = :
    
    DYNAMIC_BY_DEFAULT   = NO
    DYNAMIC_GHC_PROGRAMS = NO

--- compiler/GHC/CmmToAsm/X86/Instr.hs
+++ compiler/GHC/CmmToAsm/X86/Instr.hs
@@ -674,15 +674,15 @@ mkSpillInstr
     -> Reg      -- register to spill
     -> Int      -- current stack delta
     -> Int      -- spill slot to use
-    -> (Int, [Instr])
+    -> Instr
 
 mkSpillInstr config reg delta slot
   = let off     = spillSlotToOffset platform slot - delta
     in
     case targetClassOfReg platform reg of
-           RcInteger   -> (delta, [MOV (archWordFormat is32Bit)
-                                   (OpReg reg) (OpAddr (spRel platform off))])
-           RcDouble    -> (delta, [MOV FF64 (OpReg reg) (OpAddr (spRel platform off))])
+           RcInteger   -> MOV (archWordFormat is32Bit)
+                                   (OpReg reg) (OpAddr (spRel platform off))
+           RcDouble    -> MOV FF64 (OpReg reg) (OpAddr (spRel platform off))
            _         -> panic "X86.mkSpillInstr: no match"
     where platform = ncgPlatform config
           is32Bit = target32Bit platform
@@ -693,7 +693,7 @@ mkLoadInstr
     -> Reg      -- register to load
     -> Int      -- current stack delta
     -> Int      -- spill slot to use
-    -> (Int, [Instr])
+    -> Instr
 
 mkLoadInstr config reg delta slot
   = let off     = spillSlotToOffset platform slot - delta
