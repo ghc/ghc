f0585086d6b0fe0a3ea013de0df99b7ab1b0fe3a
Moritz Angermann <moritz.angermann@gmail.com>
2020-08-20 02:11:26 +0000

    Disable trivial deadlock detection

--- compiler/GHC/CmmToAsm/AArch64/Ppr.hs
+++ compiler/GHC/CmmToAsm/AArch64/Ppr.hs
@@ -120,7 +120,7 @@ pprBasicBlock :: NCGConfig -> LabelMap RawCmmStatics -> NatBasicBlock Instr
 pprBasicBlock config info_env (BasicBlock blockid instrs)
   = maybe_infotable $
     pprLabel platform asmLbl $$
-    vcat (map (pprInstr platform) (detectTrivialDeadlock optInstrs)) $$
+    vcat (map (pprInstr platform) (id {-detectTrivialDeadlock-} optInstrs)) $$
     (if  ncgDebugLevel config > 0
       then ppr (mkAsmTempEndLabel asmLbl) <> char ':'
       else empty
@@ -483,22 +483,23 @@ pprInstr platform instr = case instr of
     text "\tstrh" <+> pprOp o1 <> comma <+> pprOp o2
   STR f o1 o2 -> text "\tstr" <+> pprOp o1 <> comma <+> pprOp o2
 
+  LDR f o1 (OpImm (ImmIndex lbl' off)) | Just (_info, lbl) <- dynamicLinkerLabelInfo lbl' ->
+    text "\tadrp" <+> pprOp o1 <> comma <+> text ":got:" <> ppr lbl $$
+    text "\tldr" <+> pprOp o1 <> comma <+> text "[" <> pprOp o1 <> comma <+> text ":got_lo12:" <> ppr lbl <> text "]" $$
+    text "\tadd" <+> pprOp o1 <> comma <+> pprOp o1 <> comma <+> char '#' <> int off -- XXX: check that off is in 12bits.
+
   LDR f o1 (OpImm (ImmIndex lbl off)) ->
     text "\tadrp" <+> pprOp o1 <> comma <+> ppr lbl $$
     text "\tadd" <+> pprOp o1 <> comma <+> pprOp o1 <> comma <+> text ":lo12:" <> ppr lbl $$
     text "\tadd" <+> pprOp o1 <> comma <+> pprOp o1 <> comma <+> char '#' <> int off -- XXX: check that off is in 12bits.
-  -- always GOT loads
-  -- LDR f o1 (OpImm (ImmIndex lbl off)) ->
-  --   text "\tadrp" <+> pprOp o1 <> comma <+> text ":got:" <> ppr lbl $$
-  --   text "\tldr" <+> pprOp o1 <> comma <+> text "[" <> pprOp o1 <> comma <+> text":got_lo12:" <> ppr lbl <> text "]" $$
-  --   text "\tadd" <+> pprOp o1 <> comma <+> pprOp o1 <> comma <+> char '#' <> int off -- XXX: check that off is in 12bits.
+
+  LDR f o1 (OpImm (ImmCLbl lbl')) | Just (_info, lbl) <- dynamicLinkerLabelInfo lbl' ->
+    text "\tadrp" <+> pprOp o1 <> comma <+> text ":got:" <> ppr lbl $$
+    text "\tldr" <+> pprOp o1 <> comma <+> text "[" <> pprOp o1 <> comma <+> text ":got_lo12:" <> ppr lbl <> text "]"
 
   LDR f o1 (OpImm (ImmCLbl lbl)) ->
     text "\tadrp" <+> pprOp o1 <> comma <+> ppr lbl $$
     text "\tadd" <+> pprOp o1 <> comma <+> pprOp o1 <> comma <+> text ":lo12:" <> ppr lbl
-  -- LDR f o1 (OpImm (ImmCLbl lbl)) ->
-  --   text "\tadrp" <+> pprOp o1 <> comma <+> text ":got:" <> ppr lbl $$
-  --   text "\tldr" <+> pprOp o1 <> comma <+> text "[" <> pprOp o1 <> comma <+> text":got_lo12:" <> ppr lbl <> text "]"
 
   LDR f o1@(OpReg W8 (RegReal (RealRegSingle i))) o2 | i < 32 ->
     text "\tldrsb" <+> pprOp o1 <> comma <+> pprOp o2
