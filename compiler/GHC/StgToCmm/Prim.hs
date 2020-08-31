9784a81dfba8cf48a9aa1ef9f65e7f71d469cc0a
buggymcbugfix <4444-buggymcbugfix@users.noreply.gitlab.haskell.org>
2020-08-31 19:44:44 +0200

    Implement appendArray# (external) primop

--- compiler/GHC/StgToCmm/Prim.hs
+++ compiler/GHC/StgToCmm/Prim.hs
@@ -219,6 +219,8 @@ emitPrimOp dflags primop = case primop of
       -> opIntoRegs $ \ [res] -> emitCloneArray mkMAP_DIRTY_infoLabel res src src_off (fromInteger n)
     _ -> PrimopCmmEmit_External
 
+  AppendArrays -> const PrimopCmmEmit_External
+
   NewSmallArrayOp -> \case
     [(CmmLit (CmmInt n w)), init]
       | wordsToBytes platform (asUnsigned w n) <= fromIntegral (maxInlineAllocSize dflags)
@@ -263,6 +265,8 @@ emitPrimOp dflags primop = case primop of
       | wordsToBytes platform (asUnsigned w n) <= fromIntegral (maxInlineAllocSize dflags)
       -> opIntoRegs $ \ [res] -> emitCloneSmallArray mkSMAP_DIRTY_infoLabel res src src_off (fromInteger n)
     _ -> PrimopCmmEmit_External
+
+  AppendSmallArrays -> const PrimopCmmEmit_External
 
 -- First we handle various awkward cases specially.
 
