c2dbcacb523e327bec31fe2aa4c19ab625ed92b7
Ryan Scott <ryan.gl.scott@gmail.com>
2020-09-08 07:20:02 -0400

    Postpone associated tyfam default checks until after typechecking
    Previously, associated type family defaults were validity-checked
    during typechecking. Unfortunately, the error messages that these
    checks produce run the risk of printing knot-tied type constructors,
    which will cause GHC to diverge. In order to preserve the current
    error message's descriptiveness, this patch postpones these validity
    checks until after typechecking, which are now located in the new
    function `GHC.Tc.Validity.checkValidAssocTyFamDeflt`.
    
    Fixes #18648.
    
    (cherry picked from commit 5e883375409efc2336da6295c7d81bd10b542210)

--- compiler/GHC/IfaceToCore.hs
+++ compiler/GHC/IfaceToCore.hs
@@ -791,7 +791,7 @@ tc_iface_decl _parent ignore_prags
                       Just def -> forkM (mk_at_doc tc)                 $
                                   extendIfaceTyVarEnv (tyConTyVars tc) $
                                   do { tc_def <- tcIfaceType def
-                                     ; return (Just (tc_def, noSrcSpan)) }
+                                     ; return (Just (tc_def, NoATVI)) }
                   -- Must be done lazily in case the RHS of the defaults mention
                   -- the type constructor being defined here
                   -- e.g.   type AT a; type AT b = AT [b]   #8002
