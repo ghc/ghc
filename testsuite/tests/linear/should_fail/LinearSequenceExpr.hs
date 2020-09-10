8dbaa00e640c2d9f1ab274d44640f1d193895fd0
Vladislav Zavialov <vlad.z.4096@gmail.com>
2020-09-10 14:41:25 +0300

    New linear types syntax: a %p -> b
    Implements GHC Proposal 356

--- testsuite/tests/linear/should_fail/LinearSequenceExpr.hs
+++ testsuite/tests/linear/should_fail/LinearSequenceExpr.hs
@@ -3,6 +3,6 @@
 
 module LinearSequenceExpr where
 
-f :: Char #-> Char #-> [Char]
+f :: Char %1 -> Char %1 -> [Char]
 f x y = [x .. y]
 -- This ought to fail, because `fromList` in base, is unrestricted
