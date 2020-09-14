35ea92708e17c90e476167163ae24747a3f5508e
Ben Gamari <ben@smart-cactus.org>
2020-09-14 10:41:34 -0400

    rts: Fix erroneous usage of vsnprintf
    As pointed out in #18685, this should be snprintf not vsnprintf. This
    appears to be due to a cut-and-paste error.
    
    Fixes #18658.

--- rts/RtsMessages.c
+++ rts/RtsMessages.c
@@ -248,7 +248,7 @@ rtsSysErrorMsgFn(const char *s, va_list ap)
 
         r = vsnprintf(buf, BUFSIZE, s, ap);
         if (r > 0 && r < BUFSIZE) {
-            r = vsnprintf(buf+r, BUFSIZE-r, ": %s", syserr);
+            r = snprintf(buf+r, BUFSIZE-r, ": %s", syserr);
             MessageBox(NULL /* hWnd */,
                        buf,
                        prog_name,
