980dba09f2594ed839fb9ea0bf96b0aeca2ba789
Leif Metcalf <me@leif.nz>
2020-09-14 14:47:27 +1200

    Cosmetic

--- compiler/GHC/Data/FastString.hs
+++ compiler/GHC/Data/FastString.hs
@@ -196,7 +196,7 @@ data FastString = FastString {
       n_chars :: {-# UNPACK #-} !Int, -- number of chars
       fs_sbs  :: {-# UNPACK #-} !ShortByteString,
       fs_zenc :: FastZString
-      -- ^ Lazily computed z-encoding of this string. See Note [Z-Encoding] in
+      -- ^ Lazily computed Z-encoding of this string. See Note [Z-Encoding] in
       -- GHC.Utils.Encoding.
       --
       -- Since 'FastString's are globally memoized this is computed at most
