087d1f00b0612f75523e6a21a6389fe2a305a4cc
Artyom Kuznetsov <hi@wzrd.ht>
2020-09-17 22:39:46 +0300

    Remove GADT self-reference check (#11554, #12081, #12174, fixes #15942)
    Reverts 430f5c84dac1eab550110d543831a70516b5cac8

--- testsuite/tests/polykinds/T11554.hs
+++ testsuite/tests/polykinds/T11554.hs
@@ -1,10 +1,17 @@
-{-# LANGUAGE GADTs, PolyKinds, RankNTypes #-}
+{-# LANGUAGE GADTs, PolyKinds, RankNTypes, TypeApplications, DataKinds #-}
 
 module T11554 where
 
 import Data.Kind
 
-data P (x :: k) = Q
+data P1 (x :: k) = Q1
+data A1 :: Type where
+  B1 :: forall (a :: A1). P1 a -> A1
 
-data A :: Type where
-  B :: forall (a :: A). P a -> A
+data P2 k (x :: k) = Q2
+data A2 :: Type where
+  B2 :: P2 A2 a -> A2
+
+data P3 (x :: k) = Q3
+data A3 :: Type where
+  B3 :: P3 @A3 a -> A3
