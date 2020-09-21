c71e296c321b7f7036b33b68e747695e5bb0e94a
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-09-21 23:46:05 +0900

    make extendPatSynEnv aware of FieldSelectors

--- compiler/GHC/Rename/Module.hs
+++ compiler/GHC/Rename/Module.hs
@@ -134,7 +134,8 @@ rnSrcDecls group@(HsGroup { hs_valds   = val_decls,
    --      Need to do this before (D2) because rnTopBindsLHS
    --      looks up those pattern synonyms (#9889)
 
-   extendPatSynEnv val_decls local_fix_env $ \pat_syn_bndrs -> do {
+   has_sel <- (\x -> if x then FieldSelectors else NoFieldSelectors) <$> xoptM LangExt.FieldSelectors;
+   extendPatSynEnv has_sel val_decls local_fix_env $ \pat_syn_bndrs -> do {
 
    -- (D2) Rename the left-hand sides of the value bindings.
    --     This depends on everything from (B) being in scope.
@@ -2294,9 +2295,9 @@ rnConDeclDetails con doc (RecCon (L l fields))
 
 -- | Brings pattern synonym names and also pattern synonym selectors
 -- from record pattern synonyms into scope.
-extendPatSynEnv :: HsValBinds GhcPs -> MiniFixityEnv
+extendPatSynEnv :: FieldSelectors -> HsValBinds GhcPs -> MiniFixityEnv
                 -> ([Name] -> TcRnIf TcGblEnv TcLclEnv a) -> TcM a
-extendPatSynEnv val_decls local_fix_env thing = do {
+extendPatSynEnv has_sel val_decls local_fix_env thing = do {
      names_with_fls <- new_ps val_decls
    ; let pat_syn_bndrs = concat [ name: map flSelector fields
                                 | (name, fields) <- names_with_fls ]
@@ -2323,7 +2324,7 @@ extendPatSynEnv val_decls local_fix_env thing = do {
               mkFieldOcc :: Located RdrName -> LFieldOcc GhcPs
               mkFieldOcc (L l name) = L l (FieldOcc noExtField (L l name))
               field_occs =  map mkFieldOcc rnames
-          flds     <- mapM (newRecordSelector NoDuplicateRecordFields FieldSelectors [bnd_name]) field_occs
+          flds     <- mapM (newRecordSelector NoDuplicateRecordFields has_sel [bnd_name]) field_occs
           return ((bnd_name, flds): names)
       | L bind_loc (PatSynBind _ (PSB { psb_id = L _ n})) <- bind
       = do
