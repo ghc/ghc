be406df6805c767666bec7fd1525fc2aedbac26f
Daniel Rogozin <daniel.rogozin@serokell.io>
2020-09-24 16:30:59 +0300

    Fall back to types when looking up data constructors (#18740)
    Before this patch, referring to a data constructor in a term-level
    context led to a scoping error:
    
        ghci> id Int
        <interactive>:1:4: error: Data constructor not in scope: Int
    
    After this patch, the renamer falls back to the type namespace
    and successfully finds the Int. It is then rejected in the type
    checker with a more useful error message:
    
        <interactive>:1:4: error:
        Type constructor 'Int' used where a value identifier was expected
    
    We also do this for type variables.

--- compiler/GHC/Rename/Env.hs
+++ compiler/GHC/Rename/Env.hs
@@ -5,7 +5,7 @@ GHC.Rename.Env contains functions which convert RdrNames into Names.
 
 -}
 
-{-# LANGUAGE CPP, MultiWayIf, NamedFieldPuns #-}
+{-# LANGUAGE CPP, MultiWayIf, NamedFieldPuns, TypeApplications #-}
 
 module GHC.Rename.Env (
         newTopSrcBinder,
@@ -1005,6 +1005,13 @@ lookup_demoted rdr_name
            , text "instead of"
            , quotes (ppr name) <> dot ]
 
+lookup_promoted :: RdrName -> RnM (Maybe Name)
+lookup_promoted rdr_name
+  | Just promoted_rdr <- promoteRdrName rdr_name
+  = lookupOccRn_maybe promoted_rdr
+  | otherwise
+  = return Nothing
+
 badVarInType :: RdrName -> RnM Name
 badVarInType rdr_name
   = do { addErr (text "Illegal promoted term variable in a type:"
@@ -1054,14 +1061,18 @@ lookupOccRn_maybe = lookupOccRnX_maybe lookupGlobalOccRn_maybe id
 
 lookupOccRn_overloaded :: Bool -> RdrName
                        -> RnM (Maybe (Either Name [Name]))
-lookupOccRn_overloaded overload_ok
-  = lookupOccRnX_maybe global_lookup Left
-      where
-        global_lookup :: RdrName -> RnM (Maybe (Either Name [Name]))
-        global_lookup n =
-          runMaybeT . msum . map MaybeT $
-            [ lookupGlobalOccRn_overloaded overload_ok n
-            , fmap Left . listToMaybe <$> lookupQualifiedNameGHCi n ]
+lookupOccRn_overloaded overload_ok rdr_name
+  = do { mb_name <- lookupOccRnX_maybe global_lookup Left rdr_name
+       ; case mb_name of
+           Nothing   -> fmap @Maybe Left <$> lookup_promoted rdr_name
+           p         -> return p }
+
+  where
+    global_lookup :: RdrName -> RnM (Maybe (Either Name [Name]))
+    global_lookup n =
+      runMaybeT . msum . map MaybeT $
+        [ lookupGlobalOccRn_overloaded overload_ok n
+        , fmap Left . listToMaybe <$> lookupQualifiedNameGHCi n ]
 
 
 
