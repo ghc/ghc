9a757d0590b24cfd79f90d795c56d2bd780b7c99
Hécate <hecate+gitlab@glitchbra.in>
2020-09-24 19:28:53 +0200

    Refactor the rules under one module

--- hadrian/src/Main.hs
+++ hadrian/src/Main.hs
@@ -11,8 +11,7 @@ import qualified CommandLine
 import qualified Environment
 import qualified Rules
 import qualified Rules.Clean
-import qualified Rules.Lint.Base
-import qualified Rules.Lint.Compiler
+import qualified Rules.Lint
 import qualified Rules.Documentation
 import qualified Rules.Nofib
 import qualified Rules.SourceDist
@@ -86,8 +85,7 @@ main = do
             Rules.buildRules
             Rules.Documentation.documentationRules
             Rules.Clean.cleanRules
-            Rules.Lint.Base.lintRules
-            Rules.Lint.Compiler.lintRules
+            Rules.Lint.lintRules
             Rules.Nofib.nofibRules
             Rules.oracleRules
             Rules.Selftest.selftestRules
