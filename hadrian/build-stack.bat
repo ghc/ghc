aaf18aaf8fc3fd1e21eaecbd8e32ca4a3c654222
Ben Gamari <ben@smart-cactus.org>
2020-09-24 11:58:56 -0400

    gitlab-ci: Verify that Hadrian builds with Stack
    As noted in #18726, this regularly breaks. Let's test it.
    
    Note that we don't actually perform a build of GHC itself; we merely
    test that the Hadrian executable builds and works (by invoking `hadrian
    --version`).

--- hadrian/build-stack.bat
+++ hadrian/build-stack.bat
@@ -3,5 +3,9 @@ setlocal
 rem Change the current directory to the one containing this script
 cd %~dp0
 
+if "%STACK%"=="" (
+    set STACK=stack
+)
+
 rem Build and run Hadrian in GHC top directory forwarding additional user arguments
-stack run hadrian --cwd=.. -- %*
+%STACK% run hadrian --cwd=.. -- %*
