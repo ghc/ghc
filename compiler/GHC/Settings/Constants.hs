6b14c4185ca944295d5cfa60ebc6f7ab2a257fc9
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2020-09-26 12:56:01 +0000

    Extend mAX_TUPLE_SIZE to 64
    As well a ctuples and sums.

--- compiler/GHC/Settings/Constants.hs
+++ compiler/GHC/Settings/Constants.hs
@@ -11,14 +11,14 @@ hiVersion = read (cProjectVersionInt ++ cProjectPatchLevel) :: Integer
 -- All pretty arbitrary:
 
 mAX_TUPLE_SIZE :: Int
-mAX_TUPLE_SIZE = 62 -- Should really match the number
-                    -- of decls in Data.Tuple
+mAX_TUPLE_SIZE = 64 -- Should really match the number
+                    -- of decls in GHC.Tuple
 
 mAX_CTUPLE_SIZE :: Int   -- Constraint tuples
-mAX_CTUPLE_SIZE = 62     -- Should match the number of decls in GHC.Classes
+mAX_CTUPLE_SIZE = 64     -- Should match the number of decls in GHC.Classes
 
 mAX_SUM_SIZE :: Int
-mAX_SUM_SIZE = 62
+mAX_SUM_SIZE = 64
 
 -- | Default maximum depth for both class instance search and type family
 -- reduction. See also #5395.
