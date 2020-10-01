28d56aba4932402d78142ea4665b96029e1da4d7
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2020-10-01 20:19:35 +0200

    Reject linearity in kinds in checkValidType (#18780)
    Patch taken from https://gitlab.haskell.org/ghc/ghc/-/issues/18624#note_300673

--- compiler/GHC/Tc/Gen/HsType.hs
+++ compiler/GHC/Tc/Gen/HsType.hs
@@ -1042,10 +1042,7 @@ tc_hs_type _ ty@(HsSpliceTy {}) _exp_kind
 
 ---------- Functions and applications
 tc_hs_type mode ty@(HsFunTy _ mult ty1 ty2) exp_kind
-  | mode_tyki mode == KindLevel && not (isUnrestricted mult)
-    = failWithTc (text "Linear arrows disallowed in kinds:" <+> ppr ty)
-  | otherwise
-    = tc_fun_type mode mult ty1 ty2 exp_kind
+  = tc_fun_type mode mult ty1 ty2 exp_kind
 
 tc_hs_type mode (HsOpTy _ ty1 (L _ op) ty2) exp_kind
   | op `hasKey` funTyConKey
