397afa23131145f292a0d68b15c44d81f1ca8508
Andreas Schwab <schwab@suse.de>
2020-10-02 23:49:01 +0200

    Implement riscv64 LLVM backend
    Add support for the riscv64 architecture for the LLVM code generator.

---
+++ compiler/GHC/Platform/RISCV64.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE CPP #-}
+
+module GHC.Platform.RISCV64 where
+
+import GHC.Prelude
+
+#define MACHREGS_NO_REGS 0
+#define MACHREGS_riscv64 1
+#include "../../../includes/CodeGen.Platform.hs"
+
