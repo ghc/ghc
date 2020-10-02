2105d5e47d50bbea79aca09f2f6916aa751d49d1
cgibbard <cgibbard@gmail.com>
2020-10-02 17:28:52 -0400

    Remove trailing whitespace in TyAppPat_KindDependency.hs

--- testsuite/tests/typecheck/should_compile/TyAppPat_KindDependency.hs
+++ testsuite/tests/typecheck/should_compile/TyAppPat_KindDependency.hs
@@ -11,7 +11,7 @@ data Proxy (a :: k) = Proxy
 
 data Con k (a :: k) = Con (Proxy a)
 
-tyApp :: Con k a -> Proxy a 
+tyApp :: Con k a -> Proxy a
 tyApp (Con @kx @ax (x :: Proxy ax)) = x :: Proxy (ax :: kx)
 
-main = return () 
+main = return ()
