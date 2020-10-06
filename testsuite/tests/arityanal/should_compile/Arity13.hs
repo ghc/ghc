7a41407ef95252f415209d4c03af2da9ec7561c1
Sebastian Graf <sebastian.graf@kit.edu>
2020-10-06 17:10:55 +0200

    Testsuite: Add dead arity analysis tests
    We didn't seem to test these old tests at all, judging from their
    expected output.

--- testsuite/tests/arityanal/should_compile/Arity13.hs
+++ testsuite/tests/arityanal/should_compile/Arity13.hs
@@ -1,8 +1,9 @@
 module F13 where
 
+f13 :: Int -> Int -> Int -> Int
 f13 x y = let f13f = if (x>0) then \z -> z x y
                                 else \z -> y
-              f13h = let v = f13f 5
+              f13h = let v = f13f (*)
                      in \w -> w + v
           in \u -> f13h u
 
