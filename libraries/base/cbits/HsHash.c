cf90ee9903a56057db0c7679cc0cd352e2156e9b
Tamar Christina <tamar@zhox.com>
2020-10-09 13:23:36 +0100

    rts: Update inpackage XXHASH to 0.8 and use XXH3 on 64-bit platforms.

---
+++ libraries/base/cbits/HsHash.c
@@ -0,0 +1,39 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The University of Glasgow 2020
+ *
+ * Definitions for package `base' which are visible in Haskell land.
+ * Defines the streaming interface to the high speed xxHash function
+ *
+ * ---------------------------------------------------------------------------*/
+
+/* This file needs to be compiled with vectorization enabled.  Unfortunately
+   since we compile these things these days with cabal we can no longer
+   specify optimization per file.  So we have to resort to pragmas.  */
+#if defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC push_options
+#pragma GCC optimize ("O3")
+#endif
+
+#define XXH_NAMESPACE __hsbase_
+#define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
+#define XXH_IMPLEMENTATION   /* access definitions */
+#include "HsHash.h"
+
+// Returns 0 on success.
+int __hsbase_hash_init (XXH3_state_t* statePtr) {
+  return XXH3_128bits_reset (statePtr) != XXH_OK;
+}
+
+// Returns 0 on success.
+int __hsbase_hash_update (XXH3_state_t* state, const void* input, size_t len) {
+  return XXH3_128bits_update (state, input, len) != XXH_OK;
+}
+
+void  __hsbase_hash_final (const XXH3_state_t* state, XXH128_hash_t *res) {
+  *res = XXH3_128bits_digest (state);
+}
+
+#if defined(__GNUC__) || defined(__GNUG__)
+#pragma GCC pop_options
+#endif
