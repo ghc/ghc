06c1f30ba529484dcaf992022619adcb8f05303a
Hécate <hecate+gitlab@glitchbra.in>
2020-10-11 11:20:56 +0200

    Fix a leak in `transpose`
    This patch was authored by David Feuer <david.feuer@gmail.com>

---
+++ libraries/base/tests/T18642.hs
@@ -0,0 +1,25 @@
+{-# language NumericUnderscores #-}
+
+module Main where
+
+import Data.List (transpose, foldl')
+import System.Mem
+
+thingy :: [[[Int]]]
+thingy = [ [[1],[2]], [[1..10^7], [3]]]
+
+thingy2 :: [[[Int]]]
+thingy2 = [ [[1],[2]], [[3], [2..10^7+1]]]
+
+main = do
+  setAllocationCounter 200_000 -- 200K alloc counter
+  enableAllocationLimit -- set max allocated bytes to counter
+
+  htr : ttr <- pure $ transpose thingy
+  print $ foldl' (+) 0 . head . tail $ htr
+  print ttr
+
+  htr2 : ttr2 <- pure $ transpose thingy2
+  print $ foldl' (+) 0 . head . tail . head $ ttr2
+  print htr2
+
