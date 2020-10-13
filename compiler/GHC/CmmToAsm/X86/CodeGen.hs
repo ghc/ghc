2589b32785a12bbab5a33d2eff10e1ede9048d11
Hécate <hecate+gitlab@glitchbra.in>
2020-10-13 20:47:35 +0200

    fixup! Remove redundant keywords from compiler

--- compiler/GHC/CmmToAsm/X86/CodeGen.hs
+++ compiler/GHC/CmmToAsm/X86/CodeGen.hs
@@ -1403,7 +1403,7 @@ getNonClobberedOperand (CmmLoad mem pk) = do
                 else
                    return (src, nilOL)
       return (OpAddr src', mem_code `appOL` save_code)
-    else do
+    else
       -- if its a word or gcptr on 32bit?
       getNonClobberedOperand_generic (CmmLoad mem pk)
 
@@ -1791,7 +1791,7 @@ genJump (CmmLoad mem _) regs = do
   Amode target code <- getAmode mem
   return (code `snocOL` JMP (OpAddr target) regs)
 
-genJump (CmmLit lit) regs = do
+genJump (CmmLit lit) regs =
   return (unitOL (JMP (OpImm (litToImm lit)) regs))
 
 genJump expr regs = do
