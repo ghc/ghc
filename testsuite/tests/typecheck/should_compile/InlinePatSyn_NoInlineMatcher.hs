f42f879da738bbff14c626f20f80d0410658ffeb
Madeline Haraj <madeline.haraj@obsidian.systems>
2020-10-15 14:57:35 -0400

    Add documentation for INLINABLE pragmas for pattern synonyms and add tests for that case

--- testsuite/tests/typecheck/should_compile/InlinePatSyn_NoInlineMatcher.hs
+++ testsuite/tests/typecheck/should_compile/InlinePatSyn_NoInlineMatcher.hs
@@ -3,8 +3,8 @@
 module T12178 where
 
 -- Pattern with "NOINLINE" pragma, neither builder nor matcher should be inlined
-pattern NonInlineablePattern a = Left a
-{-# NOINLINE NonInlineablePattern #-}
+pattern NonInlinablePattern a = Left a
+{-# NOINLINE NonInlinablePattern #-}
 
-testNonMatcher (NonInlineablePattern x) = 1
+testNonMatcher (NonInlinablePattern x) = 1
 testNonMatcher _ = 2
