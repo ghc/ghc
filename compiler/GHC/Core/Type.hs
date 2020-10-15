7a92bf457b225c58f3396cd950e75f42915ddb27
Richard Eisenberg <rae@richarde.dev>
2020-10-15 17:23:04 -0400

    Tiny little changes

--- compiler/GHC/Core/Type.hs
+++ compiler/GHC/Core/Type.hs
@@ -1974,7 +1974,8 @@ buildSynTyCon name binders res_kind roles rhs
   where
     is_tau       = isTauTy rhs
     is_fam_free  = isFamFreeTy rhs
-    is_forgetful = any (not . (`elemVarSet` tyCoVarsOfType rhs) . binderVar) binders
+    is_forgetful = any (not . (`elemVarSet` tyCoVarsOfType rhs) . binderVar) binders ||
+                   uniqSetAny isForgetfulSynTyCon (tyConsOfType rhs)
 
 {-
 ************************************************************************
