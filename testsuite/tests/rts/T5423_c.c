7c9b13ee70835352d6cf808aad8ef5efbdc7e2ec
Moritz Angermann <moritz.angermann@gmail.com>
2020-10-16 03:42:10 -0400

    Update T5423_c.c

--- testsuite/tests/rts/T5423_c.c
+++ testsuite/tests/rts/T5423_c.c
@@ -12,13 +12,13 @@ void print_it(
     StgWord r3,
     StgWord r4,
     StgWord r5,
-    StgWord r5,
+    StgWord r6,
     StgWord r7,
     StgWord r8,
     StgWord r9,
     StgWord r10
     )
 {
-  printf("%d  %d  %d  %d  %d  %d  %d  %d  %d  %d\n",
+  printf("%llu  %llu  %llu  %llu  %llu  %llu  %llu  %llu  %llu  %llu\n",
          r1, r2, r3, r4, r5, r6, r7, r8, r9, r10);
 }
