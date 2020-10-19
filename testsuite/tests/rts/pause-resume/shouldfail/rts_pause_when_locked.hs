81006a06736c7300626f9d692a118b493b585cd5
David Eichmann <EichmannD@gmail.com>
2020-10-19 17:03:41 +0100

    RtsAPI: pause and resume the RTS
    The `rts_pause` and `rts_resume` functions have been added to `RtsAPI.h` and
    allow an external process to completely pause and resume the RTS.
    
    Co-authored-by: Sven Tennie <sven.tennie@gmail.com>
    Co-authored-by: Matthew Pickering <matthewtpickering@gmail.com>
    Co-authored-by: Ben Gamari <bgamari.foss@gmail.com>

---
+++ testsuite/tests/rts/pause-resume/shouldfail/rts_pause_when_locked.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+import Control.Concurrent
+import Foreign
+import Foreign.C
+import System.Exit
+import System.Timeout
+
+foreign import ccall safe "rts_pause_lock.h assertDoneAfterOneSecond"
+    safe_assertDoneAfterOneSecond_c :: Ptr CInt -> IO ()
+
+foreign import ccall safe "rts_pause_lock.h pauseThenLock"
+    safe_pauseThenLock_c :: Ptr CInt -> IO ()
+
+main :: IO ()
+main = alloca $ \donePtr -> do
+  -- We don't expect a deadlock, but we want to avoid one in the case of a
+  -- failed test.
+  poke donePtr 0
+  forkIO $ safe_assertDoneAfterOneSecond_c donePtr
+
+  -- The actual test.
+  safe_pauseThenLock_c donePtr
