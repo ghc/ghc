f619ea8faae670bd2afb5545ad8b4a004eced3c7
Ben Gamari <ben@smart-cactus.org>
2020-10-24 19:51:15 -0400

    rts: Tear down stats_mutex after exitHeapProfiling
    Since the latter wants to call getRTSStats.

--- rts/RtsStartup.c
+++ rts/RtsStartup.c
@@ -511,6 +511,9 @@ hs_exit_(bool wait_foreign)
     shutdownAsyncIO(wait_foreign);
 #endif
 
+    /* tear down statistics subsystem */
+    stat_exit();
+
     /* free hash table storage */
     exitHashTable();
 
