8e596cdffa0db82a9ceb40095df3f24b8586b58f
Ben Gamari <ben@smart-cactus.org>
2020-10-27 10:34:59 -0400

    Bump time submodule to 1.11.1
    Also bumps directory, Cabal, hpc, time, and unix submodules.
    
    Closes #18847.

--- utils/ghc-cabal/ghc.mk
+++ utils/ghc-cabal/ghc.mk
@@ -38,20 +38,20 @@ $(ghc-cabal_INPLACE) : $(ghc-cabal_DIST_BINARY) | $$(dir $$@)/.
 	"$(CP)" $< $@
 
 # Minor hack, since we can't reuse the `hs-suffix-rules-srcdir` macro
-ifneq ($(wildcard libraries/Cabal/Cabal/Distribution/Fields/Lexer.x),)
+ifneq ($(wildcard libraries/Cabal/Cabal/src/Distribution/Fields/Lexer.x),)
 # Lexer.x exists so we have to call Alex ourselves
-CABAL_LEXER_DEP := bootstrapping/Cabal/Distribution/Fields/Lexer.hs
+CABAL_LEXER_DEP := bootstrapping/Cabal/src/Distribution/Fields/Lexer.hs
 
-bootstrapping/Cabal/Distribution/Fields/Lexer.hs: libraries/Cabal/Cabal/Distribution/Fields/Lexer.x
-	mkdir -p bootstrapping/Cabal/Distribution/Fields
+bootstrapping/Cabal/src/Distribution/Fields/Lexer.hs: libraries/Cabal/Cabal/src/Distribution/Fields/Lexer.x
+	mkdir -p bootstrapping/Cabal/src/Distribution/Fields
 	$(call cmd,ALEX) $< -o $@
 else
-CABAL_LEXER_DEP := libraries/Cabal/Cabal/Distribution/Fields/Lexer.hs
+CABAL_LEXER_DEP := libraries/Cabal/Cabal/src/Distribution/Fields/Lexer.hs
 endif
 
-$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/Distribution/*/*/*.hs)
-$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/Distribution/*/*.hs)
-$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/Distribution/*.hs)
+$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/src/Distribution/*/*/*.hs)
+$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/src/Distribution/*/*.hs)
+$(ghc-cabal_DIST_BINARY): $(wildcard libraries/Cabal/Cabal/src/Distribution/*.hs)
 
 # N.B. Compile with -O0 since this is not a performance-critical executable
 # and the Cabal takes nearly twice as long to build with -O1. See #16817.
@@ -70,7 +70,7 @@ $(ghc-cabal_DIST_BINARY): $(CABAL_LEXER_DEP) utils/ghc-cabal/Main.hs $(TOUCH_DEP
 	       -odir  bootstrapping \
 	       -hidir bootstrapping \
 	       $(CABAL_LEXER_DEP) \
-	       -ilibraries/Cabal/Cabal \
+	       -ilibraries/Cabal/Cabal/src \
 	       -ilibraries/binary/src \
 	       -ilibraries/filepath \
 	       -ilibraries/hpc \
