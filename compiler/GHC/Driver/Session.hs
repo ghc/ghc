45af76cd03439ed6b1fed420b2dced0f06e69a95
Moritz Angermann <moritz.angermann@gmail.com>
2020-10-27 10:52:25 +0800

    [AArch64] Aarch64 Always PIC

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -3796,8 +3796,10 @@ validHoleFitsImpliedGFlags
 default_PIC :: Platform -> [GeneralFlag]
 default_PIC platform =
   case (platformOS platform, platformArch platform) of
-    (OSDarwin, ArchX86_64) -> [Opt_PIC]
-    (OSOpenBSD, ArchX86_64) -> [Opt_PIC] -- Due to PIE support in
+    (OSDarwin,  ArchX86_64)  -> [Opt_PIC]
+    (OSDarwin,  ArchAarch64) -> [Opt_PIC]
+    (OSLinux,   ArchAarch64) -> [Opt_PIC, Opt_ExternalDynamicRefs]
+    (OSOpenBSD, ArchX86_64)  -> [Opt_PIC] -- Due to PIE support in
                                          -- OpenBSD since 5.3 release
                                          -- (1 May 2013) we need to
                                          -- always generate PIC. See
