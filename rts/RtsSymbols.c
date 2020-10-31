3453d2490d51cea78d45916f4b1c04089ce96369
Sven Tennie <sven.tennie@gmail.com>
2020-10-31 13:31:12 -0400

    Introduce cloning of other threads' stacks
    Introduce `cloneThreadStack` function, allowing threads to request
    snapshots of other threads' stacks.
    
    For technical details, please see note [Stack Cloning].
    
    Co-authored-by: Ben Gamari <bgamari.foss@gmail.com>

--- rts/RtsSymbols.c
+++ rts/RtsSymbols.c
@@ -1019,6 +1019,7 @@
       SymI_HasProto(keepCAFs)                                           \
       SymI_HasProto(registerInfoProvList)                               \
       SymI_HasProto(lookupIPE)                                          \
+      SymI_HasProto(sendCloneStackMessage)                              \
       RTS_USER_SIGNALS_SYMBOLS                                          \
       RTS_INTCHAR_SYMBOLS
 
