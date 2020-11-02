cebcd195aeaa51be486663b38acdb15fc7bbac2b
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-11-02 13:23:58 +0100

    Testsuite: Support for user supplied package dbs
    We can now supply additional package dbs to the testsuite.
    For make the package db can be supplied by
    passing PACKAGE_DB=/path/to/db.
    
    In the testsuite driver it's passed via the --test-package-db
    argument.

--- testsuite/mk/test.mk
+++ testsuite/mk/test.mk
@@ -216,6 +216,10 @@ ifneq "$(THREADS)" ""
 RUNTEST_OPTS += --threads=$(THREADS)
 endif
 
+ifneq "$(PACKAGE_DB)" ""
+RUNTEST_OPTS += --test-package-db=$(PACKAGE_DB)
+endif
+
 ifneq "$(VERBOSE)" ""
 RUNTEST_OPTS += --verbose=$(VERBOSE)
 endif
