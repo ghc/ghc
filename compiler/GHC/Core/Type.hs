ebfebd6fd6e6407bc0c8ad81d31616b76fc8ce3d
Richard Eisenberg <rae@richarde.dev>
2020-11-02 18:02:22 -0500

    Subtleties in Note [Instance and Given overlap]

--- compiler/GHC/Core/Type.hs
+++ compiler/GHC/Core/Type.hs
@@ -156,6 +156,7 @@ module GHC.Core.Type (
         coVarsOfType,
         coVarsOfTypes,
 
+        anyFreeVarsOfType,
         noFreeVarsOfType,
         splitVisVarsOfType, splitVisVarsOfTypes,
         expandTypeSynonyms,
