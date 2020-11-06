cbf62337b8d72a0c689a300863ab897aa1f77b6d
Simon Peyton Jones <simonpj@microsoft.com>
2020-11-06 17:38:14 +0000

    Fix kind inference for data types. Again.
    This patch improves kcConDecls, when we are inferring the kind
    of a data type decl.
    
    Specifically
    
    * In kcConDecls/kcConDecl make it clear that the tc_res_kind argument
      is only used in the H98 case; and in that case there is not result
      kind signature; and hence no need for the disgusting splitPiTys in
      kcConDecls (now thankfully gone).
    
      The GADT case is a bit different to before, and much nicer.
      This is what fixes #18891.
    
      See Note [kcConDecls: kind-checking data type decls]
    
    * Do not look at the constructor decls of a data/newtype instance
      in tcDataFamInstanceHeader. See new
      Note [Kind inference for data family instances]
    
      This causes a few knock-on effects in the tests suite, because
      we require more information than before in the instance /header/
    
    * Minor improvement in kcTyClDecl, combining GADT and H98 case.
    
    Fixes #18891

--- testsuite/tests/deriving/should_compile/T9359.hs
+++ testsuite/tests/deriving/should_compile/T9359.hs
@@ -5,10 +5,12 @@ import Data.Kind (Type)
 
 data Cmp a where
  Sup ::      Cmp a
- V   :: a -> Cmp a
- deriving (Show, Eq)
+-- V   :: a -> Cmp a
+-- deriving (Show, Eq)
 
+{-
 data family   CmpInterval (a :: Cmp k) (b :: Cmp k) :: Type
 data instance CmpInterval (V c)         Sup          = Starting c
   deriving( Show )
 
+-}
