6815603f271484766425ff2e37043b78da2d073c
Ben Gamari <ben@smart-cactus.org>
2020-11-11 17:00:57 -0500

    hadrian: Drop redundant flavour definitions
    Drop the profiled, LLVM, and ThreadSanitizer flavour definitions as
    these can now be realized with flavour transformers.

--- hadrian/src/Settings/Flavours/ThreadSanitizer.hs
+++
@@ -1,9 +0,0 @@
-module Settings.Flavours.ThreadSanitizer (threadSanitizerFlavour) where
-
-import Flavour
-import Settings.Flavours.Validate
-
-threadSanitizerFlavour :: Flavour
-threadSanitizerFlavour =
-  enableThreadSanitizer (validateFlavour
-    { name = "thread-sanitizer" })
