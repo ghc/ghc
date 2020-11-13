cebaa059995c4694fc2f3a6ae94d0ea4e7215a47
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2020-11-13 21:04:12 +0100

    Add -Winvalid-haddock to -Wall (#18905)

--- libraries/base/GHC/Fingerprint/Type.hs
+++ libraries/base/GHC/Fingerprint/Type.hs
@@ -30,7 +30,7 @@ data Fingerprint = Fingerprint {-# UNPACK #-} !Word64 {-# UNPACK #-} !Word64
 instance Show Fingerprint where
   show (Fingerprint w1 w2) = hex16 w1 ++ hex16 w2
     where
-      -- | Formats a 64 bit number as 16 digits hex.
+      -- Formats a 64 bit number as 16 digits hex.
       hex16 :: Word64 -> String
       hex16 i = let hex = showHex i ""
                  in replicate (16 - length hex) '0' ++ hex
