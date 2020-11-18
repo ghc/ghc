82d11a293b0febcac3c4f6510855049602093798
Matthew Pickering <matthewtpickering@gmail.com>
2020-11-18 11:56:30 +0000

    Add whereFrom# primop

--- compiler/GHC/StgToCmm/Prim.hs
+++ compiler/GHC/StgToCmm/Prim.hs
@@ -1515,6 +1515,7 @@ emitPrimOp dflags primop = case primop of
   NewBCOOp -> alwaysExternal
   UnpackClosureOp -> alwaysExternal
   ClosureSizeOp -> alwaysExternal
+  WhereFromOp   -> alwaysExternal
   GetApStackValOp -> alwaysExternal
   ClearCCSOp -> alwaysExternal
   TraceEventOp -> alwaysExternal
