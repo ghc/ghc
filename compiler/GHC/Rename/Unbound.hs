4c0baad2f68c8a1f93df55ff2053f9cae37e0a00
Alfredo Di Napoli <alfredo@well-typed.com>
2020-11-23 16:08:49 +0100

    Move notInScopeErr in Tc.Errors.Ppr

--- compiler/GHC/Rename/Unbound.hs
+++ compiler/GHC/Rename/Unbound.hs
@@ -13,8 +13,7 @@ module GHC.Rename.Unbound
    , WhereLooking(..)
    , unboundName
    , unboundNameX
-   , notInScopeErr
-   , exactNameErr
+   , module Ppr
    )
 where
 
@@ -23,6 +22,7 @@ import GHC.Prelude
 import GHC.Driver.Session
 import GHC.Driver.Ppr
 
+import GHC.Tc.Errors.Ppr as Ppr
 import GHC.Tc.Errors.Types as TcRn (
                              OutOfScopeSuggestions(..)
                            , NameSuggestions(..)
@@ -80,11 +80,10 @@ unboundNameX :: WhereLooking -> RdrName -> SDoc -> RnM Name
 unboundNameX where_look rdr_name extra
   = do  { dflags <- getDynFlags
         ; let show_helpful_errors = gopt Opt_HelpfulErrors dflags
-              err = notInScopeErr rdr_name $$ extra
         ; loc <- getSrcSpanM
         ; if not show_helpful_errors
           then addTcRnErr loc
-                 (TcRn.ErrOutOfScope rdr_name noOutOfScopeSuggestions err)
+                 (TcRn.ErrOutOfScope rdr_name noOutOfScopeSuggestions extra)
           else do { local_env  <- getLocalRdrEnv
                   ; global_env <- getGlobalRdrEnv
                   ; impInfo <- getImports
@@ -93,17 +92,8 @@ unboundNameX where_look rdr_name extra
                   ; let suggestions = unknownNameSuggestions_ where_look
                           dflags hpt currmod global_env local_env impInfo
                           rdr_name
-                  ; addTcRnErr loc (TcRn.ErrOutOfScope rdr_name suggestions err) }
+                  ; addTcRnErr loc (TcRn.ErrOutOfScope rdr_name suggestions extra) }
         ; return (mkUnboundNameRdr rdr_name) }
-
-notInScopeErr :: RdrName -> SDoc
-notInScopeErr rdr_name
-  = case isExact_maybe rdr_name of
-      Just name -> exactNameErr name
-      Nothing -> hang (text "Not in scope:")
-                  2 (what <+> quotes (ppr rdr_name))
-  where
-    what = pprNonVarNameSpace (occNameSpace (rdrNameOcc rdr_name))
 
 type HowInScope = Either SrcSpan ImpDeclSpec
      -- Left loc    =>  locally bound at loc
@@ -290,7 +280,7 @@ importSuggestions where_look global_env hpt currMod imports rdr_name
   (helpful_imports_hiding, helpful_imports_non_hiding)
     = partition (imv_is_hiding . snd) helpful_imports
 
-  -- See note [When to show/hide the module-not-imported line]
+  -- See note [When to show/hide the module-not-imported line] in 'GHC.Tc.Errors.Ppr'.
   show_not_imported_line :: ModuleName -> Bool                    -- #15611
   show_not_imported_line modnam
       | modnam `elem` globMods                = False    -- #14225     -- 1
@@ -328,27 +318,3 @@ isGreOk where_look = case where_look of
                          WL_LocalTop  -> isLocalGRE
                          WL_LocalOnly -> const False
                          _            -> const True
-
-{- Note [When to show/hide the module-not-imported line]           -- #15611
-For the error message:
-    Not in scope X.Y
-    Module X does not export Y
-    No module named ‘X’ is imported:
-there are 2 cases, where we hide the last "no module is imported" line:
-1. If the module X has been imported.
-2. If the module X is the current module. There are 2 subcases:
-   2.1 If the unknown module name is in a input source file,
-       then we can use the getModule function to get the current module name.
-       (See test T15611a)
-   2.2 If the unknown module name has been entered by the user in GHCi,
-       then the getModule function returns something like "interactive:Ghci1",
-       and we have to check the current module in the last added entry of
-       the HomePackageTable. (See test T15611b)
--}
-
-exactNameErr :: Name -> SDoc
-exactNameErr name =
-  hang (text "The exact Name" <+> quotes (ppr name) <+> ptext (sLit "is not in scope"))
-    2 (vcat [ text "Probable cause: you used a unique Template Haskell name (NameU), "
-            , text "perhaps via newName, but did not bind it"
-            , text "If that's it, then -ddump-splices might be useful" ])
