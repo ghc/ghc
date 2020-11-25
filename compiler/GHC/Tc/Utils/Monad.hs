00ba2d89742f4a28cd69e64812c982c1496c925a
Cale Gibbard <cgibbard@gmail.com>
2020-11-25 11:01:23 -0500

    Broaden the circumstances under which inaccessible code warnings are reported due to unsatisfiable constraints. Fix insoluble constraints arising from the ambiguity checker (#17295)
    Edited to fix an issue where these warnings would sometimes be reported in code generated by deriving declarations/clauses.
    
    There is still lots to relitigate invovling local instances, absurdity,
    and implication constraints, as witnessed by the thread, but I think a
    safe first step that fixes the original issues is removing ering on
    insoluble constraints even when `-XAllowAmbiguousTypes` is on.
    
    As far as I can tell, this extra error checking was not done for any
    deap theoretical reasons, but because concerns that users would be
    mislead from incorrect advice in an error message. The check was added
    in 43856a003c9abb1bf8bf1822e6e7f5781b16ac4d, which references #8392,
    where it was written in comment
    https://gitlab.haskell.org/ghc/ghc/issues/8392#note_75571 that the naive
    fix then made
    
    ```haskell
    f :: Bool ~ Int => ()
    f = ()
    ```
    
    Report
    
    ```
    Couldn't match type ‛Bool’ with ‛Int’
    Inaccessible code in the type signature for f :: Bool ~ Int =>
    ()
    To defer this ambiguity check to use sites, enable
    AllowAmbiguousTypes
    In the ambiguity check for: Bool ~ Int => ()
    In the type signature for ‛f’: f :: Bool ~ Int => ()
    ```
    
    It no longer does with this same change years later, so problem avoided.
    
    Add a test for this warning where the constraint is obviously counterfactual.
    
    Add TcReason, and try turning off inaccessible code warnings during ambiguity checks. Maybe we want to disable all warnings during that time.

--- compiler/GHC/Tc/Utils/Monad.hs
+++ compiler/GHC/Tc/Utils/Monad.hs
@@ -64,6 +64,7 @@ module GHC.Tc.Utils.Monad(
   getSrcSpanM, setSrcSpan, setSrcSpanA, addLocM, addLocMA, inGeneratedCode,
   wrapLocM, wrapLocAM, wrapLocFstM, wrapLocFstMA, wrapLocSndM, wrapLocSndMA, wrapLocM_,
   wrapLocMA_,wrapLocMA,
+  withProvenance,
   getErrsVar, setErrsVar,
   addErr,
   failWith, failAt,
@@ -86,6 +87,7 @@ module GHC.Tc.Utils.Monad(
   -- * Context management for the type checker
   getErrCtxt, setErrCtxt, addErrCtxt, addErrCtxtM, addLandmarkErrCtxt,
   addLandmarkErrCtxtM, popErrCtxt, getCtLocM, setCtLocM,
+  setDeriving,
 
   -- * Error message generation (type checker)
   addErrTc,
@@ -367,7 +369,8 @@ initTcWithGbl hsc_env gbl_env loc do_this
                 tcl_errs       = errs_var,
                 tcl_loc        = loc,
                 -- tcl_loc should be over-ridden very soon!
-                tcl_in_gen_code = False,
+                tcl_provenance = OtherCP,
+                tcl_reason     = OtherTR,
                 tcl_ctxt       = [],
                 tcl_rdr        = emptyLocalRdrEnv,
                 tcl_th_ctxt    = topStage,
@@ -904,19 +907,19 @@ getSrcSpanM = do { env <- getLclEnv; return (RealSrcSpan (tcl_loc env) Nothing) 
 inGeneratedCode :: TcRn Bool
 inGeneratedCode = tcl_in_gen_code <$> getLclEnv
 
+withProvenance :: CodeProvenance -> TcRn a -> TcRn a
+withProvenance p = updLclEnv (\env -> setCodeProvenance p env)
+
 setSrcSpan :: SrcSpan -> TcRn a -> TcRn a
 -- See Note [Error contexts in generated code]
--- for the tcl_in_gen_code manipulation
-setSrcSpan (RealSrcSpan loc _) thing_inside
-  = updLclEnv (\env -> env { tcl_loc = loc, tcl_in_gen_code = False })
-              thing_inside
-
+-- for the provenance manipulation
+setSrcSpan (RealSrcSpan loc _) thing_inside =
+  updLclEnv (\env -> env { tcl_loc = loc }) $
+    withProvenance OtherCP thing_inside
 setSrcSpan loc@(UnhelpfulSpan _) thing_inside
-  | isGeneratedSrcSpan loc
-  = updLclEnv (\env -> env { tcl_in_gen_code = True }) thing_inside
-
-  | otherwise
-  = thing_inside
+  | isGeneratedSrcSpan loc =
+    withProvenance RebindableSyntaxCP thing_inside
+  | otherwise = thing_inside
 
 setSrcSpanA :: SrcSpanAnn' ann -> TcRn a -> TcRn a
 setSrcSpanA l = setSrcSpan (locA l)
@@ -1219,6 +1222,8 @@ setCtLocM (CtLoc { ctl_env = lcl }) thing_inside
                            , tcl_ctxt  = tcl_ctxt lcl })
               thing_inside
 
+setDeriving :: TcM a -> TcM a
+setDeriving = updLclEnv (\e -> setCodeProvenance DerivingCP e)
 
 {- *********************************************************************
 *                                                                      *
