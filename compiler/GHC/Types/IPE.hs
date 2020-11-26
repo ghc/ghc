fbc4664841689bb4001dde069079af2198f5204a
Matthew Pickering <matthewtpickering@gmail.com>
2020-11-26 15:24:51 +0000

    Some more comments

--- compiler/GHC/Types/IPE.hs
+++ compiler/GHC/Types/IPE.hs
@@ -11,12 +11,19 @@ import GHC.Types.Unique.Map
 
 -- | A map from a 'Name' to the best approximate source position that
 -- name arose from.
-type ClosureMap = UniqMap
-                                        Name  -- The binding
-                                        (String, RealSrcSpan, String) -- The best approximate source position.
+type ClosureMap = UniqMap Name  -- The binding
+                          (String, RealSrcSpan, String)
+                          -- The best approximate source position.
+                          -- (rendered type, source position, source note
+                          -- label)
 
 -- | A map storing all the different uses of a specific data constructor and the
 -- approximate source position that usage arose from.
+-- The `Int` is an incrementing identifier which distinguishes each usage
+-- of a constructor in a module. It is paired with the source position
+-- the constructor was used at, if possible and a string which names
+-- the source location. This is the same information as is the payload
+-- for the `GHC.Core.SourceNote` constructor.
 type DCMap = UniqMap DataCon [(Int, Maybe (RealSrcSpan, String))]
 
 data InfoTableProvMap = InfoTableProvMap
