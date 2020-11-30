9e1a58a469f3f64a667b3e3aeb8218a26f36077d
Ben Gamari <ben@smart-cactus.org>
2020-11-30 14:00:07 -0500

    iFix it

--- libraries/base/GHC/ForeignPtr/Ops.hs
+++ libraries/base/GHC/ForeignPtr/Ops.hs
@@ -55,17 +55,17 @@ import GHC.ForeignPtr
 peekWord8ForeignPtr :: ForeignPtr ty -> Int -> IO Word8
 peekWord8ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readWord8OffAddr# addr d) of
-      (# s1, r #) -> (# s1, W8# r #)
+      (# s1, r #) -> (# s1, W8# (narrowWord8# r) #)
 
 peekWord16ForeignPtr :: ForeignPtr ty -> Int -> IO Word16
 peekWord16ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readWord16OffAddr# addr d) of
-      (# s1, r #) -> (# s1, W16# r #)
+      (# s1, r #) -> (# s1, W16# (narrowWord16# r) #)
 
 peekWord32ForeignPtr :: ForeignPtr ty -> Int -> IO Word32
 peekWord32ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readWord32OffAddr# addr d) of
-      (# s1, r #) -> (# s1, W32# r #)
+      (# s1, r #) -> (# s1, W32# (narrowWord32# r) #)
 
 peekWord64ForeignPtr :: ForeignPtr ty -> Int -> IO Word64
 peekWord64ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
@@ -80,17 +80,17 @@ peekWordForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
 peekInt8ForeignPtr :: ForeignPtr ty -> Int -> IO Int8
 peekInt8ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readInt8OffAddr# addr d) of
-      (# s1, r #) -> (# s1, I8# r #)
+      (# s1, r #) -> (# s1, I8# (narrowInt8# r) #)
 
 peekInt16ForeignPtr :: ForeignPtr ty -> Int -> IO Int16
 peekInt16ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readInt16OffAddr# addr d) of
-      (# s1, r #) -> (# s1, I16# r #)
+      (# s1, r #) -> (# s1, I16# (narrowInt16# r) #)
 
 peekInt32ForeignPtr :: ForeignPtr ty -> Int -> IO Int32
 peekInt32ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
     case keepAlive# c s0 (readInt32OffAddr# addr d) of
-      (# s1, r #) -> (# s1, I32# r #)
+      (# s1, r #) -> (# s1, I32# (narrowInt32# r) #)
 
 peekInt64ForeignPtr :: ForeignPtr ty -> Int -> IO Int64
 peekInt64ForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
@@ -109,17 +109,17 @@ peekCharForeignPtr (ForeignPtr addr c) (I# d) = IO $ \s0 ->
 
 pokeWord8ForeignPtr :: ForeignPtr ty -> Int -> Word8 -> IO ()
 pokeWord8ForeignPtr (ForeignPtr addr c) (I# d) (W8# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeWord8OffAddr# addr d n) of
+    case keepAlive# c s0 (writeWord8OffAddr# addr d (extendWord8# n)) of
       s1 -> (# s1, () #)
 
 pokeWord16ForeignPtr :: ForeignPtr ty -> Int -> Word16 -> IO ()
 pokeWord16ForeignPtr (ForeignPtr addr c) (I# d) (W16# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeWord16OffAddr# addr d n) of
+    case keepAlive# c s0 (writeWord16OffAddr# addr d (extendWord16# n)) of
       s1 -> (# s1, () #)
 
 pokeWord32ForeignPtr :: ForeignPtr ty -> Int -> Word32 -> IO ()
 pokeWord32ForeignPtr (ForeignPtr addr c) (I# d) (W32# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeWord32OffAddr# addr d n) of
+    case keepAlive# c s0 (writeWord32OffAddr# addr d (extendWord32# n)) of
       s1 -> (# s1, () #)
 
 pokeWord64ForeignPtr :: ForeignPtr ty -> Int -> Word64 -> IO ()
@@ -134,17 +134,17 @@ pokeWordForeignPtr (ForeignPtr addr c) (I# d) (W# n) = IO $ \s0 ->
 
 pokeInt8ForeignPtr :: ForeignPtr ty -> Int -> Int8 -> IO ()
 pokeInt8ForeignPtr (ForeignPtr addr c) (I# d) (I8# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeInt8OffAddr# addr d n) of
+    case keepAlive# c s0 (writeInt8OffAddr# addr d (extendInt8# n)) of
       s1 -> (# s1, () #)
 
 pokeInt16ForeignPtr :: ForeignPtr ty -> Int -> Int16 -> IO ()
 pokeInt16ForeignPtr (ForeignPtr addr c) (I# d) (I16# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeInt16OffAddr# addr d n) of
+    case keepAlive# c s0 (writeInt16OffAddr# addr d (extendInt16# n)) of
       s1 -> (# s1, () #)
 
 pokeInt32ForeignPtr :: ForeignPtr ty -> Int -> Int32 -> IO ()
 pokeInt32ForeignPtr (ForeignPtr addr c) (I# d) (I32# n) = IO $ \s0 ->
-    case keepAlive# c s0 (writeInt32OffAddr# addr d n) of
+    case keepAlive# c s0 (writeInt32OffAddr# addr d (extendInt32# n)) of
       s1 -> (# s1, () #)
 
 pokeInt64ForeignPtr :: ForeignPtr ty -> Int -> Int64 -> IO ()
