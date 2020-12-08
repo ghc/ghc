316960883e77d47d30319111780e6f63da6ad40f
alexbiehl <alexbiehl@gmail.com>
2020-12-08 21:00:50 +0100

    Fix haddock-library tests

--- haddock-library/test/Documentation/Haddock/ParserSpec.hs
+++ haddock-library/test/Documentation/Haddock/ParserSpec.hs
@@ -437,7 +437,7 @@ spec = do
         "\"Foo#bar\"" `shouldParseTo` DocModule "Foo#bar"
 
       it "accepts anchor with hyphen as DocModule" $ do
-        "\"Foo#bar-baz\"" `shouldParseTo` DocModule "Foo\\#bar-baz"
+        "\"Foo#bar-baz\"" `shouldParseTo` DocModule "Foo#bar-baz"
 
       it "accepts old anchor reference syntax as DocModule" $ do
         "\"Foo\\#bar\"" `shouldParseTo` DocModule "Foo\\#bar"
