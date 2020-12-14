76b19dd5114be1d516d1b811135eb3ee48fd2aad
Alina Banerjee <alina@glitchgirl.us>
2020-12-14 04:36:37 -0600

    Add SourceText to functions that parse InlineSpec type constructor

--- compiler/GHC/Parser/PostProcess.hs
+++ compiler/GHC/Parser/PostProcess.hs
@@ -2178,8 +2178,8 @@ mkInlinePragma src (inl, match_info) mb_act
             Just act -> act
             Nothing  -> -- No phase specified
                         case inl of
-                          NoInline -> NeverActive
-                          _other   -> AlwaysActive
+                          NoInline (SourceText _ ) -> NeverActive
+                          _other                   -> AlwaysActive
 
 -----------------------------------------------------------------------------
 -- utilities for foreign declarations
