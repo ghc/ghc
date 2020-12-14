659fcb14937e60510e3eea4c1211ea117419905b
Sylvain Henry <sylvain@haskus.fr>
2020-12-14 13:32:45 +0100

    Fix project version for ProjectVersionMunged (fix #19058)

--- .gitlab/linters/check-version-number.sh
+++ .gitlab/linters/check-version-number.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-grep -E -q '\[[0-9]+\.[0-9]+\.[0-9]+\]' configure.ac ||
-  ( echo "error: configure.ac: GHC version number must have three components."; exit 1 )
+grep -E -q 'RELEASE=NO' configure.ac ||
+  grep -E -q '\[[0-9]+\.[0-9]+\.[0-9]+\]' configure.ac ||
+  ( echo "error: configure.ac: GHC version number must have three components when RELEASE=YES."; exit 1 )
