fa91e7190dff2a2af40f3436174a18757bb4d9a1
Ben Gamari <ben@smart-cactus.org>
2020-12-14 10:10:38 -0500

    hadrian: Reindent Settings.Warnings
    The previous state was quite illegible.

--- hadrian/src/Settings/Warnings.hs
+++ hadrian/src/Settings/Warnings.hs
@@ -20,38 +20,39 @@ ghcWarningsArgs = do
     isIntegerSimple <- (== integerSimple) <$> getIntegerPackage
     mconcat
         [ stage0 ? mconcat
-        [ libraryPackage       ? pure [ "-fno-warn-deprecated-flags" ]
-        , package terminfo     ? pure [ "-fno-warn-unused-imports" ]
-        , package transformers ? pure [ "-fno-warn-unused-matches"
-                                      , "-fno-warn-unused-imports" ] ]
+          [ libraryPackage       ? pure [ "-fno-warn-deprecated-flags" ]
+          , package terminfo     ? pure [ "-fno-warn-unused-imports" ]
+          , package transformers ? pure [ "-fno-warn-unused-matches"
+                                        , "-fno-warn-unused-imports" ] ]
         , notStage0 ? mconcat
-        [ libraryPackage       ? pure [ "-Wno-deprecated-flags" ]
-        , package base         ? pure [ "-Wno-trustworthy-safe" ]
-        , package binary       ? pure [ "-Wno-deprecations" ]
-        , package bytestring   ? pure [ "-Wno-inline-rule-shadowing" ]
-        , package compiler     ? pure [ "-Wcpp-undef" ]
-        , package directory    ? pure [ "-Wno-unused-imports" ]
-        , package ghc          ? pure [ "-Wcpp-undef" ]
-        , package ghcPrim      ? pure [ "-Wno-trustworthy-safe" ]
-        , package haddock      ? pure [ "-Wno-unused-imports"
-                                      , "-Wno-deprecations" ]
-        , package haskeline    ? pure [ "-Wno-deprecations"
-                                      , "-Wno-unused-imports"
-                                      , "-Wno-redundant-constraints"
-                                      , "-Wno-simplifiable-class-constraints" ]
-        , package pretty       ? pure [ "-Wno-unused-imports" ]
-        , package primitive    ? pure [ "-Wno-unused-imports"
-                                      , "-Wno-deprecations" ]
-        , package rts          ? pure [ "-Wcpp-undef" ]
-        , package terminfo     ? pure [ "-Wno-unused-imports" ]
-        , isIntegerSimple      ?
-          package text         ? pure [ "-Wno-unused-imports" ]
-        , package transformers ? pure [ "-Wno-unused-matches"
-                                      , "-Wno-unused-imports"
-                                      , "-Wno-redundant-constraints"
-                                      , "-Wno-orphans" ]
-        , package win32        ? pure [ "-Wno-trustworthy-safe" ]
-        , package xhtml        ? pure [ "-Wno-unused-imports" ] ]
+          [ libraryPackage       ? pure [ "-Wno-deprecated-flags" ]
+          , package base         ? pure [ "-Wno-trustworthy-safe" ]
+          , package binary       ? pure [ "-Wno-deprecations" ]
+          , package bytestring   ? pure [ "-Wno-inline-rule-shadowing" ]
+          , package compiler     ? pure [ "-Wcpp-undef" ]
+          , package directory    ? pure [ "-Wno-unused-imports" ]
+          , package ghc          ? pure [ "-Wcpp-undef" ]
+          , package ghcPrim      ? pure [ "-Wno-trustworthy-safe" ]
+          , package haddock      ? pure [ "-Wno-unused-imports"
+                                        , "-Wno-deprecations" ]
+          , package haskeline    ? pure [ "-Wno-deprecations"
+                                        , "-Wno-unused-imports"
+                                        , "-Wno-redundant-constraints"
+                                        , "-Wno-simplifiable-class-constraints" ]
+          , package pretty       ? pure [ "-Wno-unused-imports" ]
+          , package primitive    ? pure [ "-Wno-unused-imports"
+                                        , "-Wno-deprecations" ]
+          , package rts          ? pure [ "-Wcpp-undef" ]
+          , package terminfo     ? pure [ "-Wno-unused-imports" ]
+          , isIntegerSimple      ?
+            package text         ? pure [ "-Wno-unused-imports" ]
+          , package transformers ? pure [ "-Wno-unused-matches"
+                                        , "-Wno-unused-imports"
+                                        , "-Wno-redundant-constraints"
+                                        , "-Wno-orphans" ]
+          , package win32        ? pure [ "-Wno-trustworthy-safe" ]
+          , package xhtml        ? pure [ "-Wno-unused-imports" ]
+          ]
         , mconcat
-        [ package cabal        ? pure [ "-Wno-error=deprecations" ] ]
+          [ package cabal        ? pure [ "-Wno-error=deprecations" ] ]
         ]
