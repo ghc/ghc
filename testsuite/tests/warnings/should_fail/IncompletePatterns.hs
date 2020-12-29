8987d5432745941f7fc5369329c881a76c8eeec3
Dan Bornside <dan.bornside@gmail.com>
2020-12-29 11:30:34 -0500

    sort out some test output.

---
+++ testsuite/tests/warnings/should_fail/IncompletePatterns.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE NoIncomplete #-}
+
+module A where
+
+-- this should generate incomplete-patterns warning
+foo :: Maybe a -> ()
+foo Nothing = ()
+
+data S = C1 Int | C2 Int
+
+-- incomplete pattern
+sInt s = case s of
+           C1 i -> i
