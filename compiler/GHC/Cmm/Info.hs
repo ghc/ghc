3035c9b6c4437c4f5db929449060e2dcab6ee824
Andreas Klebinger <klebinger.andreas@gmx.at>
2020-12-30 16:08:12 +0100

    Minimize diff slightly

--- compiler/GHC/Cmm/Info.hs
+++ compiler/GHC/Cmm/Info.hs
@@ -1,6 +1,4 @@
 {-# LANGUAGE CPP #-}
-
-
 module GHC.Cmm.Info (
   mkEmptyContInfoTable,
   cmmToRawCmm,
--- compiler/GHC/Cmm/Utils.hs
+++ compiler/GHC/Cmm/Utils.hs
@@ -4,7 +4,6 @@
 
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-
 -----------------------------------------------------------------------------
 --
 -- Cmm utilities.
