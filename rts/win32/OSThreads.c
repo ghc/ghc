4b34667aed3b0ea213648c24e76a2b42c622757b
Douglas Wilson <douglas.wilson@gmail.com>
2021-01-04 09:46:58 +0000

    rts: add timedWaitCondition

--- rts/win32/OSThreads.c
+++ rts/win32/OSThreads.c
@@ -559,6 +559,16 @@ waitCondition ( Condition* pCond, Mutex* pMut )
   return true;
 }
 
+bool
+timedWaitCondition ( Condition* pCond, Mutex* pMut, Time timeout )
+{
+  // If we pass a timeout of 0 SleepConditionVariableSRW will return immediately
+  // https://docs.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-sleepconditionvariablesrw
+  DWORD ms = (DWORD)stg_min(1, TimeToMS(timeout));
+  SleepConditionVariableSRW(pCond, pMut, ms, 0);
+  return true;
+}
+
 void
 initMutex (Mutex* pMut)
 {
