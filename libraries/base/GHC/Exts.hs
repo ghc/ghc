e0bb65320ae0fe3c5c1389749bcc45375f27d48e
Sylvain Henry <sylvain@haskus.fr>
2021-01-06 10:14:13 +0100

    Revert "Remove SpecConstrAnnotation (#13681)" (#19168)
    This reverts commit 7bc3a65b467c4286377b9bded277d5a2f69160b3.
    
    NoSpecConstr is used in the wild (see #19168)
    
    Cherry-picked from 84dcb8440e94fab5aaba66e613de27d89264f076

--- libraries/base/GHC/Exts.hs
+++ libraries/base/GHC/Exts.hs
@@ -94,6 +94,9 @@ module GHC.Exts
         -- * Event logging
         traceEvent,
 
+        -- * SpecConstr annotations
+        SpecConstrAnnotation(..),
+
         -- * The call stack
         currentCallStack,
 
@@ -119,6 +122,7 @@ import GHC.Stack
 import qualified Data.Coerce
 import Data.String
 import Data.OldList
+import Data.Data
 import Data.Ord
 import Data.Version ( Version(..), makeVersion )
 import qualified Debug.Trace
@@ -164,6 +168,25 @@ groupByFB c n eq xs0 = groupByFBCore xs0
 traceEvent :: String -> IO ()
 traceEvent = Debug.Trace.traceEventIO
 {-# DEPRECATED traceEvent "Use 'Debug.Trace.traceEvent' or 'Debug.Trace.traceEventIO'" #-} -- deprecated in 7.4
+
+
+{- **********************************************************************
+*                                                                       *
+*              SpecConstr annotation                                    *
+*                                                                       *
+********************************************************************** -}
+
+-- Annotating a type with NoSpecConstr will make SpecConstr
+-- not specialise for arguments of that type.
+
+-- This data type is defined here, rather than in the SpecConstr module
+-- itself, so that importing it doesn't force stupidly linking the
+-- entire ghc package at runtime
+
+data SpecConstrAnnotation = NoSpecConstr | ForceSpecConstr
+                deriving ( Data -- ^ @since 4.3.0.0
+                         , Eq   -- ^ @since 4.3.0.0
+                         )
 
 
 {- **********************************************************************
