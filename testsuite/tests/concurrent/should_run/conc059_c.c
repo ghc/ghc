f62e8c98a3c32138681065313caf932f970b73c9
Ben Gamari <ben@smart-cactus.org>
2021-01-05 19:51:07 -0500

    testsuite: Increase delay in conc059
    As noted in #19179, conc059 can sometimes fail due to too short of a
    delay in the its Haskell threads. Address this by increasing the delay
    by an order of magnitude to 5 seconds.
    
    While I'm in town I refactored the test to eliminate a great deal of
    unnecessary platform dependence, eliminate use of the deprecated
    usleep, and properly handle interruption by signals.
    
    Fixes #19179.

--- testsuite/tests/concurrent/should_run/conc059_c.c
+++ testsuite/tests/concurrent/should_run/conc059_c.c
@@ -1,29 +1,53 @@
 #include "HsFFI.h"
 #include "conc059_stub.h"
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
 #if mingw32_HOST_OS
 #include <windows.h>
 #endif
 
+void millisleep(int milliseconds);
+
 int main(int argc, char *argv[])
 {
     hs_init(&argc,&argv);
-    f(500000);
-#if mingw32_HOST_OS
-    Sleep(100);
-#else
-    usleep(100000);
-#endif
+    f(5000); // this should be considerably longer than the delay on the next
+             // line
+    millisleep(100);
+
     printf("exiting...\n");
     fflush(stdout);
     hs_exit();
     printf("exited.\n");
-#if mingw32_HOST_OS
-    Sleep(1000);
-#else
-    usleep(1000000);
-#endif
+    millisleep(1000);
+
     exit(0);
+}
+
+void millisleep(int milliseconds) {
+#if defined(mingw32_HOST_OS)
+    Sleep(milliseconds);
+#else
+    struct timespec ts = {
+        .tv_sec = milliseconds / 1000,
+        .tv_nsec = (milliseconds % 1000) * 1000000
+    };
+
+    while (true) {
+        int ret = nanosleep(&ts, &ts);
+        if (ret == -1) {
+            if (errno != EINTR) {
+                printf("nanosleep failed\n");
+                exit(1);
+            } else {
+                continue;
+            }
+        } else {
+            return;
+        }
+    }
+#endif
 }
