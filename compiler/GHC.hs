e11a7c3106c0b364c194545effb52099618d144a
Vladislav Zavialov <vlad.z.4096@gmail.com>
2021-01-07 23:56:33 +0300

    Introduce Strict.Maybe (#19156)
    This should reduce compile-time memory usage (I have not measured though).

--- compiler/GHC.hs
+++ compiler/GHC.hs
@@ -275,7 +275,7 @@ module GHC (
         parser,
 
         -- * API Annotations
-        ApiAnns(..),AnnKeywordId(..),AnnotationComment(..), ApiAnnKey,
+        ApiAnns(..),AnnKeywordId(..),AnnotationComment(..), ApiAnnKey(..),
         getAnnotation, getAndRemoveAnnotation,
         getAnnotationComments, getAndRemoveAnnotationComments,
         unicodeAnn,
