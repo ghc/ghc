fe344da9be83be4c7c0c7f76183acfe0a234cc5d
Sylvain Henry <sylvain@haskus.fr>
2021-01-07 15:09:38 +0100

    Missing fields: enhance error messages (#18869)
    This patch delays the detection of missing fields in record creation
    after type-checking. This gives us better error messages (see updated
    test outputs).

--- compiler/GHC/Tc/Types/Constraint.hs
+++ compiler/GHC/Tc/Types/Constraint.hs
@@ -289,7 +289,7 @@ instance Outputable Hole where
 instance Outputable HoleSort where
   ppr (ExprHole ref) = text "ExprHole:" <+> ppr ref
   ppr TypeHole       = text "TypeHole"
-  ppr ConstraintHole = text "CosntraintHole"
+  ppr ConstraintHole = text "ConstraintHole"
 
 ------------
 -- | Used to indicate extra information about why a CIrredCan is irreducible
