a886b57c069cf3adaae0a50a4a343357d8bfb9e5
Ben Gamari <ben@smart-cactus.org>
2021-01-07 14:35:01 -0500

    tidyType: Use mkTyConApp
    Previously we just used the constructor, meaning that the optimisation
    described in Note [Prefer Type over TYPE 'LiftedRep] did not fire.
    
    Thanks to @mpickering for noticing this.

--- compiler/GHC/Core/TyCo/Tidy.hs
+++ compiler/GHC/Core/TyCo/Tidy.hs
@@ -146,7 +146,7 @@ tidyType _   (LitTy n)             = LitTy n
 tidyType env (TyVarTy tv)          = TyVarTy (tidyTyCoVarOcc env tv)
 tidyType env (TyConApp tycon tys)  = let args = strictMap (tidyType env) tys
                                          -- See Note [Strict mapping during tidying]
-                                     in args `seq` TyConApp tycon args
+                                     in args `seq` mkTyConApp tycon args
 tidyType env (AppTy fun arg)       = (AppTy $! (tidyType env fun)) $! (tidyType env arg)
 tidyType env ty@(FunTy _ w arg res)  = let { !w'   = tidyType env w
                                            ; !arg' = tidyType env arg
