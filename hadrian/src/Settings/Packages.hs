c45e90e1a77e9f86afa9f39a74996306f6c55fea
Matthew Pickering <matthewtpickering@gmail.com>
2021-01-08 11:26:17 +0000

    Add support for ghc-debug to ghc executable

--- hadrian/src/Settings/Packages.hs
+++ hadrian/src/Settings/Packages.hs
@@ -115,6 +115,7 @@ packageArgs = do
 
           , builder (Cabal Flags) ? mconcat
             [ (expr (ghcWithInterpreter stage)) `cabalFlag` "internal-interpreter"
+            , notStage0 `cabalFlag` "ghc-debug"
             , ifM stage0
                   -- We build a threaded stage 1 if the bootstrapping compiler
                   -- supports it.
