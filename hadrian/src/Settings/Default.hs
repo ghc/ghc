b97ff73e878cc80421398780f00d965d6ee8a184
Matthew Pickering <matthewtpickering@gmail.com>
2021-01-08 11:26:17 +0000

    Add support for ghc-debug to ghc executable

--- hadrian/src/Settings/Default.hs
+++ hadrian/src/Settings/Default.hs
@@ -171,6 +171,8 @@ stage1Packages = do
         , unlit
         , xhtml
         , if winTarget then win32 else unix
+        , ghc_debug_convention
+        , ghc_debug_stub
         ]
       , when (not cross)
         [ haddock
