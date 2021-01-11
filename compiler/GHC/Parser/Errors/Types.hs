21aa5f17c9f5f69e49c099e86c62d6622622f761
Alfredo Di Napoli <alfredo@well-typed.com>
2021-01-11 12:22:55 +0100

    Adds GhcMessage and ancillary types
    This commit:
    
    * Adds GhcMessage and ancillary (PsMessage, TcRnMessage, ..) types;
    * Move error types and ppr where they belong
    * Add a selection of Driver messages and their pretty-printers
    * Add a selection of TcRn messages and their pretty-printers
    
    Metric Increase:
        MultiLayerModules

---
+++ compiler/GHC/Parser/Errors/Types.hs
@@ -0,0 +1,8 @@
+
+module GHC.Parser.Errors.Types where
+
+import GHC.Prelude ()
+
+-- NOTE(adn): This is an opaque type where constructors will be added
+-- in the context of #18516.
+data PsMessage
