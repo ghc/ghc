9ea9cf9b1485731f834c14f6fdb6b6a4b900da9d
Ben Gamari <ben@smart-cactus.org>
2021-01-13 10:54:23 -0500

    FastMutInt: Ensure that newFastMutInt initializes value

--- compiler/GHC/Data/FastMutInt.hs
+++ compiler/GHC/Data/FastMutInt.hs
@@ -17,21 +17,25 @@ import GHC.Prelude
 import Data.Bits
 import GHC.Base
 
-newFastMutInt :: IO FastMutInt
+data FastMutInt = FastMutInt !(MutableByteArray# RealWorld)
+
+newFastMutInt :: Int -> IO FastMutInt
+newFastMutInt n = do
+    x <- create
+    writeFastMutInt x n
+    return x
+  where
+    !(I# size) = finiteBitSize (0 :: Int)
+    create = IO $ \s ->
+      case newByteArray# size s of { (# s, arr #) ->
+        (# s, FastMutInt arr #) }
+
 readFastMutInt :: FastMutInt -> IO Int
-writeFastMutInt :: FastMutInt -> Int -> IO ()
-
-data FastMutInt = FastMutInt (MutableByteArray# RealWorld)
-
-newFastMutInt = IO $ \s ->
-  case newByteArray# size s of { (# s, arr #) ->
-  (# s, FastMutInt arr #) }
-  where !(I# size) = finiteBitSize (0 :: Int)
-
 readFastMutInt (FastMutInt arr) = IO $ \s ->
   case readIntArray# arr 0# s of { (# s, i #) ->
   (# s, I# i #) }
 
+writeFastMutInt :: FastMutInt -> Int -> IO ()
 writeFastMutInt (FastMutInt arr) (I# i) = IO $ \s ->
   case writeIntArray# arr 0# i s of { s ->
   (# s, () #) }
