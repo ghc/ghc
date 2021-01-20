152c3338d46812b8e0229dd1587f899cd3258a65
Thomas Winant <dewinant@gmail.com>
2021-01-20 18:25:33 +0100

    Add -Wmissing-exported-pattern-synonym-signatures
    After !4741, it was no longer possible to silence a warning about a missing
    pattern synonym signature if the `-Wmissing-signatures` flag was on.
    
    Restore the previous semantics while still adhering to the principle "enabling
    an additional warning flag should never make prior warnings disappear".
    
    For more symmetry and granularity, introduce
    `-Wmissing-exported-pattern-synonym-signatures`.
    
    See Note [Missing signatures] for an overview of all flags involved.

--- testsuite/tests/warnings/should_compile/T14794f.hs
+++ testsuite/tests/warnings/should_compile/T14794f.hs
@@ -1,31 +1,16 @@
 {-# LANGUAGE PatternSynonyms #-}
-{-# OPTIONS_GHC -Wmissing-exported-signatures #-}
+{-# OPTIONS_GHC -Wmissing-pattern-synonym-signatures -Wmissing-exported-pattern-synonym-signatures #-}
 
-module T14794f (test1, pattern Test2, test3, pattern Test4) where
+module T14794d (testExported, pattern TestExported) where
 
--- This should generate warnings with;
--- -Wmissing-exported-signatures
+-- These should generate warnings:
 
-test3 = True
+pattern TestExported <- True
 
+pattern TestUnexported <- True
 
--- This should not generate warnings with;
--- -Wmissing-exported-signatures
+-- These should not generate warnings:
 
-test1 :: Bool
-test1 = True
+testExported = True
 
-pattern Test2 :: Bool
-pattern Test2 <- True
-
-pattern Test4 <- True
-
-test5 :: Bool
-test5 = True
-
-pattern Test6 :: Bool
-pattern Test6 <- True
-
-test7 = True
-
-pattern Test8 <- True
+testUnexported = True
