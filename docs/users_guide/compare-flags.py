ef4b9605fcecff04236111692fab9e0a085b8480
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2021-01-21 13:10:36 -0500

    compare-flags: Strip flags read from --show-options
    Otherwise we end up with terminating \r characters on Windows.

--- docs/users_guide/compare-flags.py
+++ docs/users_guide/compare-flags.py
@@ -49,7 +49,7 @@ def read_documented_flags(doc_flags) -> Set[str]:
 
 def read_ghc_flags(ghc_path: str) -> Set[str]:
     ghc_output = subprocess.check_output([ghc_path, '--show-options'])
-    return {flag
+    return {flag.strip()
             for flag in ghc_output.decode('UTF-8').split('\n')
             if not expected_undocumented(flag)
             if flag != ''}
