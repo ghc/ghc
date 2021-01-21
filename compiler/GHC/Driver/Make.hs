25b3c10b45c55f6308efb263c139c48cf45ef9b1
Luite Stegeman <stegeman@gmail.com>
2021-01-22 00:09:17 +0100

    Generate GHCi bytecode from STG instead of Core and support unboxed tuples and sums.
    fixes #1257

--- compiler/GHC/Driver/Make.hs
+++ compiler/GHC/Driver/Make.hs
@@ -2265,7 +2265,6 @@ downsweep hsc_env old_summaries excl_mods allow_dup_roots
            home_unit       = hsc_home_unit hsc_env
        map1 <- case backend dflags of
          NoBackend   -> enableCodeGenForTH logger home_unit default_backend map0
-         Interpreter -> enableCodeGenForUnboxedTuplesOrSums logger default_backend map0
          _           -> return map0
        if null errs
          then pure $ concat $ modNodeMapElems map1
@@ -2374,32 +2373,8 @@ enableCodeGenForTH logger home_unit =
       -- can't compile anything anyway! See #16219.
       isHomeUnitDefinite home_unit
 
--- | Update the every ModSummary that is depended on
--- by a module that needs unboxed tuples. We enable codegen to
--- the specified target, disable optimization and change the .hi
--- and .o file locations to be temporary files.
---
--- This is used in order to load code that uses unboxed tuples
--- or sums into GHCi while still allowing some code to be interpreted.
-enableCodeGenForUnboxedTuplesOrSums
-  :: Logger
-  -> Backend
-  -> ModNodeMap [Either ErrorMessages ExtendedModSummary]
-  -> IO (ModNodeMap [Either ErrorMessages ExtendedModSummary])
-enableCodeGenForUnboxedTuplesOrSums logger =
-  enableCodeGenWhen logger condition should_modify TFL_GhcSession TFL_CurrentModule
-  where
-    condition ms =
-      unboxed_tuples_or_sums (ms_hspp_opts ms) &&
-      not (gopt Opt_ByteCode (ms_hspp_opts ms)) &&
-      (isBootSummary ms == NotBoot)
-    unboxed_tuples_or_sums d =
-      xopt LangExt.UnboxedTuples d || xopt LangExt.UnboxedSums d
-    should_modify (ModSummary { ms_hspp_opts = dflags }) =
-      backend dflags == Interpreter
-
--- | Helper used to implement 'enableCodeGenForTH' and
--- 'enableCodeGenForUnboxedTuples'. In particular, this enables
+-- | Helper used to implement 'enableCodeGenForTH'.
+-- In particular, this enables
 -- unoptimized code generation for all modules that meet some
 -- condition (first parameter), or are dependencies of those
 -- modules. The second parameter is a condition to check before
