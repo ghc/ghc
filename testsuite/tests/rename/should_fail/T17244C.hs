13ba4b0c718f4db6414339a2b657a0798a616ae7
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-01-24 15:29:37 +0200

    Data.List specialization to []
    - Remove GHC.OldList
    - Remove Data.OldList
    - compat-unqualified-imports is no-op
    - update haddock

--- testsuite/tests/rename/should_fail/T17244C.hs
+++ testsuite/tests/rename/should_fail/T17244C.hs
@@ -3,6 +3,7 @@
 module T17244C (hello) where
 
 -- This should not warn with -Wcompat-unqualified-imports.
+-- But not his fails, as sum name clashes with Prelude
 import Data.List (sum)
 
 hello :: [Int] -> Int
