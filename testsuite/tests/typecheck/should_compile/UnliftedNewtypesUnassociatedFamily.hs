4eb9bf6b6649bbf68ea802a0286193530696be62
Ben Gamari <ben@smart-cactus.org>
2021-02-02 10:06:11 -0500

    Implement BoxedRep proposal
    This implements the BoxedRep proposal, refacoring the `RuntimeRep`
    hierarchy from:
    
    ```haskell
    data RuntimeRep = LiftedPtrRep | UnliftedPtrRep | ...
    ```
    
    to
    
    ```haskell
    data RuntimeRep = BoxedRep Levity | ...
    data Levity = Lifted | Unlifted
    ```
    
    Updates binary, haddock submodules.
    
    Closes #17526.
    
    Metric Increase:
        T12545

--- testsuite/tests/typecheck/should_compile/UnliftedNewtypesUnassociatedFamily.hs
+++ testsuite/tests/typecheck/should_compile/UnliftedNewtypesUnassociatedFamily.hs
@@ -12,14 +12,15 @@ module UnliftedNewtypesUnassociatedFamily where
 import GHC.Int (Int(I#))
 import GHC.Word (Word(W#))
 import GHC.Exts (Int#,Word#)
-import GHC.Exts (TYPE,RuntimeRep(LiftedRep,IntRep,WordRep,TupleRep))
+import GHC.Exts (TYPE,Levity(Lifted))
+import GHC.Exts (RuntimeRep(BoxedRep,IntRep,WordRep,TupleRep))
 
 data family DFT (r :: RuntimeRep) :: TYPE r
 newtype instance DFT 'IntRep = MkDFT1 Int#
 newtype instance DFT 'WordRep = MkDFT2 Word#
 newtype instance DFT ('TupleRep '[ 'IntRep, 'WordRep])
   = MkDFT3 (# Int#, Word# #)
-data instance DFT 'LiftedRep = MkDFT4 | MkDFT5
+data instance DFT ('BoxedRep 'Lifted) = MkDFT4 | MkDFT5
 
 data family DF :: TYPE (r :: RuntimeRep)
 
