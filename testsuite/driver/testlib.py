2cf81525ec0abae833724f55f408df22ba46a8b7
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2021-02-10 10:58:26 +0800

    [testlib] testlib

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -2127,6 +2127,11 @@ def normalise_errmsg(s: str) -> str:
     # filter out nix garbage, that just keeps on showing up as errors on darwin
     s = modify_lines(s, lambda l: re.sub('^(.+)\.dylib, ignoring unexpected dylib file$','', l))
 
+    s = re.sub('You are using an unsupported version of LLVM!', '', s)
+    s = re.sub('Currently only 9 is supported. System LLVM version: 11.0.0', '', s)
+    s = re.sub('We will try though...', '', s)    
+    s = re.sub('ld: warning: passed two min versions \(10.16.0, 10.12\) for platform macOS. Using 10.12.','',s)
+    s = re.sub('ld: warning: -sdk_version and -platform_version are not compatible, ignoring -sdk_version','',s)
     return s
 
 # normalise a .prof file, so that we can reasonably compare it against
@@ -2200,6 +2205,11 @@ def normalise_output( s: str ) -> str:
     # ghci outputs are pretty unstable with -fexternal-dynamic-refs, which is
     # requires for -fPIC
     s = re.sub('  -fexternal-dynamic-refs\n','',s)
+    s = re.sub('You are using an unsupported version of LLVM!', '', s)
+    s = re.sub('Currently only 9 is supported. System LLVM version: 11.0.0', '', s)
+    s = re.sub('We will try though...', '', s)
+    s = re.sub('ld: warning: passed two min versions \(10.16.0, 10.12\) for platform macOS. Using 10.12.','',s)
+    s = re.sub('ld: warning: -sdk_version and -platform_version are not compatible, ignoring -sdk_version','',s)
     return s
 
 def normalise_asm( s: str ) -> str:
