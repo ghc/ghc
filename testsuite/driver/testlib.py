c95007b0c216e5f70e115e456b9bc5551cfb33eb
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2021-02-10 16:35:13 +0800

    [testlib] clean more output for llvm wibbles

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -2207,10 +2207,15 @@ def normalise_output( s: str ) -> str:
     # requires for -fPIC
     s = re.sub('  -fexternal-dynamic-refs\n','',s)
     s = re.sub('You are using an unsupported version of LLVM!', '', s)
-    s = re.sub('Currently only 9 is supported. System LLVM version: 11.0.0', '', s)
+    s = re.sub('Currently only [0-9]+ is supported. System LLVM version: [\.0-9]+', '', s)
     s = re.sub('We will try though...', '', s)
-    s = re.sub('ld: warning: passed two min versions \(10.16.0, 10.12\) for platform macOS. Using 10.12.','',s)
+    s = re.sub('ld: warning: passed .* min versions \(.*\) for platform macOS. Using [\.0-9]+.','',s)
     s = re.sub('ld: warning: -sdk_version and -platform_version are not compatible, ignoring -sdk_version','',s)
+    s = re.sub(".*strip: hanges being made to the file will invalidate the code signature in.*",'',s)
+    # ignore llvm cyclone march warning
+    s = re.sub('.*warning: instruction movi.2d with immediate #0 may not function correctly on this CPU, converting to equivalent movi.16b.*','',s)
+    s = re.sub('.*movi\.2d v., #0000000000000000.*','',s)
+    s = re.sub('.*^.*','',s)
     return s
 
 def normalise_asm( s: str ) -> str:
