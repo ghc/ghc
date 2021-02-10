5a28ff7605cd549a40764ca15449e38bdc437ee9
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2021-02-10 11:12:25 +0800

    [ci] Add aarch64-darwin to CI

---
+++ .gitlab/shell.nix
@@ -0,0 +1,45 @@
+{ system ? "aarch64-darwin"
+#, nixpkgs ? fetchTarball https://github.com/angerman/nixpkgs/archive/257cb120334.tar.gz #apple-silicon.tar.gz
+, pkgs ? import <nixpkgs> { inherit system; }
+}: pkgs.mkShell {
+  # this prevents nix from trying to write the env-vars file.
+  # we can't really, as NIX_BUILD_TOP/env-vars is not set.
+  noDumpEnvVars=1;
+
+  buildInputs = with pkgs; [
+    haskell.compiler.ghc8103Binary
+    haskell.packages.ghc8103Binary.cabal-install
+    haskell.packages.ghc8103Binary.alex
+    haskell.packages.ghc8103Binary.happy_1_19_12
+
+    clang_11
+    llvm_11
+
+    automake
+    autoconf
+    m4
+
+    gmp
+    ncurses
+    libiconv
+    zlib.out
+    zlib.dev
+    glibcLocales
+    locale
+
+    git
+
+    python3
+    # python3Full
+    # python3Packages.sphinx
+
+    which
+    wget
+    file
+
+    xz
+    xlibs.lndir
+
+    cacert
+  ];
+}
