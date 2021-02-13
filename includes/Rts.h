ddb152d57356cf6a2649b8598e596b5e8051ded2
Moritz Angermann <moritz.angermann@gmail.com>
2021-02-13 16:44:19 +0800

    Allocate Adjustors and mark them readable in two steps
    This drops allocateExec for darwin, and replaces it with
    a alloc, write, mark executable strategy instead. This prevents
    us from trying to allocate an executable range and then write to
    it, which X^W will prohibit on darwin.
    
    This will *only* work if we can use mmap.

--- includes/Rts.h
+++ includes/Rts.h
@@ -29,6 +29,12 @@ extern "C" {
 #include <windows.h>
 #endif
 
+#if defined(ios_HOST_OS) || defined(darwin_HOST_OS)
+/* Inclusion of system headers usually requires _DARWIN_C_SOURCE on Mac OS X
+ * because of some specific defines like MMAP_ANON, MMAP_ANONYMOUS. */
+#define _DARWIN_C_SOURCE 1
+#endif
+
 #if !defined(IN_STG_CODE)
 #define IN_STG_CODE 0
 #endif
