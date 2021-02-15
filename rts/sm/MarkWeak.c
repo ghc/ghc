83c906c66692881afcaa0385626c98831c27dae6
Daniel Gröber <dxld@darkboxed.org>
2021-02-15 04:07:23 +0100

    Add configure flag to enable ASSERTs in all ways
    Running the test suite with asserts enabled is somewhat tricky at the
    moment as running it with a GHC compiled the DEBUG way has some hundred
    failures from the start. These seem to be unrelated to assertions
    though. So this provides a toggle to make it easier to debug failing
    assertions using the test suite.

--- rts/sm/MarkWeak.c
+++ rts/sm/MarkWeak.c
@@ -417,7 +417,6 @@ markWeakPtrList ( void )
         for (w = gen->weak_ptr_list; w != NULL; w = RELAXED_LOAD(&w->link)) {
             // w might be WEAK, EVACUATED, or DEAD_WEAK (actually CON_STATIC) here
 
-#if defined(DEBUG)
             {   // careful to do this assertion only reading the info ptr
                 // once, because during parallel GC it might change under our feet.
                 const StgInfoTable *info = RELAXED_LOAD(&w->header.info);
@@ -425,7 +424,6 @@ markWeakPtrList ( void )
                        || info == &stg_DEAD_WEAK_info
                        || INFO_PTR_TO_STRUCT(info)->type == WEAK);
             }
-#endif
 
             evacuate((StgClosure **)last_w);
             w = *last_w;
