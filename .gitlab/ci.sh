bb78df780b53ce8d37e615e5b1cea0402abfd6a9
Moritz Angermann <moritz.angermann@gmail.com>
2021-02-17 21:26:19 +0800

    [ci] default value for CONFIGURE_ARGS
    (cherry picked from commit 307d34945b7d932156e533736c91097493e6181b)

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -394,7 +394,7 @@ function configure() {
   run ./configure \
     --enable-tarballs-autodownload \
     $target_args \
-    $CONFIGURE_ARGS \
+    ${CONFIGURE_ARGS:-} \
     GHC="$GHC" \
     HAPPY="$HAPPY" \
     ALEX="$ALEX" \
