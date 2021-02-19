d177ccd881d3c0431fb446d04997e1a2d2fb29af
Matthew Pickering <matthewtpickering@gmail.com>
2021-02-19 18:58:22 +0000

    Reimplement Stream in "yoneda" style for efficiency
    'Stream' is implemented in the "yoneda" style for efficiency. By
    representing a stream in this manner 'fmap' and '>>=' operations are
    accumulated in the function parameters before being applied once when
    the stream is destroyed. In the old implementation each usage of 'mapM'
    and '>>=' would traverse the entire stream in order to apply the
    substitution at the leaves. It is well-known for free monads that this
    representation can improve performance, and the test results
    demonstrate this for GHC as well.
    
    The operation mapAccumL is not used in the compiler and can't be
    implemented efficiently because it requires destroying and rebuilding
    the stream.
    
    I removed one use of mapAccumL_ which has similar problems but the other
    use was difficult to remove. In the future it may be worth exploring
    whether the 'Stream' encoding could be modified further to capture the
    mapAccumL pattern, and likewise defer the passing of accumulation
    parameter until the stream is finally consumed.
    
    The >>= operation for 'Stream' was a hot-spot in the ticky profile for
    the "ManyConstructors" test which called the 'cg' function many times in
    "StgToCmm.hs"
    
    Metric Decrease:
        ManyConstructors

--- compiler/GHC/Driver/CodeOutput.hs
+++ compiler/GHC/Driver/CodeOutput.hs
@@ -149,7 +149,7 @@ outputC logger dflags filenm cmm_stream packages =
                           FormatC
                           doc
             printForC dflags h doc
-      Stream.consume cmm_stream writeC
+      Stream.consume cmm_stream id writeC
 
 {-
 ************************************************************************
