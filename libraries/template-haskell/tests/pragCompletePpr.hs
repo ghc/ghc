46a8f212386f0fc4ca3e658c15d048650bf72514
Cale Gibbard <cgibbard@gmail.com>
2021-02-22 15:56:22 -0500

    An initial attempt at type-directed COMPLETE pragmas.
    Implement some suggestions from Sebastian Graf
    
    Fix a test, add another

--- libraries/template-haskell/tests/pragCompletePpr.hs
+++ libraries/template-haskell/tests/pragCompletePpr.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
 import Language.Haskell.TH.Ppr    (ppr)
-import Language.Haskell.TH.Syntax (Dec (PragmaD), Pragma (CompleteP), mkName)
+import Language.Haskell.TH.Syntax (Dec (PragmaD), Pragma (CompleteP), mkName, Type(ConT))
 
 main :: IO ()
-main = print $ ppr $ PragmaD $ CompleteP [mkName "Foo", mkName "Bar"] $ Just $ mkName "Bar"
+main = print $ ppr $ PragmaD $ CompleteP [mkName "Foo", mkName "Bar"] $ Just (ConT (mkName "Bar"))
