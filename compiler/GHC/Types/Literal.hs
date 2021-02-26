833a6b84e2a43e2606e7a64a8df94c6fc3c5ba13
Sebastian Graf <sebastian.graf@kit.edu>
2021-02-26 09:49:10 +0100

    Make `Ord Literal` deterministic (#19438)
    Previously, non-determinism arising from a use of `uniqCompareFS` in
    `cmpLit` potentially crept into `CoreMap`, which we expect to behave
    deterministically.
    
    So we simply use `lexicalCompareFS` now.
    
    Fixes #19438.

--- compiler/GHC/Types/Literal.hs
+++ compiler/GHC/Types/Literal.hs
@@ -869,7 +869,7 @@ cmpLit (LitString    a)     (LitString     b)     = a `compare` b
 cmpLit (LitNullAddr)        (LitNullAddr)         = EQ
 cmpLit (LitFloat     a)     (LitFloat      b)     = a `compare` b
 cmpLit (LitDouble    a)     (LitDouble     b)     = a `compare` b
-cmpLit (LitLabel     a _ _) (LitLabel      b _ _) = a `uniqCompareFS` b
+cmpLit (LitLabel     a _ _) (LitLabel      b _ _) = a `lexicalCompareFS` b
 cmpLit (LitNumber nt1 a)    (LitNumber nt2  b)
   | nt1 == nt2 = a   `compare` b
   | otherwise  = nt1 `compare` nt2
