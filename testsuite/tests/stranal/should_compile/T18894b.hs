c8a499f3e7b7906e3bdf55bc372f43e3501e571c
Simon Peyton Jones <simonpj@microsoft.com>
2021-03-02 23:08:39 +0000

    More robust rewrite rules in GHC.Float

--- testsuite/tests/stranal/should_compile/T18894b.hs
+++ testsuite/tests/stranal/should_compile/T18894b.hs
@@ -17,4 +17,14 @@ f :: Int -> Int
 f 1 = 0
 f m
   | odd m     = eta m 2
-  | otherwise = eta 2 m
+  | otherwise = eta m m
+
+{-
+An earlier version of this test had (eta 2 m) in the otherwise case.
+But then (eta 2) could be floated out; and indeed if 'f' is applied
+many times, then sharing (eta 2) might be good.  And if we inlined
+eta, we certainly would share (expensive 2).
+
+So I made the test more robust at testing what we actually want here,
+by changing to (eta m m).
+-}
