8d4509916c7441cde1e3fd80bb5cc27a1d9786f8
Sylvain Henry <sylvain@haskus.fr>
2021-03-15 18:19:16 +0100

    Move loader state into Interp
    The loader state was stored into HscEnv. As we need to have two
    interpreters and one loader state per interpreter in #14335, it's
    natural to make the loader state a field of the Interp type.
    
    As a side effect, many functions now only require a Interp parameter
    instead of HscEnv. Sadly we can't fully free GHC.Linker.Loader of HscEnv
    yet because the loader is initialised lazily from the HscEnv the first
    time it is used. This is left as future work.
    
    HscEnv may not contain an Interp value (i.e. hsc_interp :: Maybe Interp).
    So a side effect of the previous side effect is that callers of the
    modified functions now have to provide an Interp. It is satisfying as it
    pushes upstream the handling of the case where HscEnv doesn't contain an
    Interpreter. It is better than raising a panic (less partial functions,
    "parse, don't validate", etc.).

--- testsuite/tests/rts/linker/LinkerUnload.hs
+++ testsuite/tests/rts/linker/LinkerUnload.hs
@@ -5,6 +5,7 @@ import GHC.Unit.State
 import GHC.Driver.Env
 import GHC.Driver.Session
 import GHC.Driver.Backend
+import GHC.Runtime.Interpreter
 import qualified GHC.Linker.Loader as Loader
 import System.Environment
 import GHC.Utils.Monad ( MonadIO(..) )
@@ -20,4 +21,4 @@ loadPackages = do
                          , ghcLink = LinkInMemory }
     setSessionDynFlags dflags'
     hsc_env <- getSession
-    liftIO $ Loader.loadPackages hsc_env (preloadUnits (hsc_units hsc_env))
+    liftIO $ Loader.loadPackages (hscInterp hsc_env) hsc_env (preloadUnits (hsc_units hsc_env))
