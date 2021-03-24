402198da662650355e1e646bcae4950ab3cf30e2
Moritz Angermann <moritz.angermann@gmail.com>
2021-03-24 10:07:02 +0800

    [linker] SymbolExtras are only used on PPC and X86
    (cherry picked from commit dc6fa61c7e8ca54e4b9feb6d2db59c4b365de09d)

--- rts/linker/SymbolExtras.h
+++ rts/linker/SymbolExtras.h
@@ -16,13 +16,12 @@ SymbolExtra* makeArmSymbolExtra( ObjectCode const* oc,
                                  unsigned long target,
                                  bool fromThumb,
                                  bool toThumb );
-#else
+#elif defined(powerpc_HOST_ARCH) || defined(x86_64_HOST_ARCH)
 SymbolExtra* makeSymbolExtra( ObjectCode const* oc,
                               unsigned long symbolNumber,
                               unsigned long target );
 
-#endif /* arm_HOST_ARCH */
-
+#endif /* powerpc_HOST_ARCH || x86_64_HOST_ARCH */
 #endif /* NEED_SYMBOL_EXTRAS */
 
 #include "EndPrivate.h"
