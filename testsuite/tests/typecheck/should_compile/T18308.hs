e9777ef50f742c0af9105fcc43a130c8ba7ac1b0
Ryan Scott <ryan.gl.scott@gmail.com>
2021-03-27 09:38:01 -0400

    Add regression tests for #17772 and #18308
    Resolves #17772. Addresses one part of #18308.

---
+++ testsuite/tests/typecheck/should_compile/T18308.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE NoCUSKs #-}
+{-# LANGUAGE TypeFamilies #-}
+module T18308 where
+
+import Data.Kind (Type)
+import Data.Proxy (Proxy)
+
+class Cls where
+    type Fam (k :: Type) (a :: k) :: Type
+    mtd :: Proxy k -> Proxy (a :: k) -> Fam k a -> Int
