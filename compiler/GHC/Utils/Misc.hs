8a0e11020eb2e3768828310452c9a0cdcfc3eb80
Matthew Pickering <matthewtpickering@gmail.com>
2021-03-29 23:09:25 +0100

    Fix

--- compiler/GHC/Utils/Misc.hs
+++ compiler/GHC/Utils/Misc.hs
@@ -1079,8 +1079,8 @@ strictMap f (x : xs) =
     x' : xs'
 
 strictZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-strictZipWith _ [] ys = ys
-strictZipWith _ xs [] = xs
+strictZipWith _ [] ys = []
+strictZipWith _ xs [] = []
 strictZipWith f (x : xs) (y: ys) =
   let
     !x' = f x y
