2dfdbd3dc963afa322805e04ebf70c5f9c6885de
Alfredo Di Napoli <alfredo@well-typed.com>
2021-03-29 16:38:04 +0200

    WIP Convert Tc & HsToCore to use TcRnMessage and DsMessage

--- compiler/GHC/HsToCore/Errors/Types.hs
+++ compiler/GHC/HsToCore/Errors/Types.hs
@@ -2,7 +2,12 @@
 module GHC.HsToCore.Errors.Types where
 
 import GHC.Prelude ()
+import GHC.Tc.Errors.Types
+import GHC.Types.Error (DiagnosticMessage)
 
 -- NOTE(adn): This is an opaque type where constructors will be added
 -- in the context of #18516.
-data DsMessage
+data DsMessage =
+    DsUnknownMessage !DiagnosticMessage
+  | DsLiftedTcRnMessage !TcRnMessage
+  -- ^ A diagnostic coming straight from the Typecheck-renamer.
