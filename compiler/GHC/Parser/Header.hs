892f22b7bd7afe3c3ffd2952a3b8ff64496c4e79
Alfredo Di Napoli <alfredo@well-typed.com>
2021-04-06 16:27:14 +0200

    Add GhcMessage and ancillary types
    This commit:
    
    * Adds GhcMessage and ancillary (PsMessage, TcRnMessage, ..) types;
    * Move error types and ppr where they belong
    
    Metric Increase:
        MultiLayerModules

--- compiler/GHC/Parser/Header.hs
+++ compiler/GHC/Parser/Header.hs
@@ -39,7 +39,7 @@ import GHC.Hs
 import GHC.Unit.Module
 import GHC.Builtin.Names
 
-import GHC.Types.Error hiding ( getErrorMessages, getWarningMessages )
+import GHC.Types.Error hiding ( getErrorMessages, getWarningMessages, getMessages )
 import GHC.Types.SrcLoc
 import GHC.Types.SourceError
 import GHC.Types.SourceText
