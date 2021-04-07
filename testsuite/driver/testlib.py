f61b6240d12936c7fc36a819f061c885137c59cc
Moritz Angermann <moritz.angermann@gmail.com>
2021-04-07 17:57:32 +0800

    [testsuite] fix T13702 with clang
    (cherry picked from commit 4db2d44ccbd0b615f41487bae082d1d14d37ecce)

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -2247,6 +2247,9 @@ def normalise_errmsg(s: str) -> str:
     s = re.sub('We will try though\.\.\..*\n','',s)
     # ignore warning about strip invalidating signatures
     s = re.sub('.*strip: changes being made to the file will invalidate the code signature in.*\n','',s)
+    # clang may warn about unused argument when used as assembler
+    s = re.sub('.*warning: argument unused during compilation:.*\n', '', s)
+
     return s
 
 # normalise a .prof file, so that we can reasonably compare it against
@@ -2330,6 +2333,8 @@ def normalise_output( s: str ) -> str:
     s = re.sub('We will try though\.\.\..*\n','',s)
     # ignore warning about strip invalidating signatures
     s = re.sub('.*strip: changes being made to the file will invalidate the code signature in.*\n','',s)
+    # clang may warn about unused argument when used as assembler
+    s = re.sub('.*warning: argument unused during compilation:.*\n', '', s)
 
     return s
 
