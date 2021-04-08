8e2af4b10aba133ff8eae9d435f788b8b896102d
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-04-08 18:32:04 +0000

    Print the iserv port before calling accept

--- libraries/libiserv/src/Remote/Slave.hs
+++ libraries/libiserv/src/Remote/Slave.hs
@@ -63,11 +63,11 @@ startSlave' verbose base_path port = do
 
   sock <- openSocket port
   actualPort <- socketPort sock
+  putStrLn $ "Listening on port " ++ show actualPort
 
   forever $ do
     when verbose $ trace "Opening socket"
     pipe <- acceptSocket sock >>= socketToPipe
-    putStrLn $ "Listening on port " ++ show actualPort
     when verbose $ trace "Starting serv"
     uninterruptibleMask $ serv verbose (hook verbose base_path pipe) pipe
     when verbose $ trace "serv ended"
