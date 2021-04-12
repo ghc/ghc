c8564c639a9889d4d19c68f4b96c092f670b092c
Alfredo Di Napoli <alfredo@well-typed.com>
2021-04-12 14:21:20 +0200

    Add some TcRn diagnostic messages
    This commit converts some TcRn diagnostic into proper structured
    errors.
    
    Ported by this commit:
    
    * Add TcRnImplicitLift
    
        This commit adds the TcRnImplicitLift diagnostic message and a prototype
        API to be able to log messages which requires additional err info.
    
    * Add TcRnUnusedPatternBinds
    * Add TcRnDodgyExports
    * Add TcRnDodgyImports message
    * Add TcRnMissingImportList

--- compiler/GHC/Tc/Errors/Ppr.hs
+++ compiler/GHC/Tc/Errors/Ppr.hs
@@ -1,10 +1,60 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-} -- instance Diagnostic TcRnMessage
 
-module GHC.Tc.Errors.Ppr where
+module GHC.Tc.Errors.Ppr (
+  ) where
+
+import GHC.Prelude
 
 import GHC.Tc.Errors.Types
 import GHC.Types.Error
+import GHC.Driver.Flags
+import GHC.Hs
+import GHC.Utils.Outputable
 
 instance Diagnostic TcRnMessage where
-  diagnosticMessage (TcRnUnknownMessage m) = diagnosticMessage m
-  diagnosticReason  (TcRnUnknownMessage m) = diagnosticReason m
+  diagnosticMessage = \case
+    TcRnUnknownMessage m
+      -> diagnosticMessage m
+    TcRnImplicitLift id_or_name errInfo
+      -> mkDecorated [text "The variable" <+> quotes (ppr id_or_name) <+>
+                      text "is implicitly lifted in the TH quotation"
+                     , getErrInfo errInfo
+                     ]
+    TcRnUnusedPatternBinds bind
+      -> mkDecorated [hang (text "This pattern-binding binds no variables:") 2 (ppr bind)]
+    TcRnDodgyImports name
+      -> mkDecorated [dodgy_msg (text "import") name (dodgy_msg_insert name :: IE GhcPs)]
+    TcRnDodgyExports name
+      -> mkDecorated [dodgy_msg (text "export") name (dodgy_msg_insert name :: IE GhcRn)]
+    TcRnMissingImportList ie
+      -> mkDecorated [ text "The import item" <+> quotes (ppr ie) <+>
+                       text "does not have an explicit import list"
+                     ]
+  diagnosticReason = \case
+    TcRnUnknownMessage m
+      -> diagnosticReason m
+    TcRnImplicitLift{}
+      -> WarningWithFlag Opt_WarnImplicitLift
+    TcRnUnusedPatternBinds{}
+      -> WarningWithFlag Opt_WarnUnusedPatternBinds
+    TcRnDodgyImports{}
+      -> WarningWithFlag Opt_WarnDodgyImports
+    TcRnDodgyExports{}
+      -> WarningWithFlag Opt_WarnDodgyExports
+    TcRnMissingImportList{}
+      -> WarningWithFlag Opt_WarnMissingImportList
+
+dodgy_msg :: (Outputable a, Outputable b) => SDoc -> a -> b -> SDoc
+dodgy_msg kind tc ie
+  = sep [ text "The" <+> kind <+> text "item"
+                     <+> quotes (ppr ie)
+                <+> text "suggests that",
+          quotes (ppr tc) <+> text "has (in-scope) constructors or class methods,",
+          text "but it has none" ]
+
+dodgy_msg_insert :: forall p . IdP (GhcPass p) -> IE (GhcPass p)
+dodgy_msg_insert tc = IEThingAll noAnn ii
+  where
+    ii :: LIEWrappedName (IdP (GhcPass p))
+    ii = noLocA (IEName $ noLocA tc)
