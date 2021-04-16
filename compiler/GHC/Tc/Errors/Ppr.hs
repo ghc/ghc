6054b35c3f7082f29cbc8c8ab41331ccfcc7ab9a
Richard Eisenberg <rae@richarde.dev>
2021-04-16 11:39:04 -0400

    Kill TcRnDsMessage completely

--- compiler/GHC/Tc/Errors/Ppr.hs
+++ compiler/GHC/Tc/Errors/Ppr.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-} -- instance Diagnostic {TcRnDsMessage, TcRnMessage}
+{-# OPTIONS_GHC -fno-warn-orphans #-} -- instance Diagnostic TcRnMessage
 
 module GHC.Tc.Errors.Ppr where
 
@@ -7,10 +7,6 @@ import GHC.Prelude
 import GHC.Tc.Errors.Types
 import GHC.Types.Error
 import GHC.HsToCore.Errors.Ppr () -- instance Diagnostic DsMessage
-
-instance Diagnostic TcRnDsMessage where
-  diagnosticMessage (TcRnDsMessage m) = either diagnosticMessage diagnosticMessage m
-  diagnosticReason  (TcRnDsMessage m) = either diagnosticReason  diagnosticReason m
 
 instance Diagnostic TcRnMessage where
   diagnosticMessage (TcRnUnknownMessage m) = diagnosticMessage m
