6aecb85938d970060465d8ef1760f9cac8c4c1a2
Alfredo Di Napoli <alfredo@well-typed.com>
2021-04-20 11:03:01 +0200

    Extensible Hints for diagnostic messages
    This commit extends the GHC diagnostic hierarchy with a `Hint` type,
    modelling helpful suggestions emitted by GHC which can be used to deal
    with a particular warning or error.
    
    As a direct consequence of this, the `Diagnostic` typeclass has been extended
    with a `diagnosticHints` method, which returns a `[Hint]`.
    
    On top of that, the old `PsHint` type has been superseded by the new `Hint`
    type, which de-duplicates some hints in favour of a general `SuggestExtension`
    constructor that takes a `GHC.LanguageExtensions.Extension`.

--- compiler/GHC/HsToCore/Errors/Ppr.hs
+++ compiler/GHC/HsToCore/Errors/Ppr.hs
@@ -1,11 +1,13 @@
-
-{-# OPTIONS_GHC -fno-warn-orphans #-} -- instance Diagnostic DsMessage
+{-# OPTIONS_GHC -fno-warn-orphans #-} -- instance {Diagnostic, RenderableMessage} DsMessage
 
 module GHC.HsToCore.Errors.Ppr where
 
 import GHC.Types.Error
 import GHC.HsToCore.Errors.Types
 
-instance Diagnostic DsMessage where
+instance RenderableMessage DsMessage where
   diagnosticMessage (DsUnknownMessage m) = diagnosticMessage m
+
+instance Diagnostic DsMessage where
   diagnosticReason  (DsUnknownMessage m) = diagnosticReason m
+  diagnosticHints   (DsUnknownMessage m) = diagnosticHints m
