f2dd5be19b1cec019327d7e8bf3bdbbcb3c8dd17
Divam <dfordivam@gmail.com>
2021-04-23 17:12:21 +0900

    Split the checking of StableModules from the actual compilation. This splitting is needed to be able to do these checks before obtaining the semaphore in parUpsweep_mod (see #14905 for details)

--- compiler/GHC/Driver/Make.hs
+++ compiler/GHC/Driver/Make.hs
@@ -1684,7 +1684,26 @@ upsweep_mod :: HscEnv
             -> Int  -- index of module
             -> Int  -- total number of modules
             -> IO HomeModInfo
-upsweep_mod hsc_env mHscMessage old_hpt (stable_obj, stable_bco) summary mod_index nmods
+upsweep_mod hsc_env mHscMessage old_hpt stableModules summary mod_index nmods
+   = do
+        status <- checkStableModules hsc_env old_hpt stableModules summary
+        case status of
+            Stable hmi -> return hmi
+            NotStable src_modified mb_old_iface mb_linkable ->
+                compileOne' Nothing mHscMessage hsc_env summary mod_index nmods
+                             mb_old_iface mb_linkable src_modified
+
+
+data Stability = Stable HomeModInfo
+               | NotStable SourceModified (Maybe ModIface) (Maybe Linkable)
+
+checkStableModules :: HscEnv
+                   -> HomePackageTable
+                   -> StableModules
+                   -> ModSummary
+                   -> IO Stability
+
+checkStableModules hsc_env old_hpt (stable_obj, stable_bco) summary
    =    let
             this_mod_name = ms_mod_name summary
             this_mod    = ms_mod summary
@@ -1725,16 +1744,14 @@ upsweep_mod hsc_env mHscMessage old_hpt (stable_obj, stable_bco) summary mod_ind
                                    where
                                      iface = hm_iface hm_info
 
-            compile_it :: Maybe Linkable -> SourceModified -> IO HomeModInfo
+            compile_it :: Maybe Linkable -> SourceModified -> IO Stability
             compile_it  mb_linkable src_modified =
-                  compileOne' Nothing mHscMessage hsc_env summary mod_index nmods
-                             mb_old_iface mb_linkable src_modified
+              return $ NotStable src_modified mb_old_iface mb_linkable
 
             compile_it_discard_iface :: Maybe Linkable -> SourceModified
-                                     -> IO HomeModInfo
-            compile_it_discard_iface mb_linkable  src_modified =
-                  compileOne' Nothing mHscMessage hsc_env summary mod_index nmods
-                             Nothing mb_linkable src_modified
+                                     -> IO Stability
+            compile_it_discard_iface mb_linkable src_modified =
+              return $ NotStable src_modified Nothing mb_linkable
 
             -- With NoBackend we create empty linkables to avoid recompilation.
             -- We have to detect these to recompile anyway if the backend changed
@@ -1759,7 +1776,7 @@ upsweep_mod hsc_env mHscMessage old_hpt (stable_obj, stable_bco) summary mod_ind
                 -- if it is *stable* (see checkStability).
           | is_stable_obj, Just hmi <- old_hmi -> do
                 debug_trace 5 (text "skipping stable obj mod:" <+> ppr this_mod_name)
-                return hmi
+                return $ Stable hmi
                 -- object is stable, and we have an entry in the
                 -- old HPT: nothing to do
 
@@ -1776,7 +1793,7 @@ upsweep_mod hsc_env mHscMessage old_hpt (stable_obj, stable_bco) summary mod_ind
                 ASSERT(isJust old_hmi) -- must be in the old_hpt
                 let Just hmi = old_hmi in do
                 debug_trace 5 (text "skipping stable BCO mod:" <+> ppr this_mod_name)
-                return hmi
+                return $ Stable hmi
                 -- BCO is stable: nothing to do
 
           | not (backendProducesObject bcknd),
