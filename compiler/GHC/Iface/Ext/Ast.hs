3b51cdf133e764da33eca0e3866b0d891d866ebe
Ryan Scott <ryan.gl.scott@gmail.com>
2021-04-24 08:55:53 -0400

    Experiment: Try invoking lhsExprType less in GHC.Iface.Ext.Ast

--- compiler/GHC/Iface/Ext/Ast.hs
+++ compiler/GHC/Iface/Ext/Ast.hs
@@ -749,7 +749,7 @@ instance HiePass p => HasType (LocatedA (HsExpr (GhcPass p))) where
             HsRecFld{}          -> False
             HsOverLabel{}       -> False
             HsIPVar{}           -> False
-            XExpr (WrapExpr {}) -> False
+            -- XExpr (WrapExpr {}) -> False
             HsUnboundVar{}      -> False
             HsLit{}             -> False
             HsOverLit{}         -> False
