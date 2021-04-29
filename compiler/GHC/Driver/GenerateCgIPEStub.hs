719be13fd732d668d7fa726ca72c34c57a1ac64d
Sven Tennie <sven.tennie@gmail.com>
2021-04-29 08:07:09 +0200

    Only lookup tickishes for stack rep info tables, only emit IPEs when -finfo-table-map is enabled (performance)

--- compiler/GHC/Driver/GenerateCgIPEStub.hs
+++ compiler/GHC/Driver/GenerateCgIPEStub.hs
@@ -15,21 +15,27 @@ import GHC.Data.Maybe (firstJusts)
 import GHC.Data.Stream (Stream, liftIO)
 import qualified GHC.Data.Stream as Stream
 import GHC.Driver.Env (hsc_dflags)
+import GHC.Driver.Flags (GeneralFlag (Opt_InfoTableMap))
+import GHC.Driver.Session (gopt)
 import GHC.Plugins (HscEnv, NonCaffySet)
 import GHC.Prelude
+import GHC.Runtime.Heap.Layout (isStackRep)
 import GHC.StgToCmm.Monad (getCmm, initC, runC)
 import GHC.StgToCmm.Prof (initInfoTableProv)
 import GHC.StgToCmm.Types (CgInfos (..), ModuleLFInfos)
 import GHC.Types.IPE (InfoTableProvMap (labeledInfoTablesWithTickishes))
 import GHC.Unit.Types (Module)
-import GHC.Runtime.Heap.Layout (isStackRep)
 
 -- TODO: Does this check flag to see if IPE is wanted?
 generateCgIPEStub :: HscEnv -> Module -> InfoTableProvMap -> Stream IO CmmGroupSRTs (NonCaffySet, ModuleLFInfos) -> Stream IO CmmGroupSRTs CgInfos
 generateCgIPEStub hsc_env this_mod denv s = do
   let dflags = hsc_dflags hsc_env
   cgState <- liftIO initC
-  (labeledInfoTablesWithTickishes, (nonCaffySet, moduleLFInfos)) <- Stream.mapAccumL_ collect [] s
+
+  -- Collect info tables, but only if -finfo-table-map is enabled, otherwise labeledInfoTablesWithTickishes is empty.
+  let collectFun = if gopt Opt_InfoTableMap dflags then collect else collectNothing
+  (labeledInfoTablesWithTickishes, (nonCaffySet, moduleLFInfos)) <- Stream.mapAccumL_ collectFun [] s
+
   let denv' = denv {labeledInfoTablesWithTickishes = labeledInfoTablesWithTickishes}
       ((ipeStub, cmmGroup), _) = runC dflags this_mod cgState $ getCmm (initInfoTableProv (map sndOfTriple labeledInfoTablesWithTickishes) denv' this_mod)
   (_, cmmGroupSRTs) <- liftIO $ cmmPipeline hsc_env (emptySRT this_mod) cmmGroup
@@ -41,6 +47,9 @@ generateCgIPEStub hsc_env this_mod denv s = do
       let labelsToInfoTables = collectInfoTables cmmGroupSRTs
           labelsToInfoTablesToTickishes = map (\(l, i) -> (l, i, lookupEstimatedTick cmmGroupSRTs l i)) labelsToInfoTables
       return (acc ++ labelsToInfoTablesToTickishes, cmmGroupSRTs)
+
+    collectNothing :: [(Label, CmmInfoTable, Maybe CmmTickish)] -> CmmGroupSRTs -> IO ([(Label, CmmInfoTable, Maybe CmmTickish)], CmmGroupSRTs)
+    collectNothing _ cmmGroupSRTs = pure ([], cmmGroupSRTs)
 
     sndOfTriple :: (a, b, c) -> b
     sndOfTriple (_, b, _) = b
@@ -57,11 +66,11 @@ generateCgIPEStub hsc_env this_mod denv s = do
     -- TODO: Write a note: [Looking up CmmTickish for return frame info tables]
     lookupEstimatedTick :: CmmGroupSRTs -> Label -> CmmInfoTable -> Maybe CmmTickish
     lookupEstimatedTick cmmGroup infoTableLabel infoTable = do
-      if(isStackRep . cit_rep) infoTable then do
-        let blocks = concatMap toBlockList (graphs cmmGroup)
-        firstJusts $ map (findCmmTickishForLabelInBlock infoTableLabel) blocks
-      else
-        Nothing
+      if (isStackRep . cit_rep) infoTable
+        then do
+          let blocks = concatMap toBlockList (graphs cmmGroup)
+          firstJusts $ map (findCmmTickishForLabelInBlock infoTableLabel) blocks
+        else Nothing
 
     graphs :: CmmGroupSRTs -> [CmmGraph]
     graphs = foldl' go []
