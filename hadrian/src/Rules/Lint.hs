14a848088ba037615b2e85e760c6803a0829d172
Hécate Moonlight <hecate+gitlab@glitchbra.in>
2021-05-01 09:33:41 +0200

    Disable HLint colours
    closes #19776

--- hadrian/src/Rules/Lint.hs
+++ hadrian/src/Rules/Lint.hs
@@ -13,7 +13,7 @@ lintRules = do
 
 lint :: Action () -> Action ()
 lint lintAction = do
-  isHlintPresent <- isJust <$> (liftIO $ findExecutable "hlint")
+  isHlintPresent <- isJust <$> liftIO (findExecutable "hlint")
   if isHlintPresent
   then do
     putBuild "| Running the linter…"
@@ -30,9 +30,10 @@ runHLint includeDirs defines dir = do
   threads <- shakeThreads <$> getShakeOptions
   hostArch <- (<> "_HOST_ARCH") <$> setting HostArch
   let hlintYaml = dir </> ".hlint.yaml"
-      defines' = [hostArch] ++ defines
+      defines' = hostArch : defines
       cmdLine = unwords $
         [ "hlint"
+        , "--colour=never"
         , "-j" <> show threads
         ] ++
         map ("--cpp-define=" <>) defines' ++
