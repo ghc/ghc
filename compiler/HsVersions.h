f78c25da05849797dab684a221923aeac244c69b
Sylvain Henry <sylvain@haskus.fr>
2021-05-06 11:30:57 +0200

    Move GlobalVar macros into GHC.Utils.GlobalVars
    That's the only place where they are used and they shouldn't be used
    elsewhere.

--- compiler/HsVersions.h
+++ compiler/HsVersions.h
@@ -1,8 +1,5 @@
 #pragma once
 
--- For GHC_STAGE
-#include "ghcplatform.h"
-
 #if 0
 
 IMPORTANT!  If you put extra tabs/spaces in these macro definitions,
@@ -11,32 +8,6 @@ you will screw up the layout where they are used in case expressions!
 (This is cpp-dependent, of course)
 
 #endif
-
-#define GLOBAL_VAR(name,value,ty)  \
-{-# NOINLINE name #-};             \
-name :: IORef (ty);                \
-name = GHC.Utils.GlobalVars.global (value);
-
-#define GLOBAL_VAR_M(name,value,ty) \
-{-# NOINLINE name #-};              \
-name :: IORef (ty);                 \
-name = GHC.Utils.GlobalVars.globalM (value);
-
-
-#define SHARED_GLOBAL_VAR(name,accessor,saccessor,value,ty) \
-{-# NOINLINE name #-};                                      \
-name :: IORef (ty);                                         \
-name = GHC.Utils.GlobalVars.sharedGlobal (value) (accessor);\
-foreign import ccall unsafe saccessor                       \
-  accessor :: Ptr (IORef a) -> IO (Ptr (IORef a));
-
-#define SHARED_GLOBAL_VAR_M(name,accessor,saccessor,value,ty)  \
-{-# NOINLINE name #-};                                         \
-name :: IORef (ty);                                            \
-name = GHC.Utils.GlobalVars.sharedGlobalM (value) (accessor);  \
-foreign import ccall unsafe saccessor                          \
-  accessor :: Ptr (IORef a) -> IO (Ptr (IORef a));
-
 
 #define ASSERT(e)      if debugIsOn && not (e) then (assertPanic __FILE__ __LINE__) else
 #define ASSERT2(e,msg) if debugIsOn && not (e) then (assertPprPanic __FILE__ __LINE__ (msg)) else
