711c56b48c64abf1745b5b72574335e060404a1e
Sylvain Henry <sylvain@haskus.fr>
2021-05-06 16:07:32 +0200

    Ensure assert from Control.Exception isn't used

--- compiler/GHC/CmmToAsm.hs
+++ compiler/GHC/CmmToAsm.hs
@@ -134,9 +134,11 @@ import qualified GHC.Utils.Ppr as Pretty
 import GHC.Utils.BufHandle
 import GHC.Utils.Outputable as Outputable
 import GHC.Utils.Panic
+import GHC.Utils.Error
+import GHC.Utils.Exception (evaluate)
+
 import GHC.Data.FastString
 import GHC.Types.Unique.Set
-import GHC.Utils.Error
 import GHC.Unit
 import GHC.Data.Stream (Stream)
 import qualified GHC.Data.Stream as Stream
@@ -144,7 +146,6 @@ import qualified GHC.Data.Stream as Stream
 import Data.List (sortBy, groupBy)
 import Data.Maybe
 import Data.Ord         ( comparing )
-import Control.Exception
 import Control.Monad
 import System.IO
 
