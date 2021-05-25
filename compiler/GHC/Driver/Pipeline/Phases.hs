19c245bf2df8c685e169ff978f17a3c12a166599
Matthew Pickering <matthewtpickering@gmail.com>
2021-05-25 23:09:32 +0100

    driver: Convert runPipeline to use a free monad
    This patch converts the runPipeline function to be implemented in terms
    of a free monad rather than the previous CompPipeline.
    
    The advantages of this are three-fold:
    
    1. Different parts of the pipeline can return different results, the
    limits of runPipeline were being pushed already by !5555, this opens up
    futher fine-grainedism of the pipeline.
    2. The same mechanism can be extended to build-plan at the module level
    so the whole build plan can be expressed in terms of one computation
    which can then be treated uniformly.
    3. The pipeline monad can now be interpreted in different ways, for
    example, you may want to interpret the `TPhase` action into the monad
    for your own build system (such as shake). That bit will probably
    require a bit more work, but this is a step in the right directin.
    
    There are a few more modules containing useful functions for interacting
    with the pipelines.
    
    * GHC.Driver.Pipeline: Functions for building pipelines at a high-level
    * GHC.Driver.Pipeline.Execute: Functions for providing the default
    interpretation of TPhase, in terms of normal IO.
    * GHC.Driver.Pipeline.Phases: The home for TPhase, the typed phase data
    type which dictates what the phases are.
    * GHC.Driver.Pipeline.Monad: Definitions to do with the TPipelineClass
    and MonadUse class.
    
    Hooks consumers may notice the type of the `phaseHook` has got
    slightly more restrictive, you can now no longer control the
    continuation of the pipeline by returning the next phase to execute but
    only override individual phases. If this is a problem then please open
    an issue and we will work out a solution.

---
+++ compiler/GHC/Driver/Pipeline/Phases.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+
+module GHC.Driver.Pipeline.Phases (TPhase(..), PhaseHook(..)) where
+
+import GHC.Prelude
+import GHC.Driver.Pipeline.Monad
+import GHC.Driver.Env.Types
+import GHC.Driver.Session
+import GHC.Driver.CmdLine
+import GHC.Types.SourceFile
+import GHC.Unit.Module.ModSummary
+import GHC.Unit.Module.Status
+import GHC.Tc.Types ( FrontendResult )
+import GHC.Types.Error
+import GHC.Driver.Errors.Types
+import GHC.Fingerprint.Type
+import GHC.Unit.Module.Location ( ModLocation )
+import GHC.Unit.Module.Name ( ModuleName )
+import GHC.Unit.Module.ModIface
+import GHC.Linker.Types
+import GHC.Driver.Phases
+
+-- Typed Pipeline Phases
+-- MP: TODO: We need to refine the arguments to each of these phases so recompilation
+-- can be smarter. For example, rather than passing a whole HscEnv, just pass the options
+-- which each phase depends on, then recompilation checking can decide to only rerun each
+-- phase if the inputs have been modified.
+data TPhase res where
+  T_Unlit :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_FileArgs :: HscEnv -> FilePath -> TPhase (DynFlags, [Warn])
+  T_Cpp   :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_HsPp  :: PipeEnv -> HscEnv -> FilePath -> FilePath -> TPhase FilePath
+  T_HscRecomp :: PipeEnv -> HscEnv -> FilePath -> HscSource -> TPhase (HscEnv, ModSummary, HscRecompStatus)
+  T_Hsc :: HscEnv -> ModSummary -> TPhase (FrontendResult, Messages GhcMessage)
+  T_HscPostTc :: HscEnv -> ModSummary
+              -> FrontendResult
+              -> Messages GhcMessage
+              -> Maybe Fingerprint
+              -> TPhase HscBackendAction
+  T_HscBackend :: PipeEnv -> HscEnv -> ModuleName -> HscSource -> ModLocation -> HscBackendAction -> TPhase ([FilePath], ModIface, Maybe Linkable, FilePath)
+  T_CmmCpp :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_Cmm :: PipeEnv -> HscEnv -> FilePath -> TPhase ([FilePath], FilePath)
+  T_Cc :: Phase -> PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_As :: Bool -> PipeEnv -> HscEnv -> Maybe ModLocation -> FilePath -> TPhase FilePath
+  T_LlvmOpt :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_LlvmLlc :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_LlvmMangle :: PipeEnv -> HscEnv -> FilePath -> TPhase FilePath
+  T_MergeForeign :: PipeEnv -> HscEnv -> Maybe ModLocation -> FilePath -> [FilePath] -> TPhase FilePath
+
+-- | A wrapper around the interpretation function for phases.
+data PhaseHook = PhaseHook (forall a . TPhase a -> IO a)
