021b2bc9051e1856615b8d5e055e7ac66eb6d9b5
Moritz Angermann <moritz.angermann@gmail.com>
2021-06-02 11:36:59 +0800

    platform_constants is now a Maybe

--- compiler/GHC/Platform.hs
+++ compiler/GHC/Platform.hs
@@ -96,7 +96,7 @@ genericPlatform = Platform
    , platformIsCrossCompiling        = False
    , platformLeadingUnderscore       = False
    , platformTablesNextToCode        = True
-   , platformConstants               = error "No PlatformConstants"
+   , platform_constants               = Nothing
    }
 
 data PlatformWordSize
