96526c5690ed70d1939f299780fe0a5265e2554d
sheaf <sam.derbyshire@gmail.com>
2021-06-08 21:34:24 +0200

    Fix AmbigFDs test-case

--- testsuite/tests/typecheck/should_fail/AmbigFDs.hs
+++ testsuite/tests/typecheck/should_fail/AmbigFDs.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FunctionalDependencies, NoPolyKinds #-}
 -- NB: No AllowAmbiguousTypes. The type isn't ambiguous, because of the fundeps.
 -- But it is confusing, because we don't know whether b1 and b2 are really the
 -- same or not.
