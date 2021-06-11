e33239fe261414ad1ff7cefcf95dbbf7df336f1f
GHC GitLab CI <ghc-ci@gitlab-haskell.org>
2021-06-11 00:41:19 +0000

    codeGen: Fix header size for array write barriers
    Previously the code generator's logic for invoking the nonmoving write
    barrier was inconsistent with the write barrier itself. Namely, the code
    generator treated the header size argument as being in words whereas the
    barrier expected bytes. This was the cause of #19715.
    
    Fixes #19715.
    
    (cherry picked from commit 221a104f44fdf58e4514d41ae827747c2bf938c8)

--- compiler/GHC/StgToCmm/Prim.hs
+++ compiler/GHC/StgToCmm/Prim.hs
@@ -2643,7 +2643,7 @@ emitCopyArray copy src0 src_off dst0 dst_off0 n =
         dst_off <- assignTempE dst_off0
 
         -- Nonmoving collector write barrier
-        emitCopyUpdRemSetPush platform (arrPtrsHdrSizeW dflags) dst dst_off n
+        emitCopyUpdRemSetPush platform (arrPtrsHdrSize dflags) dst dst_off n
 
         -- Set the dirty bit in the header.
         emit (setInfo dst (CmmLit (CmmLabel mkMAP_DIRTY_infoLabel)))
@@ -2709,7 +2709,7 @@ emitCopySmallArray copy src0 src_off dst0 dst_off n =
         dst     <- assignTempE dst0
 
         -- Nonmoving collector write barrier
-        emitCopyUpdRemSetPush platform (smallArrPtrsHdrSizeW dflags) dst dst_off n
+        emitCopyUpdRemSetPush platform (smallArrPtrsHdrSize dflags) dst dst_off n
 
         -- Set the dirty bit in the header.
         emit (setInfo dst (CmmLit (CmmLabel mkSMAP_DIRTY_infoLabel)))
@@ -3042,7 +3042,7 @@ emitCtzCall res x width = do
 -- | Push a range of pointer-array elements that are about to be copied over to
 -- the update remembered set.
 emitCopyUpdRemSetPush :: Platform
-                      -> WordOff    -- ^ array header size
+                      -> ByteOff    -- ^ array header size (in bytes)
                       -> CmmExpr    -- ^ destination array
                       -> CmmExpr    -- ^ offset in destination array (in words)
                       -> Int        -- ^ number of elements to copy
