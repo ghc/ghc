d2ccf7183b33d3175878c23371f30c0a3c9b2b1a
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2021-06-19 21:44:17 +0200

    Simplify error handling
    The functions were repeating the logic in 'mkRuntimeErrorApp'.

--- compiler/GHC/HsToCore/Utils.hs
+++ compiler/GHC/HsToCore/Utils.hs
@@ -405,11 +405,8 @@ mkErrorAppDs :: Id              -- The error function
 mkErrorAppDs err_id ty msg = do
     src_loc <- getSrcSpanDs
     dflags <- getDynFlags
-    let
-        full_msg = showSDoc dflags (hcat [ppr src_loc, vbar, msg])
-        core_msg = Lit (mkLitString full_msg)
-        -- mkLitString returns a result of type String#
-    return (mkApps (Var err_id) [Type (getRuntimeRep ty), Type ty, core_msg])
+    let full_msg = showSDoc dflags (hcat [ppr src_loc, vbar, msg])
+    return (mkRuntimeErrorApp err_id ty full_msg)
 
 {-
 Note [Incompleteness and linearity]
