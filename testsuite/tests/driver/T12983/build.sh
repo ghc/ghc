4d119d545c6e6bfc211930e6b740502adef81227
Matthew Pickering <matthewtpickering@gmail.com>
2021-06-23 12:12:53 +0100

    driver: Add test for #12983
    This test has worked since 8.10.2 at least but was recently broken and
    is now working again after this patch.
    
    Closes #12983

---
+++ testsuite/tests/driver/T12983/build.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+GHC="${TEST_HC}"
+
+flags="${TEST_HC_OPTS} -O0 -fhide-source-paths -isrc -ibuild -dynamic-too -outputdir build -package template-haskell"
+
+function compile()
+{
+    echo $@
+    "$GHC" -c $flags $@ || exit 1
+}
+
+function setup()
+{
+    echo
+    echo "Preparing everything ..."
+    rm -rf build
+    mkdir -p build
+    cp src/Old_Hospital.hs src/Hospital.hs
+    compile src/Hospital.hs
+    compile src/Types.hs
+    compile src/MetaHandler.hs
+    echo "Done with preparations"
+}
+
+echo
+echo "Preparing everyting with --make ..."
+echo
+rm -rf build
+mkdir -p build
+cp src/Old_Hospital.hs src/Hospital.hs
+"$GHC" --make $flags src/MetaHandler.hs
+echo "Done with preparations with --make"
+echo
+echo "Building with --make"
+echo
+cp src/New_Hospital.hs src/Hospital.hs
+"$GHC" --make $flags src/MetaHandler.hs
+
+setup
+echo "Building file-by-file, with -fforce-recomp"
+echo
+cp src/New_Hospital.hs src/Hospital.hs
+compile src/ShortText.hs
+compile src/Hospital.hs
+compile -fforce-recomp src/Types.hs
+compile src/MetaHandler.hs
+
+setup
+echo
+echo "Building file-by-file"
+echo
+cp src/New_Hospital.hs src/Hospital.hs
+compile src/ShortText.hs
+compile src/Hospital.hs
+compile src/Types.hs
+compile src/MetaHandler.hs
