5ce03012ffaeaa19cbe19be0d1be28d2231cb36c
Richard Eisenberg <rae@richarde.dev>
2021-06-29 10:16:47 -0400

    Accept error message change in T8227

--- testsuite/tests/indexed-types/should_fail/T8227.hs
+++ testsuite/tests/indexed-types/should_fail/T8227.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE TypeFamilies #-}
 module T8227
-  ( 
+  (
     absoluteToParam
   ) where
 
@@ -48,3 +48,19 @@ fuv0 ~ fuv1 -> fuv1
 -}
 
 
+{-
+Once upon a time, we reported errors with
+     Couldn't match expected type: Scalar (V a)
+                  with actual type: Scalar (V (Scalar (V a)))
+                                    -> Scalar (V (Scalar (V a)))
+Now, it's
+     Couldn't match type: Scalar (V a)
+                     with: t0 -> t0
+      Expected: Scalar (V a)
+        Actual: Scalar (V (t0 -> t0)) -> Scalar (V (t0 -> t0))
+The old message is a bit better. But the only way we can get to the old
+message is to allow a wanted to rewrite a wanted. This is a bad idea
+in general, so we accept the error message regression. The new message
+isn't wrong, and perhaps in some ways its simplicity is actually an
+improvement over the previous one.
+-}
