19195ac068514cfa9d8cce387b0b9bc4081aa95d
Wander Hillen <wjw.hillen@gmail.com>
2021-07-08 16:49:01 +0200

    Remove unneeded (?) int narrowing bits

--- libraries/base/System/Linux/IO/URing/PVar.hs
+++ libraries/base/System/Linux/IO/URing/PVar.hs
@@ -25,9 +25,9 @@ class Prim a where
 instance Prim Word32 where
   sizeOf _ = 4
   readPVar (PVar mba) = IO $ \s ->
-    case readWord32Array# mba 0# s of (# s', r #) -> (# s', W32# (narrowWord32# r) #)
+    case readWord32Array# mba 0# s of (# s', r #) -> (# s', W32# r #)
   writePVar (PVar mba) (W32# x) = IO $ \s ->
-    case writeWord32Array# mba 0# (extendWord32# x) s of s' -> (# s', () #)
+    case writeWord32Array# mba 0# x s of s' -> (# s', () #)
 
 newPVar :: forall a. Prim a => a -> IO (PVar a)
 newPVar x = do
