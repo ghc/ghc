54ffea829b52d9d42a9927b8add3ff1e0ac64b54
Ben Gamari <ben@smart-cactus.org>
2021-07-12 20:07:59 -0400

    rts: Introduce and use ExecPage abstraction
    Here we introduce a very thin abstraction for allocating, filling, and
    freezing executable pages to replace allocateExec.

--- testsuite/tests/th/T10279.hs
+++ testsuite/tests/th/T10279.hs
@@ -2,9 +2,9 @@ module T10279 where
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax
 
--- NB: rts-1.0.1 is used here because it doesn't change.
+-- NB: rts-1.0.2 is used here because it doesn't change.
 -- You do need to pick the right version number, otherwise the
 -- error message doesn't recognize it as a source package ID,
 -- (This is OK,  since it will look obviously wrong when they
 -- try to find the package in their package database.)
-blah = $(conE (Name (mkOccName "Foo") (NameG VarName (mkPkgName "rts-1.0.1") (mkModName "A"))))
+blah = $(conE (Name (mkOccName "Foo") (NameG VarName (mkPkgName "rts-1.0.2") (mkModName "A"))))
