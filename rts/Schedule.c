48cdaaefc3247b90eaf8d9dbecf1aef1cc2ababf
Sylvain Henry <sylvain@haskus.fr>
2021-07-21 11:18:14 +0200

    RTS: try to fix timer races
    * Pthread based timer was initialized started while some other parts of
      the RTS assume it is initialized stopped, e.g. in hs_init_ghc:
    
         /* Start the "ticker" and profiling timer but don't start until the
         * scheduler is up. However, the ticker itself needs to be initialized
         * before the scheduler to ensure that the ticker mutex is initialized as
         * moreCapabilities will attempt to acquire it.
         */
    
    * after a fork, don't start the timer before the IOManager is
      initialized: the timer handler (handle_tick) might call wakeUpRts to
      perform an idle GC, which calls wakeupIOManager/ioManagerWakeup
    
    Found while debugging #18033/#20132 but I couldn't confirm if it fixes
    them.
    
    (cherry picked from commit d99e76ad3600b4c5051da6923fb7454346ed0b7b)

--- rts/Schedule.c
+++ rts/Schedule.c
@@ -2196,7 +2196,6 @@ forkProcess(HsStablePtr *entry
         // On Unix, all timers are reset in the child, so we need to start
         // the timer again.
         initTimer();
-        startTimer();
 
         // TODO: need to trace various other things in the child
         // like startup event, capabilities, process info etc
@@ -2205,6 +2204,10 @@ forkProcess(HsStablePtr *entry
 #if defined(THREADED_RTS)
         ioManagerStartCap(&cap);
 #endif
+
+        // start timer after the IOManager is initialized
+        // (the idle GC may wake up the IOManager)
+        startTimer();
 
         // Install toplevel exception handlers, so interruption
         // signal will be sent to the main thread.
