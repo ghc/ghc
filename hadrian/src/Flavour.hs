323473e86d40988883062e5aeaef97bee943b901
Sylvain Henry <sylvain@haskus.fr>
2021-07-26 12:18:34 +0200

    Hadrian: disable profiled RTS with no_profiled_libs flavour transformer
    Hadrian uses the RTS ways to determine which iserv programs to embed
    into bindist. But profiled iserv program (and any other code) can't be
    built without profiling libs and Hadrian fails. So we disable the
    profiling RTS way with the no_profiled_libs flavour transformer.

--- hadrian/src/Flavour.hs
+++ hadrian/src/Flavour.hs
@@ -178,7 +178,9 @@ disableDynamicGhcPrograms flavour = flavour { dynamicGhcPrograms = pure False }
 -- | Don't build libraries in profiled 'Way's.
 disableProfiledLibs :: Flavour -> Flavour
 disableProfiledLibs flavour =
-    flavour { libraryWays = filter (not . wayUnit Profiling) <$> libraryWays flavour }
+    flavour { libraryWays = filter (not . wayUnit Profiling) <$> libraryWays flavour
+            , rtsWays     = filter (not . wayUnit Profiling) <$> rtsWays flavour
+            }
 
 -- | Build stage2 compiler with -fomit-interface-pragmas to reduce
 -- recompilation.
