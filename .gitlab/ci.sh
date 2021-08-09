c4c5b252c864a0854558a162c76cbd6f53d61c37
Zubin Duggal <zubin.duggal@gmail.com>
2021-08-09 15:25:24 +0530

    Bump process submodule to 1.6.13.2

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -347,7 +347,7 @@ function push_perf_notes() {
 
 function test_make() {
   run "$MAKE" test_bindist TEST_PREP=YES
-  run "$MAKE" V=0 VERBOSE=1 test \
+  run "$MAKE" V=0 VERBOSE=1 BINDIST=YES test \
     THREADS="$cores" \
     JUNIT_FILE=../../junit.xml
 }
