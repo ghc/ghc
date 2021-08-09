232635a59de2b816eeeb65d11d5ca98834d30a81
Zubin Duggal <zubin.duggal@gmail.com>
2021-08-09 19:05:45 +0530

    Test bindist on 8.10 CI (#20211)

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -348,6 +348,7 @@ function push_perf_notes() {
 function test_make() {
   run "$MAKE" test_bindist TEST_PREP=YES
   run "$MAKE" V=0 VERBOSE=1 test \
+    BINDIST=YES
     THREADS="$cores" \
     JUNIT_FILE=../../junit.xml
 }
