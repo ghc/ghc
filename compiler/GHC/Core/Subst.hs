df1d808f26544cbb77d85773d672137c65fd3cc7
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2021-08-19 16:48:49 +0200

    Convert lookupIdSubst panic back to a warning (#20200)

--- compiler/GHC/Core/Subst.hs
+++ compiler/GHC/Core/Subst.hs
@@ -61,6 +61,7 @@ import GHC.Types.Unique.Supply
 import GHC.Builtin.Names
 import GHC.Data.Maybe
 
+import GHC.Utils.Trace
 import GHC.Utils.Misc
 import GHC.Utils.Outputable
 import GHC.Utils.Panic
@@ -255,9 +256,10 @@ lookupIdSubst (Subst in_scope ids _ _) v
   | Just e  <- lookupVarEnv ids       v = e
   | Just v' <- lookupInScope in_scope v = Var v'
         -- Vital! See Note [Extending the Subst]
-        -- If v isn't in the InScopeSet, we panic, because
-        -- it's a bad bug and we reallly want to know
-  | otherwise = pprPanic "lookupIdSubst" (ppr v $$ ppr in_scope)
+        -- See #20200
+  | otherwise = warnPprTrace True (text "GHC.Core.Subst.lookupIdSubst" <+> ppr v
+                            $$ ppr in_scope) $
+                Var v
 
 -- | Find the substitution for a 'TyVar' in the 'Subst'
 lookupTCvSubst :: Subst -> TyVar -> Type
