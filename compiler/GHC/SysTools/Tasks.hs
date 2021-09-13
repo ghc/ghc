98b2035bee3e5a51dae934a4b44dc91dfb5dee97
Matthew Pickering <matthewtpickering@gmail.com>
2021-09-13 12:02:14 +0100

    Stop leaking <defunct> llc processes
    We needed to wait for the process to exit in the clean-up script as
    otherwise the `llc` process will not be killed until compilation
    finishes. This leads to running out of process spaces on some OSs.
    
    Thanks to Edsko de Vries for suggesting this fix.
    
    Fixes #20305
    
    (cherry picked from commit 58421c8e27be6774dbb6dd37c93e1cb906d58dcc)

--- compiler/GHC/SysTools/Tasks.hs
+++ compiler/GHC/SysTools/Tasks.hs
@@ -211,7 +211,7 @@ figureLlvmVersion logger dflags = traceToolCommand logger dflags "llc" $ do
       -- options are specified when '-version' is used.
       args' = args ++ ["-version"]
   catchIO (do
-              (pin, pout, perr, _) <- runInteractiveProcess pgm args'
+              (pin, pout, perr, p) <- runInteractiveProcess pgm args'
                                               Nothing Nothing
               {- > llc -version
                   LLVM (http://llvm.org/):
@@ -225,6 +225,7 @@ figureLlvmVersion logger dflags = traceToolCommand logger dflags "llc" $ do
               hClose pin
               hClose pout
               hClose perr
+              _ <- waitForProcess p
               return mb_ver
             )
             (\err -> do
