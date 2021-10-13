c07cc286a23dd9bc8a8509915ed7beecd5e3e7bc
Ben Gamari <ben@smart-cactus.org>
2021-10-13 11:11:06 -0400

    Increase size of T19703

--- testsuite/tests/perf/compiler/T19703/genT19703.py
+++ testsuite/tests/perf/compiler/T19703/genT19703.py
@@ -13,15 +13,13 @@ def gen_fam():
         type family Fam a
     '''))
 
-def gen_join():
+def gen_join(n: int):
     write_file('T19703.hs', dedent('''
         {-# LANGUAGE TypeFamilies #-}
         module Join where
-        import A
-        import B
-    '''))
+    ''') + "\n".join(f"import M{n}" for n in range(n)))
 
-def gen_instances(mod_name: str, n: int):
+def gen_instances(mod_name: str, n_instances: int):
     out = dedent('''
             {{-# LANGUAGE TypeFamilies #-}}
             module {mod_name} where
@@ -32,10 +30,12 @@ def gen_instances(mod_name: str, n: int):
             data T{i} = T{i}
             type instance Fam T{i} = Int
             ''').format(i=str(i))
-            for i in range(n))
+            for i in range(n_instances))
     write_file('{}.hs'.format(mod_name), out)
 
 gen_fam()
-gen_instances('A', n=5000)
-gen_instances('B', n=10000)
-gen_join()
+n = 1000
+for i in range(n):
+    gen_instances(f'M{i}', n_instances=1)
+
+gen_join(n)
