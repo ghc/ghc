e449e5dd6c8bcc781a76fb992f9f3c6df39d8e39
Simon Peyton Jones <simonpj@microsoft.com>
2021-10-27 14:19:59 +0100

    Remove record field from Solo
    Ticket #20562 revealed that Solo, which is a wired-in TyCon, had
    a record field that wasn't being added to the type env. Why not?
    Because wired-in TyCons don't have record fields.
    
    It's not hard to change that, but it's tiresome for this one use-case,
    and it seems easier simply to make `getSolo` into a standalone
    function.
    
    On the way I refactored the handling of Solo slightly, to put it
    into wiredInTyCons (where it belongs) rather than only in
    knownKeyNames
    
    (cherry picked from commit 6b447da26c30889b92c6a06e93f26d8e96b45323)

--- libraries/base/tests/read-show-solo.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-import Data.Tuple (Solo (..))
-
-main = do
-  print $ Solo (3 :: Int)
-  print $ Solo (Just "")
-  print $ Just (Solo "")
-  print (read (show (Solo (3 :: Int))) :: Solo Int)
-  print (read "Just Solo { getSolo = 5 }" :: Maybe (Solo Int))
