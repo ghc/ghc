4f4e8a6044ebd052e3500530a657bd203c677830
John Ericson <John.Ericson@Obsidian.Systems>
2021-11-12 02:00:43 +0000

    Delete dead code knobs for building GHC itself
    As GHC has become target agnostic, we've left behind some now-useless
    logic in both build systems.

--- ghc/ghc.mk
+++ ghc/ghc.mk
@@ -38,9 +38,6 @@ ghc_stage1_UseGhcForCC = YES
 ghc_stage2_UseGhcForCC = YES
 ghc_stage3_UseGhcForCC = YES
 
-ghc_stage2_MKDEPENDC_OPTS = -DMAKING_GHC_BUILD_SYSTEM_DEPENDENCIES
-ghc_stage3_MKDEPENDC_OPTS = -DMAKING_GHC_BUILD_SYSTEM_DEPENDENCIES
-
 ifeq "$(GhcDebugged)" "YES"
 ghc_stage1_MORE_HC_OPTS += -debug
 ghc_stage2_MORE_HC_OPTS += -debug
