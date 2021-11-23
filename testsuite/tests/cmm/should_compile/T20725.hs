cc2bf8e9aa7237aa5d9c0bf6e839136774bd51bd
Norman Ramsey <nr@cs.tufts.edu>
2021-11-23 14:45:52 -0500

    generalize GHC.Cmm.Dataflow to work over any node type
    See #20725.
    
    The commit includes source-code changes and a test case.

---
+++ testsuite/tests/cmm/should_compile/T20725.hs
@@ -0,0 +1,25 @@
+module T20725 where
+
+import GHC.Cmm
+import GHC.Cmm.Dataflow
+import GHC.Cmm.Dataflow.Block
+import GHC.Cmm.Dataflow.Collections
+import GHC.Cmm.Dataflow.Graph
+import GHC.Cmm.Dataflow.Label
+
+data TrivialFact = TrivialFact
+
+trivialLattice :: DataflowLattice TrivialFact
+trivialLattice = DataflowLattice TrivialFact (\_ _ -> NotChanged TrivialFact)
+
+-- | ensures that analysis is possible independent of node type
+trivialMap :: forall node .
+              (NonLocal node)
+           => GenCmmGraph node
+           -> LabelMap TrivialFact
+trivialMap g =
+  analyzeCmmFwd trivialLattice transfer g startFacts
+      where startFacts = mkFactBase trivialLattice []
+            transfer block facts =
+                asBase [(successor, TrivialFact) | successor <- successors block]
+                    where asBase = mkFactBase trivialLattice
