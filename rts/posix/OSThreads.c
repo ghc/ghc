500d6c0d67576e5787fb1d531ba2d2acc5714679
Gleb Popov <6yearold@gmail.com>
2021-12-26 21:37:31 +0300

    Fix build on recent FreeBSD.
    Recent FreeBSD versions gained the sched_getaffinity function, which made two
    mutually exclusive #ifdef blocks to be enabled.
    
    (cherry picked from commit 29c8c561a39dac433da72091157dc035ccaa58f3)

--- rts/posix/OSThreads.c
+++ rts/posix/OSThreads.c
@@ -325,7 +325,7 @@ getNumberOfProcessors (void)
                     nproc = 1;
             }
         }
-#elif defined(freebsd_HOST_OS)
+#elif defined(freebsd_HOST_OS) && !defined(HAVE_SCHED_GETAFFINITY)
         cpuset_t mask;
         CPU_ZERO(&mask);
         if(cpuset_getaffinity(CPU_LEVEL_CPUSET, CPU_WHICH_PID, -1, sizeof(mask), &mask) == 0) {
