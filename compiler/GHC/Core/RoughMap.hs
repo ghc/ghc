78cb8adb2d4b577ea4e077527bc0e05e7117a72a
Matthew Pickering <matthewtpickering@gmail.com>
2022-01-14 15:14:07 +0000

    Comment

--- compiler/GHC/Core/RoughMap.hs
+++ compiler/GHC/Core/RoughMap.hs
@@ -49,6 +49,71 @@ import Data.Bifunctor
 Note [Rough maps of Types]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+Note [Matching a RoughMap]
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The lookup key into a rough map is slightly different to the insertion key.
+Like the insertion key each argument is classified to a simpler key which describes
+what could match that position. There are three possibilities:
+
+* LookupKnownTc Name: The argument is headed by a known type constructor.
+* NoKnownTc: The argument is definitely not headed by any known type constructor.
+* LookupOtherTc: The argument could match anything, we don't know enough about it.
+
+The interesting case is the second case as that's different to the insertion key
+for a rough map. The second case arises in two situations:
+
+1. The head of the application is a skolem type variable. The type variable definitely
+   doesn't match with any of the KnownTC instances so we can discard them all. For example:
+    Show a[sk] or Show (a[sk] b[sk]). One place constraints like this arise is when
+    typechecking derived instances.
+2. The head of the application is a known type family whose arguments contain no
+   meta variables. For example: F a[sk]. The application of F is stuck, and because
+   F is a type family it won't match any KnownTC instance so it's safe to discard
+   all these instances.
+
+
+The lookup key into a rough map is slightly different to the insertion key.
+Like the insertion key each argument is classified to a simpler key which describes
+what could match that position. There are three possibilities:
+
+* LookupKnownTc Name: The argument is headed by a known type constructor.
+* NoKnownTc: The argument is definitely not headed by any known type constructor.
+* LookupOtherTc: The argument could match anything, we don't know enough about it.
+
+The interesting case is the second case as that's different to the insertion key
+for a rough map. The second case arises in two situations:
+
+1. The head of the application is a skolem type variable. The type variable definitely
+   doesn't match with any of the KnownTC instances so we can discard them all. For example:
+    Show a[sk] or Show (a[sk] b[sk]). One place constraints like this arise is when
+    typechecking derived instances.
+2. The head of the application is a known type family whose arguments contain no
+   meta variables. For example: F a[sk]. The application of F is stuck, and because
+   F is a type family it won't match any KnownTC instance so it's safe to discard
+   all these instances.
+
+Of course, these two cases can still match instances of the form `forall a . Show a =>`,
+and those instances are retained as they are not classified as KnownTC instances.
+
+
+Note [Matches vs Unifiers]
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+The lookupRM' function returns a pair of potential matches and potential unifiers.
+The potential matches is likely to be much smaller than the bag of potential unifiers due
+to the reasoning about rigid type variables described in Note [Matching a RoughMap].
+On the other hand, the instances captured by the NoKnownTC case can still potentially unify
+with any instance (depending on the substituion of said rigid variable) so they can't be discounted
+from the list of potential unifiers. This is achieved by the NoKnownTC case continuing
+the lookup for unifiers by replacing NoKnownTC with LookupOtherTC.
+
+In addition to this, we usually only care whether there are zero, one or two potential
+unifiers, even if we have many candidates, the search can stop before consulting
+each candidate. We only need the full list of unifiers when displaying error messages.
+Therefore the bag is computed lazily so much work can be avoided constructing the
+bag in the first place.
+
 -}
 
 -- Key for insertion into a RoughMap
@@ -60,6 +125,7 @@ data RoughMatchTc
   deriving( Data )
 
 -- Key for lookup into a RoughMap
+-- See Note [Matching a RoughMap]
 data RoughMatchLookupTc
   = LookupKnownTc Name -- ^ The position only matches the specified KnownTc
   | NoKnownTc -- ^ The position definitely doesn't match any KnownTc
@@ -86,15 +152,18 @@ isRoughOtherTc (KnownTc {}) = False
 typeToRoughMatchLookupTc :: Type -> RoughMatchLookupTc
 typeToRoughMatchLookupTc ty
   | Just (ty', _) <- splitCastTy_maybe ty   = typeToRoughMatchLookupTc ty'
-  | (TyVarTy tv, _) <- splitAppTys ty, isSkolemTyVar tv = NoKnownTc
-  | Just (tc,_)   <- splitTyConApp_maybe ty
-  , not (isTypeFamilyTyCon tc)              = -- assertPpr (isGenerativeTyCon tc Nominal) (ppr tc)
-                                              LookupKnownTc $! tyConName tc
-  | Just (tc, tys)   <- splitTyConApp_maybe ty
-  , isTypeFamilyTyCon tc
-  , not (anyVarSet isMetaTyVar (tyCoVarsOfTypes tys))
-    = NoKnownTc
-  | otherwise = LookupOtherTc
+  | otherwise =
+      case splitAppTys ty of
+        -- Case 1: Head of application is a SkolemTV, does not match any KnownTc.
+        (TyVarTy tv, _) | isSkolemTyVar tv -> NoKnownTc
+        (TyConApp tc [], tys)
+          -- Case 2: Head of application is a known type constructor, hence KnownTc.
+          | not (isTypeFamilyTyCon tc) -> LookupKnownTc $! tyConName tc
+          -- Case 3: Head is a type family, but it's applied to skolem types so
+          --         it's stuck and can't match any KnownTC instances.
+          | not (anyVarSet isMetaTyVar (tyCoVarsOfTypes tys)) -> NoKnownTc
+        -- Fallthrough: Otherwise, anything might match this position
+        _ -> LookupOtherTc
 
 typeToRoughMatchTc :: Type -> RoughMatchTc
 typeToRoughMatchTc ty
@@ -132,6 +201,9 @@ lookupRM tcs rm = bagToList (fst $ lookupRM' tcs rm)
 -- | N.B. Returns a 'Bag', which allows us to avoid rebuilding all of the lists
 -- we find in 'rm_empty', which would otherwise be necessary due to '++' if we
 -- returned a list.
+--
+-- See Note [Matching a RoughMap]
+-- See Note [Matces vs Unifiers]
 lookupRM' :: [RoughMatchLookupTc] -> RoughMap a -> (Bag a -- Potential matches
                                                    , Bag a) -- Potential unifiers
 lookupRM' _                  RMEmpty = (emptyBag, emptyBag)
@@ -140,7 +212,8 @@ lookupRM' []                 rm      = let m = listToBag $ elemsRM rm
 lookupRM' (LookupKnownTc tc : tcs) rm  =
   let (common_m, common_u) = lookupRM' tcs (rm_unknown rm)
       (m, u) = maybe (emptyBag, emptyBag) (lookupRM' tcs) (lookupDNameEnv (rm_known rm) tc)
-  in (rm_empty rm `unionBags` common_m `unionBags` m , rm_empty rm `unionBags` common_u `unionBags` u)
+  in (rm_empty rm `unionBags` common_m `unionBags` m
+     , rm_empty rm `unionBags` common_u `unionBags` u)
 -- A SkolemTC does **not** match any KnownTC but can unify
 lookupRM' (NoKnownTc : tcs)  rm      =
 
@@ -154,12 +227,6 @@ lookupRM' (LookupOtherTc : tcs)    rm  =
       (u_m, u_u) = lookupRM' tcs (rm_unknown rm)
   in (rm_empty rm `unionBags` u_m `unionBags` m
      , rm_empty rm `unionBags` u_u `unionBags` u)
-
-{-
-  let m = foldl' unionBags emptyBag
-           [
-           , rm_empty rm ] in (m, m)
-           -}
 
 unionRM :: RoughMap a -> RoughMap a -> RoughMap a
 unionRM RMEmpty a = a
