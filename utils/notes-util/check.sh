2320ff67febd38d06cc8b6575817738baf40b4c3
Ben Gamari <ben@smart-cactus.org>
2022-01-30 08:16:32 -0500

    notes-util: initial commit

---
+++ utils/notes-util/check.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+CABAL_INSTALL="${CABAL_INSTALL:-cabal}"
+
+cd "$(dirname $0)"
+"$CABAL_INSTALL" build
+bin="$("$CABAL_INSTALL" list-bin ghc-notes)"
+cd "$(git rev-parse --show-toplevel)"
+"$bin" broken-refs \
+    | grep -v "utils/notes-util/expected-broken-note-refs:" \
+    | sed 's/:[0-9]\+:[0-9]\+:/:/' \
+    > broken-note-refs
+
+if diff -q utils/notes-util/expected-broken-note-refs broken-note-refs; then
+    printf "No unexpected broken note references"
+else
+    printf "Found unexpected broken note references:\n\n"
+    diff -u utils/notes-util/expected-broken-note-refs broken-note-refs || true
+    if [[ "$1" == "-a" ]]; then
+        cp broken-note-refs utils/notes-util/expected-broken-note-refs
+        printf "\n"
+        printf "Accepted new broken note references."
+    else
+        exit 1
+    fi
+fi
+
