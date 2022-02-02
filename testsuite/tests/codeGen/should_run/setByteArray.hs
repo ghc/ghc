8c18feba88aaa20b75b82c3fee7e8f742299461e
Ben Gamari <ben@smart-cactus.org>
2022-02-02 14:43:53 -0500

    primops: Fix documentation of setByteArray#
    Previously the documentation was subtly incorrect regarding the bounds
    of the operation. Fix this and add a test asserting that a zero-length
    operation is in fact a no-op.

--- testsuite/tests/codeGen/should_run/setByteArray.hs
+++ testsuite/tests/codeGen/should_run/setByteArray.hs
@@ -12,12 +12,15 @@ main :: IO ()
 main = do BA ba <- IO $ \s0 ->
                         case newByteArray# 8# s0 of
                         (# !s1, !mba #) ->
-                            case setByteArray# mba 0# 8# 65# s1 of
+                            case setByteArray# mba 0# 8# 65# s1 of -- ASCII 'A'
                             !s2 ->
-                                case setByteArray# mba 1# 6# 67# s2 of
+                                case setByteArray# mba 1# 6# 67# s2 of -- ASCII 'B'
                                 !s3 ->
-                                    case unsafeFreezeByteArray# mba s3 of
-                                    (# s4, ba #) -> (# s4, BA ba #)
+                                    -- N.B. 0-length should be a no-op
+                                    case setByteArray# mba 2# 0# 68# s3 of -- ASCII 'C'
+                                    !s4 ->
+                                        case unsafeFreezeByteArray# mba s4 of
+                                        (# s5, ba #) -> (# s5, BA ba #)
           let f (I# i) = putStrLn [C# (indexCharArray# ba i)]
           mapM_ f [0..7]
 
