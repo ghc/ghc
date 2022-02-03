5c31cd4ce13a980320fc44fd62c6984c7ed84ed2
Ben Gamari <ben@smart-cactus.org>
2022-02-03 10:06:35 -0500

    rts: Dump memory map on memory mapping failures
    Fixes #20992.
    
    (cherry picked from commit fc083b480adedf26d47f880402f111680ec34183)

---
+++ rts/MemoryMap.h
@@ -0,0 +1,13 @@
+/* -----------------------------------------------------------------------------
+ *
+ * (c) The GHC Team, 1998-2004
+ *
+ * Memory-map dumping.
+ *
+ * This is intended to be used for reporting the process memory-map
+ * in diagnostics when the RTS fails to map a block of memory.
+ *
+ * ---------------------------------------------------------------------------*/
+
+void reportMemoryMap(void);
+
