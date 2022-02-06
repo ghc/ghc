66eec633677cbedeab6b334d2ced29abe2c11bb6
Ben Gamari <ben@smart-cactus.org>
2022-02-05 23:12:07 -0500

    rts/linker: Catch archives masquerading as object files
    Check the file's header to catch static archive bearing the `.o`
    extension, as may happen on Windows after the Clang refactoring.
    
    See #21068

--- rts/Linker.c
+++ rts/Linker.c
@@ -1356,7 +1356,7 @@ preloadObjectFile (pathchar *path)
 
    image = stgMallocBytes(fileSize, "loadObj(image)");
 
-#endif
+#endif /* !defined(darwin_HOST_OS) */
 
    int n;
    n = fread ( image, 1, fileSize, f );
@@ -1399,6 +1399,15 @@ static HsInt loadObj_ (pathchar *path)
        IF_DEBUG(linker,
                 debugBelch("ignoring repeated load of %" PATH_FMT "\n", path));
        return 1; // success
+   }
+
+   if (isArchive(path)) {
+       if (loadArchive_(path)) {
+            return 1; // success
+       } else {
+            IF_DEBUG(linker,
+                        debugBelch("tried and failed to load %" PATH_FMT " as an archive\n", path));
+       }
    }
 
    ObjectCode *oc = preloadObjectFile(path);
