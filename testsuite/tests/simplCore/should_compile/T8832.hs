677c6c913a993a5298dcf19020641f24cede0239
Sylvain Henry <sylvain@haskus.fr>
2022-02-24 16:24:31 +0100

    Testsuite: remove arch conditional in T8832
    Taken from !3658

--- testsuite/tests/simplCore/should_compile/T8832.hs
+++ testsuite/tests/simplCore/should_compile/T8832.hs
@@ -1,8 +1,5 @@
 {-# LANGUAGE CPP #-}
 
--- I'm concerned that the -ddump-simpl output may differ on 32 and 64-bit
--- platforms.  So far I've only put in output for 64-bit platforms.
-
 module T8832 where
 
 import Data.Bits
@@ -17,16 +14,12 @@ T(i,Int)
 T(i8,Int8)
 T(i16,Int16)
 T(i32,Int32)
-#if defined(T8832_WORDSIZE_64)
 T(i64,Int64)
-#endif
 
 T(w,Word)
 T(w8,Word8)
 T(w16,Word16)
 T(w32,Word32)
-#if defined(T8832_WORDSIZE_64)
 T(w64,Word64)
-#endif
 
 T(z,Integer)
