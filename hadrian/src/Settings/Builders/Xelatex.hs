5932247cda7a782a3d1325b9715095a7d25e8571
Ben Gamari <ben@smart-cactus.org>
2022-03-08 13:06:47 -0500

    users guide: Eliminate spurious \spxentry mentions
    We were failing to pass the style file to `makeindex`, as is done by
    the mklatex configuration generated by Sphinx.
    
    Fixes #20913.

--- hadrian/src/Settings/Builders/Xelatex.hs
+++ hadrian/src/Settings/Builders/Xelatex.hs
@@ -3,5 +3,11 @@ module Settings.Builders.Xelatex (xelatexBuilderArgs) where
 import Settings.Builders.Common
 
 xelatexBuilderArgs :: Args
-xelatexBuilderArgs = builder Xelatex ? mconcat [ arg "-halt-on-error"
-                                               , arg =<< getInput ]
+xelatexBuilderArgs = mconcat
+    [ builder Xelatex ? mconcat [ arg "-halt-on-error"
+                                , arg =<< getInput ]
+
+      -- this fixes #20913 although is a bit of a hack given that this is
+      -- specific to the user-guide.
+    , builder Makeindex ? mconcat [ arg "-s", arg "python.ist" ]
+    ]
