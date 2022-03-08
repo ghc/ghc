8fafacf7bae432fe20a46265af497b8fb42cbd1c
Philip Hazelden <philip.hazelden@gmail.com>
2022-03-08 21:09:23 +0000

    Add tests for several trace functions.

--- testsuite/tests/rts/traceEvent.hs
+++ testsuite/tests/rts/traceEvent.hs
@@ -5,6 +5,8 @@ main = do
   traceEventIO "testing"
   traceEventIO "%s" -- see #3874
   traceEventIO $ replicate (maxSize + 1) 'A'
+  putStrLn $ traceEvent "one" "two"
+  putStrLn $ traceEventWith (show . length) "three"
 
 maxSize :: Int
 maxSize = fromIntegral (maxBound :: Word16)
