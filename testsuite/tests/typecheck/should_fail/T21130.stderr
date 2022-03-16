ghc/ghc

commit 8bb52d9186655134e3e06b4dc003e060379f5417

Remove flattening variables
This patch redesigns the flattener to simplify type family applications
directly instead of using flattening meta-variables and skolems. The key new
innovation is the CanEqLHS type and the new CEqCan constraint (Ct). A CanEqLHS
is either a type variable or exactly-saturated type family application; either
can now be rewritten using a CEqCan constraint in the inert set.

Because the flattener no longer reduces all type family applications to
variables, there was some performance degradation if a lengthy type family
application is now flattened over and over (not making progress). To
compensate, this patch contains some extra optimizations in the flattener,
leading to a number of performance improvements.

Close #18875.
Close #18910.

There are many extra parts of the compiler that had to be affected in writing
this patch:

* The family-application cache (formerly the flat-cache) sometimes stores
  coercions built from Given inerts. When these inerts get kicked out, we must
  kick out from the cache as well. (This was, I believe, true previously, but
  somehow never caused trouble.) Kicking out from the cache requires adding a
  filterTM function to TrieMap.

* This patch obviates the need to distinguish "blocking" coercion holes from
  non-blocking ones (which, previously, arose from CFunEqCans). There is thus
  some simplification around coercion holes.

* Extra commentary throughout parts of the code I read through, to preserve
  the knowledge I gained while working.

* A change in the pure unifier around unifying skolems with other types.
  Unifying a skolem now leads to SurelyApart, not MaybeApart, as documented
  in Note [Binding when looking up instances] in GHC.Core.InstEnv.

* Some more use of MCoercion where appropriate.

* Previously, class-instance lookup automatically noticed that e.g. C Int was
  a "unifier" to a target [W] C (F Bool), because the F Bool was flattened to

testsuite/tests/typecheck/should_fail/all.T
@@ -590,3 +590,4 @@ test('T18640b', normal, compile_fail, [''])
 test('T18640c', normal, compile_fail, [''])
 test('T10709', normal, compile_fail, [''])
 test('T10709b', normal, compile_fail, [''])
+test('GivenForallLoop', normal, compile_fail, [''])

testsuite/tests/indexed-types/should_compile/all.T
@@ -297,3 +297,6 @@ test('T17405', normal, multimod_compile, ['T17405c', '-v0'])
 test('T17923', normal, compile, [''])
 test('T18065', normal, compile, ['-O'])
 test('T18809', normal, compile, ['-O'])
+test('CEqCanOccursCheck', normal, compile, [''])
+test('GivenLoop', normal, compile, [''])
+test('T18875', normal, compile, [''])

testsuite/tests/indexed-types/should_fail/all.T
@@ -163,3 +163,4 @@ test('T17008a', normal, compile_fail, ['-fprint-explicit-kinds'])
 test('T13571', normal, compile_fail, [''])
 test('T13571a', normal, compile_fail, [''])
 test('T18648', normal, compile_fail, [''])
+test('ExpandTFs', normal, compile_fail, [''])

testsuite/tests/typecheck/should_compile/all.T
@@ -731,3 +731,8 @@ test('T18939_Compile', normal, compile, [''])
 test('T15942', normal, compile, [''])
 test('ClassDefaultInHsBoot', [extra_files(['ClassDefaultInHsBootA1.hs','ClassDefaultInHsBootA2.hs','ClassDefaultInHsBootA2.hs-boot','ClassDefaultInHsBootA3.hs'])], multimod_compile, ['ClassDefaultInHsBoot', '-v0'])
 test('T17186', normal, compile, [''])
+test('CbvOverlap', normal, compile, [''])
+test('InstanceGivenOverlap', normal, compile, [''])
+test('InstanceGivenOverlap2', normal, compile, [''])
+test('LocalGivenEqs', normal, compile, [''])
+test('LocalGivenEqs2', normal, compile, [''])

commit bb779b90bb093274ccf7a8e5b19f6661f4925bde

Add a regression test for #21130
This problem was due to a bug in cloneWanted, which was incorrectly
creating a coercion hole to hold an evidence variable.

This bug was introduced by 8bb52d91 and fixed in 81740ce8.

Fixes #21130

testsuite/tests/typecheck/should_fail/T21130.hs
@@ -1,0 +1,10 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+module T21130 where
+
+type family Item l
+type instance Item [a] = a
+
+f :: Enum (Item l) => l
+f = f
+
+x = (_ f) :: Int

testsuite/tests/typecheck/should_fail/T21130.stderr
@@ -1,0 +1,34 @@
+
+T21130.hs:10:6:
+     Found hole: _ :: t0 -> Int
+      Where: ‘t0’ is an ambiguous type variable
+     In the expression: (_ f) :: Int
+      In an equation for ‘x’: x = (_ f) :: Int
+     Relevant bindings include x :: Int (bound at T21130.hs:10:1)
+       Valid hole fits include
+         head :: forall a. GHC.Stack.Types.HasCallStack => [a] -> a
+           with head @Int
+          (imported from ‘Prelude’ at T21130.hs:2:8-13
+           (and originally defined in ‘GHC.List’))
+         last :: forall a. GHC.Stack.Types.HasCallStack => [a] -> a
+           with last @Int
+          (imported from ‘Prelude’ at T21130.hs:2:8-13
+           (and originally defined in ‘GHC.List’))
+         read :: forall a. Read a => String -> a
+           with read @Int
+          (imported from ‘Prelude’ at T21130.hs:2:8-13
+           (and originally defined in ‘Text.Read’))
+
+ T21130.hs:10:8:
+     Ambiguous type variable ‘t0’ arising from a use of ‘f’
+      prevents the constraint ‘(Enum (Item t0))’ from being solved.
+      Probable fix: use a type annotation to specify what ‘t0’ should be.
+       Potentially matching instances:
+        instance Enum Ordering -- Defined in ‘GHC.Enum’
+        instance Enum Integer -- Defined in ‘GHC.Enum’
+         ...plus N others
+         ...plus N instances involving out-of-scope types
+         (use -fprint-potential-instances to see them all)
+     In the first argument of ‘_’, namely ‘f’
+       In the expression: (_ f) :: Int
+      In an equation for ‘x’: x = (_ f) :: Int

testsuite/tests/typecheck/should_fail/all.T
@@ -650,3 +650,4 @@ test('T18398', normal, compile_fail, [''])
 test('T18406', normal, compile_fail, [''])
 test('AmbigFDs', normal, compile_fail, [''])
 test('T20064', normal, compile_fail, [''])
+test('T21130', normal, compile_fail, [''])