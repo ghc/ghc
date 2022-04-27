c55a0a7a2243af959fc9702a025c608332348dfe
Sylvain Henry <sylvain@haskus.fr>
2022-04-27 13:10:15 +0200

    Hadrian: disable -fllvm

--- hadrian/src/Settings/Flavours/QuickCross.hs
+++ hadrian/src/Settings/Flavours/QuickCross.hs
@@ -10,11 +10,23 @@ import {-# SOURCE #-} Settings.Default
 quickJsFlavour :: Flavour
 quickJsFlavour = defaultFlavour
     { name        = "quick-js"
-    , args        = defaultBuilderArgs <> quickCrossArgs <> defaultPackageArgs
+    , args        = defaultBuilderArgs <> quickJsArgs <> defaultPackageArgs
     , dynamicGhcPrograms = pure False
     , libraryWays = pure [vanilla]
     , rtsWays     = pure [vanilla]
                     }
+
+-- Same as quickCrossArgs (until it bitrots) but don't enable -fllvm
+quickJsArgs :: Args
+quickJsArgs = sourceArgs SourceArgs
+    { hsDefault  = mconcat $
+        [ pure ["-O0", "-H64m"]
+        ]
+    , hsLibrary  = notStage0 ? mconcat [ arg "-O" ]
+    , hsCompiler = stage0 ? arg "-O2"
+    , hsGhc      = mconcat
+                   [ stage0 ? arg "-O"
+                   , stage1 ? mconcat [ arg "-O0" ] ] }
 
 -- Please update doc/flavours.md when changing this file.
 quickCrossFlavour :: Flavour
