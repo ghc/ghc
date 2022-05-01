2c21b52cf22d4f8d78319811c217056d4cd29808
Ben Gamari <ben@smart-cactus.org>
2022-04-30 20:50:55 -0400

    Handle full SQ/SQE

--- libraries/base/GHC/IO/URing.hs
+++ libraries/base/GHC/IO/URing.hs
@@ -32,20 +32,32 @@ data PendingReq = PendingReq !SqeIndex !Completion
 data URingMgr = URingMgr { uring :: !URing.URing
                          , uniqueSource :: !UniqueSource
                          , requests :: !(IT.IntTable PendingReq)
+                         , freeSqSlot :: !(MVar ())
                          }
+
+-- | When a thread tries to submit a request but finds that there are no SQs/SQEs 
+-- if blocks on this MVar, which gets filled when a pending request completes.
+--
+-- Note that this is technically a deadlock hazard. For instance, if a program
+-- has two threads which respectively read and write to a pipe, one thread may
+-- attempt to read, filling the SQ, while the other the other thread attempts
+-- to write, which blocks.
+type FreeSqEvent = MVar ()
 
 mAX_REQS :: Int
 mAX_REQS = 256
 
 newURingMgr :: IO URingMgr
 newURingMgr = do
-    _uring <- URing.newURing mAX_REQS
+    ring <- URing.newURing mAX_REQS
     usrc <- newSource
     reqs <- IT.new mAX_REQS
-    _ <- forkIO (startCompletionThread _uring reqs)
-    return URingMgr { uring = _uring
+    free_sq <- newEmptyMVar
+    _ <- forkIO (startCompletionThread ring reqs free_sq)
+    return URingMgr { uring = ring
                     , uniqueSource = usrc
                     , requests = reqs
+                    , freeSqSlot = free_sq
                     }
 
 submit' :: URingMgr
@@ -56,20 +68,27 @@ submit' :: URingMgr
         -> IO a
 submit' mgr mkSqe compl = do
     Unique u <- newUnique (uniqueSource mgr)
-    let err = error "repeated IO request unique"
-    sqeIdx_mb <- URing.Ring.getSqe (uring mgr)
-    case sqeIdx_mb of
-      Just sqeIdx -> do
-        _ <- IT.insertWith err (fromIntegral u) (PendingReq sqeIdx compl) (requests mgr)
-        let sqe = mkSqe (fromIntegral u)
-        r <- URing.Sqe.pokeSqe sqe (URing.Ring.sqePtr (uring mgr) sqeIdx)
-        pushRes <- URing.Ring.pushSqe (uring mgr) sqeIdx
-        if pushRes
-          then do
-              _ <- URing.submit (uring mgr) 1
-              return r
-          else URing.freeSqe (uring mgr) sqeIdx >> error "SQ full" -- TODO
-      Nothing -> error "failed to get sqe" -- TODO
+    go u
+  where
+    dup_uniq_err = error "repeated IO request unique"
+
+    go u = do
+        sqeIdx_mb <- URing.Ring.getSqe (uring mgr)
+        case sqeIdx_mb of
+          Just sqeIdx -> do
+            _ <- IT.insertWith dup_uniq_err (fromIntegral u) (PendingReq sqeIdx compl) (requests mgr)
+            let sqe = mkSqe (fromIntegral u)
+            r <- URing.Sqe.pokeSqe sqe (URing.Ring.sqePtr (uring mgr) sqeIdx)
+            pushRes <- URing.Ring.pushSqe (uring mgr) sqeIdx
+            if pushRes
+              then do
+                  _ <- URing.submit (uring mgr) 1
+                  return r
+              else URing.freeSqe (uring mgr) sqeIdx >> no_sqs u
+          Nothing -> no_sqs u
+
+    -- Retry when there are no SQ or SQE slots...
+    no_sqs u = takeMVar (freeSqSlot mgr) >> go u
 
 submit :: (UserData -> SqeBuilder a)
        -> Completion
@@ -91,19 +110,21 @@ globalURingMgr = unsafePerformIO $ do
     mgr <- newURingMgr
     newMVar mgr
 
-startCompletionThread :: URing.URing
-                      -> IT.IntTable PendingReq
-                      -> IO ()
-startCompletionThread uring requests = go
+startCompletionThread
+    :: URing.URing
+    -> IT.IntTable PendingReq
+    -> FreeSqEvent
+    -> IO ()
+startCompletionThread ring requests free_sq = go
   where
     go = once >> go
 
     once :: IO ()
     once = do
-        mb_cqe <- URing.popCq uring
+        mb_cqe <- URing.popCq ring
         case mb_cqe of
           Nothing -> do
-              _ <- URing.submitAndWait uring 0 1
+              _ <- URing.submitAndWait ring 0 1
               return ()
           Just cqe -> do
               let reqId :: Int
@@ -112,6 +133,7 @@ startCompletionThread uring requests = go
               case mb_req of
                 Nothing -> error "No request"
                 Just (PendingReq sqe_idx compl) -> do
-                    URing.freeSqe uring sqe_idx
+                    URing.freeSqe ring sqe_idx
+                    tryPutMVar free_sq () -- TODO: How inefficient is this?
                     compl (cqeRes cqe)
 
