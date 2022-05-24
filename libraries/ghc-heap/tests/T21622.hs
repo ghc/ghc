49837c7f0ccb2971823d297c0fa0aea0c47a0487
Ben Gamari <bgamari.foss@gmail.com>
2022-05-23 22:59:44 -0400

    ghc-heap: Add (broken) test for #21622

---
+++ libraries/ghc-heap/tests/T21622.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -O0 #-}
+{-# LANGUAGE MagicHash #-}
+
+import Control.Monad
+import GHC.Exts.Heap
+import System.Mem
+import GHC.Exts
+import GHC.Weak
+
+main :: IO ()
+main = do
+    let x = 42 :: Integer
+    Weak w <- mkWeak x () Nothing
+    weaks <- collectWeaks w
+    performMajorGC
+    print (x, fmap length weaks)
+
+collectWeaks :: Weak# v -> IO [Closure]
+collectWeaks = \w -> getClosureData w >>= go []
+  where
+    go :: [Closure] -> Closure -> IO [Closure]
+    go acc w@(WeakClosure {link})
+      | next <- link = getBoxedClosureData next >>= go (w:acc)
+      | otherwise = return (w:acc)
+
