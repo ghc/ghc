ghc/ghc

commit acb188e0c02a114927d340dac78a68626c659cd3

ghci: Fix rec statements in interactive prompt
We desugar a recursive Stmt to somethign like

  (a,_,c) <- mfix (\(a,b,_) -> do { ... ; return (a,b,c) })
  ...stuff after the rec...

The knot-tied tuple must contain
* All the variables that are used before they are bound in the `rec` block
* All the variables that are used after the entire `rec` block

In the case of GHCi, however, we don't know what variables will be used
after the `rec` (#20206).  For example, we might have

    ghci>  rec { x <- e1; y <- e2 }
    ghci>  print x
    ghci>  print y

So we have to assume that *all* the variables bound in the `rec` are used
afterwards.  We use `Nothing` in the argument to segmentRecStmts to signal
that all the variables are used.

Fixes #20206

compiler/GHC/Rename/Expr.hs
@@ -35,7 +35,7 @@ import GHC.Hs
 import GHC.Tc.Errors.Types
 import GHC.Tc.Utils.Env ( isBrackStage )
 import GHC.Tc.Utils.Monad
-import GHC.Unit.Module ( getModule )
+import GHC.Unit.Module ( getModule, isInteractiveModule )
 import GHC.Rename.Env
 import GHC.Rename.Fixity
 import GHC.Rename.Utils ( HsDocContext(..), bindLocalNamesFV, checkDupNames
@@ -1170,7 +1170,11 @@ rnStmt ctxt rnBody (L loc (RecStmt { recS_stmts = L _ rec_stmts })) thing_inside
                               segs
           -- See Note [Deterministic ApplicativeDo and RecursiveDo desugaring]
         ; (thing, fvs_later) <- thing_inside bndrs
-        ; let (rec_stmts', fvs) = segmentRecStmts (locA loc) ctxt empty_rec_stmt segs fvs_later
+        -- In interactive mode, assume that all variables are used later
+        ; is_interactive <- isInteractiveModule . tcg_mod <$> getGblEnv
+        ; let
+             final_fvs_later = if is_interactive then Nothing else Just fvs_later
+             (rec_stmts', fvs) = segmentRecStmts (locA loc) ctxt empty_rec_stmt segs final_fvs_later
         -- We aren't going to try to group RecStmts with
         -- ApplicativeDo, so attaching empty FVs is fine.
         ; return ( ((zip rec_stmts' (repeat emptyNameSet)), thing)
@@ -1522,15 +1526,17 @@ rn_rec_stmts ctxt rnBody bndrs stmts
 segmentRecStmts :: AnnoBody body
                 => SrcSpan -> HsStmtContext GhcRn
                 -> Stmt GhcRn (LocatedA (body GhcRn))
-                -> [Segment (LStmt GhcRn (LocatedA (body GhcRn)))] -> FreeVars
+                -> [Segment (LStmt GhcRn (LocatedA (body GhcRn)))]
+                -> Maybe FreeVars -- Nothing when in interactive mode, everything can be used later
+                                  -- Note [What is "used later" in a rec stmt]
                 -> ([LStmt GhcRn (LocatedA (body GhcRn))], FreeVars)
 
-segmentRecStmts loc ctxt empty_rec_stmt segs fvs_later
+segmentRecStmts loc ctxt empty_rec_stmt segs mfvs_later
   | null segs
-  = ([], fvs_later)
+  = ([], final_fv_uses)
 
   | HsDoStmt (MDoExpr _) <- ctxt
-  = segsToStmts empty_rec_stmt grouped_segs fvs_later
+  = segsToStmts empty_rec_stmt grouped_segs later_ids
                -- Step 4: Turn the segments into Stmts
                 --         Use RecStmt when and only when there are fwd refs
                 --         Also gather up the uses from the end towards the
@@ -1540,14 +1546,20 @@ segmentRecStmts loc ctxt empty_rec_stmt segs fvs_later
   | otherwise
   = ([ L (noAnnSrcSpan loc) $
        empty_rec_stmt { recS_stmts = noLocA ss
-                      , recS_later_ids = nameSetElemsStable
-                                           (defs `intersectNameSet` fvs_later)
+                      , recS_later_ids = nameSetElemsStable later_ids
                       , recS_rec_ids   = nameSetElemsStable
                                            (defs `intersectNameSet` uses) }]
           -- See Note [Deterministic ApplicativeDo and RecursiveDo desugaring]
-    , uses `plusFV` fvs_later)
+    , uses `plusFV` final_fv_uses)
 
   where
+    final_fv_uses = case mfvs_later of
+                  Nothing -> defs
+                  Just later -> uses `plusFV` later
+    later_ids = case mfvs_later of
+                  Nothing -> defs
+                  Just fvs_later -> defs `intersectNameSet` fvs_later
+
     (defs_s, uses_s, _, ss) = unzip4 segs
     defs = plusFVs defs_s
     uses = plusFVs uses_s
@@ -1622,6 +1634,27 @@ glom it together with the first two groups
      { rec { x <- ...y...; p <- z ; y <- ...x... ;
              q <- x ; z <- y } ;
        r <- x }
+
+Note [What is "used later" in a rec stmt]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+We desugar a recursive Stmt to somethign like
+
+  (a,_,c) <- mfix (\(a,b,_) -> do { ... ; return (a,b,c) })
+  ...stuff after the rec...
+
+The knot-tied tuple must contain
+* All the variables that are used before they are bound in the `rec` block
+* All the variables that are used after the entire `rec` block
+
+In the case of GHCi, however, we don't know what variables will be used
+after the `rec` (#20206).  For example, we might have
+    ghci>  rec { x <- e1; y <- e2 }
+    ghci>  print x
+    ghci>  print y
+
+So we have to assume that *all* the variables bound in the `rec` are used
+afterwards.  We use `Nothing` in the argument to segmentRecStmts to signal
+that all the variables are used.
 -}
 
 glomSegments :: HsStmtContext GhcRn

testsuite/tests/ghci/scripts/all.T
@@ -344,3 +344,4 @@ test('T19650',
      ['T19650.script'])
 test('T20019', normal, ghci_script, ['T20019.script'])
 test('T20101', normal, ghci_script, ['T20101.script'])
+test('T20206', normal, ghci_script, ['T20206.script'])

testsuite/tests/ghci/scripts/T20206.script
@@ -1,0 +1,3 @@
+:set -XRecursiveDo
+rec x <- pure ()
+print x

testsuite/tests/ghci/scripts/T20206.stdout
@@ -1,0 +1,1 @@
+()

commit b54f6c4fefaca8ca043cccbf474fb0da3d1c66b5

Fix FreeVars computation for mdo
Commit acb188e0 introduced a regression in the computation of free
variables in mdo statements, as the logic in
GHC.Rename.Expr.segmentRecStmts was slightly different depending on
whether the recursive do block corresponded to an mdo statement or
a rec statment.

This patch restores the previous computation for mdo blocks.

Fixes #21654

compiler/GHC/Rename/Expr.hs
@@ -1210,11 +1210,11 @@ rnStmt ctxt rnBody (L loc (RecStmt { recS_stmts = L _ rec_stmts })) thing_inside
                               segs
           -- See Note [Deterministic ApplicativeDo and RecursiveDo desugaring]
         ; (thing, fvs_later) <- thing_inside bndrs
-        -- In interactive mode, assume that all variables are used later
+        -- In interactive mode, all variables could be used later. So we pass whether
+        -- we are in GHCi along to segmentRecStmts. See Note [What is "used later" in a rec stmt]
         ; is_interactive <- isInteractiveModule . tcg_mod <$> getGblEnv
         ; let
-             final_fvs_later = if is_interactive then Nothing else Just fvs_later
-             (rec_stmts', fvs) = segmentRecStmts (locA loc) ctxt empty_rec_stmt segs final_fvs_later
+             (rec_stmts', fvs) = segmentRecStmts (locA loc) ctxt empty_rec_stmt segs (fvs_later, is_interactive)
         -- We aren't going to try to group RecStmts with
         -- ApplicativeDo, so attaching empty FVs is fine.
         ; return ( ((zip rec_stmts' (repeat emptyNameSet)), thing)
@@ -1567,16 +1567,19 @@ segmentRecStmts :: AnnoBody body
                 => SrcSpan -> HsStmtContext GhcRn
                 -> Stmt GhcRn (LocatedA (body GhcRn))
                 -> [Segment (LStmt GhcRn (LocatedA (body GhcRn)))]
-                -> Maybe FreeVars -- Nothing when in interactive mode, everything can be used later
-                                  -- Note [What is "used later" in a rec stmt]
+                -> (FreeVars, Bool)
+                    -- ^ The free variables used in later statements.
+                    -- If the boolean is 'True', this might be an underestimate
+                    -- because we are in GHCi, and might thus be missing some "used later"
+                    -- FVs. See Note [What is "used later" in a rec stmt]
                 -> ([LStmt GhcRn (LocatedA (body GhcRn))], FreeVars)
 
-segmentRecStmts loc ctxt empty_rec_stmt segs mfvs_later
+segmentRecStmts loc ctxt empty_rec_stmt segs (fvs_later, might_be_more_fvs_later)
   | null segs
   = ([], final_fv_uses)
 
   | HsDoStmt (MDoExpr _) <- ctxt
-  = segsToStmts empty_rec_stmt grouped_segs later_ids
+  = segsToStmts empty_rec_stmt grouped_segs fvs_later
                -- Step 4: Turn the segments into Stmts
                 --         Use RecStmt when and only when there are fwd refs
                 --         Also gather up the uses from the end towards the
@@ -1586,19 +1589,22 @@ segmentRecStmts loc ctxt empty_rec_stmt segs mfvs_later
   | otherwise
   = ([ L (noAnnSrcSpan loc) $
        empty_rec_stmt { recS_stmts = noLocA ss
-                      , recS_later_ids = nameSetElemsStable later_ids
+                      , recS_later_ids = nameSetElemsStable final_fvs_later
                       , recS_rec_ids   = nameSetElemsStable
                                            (defs `intersectNameSet` uses) }]
           -- See Note [Deterministic ApplicativeDo and RecursiveDo desugaring]
     , uses `plusFV` final_fv_uses)
 
   where
-    final_fv_uses = case mfvs_later of
-                  Nothing -> defs
-                  Just later -> uses `plusFV` later
-    later_ids = case mfvs_later of
-                  Nothing -> defs
-                  Just fvs_later -> defs `intersectNameSet` fvs_later
+    (final_fv_uses, final_fvs_later)
+      | might_be_more_fvs_later
+      = (defs, defs)
+        -- If there might be more uses later (e.g. we are in GHCi and have not
+        -- yet seen the whole rec statement), conservatively assume that everything
+        -- will be used later (as is possible).
+      | otherwise
+      = ( uses `plusFV` fvs_later
+        , defs `intersectNameSet` fvs_later )
 
     (defs_s, uses_s, _, ss) = unzip4 segs
     defs = plusFVs defs_s
@@ -1677,7 +1683,7 @@ glom it together with the first two groups
 
 Note [What is "used later" in a rec stmt]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-We desugar a recursive Stmt to somethign like
+We desugar a recursive Stmt to something like
 
   (a,_,c) <- mfix (\(a,b,_) -> do { ... ; return (a,b,c) })
   ...stuff after the rec...
@@ -1693,8 +1699,9 @@ after the `rec` (#20206).  For example, we might have
     ghci>  print y
 
 So we have to assume that *all* the variables bound in the `rec` are used
-afterwards.  We use `Nothing` in the argument to segmentRecStmts to signal
-that all the variables are used.
+afterwards.  We pass a Boolean to segmentRecStmts to signal such a situation,
+in which case that function conservatively assumes that everything might well
+be used later.
 -}
 
 glomSegments :: HsStmtContext GhcRn

testsuite/tests/rename/should_compile/T21654.hs
@@ -1,0 +1,17 @@
+{-# LANGUAGE RecursiveDo #-}
+{-# OPTIONS_GHC -Wunused-top-binds #-}
+
+module T21654 ( patternToQ ) where
+
+import Data.Functor.Identity
+
+mergeNullViews :: () -> ()
+mergeNullViews _ = ()
+
+patternToQ :: ()
+patternToQ = runIdentity $ combineSeq
+  where
+  combineSeq :: Identity ()
+  combineSeq = mdo  -- changing this to 'do' fixes the problem
+    q <- Identity ()
+    return $ mergeNullViews q

testsuite/tests/rename/should_compile/all.T
@@ -188,3 +188,4 @@ test('T20609d', normal, compile, [''])
 test('T18862', normal, compile, [''])
 test('unused_haddock', normal, compile, ['-haddock -Wall'])
 test('T19984', normal, compile, ['-fwarn-unticked-promoted-constructors'])
+test('T21654', normal, compile, ['-Wunused-top-binds'])