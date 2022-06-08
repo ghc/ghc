7bbd38cc6dcdc4c7964fc5738052d7cf1a1689fa
John Ericson <John.Ericson@Obsidian.Systems>
2022-06-08 03:40:24 +0000

    WIP Add test

---
+++ T21705/a/Foo.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE Trustworthy #-}
+module Foo where
+
+import Unsafe.Coerce (unsafeCoerce)
+
+x = unsafeCoerce
---
+++ T21705/c/Bar.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE Safe #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# OPTIONS_GHC -fpackage-trust #-}
+module Bar where
+
+import Foo
