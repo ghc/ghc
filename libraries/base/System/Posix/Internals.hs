92367ef781a856c840c99dad45f12c0b10ad6784
doyougnu <jeffrey.young@iohk.io>
2022-06-17 13:35:04 -0400

    Base: Remove binding forms in JS ffi

--- libraries/base/System/Posix/Internals.hs
+++ libraries/base/System/Posix/Internals.hs
@@ -450,7 +450,7 @@ foreign import javascript interruptible "h$base_dup2($1,$2,$c);"
     c_dup2 :: CInt -> CInt -> IO CInt
 foreign import javascript interruptible "h$base_fstat($1,$2_1,$2_2,$c);" -- fixme wrong type
     c_fstat :: CInt -> Ptr CStat -> IO CInt
-foreign import javascript unsafe "$r = h$base_isatty($1);"
+foreign import javascript unsafe "h$base_isatty($1);"
     c_isatty :: CInt -> IO CInt
 foreign import javascript interruptible "h$base_lseek($1,$2_1,$2_2,$3,$c);"
    c_lseek :: CInt -> COff -> CInt -> IO COff
@@ -468,7 +468,7 @@ foreign import javascript interruptible "h$base_read($1,$2_1,$2_2,$3,$c);"
    c_safe_read :: CInt -> Ptr Word8 -> CSize -> IO CSsize
 foreign import javascript interruptible "h$base_stat($1_1,$1_2,$2_1,$2_2,$c);" -- fixme wrong type
    c_stat :: CFilePath -> Ptr CStat -> IO CInt
-foreign import javascript unsafe "$r = h$base_umask($1);"
+foreign import javascript unsafe "h$base_umask($1);"
    c_umask :: CMode -> IO CMode
 foreign import javascript interruptible "h$base_write($1,$2_1,$2_2,$3,$c);"
    c_write :: CInt -> Ptr Word8 -> CSize -> IO CSsize
@@ -478,7 +478,7 @@ foreign import javascript interruptible "h$base_ftruncate($1,$2_1,$2_2,$c);" -- 
    c_ftruncate :: CInt -> FileOffset -> IO CInt
 foreign import javascript interruptible "h$base_unlink($1_1,$1_2,$c);"
    c_unlink :: CString -> IO CInt
-foreign import javascript unsafe "$r = h$base_getpid();"
+foreign import javascript unsafe "h$base_getpid();"
    c_getpid :: IO CPid
 -- foreign import ccall unsafe "HsBase.h fork"
 --   c_fork :: IO CPid
@@ -488,37 +488,37 @@ foreign import javascript interruptible "h$base_mkfifo($1_1,$1_2,$2,$c);"
    c_mkfifo :: CString -> CMode -> IO CInt
 foreign import javascript interruptible "h$base_pipe($1_1,$1_2,$c);"
   c_pipe :: Ptr CInt -> IO CInt
-foreign import javascript unsafe "$r = h$base_sigemptyset($1_1,$1_2);"
+foreign import javascript unsafe "h$base_sigemptyset($1_1,$1_2);"
    c_sigemptyset :: Ptr CSigset -> IO CInt
-foreign import javascript unsafe "$r = h$base_sigaddset($1_1,$1_2,$2);"
+foreign import javascript unsafe "h$base_sigaddset($1_1,$1_2,$2);"
    c_sigaddset :: Ptr CSigset -> CInt -> IO CInt
-foreign import javascript unsafe "$r = h$base_sigprocmask($1,$2_1,$2_2,$3_1,$3_2);"
+foreign import javascript unsafe "h$base_sigprocmask($1,$2_1,$2_2,$3_1,$3_2);"
    c_sigprocmask :: CInt -> Ptr CSigset -> Ptr CSigset -> IO CInt
-foreign import javascript unsafe "$r = h$base_tcgetattr($1,$2_1,$2_2);"
+foreign import javascript unsafe "h$base_tcgetattr($1,$2_1,$2_2);"
    c_tcgetattr :: CInt -> Ptr CTermios -> IO CInt
-foreign import javascript unsafe "$r = h$base_tcsetattr($1,$2,$3_1,$3_2);"
+foreign import javascript unsafe "h$base_tcsetattr($1,$2,$3_1,$3_2);"
    c_tcsetattr :: CInt -> CInt -> Ptr CTermios -> IO CInt
-foreign import javascript unsafe "$r = h$base_utime($1_1,$1_2,$2_1,$2_2);" -- should this be async?
+foreign import javascript unsafe "h$base_utime($1_1,$1_2,$2_1,$2_2);" -- should this be async?
    c_utime :: CString -> Ptr CUtimbuf -> IO CInt
 foreign import javascript interruptible "h$base_waitpid($1,$2_1,$2_2,$3,$c);"
    c_waitpid :: CPid -> Ptr CInt -> CInt -> IO CPid
 
-foreign import javascript unsafe "$r = h$base_o_rdonly;"   o_RDONLY   :: CInt
-foreign import javascript unsafe "$r = h$base_o_wronly;"   o_WRONLY   :: CInt
-foreign import javascript unsafe "$r = h$base_o_rdwr;"     o_RDWR     :: CInt
-foreign import javascript unsafe "$r = h$base_o_append;"   o_APPEND   :: CInt
-foreign import javascript unsafe "$r = h$base_o_creat;"    o_CREAT    :: CInt
-foreign import javascript unsafe "$r = h$base_o_excl;"     o_EXCL     :: CInt
-foreign import javascript unsafe "$r = h$base_o_trunc;"    o_TRUNC    :: CInt
-foreign import javascript unsafe "$r = h$base_o_noctty;"   o_NOCTTY   :: CInt
-foreign import javascript unsafe "$r = h$base_o_nonblock;" o_NONBLOCK :: CInt
-foreign import javascript unsafe "$r = h$base_o_binary;"   o_BINARY   :: CInt
+foreign import javascript unsafe "h$base_o_rdonly"   o_RDONLY   :: CInt
+foreign import javascript unsafe "h$base_o_wronly"   o_WRONLY   :: CInt
+foreign import javascript unsafe "h$base_o_rdwr"     o_RDWR     :: CInt
+foreign import javascript unsafe "h$base_o_append"   o_APPEND   :: CInt
+foreign import javascript unsafe "h$base_o_creat"    o_CREAT    :: CInt
+foreign import javascript unsafe "h$base_o_excl"     o_EXCL     :: CInt
+foreign import javascript unsafe "h$base_o_trunc"    o_TRUNC    :: CInt
+foreign import javascript unsafe "h$base_o_noctty"   o_NOCTTY   :: CInt
+foreign import javascript unsafe "h$base_o_nonblock" o_NONBLOCK :: CInt
+foreign import javascript unsafe "h$base_o_binary"   o_BINARY   :: CInt
 
-foreign import javascript unsafe "$r = h$base_c_s_isreg($1);"  c_s_isreg  :: CMode -> CInt
-foreign import javascript unsafe "$r = h$base_c_s_ischr($1);"  c_s_ischr  :: CMode -> CInt
-foreign import javascript unsafe "$r = h$base_c_s_isblk($1);"  c_s_isblk  :: CMode -> CInt
-foreign import javascript unsafe "$r = h$base_c_s_isdir($1);"  c_s_isdir  :: CMode -> CInt
-foreign import javascript unsafe "$r = h$base_c_s_isfifo($1);" c_s_isfifo :: CMode -> CInt
+foreign import javascript unsafe "h$base_c_s_isreg($1);"  c_s_isreg  :: CMode -> CInt
+foreign import javascript unsafe "h$base_c_s_ischr($1);"  c_s_ischr  :: CMode -> CInt
+foreign import javascript unsafe "h$base_c_s_isblk($1);"  c_s_isblk  :: CMode -> CInt
+foreign import javascript unsafe "h$base_c_s_isdir($1);"  c_s_isdir  :: CMode -> CInt
+foreign import javascript unsafe "h$base_c_s_isfifo($1);" c_s_isfifo :: CMode -> CInt
 
 s_isreg  :: CMode -> Bool
 s_isreg cm = c_s_isreg cm /= 0
@@ -531,42 +531,42 @@ s_isdir cm = c_s_isdir cm /= 0
 s_isfifo :: CMode -> Bool
 s_isfifo cm = c_s_isfifo cm /= 0
 
-foreign import javascript unsafe "$r = h$base_sizeof_stat;" sizeof_stat :: Int
+foreign import javascript unsafe "h$base_sizeof_stat" sizeof_stat :: Int
 foreign import javascript unsafe "h$base_st_mtime($1_1,$1_2)" st_mtime :: Ptr CStat -> IO CTime
-foreign import javascript unsafe "$r1 = h$base_st_size($1_1,$1_2); $r2 = h$ret1;" st_size :: Ptr CStat -> IO Int64
-foreign import javascript unsafe "$r = h$base_st_mode($1_1,$1_2);" st_mode :: Ptr CStat -> IO CMode
-foreign import javascript unsafe "$r = h$base_st_dev($1_1,$1_2);" st_dev :: Ptr CStat -> IO CDev
-foreign import javascript unsafe "$r1 = h$base_st_ino($1_1,$1_2); $r2 = h$ret1;" st_ino :: Ptr CStat -> IO CIno
+foreign import javascript unsafe "h$base_st_size($1_1,$1_2)" st_size :: Ptr CStat -> IO Int64
+foreign import javascript unsafe "h$base_st_mode($1_1,$1_2)" st_mode :: Ptr CStat -> IO CMode
+foreign import javascript unsafe "h$base_st_dev($1_1,$1_2)" st_dev :: Ptr CStat -> IO CDev
+foreign import javascript unsafe "h$base_st_ino($1_1,$1_2)" st_ino :: Ptr CStat -> IO CIno
 
-foreign import javascript unsafe "$r = h$base_echo;"            const_echo :: CInt
-foreign import javascript unsafe "$r = h$base_tcsanow;"         const_tcsanow :: CInt
-foreign import javascript unsafe "$r = h$base_icanon;"          const_icanon :: CInt
-foreign import javascript unsafe "$r = h$base_vmin;"            const_vmin   :: CInt
-foreign import javascript unsafe "$r = h$base_vtime;"           const_vtime  :: CInt
-foreign import javascript unsafe "$r = h$base_sigttou;"         const_sigttou :: CInt
-foreign import javascript unsafe "$r = h$base_sig_block;"       const_sig_block :: CInt
-foreign import javascript unsafe "$r = h$base_sig_setmask;"     const_sig_setmask :: CInt
-foreign import javascript unsafe "$r = h$base_f_getfl;"         const_f_getfl :: CInt
-foreign import javascript unsafe "$r = h$base_f_setfl;"         const_f_setfl :: CInt
-foreign import javascript unsafe "$r = h$base_f_setfd;"         const_f_setfd :: CInt
-foreign import javascript unsafe "$r = h$base_fd_cloexec;"      const_fd_cloexec :: CLong
-foreign import javascript unsafe "$r = h$base_sizeof_termios;"  sizeof_termios :: Int
-foreign import javascript unsafe "$r = h$base_sizeof_sigset_t;" sizeof_sigset_t :: Int
-foreign import javascript unsafe "$r = h$base_lflag($1_1,$1_2);"           c_lflag :: Ptr CTermios -> IO CTcflag
+foreign import javascript unsafe "h$base_echo"            const_echo :: CInt
+foreign import javascript unsafe "h$base_tcsanow"         const_tcsanow :: CInt
+foreign import javascript unsafe "h$base_icanon"          const_icanon :: CInt
+foreign import javascript unsafe "h$base_vmin"            const_vmin   :: CInt
+foreign import javascript unsafe "h$base_vtime"           const_vtime  :: CInt
+foreign import javascript unsafe "h$base_sigttou"         const_sigttou :: CInt
+foreign import javascript unsafe "h$base_sig_block"       const_sig_block :: CInt
+foreign import javascript unsafe "h$base_sig_setmask"     const_sig_setmask :: CInt
+foreign import javascript unsafe "h$base_f_getfl"         const_f_getfl :: CInt
+foreign import javascript unsafe "h$base_f_setfl"         const_f_setfl :: CInt
+foreign import javascript unsafe "h$base_f_setfd"         const_f_setfd :: CInt
+foreign import javascript unsafe "h$base_fd_cloexec"      const_fd_cloexec :: CLong
+foreign import javascript unsafe "h$base_sizeof_termios"  sizeof_termios :: Int
+foreign import javascript unsafe "h$base_sizeof_sigset_t" sizeof_sigset_t :: Int
+foreign import javascript unsafe "h$base_lflag($1_1,$1_2);"           c_lflag :: Ptr CTermios -> IO CTcflag
 foreign import javascript unsafe "h$base_poke_lflag($1_1,$1_2,$2);"      poke_c_lflag :: Ptr CTermios -> CTcflag -> IO ()
-foreign import javascript unsafe "$r1 = h$base_ptr_c_cc($1_1,$1_2); $r2 = h$ret1;"   ptr_c_cc  :: Ptr CTermios -> IO (Ptr Word8)
+foreign import javascript unsafe "h$base_ptr_c_cc($1_1,$1_2)"   ptr_c_cc  :: Ptr CTermios -> IO (Ptr Word8)
 s_issock :: CMode -> Bool
 s_issock cmode = c_s_issock cmode /= 0
 foreign import javascript unsafe "h$base_c_s_issock($1)"          c_s_issock :: CMode -> CInt
-foreign import javascript unsafe "$r = h$base_default_buffer_size;" dEFAULT_BUFFER_SIZE :: Int
-foreign import javascript unsafe "$r = h$base_SEEK_CUR;"            sEEK_CUR :: CInt
-foreign import javascript unsafe "$r = h$base_SEEK_SET;"            sEEK_SET :: CInt
-foreign import javascript unsafe "$r = h$base_SEEK_END"             sEEK_END :: CInt
+foreign import javascript unsafe "h$base_default_buffer_size" dEFAULT_BUFFER_SIZE :: Int
+foreign import javascript unsafe "h$base_SEEK_CUR"            sEEK_CUR :: CInt
+foreign import javascript unsafe "h$base_SEEK_SET"            sEEK_SET :: CInt
+foreign import javascript unsafe "h$base_SEEK_END"             sEEK_END :: CInt
 
 -- fixme, unclear if these can be supported, remove?
-foreign import javascript unsafe "$r = h$base_c_fcntl_read($1,$2)"            c_fcntl_read  :: CInt -> CInt -> IO CInt
-foreign import javascript unsafe "$r = h$base_c_fcntl_write($1,$2,$3);"       c_fcntl_write :: CInt -> CInt -> CLong -> IO CInt
-foreign import javascript unsafe "$r = h$base_c_fcntl_lock($1,$2,$3_1,$3_2);" c_fcntl_lock  :: CInt -> CInt -> Ptr CFLock -> IO CInt
+foreign import javascript unsafe "h$base_c_fcntl_read($1,$2)"           c_fcntl_read  :: CInt -> CInt -> IO CInt
+foreign import javascript unsafe "h$base_c_fcntl_write($1,$2,$3)"       c_fcntl_write :: CInt -> CInt -> CLong -> IO CInt
+foreign import javascript unsafe "h$base_c_fcntl_lock($1,$2,$3_1,$3_2)" c_fcntl_lock  :: CInt -> CInt -> Ptr CFLock -> IO CInt
 
 #else
 
