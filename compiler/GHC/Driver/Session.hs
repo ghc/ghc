48efab371341b27c821566301cfc116172d2b595
Ben Gamari <ben@well-typed.com>
2022-06-23 12:12:52 -0400

    -ddump-llvm shouldn't imply -fllvm
    Previously -ddump-llvm would change the backend used, which contrasts
    with all other dump flags. This is quite surprising and cost me quite
    a bit of time. Dump flags should not change compiler behavior.
    
    Fixes #21776.
    
    (cherry picked from commit df762ae9e2d5263fb71f6df38323ac3ca400cc47)

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -2497,7 +2497,7 @@ dynamic_flags_deps = [
   , make_ord_flag defGhcFlag "ddump-asm-stats"
         (setDumpFlag Opt_D_dump_asm_stats)
   , make_ord_flag defGhcFlag "ddump-llvm"
-        (NoArg $ setObjBackend LLVM >> setDumpFlag' Opt_D_dump_llvm)
+        (NoArg $ setDumpFlag' Opt_D_dump_llvm)
   , make_ord_flag defGhcFlag "ddump-c-backend"
         (NoArg $ setDumpFlag' Opt_D_dump_c_backend)
   , make_ord_flag defGhcFlag "ddump-deriv"
