ghc/ghc

commit 0ef249aa26f653677c5368bb51af34f7577ba5b9

Introduce package to capture dependency on C++ stdlib
Here we introduce a new "virtual" package into the initial package
database, `system-cxx-std-lib`. This gives users a convenient, platform
agnostic way to link against C++ libraries, addressing #20010.

Fixes #20010.

ghc.mk
@@ -579,6 +579,10 @@ utils/check-ppr/dist-install/package-data.mk: compiler/stage2/package-data.mk
 utils/check-exact/dist-install/package-data.mk: compiler/stage2/package-data.mk
 utils/count-deps/dist-install/package-data.mk: compiler/stage2/package-data.mk
 
+# ensure that system-cxx-std-lib is installed in the inplace compiler by
+# injecting a dependency from ghc-prim
+libraries/ghc-prim/dist-install/package-data.mk : inplace/lib/package.conf.d/system-cxx-std-lib-1.0.conf
+
 # add the final package.conf dependency: ghc-prim depends on RTS
 libraries/ghc-prim/dist-install/package-data.mk : rts/dist-install/package.conf.inplace
 endif
@@ -823,6 +827,12 @@ INSTALL_LIBRARY_DOCS += libraries/dist-haddock/*
 endif
 
 # -----------------------------------------------------------------------------
+# system-cxx-std-lib
+
+inplace/lib/package.conf.d/system-cxx-std-lib-1.0.conf : mk/system-cxx-std-lib-1.0.conf $(ghc-pkg_INPLACE)
+	"$(ghc-pkg_INPLACE)" update --force $<
+
+# -----------------------------------------------------------------------------
 # Creating a local mingw copy on Windows
 
 ifeq "$(Windows_Host)" "YES"
@@ -993,7 +1003,7 @@ INSTALL_DISTDIR_compiler = stage2
 
 # Now we can do the installation
 install_packages: install_libexecs
-install_packages: rts/dist-install/package.conf.install
+install_packages: rts/dist-install/package.conf.install mk/system-cxx-std-lib-1.0.conf.install
 	$(INSTALL_DIR) "$(DESTDIR)$(topdir)"
 	$(call removeTrees,"$(INSTALLED_PACKAGE_CONF)")
 	$(INSTALL_DIR) "$(INSTALLED_PACKAGE_CONF)"
@@ -1010,6 +1020,7 @@ install_packages: rts/dist-install/package.conf.install
 	                                  '$(docdir)/html/libraries'  \
 	                                  '$(GhcLibWays)'))
 	"$(INSTALLED_GHC_PKG_REAL)" --force --global-package-db "$(INSTALLED_PACKAGE_CONF)" update rts/dist-install/package.conf.install
+	"$(INSTALLED_GHC_PKG_REAL)" --force --global-package-db "$(INSTALLED_PACKAGE_CONF)" update mk/system-cxx-std-lib-1.0.conf
 	$(foreach p, $(INSTALL_PACKAGES),                             \
 	    $(call make-command,                                      \
 	           "$(ghc-cabal_INPLACE)" register                    \
@@ -1050,6 +1061,7 @@ $(eval $(call bindist-list,.,\
     packages \
     Makefile \
     mk/config.mk.in \
+    mk/system-cxx-std-lib-1.0.conf.in \
     $(INPLACE_BIN)/mkdirhier \
     utils/ghc-cabal/dist-install/build/tmp/ghc-cabal \
     $(BINDIST_WRAPPERS) \

.gitignore
@@ -181,6 +181,7 @@ _darcs/
 /mk/config.h.in
 /mk/config.mk
 /mk/config.mk.old
+/mk/system-cxx-std-lib-1.0.conf
 /mk/install.mk
 /mk/project.mk
 /mk/project.mk.old

configure.ac
@@ -692,6 +692,10 @@ AC_SUBST(CONF_HC_OPTS_STAGE0)
 AC_SUBST(CONF_HC_OPTS_STAGE1)
 AC_SUBST(CONF_HC_OPTS_STAGE2)
 
+dnl Identify C++ standard library flavour and location
+FP_FIND_CXX_STD_LIB
+AC_CONFIG_FILES([mk/system-cxx-std-lib-1.0.conf])
+
 dnl ** Set up the variables for the platform in the settings file.
 dnl May need to use gcc to find platform details.
 dnl --------------------------------------------------------------

distrib/configure.ac.in
@@ -173,6 +173,10 @@ AC_SUBST(CONF_CPP_OPTS_STAGE0)
 AC_SUBST(CONF_CPP_OPTS_STAGE1)
 AC_SUBST(CONF_CPP_OPTS_STAGE2)
 
+dnl Identify C++ standard library flavour and location
+FP_FIND_CXX_STD_LIB
+AC_CONFIG_FILES([mk/system-cxx-std-lib-1.0.conf])
+
 dnl ** Set up the variables for the platform in the settings file.
 dnl May need to use gcc to find platform details.
 dnl --------------------------------------------------------------

hadrian/bindist/Makefile
@@ -208,7 +208,9 @@ install_wrappers: install_bin_libdir
 
 PKG_CONFS = $(shell find "$(ActualLibsDir)/package.conf.d" -name '*.conf' | sed "s:   :\0xxx\0:g")
 update_package_db: install_bin install_lib
-	@echo "$(PKG_CONFS)"
+	@echo "Installing C++ standard library virtual package"
+	cp mk/system-cxx-std-lib-1.0.conf "$(ActualLibsDir)/"
+	
 	@echo "Updating the package DB"
 	$(foreach p, $(PKG_CONFS),\
 		$(call patchpackageconf,$(shell echo $(notdir $p) | sed 's/-\([0-9]*[0-9]\.\)*conf//g'),$(shell echo "$p" | sed 's:\0xxx\0:   :g'),$(docdir),$(shell mk/relpath.sh "$(ActualLibsDir)" "$(docdir)")))

hadrian/src/Base.hs
@@ -28,6 +28,7 @@ module Base (
     stageBinPath, stageLibPath, templateHscPath,
     ghcBinDeps, ghcLibDeps, haddockDeps,
     relativePackageDbPath, packageDbPath, packageDbStamp, mingwStamp,
+    systemCxxStdLibConf, systemCxxStdLibConfPath
     ) where
 
 import Control.Applicative
@@ -93,6 +94,15 @@ packageDbPath stage = buildRoot <&> (-/- relativePackageDbPath stage)
 packageDbStamp :: FilePath
 packageDbStamp = ".stamp"
 
+systemCxxStdLibConf :: FilePath
+systemCxxStdLibConf = "system-cxx-std-lib-1.0.conf"
+
+-- | The name of the generated @system-cxx-std-lib-1.0.conf@ package database
+-- entry.
+systemCxxStdLibConfPath :: Stage -> Action FilePath
+systemCxxStdLibConfPath stage =
+    packageDbPath stage <&> (-/- systemCxxStdLibConf)
+
 -- | @bin@ directory for the given 'Stage' (including the build root)
 stageBinPath :: Stage -> Action FilePath
 stageBinPath stage = buildRoot <&> (-/- stageString stage -/- "bin")
@@ -103,11 +113,14 @@ stageLibPath stage = buildRoot <&> (-/- stageString stage -/- "lib")
 
 -- | Files the GHC library depends on
 ghcLibDeps :: Stage -> Action [FilePath]
-ghcLibDeps stage = mapM (\f -> stageLibPath stage <&> (-/- f))
-    [ "llvm-targets"
-    , "llvm-passes"
-    , "settings"
-    ]
+ghcLibDeps stage = do
+    ps <- mapM (\f -> stageLibPath stage <&> (-/- f))
+        [ "llvm-targets"
+        , "llvm-passes"
+        , "settings"
+        ]
+    cxxStdLib <- systemCxxStdLibConfPath stage
+    return (cxxStdLib : ps)
 
 -- | Files the GHC binary depends on.
 ghcBinDeps :: Stage -> Action [FilePath]

hadrian/src/Rules/BinaryDist.hs
@@ -324,7 +324,6 @@ bindistRules = do
         top <- topDirectory
         copyFile (top -/- "hadrian" -/- "bindist" -/- "Makefile") makefilePath
 
-
     -- Copy various configure-related files needed for a working
     -- './configure [...] && make install' workflow
     -- (see the list of files needed in the 'binary-dist' rule above, before
@@ -360,6 +359,7 @@ bindistInstallFiles =
     [ "config.sub", "config.guess", "install-sh"
     , "mk" -/- "config.mk.in", "mk" -/- "install.mk.in", "mk" -/- "project.mk"
     , "mk" -/- "relpath.sh"
+    , "mk" -/- "system-cxx-std-lib-1.0.conf.in"
     , "README", "INSTALL" ]
 
 -- | This auxiliary function gives us a top-level 'Filepath' that we can 'need'

hadrian/src/Rules/Generate.hs
@@ -192,6 +192,9 @@ copyRules = do
         prefix -/- "html/**"           <~ return "utils/haddock/haddock-api/resources"
         prefix -/- "latex/**"          <~ return "utils/haddock/haddock-api/resources"
 
+        root -/- relativePackageDbPath stage -/- systemCxxStdLibConf %> \file -> do
+            copyFile ("mk" -/- "system-cxx-std-lib-1.0.conf") file
+
 generateRules :: Rules ()
 generateRules = do
     root <- buildRootRules

commit 03cc5d0258294fc83f65479e78dbe937622cfc88

ghc.mk: fix 'make install' (`mk/system-cxx-std-lib-1.0.conf.install` does not exist)
before the change `make install` was failing as:

```
"mv" "/<<NIX>>/ghc-9.3.20220406/lib/ghc-9.5.20220625/bin/ghc-stage2" "/<<NIX>>/ghc-9.3.20220406/lib/ghc-9.5.20220625/bin/ghc"
make[1]: *** No rule to make target 'mk/system-cxx-std-lib-1.0.conf.install', needed by 'install_packages'.  Stop.
```

I think it's a recent regression caused by 0ef249aa where `system-cxx-std-lib-1.0.conf`
is created (somewhat manually), but not the .install varianlt of it.

The fix is to consistently use `mk/system-cxx-std-lib-1.0.conf` everywhere.

Closes: https://gitlab.haskell.org/ghc/ghc/-/issues/21784

ghc.mk
@@ -1003,7 +1003,7 @@ INSTALL_DISTDIR_compiler = stage2
 
 # Now we can do the installation
 install_packages: install_libexecs
-install_packages: rts/dist-install/package.conf.install mk/system-cxx-std-lib-1.0.conf.install
+install_packages: rts/dist-install/package.conf.install mk/system-cxx-std-lib-1.0.conf
 	$(INSTALL_DIR) "$(DESTDIR)$(topdir)"
 	$(call removeTrees,"$(INSTALLED_PACKAGE_CONF)")
 	$(INSTALL_DIR) "$(INSTALLED_PACKAGE_CONF)"