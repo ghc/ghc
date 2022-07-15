be80054133266c716349a77eec040359cdabab37
Colten Webb <coltenwebb@gmail.com>
2022-07-15 07:54:39 -0500

    Add record-dot-syntax test
    (cherry picked from commit 89d169ec0c4e9c1e6cf4a6373a1992dad7474d55)

---
+++ testsuite/tests/hiefile/should_run/TestUtils.hs
@@ -0,0 +1,43 @@
+module TestUtils
+  ( readTestHie
+  , render
+  , text
+  , DynFlags
+  , module GHC.Iface.Ext.Types
+  , module GHC.Iface.Ext.Utils
+  ) where
+
+import System.Environment
+import Data.Tree
+import GHC.Types.Name.Cache
+import GHC.Types.SrcLoc
+import GHC.Types.Unique.Supply
+import GHC.Types.Name
+import GHC.Utils.Outputable                 ( Outputable, renderWithContext, ppr, defaultUserStyle, text)
+import GHC.Iface.Ext.Binary
+import GHC.Iface.Ext.Types
+import GHC.Iface.Ext.Utils
+
+import GHC.Driver.Session
+import GHC.SysTools
+
+makeNc :: IO NameCache
+makeNc = do
+  uniq_supply <- mkSplitUniqSupply 'z'
+  pure $ initNameCache uniq_supply []
+
+dynFlagsForPrinting :: String -> IO DynFlags
+dynFlagsForPrinting libdir = do
+  systemSettings <- initSysTools libdir
+  return $ defaultDynFlags systemSettings (LlvmConfig [] [])
+
+readTestHie :: FilePath -> IO (DynFlags, HieFile)
+readTestHie fp = do
+  libdir:_ <- getArgs
+  df <- dynFlagsForPrinting libdir
+  nc <- makeNc
+  hfr <- readHieFile (NCU (\f -> pure $ snd $ f nc)) fp
+  pure (df, hie_file_result hfr)
+
+render :: Outputable a => DynFlags -> a -> String
+render df = renderWithContext (initSDocContext df defaultUserStyle) . ppr
