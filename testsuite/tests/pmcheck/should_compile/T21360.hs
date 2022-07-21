6379f9429e8f9e8f58f4c3c8206c265620feb26e
sheaf <sam.derbyshire@gmail.com>
2022-07-21 15:13:58 +0200

    Add test for #21360
    The way record updates are typechecked/desugared changed in MR !7981.
    Because we desugar in the typechecker to a simple case expression, the
    pattern match checker becomes able to spot the long-distance information
    and avoid emitting an incorrect pattern match warning.
    
    Fixes #21360

---
+++ testsuite/tests/pmcheck/should_compile/T21360.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -Wincomplete-record-updates #-}
+
+module T21360 where
+
+data Foo = A {a :: Int} | B deriving Show
+
+foo = A 4
+
+-- wibble is safe - no warning
+wibble = do
+  case foo of
+    bar@A{} -> Just bar{a = 9}
+    _ -> fail ":("
+
+-- using guards doesn't throw a warning
+twomble | bar@A{} <- foo = Just bar{a = 9}
+        | otherwise  = fail ":("
+
+-- sworble has the same semantics as wibble and twomble - but we get a warning!
+sworble = do
+  bar@A{} <- Just foo
+  Just bar{a = 9}
