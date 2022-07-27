509198cbb3cb3a03e3135308443eb478e8a38f4a
Douglas Wilson <douglas.wilson@gmail.com>
2022-07-27 18:18:08 +0100

    Update submodule Cabal to tag Cabal-v3.8.1.0
    closes #21931

--- testsuite/tests/driver/T4437.hs
+++ testsuite/tests/driver/T4437.hs
@@ -37,11 +37,7 @@ check title expected got
 -- See Note [Adding a language extension] in compiler/GHC/Driver/Session.hs.
 expectedGhcOnlyExtensions :: [String]
 expectedGhcOnlyExtensions =
-    [ "RelaxedLayout"
-    , "AlternativeLayoutRule"
-    , "AlternativeLayoutRuleTransitional"
-    , "OverloadedRecordUpdate"
-    , "DeepSubsumption"
+    [ "DeepSubsumption"
     ]
 
 expectedCabalOnlyExtensions :: [String]
