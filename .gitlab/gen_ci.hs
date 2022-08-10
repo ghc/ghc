847c59527019f11d2b298102f6c24719ce0ef48c
Ben Gamari <ben@smart-cactus.org>
2022-08-10 17:28:56 -0400

    gitlab-ci: Don't allow FreeBSD job to fail

--- .gitlab/gen_ci.hs
+++ .gitlab/gen_ci.hs
@@ -810,7 +810,7 @@ jobs = Map.fromList $ concatMap flattenJobGroup $
      , fastCI (standardBuilds Amd64 Windows)
      , disableValidate (standardBuildsWithConfig Amd64 Windows nativeInt)
      , standardBuilds Amd64 Darwin
-     , allowFailureGroup (addValidateRule FreeBSDLabel (standardBuilds Amd64 FreeBSD13))
+     , addValidateRule FreeBSDLabel (standardBuilds Amd64 FreeBSD13)
      , standardBuilds AArch64 Darwin
      , standardBuilds AArch64 (Linux Debian10)
      , allowFailureGroup (addValidateRule ARMLabel (standardBuilds ARMv7 (Linux Debian10)))
