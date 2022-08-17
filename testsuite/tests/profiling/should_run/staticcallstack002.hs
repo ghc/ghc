84612d1e6f1af9059fef70d3caeea0dbecaf8694
Ben Gamari <ben@smart-cactus.org>
2022-08-17 14:37:05 -0400

    base: Move IPE helpers to GHC.InfoProv

--- testsuite/tests/profiling/should_run/staticcallstack002.hs
+++ testsuite/tests/profiling/should_run/staticcallstack002.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE UnboxedTuples #-}
 module Main where
 
-import GHC.Stack.CCS
+import GHC.InfoProv
 
 -- Unboxed data constructors don't have info tables so there is
 -- a special case to not generate distinct info tables for unboxed
