683e0c7aef2bbfedbee02c8fa32789e32c89a4db
Ben Gamari <ben@smart-cactus.org>
2022-10-20 22:42:14 +0000

    nonmoving: Fix handling of weak pointers
    This fixes an interaction between aging and weak pointer handling which
    prevented the finalization of some weak pointers. In particular, weak
    pointers could have their keys incorrectly marked by the preparatory
    collector, preventing their finalization by the subsequent concurrent
    collection.
    
    While in the area, we also significantly improve the assertions
    regarding weak pointers.
    
    Fixes #22327.

--- rts/sm/NonMoving.h
+++ rts/sm/NonMoving.h
@@ -298,7 +298,6 @@ INLINE_HEADER void nonmovingSetClosureMark(StgPtr p)
     nonmovingSetMark(nonmovingGetSegment(p), nonmovingGetBlockIdx(p));
 }
 
-// TODO: Audit the uses of these
 /* Was the given closure marked this major GC cycle? */
 INLINE_HEADER bool nonmovingClosureMarkedThisCycle(StgPtr p)
 {
