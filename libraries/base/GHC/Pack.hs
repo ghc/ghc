270037faa124bf59dda8ba4f3d73b97d4c109a5f
Hécate Moonlight <hecate+gitlab@glitchbra.in>
2022-10-28 19:24:55 +0200

    Start the deprecation process for GHC.Pack

--- libraries/base/GHC/Pack.hs
+++ libraries/base/GHC/Pack.hs
@@ -12,6 +12,11 @@
 -- Stability   :  internal
 -- Portability :  non-portable (GHC Extensions)
 --
+-- ⚠ Warning: Starting @base-4.18@, this module is being deprecated.
+-- See https://gitlab.haskell.org/ghc/ghc/-/issues/21461 for more information.
+--
+--
+--
 -- This module provides a small set of low-level functions for packing
 -- and unpacking a chunk of bytes. Used by code emitted by the compiler
 -- plus the prelude libraries.
