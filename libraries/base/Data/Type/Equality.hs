02d3511b8d248ea9429512830f8f17b31688a6a6
Bodigrim <andrew.lelechenko@gmail.com>
2022-11-15 00:14:59 +0000

    Fix capitalization in haddock for TestEquality

--- libraries/base/Data/Type/Equality.hs
+++ libraries/base/Data/Type/Equality.hs
@@ -152,14 +152,14 @@ deriving instance a ~~ b => Bounded (a :~~: b)
 -- The result should be @Just Refl@ if and only if the types applied to @f@ are
 -- equal:
 --
--- @TestEquality (x :: f a) (y :: f b) = Just Refl ⟺ a = b@
+-- @testEquality (x :: f a) (y :: f b) = Just Refl ⟺ a = b@
 --
 -- Typically, only singleton types should inhabit this class. In that case type
 -- argument equality coincides with term equality:
 --
--- @TestEquality (x :: f a) (y :: f b) = Just Refl ⟺ a = b ⟺ x = y@
+-- @testEquality (x :: f a) (y :: f b) = Just Refl ⟺ a = b ⟺ x = y@
 --
--- @isJust (TestEquality x y) = x == y@
+-- @isJust (testEquality x y) = x == y@
 --
 -- Singleton types are not required, however, and so the latter two would-be
 -- laws are not in fact valid in general.
