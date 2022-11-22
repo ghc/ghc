def47dd32491311289bff26230b664c895f178cc
Ben Gamari <ben@smart-cactus.org>
2022-11-22 14:30:21 -0500

    testsuite: Don't use grep -q in unpack_sums_7
    `grep -q` closes stdin as soon as it finds the pattern it is looking
    for, resulting in #22484.

--- testsuite/tests/unboxedsums/unpack_sums_7.hs
+++ testsuite/tests/unboxedsums/unpack_sums_7.hs
@@ -1,6 +1,3 @@
--- NB: Compiling this module throws an exception involving Weak# at the end of compilation.
--- This is unrelated to unpacked sums but we need to include the error in the expected output for the test to pass.
-
 module UnpackedSums7 where
 
 data T = MkT {-# UNPACK #-} !MI
