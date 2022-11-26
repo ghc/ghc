cfeac57b8726f1e750831a05047ae82a0751a1dc
Sven Tennie <sven.tennie@gmail.com>
2022-11-26 11:25:02 +0000

    CLeanup / Formatting

--- libraries/ghc-heap/tests/stack_comparison.hs
+++ libraries/ghc-heap/tests/stack_comparison.hs
@@ -4,12 +4,12 @@
 
 module Main where
 
+import Data.Array.Byte
+import GHC.Exts
 import GHC.Exts.DecodeStack
+import GHC.Exts.Heap
 import GHC.Stack.CloneStack
 import TestUtils
-import GHC.Exts
-import Data.Array.Byte
-import GHC.Exts.Heap
 
 foreign import ccall "foldStackToArrayClosure" foldStackToArrayClosure# :: StackSnapshot# -> ByteArray#
 
@@ -22,25 +22,21 @@ main = do
   let ba = foldStackToArrayClosure stack
   let s = I# (sizeofByteArray# b#)
       (ByteArray b#) = ba
---  print $ "ByteArray size" ++ show (I# (sizeofByteArray# b#))
---  print $ "indices " ++ show [0..((wds s) -1)]
   print . show . toClosureTypes . toWords $ ba
-
-maxWordIndex :: ByteArray -> Int
-maxWordIndex (ByteArray ba#) =
-  let s = I# (sizeofByteArray# ba#)
-      words = s `div` 8
-  in
-    case words of
-      w | w == 0 -> error "ByteArray contains no content!"
-      w -> w - 1
 
 toWords :: ByteArray -> [Word]
 toWords ba@(ByteArray b#) =
   let s = I# (sizeofByteArray# b#)
-  in
-    -- TODO: Adjust 8 to machine word size
-    [ W# (indexWordArray# b# (toInt# i)) | i <- [0..maxWordIndex(ba)] ]
+   in -- TODO: Adjust 8 to machine word size
+      [W# (indexWordArray# b# (toInt# i)) | i <- [0 .. maxWordIndex (ba)]]
+  where
+    maxWordIndex :: ByteArray -> Int
+    maxWordIndex (ByteArray ba#) =
+      let s = I# (sizeofByteArray# ba#)
+          words = s `div` 8
+       in case words of
+            w | w == 0 -> error "ByteArray contains no content!"
+            w -> w - 1
 
 toClosureTypes :: [Word] -> [ClosureType]
 toClosureTypes = map (toEnum . fromIntegral)
