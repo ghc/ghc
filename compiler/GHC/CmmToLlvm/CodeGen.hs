87e87896bd640c39afeeb76189f989a8374f0e37
Ben Gamari <ben@well-typed.com>
2022-12-21 17:23:25 +0000

    llvmGen: Fix relaxed ordering
    Previously I used LLVM's `unordered` ordering for the C11 `relaxed`
    ordering. However, this is wrong and should rather use the LLVM
    `monotonic` ordering.
    
    Fixes #22640

--- compiler/GHC/CmmToLlvm/CodeGen.hs
+++ compiler/GHC/CmmToLlvm/CodeGen.hs
@@ -2041,7 +2041,7 @@ genLit _ CmmHighStackMark
 --
 
 convertMemoryOrdering :: MemoryOrdering -> LlvmSyncOrdering
-convertMemoryOrdering MemOrderRelaxed = SyncUnord
+convertMemoryOrdering MemOrderRelaxed = SyncMonotonic
 convertMemoryOrdering MemOrderAcquire = SyncAcquire
 convertMemoryOrdering MemOrderRelease = SyncRelease
 convertMemoryOrdering MemOrderSeqCst  = SyncSeqCst
