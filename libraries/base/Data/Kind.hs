d0d7ba0fb053ebe7f919a5932066fbc776301ccd
Vladislav Zavialov <vlad.z.4096@gmail.com>
2023-01-20 22:02:22 +0300

    base: NoImplicitPrelude in Data.Void and Data.Kind
    This change removes an unnecessary dependency on Prelude
    from two modules in the base package.

--- libraries/base/Data/Kind.hs
+++ libraries/base/Data/Kind.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE Trustworthy, ExplicitNamespaces #-}
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 -----------------------------------------------------------------------------
 -- |
