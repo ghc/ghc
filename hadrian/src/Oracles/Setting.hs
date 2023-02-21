1e0d8fdb55a38ece34fa6cf214e1d2d46f5f5bf2
Greg Steuck <greg@nest.cx>
2023-02-20 21:40:35 -0800

    Change hostSupportsRPaths to report False on OpenBSD
    OpenBSD does support -rpath but ghc build process relies on some
    related features that don't work there. See ghc/ghc#23011

--- hadrian/src/Oracles/Setting.hs
+++ hadrian/src/Oracles/Setting.hs
@@ -293,7 +293,10 @@ isElfTarget = anyTargetOs elfOSes
 -- TODO: Windows supports lazy binding (but GHC doesn't currently support
 --       dynamic way on Windows anyways).
 hostSupportsRPaths :: Action Bool
-hostSupportsRPaths = anyHostOs (elfOSes ++ machoOSes)
+hostSupportsRPaths = do
+  -- https://gitlab.haskell.org/ghc/ghc/-/issues/23011
+  isOpenBSD <- anyHostOs ["openbsd"]
+  if not isOpenBSD then anyHostOs (elfOSes ++ machoOSes) else pure False
 
 -- | Check whether the target supports GHCi.
 ghcWithInterpreter :: Action Bool
