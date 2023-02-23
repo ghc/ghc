de3fb9852a5a57ebab63ae9a2b82ca1860c4b9e3
Ubuntu <ubuntu@ip-172-31-25-209.ec2.internal>
2023-02-23 03:13:40 +0000

    Run script

---
+++ run.sh
@@ -0,0 +1,31 @@
+test=testsuite/tests/driver/T20030/test1
+
+run() {
+    (
+        cd $test
+        $WRAPPER \
+		"$HOME/ghc/_build/stage1/bin/ghc" --make \
+                I.hs K.hs \
+                -dcore-lint -dstg-lint -dcmm-lint -no-user-package-db -fno-dump-with-ways \
+                -rtsopts \
+                -v1 -j8 -fforce-recomp +RTS -DS -DZ -N8 -A128k $RTS_FLAGS -RTS \
+                || return 1
+    )
+}
+
+run_many() {
+    while true; do run || break; done
+}
+
+run_rr() {
+    rm -Rf $test/rr.out
+    WRAPPER="$HOME/rr/bin/rr record -h -o rr.out" \
+	    RTS_FLAGS=-qn1 \
+	    run
+}
+
+run_many_rr() {
+    while true; do run_rr || break; done
+}
+
+run_many
