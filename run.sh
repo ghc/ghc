4b92cc40dac205ec8e9d91b265c8aa9738cb575a
Ubuntu <ubuntu@ip-172-31-25-209.ec2.internal>
2023-02-23 22:47:39 +0000

    run

--- run.sh
+++ run.sh
@@ -1,34 +1,59 @@
 test=testsuite/tests/driver/T20030/test1
 
+build="_build"
+tree="$HOME/ghc/$build"
+
 cmdline=(
-	"$HOME/ghc/_build/stage1/bin/ghc"
+	"$tree/stage1/bin/ghc"
        	--make -i$test I K
 	-dcore-lint -dstg-lint -dcmm-lint -no-user-package-db -fno-dump-with-ways
 	-rtsopts
-	-v1 -j8 -fforce-recomp +RTS -DS -DZ -N8 -A128k $RTS_FLAGS -RTS
+	-v1 -j8 -fforce-recomp +RTS -DZ $RTS_FLAGS -RTS
 )
 
 run() {
-    $WRAPPER "${cmdline[@]}" || return 1
+    $WRAPPER "${cmdline[@]}" $@ || return 1
 }
 
 run_many() {
-    while true; do run || break; done
+    local i=0
+    while true; do
+	let i=i+1
+	printf "# Iteration %d\n" "$i"
+	run $@ || break
+    done
 }
 
 run_rr() {
-    rm -Rf $test/rr.out
+    rm -Rf rr.out
     WRAPPER="$HOME/rr/bin/rr record -h -o rr.out" \
-	    RTS_FLAGS=-qn1 \
-	    run
+	    run $@
 }
 
 run_many_rr() {
-    while true; do run_rr || break; done
+    local i=0
+    while true; do
+	let i=i+1
+	printf "# Iteration %d\n" "$i"
+        run_rr $@ || break
+    done
 }
 
 run_gdb() {
-    gdb --args "${cmdline[@]}"
+    gdb --args "${cmdline[@]}" $@
 }
 
-run_many
+build() {
+    local args=(
+        "-j4"
+       	"stage1.*.ghc.c.opts+=-optc-moutline-atomics"
+	"stage1.rts.ghc.hs.opts+=-ddump-to-file -ddump-cmm -ddump-asm"
+    )
+    hadrian/build-cabal -o_build --flavour=default+debug_info+debug_ghc "${args[@]}" &
+    hadrian/build-cabal -o_build-llvm --flavour=default+debug_info+debug_ghc+llvm "${args[@]}" &
+    wait
+}
+
+mode=$1
+shift
+$mode $@
