f8ced2411aaa92570ddad7848b8f5a60ac8927ed
Matthew Pickering <matthewtpickering@gmail.com>
2023-03-27 12:00:08 +0100

    Introduce GHCiMessage to wrap GhcMessage
    By introducing a wrapped message type we can control how certain
    messages are printed in GHCi (to add extra information for example)

---
+++ ghc/GHCi/UI/Exception.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+module GHCi.UI.Exception(printGhciException) where
+
+import GHC.Prelude
+import GHC.Utils.Logger
+import Control.Monad.IO.Class
+import GHC.Driver.Session
+import GHC.Types.SourceError
+import GHC.Driver.Errors.Types
+import GHC.Types.Error
+import GHC.Driver.Config.Diagnostic
+import GHC.Driver.Errors
+
+-- | Print the all diagnostics in a 'SourceError'.  Specialised for GHCi error reporting
+-- for some error messages.
+printGhciException :: (HasLogger m, MonadIO m, HasDynFlags m) => SourceError -> m ()
+printGhciException err = do
+  dflags <- getDynFlags
+  logger <- getLogger
+  let !diag_opts = initDiagOpts dflags
+      !print_config = initPrintConfig dflags
+  liftIO $ printMessages logger print_config diag_opts (GHCiMessage <$> (srcErrorMessages err))
+
+
+newtype GHCiMessage = GHCiMessage { getGhciMessage :: GhcMessage }
+
+instance Diagnostic GHCiMessage where
+  type DiagnosticOpts GHCiMessage = DiagnosticOpts GhcMessage
+
+  defaultDiagnosticOpts = defaultDiagnosticOpts @GhcMessage
+
+  diagnosticMessage opts (GHCiMessage msg) = diagnosticMessage opts msg
+
+  diagnosticReason (GHCiMessage msg) = diagnosticReason msg
+
+  diagnosticHints (GHCiMessage msg) = diagnosticHints msg
+
+  diagnosticCode (GHCiMessage msg)  = diagnosticCode msg
+
+
