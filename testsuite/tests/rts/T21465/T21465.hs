6ac4ca8f2ccc05dfb75d4625431181d69bbfd41d
Ben Gamari <ben@smart-cactus.org>
2023-03-28 09:52:34 -0400

    testsuite: Fix racing prints in T21465
    As noted in #23155, we previously failed to add flushes necessary to
    ensure predictable output.
    
    Fixes #23155.

--- testsuite/tests/rts/T21465/T21465.hs
+++ testsuite/tests/rts/T21465/T21465.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Main where
 
+import System.IO
+
 foreign import ccall "test_c" testC :: IO ()
 
 helper :: IO ()
-helper = putStrLn "This is the helper function"
+helper = putStrLn "This is the helper function" >> hFlush stdout
 
 foreign export ccall helper :: IO ()
 
