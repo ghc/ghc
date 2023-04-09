dbffeb4b855d1a7188fa5ae69d85c5a6657c7520
Sven Tennie <sven.tennie@gmail.com>
2023-04-09 16:27:17 +0000

    Use function level pattern match

--- libraries/ghc-heap/GHC/Exts/Stack.hs
+++ libraries/ghc-heap/GHC/Exts/Stack.hs
@@ -1,35 +1,34 @@
 {-# LANGUAGE CPP #-}
 #if MIN_TOOL_VERSION_ghc(9,7,0)
 {-# LANGUAGE RecordWildCards #-}
-module GHC.Exts.Stack (
-     -- * Stack inspection
-      decodeStack
-    , stackFrameSize
-                     )
+
+module GHC.Exts.Stack
+  ( -- * Stack inspection
+    decodeStack,
+    stackFrameSize,
+  )
 where
+
 import GHC.Exts.Heap.Closures
-import GHC.Exts.Stack.Decode
 import GHC.Exts.Stack.Constants
+import GHC.Exts.Stack.Decode
 import Prelude
 
--- TODO: Pattern match may move to function arguments
 stackFrameSize :: StackFrame -> Int
-stackFrameSize =
-  \c ->
-    case c of
-      UpdateFrame {} -> sizeStgUpdateFrame
-      CatchFrame {} -> sizeStgCatchFrame
-      CatchStmFrame {} -> sizeStgCatchSTMFrame
-      CatchRetryFrame {} -> sizeStgCatchRetryFrame
-      AtomicallyFrame {} -> sizeStgAtomicallyFrame
-      RetSmall {..} -> sizeStgClosure + length stack_payload
-      RetBig {..} -> sizeStgClosure + length stack_payload
-      RetFun {..} -> sizeStgRetFunFrame + length retFunPayload
-      -- The one additional word is a pointer to the StgBCO in the closure's payload
-      RetBCO {..} -> sizeStgClosure + 1 + length bcoArgs
-      -- The one additional word is a pointer to the next stack chunk
-      UnderflowFrame {} -> sizeStgClosure + 1
-      _ -> error "Unexpected closure type"
+stackFrameSize (UpdateFrame {}) = sizeStgUpdateFrame
+stackFrameSize (CatchFrame {}) = sizeStgCatchFrame
+stackFrameSize (CatchStmFrame {}) = sizeStgCatchSTMFrame
+stackFrameSize (CatchRetryFrame {}) = sizeStgCatchRetryFrame
+stackFrameSize (AtomicallyFrame {}) = sizeStgAtomicallyFrame
+stackFrameSize (RetSmall {..}) = sizeStgClosure + length stack_payload
+stackFrameSize (RetBig {..}) = sizeStgClosure + length stack_payload
+stackFrameSize (RetFun {..}) = sizeStgRetFunFrame + length retFunPayload
+-- The one additional word is a pointer to the StgBCO in the closure's payload
+stackFrameSize (RetBCO {..}) = sizeStgClosure + 1 + length bcoArgs
+-- The one additional word is a pointer to the next stack chunk
+stackFrameSize (UnderflowFrame {}) = sizeStgClosure + 1
+stackFrameSize _ = error "Unexpected stack frame type"
+
 #else
 module GHC.Exts.Stack where
 #endif
