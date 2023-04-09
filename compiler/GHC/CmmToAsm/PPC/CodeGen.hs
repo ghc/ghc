3d3a5218ad10231bf14f44eace3117137d205290
Peter Trommler <ptrommler@acm.org>
2023-04-09 17:53:54 +0200

    Fix conditional branch in atomicRMW ops

--- compiler/GHC/CmmToAsm/PPC/CodeGen.hs
+++ compiler/GHC/CmmToAsm/PPC/CodeGen.hs
@@ -1224,6 +1224,7 @@ genCCall _ (PrimTarget (MO_AtomicRMW width amop)) [dst] [addr, n]
             AMO_Xor  -> getSomeRegOrImm XOR False reg_dst
       Amode addr_reg addr_code <- getAmodeIndex addr
       lbl_retry <- getBlockIdNat
+      lbl_done <- getBlockIdNat
       return $ n_code `appOL` addr_code
         `appOL` toOL [ HWSYNC
                      , BCC ALWAYS lbl_retry Nothing
@@ -1233,6 +1234,9 @@ genCCall _ (PrimTarget (MO_AtomicRMW width amop)) [dst] [addr, n]
                      , instr
                      , STC fmt reg_dst addr_reg
                      , BCC NE lbl_retry (Just False)
+                     , BCC ALWAYS lbl_done Nothing
+
+                     , NEWBLOCK lbl_done
                      , ISYNC
                      ]
          where
