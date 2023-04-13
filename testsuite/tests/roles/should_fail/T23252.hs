d48fbfea5f7b760ec3d13dd2947257986c095b75
Simon Peyton Jones <simon.peytonjones@gmail.com>
2023-04-13 12:10:29 +0100

    Stop if type constructors have kind errors
    Otherwise we get knock-on errors, such as #23252.
    
    This makes GHC fail a bit sooner, and I have not attempted to add
    recovery code, to add a fake TyCon place of the erroneous one,
    in an attempt to get more type errors in one pass.  We could
    do that (perhaps) if there was a call for it.

--- testsuite/tests/roles/should_fail/T23252.hs
+++ testsuite/tests/roles/should_fail/T23252.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE PolyKinds, DataKinds, ExplicitForAll #-}
+{-# LANGUAGE RoleAnnotations #-}
 
 module T15743 where
 
@@ -7,5 +8,5 @@ import Data.Proxy
 
 data SimilarKind :: forall (c :: k) (d :: k). Proxy c -> Proxy d -> Type
 
-data T k (c :: k) (a :: Proxy c) b (x :: SimilarKind a b)
-
+data T2 k (c :: k) (a :: Proxy c) (b :: Proxy d) (x :: SimilarKind a b)
+type role T2 nominal nominal nominal nominal  -- Too few!
