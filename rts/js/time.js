6c5fcaba04c122c68c16703c2d8349b9cd68f7d2
Josh Meredith <joshmeredith2008@gmail.com>
2023-05-08 14:51:54 +0000

    JS: Implement h$clock_gettime in the JavaScript RTS (#23360)
    (cherry picked from commit 5e3f9bb57680a40f6a9531e41dc2617c5f028e5c)

---
+++ rts/js/time.js
@@ -0,0 +1,20 @@
+function h$clock_gettime(when, p_d, p_o) {
+  var is64 = p_d.i3.length == 4 && p_o == 0;
+  var o  = p_o >> 2;
+  var t  = Date.now ? Date.now() : new Date().getTime();
+  var tf = Math.floor(t / 1000);
+  var tn = 1000000 * (t - (1000 * tf));
+
+  if (is64) {
+    p_d.i3[o]   = tf|0;
+    p_d.i3[o+1] = 0;
+    p_d.i3[o+2] = tn|0;
+    p_d.i3[o+3] = 0;
+  } else {
+    p_d.i3[o]   = tf|0;
+    p_d.i3[o+1] = tn|0;
+  }
+  return 0;
+}
+
+function h$CLOCK_REALTIME() { return 0; }
