zepalmer/intensional-functions-ghc

Make GHC.Types.Id.Make.shouldUnpackTy a bit more clever
As #23307, GHC.Types.Id.Make.shouldUnpackTy was leaving money on the
table, failing to unpack arguments that are perfectly unpackable.

The fix is pretty easy; see Note [Recursive unboxing]

testsuite/tests/simplCore/should_compile/T23307.hs
@@ -1,0 +1,5 @@
+module T23307 where
+
+data List a = Nil | Cons a !(List a)
+data Unconsed a = Unconsed a !(List a)
+data MUnconsed a = No | Yes {-# UNPACK #-} !(Unconsed a)

ghc/ghc

(cherry picked from commit 902f0730b4c50f39b7767a346be324c98bf7a8a6)

testsuite/tests/simplCore/should_compile/T23307.hs
@@ -1,0 +1,5 @@
+module T23307 where
+
+data Stream a = Nil | Cons a !(Stream a)
+data Unconsed a = Unconsed a !(Stream a)
+data MUnconsed a = No | Yes {-# UNPACK #-} !(Unconsed a)