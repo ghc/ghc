5366cd35df5522bd9a1d2897b47280925c3fcff8
sheaf <sam.derbyshire@gmail.com>
2023-05-19 14:36:17 +0200

    Split off TcBinderStack into its own module
    This commit splits off TcBinderStack into its own module,
    to avoid module cycles: we might want to refer to it without also
    pulling in the TcM monad.

--- compiler/GHC/Tc/Types.hs
+++ compiler/GHC/Tc/Types.hs
@@ -109,6 +109,7 @@ import {-# SOURCE #-} GHC.Driver.Hooks
 import GHC.Hs
 
 import GHC.Tc.Utils.TcType
+import GHC.Tc.Types.TcBinder
 import GHC.Tc.Types.Constraint
 import GHC.Tc.Types.Origin
 import GHC.Tc.Types.Evidence
@@ -906,46 +907,6 @@ type TcRef a     = IORef a
 -- ToDo: when should I refer to it as a 'TcId' instead of an 'Id'?
 type TcId        = Id
 type TcIdSet     = IdSet
-
----------------------------
--- The TcBinderStack
----------------------------
-
-type TcBinderStack = [TcBinder]
-   -- This is a stack of locally-bound ids and tyvars,
-   --   innermost on top
-   -- Used only in error reporting (relevantBindings in TcError),
-   --   and in tidying
-   -- We can't use the tcl_env type environment, because it doesn't
-   --   keep track of the nesting order
-
-data TcBinder
-  = TcIdBndr
-       TcId
-       TopLevelFlag    -- Tells whether the binding is syntactically top-level
-                       -- (The monomorphic Ids for a recursive group count
-                       --  as not-top-level for this purpose.)
-
-  | TcIdBndr_ExpType  -- Variant that allows the type to be specified as
-                      -- an ExpType
-       Name
-       ExpType
-       TopLevelFlag
-
-  | TcTvBndr          -- e.g.   case x of P (y::a) -> blah
-       Name           -- We bind the lexical name "a" to the type of y,
-       TyVar          -- which might be an utterly different (perhaps
-                      -- existential) tyvar
-
-instance Outputable TcBinder where
-   ppr (TcIdBndr id top_lvl)           = ppr id <> brackets (ppr top_lvl)
-   ppr (TcIdBndr_ExpType id _ top_lvl) = ppr id <> brackets (ppr top_lvl)
-   ppr (TcTvBndr name tv)              = ppr name <+> ppr tv
-
-instance HasOccName TcBinder where
-    occName (TcIdBndr id _)             = occName (idName id)
-    occName (TcIdBndr_ExpType name _ _) = occName name
-    occName (TcTvBndr name _)           = occName name
 
 -- fixes #12177
 -- Builds up a list of bindings whose OccName has not been seen before
