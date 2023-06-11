4b89bb54a1d1d6a7b30a6bbfd21eed5d85506813
Krzysztof Gogolewski <krzysztof.gogolewski@tweag.io>
2023-06-11 14:16:46 +0200

    Use tcInferFRR to prevent bad generalisation
    Fixes #23176

--- compiler/GHC/Tc/Gen/Bind.hs
+++ compiler/GHC/Tc/Gen/Bind.hs
@@ -1210,20 +1210,10 @@ This check, mkInferredPolyId, is really in the wrong place:
 `inferred_poly_ty` doesn't obey the PKTI and it would be better not to
 generalise it in the first place; see #20686.  But for now it works.
 
-How else could we avoid generalising over escaping type variables? I
-considered:
-
-* Adjust the generalisation in GHC.Tc.Solver to directly check for
-  escaping kind variables; instead, promote or default them. But that
-  gets into the defaulting swamp and is a non-trivial and unforced
-  change, so I have left it alone for now.
-
-* When inferring the type of a binding, in `tcMonoBinds`, we create
-  an ExpSigmaType with `tcInfer`.  If we simply gave it an ir_frr field
-  that said "must have fixed runtime rep", then the kind would be made
-  Concrete; and we never generalise over Concrete variables.  A bit
-  more indirect, but we need the "don't generalise over Concrete variables"
-  stuff anyway.
+I considered adjusting the generalisation in GHC.Tc.Solver to directly check for
+escaping kind variables; instead, promoting or defaulting them. But that
+gets into the defaulting swamp and is a non-trivial and unforced
+change, so I have left it alone for now.
 
 Note [Impedance matching]
 ~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -1312,7 +1302,9 @@ tcMonoBinds is_rec sig_fn no_gen
   , Nothing <- sig_fn name   -- ...with no type signature
   = setSrcSpanA b_loc    $
     do  { ((co_fn, matches'), rhs_ty')
-            <- tcInfer $ \ exp_ty ->
+            <- tcInferFRR (FRRBinder name) $ \ exp_ty ->
+                          -- tcInferFRR: the type of a let-binder must have
+                          -- a fixed runtime rep. See #23176
                        tcExtendBinderStack [TcIdBndr_ExpType name exp_ty NotTopLevel] $
                           -- We extend the error context even for a non-recursive
                           -- function so that in type error messages we show the
@@ -1334,7 +1326,9 @@ tcMonoBinds is_rec sig_fn no_gen
   | NonRecursive <- is_rec   -- ...binder isn't mentioned in RHS
   , all (isNothing . sig_fn) bndrs
   = addErrCtxt (patMonoBindsCtxt pat grhss) $
-    do { (grhss', pat_ty) <- tcInfer $ \ exp_ty ->
+    do { (grhss', pat_ty) <- tcInferFRR FRRPatBind $ \ exp_ty ->
+                          -- tcInferFRR: the type of each let-binder must have
+                          -- a fixed runtime rep. See #23176
                              tcGRHSsPat grhss exp_ty
 
        ; let exp_pat_ty :: Scaled ExpSigmaTypeFRR
