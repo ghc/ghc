2e36d1c5f6a66aa5bf37211ed130a51445b66e12
Ben Gamari <ben@smart-cactus.org>
2023-06-23 14:34:16 -0400

    testsuite: Add AtomicModifyIORef test

---
+++ libraries/base/tests/AtomicModifyIORef.hs
@@ -0,0 +1,21 @@
+import Control.Concurrent
+import Control.Monad
+import Data.IORef
+
+main :: IO ()
+main = do
+    let nThreads = 10
+        nIncrs = 10000000
+
+    ref <- newIORef (42 :: Int)
+    dones <- replicateM nThreads $ do
+        done <- newEmptyMVar
+        forkIO $ do
+           replicateM_ nIncrs $ atomicModifyIORef' ref $ \old -> (old + 1, ())
+           putMVar done ()
+        putStrLn "."
+        return done
+
+    mapM_ takeMVar dones
+    readIORef ref >>= print
+
