887dc4fc5ad033b4dd2537e914d6d4a574b7fe23
Bryan Richter <bryan@haskell.foundation>
2023-06-27 14:51:31 +0300

    Handle unset value in -e context

--- .gitlab/darwin/toolchain.nix
+++ .gitlab/darwin/toolchain.nix
@@ -113,6 +113,8 @@ pkgs.writeTextFile {
     export CABAL="$CABAL_INSTALL"
 
     sdk_path="$(xcrun --sdk macosx --show-sdk-path)"
-    export CONFIGURE_ARGS="$CONFIGURE_ARGS --with-ffi-libraries=$sdk_path/usr/lib --with-ffi-includes=$sdk_path/usr/include/ffi --build=${targetTriple}"
+    : ''${CONFIGURE_ARGS:=}
+    CONFIGURE_ARGS+="''${CONFIGURE_ARGS:+ }--with-ffi-libraries=$sdk_path/usr/lib --with-ffi-includes=$sdk_path/usr/include/ffi --build=${targetTriple}"
+    export CONFIGURE_ARGS
   '';
 }
