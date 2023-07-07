0c2a756eb8a84effa8aba26c8e49c235b371727a
sheaf <sam.derbyshire@gmail.com>
2023-07-07 13:45:12 +0200

    Avoid incomplete record update in Haddock Hoogle
    This commit avoids running into an incomplete record update warning
    in the Hoogle backend of Haddock.
    
    This was only noticed now, because incomplete record updates were broken
    in GHC 9.6. Now that they are fixed, we have to avoid running into them!

--- haddock-api/src/Haddock/Backends/Hoogle.hs
+++ haddock-api/src/Haddock/Backends/Hoogle.hs
@@ -192,7 +192,7 @@ pp_sig dflags names (L _ typ)  =
 
 -- note: does not yet output documentation for class methods
 ppClass :: DynFlags -> TyClDecl GhcRn -> [(Name, DocForDecl Name)] -> [String]
-ppClass dflags decl subdocs =
+ppClass dflags decl@(ClassDecl {}) subdocs =
     (ppDecl ++ ppTyFams) : ppMethods
   where
     ppDecl :: String
@@ -234,7 +234,7 @@ ppClass dflags decl subdocs =
         , nest 4 . vcat . map (Outputable.<> semi) $ elems
         , rbrace
         ]
-
+ppClass _ _non_cls_decl _ = []
 
 ppFam :: DynFlags -> FamilyDecl GhcRn -> [String]
 ppFam dflags decl@(FamilyDecl { fdInfo = info })
