2bb6c062ab2bd8f9bf5d6d6f6a1bfa3935626796
Josh Meredith <joshmeredith2008@gmail.com>
2023-07-10 18:10:38 +1000

    JavaScript: support unicode code points > 2^16 in toJSString using String.fromCodePoint (#23628)
    (cherry picked from commit 09a5c6cccf8f1b517bc01e8cc924e151d9cbae49)

--- testsuite/tests/javascript/js-ffi-string.hs
+++ testsuite/tests/javascript/js-ffi-string.hs
@@ -1,13 +1,49 @@
 import GHC.JS.Prim
+import System.IO
 
 foreign import javascript "((x) => { console.log(x); })"
   log_js_string :: JSVal -> IO ()
 
-foreign import javascript "(() => { return 'a string'; })"
-  a_string :: JSVal
+foreign import javascript "((x, y) => { return x === y; })"
+  eq_JSVal :: JSVal -> JSVal -> Bool
+
+foreign import javascript "(() => { return 'abc\\uD83D\\uDE0A'; })"
+  js_utf16_string :: JSVal
+foreign import javascript "(() => { return 'abc' + String.fromCodePoint(128522); })"
+  js_codepoint_string :: JSVal
+
+-- It's important that this String is defined using a function to avoid rewrite
+-- rules optimising away the use of `toJSString` called on a literal.
+hsString :: String
+hsString = "abc" ++ "\128522"
 
 main :: IO ()
 main = do
-  log_js_string (toJSString "test")
-  putStrLn (fromJSString a_string)
-  putStrLn (fromJSString $ toJSString "test")
+  putStrLn "Does JS `String.fromCodePoint` decode to the expected UTF-16 values? "
+  print (eq_JSVal js_utf16_string js_codepoint_string)
+  hFlush stdout
+  log_js_string js_utf16_string
+  log_js_string js_codepoint_string
+
+  putStrLn "\nDoes `GHC.JS.fromJSString` convert the JavaScript literal string correctly? "
+  print (hsString == fromJSString js_utf16_string)
+  putStrLn hsString
+  putStrLn (fromJSString js_utf16_string)
+
+  putStrLn "\nDoes `GHC.JS.toJSString` convert the Haskell-defined string correctly? "
+  print (eq_JSVal js_utf16_string (toJSString hsString))
+  hFlush stdout
+  log_js_string js_utf16_string
+  log_js_string (toJSString hsString)
+
+  putStrLn "\nDo values survive the Haskell -> JavaScript -> Haskell round-trip? "
+  print (hsString == fromJSString (toJSString hsString))
+  putStrLn hsString
+  putStrLn (fromJSString js_utf16_string)
+
+  putStrLn "\nDo values survive the JavaScript -> Haskell -> JavaScript round-trip? "
+  print (eq_JSVal js_utf16_string (toJSString $ fromJSString js_utf16_string))
+  hFlush stdout
+  log_js_string js_utf16_string
+  log_js_string (toJSString $ fromJSString js_utf16_string)
+
