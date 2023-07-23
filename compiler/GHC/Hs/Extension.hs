66a4a3c39804ff436fcbc3592e6e3b358c65dcb9
Alan Zimmerman <alan.zimm@gmail.com>
2023-07-23 10:08:44 +0100

    EPA HasAnnotation 2

--- compiler/GHC/Hs/Extension.hs
+++ compiler/GHC/Hs/Extension.hs
@@ -108,6 +108,7 @@ type instance Anno Name    = SrcSpanAnnN
 type instance Anno Id      = SrcSpanAnnN
 
 type IsSrcSpanAnn p a = ( Anno (IdGhcP p) ~ SrcSpanAnn' (EpAnn a),
+                          Monoid a,
                           IsPass p)
 
 instance UnXRec (GhcPass p) where
