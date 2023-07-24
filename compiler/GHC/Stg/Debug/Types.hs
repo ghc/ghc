dc68ab808e4ed5b7593a5cab5b6408163fe23e80
Finley McIlwaine <finleymcilwaine@gmail.com>
2023-07-24 17:07:52 -0600

    Allow per constructor refinement of distinct-constructor-tables
    Introduce `-fno-distinct-constructor-tables`. A distinct constructor table
    configuration is built from the combination of flags given, in order. For
    example, to create distinct constructor tables for all constructors except for a
    specific few named `C1`,..., `CN`, pass `-fdistinct-contructor-tables` followed
    by `fno-distinct-constructor-tables=C1,...,CN`. To only generate distinct
    constuctor tables for a few specific constructors and no others, just pass
    `-fdistinct-constructor-tables=C1,...,CN`.
    
    The various configurations of these flags is included in the `DynFlags`
    fingerprints, which should result in the expected recompilation logic.
    
    Adds a test that checks for distinct tables for various given or omitted
    constructors.
    
    Updates CountDepsAst and CountDepsParser tests to account for new dependencies.
    
    Fixes #23703

---
+++ compiler/GHC/Stg/Debug/Types.hs
@@ -0,0 +1,80 @@
+module GHC.Stg.Debug.Types where
+
+import GHC.Prelude
+
+import GHC.Data.FastString
+import GHC.Types.SrcLoc
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+data SpanWithLabel = SpanWithLabel RealSrcSpan LexicalFastString
+
+data StgDebugOpts = StgDebugOpts
+  { stgDebug_infoTableMap              :: !Bool
+  , stgDebug_distinctConstructorTables :: !StgDebugDctConfig
+  }
+
+-- | Configuration describing which constructors should be given distinct info
+-- tables for each usage.
+data StgDebugDctConfig =
+    -- | Create distinct constructor tables for each usage of any data
+    -- constructor.
+    --
+    -- This is the behavior if just @-fdistinct-constructor-tables@ is supplied.
+    All
+
+    -- | Create distinct constructor tables for each usage of only these data
+    -- constructors.
+    --
+    -- This is the behavior if @-fdistinct-constructor-tables=C1,...,CN@ is
+    -- supplied.
+  | Only !(Set String)
+
+    -- | Create distinct constructor tables for each usage of any data
+    -- constructor except these ones.
+    --
+    -- This is the behavior if @-fdistinct-constructor-tables@ and
+    -- @-fno-distinct-constructor-tables=C1,...,CN@ is given.
+  | AllExcept !(Set String)
+
+    -- | Do not create distinct constructor tables for any data constructor.
+    --
+    -- This is the behavior if no @-fdistinct-constructor-tables@ is given (or
+    -- @-fno-distinct-constructor-tables@ is given).
+  | None
+
+-- | Given a distinct constructor tables configuration and a set of constructor
+-- names that we want to generate distinct info tables for, create a new
+-- configuration which includes those constructors.
+--
+-- If the given set is empty, that means the user has entered
+-- @-fdistinct-constructor-tables@ with no constructor names specified, and
+-- therefore we consider that an 'All' configuration.
+dctConfigPlus :: StgDebugDctConfig -> Set String -> StgDebugDctConfig
+dctConfigPlus cfg cs
+    | Set.null cs = All
+    | otherwise =
+        case cfg of
+          All -> All
+          Only cs' -> Only $ Set.union cs' cs
+          AllExcept cs' -> AllExcept $ Set.difference cs' cs
+          None -> Only cs
+
+-- | Given a distinct constructor tables configuration and a set of constructor
+-- names that we /do not/ want to generate distinct info tables for, create a
+-- new configuration which excludes those constructors.
+--
+-- If the given set is empty, that means the user has entered
+-- @-fno-distinct-constructor-tables@ with no constructor names specified, and
+-- therefore we consider that a 'None' configuration.
+dctConfigMinus :: StgDebugDctConfig -> Set String -> StgDebugDctConfig
+dctConfigMinus cfg cs
+    | Set.null cs = None
+    | otherwise =
+        case cfg of
+          All -> AllExcept cs
+          Only cs' -> Only $ Set.difference cs' cs
+          AllExcept cs' -> AllExcept $ Set.union cs' cs
+          None -> None
+
