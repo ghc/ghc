12481372dd87ba5983c0ab02e41617d5fc976077
Josh Meredith <joshmeredith2008@gmail.com>
2023-08-07 20:44:25 +1000

    lint

--- compiler/GHC/Types/Unique/DFM.hs
+++ compiler/GHC/Types/Unique/DFM.hs
@@ -67,6 +67,8 @@ module GHC.Types.Unique.DFM (
         nonDetStrictFoldUDFM,
         unsafeCastUDFMKey,
         alwaysUnsafeUfmToUdfm,
+        addListToUDFM_Directly,
+        insertUDFMIntoLeft,
     ) where
 
 import GHC.Prelude
@@ -267,7 +269,7 @@ plusUDFM_C f udfml@(UDFM _ i) udfmr@(UDFM _ j)
 -- set.
 
 plusUDFM :: UniqDFM key elt -> UniqDFM key elt -> UniqDFM key elt
-plusUDFM udfml@(UDFM xs i) udfmr@(UDFM ys j)
+plusUDFM (UDFM xs i) (UDFM ys j)
   = UDFM (MS.unionMap xs ys (\(TaggedVal x n) -> TaggedVal x (n + i))) (i + j)
 --  = UDFM (xs `M.union` M.map (\(TaggedVal x n) -> TaggedVal x (n + i)) ys) (i + j)
   -- we will use the upper bound on the tag as a proxy for the set size,
