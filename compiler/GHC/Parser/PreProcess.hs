29055cf03a6fd5ea83ed88c353979ca1144ef42b
Alan Zimmerman <alan.zimm@gmail.com>
2023-10-01 19:50:06 +0100

    Accumulate CPP continuations, process when ready
    Can be simplified further, we only need one CPP token

--- compiler/GHC/Parser/PreProcess.hs
+++ compiler/GHC/Parser/PreProcess.hs
@@ -4,8 +4,8 @@
 {-# LANGUAGE BangPatterns #-}
 
 module GHC.Parser.PreProcess (
-    ppLexer,
-    ppLexerDbg,
+    -- ppLexer,
+    -- ppLexerDbg,
     lexer,
     lexerDbg,
 ) where
@@ -32,114 +32,114 @@ lexerDbg = Lexer.lexerDbg
 -- -- lexer = ppLexerDbg
 -- lexerDbg = ppLexerDbg
 
-ppLexer, ppLexerDbg :: Bool -> (Located Token -> P a) -> P a
--- Use this instead of 'lexer' in GHC.Parser to dump the tokens for debugging.
-ppLexerDbg queueComments cont = ppLexer queueComments contDbg
-  where
-    contDbg tok = trace ("pptoken: " ++ show (unLoc tok)) (cont tok)
-ppLexer queueComments cont =
-    Lexer.lexer
-        queueComments
-        ( \tk ->
-            let
-                -- contInner t = (trace ("ppLexer: tk=" ++ show (unLoc tk, unLoc t)) cont) t
-                contInner t = cont t
-                contPush = pushContext (unLoc tk) >> contIgnoreTok tk
-                contIgnoreTok (L l tok) = do
-                    case l of
-                        RealSrcSpan r (Strict.Just b) -> Lexer.queueIgnoredToken (L (PsSpan r b) tok)
-                        _ -> return ()
-                    ppLexer queueComments cont
-             in
-                case tk of
-                    L _ (ITcppDefine s) -> do
-                        ppDefine (trace ("ppDefine:" ++ show s) s)
-                        popContext
-                        contIgnoreTok tk
-                    L _ (ITcppIf _) -> contPush
-                    L _ (ITcppIfdef s) -> do
-                        defined <- ppIsDefined s
-                        -- setAccepting defined
-                        setAccepting (trace ("ifdef:" ++ show (s, defined)) defined)
-                        contIgnoreTok tk
-                    L _ (ITcppIfndef s) -> do
-                        defined <- ppIsDefined s
-                        -- setAccepting (not defined)
-                        setAccepting (trace ("ifdef:" ++ show (s, defined)) (not defined))
-                        contIgnoreTok tk
-                    L _ ITcppElse -> do
-                        preprocessElse
-                        contIgnoreTok tk
-                    L _ ITcppEndif -> do
-                        preprocessEnd
-                        contIgnoreTok tk
-                    _ -> do
-                        state <- getCppState
-                        -- case (trace ("CPP state:" ++ show state) state) of
-                        case state of
-                            CppIgnoring -> contIgnoreTok tk
-                            _ -> contInner tk
-        )
+-- ppLexer, ppLexerDbg :: Bool -> (Located Token -> P a) -> P a
+-- -- Use this instead of 'lexer' in GHC.Parser to dump the tokens for debugging.
+-- ppLexerDbg queueComments cont = ppLexer queueComments contDbg
+--   where
+--     contDbg tok = trace ("pptoken: " ++ show (unLoc tok)) (cont tok)
+-- ppLexer queueComments cont =
+--     Lexer.lexer
+--         queueComments
+--         ( \tk ->
+--             let
+--                 -- contInner t = (trace ("ppLexer: tk=" ++ show (unLoc tk, unLoc t)) cont) t
+--                 contInner t = cont t
+--                 contPush = pushContext (unLoc tk) >> contIgnoreTok tk
+--                 contIgnoreTok (L l tok) = do
+--                     case l of
+--                         RealSrcSpan r (Strict.Just b) -> Lexer.queueIgnoredToken (L (PsSpan r b) tok)
+--                         _ -> return ()
+--                     ppLexer queueComments cont
+--              in
+--                 case tk of
+--                     L _ (ITcppDefine s) -> do
+--                         ppDefine (trace ("ppDefine:" ++ show s) s)
+--                         popContext
+--                         contIgnoreTok tk
+--                     L _ (ITcppIf _) -> contPush
+--                     L _ (ITcppIfdef s) -> do
+--                         defined <- ppIsDefined s
+--                         -- setAccepting defined
+--                         setAccepting (trace ("ifdef:" ++ show (s, defined)) defined)
+--                         contIgnoreTok tk
+--                     L _ (ITcppIfndef s) -> do
+--                         defined <- ppIsDefined s
+--                         -- setAccepting (not defined)
+--                         setAccepting (trace ("ifdef:" ++ show (s, defined)) (not defined))
+--                         contIgnoreTok tk
+--                     L _ ITcppElse -> do
+--                         preprocessElse
+--                         contIgnoreTok tk
+--                     L _ ITcppEndif -> do
+--                         preprocessEnd
+--                         contIgnoreTok tk
+--                     _ -> do
+--                         state <- getCppState
+--                         -- case (trace ("CPP state:" ++ show state) state) of
+--                         case state of
+--                             CppIgnoring -> contIgnoreTok tk
+--                             _ -> contInner tk
+--         )
 
-preprocessElse :: P ()
-preprocessElse = do
-    accepting <- getAccepting
-    setAccepting (not accepting)
+-- preprocessElse :: P ()
+-- preprocessElse = do
+--     accepting <- getAccepting
+--     setAccepting (not accepting)
 
-preprocessEnd :: P ()
-preprocessEnd = do
-    -- TODO: nested context
-    setAccepting True
+-- preprocessEnd :: P ()
+-- preprocessEnd = do
+--     -- TODO: nested context
+--     setAccepting True
 
--- ---------------------------------------------------------------------
--- Preprocessor state functions
+-- -- ---------------------------------------------------------------------
+-- -- Preprocessor state functions
 
-data CppState
-    = CppIgnoring
-    | CppInDefine
-    | CppInIfdef
-    | CppInIfndef
-    | CppNormal
-    deriving (Show)
+-- data CppState
+--     = CppIgnoring
+--     | CppInDefine
+--     | CppInIfdef
+--     | CppInIfndef
+--     | CppNormal
+--     deriving (Show)
 
-getCppState :: P CppState
-getCppState = do
-    context <- peekContext
-    accepting <- getAccepting
-    case context of
-        ITcppDefine _ -> return CppInDefine
-        ITcppIfndef _ -> return CppInIfndef
-        ITcppIfdef _ -> return CppInIfdef
-        _ ->
-            if accepting
-                then return CppNormal
-                else return CppIgnoring
+-- getCppState :: P CppState
+-- getCppState = do
+--     context <- peekContext
+--     accepting <- getAccepting
+--     case context of
+--         ITcppDefine _ -> return CppInDefine
+--         ITcppIfndef _ -> return CppInIfndef
+--         ITcppIfdef _ -> return CppInIfdef
+--         _ ->
+--             if accepting
+--                 then return CppNormal
+--                 else return CppIgnoring
 
 -- pp_context stack start -----------------
 
-pushContext :: Token -> P ()
-pushContext new =
-    P $ \s -> POk s{pp = (pp s){pp_context = new : pp_context (pp s)}} ()
+-- pushContext :: Token -> P ()
+-- pushContext new =
+--     P $ \s -> POk s{pp = (pp s){pp_context = new : pp_context (pp s)}} ()
 
-popContext :: P ()
-popContext =
-    P $ \s ->
-        let
-            new_context = case pp_context (pp s) of
-                [] -> []
-                (_ : t) -> t
-         in
-            POk s{pp = (pp s){pp_context = (trace ("pop:new_context:" ++ show new_context) new_context)}} ()
+-- popContext :: P ()
+-- popContext =
+--     P $ \s ->
+--         let
+--             new_context = case pp_context (pp s) of
+--                 [] -> []
+--                 (_ : t) -> t
+--          in
+--             POk s{pp = (pp s){pp_context = (trace ("pop:new_context:" ++ show new_context) new_context)}} ()
 
-peekContext :: P Token
-peekContext =
-    P $ \s ->
-        let
-            r = case pp_context (pp s) of
-                [] -> ITeof -- Anthing really, for now, except a CPP one
-                (h : _) -> h
-         in
-            POk s r
+-- peekContext :: P Token
+-- peekContext =
+--     P $ \s ->
+--         let
+--             r = case pp_context (pp s) of
+--                 [] -> ITeof -- Anthing really, for now, except a CPP one
+--                 (h : _) -> h
+--          in
+--             POk s r
 
 setAccepting :: Bool -> P ()
 setAccepting on =
