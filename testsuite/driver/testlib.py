0c92f6d99187f4f5d498f9d0ed88b76e56253d29
Ben Gamari <ben@smart-cactus.org>
2023-10-03 12:01:18 -0400

    testsuite: Fix invalid string literal
    `"\+"` is not a valid Python string; this should have been `r"\+"`.

--- testsuite/driver/testlib.py
+++ testsuite/driver/testlib.py
@@ -1382,7 +1382,7 @@ def normalise_win32_io_errors(name, opts):
 def normalise_version_( *pkgs ):
     def normalise_version__( str ):
         # (name)(-version)(-hash)(-components)
-        return re.sub('(' + '|'.join(map(re.escape,pkgs)) + r')-[0-9.]+(-[0-9a-zA-Z+]+)?(-[0-9a-zA-Z]+)?',
+        return re.sub('(' + '|'.join(map(re.escape,pkgs)) + r')-[0-9.]+(-[0-9a-zA-Z\+]+)?(-[0-9a-zA-Z]+)?',
                       r'\1-<VERSION>-<HASH>', str)
     return normalise_version__
 
