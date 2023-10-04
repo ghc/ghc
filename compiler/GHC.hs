b38f1dcb6cd329a339ffad30e005ed052c9e2049
Alan Zimmerman <alan.zimm@gmail.com>
2023-10-04 23:27:25 +0100

    Process nested include files
    Also move PpState out of Lexer.x, so it is easy to evolve it in a ghci
    session, loading utils/check-cpp/Main.hs

--- compiler/GHC.hs
+++ compiler/GHC.hs
@@ -368,7 +368,7 @@ import GHCi.RemoteTypes
 
 import qualified GHC.Parser as Parser
 import GHC.Parser.Lexer hiding (initParserState)
-import GHC.Parser.PreProcess   (initParserState)
+import GHC.Parser.PreProcess   (initParserState, initPpState)
 import GHC.Parser.Annotation
 import GHC.Parser.Utils
 
