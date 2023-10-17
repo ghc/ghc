f6bc045f57a1479d8ac80443ae02a6220fb924a2
Sebastian Graf <sebastian.graf@kit.edu>
2023-10-17 10:20:16 +0200

    perf tests: Move comments into new `Note [Sensitivity to unique increment]` (#19414)
    And additionally to T12545, link from T8095, T13386 to this new Note.

--- testsuite/tests/perf/compiler/T13386.hs
+++ testsuite/tests/perf/compiler/T13386.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds, TypeApplications, TypeFamilies, TypeOperators, UndecidableInstances #-}
 {-# OPTIONS_GHC -O0 -freduction-depth=500 #-}
-
+-- Subject to Note [Sensitivity to unique increment] with spread of 1.5%
 module T13386 where
 
 import GHC.TypeLits
