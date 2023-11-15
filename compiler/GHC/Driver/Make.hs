efc34462648836a98083e1591f575dfc75576dbb
Zubin Duggal <zubin.duggal@gmail.com>
2023-11-15 17:15:06 +0530

    driver: Ensure we actually clear the interactive context before reloading
    Previously we called discardIC, but immediately after set the session
    back to an old HscEnv that still contained the IC
    
    Partially addresses #24107
    Fixes #23405
    
    (cherry picked from commit 58d56644c54b221f265b739829e53f9f5e1216d3)

--- compiler/GHC/Driver/Make.hs
+++ compiler/GHC/Driver/Make.hs
@@ -772,6 +772,7 @@ load' mhmi_cache how_much diag_wrapper mHscMessage mod_graph = do
 
     let pruneHomeUnitEnv hme = hme { homeUnitEnv_hpt = emptyHomePackageTable }
     setSession $ discardIC $ hscUpdateHUG (unitEnv_map pruneHomeUnitEnv) hsc_env
+    hsc_env <- getSession
 
     -- Unload everything
     liftIO $ unload interp hsc_env
@@ -781,7 +782,6 @@ load' mhmi_cache how_much diag_wrapper mHscMessage mod_graph = do
 
     worker_limit <- liftIO $ mkWorkerLimit dflags
 
-    setSession $ hscUpdateHUG (unitEnv_map pruneHomeUnitEnv) hsc_env
     (upsweep_ok, new_deps) <- withDeferredDiagnostics $ do
       hsc_env <- getSession
       liftIO $ upsweep worker_limit hsc_env mhmi_cache diag_wrapper mHscMessage (toCache pruned_cache) build_plan
