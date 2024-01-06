38ce6f61aaaf6d42a3be6eebc811e4ec7bcddee0
Sebastian Graf <sgraf1337@gmail.com>
2024-01-06 20:23:00 +0100

    Write a denotationaler interpreter for Core

---
+++ compiler/GHC/Core/Semantics.hs
@@ -0,0 +1,316 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -fexpose-all-unfoldings #-} -- for specialisation
+-- {-# OPTIONS_GHC -fdefer-type-errors #-}
+-- {-# OPTIONS_GHC -Wwarn #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module GHC.Core.Semantics where
+
+import GHC.Prelude
+
+import GHC.Builtin.Uniques
+
+import GHC.Core
+import GHC.Core.Coercion
+import GHC.Core.DataCon
+
+import qualified GHC.Data.Word64Map as WM
+
+import GHC.Types.Literal
+import GHC.Types.Id
+import GHC.Types.Name
+import GHC.Types.Var.Env
+import GHC.Types.Unique.Set
+
+import GHC.Utils.Misc
+import GHC.Utils.Outputable
+
+import Control.Monad
+import Control.Monad.Trans.State
+import Data.Word
+import GHC.Core.Utils hiding (findAlt)
+import GHC.Core.Type
+import GHC.Builtin.PrimOps
+import GHC.Builtin.Types
+import GHC.Types.Var
+import GHC.Core.TyCo.Rep
+import GHC.Core.FVs
+import GHC.Core.Class
+import GHC.Types.Id.Info
+
+data Event = Lookup Id | LookupArg CoreExpr | Update | App1 | App2 | Case1 | Case2 | Let1
+
+class Trace d where
+  step :: Event -> d -> d
+
+-- A slight extension of the Domain type from the paper.
+-- Note that the 'Id's bear no semantic significance: The `Domain (D τ)`
+-- instance simply ignores them. They are needed for analyses and debugging, however.
+class Domain d where
+  stuck :: d
+  erased :: d -- Think of it like coercionToken#
+  lit :: Literal -> d
+  global :: Id -> d
+  classOp :: Id -> Class -> d
+  primOp :: Id -> PrimOp -> d
+  fun :: Id -> (d -> d) -> d
+  con :: DataCon -> [d] -> d
+  apply :: d -> d -> d
+  applyTy :: d -> d -- URGHHH otherwise we have no easy way to discern Type Apps
+  select :: d -> ((CoreExpr -> Bool) -> Bool) -> Id -> [DAlt d] -> d
+  keepAlive :: [d] -> d -- Used for coercion FVs, unfolding and RULE FVs. No simple semantic description for those; pretend that they may or may not be seq'd.
+  seq_ :: d -> d -> d -- The primitive one. Just like `select a (const False) wildCardId [(DEFAULT, [], \_a _ds -> b)]`, but we don't have available the type of the LHS.
+type DAlt d = (AltCon, [Id], d -> [d] -> d)
+
+
+data BindHint = BindArg Id | BindLet CoreBind
+class HasBind d where
+  bind :: BindHint -> [[d] -> d] -> ([d] -> d) -> d
+    -- NB: The `BindHint` bears no semantic sigificance:
+    --     `HasBind (D (ByNeed T))` does not look at it.
+    --     Still useful for analyses!
+
+feignBndr :: Name -> PiTyBinder -> Var
+feignBndr n (Anon (Scaled mult ty) _) = mkLocalIdOrCoVar n mult ty
+feignBndr n (Named (Bndr tcv _)) = tcv `setVarName` n
+
+feignId :: Name -> Type -> Id
+feignId n ty = mkLocalIdOrCoVar n ManyTy ty
+
+-- The following does not work, because we need the `idType` of `a` in `select`:
+-- seq_ :: Domain d => d -> d -> d
+-- seq_ a b = select a (const False) wildCardId [(DEFAULT, [], \_a _ds -> b)]
+--
+-- wildCardId :: Id
+-- wildCardId = feignBndr wildCardName (Anon (Scaled ManyTy panicType) FTF_T_T)
+--
+-- panicType :: HasCallStack => Type
+-- panicType = panic "The seq_ case binder should be dead, we don't need its type"
+
+anfise :: (Trace d, Domain d, HasBind d) => Name -> CoreExpr -> IdEnv d -> (d -> d) -> d
+anfise _ (Lit l) _ k = k (lit l)
+anfise _ (Var x) env k = evalVar x env k
+anfise _ (Coercion co) env k = keepAliveCo co env `seq_` k erased
+anfise _ (Type _ty) _ k = k erased
+anfise x (Tick _t e) env k = anfise x e env k
+anfise x (Cast e co) env k = keepAliveCo co env `seq_` anfise x e env k
+anfise x e env k = bind (BindArg (feignId x (exprType e))) [\_ -> eval e env]
+                   (\ds -> let d = step (LookupArg e) (only ds) in
+                           if isUnliftedType (exprType e) && not (exprOkForSpeculation e)
+                             then d `seq_` k d
+                             else k d)
+
+anfiseMany :: (Trace d, Domain d, HasBind d) => [CoreExpr] -> IdEnv d -> ([d] -> d) -> d
+anfiseMany es env k = go (zip localNames es) []
+  where
+    go [] ds = k (reverse ds)
+    go ((x,e):es) ds = anfise x e env $ \d -> go es (d:ds)
+
+keepAliveVars :: Domain d => [Id] -> IdEnv d -> d
+-- Make sure that the given (local) Ids are all "alive", that is, in scope and
+-- reachable through `keepAlive` (which itself is a rather abstract concept).
+keepAliveVars xs env | Just ds <- traverse (lookupVarEnv env) xs = keepAlive ds
+                     | otherwise                                 = stuck
+
+keepAliveCo :: Domain d => Coercion -> IdEnv d -> d
+-- Coercions are ultimately erased to `coercionToken#` because they are
+-- irrelevant to runtime behavior (of a /well-typed/ program).
+-- Nevertheless, they have a static semantics that requires its free variables
+-- to be present; otherwise the coercion is considered stuck.
+keepAliveCo co = keepAliveVars (nonDetEltsUniqSet $ coVarsOfCo co)
+
+keepAliveUnfRules :: Domain d => Id -> IdEnv d -> d
+-- ^ `keepAlive` the free Ids of an Id's unfolding and RULE RHSs.
+keepAliveUnfRules x = keepAliveVars (nonDetEltsUniqSet $ bndrRuleAndUnfoldingIds x)
+
+evalVar :: (Trace d, Domain d, HasBind d) => Var -> IdEnv d -> (d -> d) -> d
+evalVar x env k = case idDetails x of
+  _ | isTyVar x    -> erased
+  DataConWorkId dc -> k (con dc []) -- TODO
+  DataConWrapId _  -> -- pprTrace "unfolding wrapper" (ppr x $$ ppr (unfoldingTemplate (idUnfolding x))) $
+                      k (eval (unfoldingTemplate (idUnfolding x)) emptyVarEnv)
+  PrimOpId op _    -> k (primOp x op)
+  ClassOpId cls _  -> k (classOp x cls)
+  _ | isGlobalId x -> k (global x)
+  _                -> case lookupVarEnv env x of
+                        Just d -> k d
+                        _      -> stuck -- Scoping error. Actually ruled out by the Core type system
+
+eval :: (Trace d, Domain d, HasBind d) => CoreExpr -> IdEnv d -> d
+eval (Coercion co) env = keepAliveCo co env
+eval (Type _ty) _ = erased
+eval (Lit l) _ = lit l
+eval (Tick _t e) env = eval e env
+eval (Cast e _co) env = eval e env
+eval (Var x) env = evalVar x env id
+eval (Lam x e) env = fun x (\d -> step App2 (eval e (extendVarEnv env x d)))
+eval e@App{} env
+  | Var v <- f, Just dc <- isDataConWorkId_maybe v
+  = anfiseMany as env $ \es -> case compare (dataConRepArity dc) (length es) of
+      EQ -> con dc es
+      GT -> stuck                                         -- oversaturated  => stuck
+      LT -> mkPap rest_bndrs $ \etas -> con dc (es ++ etas) -- undersaturated => PAP
+        where
+          rest_bndrs = dropList es (fst $ splitPiTys (dataConRepType dc))
+  | otherwise
+  = anfiseMany (f:as) env $ \(df:das) -> -- NB: anfise is a no-op for Vars
+      go df (zipWith (\d a -> (d, isTypeArg a)) das as)
+  where
+    (f, as) = collectArgs e
+    go df [] = df
+    go df ((d,is_ty):ds) = go (step App1 $ app df is_ty d) ds
+    app df {-isTypeArg=-}True  _da = applyTy df -- There must be a better way...
+    app df               False da  = apply   df da
+eval (Let b@(NonRec x rhs) body) env =
+  bind (BindLet b)
+       -- See Note [Absence analysis for stable unfoldings and RULES]
+       [\_  -> keepAliveUnfRules x env `seq_`
+               eval rhs env]
+       (\ds -> step Let1 (eval body (extendVarEnv env x (step (Lookup x) (only ds)))))
+eval (Let b@(Rec binds) body) env =
+  bind (BindLet b)
+       [\ds -> keepAliveUnfRules x env `seq_`
+               eval rhs  (new_env ds)  | (x,rhs) <- binds]
+       (\ds -> step Let1 (eval body (new_env ds)))
+  where
+    xs = map fst binds
+    new_env ds = extendVarEnvList env (zipWith (\x d -> (x, step (Lookup x) d)) xs ds)
+eval (Case e b _ty alts) env = step Case1 $
+  select (eval e env) ($ e) b
+         [ (con, xs, cont xs rhs) | Alt con xs rhs <- alts ]
+  where
+    cont xs rhs scrut ds = step Case2 $ eval rhs (extendVarEnvList env (zipEqual "eval Case{}" (b:xs) (scrut:ds)))
+
+mkPap :: (Trace d, Domain d) => [PiTyBinder] -> ([d] -> d) -> d
+mkPap arg_bndrs f = go [] (zipWith feignBndr localNames arg_bndrs)
+  where
+    go ds []     = f (reverse ds)
+    go ds (x:xs) = fun x (\d -> step App2 $ go (d:ds) xs)
+
+x1,x2 :: Name
+localNames :: [Name]
+localNames@(x1:x2:_) = [ mkSystemName (mkBuiltinUnique i) (mkVarOcc "local") | i <- [0..] ]
+
+
+-- By-need semantics, from the paper
+
+data T v = Step Event (T v) | Ret v
+  deriving Functor
+instance Applicative T where pure = Ret; (<*>) = ap
+instance Monad T where Ret a >>= f = f a; Step ev t >>= f = Step ev (t >>= f)
+instance Trace (T v) where step = Step
+
+type D τ = τ (Value τ)
+data Value τ
+  = Stuck
+  | Erased
+  | Litt Literal
+  | Fun (D τ -> D τ)
+  | Con DataCon [D τ]
+
+instance (Trace (D τ), Monad τ) => Domain (D τ) where
+  keepAlive _ = return Erased
+  stuck = return Stuck
+  erased = return Erased
+  lit l = return (Litt l)
+  fun _x f = return (Fun f)
+  con k ds = return (Con k ds)
+  apply d a = d >>= \case Fun f -> f a; _ -> stuck
+  applyTy d = d
+  select d _f _b fs = d >>= \v -> case v of
+    Stuck                                                    -> stuck
+    Con k ds | Just (_con, _xs, f) <- findAlt (DataAlt k) fs -> f (return v) ds
+    Litt l   | Just (_con, _xs, f) <- findAlt (LitAlt l)  fs -> f (return v) []
+    _        | Just (_con, _xs, f) <- findAlt DEFAULT     fs -> f (return v) []
+    _                                                        -> stuck
+  seq_ a b = a >> b -- The caller may want to insert steps here... Not sure
+  global _        = return Stuck -- For now; looking at the unfolding would need to call `eval`
+  classOp _x _cls = return Stuck -- For now; looking at the unfolding would need to call `eval`
+  primOp _x op = case op of
+    IntAddOp -> intop (+)
+    IntMulOp -> intop (*)
+    IntRemOp -> intop rem
+    _        -> stuck
+    where
+      intop op = binop int_ty int_ty $ \v1 v2 -> case (v1,v2) of
+        (Litt (LitNumber LitNumInt i1), Litt (LitNumber LitNumInt i2))
+          -> Litt (LitNumber LitNumInt (i1 `op` i2))
+        _ -> Stuck
+      binop ty1 ty2 f = mkPap [ty1,ty2] $ \[d1,d2] -> f <$> d1 <*> d2
+      int_ty = Anon (Scaled ManyTy intTy) FTF_T_T
+
+-- The following function was copy and pasted from GHC.Core.Utils.findAlt:
+findAlt :: AltCon -> [DAlt d] -> Maybe (DAlt d)
+    -- A "Nothing" result *is* legitimate
+    -- See Note [Unreachable code]
+findAlt con alts
+  = case alts of
+        (deflt@(DEFAULT, _, _):alts) -> go alts (Just deflt)
+        _                            -> go alts Nothing
+  where
+    go []                        deflt = deflt
+    go (alt@(con1, _, _) : alts) deflt
+      = case con `cmpAltCon` con1 of
+          LT -> deflt   -- Missed it already; the alts are in increasing order
+          EQ -> Just alt
+          GT -> go alts deflt
+
+-- By-need semantics, straight from the paper
+
+type Addr = Word64
+type Heap τ = WM.Word64Map (D τ)
+newtype ByNeed τ v = ByNeed { runByNeed :: StateT (Heap (ByNeed τ)) τ v }
+  deriving (Functor, Applicative, Monad)
+
+instance (forall v. Trace (τ v)) => Trace (ByNeed τ v) where
+  step ev (ByNeed (StateT m)) = ByNeed $ StateT $ step ev . m
+
+fetch :: Monad τ => Addr -> D (ByNeed τ)
+fetch a = ByNeed get >>= \μ -> μ WM.! a
+
+memo :: forall τ. (Monad τ, forall v. Trace (τ v)) => Addr -> D (ByNeed τ) -> D (ByNeed τ)
+memo a d = d >>= ByNeed . StateT . upd
+  where upd Stuck μ = return (Stuck :: Value (ByNeed τ), μ)
+        upd v     μ = step Update (return (v, WM.insert a (memo a (return v)) μ))
+
+freeList :: Heap τ -> [Addr]
+freeList μ = [a..]
+  where a = case WM.lookupMax μ of Just (a,_) -> a+1; _ -> 0
+
+instance (Monad τ, forall v. Trace (τ v)) => HasBind (D (ByNeed τ)) where
+  bind _hint rhss body = do
+    as <- take (length rhss) . freeList <$> ByNeed get
+    let ds = map fetch as
+    ByNeed $ modify (\μ -> foldr (\(a,rhs) -> WM.insert a (memo a (rhs ds))) μ (zip as rhss))
+    body ds
+
+evalByNeed :: CoreExpr -> T (Value (ByNeed T), Heap (ByNeed T))
+evalByNeed e = runStateT (runByNeed (eval e emptyVarEnv)) WM.empty
+
+-- Boilerplate
+instance Outputable Event where
+  ppr (Lookup n) = text "Lookup" <> parens (ppr n)
+  ppr (LookupArg e) = text "LookupArg" <> parens (ppr e)
+  ppr Update = text "Update"
+  ppr App1 = text "App1"
+  ppr App2 = text "App2"
+  ppr Case1 = text "Case1"
+  ppr Case2 = text "Case2"
+  ppr Let1 = text "Let1"
+instance Outputable v => Outputable (T v) where
+  ppr (Step ev τ) = ppr ev <> arrow <> ppr τ
+  ppr (Ret v) = char '<' <> ppr v <> char '>'
+instance Outputable (Value τ) where
+  ppr Stuck = text "stuck"
+  ppr Erased = char '_'
+  ppr (Litt l) = ppr l
+  ppr (Fun _f) = text "Fun"
+  ppr (Con dc _ds) = ppr dc
+instance Outputable (Heap τ) where
+  ppr μ = brackets (pprWithCommas (\(a,_) -> ppr a <> char '↦' <> underscore) (WM.toList μ))
---
+++ ghdi.hs
@@ -0,0 +1,84 @@
+-- Import necessary modules
+import GHC
+import GHC.Driver.Config.Parser
+import GHC.Driver.Env.Types
+import GHC.Driver.Session
+import GHC.Utils.Outputable
+import GHC.Unit.Types
+import GHC.Unit.Module.ModGuts
+import GHC.Data.StringBuffer
+import GHC.Data.FastString
+import qualified GHC.Parser.Lexer as L
+import qualified GHC.Parser as P
+import GHC.Types.SrcLoc
+import GHC.Core
+import Control.Monad
+import Control.Monad.IO.Class
+import System.IO
+import System.Environment
+import System.Exit
+import System.Directory
+import System.FilePath
+import Data.List
+import GHC.Types.Name
+import GHC.Core.Semantics
+import qualified GHC.LanguageExtensions as LangExt
+
+import System.Console.Haskeline
+
+indent :: Int -> String -> String
+indent n = unlines . map (\s -> replicate n ' ' ++ s) . lines
+
+pprPrint :: Outputable a => a -> IO ()
+pprPrint = putStrLn . showSDocUnsafe . ppr
+
+compileToCore :: String -> [String] -> String -> IO CoreExpr
+compileToCore libdir args expression = do
+  tmp <- getTemporaryDirectory
+  let file = tmp </> "_interactive_.hs"
+  writeFile file ("module Interactive where import GHC.Exts; it = " ++ indent 2 expression)
+  -- Initialize GHC session
+  defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+    runGhc (Just libdir) $ do
+      -- Set up GHC session
+      dflags <- getSessionDynFlags
+      logger <- getLogger
+      (dflags, rest_args, err_messages) <- parseDynamicFlags logger dflags (map (L noSrcSpan) args)
+      when (not (null rest_args)) $ liftIO $ putStrLn ("Unhandled args: " ++ show rest_args) >> exitFailure
+      when (not (null err_messages)) $ liftIO $ pprPrint err_messages >> exitFailure
+
+      setSessionDynFlags $
+        -- flip gopt_unset Opt_FullLaziness $
+        -- flip gopt_unset Opt_WorkerWrapper $
+        -- updOptLevel 1 $ -- if you want to compile with -O1 opts, make sure to unset -ffull-laziness and -fworker-wrapper above in addition to -flocal-float-out-top-level
+        flip gopt_unset Opt_LocalFloatOutTopLevel $
+        flip gopt_unset Opt_IgnoreInterfacePragmas $ -- This enables cross-module inlining
+        flip xopt_set LangExt.MagicHash $
+        dflags
+      mod_guts <- compileToCoreSimplified file
+      let binds = cm_binds mod_guts
+      let Just (NonRec _ e) = find (\b -> case b of NonRec x e -> getOccString x == "it"; _ -> False) binds
+      return e
+
+-- Main function to handle command-line arguments
+main :: IO ()
+main = do
+  args <- getArgs
+  tmp <- getTemporaryDirectory
+  let settings = defaultSettings { historyFile = Just (tmp </> ".ghdi.hist") }
+  case args of
+    (libdir:rest) -> runInputT settings (loop libdir rest)
+    _             -> putStrLn "Usage: `ghdi <libdir>`, for example `ghdi $(ghc --print-libdir)`"
+
+loop :: FilePath -> [String] -> InputT IO ()
+loop libdir args = do
+  minput <- getInputLine "prompt> "
+  case minput of
+    Nothing      -> return ()
+    Just ":quit" -> return ()
+    Just input   -> do
+      e <- liftIO $ compileToCore libdir args input
+      outputStrLn (showSDocUnsafe (hang (text "Above expression as (optimised) Core:") 2 (ppr e)))
+      outputStrLn "Trace of denotational interpreter:"
+      outputStrLn (showSDocOneLine defaultSDocContext (hang empty 2 (ppr (evalByNeed e))))
+      loop libdir args
