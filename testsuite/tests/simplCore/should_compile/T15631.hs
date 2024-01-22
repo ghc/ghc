cfd682902da6546362a48fbeedf03e414d0fd6db
Simon Peyton Jones <simon.peytonjones@gmail.com>
2024-01-22 13:02:34 +0000

    Stop dropping a case whose binder is demanded
    This MR fixes #24251.
    
    See Note [Case-to-let for strictly-used binders]
    in GHC.Core.Opt.Simplify.Iteration, plus #24251, for
    lots of discussion.
    
    Final Nofib changes over 0.1%:
    +-----------------------------------------
    |        imaginary/digits-of-e2    -2.16%
    |                imaginary/rfib    -0.15%
    |                    real/fluid    -0.10%
    |                   real/gamteb    -1.47%
    |                       real/gg    -0.20%
    |                 real/maillist    +0.19%
    |                      real/pic    -0.23%
    |                      real/scs    -0.43%
    |               shootout/n-body    -0.41%
    |        shootout/spectral-norm    -0.12%
    +========================================
    |                     geom mean    -0.05%
    
    Pleasingly, overall executable size is down by just over 1%.
    
    Compile times (in perf/compiler) wobble around a bit +/- 0.5%, but the
    geometric mean is -0.1% which seems good.

--- testsuite/tests/simplCore/should_compile/T15631.hs
+++ testsuite/tests/simplCore/should_compile/T15631.hs
@@ -7,5 +7,5 @@ f xs = let ys = reverse xs
           let w = length xs
           in w + length (reverse (case ys of { a:as -> as; [] -> [] }))
 
-
-
+-- Feb 24: because of #24251 we now expect ys to be
+--         evaluated early, and then case-analysed later
