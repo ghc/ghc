ee01de7d0443aaf8198f3cc68a6be58998eb68f6
Cheng Shao <terrorjack@type.dance>
2024-02-12 15:14:01 +0000

    rts: define XXH_INLINE_ALL
    This commit cleans up how we include the xxhash.h header and only
    define XXH_INLINE_ALL, which is sufficient to inline the xxHash
    functions without symbol collision.

--- rts/Hash.c
+++ rts/Hash.c
@@ -26,9 +26,7 @@
 #endif
 #endif
 
-#define XXH_NAMESPACE __rts_
-#define XXH_STATIC_LINKING_ONLY   /* access advanced declarations */
-#define XXH_IMPLEMENTATION   /* access definitions */
+#define XXH_INLINE_ALL
 
 #include "xxhash.h"
 
