c71bfdff79f2d92c843dc10c63f3e6b3a566d9f4
Cheng Shao <terrorjack@type.dance>
2024-02-22 17:46:54 +0000

    hadrian/hie-bios: pass -j to hadrian
    This commit passes -j to hadrian in the hadrian/hie-bios scripts. When
    the user starts HLS in a fresh clone that has just been configured, it
    takes quite a while for hie-bios to pick up the ghc flags and start
    actual indexing, due to the fact that the hadrian build step defaulted
    to -j1, so -j speeds things up and improve HLS user experience in GHC.
    Also add -j flag to .ghcid to speed up ghcid, and sets the Windows
    build root to .hie-bios which also works and unifies with other
    platforms, the previous build root _hie-bios was missing from
    .gitignore anyway.

--- hadrian/hie-bios.bat
+++ hadrian/hie-bios.bat
@@ -1,4 +1,4 @@
 set TERM=dumb
 set CABFLAGS=-v0
 set TOOL_OUTPUT=%HIE_BIOS_OUTPUT%
-%CD%\hadrian\build-cabal.bat tool:%1 --build-root=_hie-bios --flavour=ghc-in-ghci
+%CD%\hadrian\build-cabal.bat tool:%1 --build-root=.hie-bios --flavour=ghc-in-ghci -j
