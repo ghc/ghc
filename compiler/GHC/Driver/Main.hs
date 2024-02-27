1c064ef1f3e1aa2afc996e962ad53effa99ec5f4
Cheng Shao <terrorjack@type.dance>
2024-02-27 12:56:45 +0000

    compiler: start deprecating cmmToRawCmmHook
    cmmToRawCmmHook was added 4 years ago in
    d561c8f6244f8280a2483e8753c38e39d34c1f01. Its only user is the
    Asterius project, which has been archived and deprecated in favor of
    the ghc wasm backend. This patch starts deprecating cmmToRawCmmHook by
    placing a DEPRECATED pragma, and actual removal shall happen in a
    future GHC major release if no issue to oppose the deprecation has
    been raised in the meantime.

--- compiler/GHC/Driver/Main.hs
+++ compiler/GHC/Driver/Main.hs
@@ -6,6 +6,9 @@
 
 {-# OPTIONS_GHC -fprof-auto-top #-}
 
+-- Remove this after cmmToRawCmmHook removal
+{-# OPTIONS_GHC -Wno-deprecations #-}
+
 -------------------------------------------------------------------------------
 --
 -- | Main API for compiling plain Haskell source code.
