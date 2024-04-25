022f398b09e4db4233ee746495bf77dbb58be34f
Cheng Shao <terrorjack@type.dance>
2024-04-25 14:39:26 +0000

    compiler: generate stub objects before writing iface
    This commit promotes foreign stub compilation a bit earlier before we
    actually write iface, to make it possible to actually serialize them
    into the final iface when compiling with
    `-fwrite-if-simplified-core`. Also ensures dynamic stub objects are
    generated if we're compiling with `-fwrite-if-simplified-core`.

--- compiler/GHC/Driver/Pipeline/Execute.hs
+++ compiler/GHC/Driver/Pipeline/Execute.hs
@@ -582,8 +582,19 @@ runHscBackendPhase pipe_env hsc_env mod_name src_flavour location result = do
              do
               output_fn <- phaseOutputFilenameNew next_phase pipe_env hsc_env (Just location)
               (outputFilename, mStub, foreign_files, stg_infos, cg_infos) <-
-
                 hscGenHardCode hsc_env cgguts mod_location output_fn
+
+              -- When compiling with -fprefer-byte-code, always
+              -- compile foreign stubs as shared objects to ensure
+              -- they can be properly loaded.
+              let hsc_env_stub
+                    | gopt Opt_ByteCodeAndObjectCode dflags = hscUpdateFlags setDynamicNow hsc_env
+                    | otherwise = hsc_env
+              stub_o <- mapM (compileStub hsc_env_stub) mStub
+              foreign_os <-
+                mapM (uncurry (compileForeign hsc_env_stub)) foreign_files
+              let fos = maybe [] return stub_o ++ foreign_os
+
               final_iface <- mkFullIface hsc_env partial_iface stg_infos cg_infos
 
               -- See Note [Writing interface files]
@@ -595,12 +606,6 @@ runHscBackendPhase pipe_env hsc_env mod_name src_flavour location result = do
                     return $ emptyHomeModInfoLinkable { homeMod_bytecode = Just bc }
 
                   else return emptyHomeModInfoLinkable
-
-
-              stub_o <- mapM (compileStub hsc_env) mStub
-              foreign_os <-
-                mapM (uncurry (compileForeign hsc_env)) foreign_files
-              let fos = (maybe [] return stub_o ++ foreign_os)
 
               -- This is awkward, no linkable is produced here because we still
               -- have some way to do before the object file is produced
