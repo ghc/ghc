95ef2d58782e0fdbf1420465b2f9f31febf33fa1
Matthew Pickering <matthewtpickering@gmail.com>
2024-05-28 17:21:24 +0100

    ghcup-metadata: Fix metadata generation
    There were some syntax errors in the generation script which were
    preventing it from running.
    
    I have tested this with:
    
    ```
    nix shell --extra-experimental-features nix-command -f .gitlab/rel_eng -c ghcup-metadata --metadata ghcup-0.0.7.yaml --date="2024-05-27" --pipeline-id=95534 --version=9.11.20240525
    ```
    
    which completed successfully.

--- .gitlab/rel_eng/mk-ghcup-metadata/mk_ghcup_metadata.py
+++ .gitlab/rel_eng/mk-ghcup-metadata/mk_ghcup_metadata.py
@@ -199,7 +199,7 @@ def mk_new_yaml(release_mode, version, date, pipeline_type, job_map):
     windows = mk(windowsArtifact)
     alpine3_12 = mk(alpine("3_12"))
     alpine3_18 = mk(alpine("3_18"))
-    alpine3_18_arm64 = mk(alpine("3_18"), arch='aarch64')
+    alpine3_18_arm64 = mk(alpine("3_18", arch='aarch64'))
     deb9 = mk(debian(9, "x86_64"))
     deb10 = mk(debian(10, "x86_64"))
     deb11 = mk(debian(11, "x86_64"))
@@ -233,8 +233,8 @@ def mk_new_yaml(release_mode, version, date, pipeline_type, job_map):
           , "Linux_UnknownLinux" : { "unknown_versioning": rocky8 }
           , "Darwin" : { "unknown_versioning" : darwin_x86 }
           , "Windows" : { "unknown_versioning" :  windows }
-          , "Linux_Alpine" : { "( >= 3.12 && < 3.18 )": alpine_3_12
-                             , ">= 3.18": alpine_3_18
+          , "Linux_Alpine" : { "( >= 3.12 && < 3.18 )": alpine3_12
+                             , ">= 3.18": alpine3_18
                              , "unknown_versioning": alpine3_12 }
 
           }
