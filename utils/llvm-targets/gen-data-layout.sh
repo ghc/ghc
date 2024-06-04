24f658925fe9571e1d3b7fa1506b2031a9e8873a
Cheng Shao <terrorjack@type.dance>
2024-06-04 10:38:55 +0000

    llvm-targets: remove 32-bit darwin/ios targets
    This commit removes 32-bit darwin/ios targets from llvm-targets given
    we no longer support them (#24921).

--- utils/llvm-targets/gen-data-layout.sh
+++ utils/llvm-targets/gen-data-layout.sh
@@ -89,13 +89,10 @@ TARGETS=(
     #########################
 
     # macOS
-    "i386-apple-darwin"
     "x86_64-apple-darwin"
     "arm64-apple-darwin"
     # iOS
-    "armv7-apple-ios"
     "arm64-apple-ios"
-    "i386-apple-ios"
     "x86_64-apple-ios"
 
     #########################
