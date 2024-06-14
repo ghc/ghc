b6b3bd51779bd57d44691b296441cfa54f9ab934
Peter Trommler <ptrommler@acm.org>
2024-06-14 18:24:48 +0200

    PPC NCG: Fix sign hints in C calls
    Sign hints are in the second component of the pair.
    
    Fixes #23034

---
+++ testsuite/tests/codeGen/should_run/T23034.h
@@ -0,0 +1,1 @@
+void t_printf(long a, int b, short c, char d);
---
+++ testsuite/tests/codeGen/should_run/T23034_c.c
@@ -0,0 +1,6 @@
+#include "T23034.h"
+#include <stdio.h>
+
+void t_printf(long a, int b, short c, char d) {
+  printf("%d %d %d %d\n", a, b, c, d);
+}
