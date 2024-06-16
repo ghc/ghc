05e0f4dd7dbac337b4aa7800e16b252549dd4383
Sven Tennie <sven.tennie@gmail.com>
2024-06-16 13:47:24 +0000

    Cleanup RegInfo

--- compiler/GHC/CmmToAsm/RV64/RegInfo.hs
+++ compiler/GHC/CmmToAsm/RV64/RegInfo.hs
@@ -1,31 +1,40 @@
-module GHC.CmmToAsm.RV64.RegInfo where
+-- | Minimum viable implementation of jump short-cutting: No short-cutting.
+--
+-- The functions here simply implement the no-short-cutting case. Implementing
+-- the real behaviour would be a great optimization in future.
+module GHC.CmmToAsm.RV64.RegInfo
+  ( getJumpDestBlockId,
+    canShortcut,
+    shortcutStatics,
+    shortcutJump,
+  )
+where
 
-import GHC.Prelude
-
-import GHC.CmmToAsm.RV64.Instr
-import GHC.Cmm.BlockId
 import GHC.Cmm
-
+import GHC.Cmm.BlockId
+import GHC.CmmToAsm.RV64.Instr
+import GHC.Prelude
 import GHC.Utils.Outputable
 
-data JumpDest = DestBlockId BlockId
+newtype JumpDest = DestBlockId BlockId
 
--- Debug Instance
 instance Outputable JumpDest where
   ppr (DestBlockId bid) = text "jd<blk>:" <> ppr bid
 
--- TODO: documen what this does. See Ticket 19914
+-- | Extract BlockId
+--
+-- Never `Nothing` for Riscv64 NCG.
 getJumpDestBlockId :: JumpDest -> Maybe BlockId
 getJumpDestBlockId (DestBlockId bid) = Just bid
 
--- TODO: document what this does. See Ticket 19914
+-- No `Instr`s can bet shortcut (for now)
 canShortcut :: Instr -> Maybe JumpDest
 canShortcut _ = Nothing
 
--- TODO: document what this does. See Ticket 19914
+-- Identity of the provided `RawCmmStatics`
 shortcutStatics :: (BlockId -> Maybe JumpDest) -> RawCmmStatics -> RawCmmStatics
 shortcutStatics _ other_static = other_static
 
--- TODO: document what this does. See Ticket 19914
+-- Identity of the provided `Instr`
 shortcutJump :: (BlockId -> Maybe JumpDest) -> Instr -> Instr
 shortcutJump _ other = other
