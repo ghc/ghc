70ff49b7efc8c1fca46cba6eff630c5d39a99213
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-06-19 16:36:09 +0100

    Reapply "Do uniq renaming before SRTs"
    This reverts commit 682f89732fc2a95fa011f530c0c6922bf576d229.

--- compiler/GHC/Cmm/Pipeline.hs
+++ compiler/GHC/Cmm/Pipeline.hs
@@ -19,6 +19,7 @@ import GHC.Cmm.ProcPoint
 import GHC.Cmm.Sink
 import GHC.Cmm.Switch.Implement
 import GHC.Cmm.ThreadSanitizer
+import GHC.Cmm.UniqueRenamer
 
 import GHC.Types.Unique.Supply
 
@@ -42,18 +43,27 @@ cmmPipeline
  :: Logger
  -> CmmConfig
  -> ModuleSRTInfo        -- Info about SRTs generated so far
+ -> DetUniqFM
  -> CmmGroup             -- Input C-- with Procedures
- -> IO (ModuleSRTInfo, CmmGroupSRTs) -- Output CPS transformed C--
+ -> IO (ModuleSRTInfo, (DetUniqFM, CmmGroupSRTs)) -- Output CPS transformed C--
 
-cmmPipeline logger cmm_config srtInfo prog = do
+cmmPipeline logger cmm_config srtInfo detRnEnv prog = do
   let forceRes (info, group) = info `seq` foldr seq () group
   let platform = cmmPlatform cmm_config
   withTimingSilent logger (text "Cmm pipeline") forceRes $ do
-     (procs, data_) <- {-# SCC "tops" #-} partitionWithM (cpsTop logger platform cmm_config) prog
+
+     -- To produce deterministic object code, we alpha-rename all Uniques to deterministic uniques before Cmm linting.
+     -- From here on out, the backend code generation can't use (non-deterministic) Uniques, or risk producing non-deterministic code.
+     -- For example, the fix-up action in the ASM NCG should use determinist names for potential new blocks it has to create.
+     -- Therefore, in the ASM NCG `NatM` Monad we use a deterministic `UniqSuply` (which won't be shared about multiple threads)
+     -- TODO: Put these all into notes carefully organized
+     let (rn_mapping, renamed_prog) = detRenameUniques detRnEnv prog -- TODO: if gopt Opt_DeterministicObjects dflags
+
+     (procs, data_) <- {-# SCC "tops" #-} partitionWithM (cpsTop logger platform cmm_config) renamed_prog
      (srtInfo, cmms) <- {-# SCC "doSRTs" #-} doSRTs cmm_config srtInfo procs data_
      dumpWith logger Opt_D_dump_cmm_cps "Post CPS Cmm" FormatCMM (pdoc platform cmms)
 
-     return (srtInfo, cmms)
+     return (srtInfo, (rn_mapping, cmms))
 
 
 -- | The Cmm pipeline for a single 'CmmDecl'. Returns:
