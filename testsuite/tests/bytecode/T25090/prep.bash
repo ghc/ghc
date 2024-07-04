60413ac9d8da47447290605857088467bd5cb53c
Torsten Schmits <git@tryp.io>
2024-07-04 13:20:23 +0200

    Link interface bytecode from package DBs if possible
    Part of #25090.
    
    MR !13068
    
    When splices are executed with `-fprefer-byte-code`, the loader will
    compile Core bindings to bytecode if those are present in interfaces of
    module dependencies.
    
    So far, this only applied to local modules (i.e. home modules in make
    mode and non-package deps in oneshot mode).
    
    This patch extends support to interfaces loaded from a package DB.
    In `getLinkDeps`, the dependencies chosen for recursion were restricted
    to `dep_direct_mods`, which has been changed to include external
    dependencies, stored in a new field named `dep_direct_pkg_mods`.
    In order to unify treatment of the different link variants across
    make/oneshot mode, the algorithm's intermediate results have been
    abstracted into the data types `LinkDep` and `LinkModule`.
    
    Metric Decrease:
        MultiLayerModulesTH_Make
        MultiLayerModulesTH_OneShot

---
+++ testsuite/tests/bytecode/T25090/prep.bash
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -eu
+
+ghc_cmd="$1"
+ghc_opts="$2"
+ghc_pkg_cmd="$3"
+so_ext="$4"
+library="$5"
+
+base="$PWD"
+db="$base/db"
+dep="$base/dep"
+conf_dep="${dep}/dep.conf"
+
+mkdir -p "$dep" "$db"
+mv Dep.hs DepApi.hs "$dep/"
+cp dep.conf "$dep/"
+
+ghc_pkg()
+{
+  eval "${ghc_pkg_cmd@Q} --no-user-package-db --package-db=${db@Q} $@"
+}
+
+ghc()
+{
+  eval "${ghc_cmd@Q} ${ghc_opts/-rtsopts/} -package-db ${db@Q} -hidir ${dep@Q} -O0 -this-unit-id dep-1.0 -fbyte-code-and-object-code $@"
+}
+
+version=$(ghc "--numeric-version")
+
+ghc_pkg recache
+
+ghc "-c ${dep@Q}/Dep.hs ${dep@Q}/DepApi.hs"
+ghc "-dynamic -c -osuf dyn_o -hisuf dyn_hi ${dep@Q}/Dep.hs ${dep@Q}/DepApi.hs"
+
+if [[ "$library" == 'shared' ]]
+then
+  ghc "-dynamic -shared -o ${dep@Q}/libHSdep-1.0-ghc${version}${so_ext} ${dep@Q}/Dep.dyn_o ${dep@Q}/DepApi.dyn_o"
+  echo 'hs-libraries: HSdep-1.0' >> "$conf_dep"
+elif [[ "$library" == 'shared-empty' ]]
+then
+  echo 'module Dummy where' > Dummy.hs
+  ghc "-dynamic-too -c Dummy.hs"
+  ghc "-dynamic -shared -o ${dep@Q}/libHSdep-1.0-ghc${version}${so_ext} Dummy.dyn_o"
+  echo 'hs-libraries: HSdep-1.0' >> "$conf_dep"
+elif [[ "$library" == 'archive' ]]
+then
+  $AR cqs "${dep}/libHSdep-1.0.a" "${dep}/Dep.o" "${dep}/DepApi.o"
+  echo 'hs-libraries: HSdep-1.0' >> "$conf_dep"
+elif [[ "$library" == 'none' ]]
+then
+  :
+else
+  echo "Invalid argument for 'library': $library"
+  exit 1
+fi
+
+ghc_pkg -v0 register "${conf_dep@Q}"
