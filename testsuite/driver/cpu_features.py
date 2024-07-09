4f85366ffd1068592d49431fc0d14ccf987271dd
sheaf <sam.derbyshire@gmail.com>
2024-07-09 12:59:07 +0200

    Testsuite: use py-cpuinfo to compute CPU features
    This replaces the rather hacky logic we had in place for checking
    CPU features. In particular, this means that feature availability now
    works properly on Windows.

--- testsuite/driver/cpu_features.py
+++ testsuite/driver/cpu_features.py
@@ -9,7 +9,7 @@ SUPPORTED_CPU_FEATURES = {
 
     # x86:
     'sse', 'sse2', 'sse3', 'ssse3', 'sse4_1', 'sse4_2',
-    'avx1', 'avx2',
+    'avx', 'avx2',
     'fma',
     'popcnt', 'bmi1', 'bmi2'
 }
@@ -17,50 +17,16 @@ SUPPORTED_CPU_FEATURES = {
 cpu_feature_cache = None
 
 def get_cpu_features():
-    # we're testing a cross ghc, don't attempt to detect host cpu
-    # configuration
-    if config.target_wrapper is not None:
-        return {}
-
-    if config.os in ['mingw32', 'linux'] and os.path.exists('/proc/cpuinfo'):
-        with open('/proc/cpuinfo') as x:
-            f = x.read()
-        flags = re.search(r'flags\s*:\s*.*$', f, re.M)
-        if flags is None:
-            print('get_cpu_features: failed to find cpu features')
-            return {}
-        flags = set(flags.group(0).split())
-        if 'pni' in flags:
-            flags.add('sse3')
-            flags.remove('pni')
-        return flags
-
-    elif config.os == 'darwin':
-        # we hardcode the sysctl path, otherwise we rely on /usr/sbin being in
-        # path.
-        out = subprocess.check_output(['/usr/sbin/sysctl', 'hw']).decode('UTF-8')
-        features = set()
-        def check_feature(darwin_name, our_name=None):
-            if re.search(r'hw\.optional.%s:\s*1' % darwin_name, out) is not None:
-                features.add(darwin_name if our_name is None else our_name)
-
-        for feature in SUPPORTED_CPU_FEATURES:
-            check_feature(feature)
-
-        # A few annoying cases
-        check_feature('avx1_0', 'avx1')
-        check_feature('avx2_0', 'avx2')
+    try:
+        # This import might fail, e.g. if "ctypes" is not available,
+        # in which case we report the empty set of features.
+        import cpuinfo
+        info = cpuinfo.get_cpu_info()
+        features = info.get('flags', [])
         return features
-
-    elif config.arch in [ 'powerpc', 'powerpc64' ]:
-        # Hardcode support for 'fma' on PowerPC
-        return [ 'fma' ]
-
-    else:
-        # TODO: Add {Open,Free}BSD support
+    except:
         print('get_cpu_features: Lacking support for your platform')
-
-    return {}
+        return []
 
 def have_cpu_feature(feature):
     """
