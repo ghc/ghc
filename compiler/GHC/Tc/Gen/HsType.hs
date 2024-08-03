31805ee5bcaefc2f07532e8b8e42a009bfa784f2
Brandon Chinn <brandonchinn178@gmail.com>
2024-08-03 16:23:55 -0700

    Support multiline strings in type literals (#25132)

--- compiler/GHC/Tc/Gen/HsType.hs
+++ compiler/GHC/Tc/Gen/HsType.hs
@@ -4705,6 +4705,7 @@ addTyConFlavCtxt name flav
 
 tyLitFromLit :: HsLit GhcRn -> Maybe (HsTyLit GhcRn)
 tyLitFromLit (HsString x str) = Just (HsStrTy x str)
+tyLitFromLit (HsMultilineString x str) = Just (HsStrTy x str)
 tyLitFromLit (HsChar x char) = Just (HsCharTy x char)
 tyLitFromLit _ = Nothing
 
