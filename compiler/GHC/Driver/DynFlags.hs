2c4d9f6151898e1da7721d39e0ef30bdfb0b9e44
Andreas Klebinger <klebinger.andreas@gmx.at>
2024-08-23 17:44:12 +0200

    SpecConstr: Introduce a separate argument limit for forced specs.
    We used to put no limit at all on specializations forced via the SPEC
    argument. This isn't always reasonable so we introduce a very high limit
    that applies to forced specializations, a flag to control it, and we now
    emit a warning if we fail a specialization because we exceed the
    warning.
    
    Fixes #25197

--- compiler/GHC/Driver/DynFlags.hs
+++ compiler/GHC/Driver/DynFlags.hs
@@ -396,6 +396,7 @@ data DynFlags = DynFlags {
   unfoldingOpts         :: !UnfoldingOpts,
 
   maxWorkerArgs         :: Int,
+  maxForcedSpecArgs     :: Int,
 
   ghciHistSize          :: Int,
 
@@ -678,6 +679,8 @@ defaultDynFlags mySettings =
 
         unfoldingOpts = defaultUnfoldingOpts,
         maxWorkerArgs = 10,
+        maxForcedSpecArgs = 333,
+        -- 333 is fairly arbitrary, see Note [Forcing specialisation]:FS5
 
         ghciHistSize = 50, -- keep a log of length 50 by default
 
