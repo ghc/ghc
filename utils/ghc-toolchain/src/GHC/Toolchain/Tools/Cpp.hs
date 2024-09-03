ghc/ghc

commit 25b0b40467d0a12601497117c0ad14e1fcab0b74

Split out the C-- preprocessor, and make it pass -g0
Previously, C-- was processed with the C preprocessor program.  This
means that it inherited flags passed via -optc.  A flag that is somewhat
often passed through -optc is -g.  At certain -g levels (>=2), GCC
starts emitting defines *after* preprocessing, for the purposes of
debug info generation.  This is not useful for the C-- compiler, and, in
fact, causes lexer errors.  We can suppress this effect (safely, if
supported) via -g0.

As a workaround, in older versions of GCC (<=10), GCC only emitted
defines if a certain set of -g*3 flags was passed.  Newer versions check
the debug level.  For the former, we filter out those -g*3 flags and,
for the latter, we specify -g0 on top of that.

As a compatible and effective solution, this change adds a C--
preprocessor distinct from the C compiler and preprocessor, but that
keeps its flags.  The command line produced for C-- preprocessing now
looks like:

  $pgmCmmP $optCs_without_g3 $g0_if_supported $optCmmP

Closes: https://gitlab.haskell.org/ghc/ghc/-/issues/24474

utils/ghc-toolchain/src/GHC/Toolchain/Tools/Cpp.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE NamedFieldPuns #-}
 
-module GHC.Toolchain.Tools.Cpp (HsCpp(..), findHsCpp, Cpp(..), findCpp, JsCpp(..), findJsCpp) where
+module GHC.Toolchain.Tools.Cpp
+  (HsCpp(..), findHsCpp
+  , Cpp(..), findCpp
+  , JsCpp(..), findJsCpp
+  , CmmCpp(..), findCmmCpp
+  ) where
 
 import Control.Monad
 import System.FilePath
@@ -24,6 +29,24 @@ newtype HsCpp = HsCpp { hsCppProgram :: Program
 newtype JsCpp = JsCpp { jsCppProgram :: Program
                       }
     deriving (Show, Read, Eq, Ord)
+
+data CmmCpp = CmmCpp { cmmCppProgram :: Program
+                     , cmmCppSupportsG0 :: Bool
+                     -- ^ Whether the C-- preprocessor supports -g0.  Extracted
+                     -- out as -g0 needs to be appended to the complete
+                     -- invocation, rather than prefix flags, in order to
+                     -- override other flags.
+                     }
+    deriving (Show, Read, Eq, Ord)
+
+checkFlag :: String -> Program -> String -> M ()
+checkFlag conftest cpp flag = checking ("for "++flag++" support") $
+  -- Werror to ensure that unrecognized warnings result in an error
+  callProgram cpp ["-Werror", flag, conftest]
+-- tryFlag :: String -> Program -> String -> M [String]
+-- tryFlag conftest cpp flag =
+--   ([flag] <$ checkFlag conftest cpp flag) <|> return []
+
 
 ----- Haskell Preprocessor -----
 
@@ -65,20 +88,13 @@ findHsCppArgs cpp = withTempDir $ \dir -> do
   withTmpDir $ \dir -> do
   let tmp_h = dir </> "tmp.h"
 
-      -- Werror to ensure that unrecognized warnings result in an error
-  let checkFlag flag =
-          checking ("for "++flag++" support") $ callProgram cpp ["-Werror", flag, tmp_h]
-
-      tryFlag flag =
-          ([flag] <$ checkFlag flag) <|> return []
-
   writeFile tmp_h ""
   concat <$> sequence
       [ tryFlag "-undef"
-      , ["-traditional"] <$ checkFlag "-traditional"
-      , tryFlag "-Wno-invalid-pp-token"
-      , tryFlag "-Wno-unicode"
-      , tryFlag "-Wno-trigraphs"
+      , ["-traditional"] <$ checkFlag tmp_h cpp "-traditional"
+      , tryFlag tmp_h cpp "-Wno-invalid-pp-token"
+      , tryFlag tmp_h cpp "-Wno-unicode"
+      , tryFlag tmp_h cpp "-Wno-trigraphs"
       ]
       -}
 
@@ -135,6 +151,27 @@ findJsCpp progOpt cc = checking "for JavaScript C preprocessor" $ do
       file_output_in_dir_content <- readFile file_output_in_dir
       unless (trim file_output_in_dir_content == "// 1")
         $ throwE "JavaScript C Preprocessor didn't provide correct output"
+
+----- C-- preprocessor -----
+
+findCmmCpp :: ProgOpt -> Cc -> M CmmCpp
+findCmmCpp progOpt cc = checking "for a Cmm preprocessor" $ do
+  -- Use the specified CPP or try to use the c compiler
+  foundCppProg <- findProgram "Cmm preprocessor" progOpt [] <|> pure (programFromOpt progOpt (prgPath $ ccProgram cc) [])
+  -- Check whether the C preprocessor needs -std=gnu99 (only very old toolchains need this)
+  Cc cpp <- oneOf "cc doesn't support C99" $ map checkC99Support
+        [ Cc foundCppProg
+        , Cc (foundCppProg & _prgFlags %++ "-std=gnu99")
+        ]
+
+  cmmCppSupportsG0 <- withTempDir $ \dir -> do
+    let conftest = dir </> "conftest.c"
+    writeFile conftest "int main(void) {}"
+    True <$ checkFlag conftest cpp "-g0" <|> return False
+
+  -- Always add the -E flag to the CPP, regardless of the user options
+  let cmmCppProgram = foldr addFlagIfNew cpp ["-E"]
+  return CmmCpp{cmmCppProgram, cmmCppSupportsG0}
 
 ----- C preprocessor -----
 

compiler/GHC/SysTools/Cpp.hs
@@ -48,15 +48,17 @@ data CppOpts = CppOpts
 {-
 Note [Preprocessing invocations]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-We must consider three distinct preprocessors when preprocessing Haskell.
+We must consider four distinct preprocessors when preprocessing Haskell.
 These are:
 
 (1) The Haskell C preprocessor (HsCpp), which preprocesses Haskell files that make use
   of the CPP language extension
 
-(2) The C preprocessor (Cpp), which is used to preprocess C and Cmm files
+(2) The C preprocessor (Cpp), which is used to preprocess C files
 
 (3) The JavaScript preprocessor (JsCpp), which preprocesses JavaScript files
+
+(4) The C-- preprocessor (CmmCpp), which preprocesses C-- files
 
 These preprocessors are indeed different. Despite often sharing the same
 underlying program (the C compiler), the set of flags passed determines the
@@ -86,6 +88,17 @@ minifying software (for example, Google Closure Compiler) uses JSDoc
 information to apply more strict rules to code reduction which results in
 better but more dangerous minification. JSDoc comments are usually used to
 instruct minifiers where dangerous optimizations could be applied.
+
+The fourth, the C-- preprocessor, is needed as modern compilers emit defines
+for debug info generation when preprocessing.  The C-- preprocessor avoids this
+by suppressing debug info generation.  The C-- preprocessor also inherits flags
+passed to the C compiler.  This is done for compatibility.  Following those,
+the C-- compiler receives -g0, if it was detected as supported, and flags
+passed via -optCmmP specifically for the C-- preprocessor.  The combined
+command line looks like:
+
+  $pgmCmmP $optCs_without_g3s $g0_if_supported $optCmmP
+
 -}
 
 -- | Run either the Haskell preprocessor, JavaScript preprocessor

compiler/GHC/Driver/Pipeline/Execute.hs
@@ -121,7 +121,7 @@ runPhase (T_CmmCpp pipe_env hsc_env input_fn) = do
         (hsc_dflags hsc_env)
         (hsc_unit_env hsc_env)
         (CppOpts
-          { sourceCodePreprocessor  = SCPCpp
+          { sourceCodePreprocessor  = SCPCmmCpp
           , cppLinePragmas          = True
           })
         input_fn output_fn

compiler/GHC/Driver/Session.hs
@@ -114,6 +114,8 @@ module GHC.Driver.Session (
         sOpt_P_fingerprint,
         sOpt_JSP,
         sOpt_JSP_fingerprint,
+        sOpt_CmmP,
+        sOpt_CmmP_fingerprint,
         sOpt_F,
         sOpt_c,
         sOpt_cxx,
@@ -136,11 +138,11 @@ module GHC.Driver.Session (
         ghcUsagePath, ghciUsagePath, topDir,
         versionedAppDir, versionedFilePath,
         extraGccViaCFlags, globalPackageDatabasePath,
-        pgm_L, pgm_P, pgm_JSP, pgm_F, pgm_c, pgm_cxx, pgm_cpp, pgm_a, pgm_l,
+        pgm_L, pgm_P, pgm_JSP, pgm_CmmP, pgm_F, pgm_c, pgm_cxx, pgm_cpp, pgm_a, pgm_l,
         pgm_lm, pgm_windres, pgm_ar,
         pgm_ranlib, pgm_lo, pgm_lc, pgm_las, pgm_i,
-        opt_L, opt_P, opt_JSP, opt_F, opt_c, opt_cxx, opt_a, opt_l, opt_lm, opt_i,
-        opt_P_signature, opt_JSP_signature,
+        opt_L, opt_P, opt_JSP, opt_CmmP, opt_F, opt_c, opt_cxx, opt_a, opt_l, opt_lm, opt_i,
+        opt_P_signature, opt_JSP_signature, opt_CmmP_signature,
         opt_windres, opt_lo, opt_lc, opt_las,
         updatePlatformConstants,
 
@@ -401,6 +403,8 @@ pgm_P                 :: DynFlags -> (String,[Option])
 pgm_P dflags = toolSettings_pgm_P $ toolSettings dflags
 pgm_JSP               :: DynFlags -> (String,[Option])
 pgm_JSP dflags = toolSettings_pgm_JSP $ toolSettings dflags
+pgm_CmmP              :: DynFlags -> (String,[Option])
+pgm_CmmP dflags = toolSettings_pgm_CmmP $ toolSettings dflags
 pgm_F                 :: DynFlags -> String
 pgm_F dflags = toolSettings_pgm_F $ toolSettings dflags
 pgm_c                 :: DynFlags -> String
@@ -437,6 +441,8 @@ opt_P dflags = concatMap (wayOptP (targetPlatform dflags)) (ways dflags)
 opt_JSP               :: DynFlags -> [String]
 opt_JSP dflags = concatMap (wayOptP (targetPlatform dflags)) (ways dflags)
             ++ toolSettings_opt_JSP (toolSettings dflags)
+opt_CmmP              :: DynFlags -> [String]
+opt_CmmP dflags = toolSettings_opt_CmmP $ toolSettings dflags
 
 -- This function packages everything that's needed to fingerprint opt_P
 -- flags. See Note [Repeated -optP hashing].
@@ -452,6 +458,10 @@ opt_JSP_signature dflags =
   ( concatMap (wayOptP (targetPlatform dflags)) (ways dflags)
   , toolSettings_opt_JSP_fingerprint $ toolSettings dflags
   )
+-- This function packages everything that's needed to fingerprint opt_CmmP
+-- flags. See Note [Repeated -optP hashing].
+opt_CmmP_signature     :: DynFlags -> Fingerprint
+opt_CmmP_signature = toolSettings_opt_CmmP_fingerprint . toolSettings
 
 opt_F                 :: DynFlags -> [String]
 opt_F dflags= toolSettings_opt_F $ toolSettings dflags
@@ -600,7 +610,8 @@ setObjectDir, setHiDir, setHieDir, setStubDir, setDumpDir, setOutputDir,
          setDynObjectSuf, setDynHiSuf,
          setDylibInstallName,
          setObjectSuf, setHiSuf, setHieSuf, setHcSuf, parseDynLibLoaderMode,
-         setPgmP, setPgmJSP, addOptl, addOptc, addOptcxx, addOptP, addOptJSP,
+         setPgmP, setPgmJSP, setPgmCmmP, addOptl, addOptc, addOptcxx, addOptP,
+         addOptJSP, addOptCmmP,
          addCmdlineFramework, addHaddockOpts, addGhciScript,
          setInteractivePrint
    :: String -> DynFlags -> DynFlags
@@ -694,6 +705,10 @@ setPgmP   f = alterToolSettings (\s -> s { toolSettings_pgm_P   = (pgm, map Opti
 -- Config.hs should really use Option.
 setPgmJSP   f = alterToolSettings (\s -> s { toolSettings_pgm_JSP   = (pgm, map Option args)})
   where (pgm:args) = words f
+-- XXX HACK: Prelude> words "'does not' work" ===> ["'does","not'","work"]
+-- Config.hs should really use Option.
+setPgmCmmP f = alterToolSettings (\s -> s { toolSettings_pgm_CmmP = (pgm, map Option args)})
+  where (pgm:args) = words f
 addOptl   f = alterToolSettings (\s -> s { toolSettings_opt_l   = f : toolSettings_opt_l s})
 addOptc   f = alterToolSettings (\s -> s { toolSettings_opt_c   = f : toolSettings_opt_c s})
 addOptcxx f = alterToolSettings (\s -> s { toolSettings_opt_cxx = f : toolSettings_opt_cxx s})
@@ -707,6 +722,10 @@ addOptJSP f = alterToolSettings $ \s -> s
           , toolSettings_opt_JSP_fingerprint = fingerprintStrings (f : toolSettings_opt_JSP s)
           }
           -- See Note [Repeated -optP hashing]
+addOptCmmP f = alterToolSettings $ \s -> s
+          { toolSettings_opt_CmmP = f : toolSettings_opt_CmmP s
+          , toolSettings_opt_CmmP_fingerprint = fingerprintStrings (f : toolSettings_opt_CmmP s)
+          }
 
 setDepMakefile :: FilePath -> DynFlags -> DynFlags
 setDepMakefile f d = d { depMakefile = f }
@@ -1099,6 +1118,8 @@ dynamic_flags_deps = [
       (hasArg setPgmP)
   , make_ord_flag defFlag "pgmJSP"
       (hasArg setPgmJSP)
+  , make_ord_flag defFlag "pgmCmmP"
+      (hasArg setPgmCmmP)
   , make_ord_flag defFlag "pgmF"
       $ hasArg $ \f -> alterToolSettings $ \s -> s { toolSettings_pgm_F   = f }
   , make_ord_flag defFlag "pgmc"
@@ -1155,6 +1176,8 @@ dynamic_flags_deps = [
       (hasArg addOptP)
   , make_ord_flag defFlag "optJSP"
       (hasArg addOptJSP)
+  , make_ord_flag defFlag "optCmmP"
+      (hasArg addOptCmmP)
   , make_ord_flag defFlag "optF"
       $ hasArg $ \f -> alterToolSettings $ \s -> s { toolSettings_opt_F   = f : toolSettings_opt_F s }
   , make_ord_flag defFlag "optc"

compiler/GHC/Iface/Recomp/Flags.hs
@@ -64,6 +64,12 @@ fingerprintDynFlags hsc_env this_mod nameio =
               , opt_JSP_signature dflags)
             -- See Note [Repeated -optP hashing]
 
+        -- -I, -D and -U flags affect C-- CPP Preprocessor
+        cmm = ( map normalise $ flattenIncludes includePathsMinusImplicit
+            -- normalise: eliminate spurious differences due to "./foo" vs "foo"
+              , picPOpts dflags
+              , opt_CmmP_signature dflags)
+
         -- Note [path flags and recompilation]
         paths = [ hcSuf ]
 
@@ -77,7 +83,7 @@ fingerprintDynFlags hsc_env this_mod nameio =
         -- Other flags which affect code generation
         codegen = map (`gopt` dflags) (EnumSet.toList codeGenFlags)
 
-        flags = ((mainis, safeHs, lang, cpp, js), (paths, prof, ticky, codegen, debugLevel, callerCcFilters))
+        flags = ((mainis, safeHs, lang, cpp, js, cmm), (paths, prof, ticky, codegen, debugLevel, callerCcFilters))
 
     in -- pprTrace "flags" (ppr flags) $
        computeFingerprint nameio flags

compiler/GHC/Settings.hs
@@ -27,6 +27,7 @@ module GHC.Settings
   , sPgm_L
   , sPgm_P
   , sPgm_JSP
+  , sPgm_CmmP
   , sPgm_F
   , sPgm_c
   , sPgm_cxx
@@ -48,6 +49,8 @@ module GHC.Settings
   , sOpt_P_fingerprint
   , sOpt_JSP
   , sOpt_JSP_fingerprint
+  , sOpt_CmmP
+  , sOpt_CmmP_fingerprint
   , sOpt_F
   , sOpt_c
   , sOpt_cxx
@@ -95,6 +98,7 @@ data ToolSettings = ToolSettings
   , toolSettings_ccSupportsNoPie         :: Bool
   , toolSettings_useInplaceMinGW         :: Bool
   , toolSettings_arSupportsDashL         :: Bool
+  , toolSettings_cmmCppSupportsG0        :: Bool
 
   -- commands for particular phases
   , toolSettings_pgm_L       :: String
@@ -102,6 +106,8 @@ data ToolSettings = ToolSettings
     toolSettings_pgm_P       :: (String, [Option])
   , -- | The JavaScript C preprocessor and default options (not added by -optP)
     toolSettings_pgm_JSP       :: (String, [Option])
+  , -- | The C-- C Preprocessor and default options (not added by -optP)
+    toolSettings_pgm_CmmP    :: (String, [Option])
   , toolSettings_pgm_F       :: String
   , toolSettings_pgm_c       :: String
   , toolSettings_pgm_cxx     :: String
@@ -130,12 +136,16 @@ data ToolSettings = ToolSettings
   , toolSettings_opt_L             :: [String]
   , toolSettings_opt_P             :: [String]
   , toolSettings_opt_JSP           :: [String]
+  , toolSettings_opt_CmmP          :: [String]
   , -- | cached Fingerprint of sOpt_P
     -- See Note [Repeated -optP hashing]
     toolSettings_opt_P_fingerprint   :: Fingerprint
   , -- | cached Fingerprint of sOpt_JSP
     -- See Note [Repeated -optP hashing]
     toolSettings_opt_JSP_fingerprint :: Fingerprint
+  , -- | cached Fingerprint of sOpt_CmmP
+    -- See Note [Repeated -optP hashing]
+    toolSettings_opt_CmmP_fingerprint :: Fingerprint
   , toolSettings_opt_F             :: [String]
   , toolSettings_opt_c             :: [String]
   , toolSettings_opt_cxx           :: [String]
@@ -216,6 +226,8 @@ sPgm_P :: Settings -> (String, [Option])
 sPgm_P = toolSettings_pgm_P . sToolSettings
 sPgm_JSP :: Settings -> (String, [Option])
 sPgm_JSP = toolSettings_pgm_JSP . sToolSettings
+sPgm_CmmP :: Settings -> (String, [Option])
+sPgm_CmmP = toolSettings_pgm_CmmP . sToolSettings
 sPgm_F :: Settings -> String
 sPgm_F = toolSettings_pgm_F . sToolSettings
 sPgm_c :: Settings -> String
@@ -258,6 +270,10 @@ sOpt_JSP :: Settings -> [String]
 sOpt_JSP = toolSettings_opt_JSP . sToolSettings
 sOpt_JSP_fingerprint :: Settings -> Fingerprint
 sOpt_JSP_fingerprint = toolSettings_opt_JSP_fingerprint . sToolSettings
+sOpt_CmmP :: Settings -> [String]
+sOpt_CmmP = toolSettings_opt_CmmP . sToolSettings
+sOpt_CmmP_fingerprint :: Settings -> Fingerprint
+sOpt_CmmP_fingerprint = toolSettings_opt_CmmP_fingerprint . sToolSettings
 sOpt_F :: Settings -> [String]
 sOpt_F = toolSettings_opt_F . sToolSettings
 sOpt_c :: Settings -> [String]

compiler/GHC/Settings/IO.hs
@@ -87,12 +87,15 @@ initSettings top_dir = do
   cc_args_str <- getToolSetting "C compiler flags"
   cxx_args_str <- getToolSetting "C++ compiler flags"
   gccSupportsNoPie <- getBooleanSetting "C compiler supports -no-pie"
+  cmmCppSupportsG0 <- getBooleanSetting "C-- CPP supports -g0"
   cpp_prog <- getToolSetting "CPP command"
   cpp_args_str <- getToolSetting "CPP flags"
   hs_cpp_prog <- getToolSetting "Haskell CPP command"
   hs_cpp_args_str <- getToolSetting "Haskell CPP flags"
   js_cpp_prog <- getToolSetting "JavaScript CPP command"
   js_cpp_args_str <- getToolSetting "JavaScript CPP flags"
+  cmmCpp_prog <- getToolSetting "C-- CPP command"
+  cmmCpp_args_str <- getToolSetting "C-- CPP flags"
 
   platform <- either pgmError pure $ getTargetPlatform settingsFile mySettings
 
@@ -102,6 +105,7 @@ initSettings top_dir = do
       cpp_args    = map Option (unescapeArgs cpp_args_str)
       hs_cpp_args = map Option (unescapeArgs hs_cpp_args_str)
       js_cpp_args = map Option (unescapeArgs js_cpp_args_str)
+      cmmCpp_args = map Option (unescapeArgs cmmCpp_args_str)
       cc_args  = unescapeArgs cc_args_str ++ unreg_cc_args
       cxx_args = unescapeArgs cxx_args_str
 
@@ -192,10 +196,12 @@ initSettings top_dir = do
       , toolSettings_ccSupportsNoPie         = gccSupportsNoPie
       , toolSettings_useInplaceMinGW         = useInplaceMinGW
       , toolSettings_arSupportsDashL         = arSupportsDashL
+      , toolSettings_cmmCppSupportsG0        = cmmCppSupportsG0
 
       , toolSettings_pgm_L   = unlit_path
       , toolSettings_pgm_P   = (hs_cpp_prog, hs_cpp_args)
       , toolSettings_pgm_JSP = (js_cpp_prog, js_cpp_args)
+      , toolSettings_pgm_CmmP = (cmmCpp_prog, cmmCpp_args)
       , toolSettings_pgm_F   = ""
       , toolSettings_pgm_c   = cc_prog
       , toolSettings_pgm_cxx = cxx_prog
@@ -215,8 +221,10 @@ initSettings top_dir = do
       , toolSettings_opt_L       = []
       , toolSettings_opt_P       = []
       , toolSettings_opt_JSP     = []
+      , toolSettings_opt_CmmP    = []
       , toolSettings_opt_P_fingerprint   = fingerprint0
       , toolSettings_opt_JSP_fingerprint = fingerprint0
+      , toolSettings_opt_CmmP_fingerprint = fingerprint0
       , toolSettings_opt_F       = []
       , toolSettings_opt_c       = cc_args
       , toolSettings_opt_cxx     = cxx_args

compiler/GHC/SysTools/Tasks.hs
@@ -115,6 +115,8 @@ data SourceCodePreprocessor
     -- ^ Use the Haskell C preprocessor (don't remove C comments, don't break on names including single quotes)
   | SCPJsCpp
     -- ^ Use the JavaScript preprocessor (don't remove jsdoc and multiline comments)
+  | SCPCmmCpp
+    -- ^ Use the C-- preprocessor (don't emit debug information)
   deriving (Eq)
 
 -- | Run source code preprocessor.
@@ -140,11 +142,27 @@ runSourceCodePreprocessor logger tmpfs dflags preprocessor args =
     runSomething readable_name p all_args mb_env
 
   where
+    toolSettings' = toolSettings dflags
+    cmmG0 = ["-g0" | toolSettings_cmmCppSupportsG0 toolSettings']
+    -- GCC <=10 (pre commit r11-5596-g934a54180541d2) implied -dD for debug
+    -- flags by the spec snippet %{g3|ggdb3|gstabs3|gxcoff3|gvms3:-dD}.  This
+    -- means that a g0 will not override a previously-specified -g3 causing
+    -- debug info emission (see https://gcc.gnu.org/PR97989).  We're filtering
+    -- -optc here, rather than the combined command line, in order to avoid an
+    -- issue where a user has to, for some reason, override our decision.  If
+    -- they see the need to do that, they can pass -optCmmP.
+    g3Flags = ["-g3", "-ggdb3", "-gstabs3", "-gxcoff3", "-gvms3"]
+    optCFiltered = filter (`notElem` g3Flags) . opt_c
+    -- In the wild (and GHC), there is lots of code assuming that -optc gets
+    -- passed to the C-- preprocessor too.  Note that the arguments are
+    -- reversed by getOpts.
+    cAndCmmOpt dflags =  opt_CmmP dflags ++ cmmG0 ++ optCFiltered dflags
     (logger_name, pgm_getter, opt_getter, readable_name)
       = case preprocessor of
         SCPCpp -> ("cpp", pgm_cpp, opt_c, "C pre-processor")
         SCPHsCpp -> ("hs-cpp", pgm_P, opt_P, "Haskell C pre-processor")
         SCPJsCpp -> ("js-cpp", pgm_JSP, opt_JSP, "JavaScript C pre-processor")
+        SCPCmmCpp -> ("cmm-cpp", pgm_CmmP, cAndCmmOpt, "C-- C pre-processor")
 
     runSomethingResponseFileCpp
       = runSomethingResponseFile logger tmpfs (tmpDir dflags) cc_filter
@@ -156,6 +174,7 @@ runSourceCodePreprocessor logger tmpfs dflags preprocessor args =
         SCPCpp -> runSomethingResponseFileCpp
         SCPHsCpp -> runSomethingFilteredOther
         SCPJsCpp -> runSomethingFilteredOther
+        SCPCmmCpp -> runSomethingResponseFileCpp
 
 runPp :: Logger -> DynFlags -> [Option] -> IO ()
 runPp logger dflags args = traceSystoolCommand logger "pp" $ do

commit b16605e7c135f8cfd357a60c7f358132faec6a84

ghc-toolchain: Don't leave stranded a.outs when testing for -g0
This happened because, when ghc-toolchain tests for -g0, it does so by
compiling an empty program.  This compilation creates an a.out.

Since we create a temporary directory, lets place the test program
compilation in it also, so that it gets cleaned up.

Fixes: 25b0b40467d0a12601497117c0ad14e1fcab0b74
Closes: https://gitlab.haskell.org/ghc/ghc/-/issues/25203

utils/ghc-toolchain/src/GHC/Toolchain/Tools/Cpp.hs
@@ -39,10 +39,10 @@ data CmmCpp = CmmCpp { cmmCppProgram :: Program
                      }
     deriving (Show, Read, Eq, Ord)
 
-checkFlag :: String -> Program -> String -> M ()
-checkFlag conftest cpp flag = checking ("for "++flag++" support") $
+checkFlag :: String -> Program -> String -> [String] ->  M ()
+checkFlag conftest cpp flag extra_args = checking ("for "++flag++" support") $
   -- Werror to ensure that unrecognized warnings result in an error
-  callProgram cpp ["-Werror", flag, conftest]
+  callProgram cpp $ ["-Werror", flag, conftest] ++ extra_args
 -- tryFlag :: String -> Program -> String -> M [String]
 -- tryFlag conftest cpp flag =
 --   ([flag] <$ checkFlag conftest cpp flag) <|> return []
@@ -167,7 +167,7 @@ findCmmCpp progOpt cc = checking "for a Cmm preprocessor" $ do
   cmmCppSupportsG0 <- withTempDir $ \dir -> do
     let conftest = dir </> "conftest.c"
     writeFile conftest "int main(void) {}"
-    True <$ checkFlag conftest cpp "-g0" <|> return False
+    True <$ checkFlag conftest cpp "-g0" ["-o", dir </> "conftest"] <|> return False
 
   -- Always add the -E flag to the CPP, regardless of the user options
   let cmmCppProgram = foldr addFlagIfNew cpp ["-E"]