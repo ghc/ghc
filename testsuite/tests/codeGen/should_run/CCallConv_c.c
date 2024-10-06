8043537fa1d25f7402964d2559c43b0ac6434316
Sven Tennie <sven.tennie@gmail.com>
2024-10-06 17:23:21 +0200

    CCallConv test: Align argument types
    The C calling convention / standard requires that arguments and their
    values are of the same type.

--- testsuite/tests/codeGen/should_run/CCallConv_c.c
+++ testsuite/tests/codeGen/should_run/CCallConv_c.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int64_t fun8(int8_t a0, uint8_t a1, int8_t a2, int8_t a3, int8_t a4, int8_t a5,
-             int8_t a6, int8_t a7, int8_t s0, uint8_t s1) {
+             int8_t a6, int8_t a7, uint8_t s0, int8_t s1) {
   printf("fun8:\n");
   printf("a0: %#x %hhd\n", a0, a0);
   printf("a1: %#x %hhu\n", a1, a1);
@@ -12,8 +12,8 @@ int64_t fun8(int8_t a0, uint8_t a1, int8_t a2, int8_t a3, int8_t a4, int8_t a5,
   printf("a5: %#x %hhd\n", a5, a5);
   printf("a6: %#x %hhd\n", a6, a6);
   printf("a7: %#x %hhd\n", a7, a7);
-  printf("s0: %#x %hhd\n", s0, s0);
-  printf("s1: %#x %hhu\n", s1, s1);
+  printf("s0: %#x %hhu\n", s0, s0);
+  printf("s1: %#x %hhd\n", s1, s1);
 
   fflush(stdout);
 
@@ -21,7 +21,7 @@ int64_t fun8(int8_t a0, uint8_t a1, int8_t a2, int8_t a3, int8_t a4, int8_t a5,
 }
 
 int64_t fun16(int16_t a0, uint16_t a1, int16_t a2, int16_t a3, int16_t a4,
-              int16_t a5, int16_t a6, int16_t a7, int16_t s0, uint16_t s1) {
+              int16_t a5, int16_t a6, int16_t a7, uint16_t s0, int16_t s1) {
   printf("fun16:\n");
   printf("a0: %#x %hd\n", a0, a0);
   printf("a1: %#x %hu\n", a1, a1);
@@ -31,8 +31,8 @@ int64_t fun16(int16_t a0, uint16_t a1, int16_t a2, int16_t a3, int16_t a4,
   printf("a5: %#x %hd\n", a5, a5);
   printf("a6: %#x %hd\n", a6, a6);
   printf("a7: %#x %hd\n", a7, a7);
-  printf("s0: %#x %hd\n", s0, s0);
-  printf("s1: %#x %hu\n", s1, s1);
+  printf("s0: %#x %hu\n", s0, s0);
+  printf("s1: %#x %hd\n", s1, s1);
 
   fflush(stdout);
 
@@ -40,7 +40,7 @@ int64_t fun16(int16_t a0, uint16_t a1, int16_t a2, int16_t a3, int16_t a4,
 }
 
 int64_t fun32(int32_t a0, uint32_t a1, int32_t a2, int32_t a3, int32_t a4,
-              int32_t a5, int32_t a6, int32_t a7, int32_t s0, uint32_t s1) {
+              int32_t a5, int32_t a6, int32_t a7, uint32_t s0, int32_t s1) {
   printf("fun32:\n");
   printf("a0: %#x %d\n", a0, a0);
   printf("a1: %#x %u\n", a1, a1);
@@ -50,8 +50,8 @@ int64_t fun32(int32_t a0, uint32_t a1, int32_t a2, int32_t a3, int32_t a4,
   printf("a5: %#x %d\n", a5, a5);
   printf("a6: %#x %d\n", a6, a6);
   printf("a7: %#x %d\n", a7, a7);
-  printf("s0: %#x %d\n", s0, s0);
-  printf("s1: %#x %u\n", s1, s1);
+  printf("s0: %#x %u\n", s0, s0);
+  printf("s1: %#x %d\n", s1, s1);
 
   fflush(stdout);
 
