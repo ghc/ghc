f5972fc58b63262547299d544be459106fc327fc
Ben Gamari <ben@smart-cactus.org>
2024-10-14 13:29:11 -0400

    StgToByteCode: Don't assume that data con workers are nullary
    Previously StgToByteCode assumed that all data-con workers were of a
    nullary representation. This is not a valid assumption, as seen
    in #23210, where an unsaturated application of a unary data
    constructor's worker resulted in invalid bytecode. Sadly, I have not yet
    been able to reduce a minimal testcase for this.
    
    Fixes #23210.

--- compiler/GHC/StgToByteCode.hs
+++ compiler/GHC/StgToByteCode.hs
@@ -1834,19 +1834,18 @@ pushAtom d p (StgVarArg var)
         -- PUSH_G doesn't tag constructors. So we use PACK here
         -- if we are dealing with nullary constructor.
         case isDataConWorkId_maybe var of
-          Just con -> do
-            massert (isNullaryRepDataCon con)
-            return (unitOL (PACK con 0), szb)
+          Just con
+            -- See Note [LFInfo of DataCon workers and wrappers] in GHC.Types.Id.Make.
+            | isNullaryRepDataCon con -> do
+              return (unitOL (PACK con 0), szb)
 
-          Nothing
             -- see Note [Generating code for top-level string literal bindings]
-            | isUnliftedType (idType var) -> do
+          _ | isUnliftedType (idType var) -> do
               massert (idType var `eqType` addrPrimTy)
               return (unitOL (PUSH_ADDR (getName var)), szb)
 
             | otherwise -> do
               return (unitOL (PUSH_G (getName var)), szb)
-
 
 pushAtom _ _ (StgLitArg lit) = pushLiteral True lit
 
--- rts/Interpreter.c
+++ rts/Interpreter.c
@@ -1689,7 +1689,6 @@ run_BCO:
             // n_nptrs=1, n_ptrs=0.
             ASSERT(n_ptrs + n_nptrs == n_words || (n_nptrs == 1 && n_ptrs == 0));
             ASSERT(n_ptrs + n_nptrs > 0);
-            //ASSERT(n_words > 0); // We shouldn't ever need to allocate nullary constructors
             for (W_ i = 0; i < n_words; i++) {
                 con->payload[i] = (StgClosure*)SpW(i);
             }
