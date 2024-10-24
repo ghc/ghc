9d76670ec345d4bec6fce6c1ad16d568fb4b09de
Phil Hazelden <philip.hazelden@gmail.com>
2024-10-24 19:47:57 +0100

    Add test for modifiers.
    Phil note: same 47 failing tests.

---
+++ testsuite/tests/typecheck/should_compile/Modifiers.hs
@@ -0,0 +1,34 @@
+{-# OPTIONS_GHC -Wunknown-modifiers #-}
+{-# LANGUAGE LinearTypes, RequiredTypeArguments, DataKinds #-}
+
+module Modifiers where
+
+import GHC.Types (Multiplicity(..))
+
+%() data A
+
+%() %True
+  class B a
+
+%()
+  %True
+  instance B A
+
+l1 :: forall (m :: Bool) a b . a %m -> b
+l1 = undefined
+
+l2 :: forall (m :: Multiplicity) a b . a %m -> b
+l2 = undefined
+
+l3 :: a %(m :: Multiplicity) -> b %(m :: Multiplicity) -> c
+l3 = undefined
+
+-- MODS_TODO these are expected to fail, not tested in this test case:
+-- l4 :: a %m -> b
+-- l5 :: a %(m :: Multiplicity) -> b %m -> c
+
+idt :: forall t -> t -> t
+idt _ x = x
+
+visForallApp :: Int -> Int
+visForallApp = idt (Int %() -> Int) (+ 1)
