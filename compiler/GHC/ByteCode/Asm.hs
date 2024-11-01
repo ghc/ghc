07bf37e33b6987f6598a075e9dc4be5f2152665b
Ben Gamari <ben@smart-cactus.org>
2024-11-01 14:12:58 -0400

    rts: Annotate BCOs with their Name
    This introduces a new bytecode instruction, `BCO_NAME`, to aid in debugging
    bytecode execution. This instruction is injected by `mkProtoBCO` and
    captures the Haskell name of the BCO. It is then printed by the
    disassembler, allowing ready correlation with STG dumps.
    
    (cherry picked from commit 5192a75fe9b272e8b1ef290fa834714c81bd1f79)

--- compiler/GHC/ByteCode/Asm.hs
+++ compiler/GHC/ByteCode/Asm.hs
@@ -106,7 +106,7 @@ assembleBCOs interp profile proto_bcos tycons top_strs modbreaks spt_entries = d
   bcos'   <- mallocStrings interp bcos
   return CompiledByteCode
     { bc_bcos = bcos'
-    , bc_itbls =  itblenv
+    , bc_itbls = itblenv
     , bc_ffis = concatMap protoBCOFFIs proto_bcos
     , bc_strs = top_strs
     , bc_breaks = modbreaks
@@ -178,11 +178,12 @@ assembleOneBCO interp profile pbco = do
   return ubco'
 
 assembleBCO :: Platform -> ProtoBCO Name -> IO UnlinkedBCO
-assembleBCO platform (ProtoBCO { protoBCOName       = nm
-                             , protoBCOInstrs     = instrs
-                             , protoBCOBitmap     = bitmap
-                             , protoBCOBitmapSize = bsize
-                             , protoBCOArity      = arity }) = do
+assembleBCO platform
+            (ProtoBCO { protoBCOName       = nm
+                      , protoBCOInstrs     = instrs
+                      , protoBCOBitmap     = bitmap
+                      , protoBCOBitmapSize = bsize
+                      , protoBCOArity      = arity }) = do
   -- pass 1: collect up the offsets of the local labels.
   let asm = mapM_ (assembleI platform) instrs
 
