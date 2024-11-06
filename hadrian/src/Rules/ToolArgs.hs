a362b9434da723cc8e5b75f0f2df38155d2183a5
sheaf <sam.derbyshire@gmail.com>
2024-11-06 13:45:58 +0000

    Add checkExact to toolTargets
    This change means that the Hadrian multi target will include exactprint.
    In particular, this means that HLS will work on exactprint inside the GHC tree.

--- hadrian/src/Rules/ToolArgs.hs
+++ hadrian/src/Rules/ToolArgs.hs
@@ -146,6 +146,7 @@ mkToolTarget es p = do
 toolTargets :: [Package]
 toolTargets = [ cabalSyntax
               , cabal
+              , checkExact
               , compiler
               , directory
               , process
