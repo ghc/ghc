ac3aa16b536bd71f4d862723b88d5085928d813e
Luite Stegeman <stegeman@gmail.com>
2024-11-06 10:05:29 +0000

    Add flags for switching off speculative evaluation.
    We found that speculative evaluation can increase the amount of
    allocations in some circumstances. This patch adds new flags for
    selectively disabling speculative evaluation, allowing us to
    test the effect of the optimization.
    
    The new flags are:
    
      -fspec-eval
         globally enable speculative evaluation
    
      -fspec-eval-dictfun
         enable speculative evaluation for dictionary functions (no effect
         if speculative evaluation is globally disabled)
    
    The new flags are on by default for all optimisation levels.
    
    See #25284
    
    (cherry picked from commit 2309975247543a4f77009ea5c3c7a8ebe06dc60b)

--- compiler/GHC/Driver/DynFlags.hs
+++ compiler/GHC/Driver/DynFlags.hs
@@ -1290,6 +1290,8 @@ optLevelFlags -- see Note [Documenting optimisation flags]
 --   RegsGraph suffers performance regression. See #7679
 --  , ([2],     Opt_StaticArgumentTransformation)
 --   Static Argument Transformation needs investigation. See #9374
+    , ([0,1,2], Opt_SpecEval)
+    , ([0,1,2], Opt_SpecEvalDictFun)
     ]
 
 
--- compiler/GHC/Driver/Flags.hs
+++ compiler/GHC/Driver/Flags.hs
@@ -664,6 +664,9 @@ data GeneralFlag
    | Opt_NumConstantFolding
    | Opt_CoreConstantFolding
    | Opt_FastPAPCalls                  -- #6084
+   | Opt_SpecEval
+   | Opt_SpecEvalDictFun   -- See Note [Controlling Speculative Evaluation]
+
 
    -- Inference flags
    | Opt_DoTagInferenceChecks
@@ -902,6 +905,8 @@ optimisationFlags = EnumSet.fromList
    , Opt_WorkerWrapper
    , Opt_WorkerWrapperUnlift
    , Opt_SolveConstantDicts
+   , Opt_SpecEval
+   , Opt_SpecEvalDictFun
    ]
 
 -- | The set of flags which affect code generation and can change a program's
