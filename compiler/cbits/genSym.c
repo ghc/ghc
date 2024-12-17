63d74f4d15ecaa8d711bd12fa64f77fce72b5f79
Ben Gamari <ben@well-typed.com>
2024-12-17 11:48:52 -0500

    compiler: Fix CPP guards around ghc_unique_counter64
    The `ghc_unique_counter64` symbol was introduced in the RTS in the
    64-bit unique refactor (!10568) which has been backported to %9.6.7 and
    %9.8.4. Update the CPP to reflect this.
    
    Fixes #25576.

--- compiler/cbits/genSym.c
+++ compiler/cbits/genSym.c
@@ -9,6 +9,18 @@
 //
 // The CPP is thus about the RTS version GHC is linked against, and not the
 // version of the GHC being built.
-#if !MIN_VERSION_GLASGOW_HASKELL(9,9,0,0)
+
+#if MIN_VERSION_GLASGOW_HASKELL(9,9,0,0)
+// Unique64 patch was present in 9.10 and later
+#define HAVE_UNIQUE64 1
+#elif !MIN_VERSION_GLASGOW_HASKELL(9,9,0,0) && MIN_VERSION_GLASGOW_HASKELL(9,8,4,0)
+// Unique64 patch was backported to 9.8.4
+#define HAVE_UNIQUE64 1
+#elif !MIN_VERSION_GLASGOW_HASKELL(9,7,0,0) && MIN_VERSION_GLASGOW_HASKELL(9,6,7,0)
+// Unique64 patch was backported to 9.6.7
+#define HAVE_UNIQUE64 1
+#endif
+
+#if !defined(HAVE_UNIQUE64)
 HsWord64 ghc_unique_counter64 = 0;
 #endif
