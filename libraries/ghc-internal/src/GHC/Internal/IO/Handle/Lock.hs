ea2c93f9076f6a4711a22dd2ef7ecef0cb22e863
Ben Gamari <ben@well-typed.com>
2025-01-07 15:04:57 +0000

    base: Update description of locking behavior

--- libraries/ghc-internal/src/GHC/Internal/IO/Handle/Lock.hs
+++ libraries/ghc-internal/src/GHC/Internal/IO/Handle/Lock.hs
@@ -35,14 +35,20 @@ import GHC.Internal.IO.Handle.Lock.NoOp
 --
 -- Things to be aware of:
 --
--- 1) This function may block inside a C call. If it does, in order to be able
+-- 1. This function may block inside a C call. If it does, in order to be able
 -- to interrupt it with asynchronous exceptions and/or for other threads to
 -- continue working, you MUST use threaded version of the runtime system.
 --
--- 2) The implementation uses 'LockFileEx' on Windows and 'flock' otherwise,
+-- 2. The implementation uses relies on any of a number of locking
+-- facilities, depending upon what the platform supports:
+--
+--   * 'LockFileEx' is used on Windows
+--   * On platforms that support it we use the @F_OFD_SETLK@ and @F_OFD_SETLKW@ @fnctl@s.
+--   * Otherwise we use @flock@
+--
 -- hence all of their caveats also apply here.
 --
--- 3) On non-Windows platforms that don't support 'flock' (e.g. Solaris) this
+-- 3. On non-Windows platforms that don't support 'flock' (e.g. Solaris) this
 -- function throws 'FileLockingNotImplemented'. We deliberately choose to not
 -- provide fcntl based locking instead because of its broken semantics.
 --
