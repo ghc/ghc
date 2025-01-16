a59d6b6ba94bc7ccb8b78b32e375190620a661ac
Ben Gamari <ben@well-typed.com>
2025-01-16 15:39:04 -0500

    Revert "Division by constants optimization"
    This appears to be responsible for the regression described in #25653.
    
    This reverts commit daff1e30219d136977c71f42e82ccc58c9013cfb.

--- testsuite/tests/numeric/should_run/mul2.hs
+++ testsuite/tests/numeric/should_run/mul2.hs
@@ -1,7 +1,7 @@
 
 {-# LANGUAGE MagicHash, UnboxedTuples #-}
+
 import GHC.Exts
-import GHC.Int
 import GHC.Word
 import Data.Bits
 
@@ -9,9 +9,6 @@ main :: IO ()
 main = do f 5 6
           f 0xFD94E3B7FE36FB18 49
           f 0xFD94E3B7FE36FB18 0xFC1D8A3BFB29FC6A
-          g 5 6
-          g 0xFD94E3B7FE36FB18 49
-          g 0xFD94E3B7FE36FB18 0xFC1D8A3BFB29FC6A
 
 f :: Word -> Word -> IO ()
 f wx@(W# x) wy@(W# y)
@@ -26,20 +23,4 @@ f wx@(W# x) wy@(W# y)
                     putStrLn ("High: " ++ show wh)
                     putStrLn ("Low: " ++ show wl)
                     putStrLn ("Result: " ++ show (r :: Integer))
-{-# INLINE f #-}
 
-g :: Int -> Int -> IO ()
-g ix@(I# x) iy@(I# y)
-    = do putStrLn "-----"
-         putStrLn ("Doing " ++ show ix ++ " * " ++ show iy)
-         case x `timesInt2#` y of
-             (# needsHigh, h, l #) ->
-                 do let ih = I# h
-                        il = I# l
-                        r = shiftL (fromIntegral ih) (bitSize ih)
-                          .|. (fromIntegral @Word @Integer $ fromIntegral @Int @Word il)
-                    putStrLn ("Needs high: " ++ show (isTrue# needsHigh))
-                    putStrLn ("High: " ++ show ih)
-                    putStrLn ("Low: " ++ show il)
-                    putStrLn ("Result: " ++ show (r :: Integer))
-{-# INLINE g #-}
