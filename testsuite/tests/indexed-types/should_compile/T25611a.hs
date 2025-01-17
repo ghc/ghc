0f7588febe07ae52ccddb929bd1b43e742fb139d
Patrick <fwy996602672@gmail.com>
2025-01-17 19:16:21 +0800

    update T25611a

--- testsuite/tests/indexed-types/should_compile/T25611a.hs
+++ testsuite/tests/indexed-types/should_compile/T25611a.hs
@@ -12,6 +12,6 @@ data family Fix0 :: (k -> Type) -> k
 newtype instance Fix0 f = In0 { out0 :: f (Fix0 f) }
 
 -- This is the GADT newtype instance case
--- currently not enabled since !9116 (closed) impose `A newtype must not be a GADT`
--- data family Fix2 :: (k -> Type) -> k
--- newtype instance Fix2 f where In2 :: f (Fix2 f) -> Fix2 f
+-- enabled since !13809
+data family Fix2 :: (k -> Type) -> k
+newtype instance Fix2 f where In2 :: f (Fix2 f) -> Fix2 f
