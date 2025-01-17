11941cee16fbb47a39d7a1f566859ccb2d2fe755
ARATA Mizuki <minorinoki@gmail.com>
2025-01-17 19:05:11 +0900

    x86 NCG: Use correct format for MOVD in the implementation of unpackInt64X2#
    MOVD takes the input format.
    
    Fixes #25658
    
    (cherry picked from commit 5a8f35bd5d9735d7846a53bc9d08d8eeb67bccb5)

---
+++ testsuite/tests/simd/should_run/T25658.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE MagicHash, UnboxedTuples, ExtendedLiterals #-}
+import GHC.Int
+import GHC.Prim
+
+test :: (Int64X2# -> Int64X2# -> Int64X2#) -> IO ()
+test f = do
+  let a = packInt64X2# (# 0#Int64, 11#Int64 #)
+      b = packInt64X2# (# 22#Int64, 33#Int64 #)
+      c = f a b
+      (# x0, x1 #) = unpackInt64X2# a
+      (# y0, y1 #) = unpackInt64X2# b
+      (# z0, z1 #) = unpackInt64X2# c
+  putStrLn $ "a = " ++ show (I64# x0, I64# x1)
+  putStrLn $ "b = " ++ show (I64# y0, I64# y1)
+  putStrLn $ "c = " ++ show (I64# z0, I64# z1)
+{-# NOINLINE test #-}
+
+main :: IO ()
+main = test (\a _ -> a)
