2f49fb4133348423d2f45a6b5f1a06feb1ac8eb9
Sven Tennie <sven.tennie@gmail.com>
2025-01-25 15:15:27 +0100

    Simplify VID

--- compiler/GHC/CmmToAsm/RV64/Ppr.hs
+++ compiler/GHC/CmmToAsm/RV64/Ppr.hs
@@ -750,8 +750,8 @@ pprInstr platform instr = case instr of
                 | isIntRegOp o2 -> configVec fmt $$ op2 (text "\tvmv" <> dot <> opToVInstrSuffix o1 <> dot <> text "x") o1 o2
                 | isVectorRegOp o1 && isVectorRegOp o2 -> configVec fmt $$ op2 (text "\tvmv" <> dot <> opToVInstrSuffix o1 <> dot <> text "v") o1 o2
                 | True -> pprPanic "RV64.pprInstr - impossible vector move (VMV)" (pprOp platform o1 <+> pprOp platform o2 <+> text "fmt" <> colon <> (text . show) fmt)
-  -- TODO: Remove o2 from constructor
-  VID fmt o1 -> configVec fmt $$ op1 (text "\tvid.v") o1
+  VID op@(OpReg fmt _reg) -> configVec fmt $$ op1 (text "\tvid.v") op
+  VID op -> pprPanic "RV64.pprInstr - VID can only work on registers." (pprOp platform op)
   -- TODO: This expects int register as third operand: Generalize by calculating
   -- the instruction suffix (".vx")
   VMSEQ fmt o1 o2 o3 -> configVec fmt $$ op3 (text "\tvmseq.vx") o1 o2 o3
