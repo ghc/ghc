8f6cc90c2de9937af465d0c88344eb8c998d5e41
Matthew Pickering <matthewtpickering@gmail.com>
2025-01-30 13:15:33 +0000

    perf: Speed up the bytecode assembler
    This commit contains a number of optimisations to the bytecode
    assembler. In programs which generate a large amount of bytecode, the
    assembler is called a lot of times on many instructions.
    
    1. Specialise the assembleI function for the two intepreters to avoid
    having to materialise the intermediate free-monad like structure.
    2. Directly compute the UArray and SmallArray needed rather than going
       via the intermediate SizedSeq
    3. Use optimised monads
    4. Define unrolled "any" and "mapM6" functions which can be inlined
       and avoid calling recursive functions.
    
    The resulting generated code is much more direct.
    
    Before:
    
    ./ByteCodeAsm /home/matt/ghc-profiling-light/_build/stage1/lib/ +RTS  -s
      48,923,125,664 bytes allocated in the heap
         678,221,152 bytes copied during GC
             395,648 bytes maximum residency (2 sample(s))
              50,040 bytes maximum slop
                   6 MiB total memory in use (0 MiB lost due to fragmentation)
    
                                         Tot time (elapsed)  Avg pause  Max pause
      Gen  0     11731 colls,     0 par    0.419s   0.425s     0.0000s    0.0004s
      Gen  1         2 colls,     0 par    0.001s   0.001s     0.0007s    0.0012s
    
      INIT    time    0.000s  (  0.000s elapsed)
      MUT     time    6.466s  (  6.484s elapsed)
      GC      time    0.421s  (  0.426s elapsed)
      EXIT    time    0.000s  (  0.000s elapsed)
      Total   time    6.887s  (  6.910s elapsed)
    
    After:
    
       1,518,321,200 bytes allocated in the heap
           4,299,552 bytes copied during GC
             322,288 bytes maximum residency (2 sample(s))
              50,280 bytes maximum slop
                   6 MiB total memory in use (0 MiB lost due to fragmentation)
    
                                         Tot time (elapsed)  Avg pause  Max pause
      Gen  0       369 colls,     0 par    0.003s   0.003s     0.0000s    0.0002s
      Gen  1         2 colls,     0 par    0.001s   0.001s     0.0007s    0.0012s
    
      INIT    time    0.001s  (  0.001s elapsed)
      MUT     time    0.465s  (  0.466s elapsed)
      GC      time    0.004s  (  0.004s elapsed)
      EXIT    time    0.000s  (  0.000s elapsed)
      Total   time    0.470s  (  0.471s elapsed)

---
+++ testsuite/tests/perf/should_run/ByteCodeAsm.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import GHC.Driver.Session
+import GHC
+import qualified GHC.LanguageExtensions as LangExt
+
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import System.Environment (getArgs)
+
+import GHC.ByteCode.Asm ( assembleBCO )
+import GHC.ByteCode.Instr
+import Control.Monad
+import GHC.Builtin.Names
+
+-- Testing the performance of the bytecode assembler
+
+-- A nonsensical byte-code program
+instrs = [ STKCHECK 1234
+         , PUSH_L 1
+         , PUSH_LL 1 2
+         , PUSH_LLL 2 3 4
+         , PUSH_LLL 2 3 4
+         , PUSH_LLL 2 3 4
+         , PUSH_LLL 2 3 4
+         , PUSH_LLL 2 3 4
+         , PUSH8 0
+         , PUSH16 15
+         , PUSH32 29
+         , PUSH_PAD8
+         , PUSH_APPLY_N
+         , PUSH_APPLY_V
+         , PUSH_APPLY_F
+         , PUSH_APPLY_D
+         , PUSH_APPLY_L
+         , PUSH_APPLY_P
+         , PUSH_APPLY_PP
+         , PUSH_APPLY_PPP
+         , PUSH_APPLY_PPPP
+         , PUSH_APPLY_PPPPP
+         , PUSH_APPLY_PPPPPP
+         , TESTLT_I 100 (LocalLabel 0)
+         , TESTEQ_I 100 (LocalLabel 0)
+         ]
+         ++ [ LABEL (LocalLabel n) | n <- [0..50] ]
+         ++ [ TESTEQ_I64 n (LocalLabel 49) | n <- [1243 .. 1253 + 50 ]]
+         ++ [ ENTER ]
+         ++ [ SLIDE x n | x <- [0..5], n <- [0..10] ]
+         ++ [ PUSH_G appAName | _ <- [0..100] ]
+         ++ [ PUSH_BCO fake_proto2 ]
+
+fake_proto = ProtoBCO appAName instrs [] 0 0 (Left []) []
+
+instrs2 = [ STKCHECK 77, UNPACK 4, SLIDE 0 4, ENTER ]
+
+fake_proto2 = ProtoBCO appAName instrs2 [] 0 0 (Left []) []
+
+main :: IO ()
+main = do
+  [libdir] <- getArgs
+  runGhc (Just libdir) $ do
+    dflags <- getSessionDynFlags
+    let platform = targetPlatform dflags
+
+    -- ~1s on my machine
+    liftIO $ replicateM_ 100000 (assembleBCO platform fake_proto)
