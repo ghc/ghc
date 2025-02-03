d7a16ecf03dbcdb40624b2b3644a06883a2e0346
Alan Zimmerman <alan.zimm@gmail.com>
2025-02-03 20:43:53 +0000

    Layering is now correct
    - GHC lexer, emits CPP tokens
    - accumulated in Preprocessor state
    - Lexed by CPP lexer, CPP command extracted, tokens concated with
      spaces (to get rid of token pasting via comments)
    - if directive lexed and parsed by CPP lexer/parser, and evaluated

--- utils/check-cpp/ParsePP.hs
+++ utils/check-cpp/ParsePP.hs
@@ -1,8 +1,10 @@
 module ParsePP (
-    parseCppParser,
-    plusTimesExpr,
+    -- parseCppParser,
+    -- plusTimesExpr,
+    cppLex,
+    parseDirective,
     -- testing, delete
-    cppDefinition,
+    -- cppDefinition,
 ) where
 
 import Data.Char
@@ -19,9 +21,12 @@ import qualified Text.Parsec.Expr as E
 import Text.Parsec.Language (emptyDef)
 import Text.Parsec.Prim as PS hiding (token)
 import qualified Text.Parsec.Token as P
+import Data.List
 
 import qualified Parser
 import Types
+import ParserM (Token(..),init_state)
+import Lexer
 
 -- =====================================================================
 -- First parse to CPP tokens, using a C++-like language spec
@@ -52,94 +57,128 @@ exprDef =
 type CppParser = Parsec String ()
 
 
--- parseDirective :: String -> Either String CppDirective
--- parseDirective = Parser.parseDirective
+-- Parse a CPP directive, using tokens from the CPP lexer
+parseDirective :: String -> Either String CppDirective
+parseDirective s =
+  case cppLex s of
+    Left e -> Left e
+    Right toks ->
+      case map t_str toks of
+        ("#":"define":ts) -> cppDefine ts
+        ("#":"include":ts) -> Right $ cppInclude ts
+        ("#":"if":ts) -> Right $ cppIf ts
+        ("#":"ifndef":ts) -> Right $ cppIfndef ts
+        ("#":"ifdef":ts) -> Right $ cppIfdef ts
+        ("#":"else":ts) -> Right $ cppElse ts
+        ("#":"endif":ts) -> Right $ cppEndif ts
+        other -> Left ("unexpected directive: " ++ (intercalate " " other))
 
-parseDirectiveOld :: String -> Either Parsec.ParseError CppDirective
-parseDirectiveOld = parseCppParser cppDirective
 
-parseCppParser :: CppParser a -> String -> Either Parsec.ParseError a
-parseCppParser p = PS.parse p ""
+cppDefine [] = Left "error:empty #define directive"
+cppDefine (n:ts) = Right $ CppDefine n (intercalate " " ts)
 
--- TODO: delete this
-cppDefinition :: CppParser (String, [String])
-cppDefinition = do
-    _ <- PS.char '#'
-    _ <- whiteSpace
-    eToken "define"
-    name <- cppToken
-    definition <- cppTokens
-    return (name, definition)
+cppInclude ts = CppInclude (intercalate " " ts)
+cppIf ts = CppIf (intercalate " " ts)
+cppIfdef ts = CppIfdef (intercalate " " ts)
+cppIfndef ts = CppIfndef (intercalate " " ts)
+cppElse _ts = CppElse
+cppEndif _ts = CppEndif
 
-cppDirective :: CppParser CppDirective
-cppDirective = do
-    _ <- PS.char '#'
-    _ <- whiteSpace
-    choice
-        [ cppKw "define" >> cmdDefinition
-        , try $ cppKw "include" >> cmdInclude
-        , try $ cppKw "ifdef" >> cmdIfdef
-        , try $ cppKw "ifndef" >> cmdIfndef
-        -- , try $ cppKw "if" >> cmdIf
-        , try $ cppKw "else" >> return CppElse
-        , cppKw "endif" >> return CppEndif
-        -- , cppKw "elif" CppElifKw
-        -- , cppKw "undef" CppUndefKw
-        -- , cppKw "error" CppErrorKw
-        ]
 
-cmdInclude :: CppParser CppDirective
-cmdInclude = do
-    _ <- string "\""
-    filename <- many1 (satisfy (\c -> not (isSpace c || c == '"')))
-    _ <- string "\""
-    return $ CppInclude filename
+-- ---------------------------------------------------------------------
 
-cmdDefinition :: CppParser CppDirective
-cmdDefinition = do
-    name <- cppToken
-    CppDefine name <$> cppTokens
+cppLex :: String -> Either String [Token]
+cppLex s = case lexCppTokenStream s init_state of
+    Left err -> Left err
+    Right (_inp, _st, toks) -> Right toks
 
-cmdIfdef :: CppParser CppDirective
-cmdIfdef = CppIfdef <$> cppToken
+-- ---------------------------------------------------------------------
 
-cmdIfndef :: CppParser CppDirective
-cmdIfndef = CppIfndef <$> cppToken
+-- parseDirectiveOld :: String -> Either Parsec.ParseError CppDirective
+-- parseDirectiveOld = parseCppParser cppDirective
+
+-- parseCppParser :: CppParser a -> String -> Either Parsec.ParseError a
+-- parseCppParser p = PS.parse p ""
+
+-- -- TODO: delete this
+-- cppDefinition :: CppParser (String, [String])
+-- cppDefinition = do
+--     _ <- PS.char '#'
+--     _ <- whiteSpace
+--     eToken "define"
+--     name <- cppToken
+--     definition <- cppTokens
+--     return (name, definition)
+
+-- cppDirective :: CppParser CppDirective
+-- cppDirective = do
+--     _ <- PS.char '#'
+--     _ <- whiteSpace
+--     choice
+--         [ cppKw "define" >> cmdDefinition
+--         , try $ cppKw "include" >> cmdInclude
+--         , try $ cppKw "ifdef" >> cmdIfdef
+--         , try $ cppKw "ifndef" >> cmdIfndef
+--         -- , try $ cppKw "if" >> cmdIf
+--         , try $ cppKw "else" >> return CppElse
+--         , cppKw "endif" >> return CppEndif
+--         -- , cppKw "elif" CppElifKw
+--         -- , cppKw "undef" CppUndefKw
+--         -- , cppKw "error" CppErrorKw
+--         ]
+
+-- cmdInclude :: CppParser CppDirective
+-- cmdInclude = do
+--     _ <- string "\""
+--     filename <- many1 (satisfy (\c -> not (isSpace c || c == '"')))
+--     _ <- string "\""
+--     return $ CppInclude filename
+
+-- cmdDefinition :: CppParser CppDirective
+-- cmdDefinition = do
+--     name <- cppToken
+--     CppDefine name <$> cppTokens
+
+-- cmdIfdef :: CppParser CppDirective
+-- cmdIfdef = CppIfdef <$> cppToken
+
+-- cmdIfndef :: CppParser CppDirective
+-- cmdIfndef = CppIfndef <$> cppToken
 
 -- cmdIf :: CppParser CppDirective
 -- cmdIf = CppIf <$> cppTokens
 
-cppKw :: String -> CppParser ()
-cppKw kw = void $ lexeme (PS.string kw)
+-- cppKw :: String -> CppParser ()
+-- cppKw kw = void $ lexeme (PS.string kw)
 
-cppComment :: CppParser ()
-cppComment = do
-    _ <- PS.string "/*"
-    _ <- PS.manyTill PS.anyChar (PS.try (PS.string "*/"))
-    return ()
+-- cppComment :: CppParser ()
+-- cppComment = do
+--     _ <- PS.string "/*"
+--     _ <- PS.manyTill PS.anyChar (PS.try (PS.string "*/"))
+--     return ()
 
-whiteSpace :: CppParser ()
-whiteSpace = do
-    _ <- PS.many (PS.choice [cppComment, void PS.space])
-    return ()
+-- whiteSpace :: CppParser ()
+-- whiteSpace = do
+--     _ <- PS.many (PS.choice [cppComment, void PS.space])
+--     return ()
 
-lexeme :: CppParser a -> CppParser a
-lexeme p = p <* whiteSpace
+-- lexeme :: CppParser a -> CppParser a
+-- lexeme p = p <* whiteSpace
 
-cppToken :: CppParser String
-cppToken = lexeme (PS.many1 (PS.satisfy (not . isSpace)))
+-- cppToken :: CppParser String
+-- cppToken = lexeme (PS.many1 (PS.satisfy (not . isSpace)))
 
-cppTokens :: CppParser [String]
-cppTokens = PS.many cppToken
+-- cppTokens :: CppParser [String]
+-- cppTokens = PS.many cppToken
 
--- -------------------------------------
+-- -- -------------------------------------
 
-plusTimesExpr :: CppParser Expr
-plusTimesExpr = E.buildExpressionParser eTable eTerm
+-- plusTimesExpr :: CppParser Expr
+-- plusTimesExpr = E.buildExpressionParser eTable eTerm
 
-eTable :: [[E.Operator String () Data.Functor.Identity.Identity Expr]]
-eTable =
-  []
+-- eTable :: [[E.Operator String () Data.Functor.Identity.Identity Expr]]
+-- eTable =
+--   []
     -- -- Via https://learn.microsoft.com/en-us/cpp/cpp/cpp-built-in-operators-precedence-and-associativity?view=msvc-170
     -- [ [E.Infix (Times <$ symbol "*") E.AssocLeft]
     -- , [E.Infix (Plus <$ symbol "+") E.AssocLeft]
@@ -154,17 +193,17 @@ eTable =
     -- , [E.Infix (BinOp LogicalOr <$ symbol "||") E.AssocLeft]
     -- ]
 
-eTerm :: CppParser Expr
-eTerm =
-    eVariable -- <|> pteNum
-        <|> pteParens
-        <|> eInteger
+-- eTerm :: CppParser Expr
+-- eTerm =
+--     eVariable -- <|> pteNum
+--         <|> pteParens
+--         <|> eInteger
 
-pteParens :: CppParser Expr
-pteParens = Parens <$> between (symbol "(") (symbol ")") plusTimesExpr
+-- pteParens :: CppParser Expr
+-- pteParens = Parens <$> between (symbol "(") (symbol ")") plusTimesExpr
 
-symbol :: String -> CppParser String
-symbol s = lexeme $ string s
+-- symbol :: String -> CppParser String
+-- symbol s = lexeme $ string s
 
 -- -- -------------------------------------
 
@@ -189,26 +228,26 @@ symbol s = lexeme $ string s
 --     op <- P.operator (trace "foo" lexer)
 --     return $ trace ("op=" ++ show op) LogicalOr
 
--- TODO: Do we need this? the expression should be fully expanded by
--- the time we get it
-eVariable :: CppParser Expr
-eVariable = do
-    v <- P.identifier lexer
-    return $ Var v
+-- -- TODO: Do we need this? the expression should be fully expanded by
+-- -- the time we get it
+-- eVariable :: CppParser Expr
+-- eVariable = do
+--     v <- P.identifier lexer
+--     return $ Var v
 
-eToken :: String -> CppParser ()
-eToken = P.reserved lexer
+-- eToken :: String -> CppParser ()
+-- eToken = P.reserved lexer
 
-eInteger :: CppParser Expr
-eInteger = IntVal <$> integer
+-- eInteger :: CppParser Expr
+-- eInteger = IntVal <$> integer
 
-integer :: CppParser Int
-integer = read <$> lexeme (many1 digit)
+-- integer :: CppParser Int
+-- integer = read <$> lexeme (many1 digit)
 
 -- ---------------------------------------------------------------------
 
 doATest :: String -> Either String CppDirective
-doATest str = Parser.parseDirective str
+doATest str = parseDirective str
 -- doATest str = parseDirectiveOld str
 
 t0 :: Either String CppDirective
@@ -236,4 +275,4 @@ t2 = doATest "# if ((m1) <  1 || (m1) == 1 && (m2) <  7 || (m1) == 1 && (m2) == 
 --           (Comp CmpLtE (Var "m") (IntVal 0)))))
 
 t3 :: Either String CppDirective
-t3 = Parser.parseDirective "# if FOO == 4"
+t3 = parseDirective "# if FOO == 4"
