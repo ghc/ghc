fd87a14bcfaee9f835ab73608e9f22c22a5121b3
Serge S. Gulin <gulin.serge@gmail.com>
2025-02-06 15:15:45 +0000

    Remove trailing whtespace to make CI greener

--- compiler/GHC/Driver/Session.hs
+++ compiler/GHC/Driver/Session.hs
@@ -3611,8 +3611,8 @@ makeDynFlagsConsistent dflags
  , Nothing <- outputFile dflags
  = pgmError "--output must be specified when using --merge-objs"
 
- | platformTablesNextToCode platform 
-   && os == OSMinGW32 
+ | platformTablesNextToCode platform
+   && os == OSMinGW32
    && arch == ArchAArch64
     = case backendCodeOutput (backend dflags) of
         LlvmCodeOutput -> pgmError "-fllvm is incompatible with enabled TablesNextToCode at Windows Aarch64"
