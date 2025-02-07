b1b9a12853754ed0bd9060aa114538f1e04a8e79
Patrick <fwy996602672@gmail.com>
2025-02-07 22:13:34 +0800

    align wildcard with named typevar on wether it is skolem

--- testsuite/tests/indexed-types/should_compile/T17536c.hs
+++ testsuite/tests/indexed-types/should_compile/T17536c.hs
@@ -11,7 +11,15 @@ import GHC.Exts
 
 type R :: forall (r :: RuntimeRep) -> TYPE r -> Type
 type family R r a where
-  R _ _ = Int
+  R r a = Int
 
 r :: R FloatRep Float# -> Int
 r x = x
+
+-- make sure wildcard and non-wildcard type variables are treated the same
+type R1 :: forall (r :: RuntimeRep) -> TYPE r -> Type
+type family R1 r a where
+  R1 r a = Int
+
+r1 :: R1 FloatRep Float# -> Int
+r1 x = x
