bcb70fccbd926454c683954200abe02483a8312b
Cheng Shao <terrorjack@type.dance>
2025-02-14 00:16:32 +0000

    compiler: do not allocate strings in bytecode assembler

--- compiler/GHC/ByteCode/Types.hs
+++ compiler/GHC/ByteCode/Types.hs
@@ -199,9 +199,7 @@ data BCONPtr
   -- | A reference to a top-level string literal; see
   -- Note [Generating code for top-level string literal bindings] in GHC.StgToByteCode.
   | BCONPtrAddr  !Name
-  -- | Only used internally in the assembler in an intermediate representation;
-  -- should never appear in a fully-assembled UnlinkedBCO.
-  -- Also see Note [Allocating string literals] in GHC.ByteCode.Asm.
+  -- | A top-level string literal.
   | BCONPtrStr   !ByteString
 
 instance NFData BCONPtr where
