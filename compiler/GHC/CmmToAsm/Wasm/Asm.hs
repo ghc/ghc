dff0870b6bc7834c9d9e38b120c4f60021a0b169
Fangyi Zhou <me@fangyi.io>
2025-02-21 16:31:08 +0000

    wasm: use primitive opcodes for fabs and sqrt
    - Add new `WasmInstr` constructor `WasmSqrt` for sqrt, corresponding to
      primitivie operations in wasm.
    - When lowering CallishMachOp, use `WasmAbs` and `WasmSqrt` for F32 and
      F64 fabs and sqrt.
    
    (cherry picked from commit 64b0d4d061902c0f7443355fa4877ff6aad946d5)

--- compiler/GHC/CmmToAsm/Wasm/Asm.hs
+++ compiler/GHC/CmmToAsm/Wasm/Asm.hs
@@ -361,6 +361,7 @@ asmTellWasmInstr ty_word instr = case instr of
   WasmF32DemoteF64 -> asmTellLine "f32.demote_f64"
   WasmF64PromoteF32 -> asmTellLine "f64.promote_f32"
   WasmAbs ty -> asmTellLine $ asmFromWasmType ty <> ".abs"
+  WasmSqrt ty -> asmTellLine $ asmFromWasmType ty <> ".sqrt"
   WasmNeg ty -> asmTellLine $ asmFromWasmType ty <> ".neg"
   WasmCond t -> do
     asmTellLine "if"
