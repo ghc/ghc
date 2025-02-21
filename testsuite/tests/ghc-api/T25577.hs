3b037e97b84f41a9b47fcedd040322f33ff40f20
Ben Gamari <ben@well-typed.com>
2025-02-20 19:06:34 -0500

    testsuite: Add testcase for #25577

---
+++ testsuite/tests/ghc-api/T25577.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import GHC
+import GHC.Paths
+import Unsafe.Coerce
+import Control.Monad.IO.Class
+import System.Environment (getArgs)
+
+main :: IO ()
+main = do
+    [libdir] <- getArgs
+    runGhc (Just libdir) run
+
+run :: Ghc ()
+run = do
+    dyn_flags <- getSessionDynFlags
+    _ <- setSessionDynFlags dyn_flags
+
+    setContext [ IIDecl . simpleImportDecl . mkModuleName $ "Prelude"
+               , IIDecl . simpleImportDecl . mkModuleName $ "Unsafe.Coerce" ]
+
+    wrong
+
+wrong :: Ghc ()
+wrong = do
+    let chck = "5.5626902089526504e-303 :: Double"
+    v <- compileExpr chck
+
+    liftIO $ do
+        putStr "Direct: "
+        print (5.5626902089526504e-303 :: Double)
+        putStr "API:    "
+        print (unsafeCoerce v :: Double)
+
+    let chck2 = "5.56269020895265e-303 :: Double"
+    v2 <- compileExpr chck2
+
+    liftIO $ do
+        putStr "Direct: "
+        print (5.56269020895265e-303 :: Double)
+        putStr "API:    "
+        print (unsafeCoerce v2 :: Double)
