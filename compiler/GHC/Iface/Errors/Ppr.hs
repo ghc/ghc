baa1d00b31749f6bbcb5d2a3aa413779eec6fa5c
Sylvain Henry <sylvain@haskus.fr>
2025-02-26 11:24:33 +0100

    Display unit-id on module name mistmatch
    https://github.com/stable-haskell/ghc/issues/19

--- compiler/GHC/Iface/Errors/Ppr.hs
+++ compiler/GHC/Iface/Errors/Ppr.hs
@@ -336,8 +336,8 @@ hiModuleNameMismatchWarn requested_mod read_mod
             ]
         ]
  | otherwise =
-  -- ToDo: This will fail to have enough qualification when the package IDs
-  -- are the same
+  -- Display fully qualified unit names by enabling ppr-debug
+  updSDocContext (\ctx -> ctx { sdocPprDebug = True}) $
   withPprStyle (mkUserStyle alwaysQualify AllTheWay) $
     -- we want the Modules below to be qualified with package names,
     -- so reset the NamePprCtx setting.
@@ -345,7 +345,6 @@ hiModuleNameMismatchWarn requested_mod read_mod
          , ppr requested_mod
          , text "differs from name found in the interface file"
          , ppr read_mod
-         , parens (text "if these names look the same, try again with -dppr-debug")
          ]
 
 dynamicHashMismatchError :: Module -> ModLocation -> SDoc
