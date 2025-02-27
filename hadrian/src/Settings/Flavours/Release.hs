cf4972430f38429b07ca978faef21d3d4a17d12c
Matthew Pickering <matthewtpickering@gmail.com>
2025-02-27 12:28:15 +0000

    Disable self recomp in release flavour
    The interface files that we distribute should not contain any
    information which is used by the recompilation checking logic since
    source file will never be compiled again.
    
    I am not 100% sure this won't cause unexpected issues, there many be
    downstream consumers which are incorrectly using the information from
    interfaces, but this commit can be reverted if we detect issues.

--- hadrian/src/Settings/Flavours/Release.hs
+++ hadrian/src/Settings/Flavours/Release.hs
@@ -4,4 +4,8 @@ import Settings.Flavours.Performance
 import Flavour
 
 releaseFlavour :: Flavour
-releaseFlavour = enableHaddock performanceFlavour { name = "release" }
+releaseFlavour =
+  -- 1. These interface files will be distributed and the source files never recompiled.
+  disableSelfRecompInfo
+  -- 2. Include documentation in the interface for tools such as haddock and HLS to use
+  $ enableHaddock performanceFlavour { name = "release" }
