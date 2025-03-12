4de3195ff3c483459558cc8dff7b929e52cabb5c
Patrick <fwy996602672@gmail.com>
2025-03-12 08:29:52 +0800

    update tests to reflect changes in error messages

--- testsuite/tests/indexed-types/should_fail/T14230a.hs
+++ testsuite/tests/indexed-types/should_fail/T14230a.hs
@@ -8,7 +8,7 @@ module T14230a where
 import Data.Kind
 
 class C a where
-  data CD k (a :: k) :: k -> *
+  data CD k (a :: k) :: k -> Type
 
 instance C (Maybe a) where
-  data CD k (a :: k -> *) :: (k -> *) -> *
+  data CD k (a :: Type) :: Type -> Type
--- testsuite/tests/typecheck/should_compile/T25725.hs
+++ testsuite/tests/typecheck/should_compile/T25725.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, PolyKinds #-}
+{-# LANGUAGE TypeFamilies, PolyKinds, UnliftedNewtypes #-}
 
 module T25725 where
 
@@ -13,5 +13,20 @@ data D :: TYPE r -> Type where
 data family Dix4 :: Type -> k
 data instance Dix4 Int :: TYPE r -> Type where
   DIn4 :: p -> Dix4 Int p
+
+data family Dix5 :: TYPE r -> TYPE r
+newtype instance Dix5 f :: TYPE c where
+  DIn5 :: g -> (Dix5 g :: TYPE k)
+
+-- data family Dix6 :: TYPE r -> TYPE r
+-- newtype instance Dix6 f :: TYPE r where
+--   DIn6 :: l -> Dix6 l
+
+-- this one would be rejected
+--  in data con sig, we have (Dix6 f) :: Type,
+--  but in newtype instance, we have (Dix6 f) :: TYPE r
+--  we need coercion between Type and TYPE r
+--  which is not possible for newtypes
+
 
 
