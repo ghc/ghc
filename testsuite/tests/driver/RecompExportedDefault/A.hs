476886248ec35e3a3bfba5e115ec1102fc9297c8
Matthew Pickering <matthewtpickering@gmail.com>
2025-03-13 11:31:55 +0000

    driver: Fix recompilation checking for exported defaults
    Since the exported defaults are not associated with any identifier from
    the module, they are just added to the export hash rather than
    the fine-grained recompilation logic.
    
    Fixes #25855

---
+++ testsuite/tests/driver/RecompExportedDefault/A.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE NamedDefaults #-}
+module A (
+    Stringify(..),
+    default Stringify,
+    Bingify(..),
+    default Bingify
+  ) where
+
+class Stringify a where
+  stringify :: a -> String
+
+instance Stringify Int where
+  stringify n = "Int"
+
+instance Stringify Bool where
+  stringify b = "Bool"
+
+instance Stringify [Char] where
+  stringify s = "String"
+
+class Bingify a where
+  bingify :: a -> String
+
+instance Bingify Int where
+  bingify n = "Int"
+
+instance Bingify Bool where
+  bingify b = "Bool"
+
+instance Bingify [Char] where
+  bingify s = "String"
+
+default Stringify (Int)
+default Bingify (Int)
