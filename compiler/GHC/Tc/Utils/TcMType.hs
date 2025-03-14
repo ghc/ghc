dc226deea84021e3a41b1dffdd95da9442f401a5
Patrick <fwy996602672@gmail.com>
2025-03-14 15:55:59 +0800

    now we experiment default both wildcards and implicit binders

--- compiler/GHC/Tc/Utils/TcMType.hs
+++ compiler/GHC/Tc/Utils/TcMType.hs
@@ -1783,10 +1783,10 @@ quantifyTyVarsWithBinders skol_info dvs outer_wcs_imp_tvs
             text "outer_wc_imp_qtvs=" <+> ppr outer_wcs_imp_tvs
             ])
 
-       ; undefaulted <- defaultTyVars $ dvs `delCandidates` outer_wcs_imp_tvs
+       ; undefaulted <- defaultTyVars dvs
        ; final_qtvs  <- liftZonkM $ do
             -- resume order and then skolemise
-            qtvs <- mapMaybeM zonk_quant $ undefaulted ++ outer_wcs_imp_tvs
+            qtvs <- mapMaybeM zonk_quant undefaulted
             return qtvs
 
        ; traceTc "quantifyTyVars }"
