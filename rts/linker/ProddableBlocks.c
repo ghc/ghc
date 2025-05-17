88dc662f3040a57c1afcb1a98c5e33f5a88e0011
Ben Gamari <ben@well-typed.com>
2025-05-17 14:39:15 -0400

    rts/linker: Improve efficiency of proddable blocks structure
    Previously the linker's "proddable blocks" check relied on a simple
    linked list of spans. This resulted in extremely poor complexity while
    linking objects with lots of small sections (e.g. objects built with
    split sections).
    
    Rework the mechanism to instead use a simple interval set implemented
    via binary search.
    
    Fixes #26009.

--- rts/linker/ProddableBlocks.c
+++ rts/linker/ProddableBlocks.c
@@ -6,65 +6,124 @@
  *
  * ---------------------------------------------------------------------------*/
 
-#include "Rts.h"
-#include "RtsUtils.h"
-#include "linker/ProddableBlocks.h"
 
-struct _ProddableBlock {
-  void* start;
-  int   size;
-  struct _ProddableBlock* next;
-};
-
-typedef struct _ProddableBlock ProddableBlock;
-
-void
-initProddableBlockSet ( ProddableBlockSet* set )
-{
-    set->head = NULL;
-}
-
-/* -----------------------------------------------------------------------------
+/*
  * Sanity checking.  For each ObjectCode, maintain a list of address ranges
  * which may be prodded during relocation, and abort if we try and write
  * outside any of these.
  */
+
+#include "Rts.h"
+#include "RtsUtils.h"
+#include "linker/ProddableBlocks.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct _ProddableBlock {
+    uintptr_t start;  // inclusive
+    uintptr_t end;    // inclusive
+} ProddableBlock;
+
 void
-addProddableBlock ( ProddableBlockSet* set, void* start, int size )
+initProddableBlockSet ( ProddableBlockSet* set )
 {
-    ProddableBlock* pb = stgMallocBytes(sizeof(ProddableBlock), "addProddableBlock");
-
-    IF_DEBUG(linker, debugBelch("addProddableBlock: %p %d\n", start, size));
-    ASSERT(size > 0);
-    pb->start = start;
-    pb->size  = size;
-    pb->next  = set->head;
-    set->head = pb;
-}
-
-void
-checkProddableBlock (ProddableBlockSet *set, void *addr, size_t size )
-{
-    ProddableBlock* pb;
-
-    for (pb = set->head; pb != NULL; pb = pb->next) {
-        char* s = (char*)(pb->start);
-        char* e = s + pb->size;
-        char* a = (char*)addr;
-        if (a >= s && (a+size) <= e)
-            return;
-    }
-    barf("checkProddableBlock: invalid fixup in runtime linker: %p", addr);
+    set->data = NULL;
+    set->capacity = 0;
+    set->size = 0;
 }
 
 void freeProddableBlocks (ProddableBlockSet *set)
 {
-    ProddableBlock *pb, *next;
+    stgFree(set->data);
+    set->data = NULL;
+    set->size = 0;
+    set->capacity = 0;
+}
 
-    for (pb = set->head; pb != NULL; pb = next) {
-        next = pb->next;
-        stgFree(pb);
+// Binary search for the first interval with start >= value. Returns index or
+// size if none.
+static size_t
+findLower(const ProddableBlockSet *set, uintptr_t value)
+{
+    size_t l = 0;
+    size_t r = set->size;
+    while (l < r) {
+        size_t mid = l + (r - l) / 2;
+        if (set->data[mid].start < value) {
+            l = mid + 1;
+        } else {
+            r = mid;
+        }
     }
-    set->head = NULL;
+    return l;
+}
+
+// Check whether a given value is a member of the set.
+bool
+containsSpan ( const ProddableBlockSet *set, uintptr_t start, uintptr_t end )
+{
+    size_t i = findLower(set, start+1);
+    return i > 0
+      && set->data[i-1].start <= start
+      && end <= set->data[i-1].end;
+}
+
+void
+checkProddableBlock (const ProddableBlockSet *set, void *addr, size_t size )
+{
+    if (! containsSpan(set, (uintptr_t) addr, (uintptr_t) addr+size)) {
+        barf("checkProddableBlock: invalid fixup in runtime linker: %p", addr);
+    }
+}
+
+// Ensure capacity for at least new_capacity intervals
+static void
+ensureCapacity(ProddableBlockSet *set, size_t new_capacity) {
+    if (new_capacity > set->capacity) {
+        size_t cap = set->capacity ? set->capacity * 2 : 4;
+        if (cap < new_capacity) {
+            cap = new_capacity;
+        }
+        ProddableBlock *tmp = stgReallocBytes(set->data, cap * sizeof(ProddableBlock), "addProddableBlock");
+        set->data = tmp;
+        set->capacity = cap;
+    }
+}
+
+void
+addProddableBlock ( ProddableBlockSet* set, void* start_ptr, size_t size )
+{
+    const uintptr_t start = (uintptr_t) start_ptr;
+    const uintptr_t end = (uintptr_t) start + size;
+    size_t i = findLower(set, start);
+
+    // check previous interval if it is overlapping or adjacent
+    if (i > 0 && start <= set->data[i-1].end + 1) {
+        // merge with left interval
+        i--;
+        if (end > set->data[i].end) {
+            set->data[i].end = end;
+        }
+    } else {
+        // insert new interval
+        ensureCapacity(set, set->size + 1);
+        memmove(&set->data[i+1], &set->data[i], sizeof(ProddableBlock) * (set->size - i));
+        set->data[i].start = start;
+        set->data[i].end = end;
+        set->size++;
+    }
+
+    // coalesce overlaps on right
+    size_t j = i;
+    while (j < set->size && set->data[j].start <= set->data[i].end + 1) {
+        set->data[i].end = set->data[j].end;
+        j++;
+    }
+
+    if (j != i) {
+        memmove(&set->data[i+1], &set->data[j], sizeof(ProddableBlock) * (set->size - j));
+        set->size -= j - i - 1;
+    }
 }
 
--- rts/linker/ProddableBlocks.h
+++ rts/linker/ProddableBlocks.h
@@ -8,20 +8,31 @@
 
 #pragma once
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
 // An interval set on uintptr_t.
 struct _ProddableBlock;
 
 typedef struct {
-    struct _ProddableBlock *head;
+    size_t size;
+    size_t capacity;
+    // sorted list of disjoint (start,end) pairs
+    struct _ProddableBlock *data;
 } ProddableBlockSet;
 
 void initProddableBlockSet ( ProddableBlockSet* set );
 
 // Insert an interval.
-void addProddableBlock ( ProddableBlockSet* set, void* start, int size );
+void addProddableBlock ( ProddableBlockSet* set, void* start, size_t size );
 
 // Check that an address belongs to the set.
-void checkProddableBlock (ProddableBlockSet *set, void *addr, size_t size );
+void checkProddableBlock (const ProddableBlockSet *set, void *addr, size_t size );
+
 
 // Free a set.
 void freeProddableBlocks (ProddableBlockSet *set);
+
+// For testing.
+bool containsSpan ( const ProddableBlockSet *set, uintptr_t start, uintptr_t end );
