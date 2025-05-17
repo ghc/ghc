b1187325189550873c0c53881592e47a217c10d8
Jakob Bruenker <jakob.bruenker@gmail.com>
2025-05-18 01:50:44 +0200

    Allow running indiv examples

--- issue23515_examples/run_examples.sh
+++ issue23515_examples/run_examples.sh
@@ -9,11 +9,27 @@ GHC_PATH="$DIR/../_build/stage1/bin/ghc"
 # Make sure the script is executable
 chmod +x "$0"
 
-# Run each example file with GHC in interactive mode
-for example in "$DIR"/Example*.hs; do
-  echo "-----------------------------------------------"
-  echo "Running: $example"
-  echo "-----------------------------------------------"
-  "$GHC_PATH" --interactive "$example"
-  echo ""
-done
+# Check if an example number was specified
+if [ -n "$1" ]; then
+  # Run a specific example
+  example="$DIR/Example$1.hs"
+  if [ -f "$example" ]; then
+    echo "-----------------------------------------------"
+    echo "Running: $example"
+    echo "-----------------------------------------------"
+    "$GHC_PATH" --interactive "$example"
+    echo ""
+  else
+    echo "Example file $example not found"
+    exit 1
+  fi
+else
+  # Run all examples
+  for example in "$DIR"/Example*.hs; do
+    echo "-----------------------------------------------"
+    echo "Running: $example"
+    echo "-----------------------------------------------"
+    "$GHC_PATH" --interactive "$example"
+    echo ""
+  done
+fi
