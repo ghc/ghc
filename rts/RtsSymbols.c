3a96aa1374f6cf56331e453ddec4065bfb900a9e
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2025-05-19 12:03:14 +0100

    debugger/rts: Allow toggling step-in per thread
    The RTS global flag `rts_stop_next_breakpoint` globally sets the
    interpreter to stop at the immediate next breakpoint.
    
    With this commit, single step mode can additionally be set per thread in
    the TSO flag (TSO_STOP_NEXT_BREAKPOINT).
    
    Being able to toggle "stop at next breakpoint" per thread is an
    important requirement for implementing "stepping out" of a function in a
    multi-threaded context.
    
    And, more generally, having a per-thread flag for single-stepping paves the
    way for multi-threaded debugging.
    
    That said, when we want to enable "single step" mode for the whole
    interpreted program we still want to stop at the immediate next
    breakpoint, whichever thread it belongs to.
    
    That's why we also keep the global `rts_stop_next_breakpoint` flag, with
    `rts_enableStopNextBreakpointAll` and `rts_disableStopNextBreakpointAll` helpers.
    
    Preparation for #26042

--- rts/RtsSymbols.c
+++ rts/RtsSymbols.c
@@ -906,7 +906,10 @@ extern char **environ;
       SymI_HasProto(revertCAFs)                                         \
       SymI_HasProto(RtsFlags)                                           \
       SymI_NeedsDataProto(rts_breakpoint_io_action)                     \
-      SymI_NeedsDataProto(rts_stop_next_breakpoint)                     \
+      SymI_NeedsDataProto(rts_enableStopNextBreakpointAll)              \
+      SymI_NeedsDataProto(rts_disableStopNextBreakpointAll)             \
+      SymI_NeedsDataProto(rts_enableStopNextBreakpoint)                 \
+      SymI_NeedsDataProto(rts_disableStopNextBreakpoint)                \
       SymI_NeedsDataProto(rts_stop_on_exception)                        \
       SymI_HasProto(stopTimer)                                          \
       SymI_HasProto(n_capabilities)                                     \
