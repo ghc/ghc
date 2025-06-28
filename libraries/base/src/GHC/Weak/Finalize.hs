86a6ca1e3dd2026cf9999c276fb6a0418a7a7a34
Duncan Coutts <duncan@well-typed.com>
2025-06-28 21:48:23 +0100

    base: Deprecate GHC.Weak.Finalize.runFinalizerBatch
    https://github.com/haskell/core-libraries-committee/issues/342
    (cherry picked from commit 360fa82cc0e06163c7d712a22e7a33cf30e6b852)

--- libraries/base/src/GHC/Weak/Finalize.hs
+++ libraries/base/src/GHC/Weak/Finalize.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE MagicHash #-}
 module GHC.Weak.Finalize
     ( -- * Handling exceptions
       -- | When an exception is thrown by a finalizer called by the
@@ -8,7 +9,30 @@ module GHC.Weak.Finalize
     , getFinalizerExceptionHandler
     , printToHandleFinalizerExceptionHandler
       -- * Internal
-    , runFinalizerBatch
+    , GHC.Weak.Finalize.runFinalizerBatch
     ) where
 
 import GHC.Internal.Weak.Finalize
+
+-- These imports can be removed once runFinalizerBatch is removed,
+-- as can MagicHash above.
+import GHC.Internal.Base (Int, Array#, IO, State#, RealWorld)
+
+
+{-# DEPRECATED runFinalizerBatch
+    "This function is internal to GHC. It will not be exported in future." #-}
+-- | Run a batch of finalizers from the garbage collector. Given an
+-- array of finalizers and the length of the array, just call each one
+-- in turn.
+--
+-- This is an internal detail of the GHC RTS weak pointer finaliser
+-- mechanism. It should no longer be exported from base. There is no
+-- good reason to use it. It will be removed in the next major version
+-- of base (4.23.*).
+--
+-- See <https://github.com/haskell/core-libraries-committee/issues/342>
+--
+runFinalizerBatch :: Int
+                  -> Array# (State# RealWorld -> State# RealWorld)
+                  -> IO ()
+runFinalizerBatch = GHC.Internal.Weak.Finalize.runFinalizerBatch
