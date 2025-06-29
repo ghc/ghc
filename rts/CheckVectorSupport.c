4da00f8a048f87168638d37e82864c55de6b7bc2
Sven Tennie <sven.tennie@gmail.com>
2025-06-29 12:13:42 +0200

    Implement CPU vector support (RVV) detection for RISC-V

--- rts/CheckVectorSupport.c
+++ rts/CheckVectorSupport.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include "CheckVectorSupport.h"
 
+#if defined(__riscv_v) && defined(__riscv_v_intrinsic)
+#include <riscv_vector.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <setjmp.h>
+
+static jmp_buf jmpbuf;
+
+// Signal handler for SIGILL (Illegal Instruction)
+static void sigill_handler(int);
+static void sigill_handler(__attribute__((unused)) int sig) {
+    // If we get here, the vector instruction caused an illegal instruction
+    // exception. We just swallow it.
+    longjmp(jmpbuf, 1);
+}
+#endif
+
 // Check support for vector registers (conservative).
 //
 // 0: no support for vector registers
@@ -64,18 +81,34 @@ int checkVectorSupport(void) {
     supports_V32 = hwcap & PPC_FEATURE_HAS_VSX;
 */
 
-  #elif defined(__riscv)
-// csrr instruction nott allowed in user-mode qemu emulation of riscv
-// Backend doesn't yet support vector registers, so hard-coded to no vector support
-// for now.
-//
-//    unsigned long vlenb;
-//    asm volatile ("csrr %0, vlenb" : "=r" (vlenb));
-    // VLENB gives the length in bytes
-    supports_V16 = 0;
-    supports_V32 = 0;
-    supports_V64 = 0;
+  // Detect RISC-V support
+  #elif defined(__riscv_v) && defined(__riscv_v_intrinsic)
+    // __riscv_v ensures we only get here when the compiler target (arch)
+    // supports vectors.
+    // Unfortunately, the status registers that could tell about RVV support
+    // are part of the priviledged ISA. So, we try to get VLENB from the `vlenb`
+    // register that only exists with RVV. If this throws an illegal instruction
+    // exception, we know that RVV is not supported by the executing CPU.
 
+    // Set up signal handler to catch illegal instruction
+    struct sigaction sa, old_sa;
+    sa.sa_handler = sigill_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGILL, &sa, &old_sa);
+    
+    unsigned vlenb = 0;
+    if (setjmp(jmpbuf) == 0) {
+      // Try to execute a vector instruction
+      vlenb = __riscv_vlenb();
+    }
+    // Restore original signal handler
+    sigaction(SIGILL, &old_sa, NULL);
+
+    // VLENB gives the length in bytes
+    supports_V16 = vlenb >= 16;
+    supports_V32 = vlenb >= 32;
+    supports_V64 = vlenb >= 64;
   #else
     // On other platforms, we conservatively return no vector support.
     supports_V16 = 0;
