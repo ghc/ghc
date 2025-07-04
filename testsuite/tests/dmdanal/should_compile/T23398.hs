ca7a9d421f602355822b674dd01fb48cd6291108
Simon Peyton Jones <simon.peytonjones@gmail.com>
2025-07-04 13:52:43 +0100

    Treat tuple dictionaries uniformly; don't unbox them
    See `Note [Do not unbox class dictionaries]` in DmdAnal.hs,
    sep (DNB1).
    
    This MR reverses the plan in #23398, which suggested a special case to
    unbox tuple dictionaries in worker/wrapper.  But:
    
    - This was the cause of a pile of complexity in the specialiser (#26158)
    
    - Even with that complexity, specialision was still bad, very bad
      See https://gitlab.haskell.org/ghc/ghc/-/issues/19747#note_626297
    
    And it's entirely unnecessary!  Specialision works fine without
    unboxing tuple dictionaries.

--- testsuite/tests/dmdanal/should_compile/T23398.hs
+++ testsuite/tests/dmdanal/should_compile/T23398.hs
@@ -6,10 +6,11 @@ type PairDict a = (Eq a, Show a)
 foo :: PairDict a => a -> a -> String
 foo x y | x==y      = show x
         | otherwise = show y
-
--- In worker/wrapper we'd like to unbox the pair
--- but not (Eq a) and (Show a)
+-- In worker/wrapper we don't want to unbox PairDict
+-- See (DNB1) Note [Do not unbox class dictionaries] in GHC.Core.Opt.DmdAnal
 
 bar :: (a ~ b, Show a) => Int -> a -> (b, String)
 bar 0 x = (x, show x)
 bar n x = bar (n-1) x
+-- ...but we do want to unbox the (a~b)
+-- see (DNB2) in the same Note
