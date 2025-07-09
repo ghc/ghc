49a44ab79d644590abdeff8699406bbd2d310715
Simon Hengel <sol@typeful.net>
2025-07-09 15:33:53 +0700

    Refactor GHC.Driver.Errors.printMessages

--- compiler/GHC/Driver/Errors.hs
+++ compiler/GHC/Driver/Errors.hs
@@ -7,27 +7,45 @@ module GHC.Driver.Errors (
 
 import GHC.Driver.Errors.Types
 import GHC.Prelude
+import GHC.Types.SrcLoc
 import GHC.Types.SourceError
 import GHC.Types.Error
 import GHC.Utils.Error
-import GHC.Utils.Outputable (hang, ppr, ($$),  text, mkErrStyle, sdocStyle, updSDocContext )
+import GHC.Utils.Outputable
 import GHC.Utils.Logger
 
 printMessages :: forall a. (Diagnostic a) => Logger -> DiagnosticOpts a -> DiagOpts -> Messages a -> IO ()
-printMessages logger msg_opts opts msgs
-  = sequence_ [ let style = mkErrStyle name_ppr_ctx
-                    ctx   = (diag_ppr_ctx opts) { sdocStyle = style }
-                in (if log_diags_as_json
-                    then logJsonMsg logger (MCDiagnostic sev reason (diagnosticCode dia)) msg
-                    else logMsg logger (MCDiagnostic sev reason (diagnosticCode dia)) s $
-                  updSDocContext (\_ -> ctx) (messageWithHints dia))
-              | msg@MsgEnvelope { errMsgSpan       = s,
-                                  errMsgDiagnostic = dia,
-                                  errMsgSeverity   = sev,
-                                  errMsgReason     = reason,
-                                  errMsgContext    = name_ppr_ctx }
-                  <- sortMsgBag (Just opts) (getMessages msgs) ]
+printMessages logger msg_opts opts = mapM_ (printMessage logger msg_opts opts) . sortMessages
   where
+    sortMessages :: Messages a -> [MsgEnvelope a]
+    sortMessages = sortMsgBag (Just opts) . getMessages
+
+printMessage :: forall a. (Diagnostic a) => Logger -> DiagnosticOpts a -> DiagOpts -> MsgEnvelope a -> IO ()
+printMessage logger msg_opts opts message
+  | log_diags_as_json = logJsonMsg logger messageClass message
+  | otherwise = logMsg logger messageClass location doc
+  where
+    doc :: SDoc
+    doc = updSDocContext (\_ -> ctx) (messageWithHints diagnostic)
+
+    messageClass :: MessageClass
+    messageClass = MCDiagnostic severity (errMsgReason message) (diagnosticCode diagnostic)
+
+    style :: PprStyle
+    style = mkErrStyle (errMsgContext message)
+
+    location :: SrcSpan
+    location = errMsgSpan message
+
+    ctx :: SDocContext
+    ctx = (diag_ppr_ctx opts) { sdocStyle = style }
+
+    diagnostic :: a
+    diagnostic = errMsgDiagnostic message
+
+    severity :: Severity
+    severity = errMsgSeverity message
+
     messageWithHints :: a -> SDoc
     messageWithHints e =
       let main_msg = formatBulleted $ diagnosticMessage msg_opts e
@@ -36,6 +54,8 @@ printMessages logger msg_opts opts msgs
                [h] -> main_msg $$ hang (text "Suggested fix:") 2 (ppr h)
                hs  -> main_msg $$ hang (text "Suggested fixes:") 2
                                        (formatBulleted  $ mkDecorated . map ppr $ hs)
+
+    log_diags_as_json :: Bool
     log_diags_as_json = log_diagnostics_as_json (logFlags logger)
 
 -- | Given a bag of diagnostics, turn them into an exception if
