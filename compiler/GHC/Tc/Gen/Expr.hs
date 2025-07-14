62f356cbc3776426124b3eb3af91c29c1d66a364
Apoorv Ingle <apoorv-ingle@uiowa.edu>
2025-07-13 21:14:25 -0500

    kill ExpectedFunTyOrig

--- compiler/GHC/Tc/Gen/Expr.hs
+++ compiler/GHC/Tc/Gen/Expr.hs
@@ -976,7 +976,7 @@ tcSynArgE orig op sigma_ty syn_ty thing_inside
                -- fixed RuntimeRep, as needed to call mkWpFun.
            ; return (result, match_wrapper <.> fun_wrap) }
       where
-        herald = ExpectedFunTySyntaxOp orig op
+        herald = ExpectedFunTySyntaxOp 1 orig op
 
     go rho_ty (SynType the_ty)
       = do { wrap   <- tcSubTypePat orig GenSigCtxt the_ty rho_ty
@@ -1005,7 +1005,7 @@ tcSynArgA orig op sigma_ty arg_shapes res_shape thing_inside
               thing_inside (arg_results ++ res_results) (map scaledMult arg_tys ++ arg_res_mults)
        ; return (result, match_wrapper, arg_wrappers, res_wrapper) }
   where
-    herald = ExpectedFunTySyntaxOp orig op
+    herald = ExpectedFunTySyntaxOp (length arg_shapes) orig op
 
     tc_syn_args_e :: [TcSigmaTypeFRR] -> [SyntaxOpType]
                   -> ([TcSigmaTypeFRR] -> [Mult] -> TcM a)
