9eff634ce67b81556c18aebed37997dee65401c6
fendor <fendor@posteo.de>
2025-08-07 09:36:55 +0200

    Expose Backtraces internals from ghc-experimental
    Additionally, expose the same API `base:Control.Exception.Backtrace`
    to make it easier to use as a drop-in replacement.
    
    (cherry picked from commit 6602472273e1cdffcdb7753c9133047e571896bd)

--- libraries/ghc-experimental/src/GHC/Exception/Backtrace/Experimental.hs
+++ libraries/ghc-experimental/src/GHC/Exception/Backtrace/Experimental.hs
@@ -9,12 +9,20 @@ Portability :  non-portable (GHC extensions)
 
 This module exposes experimental extensions to the Backtrace mechanism of GHC.
 -}
-module GHC.Exception.Backtrace.Experimental (
-  -- * Collecting exception annotations (like backtraces)
-  CollectExceptionAnnotationMechanism,
-  getCollectExceptionAnnotationMechanism,
-  setCollectExceptionAnnotation,
-  collectExceptionAnnotation,
+module GHC.Exception.Backtrace.Experimental
+    ( -- * Backtrace mechanisms
+      BacktraceMechanism(..)
+    , getBacktraceMechanismState
+    , setBacktraceMechanismState
+    -- * Collecting backtraces
+    , Backtraces(..),
+    , displayBacktraces
+    , collectBacktraces
+    -- * Collecting exception annotations on throwing 'Exception's
+    , CollectExceptionAnnotationMechanism
+    , getCollectExceptionAnnotationMechanism
+    , setCollectExceptionAnnotation
+    , collectExceptionAnnotation
   ) where
 
 import GHC.Internal.Exception.Backtrace
