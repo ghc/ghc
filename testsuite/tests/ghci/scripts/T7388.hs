fbe34ee6af5494af636670b98c695a246e6f14a5
Sylvain Henry <sylvain@haskus.fr>
2025-09-04 17:57:03 +0200

    Fix some tests (statically linked GHC vs libc)
    When GHC is linked statically, the stdout C global variable that GHC uses
    isn't shared with the stdout C global variable used by loaded code.
    
    As a consequence, the latter must be explicitly flushed because GHC
    won't flush it before exiting.
    
    (cherry picked from commit 11eeeba7dd50d16c8c7d62165d006bba8fa339a9)

--- testsuite/tests/ghci/scripts/T7388.hs
+++ testsuite/tests/ghci/scripts/T7388.hs
@@ -2,5 +2,7 @@
 module T7388 where
 
 import Foreign.C
+import Foreign.Ptr
 
 foreign import capi "stdio.h printf" printfb :: CString -> CInt -> IO ()
+foreign import capi "stdio.h fflush" fflushb :: Ptr () -> IO ()
