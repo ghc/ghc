a1edbb57a9febd57dc1c7c4089a4d3c7a3e27bff
Moritz Angermann <moritz@stable-haskell.org>
2025-09-05 11:14:56 +0900

    Conditionalize the ghc-internal dependency on the ghc version.

--- libraries/ghci/GHCi/CreateBCO.hs
+++ libraries/ghci/GHCi/CreateBCO.hs
@@ -6,10 +6,10 @@
 {-# LANGUAGE UnboxedTuples #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE CPP #-}
+-- Only needed when we don't have ghc-internal (and must import deprecated names)
+#ifndef HAVE_GHC_INTERNAL
 {-# OPTIONS_GHC -Wno-warnings-deprecations #-}
--- TODO We want to import GHC.Internal.Base (BCO, mkApUpd0#, newBCO#) instead
--- of from GHC.Exts when we can require of the bootstrap compiler to have
--- ghc-internal.
+#endif
 
 --
 --  (c) The University of Glasgow 2002-2006
@@ -30,7 +30,13 @@ import Data.Array.Base
 import Foreign hiding (newArray)
 import Unsafe.Coerce (unsafeCoerce)
 import GHC.Arr          ( Array(..) )
+-- When ghc-internal is available prefer the non-deprecated exports.
+#ifdef HAVE_GHC_INTERNAL
+import GHC.Exts hiding ( BCO, mkApUpd0#, newBCO# )
+import GHC.Internal.Base ( BCO, mkApUpd0#, newBCO# )
+#else
 import GHC.Exts
+#endif
 import GHC.IO
 import Control.Exception ( ErrorCall(..) )
 
