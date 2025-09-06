5654077509a5072ce0308d722bf72ee197e35dea
Ben Gamari <ben@well-typed.com>
2025-09-06 18:00:29 -0400

    gitlab-ci: Disable split sections on FreeBSD
    Due to #26303.

--- .gitlab/generate-ci/gen_ci.hs
+++ .gitlab/generate-ci/gen_ci.hs
@@ -1235,7 +1235,7 @@ darwin =
 
 freebsd_jobs :: [JobGroup Job]
 freebsd_jobs =
-  [ addValidateRule FreeBSDLabel (standardBuilds Amd64 FreeBSD14)
+  [ addValidateRule FreeBSDLabel (standardBuildsWithConfig Amd64 FreeBSD14 (splitSectionsBroken vanilla))
   ]
 
 alpine_x86 :: [JobGroup Job]
