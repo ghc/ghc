757b4319d11bba64ff23be588b97d2f1a52bb426
Ben Gamari <ben@well-typed.com>
2025-09-25 23:20:41 -0400

    rts: Dynamically initialize built-in closures
    To resolve #26166 we need to eliminate references to undefined symbols
    in the runtime system. One such source of these is the runtime's
    static references to `I#` and `C#` due the `stg_INTLIKE` and
    `stg_CHARLIKE` arrays.
    
    To avoid this we make these dynamic, initializing them during RTS
    start-up.

--- rts/include/stg/MiscClosures.h
+++ rts/include/stg/MiscClosures.h
@@ -277,8 +277,8 @@ RTS_ENTRY(stg_NO_FINALIZER);
 extern StgWordArray stg_CHARLIKE_closure;
 extern StgWordArray stg_INTLIKE_closure;
 #else
-extern StgIntCharlikeClosure stg_CHARLIKE_closure[];
-extern StgIntCharlikeClosure stg_INTLIKE_closure[];
+extern StgIntCharlikeClosure stg_CHARLIKE_closure[MAX_CHARLIKE - MIN_CHARLIKE + 1];
+extern StgIntCharlikeClosure stg_INTLIKE_closure[MAX_INTLIKE - MIN_INTLIKE + 1];
 #endif
 
 /* StgStartup */
