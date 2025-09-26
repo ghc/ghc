952655255c86e6f114103c9e9413a242ec772db2
Ben Gamari <ben@well-typed.com>
2025-09-25 23:20:41 -0400

    rts: Dynamically initialize built-in closures
    To resolve #26166 we need to eliminate references to undefined symbols
    in the runtime system. One such source of these is the runtime's
    static references to `I#` and `C#` due the `stg_INTLIKE` and
    `stg_CHARLIKE` arrays.
    
    To avoid this we make these dynamic, initializing them during RTS
    start-up.
    
    (cherry picked from commit 39eaaaba5356e3fc9218d8e27375d6de24778cbc)

---
+++ rts/BuiltinClosures.c
@@ -0,0 +1,30 @@
+#include "Rts.h"
+#include "Prelude.h"
+#include "BuiltinClosures.h"
+
+/*
+ * Note [CHARLIKE and INTLIKE closures]
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * These are static representations of Chars and small Ints, so that
+ * we can remove dynamic Chars and Ints during garbage collection and
+ * replace them with references to the static objects.
+ */
+
+StgIntCharlikeClosure stg_INTLIKE_closure[MAX_INTLIKE - MIN_INTLIKE + 1];
+StgIntCharlikeClosure stg_CHARLIKE_closure[MAX_CHARLIKE - MIN_CHARLIKE + 1];
+
+void initBuiltinClosures(void) {
+    // INTLIKE closures
+    for (int i = MIN_INTLIKE; i <= MAX_INTLIKE; i++) {
+        StgIntCharlikeClosure *c = &stg_INTLIKE_closure[i - MIN_INTLIKE];
+        SET_HDR((StgClosure* ) c, Izh_con_info, CCS_SYSTEM_OR_NULL);
+        c->data = i;
+    }
+
+    // CHARLIKE closures
+    for (int i = MIN_CHARLIKE; i <= MAX_CHARLIKE; i++) {
+        StgIntCharlikeClosure *c = &stg_CHARLIKE_closure[i - MIN_CHARLIKE];
+        SET_HDR((StgClosure* ) c, Czh_con_info, CCS_SYSTEM_OR_NULL);
+        c->data = i;
+    }
+}
