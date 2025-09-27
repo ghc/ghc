eaef90290a149f8d95020370b7f3a5ebec9974f4
Cheng Shao <terrorjack@type.dance>
2025-09-27 20:32:49 +0200

    rts: provide stub implementations of ExecPage functions for wasm
    This patch provides stub implementations of ExecPage functions for
    wasm. They are never actually invoked at runtime for any non-TNTC
    platform, yet they can cause link-time errors of missing symbols when
    the GHCi.InfoTable module gets linked into the final wasm module (e.g.
    a GHC API program).

--- rts/ExecPage.c
+++ rts/ExecPage.c
@@ -10,15 +10,23 @@
 #include "linker/MMap.h"
 
 ExecPage *allocateExecPage(void) {
+#if defined(wasm32_HOST_ARCH)
+    return NULL;
+#else
     ExecPage *page = (ExecPage *) mmapAnon(getPageSize());
     return page;
+#endif
 }
 
 void freezeExecPage(ExecPage *page) {
+#if !defined(wasm32_HOST_ARCH)
     mprotectForLinker(page, getPageSize(), MEM_READ_EXECUTE);
     flushExec(getPageSize(), page);
+#endif
 }
 
 void freeExecPage(ExecPage *page) {
+#if !defined(wasm32_HOST_ARCH)
     munmapForLinker(page, getPageSize(), "freeExecPage");
+#endif
 }
