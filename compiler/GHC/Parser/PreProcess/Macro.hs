6cb63fc2d573e94a2f4e12a2e1594d6d9b119688
Alan Zimmerman <alan.zimm@gmail.com>
2025-10-12 19:47:48 +0100

    GHC_CPP: move Eval.hs into GHC.Parser.PreProcess.Macro

--- compiler/GHC/Parser/PreProcess/Macro.hs
+++ compiler/GHC/Parser/PreProcess/Macro.hs
@@ -39,7 +39,6 @@ import Data.Semigroup qualified as S
 
 import GHC.Base
 import GHC.Parser.Lexer (P (..), PState (..), ParseResult (..))
-import GHC.Parser.PreProcess.Eval
 import GHC.Parser.PreProcess.ParsePP
 import GHC.Parser.PreProcess.Parser qualified as Parser
 import GHC.Parser.PreProcess.ParserM
@@ -384,6 +383,40 @@ additional CPP errors after the originating cause, and simplifies the
 coding to not have to match on returned Maybe results
 -}
 
+-- ---------------------------------------------------------------------
+--  Evaluate a CPP expression
+-- ---------------------------------------------------------------------
+
+eval :: Expr -> Int
+eval (Parens e) = eval e
+eval (Not e) = fromBool $ not (toBool $ eval e)
+eval (Var _) = 0 -- Spec says remaining identifiers are replaces with zero
+eval (IntVal i) = i
+eval (Plus e1 e2) = (eval e1) + (eval e2)
+eval (Minus e1 e2) = (eval e1) - (eval e2)
+eval (Times e1 e2) = (eval e1) * (eval e2)
+eval (Logic op e1 e2) = evalLogicOp op (eval e1) (eval e2)
+eval (Comp op e1 e2) = evalCompOp op (eval e1) (eval e2)
+
+evalLogicOp :: LogicOp -> Int -> Int -> Int
+evalLogicOp LogicalOr e1 e2 = fromBool $ (toBool e1) || (toBool e2)
+evalLogicOp LogicalAnd e1 e2 = fromBool $ (toBool e1) && (toBool e2)
+
+evalCompOp :: CompOp -> Int -> Int -> Int
+evalCompOp CmpEqual e1 e2 = fromBool $ e1 == e2
+evalCompOp CmpNotEqual e1 e2 = fromBool $ e1 /= e2
+evalCompOp CmpGt e1 e2 = fromBool $ e1 > e2
+evalCompOp CmpGtE e1 e2 = fromBool $ e1 >= e2
+evalCompOp CmpLt e1 e2 = fromBool $ e1 < e2
+evalCompOp CmpLtE e1 e2 = fromBool $ e1 <= e2
+
+toBool :: Int -> Bool
+toBool 0 = False
+toBool _ = True
+
+fromBool :: Bool -> Int
+fromBool False = 0
+fromBool True = 1
 
 -- ---------------------------------------------------------------------
 
