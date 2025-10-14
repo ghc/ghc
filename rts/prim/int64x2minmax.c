25e00507e002231c408d3435171207c97fdb4d18
Ben Gamari <ben@smart-cactus.org>
2025-10-14 18:55:18 -0400

    rts: Minimize header dependencies of Prim.h
    Otherwise we will end up with redundant and incompatible declarations
    resulting in warnings during the unregisterised build.

--- rts/prim/int64x2minmax.c
+++ rts/prim/int64x2minmax.c
@@ -12,44 +12,44 @@
 //   * enable SSE4.2, or
 //   * implement min/max in NCG.
 
-__m128i hs_minInt64X2(__m128i xx, __m128i yy)
+v128 hs_minInt64X2(v128 xx, v128 yy)
 {
   int64_t x[2], y[2];
   memcpy(x, &xx, 16);
   memcpy(y, &yy, 16);
   int64_t z0 = x[0] < y[0] ? x[0] : y[0];
   int64_t z1 = x[1] < y[1] ? x[1] : y[1];
-  return _mm_set_epi64x(z1, z0);
+  return (v128) _mm_set_epi64x(z1, z0);
 }
 
-__m128i hs_maxInt64X2(__m128i xx, __m128i yy)
+v128 hs_maxInt64X2(v128 xx, v128 yy)
 {
   int64_t x[2], y[2];
   memcpy(x, &xx, 16);
   memcpy(y, &yy, 16);
   int64_t z0 = x[0] < y[0] ? y[0] : x[0];
   int64_t z1 = x[1] < y[1] ? y[1] : x[1];
-  return _mm_set_epi64x(z1, z0);
+  return (v128) _mm_set_epi64x(z1, z0);
 }
 
-__m128i hs_minWord64X2(__m128i xx, __m128i yy)
+v128 hs_minWord64X2(v128 xx, v128 yy)
 {
   uint64_t x[2], y[2];
   memcpy(x, &xx, 16);
   memcpy(y, &yy, 16);
   uint64_t z0 = x[0] < y[0] ? x[0] : y[0];
   uint64_t z1 = x[1] < y[1] ? x[1] : y[1];
-  return _mm_set_epi64x(z1, z0);
+  return (v128) _mm_set_epi64x(z1, z0);
 }
 
-__m128i hs_maxWord64X2(__m128i xx, __m128i yy)
+v128 hs_maxWord64X2(v128 xx, v128 yy)
 {
   uint64_t x[2], y[2];
   memcpy(x, &xx, 16);
   memcpy(y, &yy, 16);
   uint64_t z0 = x[0] < y[0] ? y[0] : x[0];
   uint64_t z1 = x[1] < y[1] ? y[1] : x[1];
-  return _mm_set_epi64x(z1, z0);
+  return (v128) _mm_set_epi64x(z1, z0);
 }
 
 #endif
