863db4f4ac3ca163de98dd47a5e28b64281c8fcf
Duncan Coutts <duncan@well-typed.com>
2025-10-20 12:07:35 +0100

    Enable dynamic lib support on Windows
    This does not mean it works, it means the build system will now try to
    build dynamic libs. We'll find out what breaks!

--- hadrian/src/Oracles/Flag.hs
+++ hadrian/src/Oracles/Flag.hs
@@ -92,11 +92,10 @@ arSupportsAtFile stage = Toolchain.arSupportsAtFile . tgtAr <$> targetStage stag
 platformSupportsSharedLibs :: Action Bool
 -- FIXME: This is querying about the target but is named "platformXXX", targetSupportsSharedLibs would be better
 platformSupportsSharedLibs = do
-    windows       <- isWinTarget
     ppc_linux     <- (&&) <$> anyTargetArch [ ArchPPC ] <*> anyTargetOs [ OSLinux ]
     solaris       <- (&&) <$> anyTargetArch [ ArchX86 ] <*> anyTargetOs [ OSSolaris2 ]
     javascript    <- anyTargetArch     [ ArchJavaScript ]
-    return $ not (windows || javascript || ppc_linux || solaris)
+    return $ not (javascript || ppc_linux || solaris)
 
 -- | Does the target support threaded RTS?
 targetSupportsThreadedRts :: Action Bool
