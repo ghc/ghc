d1c0af252cb04a638931c90f9117db2eaac000bb
Duncan Coutts <duncan@well-typed.com>
2025-10-30 22:16:04 +0000

    Split posix/MIO.c out of posix/Signals.c
    The MIO I/O manager was secretly living inside the Signals file.
    
    Now it gets its own file, like any other self-respecting I/O manager.

--- rts/posix/Signals.c
+++ rts/posix/Signals.c
@@ -9,21 +9,12 @@
 #include "rts/PosixSource.h"
 #include "Rts.h"
 
-#include "Schedule.h"
 #include "RtsSignals.h"
-#include "Signals.h"
-#include "IOManager.h"
 #include "RtsUtils.h"
+#include "Schedule.h"
 #include "Prelude.h"
-#include "Ticker.h"
 #include "ThreadLabels.h"
-#include "Libdw.h"
-
-/* TODO: eliminate this include. This file should be about signals, not be
- * part of an I/O manager implementation. The code here that are really part
- * of an I/O manager should be moved into an appropriate I/O manager impl.
- */
-#include "IOManagerInternals.h"
+#include "MIO.h"
 
 #if defined(alpha_HOST_ARCH)
 # if defined(linux_HOST_OS)
@@ -43,10 +34,6 @@
 
 #if defined(HAVE_ERRNO_H)
 # include <errno.h>
-#endif
-
-#if defined(HAVE_EVENTFD_H)
-# include <sys/eventfd.h>
 #endif
 
 #if defined(HAVE_TERMIOS_H)
@@ -134,110 +121,6 @@ more_handlers(int sig)
     nHandlers = sig + 1;
 }
 
-// Here's the pipe into which we will send our signals
-static int io_manager_wakeup_fd = -1;
-static int timer_manager_control_wr_fd = -1;
-
-#define IO_MANAGER_WAKEUP 0xff
-#define IO_MANAGER_DIE    0xfe
-#define IO_MANAGER_SYNC   0xfd
-
-void setTimerManagerControlFd(int fd) {
-    RELAXED_STORE(&timer_manager_control_wr_fd, fd);
-}
-
-void
-setIOManagerWakeupFd (int fd)
-{
-    // only called when THREADED_RTS, but unconditionally
-    // compiled here because GHC.Event.Control depends on it.
-    SEQ_CST_STORE(&io_manager_wakeup_fd, fd);
-}
-
-/* -----------------------------------------------------------------------------
- * Wake up at least one IO or timer manager HS thread.
- * -------------------------------------------------------------------------- */
-void
-ioManagerWakeup (void)
-{
-    int r;
-    const int wakeup_fd = SEQ_CST_LOAD(&io_manager_wakeup_fd);
-    // Wake up the IO Manager thread by sending a byte down its pipe
-    if (wakeup_fd >= 0) {
-#if defined(HAVE_EVENTFD)
-        StgWord64 n = (StgWord64)IO_MANAGER_WAKEUP;
-        r = write(wakeup_fd, (char *) &n, 8);
-#else
-        StgWord8 byte = (StgWord8)IO_MANAGER_WAKEUP;
-        r = write(wakeup_fd, &byte, 1);
-#endif
-        /* N.B. If the TimerManager is shutting down as we run this
-         * then there is a possibility that our first read of
-         * io_manager_wakeup_fd is non-negative, but before we get to the
-         * write the file is closed. If this occurs, io_manager_wakeup_fd
-         * will be written into with -1 (GHC.Event.Control does this prior
-         * to closing), so checking this allows us to distinguish this case.
-         * To ensure we observe the correct ordering, we declare the
-         * io_manager_wakeup_fd as volatile.
-         * Since this is not an error condition, we do not print the error
-         * message in this case.
-         */
-        if (r == -1 && SEQ_CST_LOAD(&io_manager_wakeup_fd) >= 0) {
-            sysErrorBelch("ioManagerWakeup: write");
-        }
-    }
-}
-
-#if defined(THREADED_RTS)
-void
-ioManagerDie (void)
-{
-    StgWord8 byte = (StgWord8)IO_MANAGER_DIE;
-    uint32_t i;
-    int r;
-
-    {
-        // Shut down timer manager
-        const int fd = RELAXED_LOAD(&timer_manager_control_wr_fd);
-        if (0 <= fd) {
-            r = write(fd, &byte, 1);
-            if (r == -1) { sysErrorBelch("ioManagerDie: write"); }
-            RELAXED_STORE(&timer_manager_control_wr_fd, -1);
-        }
-    }
-
-    {
-        // Shut down IO managers
-        for (i=0; i < getNumCapabilities(); i++) {
-            const int fd = RELAXED_LOAD(&getCapability(i)->iomgr->control_fd);
-            if (0 <= fd) {
-                r = write(fd, &byte, 1);
-                if (r == -1) { sysErrorBelch("ioManagerDie: write"); }
-                RELAXED_STORE(&getCapability(i)->iomgr->control_fd, -1);
-            }
-        }
-    }
-}
-
-void
-ioManagerStartCap (Capability **cap)
-{
-    rts_evalIO(cap,ensureIOManagerIsRunning_closure,NULL);
-}
-
-void
-ioManagerStart (void)
-{
-    // Make sure the IO manager thread is running
-    Capability *cap;
-    if (SEQ_CST_LOAD(&timer_manager_control_wr_fd) < 0 || SEQ_CST_LOAD(&io_manager_wakeup_fd) < 0) {
-        cap = rts_lock();
-        ioManagerStartCap(&cap);
-        rts_unlock(cap);
-    }
-}
-#endif
-
 #if !defined(THREADED_RTS)
 
 #define N_PENDING_HANDLERS 16
@@ -260,31 +143,9 @@ generic_handler(int sig USED_IF_THREADS,
                 void *p STG_UNUSED)
 {
 #if defined(THREADED_RTS)
-
-    StgWord8 buf[sizeof(siginfo_t) + 1];
-    int r;
-
-    buf[0] = sig;
-    if (info == NULL) {
-        // info may be NULL on Solaris (see #3790)
-        memset(buf+1, 0, sizeof(siginfo_t));
-    } else {
-        memcpy(buf+1, info, sizeof(siginfo_t));
-    }
-
-    int timer_control_fd = RELAXED_LOAD(&timer_manager_control_wr_fd);
-    if (0 <= timer_control_fd)
-    {
-        r = write(timer_control_fd, buf, sizeof(siginfo_t)+1);
-        if (r == -1 && errno == EAGAIN) {
-            errorBelch("lost signal due to full pipe: %d\n", sig);
-        }
-    }
-
-    // If the IO manager hasn't told us what the FD of the write end
-    // of its pipe is, there's not much we can do here, so just ignore
-    // the signal..
-
+    //TODO: This calls MIO directly. We should go via IOManager API.
+    // The IOManager API should be extended to cover signals.
+    timerManagerNotifySignal(sig, info);
 #else /* not THREADED_RTS */
 
     /* Can't call allocate from here.  Probably can't call malloc
