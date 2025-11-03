0b32d9c42f641d7b6f79099a7fd13eb99e782524
Simon Peyton Jones <simon.peytonjones@gmail.com>
2025-11-03 23:34:01 +0000

    Tidy up occurs-checking a little
    In investigating #26543, I re-discovered the wobbliness around doing an
    occurs check when there is a coercion hole in the RHS type.  Moreover
    I found that the two occurs-checkers, in Unify.simpleOccursCheck and
    Unify.checkTyEqRhs didn't even agree about how to handle coersion holes.
    
    This patch doesn't fully solve the issue, but it tidies it up quite a bit:
    
    * Rewrote Note [CoercionHoles and their free variables]
      in GHC.Core.TyCo.FVs
    
    * When finding the free vars of a CoHole, always look in the kind of the hole.
      (This was inconsistent before.)
      See Note [CoercionHoles and their free variables]
    
    * Make Unify.mkOccFolders look in the kind of coercion holes
    
    * In Unify.checkCo, use `mkOccFolders` rather than `tyCoVarsOfCo`.  The two
      should be the same, but it's more uniform to use the same function as we
      do for `simpleOccursCheck`.  And maybe more efficient.
    
    * Made the CoVar case of tyCoFVsOfCo use the same auxiliary function as the
      TyVar case of tyCoFVsOfTy
    
    * Made the CoHole case of tyCoFVsOfCo look in the kind of the coercion hole.
    
    * Added (UQL5) to Note [QuickLook unification], to stress that the "simple
      unification check" has a real effect on what it typeable with QuickLook; it
      is not just a fast path.
    
    I added a regression test for #26543.  It works in HEAD but we need to be sure
    that it stays working.

--- compiler/GHC/Tc/Gen/App.hs
+++ compiler/GHC/Tc/Gen/App.hs
@@ -2174,6 +2174,7 @@ qlUnify ty1 ty2
               -- Here we are in the TcM monad, which does not track enclosing
               -- Given equalities; so for quick-look unification we conservatively
               -- treat /any/ level outside this one as untouchable. Hence cur_lvl.
+              -- See (UQL5) in Note [QuickLook unification]
            ; case simpleUnifyCheck UC_QuickLook cur_lvl kappa ty2 of
               SUC_CanUnify ->
                 do { co <- unifyKind (Just (TypeThing ty2)) ty2_kind kappa_kind
@@ -2244,6 +2245,21 @@ That is the entire point of qlUnify!   Wrinkles:
 
   It's just not worth the trouble, we think (for now at least).
 
+(UQL5) We call `simpleUnifyCheck` for the occurs-check etc; see (UQL1). Usually it's fine
+  for `simpleUnifyCheck` to bale out saying `SUC_NotSure`, because usually we'll just generate
+  an equality we solve later. But in QuickLook, baling out may mean we don't find a good
+  type for an instantiation variable, which in turn may make the program fail to typecheck.
+  So `simpleUnifyCheck` is more than just an efficiency thing.
+
+  Coercion holes are a tricky point. Example (#26543): at one stage we got
+           qlUnify( (Eq (alpha |> {co}) => beta) -> Int
+                  , kappa1                       -> Int )
+  where `{co}` was a coercion hole. We want to unify
+           kappa1 := Eq (alpha |> {co}) => beta
+  Failing to do so rejects the program.
+
+  In fact test T26543 no longer has a coercion hole in the type, but the possibility
+  remains a worry.
 
 Sadly discarded design alternative
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
