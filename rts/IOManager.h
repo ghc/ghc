14f56d0ed9a4921725d2989d9ef1e612004fa340
Duncan Coutts <duncan@well-typed.com>
2025-11-05 15:12:52 +0100

    wakeupIOManager is now required for all I/O managers
    We are going to rely on it. Previously it could be a no-op. Update the
    docs in the header file.
    
    Also, temporarily disable awaitCompletedTimeoutsOrIO post-condition
    assertion. It will become more complicated due to wakeupIOManager, and
    it's not yet clear how to express it.
    
    We will re-introduce a post condition after a few more changes.

--- rts/IOManager.h
+++ rts/IOManager.h
@@ -303,19 +303,13 @@ void stopIOManager(void);
 void exitIOManager(bool wait_threads);
 
 
-/* Wakeup hook: called from the scheduler's wakeUpRts (currently only in
- * threaded mode).
+/* Wakeup hook: called from the scheduler's wakeUpRts().
  *
  * The I/O manager can be blocked waiting on I/O or timers. Sometimes there are
  * other external events where we need to wake up the I/O manager and return
- * to the schedulr.
+ * to the scheduler.
  *
- * At the moment, all the non-threaded I/O managers will do this automagically
- * since a signal will interrupt any waiting system calls, so at the moment
- * the implementation for the non-threaded I/O managers does nothing.
- *
- * For the I/O managers in threaded mode, this arranges to unblock the I/O
- * manager if it waa blocked waiting.
+ * This arranges to unblock the I/O manager if it was blocked waiting.
  */
 void wakeupIOManager(void);
 
