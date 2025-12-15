6bc67e0446446e407b74e9f4d9447336906432f0
Cheng Shao <terrorjack@type.dance>
2025-12-15 11:16:36 +0100

    rts: use __builtin_mul_overflow for hs_mulIntMayOflo
    This patch uses `__builtin_mul_overflow` to implement
    `hs_mulIntMayOflo`. This is a GNU C checked arithmetic builtin
    function supported by gcc/clang, is type-generic so works for both
    32-bit/64-bit, and makes the code both more efficient and easier to
    read/maintain than the previous hand rolled logic.

--- rts/prim/mulIntMayOflo.c
+++ rts/prim/mulIntMayOflo.c
@@ -1,3 +1,6 @@
 #include "Rts.h"
 
-W_ hs_mulIntMayOflo(W_ a, W_ b) { return mulIntMayOflo(a, b); }
+W_ hs_mulIntMayOflo(W_ a, W_ b) {
+  I_ r;
+  return (W_)__builtin_mul_overflow((I_)a, (I_)b, &r);
+}
