7fd6dc18956c3d12aa454c4b61c83d21898ec535
Duncan Coutts <duncan@well-typed.com>
2026-01-09 14:23:18 +0000

    Use response files for hadrian linking with ghc (support long command lines)
    In future support for windows dynamic linking, we expect long command
    lines for linking dll files with ghc. Experiments with dynamic linking the
    ghc-internal library yielded a link command well over 32kb. We did not
    encounter this before for static libs, since we already use ar's @file
    feature (if available, which it is for the llvm toolchain).
    
    Co-authored-by: David Eichmann <davide@well-typed.com>

--- hadrian/src/Builder.hs
+++ hadrian/src/Builder.hs
@@ -345,11 +345,7 @@ instance H.Builder Builder where
 
                 Haddock BuildPackage -> runHaddock path buildArgs buildInputs
 
-                Ghc FindHsDependencies _ -> do
-                  -- Use a response file for ghc -M invocations, to
-                  -- avoid issues with command line size limit on
-                  -- Windows (#26637)
-                  runGhcWithResponse path buildArgs buildInputs
+                Ghc _ _ -> runGhcWithResponse path buildArgs buildInputs buildOptions
 
                 HsCpp    -> captureStdout
 
@@ -393,14 +389,19 @@ runHaddock haddockPath flagArgs fileInputs = withResponseFile $ \tmp -> do
     writeFile' tmp $ escapeArgs fileInputs
     cmd [haddockPath] flagArgs ('@' : tmp)
 
-runGhcWithResponse :: FilePath -> [String] -> [FilePath] -> Action ()
-runGhcWithResponse ghcPath flagArgs fileInputs = withResponseFile $ \tmp -> do
-    writeFile' tmp $ escapeArgs fileInputs
-    -- We can't put the flags in a response file, because some flags
-    -- require empty arguments (such as the -dep-suffix flag), but
-    -- that isn't supported yet due to #26560.
-    cmd [ghcPath] flagArgs ('@' : tmp)
-
+-- | Use a response file for ghc invocations to avoid issues with command line
+-- size limit on Windows (#26637).
+runGhcWithResponse :: FilePath -- ^ Path to ghc
+  -> [String] -- ^ Arguments passed on the command line
+  -> [FilePath] -- ^ Input file paths (passed via response file)
+  -> [CmdOption]
+  -> Action ()
+runGhcWithResponse ghcPath buildArgs buildInputs buildOptions = withResponseFile $ \tmp -> do
+  -- We can't put the buildArgs in a response file, because some flags require
+  -- empty arguments (such as the -dep-suffix flag), but that isn't supported
+  -- yet due to #26560.
+  writeFile' tmp (escapeArgs buildInputs)
+  cmd [ghcPath] buildArgs ('@' : tmp) buildOptions
 
 -- TODO: Some builders are required only on certain platforms. For example,
 -- 'Objdump' is only required on OpenBSD and AIX. Add support for platform
