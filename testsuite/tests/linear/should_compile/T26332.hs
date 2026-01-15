8624f459d4c787d3869dfa465da718695c57e895
Simon Peyton Jones <simon.peytonjones@gmail.com>
2026-01-15 12:56:36 +0000

    T26332 really should fail with -dlinear-core-lint

--- testsuite/tests/linear/should_compile/T26332.hs
+++ testsuite/tests/linear/should_compile/T26332.hs
@@ -5,6 +5,12 @@ module T26332 where
 
 import Unsafe.Coerce
 
+-- This function should be accepted by the typechecker, and should be
+-- linear-correct in the output of the desugarer, but will fail
+-- -dlinear-core-lint (which does a linear-lint check after every simplifier
+-- pass.  Because the optimiser discards a cast on `f` that only affects
+-- linearity
+
 toLinear
   :: forall a b p q.
      (a %p-> b) %1-> (a %q-> b)
