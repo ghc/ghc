811f56e5df1d238a5be7ac4cc10584a9396a8d25
Peter Trommler <ptrommler@acm.org>
2026-01-24 15:45:19 +0100

    hadrian: Fix dependency generation for assembler
    Assembler files allow # for comments unless in column 1. A modern
    cpp for C treats those a preprocessor directives. We tell gcc that
    a .S file is assembler with cpp and not C.
    
    Fixes #26819

--- hadrian/src/Settings/Builders/Cc.hs
+++ hadrian/src/Settings/Builders/Cc.hs
@@ -18,6 +18,7 @@ ccBuilderArgs = do
             , arg "-o", arg =<< getOutput ]
         , builder (Cc (FindCDependencies CDep)) ? findCDepExpr CDep
         , builder (Cc (FindCDependencies CxxDep)) ? findCDepExpr CxxDep
+        , builder (Cc (FindCDependencies AsmDep)) ? findCDepExpr AsmDep
         ]
     where
         findCDepExpr depType = do
@@ -26,10 +27,10 @@ ccBuilderArgs = do
                     , arg "-MM", arg "-MG"
                     , arg "-MF", arg output
                     , arg "-MT", arg $ dropExtension output -<.> "o"
-                    , case depType of CDep -> mempty; CxxDep -> arg "-std=c++11"
+                    , case depType of CDep -> mempty; CxxDep -> arg "-std=c++11"; AsmDep -> mempty
                     , cIncludeArgs
-                    , arg "-x", arg (case depType of CDep -> "c"; CxxDep -> "c++")
-                    , case depType of CDep -> mempty; CxxDep -> getContextData cxxOpts
+                    , arg "-x", arg (case depType of CDep -> "c"; CxxDep -> "c++"; AsmDep -> "assembler-with-cpp")
+                    , case depType of CDep -> mempty; CxxDep -> getContextData cxxOpts; AsmDep -> mempty
                     -- Pass 'ghcversion.h' to give sources access to the
                     -- `MIN_VERSION_GLASGOW_HASKELL` macro.
                     , notStage0 ? arg "-include" <> arg "rts/include/ghcversion.h"
