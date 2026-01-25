dfc2b01bc3b5ad13b01d25f5e5ac57fa4daff6f5
Cheng Shao <terrorjack@type.dance>
2026-01-25 18:48:08 +0100

    hadrian: remove redundant library/rts ways definitions from stock flavours
    This patch removes redundant library/rts ways definitions from stock
    flavours in hadrian; they can be replaced by applying appropriate
    filters on `defaultFlavour`.

--- hadrian/src/Settings/Flavours/Quick.hs
+++ hadrian/src/Settings/Flavours/Quick.hs
@@ -4,33 +4,16 @@ module Settings.Flavours.Quick
    )
 where
 
-import qualified Data.Set as Set
-
 import Expression
 import Flavour
-import Oracles.Flag
 import {-# SOURCE #-} Settings.Default
 
 -- Please update doc/flavours.md when changing this file.
 quickFlavour :: Flavour
-quickFlavour = defaultFlavour
+quickFlavour = disableProfiledLibs $ defaultFlavour
     { name        = "quick"
     , extraArgs        = quickArgs
-    , libraryWays = Set.fromList <$>
-                    mconcat
-                    [ pure [vanilla]
-                    , notStage0 ? platformSupportsSharedLibs ? pure [dynamic] ]
-    , rtsWays     = Set.fromList <$>
-                    mconcat
-                    [ pure
-                      [ vanilla, debug ]
-                    , targetSupportsThreadedRts ? pure [ threaded, threadedDebug ]
-                    , notStage0 ? platformSupportsSharedLibs ? pure
-                      [ dynamic, debugDynamic ]
-                    , notStage0 ? platformSupportsSharedLibs ? targetSupportsThreadedRts ? pure [
-                      threadedDynamic, threadedDebugDynamic
-                    ]
-                    ] }
+    }
 
 quickArgs :: Args
 quickArgs = sourceArgs SourceArgs
