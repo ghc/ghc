abf3d2598077fec507927d3f94b81d6ac04e524e
Duncan Coutts <duncan@well-typed.com>
2026-02-06 23:02:41 +0000

    Use BlockInfoForceNonClosure in the select I/O manager
    (cherry picked from commit 7c64632b46ac660fc0779ae96d9a05d11537b743)

--- rts/IOManager.c
+++ rts/IOManager.c
@@ -720,7 +720,8 @@ void syncIOWaitReady(Capability   *cap,
 #if defined(IOMGR_ENABLED_SELECT)
         case IO_MANAGER_SELECT:
         {
-            StgWord why_blocked = rw == IORead ? BlockedOnRead : BlockedOnWrite;
+            StgWord why_blocked = (rw == IORead ? BlockedOnRead : BlockedOnWrite)
+                                | BlockInfoForceNonClosure;
             tso->block_info.fd = fd;
             RELEASE_STORE(&tso->why_blocked, why_blocked);
             appendToIOBlockedQueue(cap, tso);
@@ -771,7 +772,7 @@ void syncDelay(Capability *cap, StgTSO *tso, HsInt us_delay)
         {
             LowResTime target = getDelayTarget(us_delay);
             tso->block_info.target = target;
-            RELEASE_STORE(&tso->why_blocked, BlockedOnDelay);
+            RELEASE_STORE(&tso->why_blocked, BlockedOnDelay | BlockInfoForceNonClosure);
             insertIntoSleepingQueue(cap, tso, target);
             break;
         }
@@ -805,6 +806,7 @@ void syncDelayCancel(Capability *cap, StgTSO *tso)
     switch (iomgr_type) {
 #if defined(IOMGR_ENABLED_SELECT)
         case IO_MANAGER_SELECT:
+            ASSERT(tso->why_blocked == (BlockedOnDelay | BlockInfoForceNonClosure));
             removeThreadFromQueue(cap, &cap->iomgr->sleeping_queue, tso);
             break;
 #endif
