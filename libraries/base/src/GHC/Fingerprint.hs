971259a91b0e269a447eede6a0e2a0a938713d84
Wolfgang Jeltsch <wolfgang@well-typed.com>
2026-02-19 22:13:41 +0200

    Make `GHC.Internal.Fingerprint` independent of I/O support

--- libraries/base/src/GHC/Fingerprint.hs
+++ libraries/base/src/GHC/Fingerprint.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Safe #-}
+{-# LANGUAGE Trustworthy #-}
 
 module GHC.Fingerprint (
         Fingerprint(..), fingerprint0,
@@ -9,3 +9,50 @@ module GHC.Fingerprint (
    ) where
 
 import GHC.Internal.Fingerprint
+
+import GHC.Internal.Base
+       (
+           ($),
+           return,
+           when,
+           not,
+           (&&),
+           (/=),
+           (++),
+           Int,
+           errorWithoutStackTrace
+       )
+import GHC.Internal.Show (show)
+import GHC.Internal.Data.Maybe (Maybe (Nothing, Just))
+import GHC.Internal.Word (Word8)
+import GHC.Internal.System.IO
+       (
+           IO,
+           FilePath,
+           IOMode (ReadMode),
+           withBinaryFile,
+           hGetBuf,
+           hIsEOF
+       )
+import GHC.Internal.Ptr (Ptr)
+
+-- | Computes the hash of a given file.
+-- This function runs in constant memory.
+--
+-- @since base-4.7.0.0
+getFileHash :: FilePath -> IO Fingerprint
+getFileHash path = withBinaryFile path ReadMode $ \ hdl ->
+    let
+        readChunk :: Ptr Word8 -> Int -> IO (Maybe Int)
+        readChunk bufferPtr bufferSize = do
+            chunkSize <- hGetBuf hdl bufferPtr bufferSize
+            isFinished <- hIsEOF hdl
+            when (chunkSize /= bufferSize && not isFinished)
+                 (
+                     errorWithoutStackTrace $
+                     "GHC.Fingerprint.getFileHash: could only read " ++
+                     show chunkSize                                  ++
+                     " bytes, but more are available"
+                 )
+            return (if isFinished then Just chunkSize else Nothing)
+    in fingerprintBufferedStream readChunk
