c7466bcc70ee3a178872597ff80722bb3c34ffe0
Brandon Chinn <brandonchinn178@gmail.com>
2026-02-25 08:27:09 -0800

    Add Infinity/NegInfinity/NaN pattern synonyms (#26961)

--- libraries/base/src/Data/RealFloat.hs
+++ libraries/base/src/Data/RealFloat.hs
@@ -1,4 +1,6 @@
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE Safe #-}
+{-# LANGUAGE ViewPatterns #-}
 
 -- |
 --
@@ -13,6 +15,41 @@
 
 module Data.RealFloat (
   RealFloat (..),
+
+  -- * Infinity + NaN
+  pattern Infinity,
+  pattern NegInfinity,
+  pattern NaN,
 ) where
 
+import Data.Bool
+import GHC.Internal.Data.Ord
 import GHC.Internal.Float
+import GHC.Internal.Real
+
+pattern Infinity :: (RealFloat a) => a
+pattern Infinity <- ((\x -> isInfinite x && x > 0) -> True) where Infinity = 1/0
+
+-- | Negative infinity
+--
+-- Provided for convenience. Could also use the following instead:
+--   * Pattern matching: @(negate -> Infinity)@
+--   * Expressions: @-Infinity@
+pattern NegInfinity :: (RealFloat a) => a
+pattern NegInfinity <- ((\x -> isInfinite x && x < 0) -> True) where NegInfinity = -1/0
+
+-- | A pattern synonym for NaN values.
+--
+-- Note: Per IEEE 754, NaN is never equal to itself, thus these two snippets
+-- have different behavior:
+--
+-- @
+-- -- foo1 NaN == "a"
+-- foo1 NaN = "a"
+-- foo1 _ = "b"
+--
+-- -- foo2 NaN == "b"
+-- foo2 x = if x == NaN then "a" else "b"
+-- @
+pattern NaN :: (RealFloat a) => a
+pattern NaN <- (isNaN -> True) where NaN = 0/0
