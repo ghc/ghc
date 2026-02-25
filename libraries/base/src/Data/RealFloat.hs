b91867d52550ddf8cb3a6273cdab552ecb08c436
Brandon Chinn <brandonchinn178@gmail.com>
2026-02-25 08:22:16 -0800

    Add Data.RealFloat

---
+++ libraries/base/src/Data/RealFloat.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE Safe #-}
+
+-- |
+--
+-- Module      :  Data.RealFloat
+-- Copyright   :  (c) The University of Glasgow 2026
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  stable
+-- Portability :  portable
+--
+
+module Data.RealFloat (
+  RealFloat (..),
+) where
+
+import GHC.Internal.Float
