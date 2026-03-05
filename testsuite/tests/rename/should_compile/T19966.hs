ghc/ghc

commit 990ea991a1c35fdb894fcb91f919fb8f8fed33dd

Fall back to types when looking up data constructors (#18740)
Before this patch, referring to a data constructor in a term-level
context led to a scoping error:

    ghci> id Int
    <interactive>:1:4: error: Data constructor not in scope: Int

After this patch, the renamer falls back to the type namespace
and successfully finds the Int. It is then rejected in the type
checker with a more useful error message:

    <interactive>:1:4: error:
    • Illegal term-level use of the type constructor ‘Int’
        imported from ‘Prelude’ (and originally defined in ‘GHC.Types’)
    • In the first argument of ‘id’, namely ‘Int’
      In the expression: id Int

We also do this for type variables.

compiler/GHC/Rename/Env.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE CPP            #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
 
 {-
 (c) The GRASP/AQUA Project, Glasgow University, 1992-2006
@@ -1006,6 +1007,17 @@ lookup_demoted rdr_name
            , text "instead of"
            , quotes (ppr name) <> dot ]
 
+-- If the given RdrName can be promoted to the type level and its promoted variant is in scope,
+-- lookup_promoted returns the corresponding type-level Name.
+-- Otherwise, the function returns Nothing.
+-- See Note [Promotion] below.
+lookup_promoted :: RdrName -> RnM (Maybe Name)
+lookup_promoted rdr_name
+  | Just promoted_rdr <- promoteRdrName rdr_name
+  = lookupOccRn_maybe promoted_rdr
+  | otherwise
+  = return Nothing
+
 badVarInType :: RdrName -> RnM Name
 badVarInType rdr_name
   = do { addErr (text "Illegal promoted term variable in a type:"
@@ -1041,6 +1053,26 @@ its namespace to DataName and do a second lookup.
 
 The final result (after the renamer) will be:
   HsTyVar ("Zero", DataName)
+
+Note [Promotion]
+~~~~~~~~~~~~~~~
+When the user mentions a type constructor or a type variable in a
+term-level context, then we report that a value identifier was expected
+instead of a type-level one. That makes error messages more precise.
+Previously, such errors contained only the info that a given value was out of scope (#18740).
+We promote the namespace of RdrName and look up after that
+(see the functions promotedRdrName and lookup_promoted).
+
+In particular, we have the following error message
+  • Illegal term-level use of the type constructor ‘Int’
+      imported from ‘Prelude’ (and originally defined in ‘GHC.Types’)
+  • In the first argument of ‘id’, namely ‘Int’
+    In the expression: id Int
+    In an equation for ‘x’: x = id Int
+
+when the user writes the following declaration
+
+  x = id Int
 -}
 
 lookupOccRnX_maybe :: (RdrName -> RnM (Maybe r)) -> (Name -> r) -> RdrName
@@ -1055,14 +1087,22 @@ lookupOccRn_maybe = lookupOccRnX_maybe lookupGlobalOccRn_maybe id
 
 lookupOccRn_overloaded :: Bool -> RdrName
                        -> RnM (Maybe (Either Name [Name]))
-lookupOccRn_overloaded overload_ok
-  = lookupOccRnX_maybe global_lookup Left
-      where
-        global_lookup :: RdrName -> RnM (Maybe (Either Name [Name]))
-        global_lookup n =
-          runMaybeT . msum . map MaybeT $
-            [ lookupGlobalOccRn_overloaded overload_ok n
-            , fmap Left . listToMaybe <$> lookupQualifiedNameGHCi n ]
+lookupOccRn_overloaded overload_ok rdr_name
+  = do { mb_name <- lookupOccRnX_maybe global_lookup Left rdr_name
+       ; case mb_name of
+           Nothing   -> fmap @Maybe Left <$> lookup_promoted rdr_name
+                        -- See Note [Promotion].
+                        -- We try looking up the name as a
+                        -- type constructor or type variable, if
+                        -- we failed to look up the name at the term level.
+           p         -> return p }
+
+  where
+    global_lookup :: RdrName -> RnM (Maybe (Either Name [Name]))
+    global_lookup n =
+      runMaybeT . msum . map MaybeT $
+        [ lookupGlobalOccRn_overloaded overload_ok n
+        , fmap Left . listToMaybe <$> lookupQualifiedNameGHCi n ]
 
 
 

compiler/GHC/Tc/Gen/Head.hs
@@ -755,6 +755,7 @@ tc_infer_assert assert_name
 tc_infer_id :: Name -> TcM (HsExpr GhcTc, TcSigmaType)
 tc_infer_id id_name
  = do { thing <- tcLookup id_name
+      ; global_env <- getGlobalRdrEnv
       ; case thing of
              ATcId { tct_id = id }
                -> do { check_local_id occ id
@@ -772,9 +773,31 @@ tc_infer_id id_name
                    | otherwise
                    -> nonBidirectionalErr id_name
 
+             AGlobal (ATyCon ty_con)
+               -> fail_tycon global_env ty_con
+
+             ATyVar name _
+                -> failWithTc $
+                     text "Illegal term-level use of the type variable"
+                       <+> quotes (ppr name)
+                       $$ nest 2 (text "bound at" <+> ppr (getSrcLoc name))
+
+             ATcTyCon ty_con
+               -> fail_tycon global_env ty_con
+
              _ -> failWithTc $
                   ppr thing <+> text "used where a value identifier was expected" }
   where
+    fail_tycon global_env ty_con =
+      let pprov = case lookupGRE_Name global_env (tyConName ty_con) of
+            Just gre -> nest 2 (pprNameProvenance gre)
+            Nothing  -> empty
+      in failWithTc (term_level_tycons ty_con $$ pprov)
+
+    term_level_tycons ty_con
+      = text "Illegal term-level use of the type constructor"
+          <+> quotes (ppr (tyConName ty_con))
+
     occ = nameOccName id_name
 
     return_id id = return (HsVar noExtField (noLoc id), idType id)
@@ -1144,4 +1167,3 @@ addExprCtxt e thing_inside
 
 exprCtxt :: HsExpr GhcRn -> SDoc
 exprCtxt expr = hang (text "In the expression:") 2 (ppr (stripParensHsExpr expr))
-

testsuite/tests/rename/should_fail/RnStaticPointersFail02.stderr
@@ -1,3 +1,7 @@
 
-RnStaticPointersFail02.hs:5:12: error:
-    Data constructor not in scope: T
+RnStaticPointersFail02.hs:5:12:
+Illegal term-level use of the type constructor ‘T’
+  defined at RnStaticPointersFail02.hs:7:1
+In the body of a static form: T
+  In the expression: static T
+  In an equation for ‘f’: f = static T

testsuite/tests/rename/should_fail/all.T
@@ -156,3 +156,5 @@ test('T17593', normal, compile_fail, [''])
 test('T18145', normal, compile_fail, [''])
 test('T18240a', normal, compile_fail, [''])
 test('T18240b', normal, compile_fail, [''])
+test('T18740a', normal, compile_fail, [''])
+test('T18740b', normal, compile_fail, [''])

testsuite/tests/th/all.T
@@ -515,3 +515,5 @@ test('T18121', normal, compile, [''])
 test('T18123', normal, compile, [''])
 test('T18388', normal, compile, [''])
 test('T18612', normal, compile, [''])
+test('T18740c', normal, compile_fail, [''])
+test('T18740d', normal, compile_fail, [''])

compiler/GHC/HsToCore/Quote.hs
@@ -2247,8 +2247,11 @@ repPsig (MkC p) (MkC t) = rep2 sigPName [p, t]
 
 --------------- Expressions -----------------
 repVarOrCon :: Name -> Core TH.Name -> MetaM (Core (M TH.Exp))
-repVarOrCon vc str | isDataOcc (nameOccName vc) = repCon str
-                   | otherwise                  = repVar str
+repVarOrCon vc str
+    | isVarNameSpace ns = repVar str  -- Both type and term variables (#18740)
+    | otherwise         = repCon str
+  where
+    ns = nameNameSpace vc
 
 repVar :: Core TH.Name -> MetaM (Core (M TH.Exp))
 repVar (MkC s) = rep2 varEName [s]

compiler/GHC/Types/Name/Occurrence.hs
@@ -52,6 +52,7 @@ module GHC.Types.Name.Occurrence (
         mkDFunOcc,
         setOccNameSpace,
         demoteOccName,
+        promoteOccName,
         HasOccName(..),
 
         -- ** Derived 'OccName's
@@ -208,12 +209,20 @@ pprNameSpaceBrief TcClsName = text "tc"
 
 -- demoteNameSpace lowers the NameSpace if possible.  We can not know
 -- in advance, since a TvName can appear in an HsTyVar.
--- See Note [Demotion] in GHC.Rename.Env
+-- See Note [Demotion] in GHC.Rename.Env.
 demoteNameSpace :: NameSpace -> Maybe NameSpace
 demoteNameSpace VarName = Nothing
 demoteNameSpace DataName = Nothing
 demoteNameSpace TvName = Nothing
 demoteNameSpace TcClsName = Just DataName
+
+-- promoteNameSpace promotes the NameSpace as follows.
+-- See Note [Promotion] in GHC.Rename.Env.
+promoteNameSpace :: NameSpace -> Maybe NameSpace
+promoteNameSpace DataName = Just TcClsName
+promoteNameSpace VarName = Just TvName
+promoteNameSpace TcClsName = Nothing
+promoteNameSpace TvName = Nothing
 
 {-
 ************************************************************************
@@ -336,10 +345,17 @@ mkClsOccFS :: FastString -> OccName
 mkClsOccFS = mkOccNameFS clsName
 
 -- demoteOccName lowers the Namespace of OccName.
--- see Note [Demotion]
+-- See Note [Demotion] in GHC.Rename.Env.
 demoteOccName :: OccName -> Maybe OccName
 demoteOccName (OccName space name) = do
   space' <- demoteNameSpace space
+  return $ OccName space' name
+
+-- promoteOccName promotes the NameSpace of OccName.
+-- See Note [Promotion] in GHC.Rename.Env.
+promoteOccName :: OccName -> Maybe OccName
+promoteOccName (OccName space name) = do
+  space' <- promoteNameSpace space
   return $ OccName space' name
 
 -- Name spaces are related if there is a chance to mean the one when one writes

compiler/GHC/Types/Name/Reader.hs
@@ -32,7 +32,7 @@ module GHC.Types.Name.Reader (
         nameRdrName, getRdrName,
 
         -- ** Destruction
-        rdrNameOcc, rdrNameSpace, demoteRdrName,
+        rdrNameOcc, rdrNameSpace, demoteRdrName, promoteRdrName,
         isRdrDataCon, isRdrTyVar, isRdrTc, isQual, isQual_maybe, isUnqual,
         isOrig, isOrig_maybe, isExact, isExact_maybe, isSrcRdrName,
 
@@ -182,12 +182,20 @@ rdrNameSpace :: RdrName -> NameSpace
 rdrNameSpace = occNameSpace . rdrNameOcc
 
 -- demoteRdrName lowers the NameSpace of RdrName.
--- see Note [Demotion] in GHC.Types.Name.Occurrence
+-- See Note [Demotion] in GHC.Rename.Env
 demoteRdrName :: RdrName -> Maybe RdrName
 demoteRdrName (Unqual occ) = fmap Unqual (demoteOccName occ)
 demoteRdrName (Qual m occ) = fmap (Qual m) (demoteOccName occ)
 demoteRdrName (Orig _ _) = Nothing
 demoteRdrName (Exact _) = Nothing
+
+-- promoteRdrName promotes the NameSpace of RdrName.
+-- See Note [Promotion] in GHC.Rename.Env.
+promoteRdrName :: RdrName -> Maybe RdrName
+promoteRdrName (Unqual occ) = fmap Unqual (promoteOccName occ)
+promoteRdrName (Qual m occ) = fmap (Qual m) (promoteOccName occ)
+promoteRdrName (Orig _ _) = Nothing
+promoteRdrName (Exact _)  = Nothing
 
         -- These two are the basic constructors
 mkRdrUnqual :: OccName -> RdrName

commit 6f7ac47bfff5cf92f3d345aff497fd12a53bedeb

TyCons in terms with -fdefer-out-of-scope-variables (#19966)
This fixes a regression introduced in 990ea991a1c35fdb894fcb91f919fb8f8fed33dd
which prevented the following program from compiling:

        {-# OPTIONS -fdefer-out-of-scope-variables #-}
        f = Int

In addition to that, we no longer abort type checking on
TcRnIllegalTermLevelUse, allowing GHC to report more errors.

compiler/GHC/Rename/Env.hs
@@ -424,7 +424,7 @@ lookupConstructorInfo qcon@(WithUserRdr _ con_name)
        ; case info of
             IAmConLike con_info -> return con_info
             UnboundGRE          -> return $ ConInfo (ConIsData []) ConHasPositionalArgs
-            IAmTyCon {}         -> failIllegalTyCon WL_ConLike qcon
+            IAmTyCon {}         -> failWithTc =<< mkIllegalTyConMessage ErrorWithoutFlag WL_ConLike qcon
             _ -> pprPanic "lookupConstructorInfo: not a ConLike" $
                       vcat [ text "name:" <+> ppr con_name ]
        }

compiler/GHC/Tc/Errors.hs
@@ -69,7 +69,7 @@ import GHC.Core.TyCo.Tidy
 import GHC.Core.InstEnv
 import GHC.Core.TyCon
 
-import GHC.Utils.Error  (diagReasonSeverity,  pprLocMsgEnvelope )
+import GHC.Utils.Error  (diagReasonSeverity)
 import GHC.Utils.Misc
 import GHC.Utils.Outputable as O
 import GHC.Utils.Panic
@@ -1387,11 +1387,9 @@ mkErrorTerm ct_loc ty ctxt msg supp hints
                   hints
          -- This will be reported at runtime, so we always want "error:" in the report, never "warning:"
        ; dflags <- getDynFlags
-       ; let err_msg = pprLocMsgEnvelope (initTcMessageOpts dflags) msg
-             err_str = showSDoc dflags $
-                       err_msg $$ text "(deferred type error)"
-
-       ; return $ evDelayedError ty err_str }
+       ; let msg_opts = initTcMessageOpts dflags
+             err_msg  = showSDoc dflags $ pprDeferredTypeError msg_opts msg
+       ; return $ evDelayedError ty err_msg }
 
 tryReporters :: SolverReportErrCtxt -> [ReporterSpec] -> [ErrorItem] -> TcM (SolverReportErrCtxt, [ErrorItem])
 -- Use the first reporter in the list whose predicate says True

compiler/GHC/Tc/Errors/Ppr.hs
@@ -19,6 +19,7 @@ module GHC.Tc.Errors.Ppr
   , TcRnMessageOpts(..)
   , pprTyThingUsedWrong
   , pprUntouchableVariable
+  , pprDeferredTypeError
 
   --
   , mismatchMsg_ExpectedActuals
@@ -123,6 +124,7 @@ import GHC.Utils.Lexeme
 import GHC.Utils.Misc
 import GHC.Utils.Outputable
 import GHC.Utils.Panic
+import GHC.Utils.Error
 
 import qualified GHC.LanguageExtensions as LangExt
 
@@ -1149,7 +1151,7 @@ instance Diagnostic TcRnMessage where
       -> mkSimpleDecorated $
            vcat [ text "The literal" <+> quotes (ppr lit) <+> text "cannot be promoted."
                 , text "It is of the unpromotable type" <+> quotes (ppr (hsLitType lit)) <> dot ]
-    TcRnIllegalTermLevelUse simple_msg rdr name err
+    TcRnIllegalTermLevelUse simple_msg _ rdr name err
       -> mkSimpleDecorated $
            if simple_msg
            then
@@ -2378,8 +2380,8 @@ instance Diagnostic TcRnMessage where
       -> ErrorWithoutFlag
     TcRnUnpromotableLit{}
       -> ErrorWithoutFlag
-    TcRnIllegalTermLevelUse{}
-      -> ErrorWithoutFlag
+    TcRnIllegalTermLevelUse _ reason _ _ _
+      -> reason -- Error, or a Warning if we are deferring type errors
     TcRnMatchesHaveDiffNumArgs{}
       -> ErrorWithoutFlag
     TcRnCannotBindScopedTyVarInPatSig{}
@@ -4459,6 +4461,11 @@ pprUntouchableVariable tv (Implic { ic_given = given, ic_info = skol_info, ic_en
         , nest 2 $ text "inside the constraints:" <+> pprEvVarTheta given
         , nest 2 $ text "bound by" <+> ppr skol_info
         , nest 2 $ text "at" <+> ppr (getCtLocEnvLoc env) ]
+
+pprDeferredTypeError :: DiagnosticOpts TcRnMessage -> MsgEnvelope TcRnMessage -> SDoc
+pprDeferredTypeError opts msg =
+  pprLocMsgEnvelope opts msg $$
+  text "(deferred type error)"
 
 -- | Which invisible bits of types should be displayed to the user when
 -- rendering a 'MismatchMsg'?

compiler/GHC/Tc/Errors/Types.hs
@@ -2495,6 +2495,8 @@ data TcRnMessage where
   TcRnIllegalTermLevelUse
     :: !Bool -- ^ should we give a simple "out of scope" message,
              -- instead of a full-blown "Illegal term level use" message?
+
+    -> !DiagnosticReason
     -> !RdrName -- ^ the user-written identifier
     -> !Name    -- ^ the type-level 'Name' we resolved it to
     -> !TermLevelUseErr

compiler/GHC/Tc/Gen/Head.hs
@@ -39,6 +39,7 @@ import GHC.Tc.Utils.Unify
 import GHC.Tc.Utils.Instantiate
 import GHC.Tc.Instance.Family ( tcLookupDataFamInst )
 import GHC.Tc.Errors.Types
+import GHC.Tc.Errors.Ppr ( pprDeferredTypeError )
 import GHC.Tc.Solver          ( InferMode(..), simplifyInfer )
 import GHC.Tc.Utils.Env
 import GHC.Tc.Utils.TcMType
@@ -50,7 +51,7 @@ import GHC.Tc.Zonk.TcType
 
 
 import GHC.Core.FamInstEnv    ( FamInstEnvs )
-import GHC.Core.UsageEnv      ( singleUsageUE, UsageEnv )
+import GHC.Core.UsageEnv      ( singleUsageUE, bottomUE, UsageEnv )
 import GHC.Core.PatSyn( PatSyn, patSynName )
 import GHC.Core.ConLike( ConLike(..) )
 import GHC.Core.DataCon
@@ -67,6 +68,8 @@ import GHC.Types.Error
 import GHC.Builtin.Names
 
 import GHC.Driver.DynFlags
+import GHC.Driver.Ppr (showSDoc)
+import GHC.Driver.Config.Diagnostic (initTcMessageOpts)
 import GHC.Utils.Misc
 import GHC.Utils.Outputable as Outputable
 import GHC.Utils.Panic
@@ -799,7 +802,7 @@ tcInferId lname@(L loc (WithUserRdr rdr id_name))
   = tc_infer_id lname
 
 tc_infer_id :: LocatedN (WithUserRdr Name) -> TcM (HsExpr GhcTc, TcSigmaType)
-tc_infer_id (L loc (WithUserRdr rdr id_name))
+tc_infer_id (L loc qnm@(WithUserRdr rdr id_name))
  = do { thing <- tcLookup id_name
       ; (expr,ty) <- case thing of
              ATcId { tct_id = id }
@@ -813,8 +816,9 @@ tc_infer_id (L loc (WithUserRdr rdr id_name))
 
              AGlobal (AConLike cl) -> tcInferConLike cl
 
-             (tcTyThingTyCon_maybe -> Just tc) -> failIllegalTyCon WL_Term (WithUserRdr rdr (tyConName tc))
-             ATyVar name _ -> failIllegalTyVar (WithUserRdr rdr name)
+             (tcTyThingTyCon_maybe -> Just _)
+                        -> defer_or_fail $ \reason -> mkIllegalTyConMessage reason WL_Term qnm
+             ATyVar _ _ -> defer_or_fail $ \reason -> mkIllegalTyVarMessage reason qnm
 
              _ -> failWithTc $ TcRnExpectedValueId thing
 
@@ -822,6 +826,27 @@ tc_infer_id (L loc (WithUserRdr rdr id_name))
        ; return (expr, ty) }
   where
     return_id id = return (mkHsVar (L loc id), idType id)
+
+    defer_or_fail :: (DiagnosticReason -> TcM TcRnMessage) -> TcM (HsExpr GhcTc, TcSigmaType)
+    defer_or_fail mk_msg = do
+      defer_out_of_scope <- goptM Opt_DeferOutOfScopeVariables
+      let reason | defer_out_of_scope = WarningWithFlag Opt_WarnDeferredOutOfScopeVariables
+                 | otherwise = ErrorWithoutFlag
+      msg <- mk_msg reason
+      addDiagnosticTc msg
+      msg_to_hole msg
+
+    msg_to_hole :: TcRnMessage -> TcM (HsExpr GhcTc, TcType)
+    msg_to_hole msg = do
+      dflags <- getDynFlags
+      let lrdr = L loc rdr
+      msg_envelope <- mkTcRnMessage (locA loc) msg
+      let msg_opts = initTcMessageOpts dflags
+          err_msg  = showSDoc dflags $ pprDeferredTypeError msg_opts msg_envelope
+      ty  <- newOpenFlexiTyVarTy
+      her <- newExprHoleRef ty (evDelayedError ty err_msg)
+      tcEmitBindingUsage bottomUE   -- Holes fit any usage environment (#18491)
+      return (HsHole (HoleVar lrdr, her), ty)
 
 {- Note [Overview of assertions]
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

compiler/GHC/Tc/Utils/Env.hs
@@ -26,7 +26,7 @@ module GHC.Tc.Utils.Env(
         tcLookupLocatedClass, tcLookupAxiom,
         lookupGlobal, lookupGlobal_maybe,
         addTypecheckedBinds, addEvBinds, addTopEvBinds,
-        failIllegalTyCon, failIllegalTyVar,
+        mkIllegalTyConMessage, mkIllegalTyVarMessage,
 
         -- Local environment
         tcExtendKindEnv, tcExtendKindEnvList,
@@ -145,6 +145,7 @@ import Control.Monad
 import Data.IORef
 import Data.List          ( intercalate )
 import qualified Data.List.NonEmpty as NE
+
 
 {- *********************************************************************
 *                                                                      *
@@ -296,7 +297,7 @@ tcLookupConLike qname@(WithUserRdr _ name) = do
     thing <- tcLookupGlobal name
     case thing of
         AConLike cl -> return cl
-        ATyCon  {}  -> failIllegalTyCon WL_ConLike qname
+        ATyCon  {}  -> failWithTc =<< mkIllegalTyConMessage ErrorWithoutFlag WL_ConLike qname
         _           -> wrongThingErr WrongThingConLike (AGlobal thing) name
 
 tcLookupRecSelParent :: HsRecUpdParent GhcRn -> TcM RecSelParent
@@ -383,29 +384,30 @@ instance MonadThings (IOEnv (Env TcGblEnv TcLclEnv)) where
     lookupThing = tcLookupGlobal
 
 -- Illegal term-level use of type things
-failIllegalTyCon :: WhatLooking -> WithUserRdr Name -> TcM a
-failIllegalTyVar :: WithUserRdr Name -> TcM a
-(failIllegalTyCon, failIllegalTyVar) = (fail_tycon, fail_tyvar)
+mkIllegalTyConMessage :: DiagnosticReason -> WhatLooking -> WithUserRdr Name -> TcM TcRnMessage
+mkIllegalTyVarMessage :: DiagnosticReason -> WithUserRdr Name -> TcM TcRnMessage
+(mkIllegalTyConMessage, mkIllegalTyVarMessage) = (fail_tycon, fail_tyvar)
   where
-    fail_tycon what_looking (WithUserRdr rdr tc_nm) = do
+    fail_tycon reason what_looking (WithUserRdr rdr tc_nm) = do
       gre <- getGlobalRdrEnv
       let mb_gre = lookupGRE_Name gre tc_nm
           err = case greInfo <$> mb_gre of
             Just (IAmTyCon ClassFlavour) -> ClassTE
             _ -> TyConTE
-      fail_with_msg what_looking dataName rdr tc_nm (TermLevelUseGRE <$> mb_gre) err
+      fail_with_msg reason what_looking dataName rdr tc_nm (TermLevelUseGRE <$> mb_gre) err
 
-    fail_tyvar (WithUserRdr rdr nm) =
-      fail_with_msg WL_Term varName rdr nm (Just TermLevelUseTyVar) TyVarTE
+    fail_tyvar reason (WithUserRdr rdr nm) =
+      fail_with_msg reason WL_Term varName rdr nm (Just TermLevelUseTyVar) TyVarTE
 
-    fail_with_msg what_looking whatName rdr nm pprov err = do
-      required_type_arguments <- xoptM LangExt.RequiredTypeArguments
-      (imp_errs, hints) <- get_suggestions required_type_arguments what_looking whatName rdr
+    fail_with_msg :: DiagnosticReason -> WhatLooking -> NameSpace -> RdrName -> Name
+                  -> Maybe TermLevelUseCtxt -> TermLevelUseErr -> TcM TcRnMessage
+    fail_with_msg reason what_looking whatName rdr nm pprov err = do
+      (imp_errs, hints) <- get_suggestions what_looking whatName rdr
       hfdc <- getHoleFitDispConfig
       unit_state <- hsc_units <$> getTopEnv
       let
         want_simple = want_simple_msg hints
-        msg = TcRnIllegalTermLevelUse want_simple rdr nm err
+        msg = TcRnIllegalTermLevelUse want_simple reason rdr nm err
         info = ErrInfo { errInfoContext =
                            if want_simple
                            then []
@@ -415,9 +417,11 @@ failIllegalTyVar :: WithUserRdr Name -> TcM a
                              NE.nonEmpty imp_errs
                        , errInfoHints = hints
                        }
-      failWithTc $ TcRnMessageWithInfo unit_state (mkDetailedMessage info msg)
+        msg_w_info = TcRnMessageWithInfo unit_state (mkDetailedMessage info msg)
+      return msg_w_info
 
-    get_suggestions required_type_arguments what_looking ns rdr = do
+    get_suggestions what_looking ns rdr = do
+      required_type_arguments <- xoptM LangExt.RequiredTypeArguments
       show_helpful_errors <- goptM Opt_HelpfulErrors
       if not show_helpful_errors || (required_type_arguments && isVarNameSpace ns)
       then return ([], [])  -- See Note [Suppress hints with RequiredTypeArguments]
@@ -460,7 +464,7 @@ rather than:
 
 This was reported in #23982.
 
-To achieve this, in 'failIllegalTyCon' and 'failIllegalTyVar', we include a
+To achieve this, in 'mkIllegalTyConMessage' and 'mkIllegalTyVarMessage', we include a
 little heuristic to decide whether to emit an "out of scope" message rather than
 an "illegal term-level use" message: when we have a term to suggest to the user,
 then give the simpler "out of scope" error message.

compiler/GHC/Tc/Utils/TcMType.hs
@@ -45,6 +45,8 @@ module GHC.Tc.Utils.TcMType (
   emitWantedEqs, emitNewExprHole,
   newTcEvBinds, newNoTcEvBinds, addTcEvBind,
 
+  newExprHoleRef,
+
   newCoercionHole, fillCoercionHole, isFilledCoercionHole,
   checkCoercionHole,
 
@@ -313,6 +315,12 @@ emitNewExprHole occ ty
                          , hole_loc  = loc }
        ; emitHole hole
        ; return her }
+
+newExprHoleRef :: Type -> EvTerm -> TcM HoleExprRef
+newExprHoleRef ty ev
+  = do { u <- newUnique
+       ; ref <- newTcRef ev
+       ; return $ HER ref ty u }
 
 newDict :: Class -> [TcType] -> TcM DictId
 newDict cls tys

testsuite/tests/rename/should_compile/T19966.hs
@@ -1,7 +1,29 @@
+{-# OPTIONS -fdefer-out-of-scope-variables #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module T19966 where
 
--- I is out of scope, so out-of-scope is a warning.
-x = I
+import Control.Exception (evaluate)
+import Data.Proxy
+
+-- "I" is out of scope, but we accept the definition
+-- thanks to -fdefer-out-of-scope-variables.
+ex1 = I
+
+-- "Bool" is in scope, but it is a type constructor and
+-- cannot be used as a term.
+--
+-- The -fdefer-out-of-scope-variables flag plays a double duty
+-- and recovers from this failure, too.
+ex2 = Bool
+
+data T =
+  Boo1 -- we want this to be suggested instead of Bool in ex2
 
 -- IO is out of scope, so out-of-scope should be a warning, but fails.
-y = IO
+-- "a" is in scope, but it is a type variable and
+-- cannot be used as a term.
+--
+-- The -fdefer-out-of-scope-variables flag permits this, too.
+ex3 :: forall a. Proxy a -> Bool
+ex3 _ = not a

testsuite/tests/rename/should_compile/T19966.stderr
@@ -1,0 +1,17 @@
+T19966.hs:11:7: warning: [GHC-88464] [-Wdeferred-out-of-scope-variables (in -Wdefault)]
+    Data constructor not in scope: I
+
+T19966.hs:18:7: warning: [GHC-01928] [-Wdeferred-out-of-scope-variables (in -Wdefault)]
+    • Data constructor out of scope: ‘Bool’.
+      NB: the type constructor ‘Bool’ cannot appear in this position.
+    • In the expression: Bool
+      In an equation for ‘ex2’: ex2 = Bool
+    Suggested fix: Perhaps use ‘Boo1’ (line 21)
+
+T19966.hs:29:13: warning: [GHC-01928] [-Wdeferred-out-of-scope-variables (in -Wdefault)]
+    • Illegal term-level use of the type variable ‘a’
+    • bound at T19966.hs:28:15
+    • In the first argument of ‘not’, namely ‘a’
+      In the expression: not a
+      In an equation for ‘ex3’: ex3 _ = not a
+

testsuite/tests/rename/should_compile/T19966_main.hs
@@ -1,0 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Control.Exception (handle, evaluate, TypeError, displayException)
+import Data.Proxy (Proxy(Proxy))
+
+import T19966 (ex1, ex2, ex3)
+
+check :: a -> IO ()
+check e =
+  handle (putStrLn . displayException @TypeError)
+         (evaluate (e `seq` ()))
+
+main :: IO ()
+main = do
+  check ex1
+  check ex2
+  check (ex3 Proxy)

testsuite/tests/rename/should_compile/T19966_main.stdout
@@ -1,0 +1,12 @@
+T19966.hs:11:7: error: [GHC-88464]
+    • Data constructor not in scope: I
+    • In an equation for ‘ex1’: ex1 = I
+(deferred type error)
+T19966.hs:18:7: warning: [GHC-01928] [-Wdeferred-out-of-scope-variables (in -Wdefault)]
+    Data constructor out of scope: ‘Bool’.
+    NB: the type constructor ‘Bool’ cannot appear in this position.
+(deferred type error)
+T19966.hs:29:13: warning: [GHC-01928] [-Wdeferred-out-of-scope-variables (in -Wdefault)]
+    • Illegal term-level use of the type variable ‘a’
+    • bound at T19966.hs:28:15
+(deferred type error)

testsuite/tests/rename/should_compile/all.T
@@ -176,7 +176,8 @@ test('T17837', normal, compile, [''])
 test('T18497', [], makefile_test, ['T18497'])
 test('T18264', [], makefile_test, ['T18264'])
 test('T17853', [], multimod_compile, ['T17853', '-v0'])
-test('T19966', expect_broken(19966), compile, ['-fdefer-out-of-scope-variables'])
+test('T19966', normal, compile, ['-fdefer-out-of-scope-variables'])
+test('T19966_main', [extra_files(['T19966.hs'])], multimod_compile_and_run, ['T19966_main', '-v0'])
 test('T20472', normal, compile, [''])
 test('T18862', normal, compile, [''])
 test('unused_haddock', normal, compile, ['-haddock -Wall'])

testsuite/tests/rename/should_fail/RnStaticPointersFail02.stderr
@@ -1,3 +1,9 @@
+RnStaticPointersFail02.hs:5:5: error: [GHC-39999]
+    • No instance for ‘ghc-internal-9.1500.0:GHC.Internal.Data.Typeable.Internal.Typeable
+                         t0’
+        arising from a static form
+    • In the expression: static T
+      In an equation for ‘f’: f = static T
 
 RnStaticPointersFail02.hs:5:12: error: [GHC-01928]
     • Illegal term-level use of the type constructor ‘T’
@@ -5,3 +11,4 @@ RnStaticPointersFail02.hs:5:12: error: [GHC-01928]
     • In the body of a static form: T
       In the expression: static T
       In an equation for ‘f’: f = static T
+