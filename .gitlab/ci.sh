19b04530ce12d2d87d5fb1213df7b5ef320cb81e
Cheng Shao <terrorjack@type.dance>
2026-03-09 15:09:22 +0100

    hadrian: bump index state & bootstrap plans
    This patch bumps hadrian index state & bootstrap plans:
    
    - The updated index state allows bootstrapping from 9.14 without cabal
      allow-newer hacks
    - The updated bootstrap plans all contain shake-0.19.9 containing
      important bugfix, allowing a subsequent patch to bump shake bound to
      ensure the bugfix is included
    - ghc 9.14.1 bootstrap plan is added

--- .gitlab/ci.sh
+++ .gitlab/ci.sh
@@ -8,7 +8,7 @@ set -Eeuo pipefail
 
 # Configuration:
 # N.B. You may want to also update the index-state in hadrian/cabal.project.
-HACKAGE_INDEX_STATE="2025-12-19T19:24:24Z"
+HACKAGE_INDEX_STATE="2026-03-10T17:36:36Z"
 MIN_HAPPY_VERSION="1.20"
 MIN_ALEX_VERSION="3.2.6"
 
