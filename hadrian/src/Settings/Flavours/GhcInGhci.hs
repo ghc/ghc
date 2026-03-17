59862d76789401f8c90fc1c19a04a3140d772390
Cheng Shao <terrorjack@type.dance>
2026-03-17 20:03:07 +0000

    hadrian: fix ghc-in-ghci flavour stage0 shared libraries
    This patch fixes missing stage0 shared libraries in hadrian
    ghc-in-ghci flavour, which was accidentally dropped in
    669d09f950a6e88b903d9fd8a7571531774d4d5d and resulted in a regression
    in HLS support on linux/macos. Fixes #27057.

--- hadrian/src/Settings/Flavours/GhcInGhci.hs
+++ hadrian/src/Settings/Flavours/GhcInGhci.hs
@@ -1,14 +1,22 @@
 module Settings.Flavours.GhcInGhci (ghcInGhciFlavour) where
 
+import qualified Data.Set as Set
 import Expression
 import Flavour
+import Oracles.Flag
 import {-# SOURCE #-} Settings.Default
 
 -- Please update doc/flavours.md when changing this file.
 ghcInGhciFlavour :: Flavour
 ghcInGhciFlavour = disableProfiledLibs $ defaultFlavour
     { name        = "ghc-in-ghci"
-    , extraArgs        = ghciArgs
+    , extraArgs   = ghciArgs
+    , libraryWays =
+        Set.fromList
+            <$> mconcat
+                [ pure [vanilla]
+                , platformSupportsSharedLibs ? pure [dynamic]
+                ]
     }
 
 ghciArgs :: Args
