c6b091d82bb59a64c427b8cd885d4ff8e670ea93
Matthew Pickering <matthewtpickering@gmail.com>
2026-03-17 15:22:21 +0000

    Make home unit dependencies stored as sets
    Co-authored-by: Wolfgang Jeltsch <wolfgang@well-typed.com>
    (cherry picked from commit d17d1435d1ac1f1e4f806181da944ec541e79d19)

--- compiler/GHC/Unit/Finder.hs
+++ compiler/GHC/Unit/Finder.hs
@@ -69,6 +69,7 @@ import GHC.Driver.Env
 import GHC.Driver.Config.Finder
 import GHC.Types.Unique.Set
 import Data.List.NonEmpty ( NonEmpty (..) )
+import qualified Data.Set as Set (toList)
 import qualified System.OsPath as OsPath
 import qualified Data.List.NonEmpty as NE
 
@@ -216,7 +217,7 @@ findImportedModuleNoHsc fc fopts ue mhome_unit mod_name mb_pkg =
                   Nothing -> ue_homeUnitState ue
                   Just home_unit -> HUG.homeUnitEnv_units $ ue_findHomeUnitEnv (homeUnitId home_unit) ue
     hpt_deps :: [UnitId]
-    hpt_deps  = homeUnitDepends units
+    hpt_deps  = Set.toList (homeUnitDepends units)
     other_fopts  = map (\uid -> (uid, initFinderOpts (homeUnitEnv_dflags (ue_findHomeUnitEnv uid ue)))) hpt_deps
 
 -- | Locate a plugin module requested by the user, for a compiler
