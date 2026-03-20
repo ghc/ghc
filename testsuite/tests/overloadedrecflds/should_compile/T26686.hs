ghc/ghc

commit fbdc623a33d6ff02c2295eeda863180ffcccd4de

Add hints for unsolved HasField constraints
This commit adds hints and explanations for unsolved 'HasField'
constraints.

GHC will now provide additional explanations for an unsolved constraint
of the form 'HasField fld_name rec_ty fld_ty'; the details are laid out in
Note [Error messages for unsolved HasField constraints], but briefly:

  1. Provide similar name suggestions (e.g. mis-spelled field name)
     and import suggestions (record field not in scope).
     These result in actionable 'GhcHints', which is helpful to provide
     code actions in HLS.

  2. Explain why GHC did not solve the constraint, e.g.:

    - 'fld_name' is not a string literal (e.g. a type variable)
    - 'rec_ty' is a TyCon without any fields, e.g. 'Int' or 'Bool'.
    - 'fld_ty' contains existentials variables or foralls.
    - The record field is a pattern synonym field (GHC does not generate
      HasField instances for those).
    - 'HasField' is a custom 'TyCon', not actually the built-in
      'HasField' typeclass from 'GHC.Records'.

On the way, we slightly refactor the mechanisms for import suggestions
in GHC.Rename.Unbound. This is to account for the fact that, for
'HasField', we don't care whether the field is imported qualified or
unqualified. 'importSuggestions' was refactored, we now have
'sameQualImportSuggestions' and 'anyQualImportSuggestions'.

Fixes #18776 #22382 #26480

compiler/GHC/Tc/Instance/Class.hs
@@ -1247,6 +1247,11 @@ matchHasField dflags short_cut clas tys mb_ct_loc
                      -- The selector must not be "naughty" (i.e. the field
                      -- cannot have an existentially quantified type),
                      -- and it must not be higher-rank.
+                     --
+                     -- See also 'GHC.Tc.Errors.hasFieldInfo_maybe', which is
+                     -- responsible for the error messages in cases of unsolved
+                     -- HasField constraints when the field type runs afoul
+                     -- of these conditions.
                    ; if (isNaughtyRecordSelector sel_id) || not (isTauTy sel_ty)
                      then try_user_instances
                      else
@@ -1306,6 +1311,11 @@ lookupHasFieldLabel
 -- A complication is that `T` might be a data family, so we need to
 -- look it up in the `fam_envs` to find its representation tycon.
 lookupHasFieldLabel fam_inst_envs rdr_env arg_tys
+
+  -- NB: if you edit this function, you might also want to update
+  -- GHC.Tc.Errors.hasfieldInfo_maybe which is responsible for error messages
+  -- when GHC /does not/ solve a 'HasField' constraint.
+
   |  -- We are matching HasField {k} {r_rep} {a_rep} x r a...
     (_k : _rec_rep : _fld_rep : x_ty : rec_ty : fld_ty : _) <- arg_tys
     -- x should be a literal string

compiler/GHC/Tc/Types/Origin.hs
@@ -524,7 +524,7 @@ data CtOrigin
   ----------- Below here, all are Origins for Wanted constraints ------------
 
   | OccurrenceOf Name          -- ^ Occurrence of an overloaded identifier
-  | OccurrenceOfRecSel RdrName -- ^ Occurrence of a record selector
+  | OccurrenceOfRecSel (LocatedN RdrName) -- ^ Occurrence of a record selector
   | AppOrigin                  -- ^ An application of some kind
 
   | SpecPragOrigin UserTypeCtxt    -- ^ Specialisation pragma for
@@ -558,7 +558,10 @@ data CtOrigin
                         -- IMPORTANT: These constraints will never cause errors;
                         -- See Note [Constraints to ignore] in GHC.Tc.Errors
   | SectionOrigin
-  | GetFieldOrigin FastString
+  | GetFieldOrigin (LocatedN FastString)
+
+  -- | A overloaded record field projection like @.fld@ or @.fld1.fld2.fld@.
+  | RecordFieldProjectionOrigin (FieldLabelStrings GhcRn)
   | TupleOrigin         -- (..,..)
   | ExprSigOrigin       -- e :: ty
   | PatSigOrigin        -- p :: ty
@@ -566,7 +569,7 @@ data CtOrigin
   | ProvCtxtOrigin      -- The "provided" context of a pattern synonym signature
         (PatSynBind GhcRn GhcRn) -- Information about the pattern synonym, in
                                  -- particular the name and the right-hand side
-  | RecordUpdOrigin
+  | RecordUpdOrigin (LHsRecUpdFields GhcRn)
   | ViewPatOrigin
 
   -- | 'ScOrigin' is used only for the Wanted constraints for the
@@ -737,7 +740,7 @@ lexprCtOrigin (L _ e) = exprCtOrigin e
 
 exprCtOrigin :: HsExpr GhcRn -> CtOrigin
 exprCtOrigin (HsVar _ (L _ (WithUserRdr _ name))) = OccurrenceOf name
-exprCtOrigin (HsGetField _ _ (L _ f)) = GetFieldOrigin (field_label $ unLoc $ dfoLabel f)
+exprCtOrigin (HsGetField _ _ (L _ f)) = GetFieldOrigin (fmap field_label $ dfoLabel f)
 exprCtOrigin (HsOverLabel _ l)  = OverLabelOrigin l
 exprCtOrigin (ExplicitList {})    = ListOrigin
 exprCtOrigin (HsIPVar _ ip)       = IPOccOrigin ip
@@ -749,9 +752,9 @@ exprCtOrigin (HsAppType _ e1 _)   = lexprCtOrigin e1
 exprCtOrigin (OpApp _ _ op _)     = lexprCtOrigin op
 exprCtOrigin (NegApp _ e _)       = lexprCtOrigin e
 exprCtOrigin (HsPar _ e)          = lexprCtOrigin e
-exprCtOrigin (HsProjection _ _)   = SectionOrigin
-exprCtOrigin (SectionL _ _ _)     = SectionOrigin
-exprCtOrigin (SectionR _ _ _)     = SectionOrigin
+exprCtOrigin (HsProjection _ p)   = RecordFieldProjectionOrigin (FieldLabelStrings $ fmap noLocA p)
+exprCtOrigin (SectionL {})        = SectionOrigin
+exprCtOrigin (SectionR {})        = SectionOrigin
 exprCtOrigin (ExplicitTuple {})   = Shouldn'tHappenOrigin "explicit tuple"
 exprCtOrigin ExplicitSum{}        = Shouldn'tHappenOrigin "explicit sum"
 exprCtOrigin (HsCase _ _ matches) = matchesCtOrigin matches
@@ -760,7 +763,7 @@ exprCtOrigin (HsMultiIf _ rhs)   = lGRHSCtOrigin rhs
 exprCtOrigin (HsLet _ _ e)       = lexprCtOrigin e
 exprCtOrigin (HsDo {})           = DoOrigin
 exprCtOrigin (RecordCon {})      = Shouldn'tHappenOrigin "record construction"
-exprCtOrigin (RecordUpd {})      = RecordUpdOrigin
+exprCtOrigin (RecordUpd _ _ flds)= RecordUpdOrigin flds
 exprCtOrigin (ExprWithTySig {})  = ExprSigOrigin
 exprCtOrigin (ArithSeq {})       = Shouldn'tHappenOrigin "arithmetic sequence"
 exprCtOrigin (HsPragE _ _ e)     = lexprCtOrigin e
@@ -779,7 +782,7 @@ exprCtOrigin (XExpr (ExpandedThingRn thing _)) | OrigExpr a <- thing = exprCtOri
                                                | OrigStmt _ <- thing = DoOrigin
                                                | OrigPat p  <- thing = DoPatOrigin p
 exprCtOrigin (XExpr (PopErrCtxt {})) = Shouldn'tHappenOrigin "PopErrCtxt"
-exprCtOrigin (XExpr (HsRecSelRn f))  = OccurrenceOfRecSel (foExt f)
+exprCtOrigin (XExpr (HsRecSelRn f))  = OccurrenceOfRecSel $ L (getLoc $ foLabel f) (foExt f)
 
 -- | Extract a suitable CtOrigin from a MatchGroup
 matchesCtOrigin :: MatchGroup GhcRn (LHsExpr GhcRn) -> CtOrigin
@@ -937,7 +940,7 @@ ppr_br AppOrigin             = text "an application"
 ppr_br (IPOccOrigin name)    = hsep [text "a use of implicit parameter", quotes (ppr name)]
 ppr_br (OverLabelOrigin l)   = hsep [text "the overloaded label"
                                     ,quotes (char '#' <> ppr l)]
-ppr_br RecordUpdOrigin       = text "a record update"
+ppr_br (RecordUpdOrigin {})  = text "a record update"
 ppr_br ExprSigOrigin         = text "an expression type signature"
 ppr_br PatSigOrigin          = text "a pattern type signature"
 ppr_br PatOrigin             = text "a pattern"
@@ -945,6 +948,7 @@ ppr_br ViewPatOrigin         = text "a view pattern"
 ppr_br (LiteralOrigin lit)   = hsep [text "the literal", quotes (ppr lit)]
 ppr_br (ArithSeqOrigin seq)  = hsep [text "the arithmetic sequence", quotes (ppr seq)]
 ppr_br SectionOrigin         = text "an operator section"
+ppr_br (RecordFieldProjectionOrigin p) = text "the record selector" <+> quotes (ppr p)
 ppr_br (GetFieldOrigin f)    = hsep [text "selecting the field", quotes (ppr f)]
 ppr_br AssocFamPatOrigin     = text "the LHS of a family instance"
 ppr_br TupleOrigin           = text "a tuple"

testsuite/tests/overloadedrecflds/should_fail/all.T
@@ -33,6 +33,8 @@ test('hasfieldfail03', normal, compile_fail, [''])
 test('hasfieldfail04', normal, compile_fail, [''])
 test('T14953', [extra_files(['T14953_A.hs', 'T14953_B.hs'])],
      multimod_compile_fail, ['T14953', ''])
+test('T26480', extra_files(['T26480_aux1.hs', 'T26480_aux2.hs']), multimod_compile_fail, ['T26480', '-v0'])
+test('T26480b', normal, compile_fail, [''])
 test('DuplicateExports', normal, compile_fail, [''])
 test('T17420', [extra_files(['T17420A.hs'])], multimod_compile_fail,
      ['T17420', ''])

compiler/GHC/Core/ConLike.hs
@@ -26,6 +26,8 @@ module GHC.Core.ConLike (
         , conLikeFieldType
         , conLikeIsInfix
         , conLikeHasBuilder
+
+        , isExistentialRecordField
     ) where
 
 import GHC.Prelude
@@ -35,7 +37,7 @@ import GHC.Core.Multiplicity
 import GHC.Core.PatSyn
 import GHC.Core.TyCo.Rep (Type, ThetaType)
 import GHC.Core.TyCon (tyConDataCons)
-import GHC.Core.Type(mkTyConApp)
+import GHC.Core.Type(mkTyConApp, tyCoVarsOfType)
 import GHC.Types.Unique
 import GHC.Types.Name
 import GHC.Types.Name.Reader
@@ -43,6 +45,7 @@ import GHC.Types.Basic
 
 import GHC.Types.GREInfo
 import GHC.Types.Var
+import GHC.Types.Var.Set
 import GHC.Utils.Misc
 import GHC.Utils.Outputable
 
@@ -239,3 +242,23 @@ conLikeFieldType (RealDataCon dc) label = dataConFieldType dc label
 conLikeIsInfix :: ConLike -> Bool
 conLikeIsInfix (RealDataCon dc) = dataConIsInfix dc
 conLikeIsInfix (PatSynCon ps)   = patSynIsInfix  ps
+
+-- | Is this record field a naughty record field due to the presence of
+-- existential type variables?
+--
+-- Different from 'isNaughtyRecordSelector' because the latter is also true
+-- in the presence of @-XNoFieldSelectors@.
+--
+-- See Note [Naughty record selectors] in GHC.Tc.TyCl.Utils.
+isExistentialRecordField :: Type -> ConLike -> Bool
+isExistentialRecordField field_ty con =
+  case con of
+    RealDataCon {} -> not $ field_ty_tvs `subVarSet` res_ty_tvs
+    PatSynCon {}   -> not $ field_ty_tvs `subVarSet` mkVarSet univ_tvs
+       -- In the PatSynCon case, the selector type is (data_ty -> field_ty), but
+       -- fvs(data_ty) are all universals (see Note [Pattern synonym result type] in
+       -- GHC.Core.PatSyn, so no need to check them.
+  where
+    field_ty_tvs = tyCoVarsOfType field_ty
+    res_ty_tvs   = tyCoVarsOfType data_ty
+    (univ_tvs, _, _, _, _, _, data_ty) = conLikeFullSig con

compiler/GHC/Rename/Names.hs
@@ -2453,8 +2453,8 @@ badImportItemErr iface decl_spec ie sub avails = do
           -- Only keep imported items, and set the "HowInScope" to
           -- "Nothing" to avoid printing "imported from..." in the suggestion
           -- error message.
-          imported_item (SimilarRdrName rdr_name (Just (ImportedBy {})))
-            = Just (SimilarRdrName rdr_name Nothing)
+          imported_item (SimilarRdrName rdr_name gre (Just (ImportedBy {})))
+            = Just (SimilarRdrName rdr_name gre Nothing)
           imported_item _ = Nothing
 
     checkIfDataCon = checkIfAvailMatches isDataConName

commit 52c3e6ba9f03d19a4fa85aee6a4c417b538160f7

Improve incomplete record selector warnings
This commit stops GHC from emitting spurious incomplete record selector
warnings for bare selectors/projections such as .fld

There are two places we currently emit incomplete record selector
warnings:

  1. In the desugarer, when we see a record selector or an occurrence
     of 'getField'. Here, we can use pattern matching information to
     ensure we don't give false positives.
  2. In the typechecker, which might sometimes give false positives but
     can emit warnings in cases that the pattern match checker would
     otherwise miss.

This is explained in Note [Detecting incomplete record selectors]
in GHC.HsToCore.Pmc.

Now, we obviously don't want to emit the same error twice, and generally
we prefer (1), as those messages contain fewer false positives. So we
suppress (2) when we are sure we are going to emit (1); the logic for
doing so is in GHC.Tc.Instance.Class.warnIncompleteRecSel,
and works by looking at the CtOrigin.

Now, the issue was that this logic handled explicit record selectors as
well as overloaded record field selectors such as "x.r" (which turns
into a simple GetFieldOrigin CtOrigin), but it didn't properly handle
record projectors like ".fld" or ".fld1.fld2" (which result in other
CtOrigins such as 'RecordFieldProjectionOrigin').

To solve this problem, we re-use the 'isHasFieldOrigin' introduced in
fbdc623a (slightly adjusted).

On the way, we also had to update the desugarer with special handling
for the 'ExpandedThingTc' case in 'ds_app', to make sure that
'ds_app_var' sees all the type arguments to 'getField' in order for it
to indeed emit warnings like in (1).

Fixes #26686

compiler/GHC/HsToCore/Expr.hs
@@ -721,6 +721,15 @@ ds_app (XExpr (ConLikeTc con)) _hs_args core_args
 ds_app (XExpr (HsRecSelTc (FieldOcc { foLabel = L _ sel_id }))) _hs_args core_args
   = ds_app_rec_sel sel_id sel_id core_args
 
+ds_app (XExpr (ExpandedThingTc _orig e)) hs_args core_args
+  = ds_app e hs_args core_args
+  -- NB: this is important for the 'getField' case of 'ds_app_var', which needs
+  -- to see all type arguments to 'getField' at once, while for record field
+  -- projections such as (.fld) we may get:
+  --
+  --   XExpr (ExpandedThingTc (.fld) (getField @Symbol @LiftedRep @LiftedRep "fld"))
+  --     `HsAppType` rec_ty `HsAppType` fld
+
 ds_app (HsVar _ lfun) hs_args core_args
   = ds_app_var lfun hs_args core_args
 
@@ -736,8 +745,10 @@ ds_app_var (L loc fun_id) hs_args core_args
   -----------------------
   -- Deal with getField applications. General form:
   --   getField
-  --     @GHC.Types.Symbol                        {k}
-  --     @"sel"                                   x_ty
+  --     @Symbol                                  {k}
+  --     @LiftedRep                               {r_rep}
+  --     @LiftedRep                               {a_rep}
+  --     @"sel"                                   fld
   --     @T                                       r_ty
   --     @Int                                     a_ty
   --     ($dHasField :: HasField "sel" T Int)     dict

compiler/GHC/HsToCore/Pmc.hs
@@ -375,6 +375,10 @@ Finally, there are two more items addressing -XOverloadedRecordDot:
   the (IRS6) warning in the typechecker for a `HasField` constraint that
   arises from a record-dot HsGetField occurrence.  Happily, this is easy to do
   by looking at its `CtOrigin`. Tested in T24891.
+
+  The same applies for record field projection operators such as (.fld) and
+  (.fld1.fld2), which have different 'CtOrigin's. The 'isHasFieldOrigin'
+  function catches those as well. Tested in T26686.
 -}
 
 pmcRecSel :: Id       -- ^ Id of the selector

compiler/GHC/Tc/Errors.hs
@@ -97,6 +97,7 @@ import Data.Ord           ( comparing )
 import Data.Either        ( partitionEithers )
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import qualified Data.Semigroup as Semi
 
 {-
 ************************************************************************
@@ -2714,14 +2715,33 @@ hasFieldInfo_maybe rdr_env fam_inst_envs item
 
   -- (HF2e) It's a custom HasField constraint, not the one from GHC.Records.
   | Just (tc, _) <- splitTyConApp_maybe (errorItemPred item)
-  , getOccString tc == "HasField"
-  , isHasFieldOrigin (errorItemOrigin item)
-  = return $ Just $ CustomHasField tc
+  = do { rebindable_syntax <- xoptM LangExt.RebindableSyntax
+       ; return $
+           if want_custom_hasfield_msg tc rebindable_syntax
+           then Just $ CustomHasField tc
+           else Nothing
+       }
 
   | otherwise
   = return Nothing
 
   where
+
+    orig = errorItemOrigin item
+
+    want_custom_hasfield_msg tc rebindable_syntax
+      | getOccString tc == "HasField"
+      = Semi.getAny $ foldMapCtOrigin (Semi.Any . is_has_field) orig
+      | otherwise
+      = False
+      where
+        -- Handle custom 'getField'/'setField' with RebindableSyntax.
+        is_has_field (OccurrenceOf n)
+          | rebindable_syntax
+          , getOccString n `elem` ["getField", "setField"]
+          = True
+        is_has_field o
+          = isHasFieldOrigin o
 
     get_parent_nm :: Name -> TcM (Maybe (Either PatSyn TyCon))
     get_parent_nm nm =
@@ -2761,22 +2781,6 @@ hasField_maybe pred =
     _ -> Nothing
   -- NB: we deliberately don't handle rebound 'HasField' (with -XRebindableSyntax),
   -- as GHC only has built-in instances for the built-in 'HasField' class.
-
--- | Does this constraint arise from GHC internal mechanisms that desugar to
--- usage of the 'HasField' typeclass (e.g. OverloadedRecordDot, etc)?
---
--- Just used heuristically to decide whether to print an informative message to
--- the user (see (H2e) in Note [Error messages for unsolved HasField constraints]).
-isHasFieldOrigin :: CtOrigin -> Bool
-isHasFieldOrigin = \case
-  OccurrenceOf n ->
-    -- A heuristic...
-    getOccString n `elem` ["getField", "setField"]
-  OccurrenceOfRecSel {} -> True
-  RecordUpdOrigin {} -> True
-  RecordFieldProjectionOrigin {} -> True
-  GetFieldOrigin {} -> True
-  _ -> False
 
 -----------------------
 -- relevantBindings looks at the value environment and finds values whose

compiler/GHC/Tc/Instance/Class.hs
@@ -20,7 +20,7 @@ import GHC.Tc.Instance.Typeable
 import GHC.Tc.Utils.TcMType
 import GHC.Tc.Types.Evidence
 import GHC.Tc.Types.CtLoc
-import GHC.Tc.Types.Origin ( InstanceWhat (..), SafeOverlapping, CtOrigin(GetFieldOrigin) )
+import GHC.Tc.Types.Origin ( InstanceWhat (..), SafeOverlapping, isHasFieldOrigin )
 import GHC.Tc.Instance.Family( tcGetFamInstEnvs, tcLookupDataFamInst, FamInstEnvs )
 
 import GHC.Rename.Env( addUsedGRE, addUsedDataCons, DeprecationWarnings (..) )
@@ -1275,8 +1275,8 @@ warnIncompleteRecSel :: DynFlags -> Id -> CtLoc -> TcM ()
 -- Warn about incomplete record selectors
 -- See (IRS6) in Note [Detecting incomplete record selectors] in GHC.HsToCore.Pmc
 warnIncompleteRecSel dflags sel_id ct_loc
-  | not (isGetFieldOrigin (ctLocOrigin ct_loc))
-      -- isGetFieldOrigin: see (IRS7) in
+  | not $ isHasFieldOrigin (ctLocOrigin ct_loc)
+      -- isHasFieldOrigin: see (IRS7) in
       -- Note [Detecting incomplete record selectors] in GHC.HsToCore.Pmc
   , RecSelId { sel_cons = RSI { rsi_undef = fallible_cons } } <- idDetails sel_id
   , not (null fallible_cons)
@@ -1287,11 +1287,6 @@ warnIncompleteRecSel dflags sel_id ct_loc
   = return ()
   where
     maxCons = maxUncoveredPatterns dflags
-
-    -- GHC.Tc.Gen.App.tcInstFun arranges that the CtOrigin of (r.x) is GetFieldOrigin,
-    -- despite the expansion to (getField @"x" r)
-    isGetFieldOrigin (GetFieldOrigin {}) = True
-    isGetFieldOrigin _                   = False
 
 lookupHasFieldLabel
   :: FamInstEnvs -> GlobalRdrEnv -> [Type]

compiler/GHC/Tc/Types/Origin.hs
@@ -16,7 +16,8 @@ module GHC.Tc.Types.Origin (
   CtOrigin(..), exprCtOrigin, lexprCtOrigin, matchesCtOrigin, grhssCtOrigin,
   invisibleOrigin_maybe, isVisibleOrigin, toInvisibleOrigin,
   pprCtOrigin, pprCtOriginBriefly, isGivenOrigin,
-  defaultReprEqOrigins, isWantedSuperclassOrigin,
+  foldMapCtOrigin,
+  defaultReprEqOrigins, isWantedSuperclassOrigin, isHasFieldOrigin,
   ClsInstOrQC(..), NakedScFlag(..), NonLinearPatternReason(..),
   HsImplicitLiftSplice(..),
   StandaloneDeriv,
@@ -52,6 +53,8 @@ import GHC.Tc.Utils.TcType
 
 import GHC.Hs
 
+import GHC.Builtin.Names (getFieldName)
+
 import GHC.Core.DataCon
 import GHC.Core.ConLike
 import GHC.Core.TyCon
@@ -79,6 +82,8 @@ import GHC.Types.Unique.Supply
 import qualified Data.Kind as Hs
 import Data.List.NonEmpty (NonEmpty (..))
 import Data.Maybe (isNothing)
+import qualified Data.Semigroup as Semi
+import GHC.Generics
 
 {- *********************************************************************
 *                                                                      *
@@ -993,6 +998,95 @@ pprNonLinearPatternReason PatternSynonymReason = parens (text "pattern synonyms 
 pprNonLinearPatternReason ViewPatternReason = parens (text "view patterns aren't linear")
 pprNonLinearPatternReason OtherPatternReason = empty
 
+
+{- *********************************************************************
+*                                                                      *
+                     Recursing through CtOrigin
+*                                                                      *
+********************************************************************* -}
+
+-- | Fold over a 'CtOrigin', looking through all recursive
+-- occurrences of 'CtOrigin' within 'CtOrigin'.
+foldMapCtOrigin :: forall m. Semigroup m => (CtOrigin -> m) -> CtOrigin -> m
+foldMapCtOrigin f = go
+  where
+    go :: CtOrigin -> m
+    go orig =
+      case orig of
+        KindEqOrigin _ _ o _ -> recur o
+        CycleBreakerOrigin o -> recur o
+        WantedSuperclassOrigin _ o -> recur o
+        DefaultReprEqOrigin _ _ o -> recur o
+        ScOrigin cls_or_qc _sc_flag ->
+          case cls_or_qc of
+            IsQC _ o -> recur o
+            IsClsInst -> f orig
+
+        -- Explicit pattern match on remaining constructors, in order to get
+        -- better pattern-match warnings when constructors are changed or
+        -- added/removed. This isn't entirely fool-proof, as someone may still
+        -- change the type of one of the fields and hide a 'CtOrigin' inside.
+        --
+        -- This approach was chosen instead of using 'syb'/'GHC.Generics',
+        -- because those would require deriving 'Data.Data'/'Generic' on
+        -- a huge number of datatypes.
+        GivenOrigin {} -> f orig
+        GivenSCOrigin {} -> f orig
+        OccurrenceOf {} -> f orig
+        OccurrenceOfRecSel {} -> f orig
+        AppOrigin {} -> f orig
+        SpecPragOrigin {} -> f orig
+        TypeEqOrigin {}-> f orig
+        IPOccOrigin {} -> f orig
+        OverLabelOrigin {} -> f orig
+        LiteralOrigin {} -> f orig
+        QualLiteralOrigin {} -> f orig
+        NegateOrigin {} -> f orig
+        ArithSeqOrigin {} -> f orig
+        AssocFamPatOrigin {} -> f orig
+        SectionOrigin {} -> f orig
+        GetFieldOrigin {} -> f orig
+        RecordFieldProjectionOrigin {} -> f orig
+        TupleOrigin {} -> f orig
+        ExprSigOrigin {} -> f orig
+        PatSigOrigin {} -> f orig
+        PatOrigin {} -> f orig
+        ProvCtxtOrigin {} -> f orig
+        RecordUpdOrigin {} -> f orig
+        ViewPatOrigin {} -> f orig
+        DerivOrigin {} -> f orig
+        DerivOriginDC {} -> f orig
+        DerivOriginCoerce {}  -> f orig
+        DefaultOrigin {} -> f orig
+        DoOrigin {} -> f orig
+        DoPatOrigin {} -> f orig
+        MCompOrigin {} -> f orig
+        MCompPatOrigin {} -> f orig
+        ProcOrigin {} -> f orig
+        ArrowCmdOrigin {} -> f orig
+        AnnOrigin {} -> f orig
+        FunDepOrigin {} -> f orig
+        ExprHoleOrigin {} -> f orig
+        TypeHoleOrigin {} -> f orig
+        PatCheckOrigin {} -> f orig
+        ListOrigin {} -> f orig
+        IfThenElseOrigin {} -> f orig
+        BracketOrigin {} -> f orig
+        StaticOrigin {} -> f orig
+        ImpedanceMatching {} -> f orig
+        Shouldn'tHappenOrigin {} -> f orig
+        InstProvidedOrigin {} -> f orig
+        NonLinearPatternOrigin {} -> f orig
+        OmittedFieldOrigin {} -> f orig
+        UsageEnvironmentOf {} -> f orig
+        FRROrigin {} -> f orig
+        InstanceSigOrigin {} -> f orig
+        AmbiguityCheckOrigin {} -> f orig
+        ImplicitLiftOrigin {} -> f orig
+
+      where
+        recur o = f orig Semi.<> go o
+
 {- *********************************************************************
 *                                                                      *
                Defaulting of representational equalities
@@ -1004,21 +1098,10 @@ pprNonLinearPatternReason OtherPatternReason = empty
 -- That is, this function extracts all occurrences of the 'DefaultReprEqOrigin'
 -- constructor from within a 'CtOrigin'.
 defaultReprEqOrigins :: CtOrigin -> [(CtOrigin, (TcType, TcType))]
-defaultReprEqOrigins = go
+defaultReprEqOrigins = foldMapCtOrigin go
   where
     go = \case
-      DefaultReprEqOrigin l r o -> (o, (l, r)) : go o
-
-      -- Handle recursive occurrences of 'CtOrigin' within 'CtOrigin'.
-      -- TODO: use syb to derive this, so that the following never goes out of date.
-      ScOrigin cls_or_qc _ ->
-        case cls_or_qc of
-          IsClsInst -> []
-          IsQC _ o -> go o
-      KindEqOrigin _ _ o _ -> go o
-      CycleBreakerOrigin o -> go o
-      WantedSuperclassOrigin _ o -> go o
-
+      DefaultReprEqOrigin l r o -> [(o, (l, r))]
       _ -> []
 
 {- *********************************************************************
@@ -1045,6 +1128,37 @@ isPushCallStackOrigin_maybe orig               = Just orig_fs
   --      but we can perhaps improve it in the light of user feedback
   where
     orig_fs = mkFastString (showSDocUnsafe (pprCtOriginBriefly orig))
+
+{- *********************************************************************
+*                                                                      *
+                       HasField and CtOrigin
+*                                                                      *
+********************************************************************* -}
+
+-- | Does this constraint arise from GHC internal mechanisms that desugar to
+-- usage of the 'HasField' typeclass (e.g. OverloadedRecordDot, etc)?
+--
+-- Used in two places:
+--
+--  - When reporting an unsolved 'HasField' constraint, to decide whether to
+--    print an informative message to the user.
+--    See (H2e) in Note [Error messages for unsolved HasField constraints]
+--    in GHC.Tc.Errors.
+--  - To avoid emitting a poor "incomplete record selector" warning directly
+--    in typechecker, in cases when the desugarer will be able to emit a better
+--    error message, due to having better pattern match checking information.
+--    See (IRS7) in Note [Detecting incomplete record selectors]
+--    in GHC.HsToCore.Pmc
+isHasFieldOrigin :: CtOrigin -> Bool
+isHasFieldOrigin = Semi.getAny . foldMapCtOrigin (Semi.Any . go)
+  where
+    go = \case
+      OccurrenceOf n -> n == getFieldName
+      OccurrenceOfRecSel {} -> True
+      RecordFieldProjectionOrigin {} -> True
+      GetFieldOrigin {} -> True
+      RecordUpdOrigin {} -> True
+      _ -> False
 
 {-
 ************************************************************************

testsuite/tests/overloadedrecflds/should_compile/T26686.hs
@@ -1,0 +1,37 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE GADTs #-}
+
+{-# OPTIONS_GHC -Wincomplete-record-selectors #-}
+
+module T26686 where
+
+import Data.Kind
+
+data A
+data B
+
+data G = G { f2 :: Int }
+
+data T x where
+  TA :: { ta :: G } -> T x
+  TB :: { tb :: G } -> T B
+
+data H a = H { f1 :: T a }
+
+test1_ok :: T A -> G
+test1_ok = (.ta)
+test2_ok :: T A -> Int
+test2_ok = (.ta.f2)
+test3_ok :: H A -> G
+test3_ok = (.f1.ta)
+test4_ok :: H A -> Int
+test4_ok = (.f1.ta.f2)
+
+test1_bad :: T x -> G
+test1_bad = (.ta)
+test2_bad :: T x -> Int
+test2_bad = (.ta.f2)
+test3_bad :: H x -> G
+test3_bad = (.f1.ta)
+test4_bad :: H x -> Int
+test4_bad = (.f1.ta.f2)

testsuite/tests/overloadedrecflds/should_compile/T26686.stderr
@@ -1,0 +1,16 @@
+T26686.hs:31:13: warning: [GHC-17335] [-Wincomplete-record-selectors (in -Wall)]
+    Selecting the record field ‘ta’ may fail for the following constructors:
+      TB
+
+T26686.hs:33:13: warning: [GHC-17335] [-Wincomplete-record-selectors (in -Wall)]
+    Selecting the record field ‘ta’ may fail for the following constructors:
+      TB
+
+T26686.hs:35:13: warning: [GHC-17335] [-Wincomplete-record-selectors (in -Wall)]
+    Selecting the record field ‘ta’ may fail for the following constructors:
+      TB
+
+T26686.hs:37:13: warning: [GHC-17335] [-Wincomplete-record-selectors (in -Wall)]
+    Selecting the record field ‘ta’ may fail for the following constructors:
+      TB
+

testsuite/tests/overloadedrecflds/should_compile/all.T
@@ -30,6 +30,7 @@ test('T21720', req_th, compile, [''])
 test('T21898', normal, compile, [''])
 test('T22160', [extra_files(['T22160_A.hs', 'T22160_B.hs', 'T22160_C.hs'])]
              , multimod_compile, ['T22160_A T22160_B T22160_C T22160', '-v0'])
+test('T26686', normal, compile, [''])
 test('DupFldFixity3', normal, compile, [''])
 test('overloadedrecflds10'
     , [extra_files(['OverloadedRecFlds10_A.hs', 'OverloadedRecFlds10_B.hs', 'OverloadedRecFlds10_C.hs'])]