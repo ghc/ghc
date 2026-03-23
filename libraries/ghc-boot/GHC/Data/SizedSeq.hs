cf94211929dba3cbe4b9a9aba0de1be68dfbe5e9
Cheng Shao <terrorjack@type.dance>
2026-03-23 19:55:52 +0000

    ghc-boot: remove unused SizedSeq instances and functions
    This commit removes unused `SizedSeq` instances and functions, only
    keeping the bits we need for hpc tick sequence for now.

--- libraries/ghc-boot/GHC/Data/SizedSeq.hs
+++ libraries/ghc-boot/GHC/Data/SizedSeq.hs
@@ -1,48 +1,20 @@
-{-# LANGUAGE StandaloneDeriving, DeriveGeneric, CPP #-}
 module GHC.Data.SizedSeq
   ( SizedSeq(..)
   , emptySS
   , addToSS
-  , addListToSS
   , ssElts
   , sizeSS
   ) where
 
 import Prelude -- See note [Why do we import Prelude here?]
-import Control.DeepSeq
-import Data.Binary
-import GHC.Generics
-
-#if ! MIN_VERSION_base(4,20,0)
-import Data.List (foldl')
-#endif
 
 data SizedSeq a = SizedSeq {-# UNPACK #-} !Word [a]
-  deriving (Generic, Show)
-
-instance Functor SizedSeq where
-  fmap f (SizedSeq sz l) = SizedSeq sz (fmap f l)
-
-instance Foldable SizedSeq where
-  foldr f c ss = foldr f c (ssElts ss)
-
-instance Traversable SizedSeq where
-  traverse f (SizedSeq sz l) = SizedSeq sz . reverse <$> traverse f (reverse l)
-
-instance Binary a => Binary (SizedSeq a)
-
-instance NFData a => NFData (SizedSeq a) where
-  rnf (SizedSeq _ xs) = rnf xs
 
 emptySS :: SizedSeq a
 emptySS = SizedSeq 0 []
 
 addToSS :: SizedSeq a -> a -> SizedSeq a
 addToSS (SizedSeq n r_xs) x = SizedSeq (n+1) (x:r_xs)
-
--- NB, important this is eta-expand so that foldl' is inlined.
-addListToSS :: SizedSeq a -> [a] -> SizedSeq a
-addListToSS s xs = foldl' addToSS s xs
 
 ssElts :: SizedSeq a -> [a]
 ssElts (SizedSeq _ r_xs) = reverse r_xs
