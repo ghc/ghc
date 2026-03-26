22c5b7ccbc2b7aa3629b9e9e493f13da172d0ced
Cheng Shao <terrorjack@type.dance>
2026-03-26 15:04:29 +0000

    ghci: remove unused GHCi.BinaryArray
    This patch removes the unused `GHCi.BinaryArray` module from `ghci`.
    Closes #27108.

--- testsuite/tests/ghci/should_run/BinaryArray.hs
+++ testsuite/tests/ghci/should_run/BinaryArray.hs
@@ -4,24 +4,9 @@ import Data.Binary.Put
 import Data.Binary (Binary, get, put)
 import Data.Array.Byte
 import Data.Array.Unboxed as AU
-import Data.Array.IO (IOUArray)
-import Data.Array.MArray (MArray)
-import Data.Array as A
 import Data.Array.Base as A
-import Foreign.Storable
-import GHCi.BinaryArray
 import GHCi.ResolvedBCO
 import GHC.Word
-
-roundtripTest :: (IArray UArray a, MArray IOUArray a IO, Eq a)
-              => UArray Int a -> IO ()
-roundtripTest arr =
-    let ser  = Data.Binary.Put.runPut $ putArray arr
-    in case Data.Binary.Get.runGetOrFail getArray ser of
-         Right (_, _, arr')
-           | arr == arr'  -> return ()
-           | otherwise    -> putStrLn "failed to round-trip"
-         Left _           -> putStrLn "deserialization failed"
 
 -- See Note [BCOByteArray serialization]
 roundtripTestByteArray :: forall a . (IArray UArray a, Eq a, Binary (BCOByteArray a))
@@ -37,12 +22,5 @@ roundtripTestByteArray (UArray _ _ _ arr#) =
 
 main :: IO ()
 main = do
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Int)
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Word)
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Word8)
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Word16)
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Word32)
-    roundtripTest (AU.listArray (1,500) [1..] :: UArray Int Word64)
-    roundtripTest (AU.listArray (1,500) ['a'..] :: UArray Int Char)
     roundtripTestByteArray (AU.listArray (1,500) [1..] :: UArray Int Word)
     roundtripTestByteArray (AU.listArray (1,500) [1..] :: UArray Int Word16)
