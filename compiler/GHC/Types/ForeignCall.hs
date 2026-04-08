3cc4305fe6dd0bf2f30083a8c33d557c05f2b3d5
Duncan Coutts <duncan@well-typed.com>
2026-04-08 15:43:43 +0100

    Remove unused TargetIsInThisUnit case from CCallStaticTargetUnit
    The CCallStaticTargetUnit infortmation in the XStaticTarget (TTG
    exstension point) is actually pretty dubious.
    
    Historically it was a (Maybe Unit) and the comments say that it is only
    used by out-of-line primop calls on Windows. Indeed it is only used on
    Windows for the dynamic way, when each unit is built as a separate DLL
    (which we currently do not support). And even in that hypothetical
    situation, the information that actually gets filled in is completely
    bogus.
    
    It _could_ be used in the windows dll case to correctly distinguish
    making an out-of-line call to a cmm primop that is defined in the
    current dll or in an external dll. But the information gets filled in in
    the renaming phase with the _current_ unit. This is blatently wrong.
    Many out-of-line primops are defined in the rts, which is obviously not
    the current unit. It _could_ be a reasonable default, that cmm primops
    are expected to be in the current dll, but we would have to have support
    to specify a different location, i.e. the rts.
    
    Furthermore, in addition to TargetIsInThisUnit being unused (after the
    previous patch) it should never be used because it uses an implicit
    "this" which will be wrong as soon as the information is inlined across
    units. Instead of an implicit unit, it should use an explicit unit which
    can be initialised to the defining unit and then be compared later to
    the current unit. This will work correctly when looking at unfoldings
    across unit boundaries.

--- compiler/GHC/Types/ForeignCall.hs
+++ compiler/GHC/Types/ForeignCall.hs
@@ -204,7 +204,6 @@ instance Outputable CCallSpec where
                 ForeignValue    -> text "__ffi_static_ccall_value"
                 ForeignFunction -> text "__ffi_static_ccall"
               pprUnit ext = case staticTargetUnit ext of
-                TargetIsInThisUnit  -> empty
                 TargetIsInThat unit -> ppr unit
               (srcTxt, pPkgId) = (staticTargetLabel ext, pprUnit ext)
           in pCallType
@@ -314,8 +313,7 @@ instance Binary CCallConv where
 -- either it is in this currently compiling compilation 'Unit',
 -- or it is in /that other/, compilation 'Unit'.
 data CCallStaticTargetUnit
-  = TargetIsInThisUnit  -- ^ In this current 'Unit'.
-  | TargetIsInThat Unit -- ^ In that other   'Unit'.
+  = TargetIsInThat Unit -- ^ In that other 'Unit'.
   deriving (Data, Eq)
 
 data StaticTargetGhc = StaticTargetGhc
@@ -355,16 +353,13 @@ deriving instance Eq (Header (GhcPass p))
 
 instance NFData CCallStaticTargetUnit where
     rnf = \case
-      TargetIsInThisUnit  -> ()
       TargetIsInThat unit -> rnf unit
 
 instance Binary CCallStaticTargetUnit where
     put_ bh = \case
-      TargetIsInThisUnit  -> putByte bh 0
       TargetIsInThat unit -> putByte bh 1 *> put_ bh unit
 
     get bh = getByte bh >>= \case
-      0 -> pure TargetIsInThisUnit
       _ -> TargetIsInThat <$> get bh
 
 instance NFData CTypeGhc where
