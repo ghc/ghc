8cb99552f607f6bc4000e45ab32532d50c8bb996
Sylvain Henry <sylvain@haskus.fr>
2026-04-08 14:32:56 +0200

    hadrian: warn when package index is missing (#16484)
    Since cabal-install 3.0 we can query the path of remote-repo-cache and
    check if hackage package index is present.
    
    Fixes #16484

--- hadrian/build-cabal.bat
+++ hadrian/build-cabal.bat
@@ -34,18 +34,20 @@ for /F "delims=. tokens=1,2,3,4" %%a in ("%CABVERSTR%") do (
     set CABPATCH=%%d
 )
 
-set "_cabal_ok=0"
-if %CABMAJOR% gtr 2 set _cabal_ok=1
-if %CABMAJOR% equ 2 (
-    if %CABMINOR% geq 2 set _cabal_ok=1
-)
-if %_cabal_ok% equ 1 (
-    "%CABAL%" --project-file=%PROJ% new-build %CABFLAGS% -j exe:hadrian
-    rem use new-exec instead of new-run to make sure that the build-tools (alex & happy) are in PATH
-    "%CABAL%" --project-file=%PROJ% new-exec  %CABFLAGS%    hadrian -- ^
-        --directory "%CD%" ^
-        %*
-) else (
-    echo Cabal version is too old; you need at least cabal-install 2.2
+if %CABMAJOR% lss 3 (
+    echo Cabal version is too old; you need at least cabal-install 3.0
     exit /B 2
 )
+
+for /F "tokens=*" %%a in ('"%CABAL%" --with-compiler=%GHC% path --output-format=key-value 2^>NUL ^| findstr /B "remote-repo-cache:"') do set REMOTE_REPO_CACHE=%%a
+set REMOTE_REPO_CACHE=%REMOTE_REPO_CACHE:remote-repo-cache: =%
+if not exist "%REMOTE_REPO_CACHE%\hackage.haskell.org" (
+    echo Please run 'cabal update' first
+    exit /B 2
+)
+
+"%CABAL%" --project-file=%PROJ% new-build %CABFLAGS% -j exe:hadrian
+rem use new-exec instead of new-run to make sure that the build-tools (alex & happy) are in PATH
+"%CABAL%" --project-file=%PROJ% new-exec  %CABFLAGS%    hadrian -- ^
+    --directory "%CD%" ^
+    %*
