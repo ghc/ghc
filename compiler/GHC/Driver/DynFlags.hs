ffa245f11984d6088590c76b75227d3fcb8934b6
mangoiv <contact@mangoiv.com>
2026-05-11 14:12:35 +0200

    driver: enable -finter-module-far-jumps by default
    this fixes a compatibility bug with certain binutils/gcc versions where
    we were seeing jump offset overflow errors.
    This commit can probably reverted if we stop supporting the problematic
    binutils/gcc verions (2.44 and 14.2, respectively)
    
    Reolves #26994

--- compiler/GHC/Driver/DynFlags.hs
+++ compiler/GHC/Driver/DynFlags.hs
@@ -1241,7 +1241,6 @@ defaultFlags settings
       Opt_SpecialiseIncoherents,
       Opt_WriteSelfRecompInfo
     ]
-
     ++ [f | (ns,f) <- optLevelFlags, 0 `elem` ns]
              -- The default -O0 options
 
