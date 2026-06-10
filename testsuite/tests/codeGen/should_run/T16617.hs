7aba67c2d3f6d008f3bc9fd7ce3d5953bff36283
ARATA Mizuki <minorinoki@gmail.com>
2026-06-10 17:34:36 +0900

    RISC-V NCG: Zero-extend the result of castFloatToWord32
    According to the ISA manual, FMV.X.W sign-extends the result.
    We need to truncate the result to avoid creating an exotic Word32 value.
    
    Fixes #27300
    
    (cherry picked from commit 291ce3aafe4ca3d2562154a28c595a858987d9f1)

--- testsuite/tests/codeGen/should_run/T16617.hs
+++ testsuite/tests/codeGen/should_run/T16617.hs
@@ -1,10 +1,19 @@
 import GHC.Float
 
+{-# OPAQUE noinline #-}
+noinline :: a -> a
+noinline x = x
+
 main :: IO ()
 main = do
   -- As per #16617, Word32s should be non-negative
   print $ castFloatToWord32 (-1)
   print $ toInteger (castFloatToWord32 (-1)) > 0
+  -- Disable constant folding; see #27300
+  print $ castFloatToWord32 (noinline $ -1)
+  print $ toInteger (castFloatToWord32 (noinline $ -1)) > 0
   -- For completeness, so should Word64s
   print $ castDoubleToWord64 (-1)
   print $ toInteger (castDoubleToWord64 (-1)) > 0
+  print $ castDoubleToWord64 (noinline $ -1)
+  print $ toInteger (castDoubleToWord64 (noinline $ -1)) > 0
