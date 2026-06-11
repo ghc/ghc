6a01ae129428b3c8969b06b27ee97edde58ec37c
Duncan Coutts <duncan@well-typed.com>
2026-06-11 10:19:40 +0100

    win32 ticker: provide guarantee about concurrency and idempotency
    Use a lock to ensure pause/unpause can be used concurrently. Use a
    paused variable, protected by the lock, to ensure that pause and unpause
    are both idempotent. This is what the portable API expects.

--- rts/win32/Ticker.c
+++ rts/win32/Ticker.c
@@ -11,7 +11,11 @@
 
 static TickProc tick_proc = NULL;
 static HANDLE timer_queue = NULL;
+
+static Mutex lock; // To protect the timer and paused var below
 static HANDLE timer       = NULL;
+static bool paused;
+
 static Time tick_interval = 0;
 
 static VOID CALLBACK tick_callback(
@@ -43,9 +47,12 @@ static void stopTicker(bool synchronous);
 void
 initTicker (Time interval, TickProc handle_tick)
 {
+    ASSERT(timer_queue == NULL);
     tick_interval = interval;
     tick_proc = handle_tick;
 
+    OS_INIT_LOCK(&lock);
+    paused = true; // starts paused
     timer_queue = CreateTimerQueue();
     if (timer_queue == NULL) {
         sysErrorBelch("CreateTimerQueue");
@@ -53,36 +60,51 @@ initTicker (Time interval, TickProc handle_tick)
     }
 }
 
+// Asynchronous. Idempotent.
 void
 unpauseTicker(void)
 {
-    startTicker();
+    OS_ACQUIRE_LOCK(&lock);
+    if (paused) {
+        startTicker();
+    }
+    paused = false;
+    OS_RELEASE_LOCK(&lock);
 }
 
+// Asynchronous. Idempotent.
 void
 pauseTicker(void)
 {
-    if (timer_queue != NULL && timer != NULL) {
+    OS_ACQUIRE_LOCK(&lock);
+    if (!paused) {
         /* pauseTicker is called from within the handle_tick, so stopping
          * the ticker here /must/ be asynchronous or we will deadlock! */
         stopTicker(false /* asynchronous */);
     }
+    paused = true;
+    OS_RELEASE_LOCK(&lock);
 }
 
 void
 exitTicker(void)
 {
-    if (timer != NULL) {
+    ASSERT(timer_queue != NULL);
+
+    OS_ACQUIRE_LOCK(&lock);
+    if (!paused) {
         stopTicker(true /* synchronous */);
     }
-    if (timer_queue != NULL) {
-        // From the docs for DeleteTimerQueueEx:
-        //   If this parameter is INVALID_HANDLE_VALUE, the function waits
-        //   for all callback functions to complete before returning.
-        HANDLE completion = INVALID_HANDLE_VALUE;
-        DeleteTimerQueueEx(timer_queue, completion);
-        timer_queue = NULL;
-    }
+    OS_RELEASE_LOCK(&lock);
+
+    // From the docs for DeleteTimerQueueEx:
+    //   If this parameter is INVALID_HANDLE_VALUE, the function waits
+    //   for all callback functions to complete before returning.
+    // This is a belt-and-braces approach to ensuring exitTicker is synchronous,
+    // since stopTicker(true) is already synchronous and there's only one timer.
+    HANDLE completion = INVALID_HANDLE_VALUE;
+    DeleteTimerQueueEx(timer_queue, completion);
+    timer_queue = NULL;
 }
 
 static void startTicker(void) {
