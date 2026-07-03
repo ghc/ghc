030daa65ed55870d58203345451066feb0fded90
Andreas Klebinger <klebinger.andreas@gmx.at>
2026-07-03 10:47:32 +0200

    Add test for #27430.

---
+++ testsuite/tests/codeGen/should_run/T27430_c.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+uint8_t u64_to_u8(uint64_t v) { return (uint8_t)v; }
+uint8_t u64_to_u16(uint64_t v) { return (uint16_t)v; }
+uint8_t u64_to_u32(uint64_t v) { return (uint32_t)v; }
