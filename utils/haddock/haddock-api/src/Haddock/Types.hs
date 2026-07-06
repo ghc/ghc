2ca87972f6dbcf9440eca80c46f19b71e6b401ca
Alan Zimmerman <alan.zimm@gmail.com>
2026-07-06 22:03:24 +0100

    EPA: Remove LocatedBC / SrcSpanBF
    The custom annotations are now in the BooleanFormula TTG extension
    points, so LBooleanFormula can now use the standard LocatedA.

--- utils/haddock/haddock-api/src/Haddock/Types.hs
+++ utils/haddock/haddock-api/src/Haddock/Types.hs
@@ -834,7 +834,7 @@ type instance Anno (HsDecl DocNameI) = SrcSpanAnnA
 type instance Anno (FamilyResultSig DocNameI) = EpAnn NoEpAnns
 type instance Anno (HsOuterTyVarBndrs Specificity DocNameI) = SrcSpanAnnA
 type instance Anno (HsSigType DocNameI) = SrcSpanAnnA
-type instance Anno (BooleanFormula DocNameI) = SrcSpanAnnBF
+type instance Anno (BooleanFormula DocNameI) = SrcSpanAnnA
 type instance Anno (OverlapMode DocNameI) = SrcSpanAnnA
 type instance Anno (CType DocNameI) = SrcSpanAnnA
 type instance Anno (Header DocNameI) = SrcSpanAnnA
@@ -1041,7 +1041,7 @@ type instance XXHsContextDetails DocNameI = DataConCantHappen
 type instance XBFVar           DocNameI = NoExtField
 type instance XBFAnd           DocNameI = NoExtField
 type instance XBFOr            DocNameI = NoExtField
-type instance XBFParens        DocNameI = NoExtField
+type instance XBFParens        DocNameI = (EpToken "(", EpToken ")")
 type instance XXBooleanFormula DocNameI = DataConCantHappen
 
 -----------------------------------------------------------------------------
