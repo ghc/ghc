6bb34033f258388b732b22572dfda00aba4a4f14
Duncan Coutts <duncan@well-typed.com>
2026-07-07 11:45:19 +0100

    In releaseCapability_ prioritise pending sync over returning tasks
    Fixes issue #27460
    
    As explained in the issue, a pending sync (e.g. for GC) should be dealt
    with promptly. Returning tasks are a lower priority.
    
    Historically however we had to check returning tasks first, because the
    synchronisation mechanism mixed up the task doing a sync with the tasks
    returning from safe FFI calls. The task performing the sync was very
    likely to be queued on the returning task list (and historically it was
    at the _end_ of this list!).
    
    We have now arranged that the task performing the sync is at the front
    of the returning task list, and in the pending sync case we now check
    the returning task list and run the first task from there if it there is
    one.
    
    This is by no means perfect, but it is better. See issue #27473 for a
    more general issue of cleaning up the design of the pending sync.

--- rts/Capability.c
+++ rts/Capability.c
@@ -609,14 +609,6 @@ releaseCapability_ (Capability* cap,
     // Remove the current Task owning the Capability (if any, see purpose 2).
     RELAXED_STORE(&cap->running_task, NULL);
 
-    // Check to see whether a worker thread can be given
-    // the go-ahead to return the result of an external call..
-    if (cap->n_returning_tasks != 0) {
-        giveCapabilityToTask(cap,cap->returning_tasks_hd);
-        // The Task pops itself from the queue (see waitForCapability())
-        return;
-    }
-
     // If there is a pending sync, the task that requested the sync will
     // subsequently use acquireAllCapabilities to place itself on the (front of
     // the) returning_task list (of all capabilities). We will then be in one
@@ -661,6 +653,14 @@ releaseCapability_ (Capability* cap,
         } else {
             debugTrace(DEBUG_sched, "sync pending, freeing capability %d", cap->no);
         }
+        return;
+    }
+
+    // Check to see whether a worker thread can be given
+    // the go-ahead to return the result of an external call..
+    if (cap->n_returning_tasks != 0) {
+        giveCapabilityToTask(cap,cap->returning_tasks_hd);
+        // The Task pops itself from the queue (see waitForCapability())
         return;
     }
 
