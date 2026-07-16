085b758cb21857daecc733a487b97d39f939f551
Simon Peyton Jones <simon.peytonjones@gmail.com>
2026-07-16 14:45:25 +0100

    Wibble

--- compiler/GHC/Core/Ppr.hs
+++ compiler/GHC/Core/Ppr.hs
@@ -776,7 +776,7 @@ instance Outputable UnfoldingCache where
     ppr (UnfoldingCache { uf_is_value = hnf, uf_is_conlike = conlike
                         , uf_is_work_free = wf, uf_expandable = exp
                         , uf_is_vlam = vl })
-        = fsep $ punctuate comma
+        = hsep $ punctuate comma
           [ text "Value="      <> ppr hnf
           , text "ConLike="    <> ppr conlike
           , text "WorkFree="   <> ppr wf
