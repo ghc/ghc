a358c3f8c8458b18eb31bd161499f0106239f4d3
Simon Jakobi <simon.jakobi@gmail.com>
2026-07-16 13:59:51 +0200

    testsuite: Add simpler T27507 reproducer

---
+++ testsuite/tests/simplStg/should_run/T27507a.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE BangPatterns #-}
+-- Ensure the Box is returned boxed rather than unwrapped by CPR.
+-- Ensure fun_exit is not floated to top, but works either way.
+{-# OPTIONS_GHC -fno-cpr-anal -fno-full-laziness #-}
+
+module Main where
+
+import GHC.Exts (noinline)
+import GHC.Exts.Heap (getClosureData, info, tipe)
+
+data Box a = Box !a
+
+foo :: Bool -> Int -> (Int, Box Int)
+foo b !x = case x of
+  x' -> let -- Keep fun_exit monomorphic so that it can become a join point.
+            -- Without this signature its polymorphic result type prevents contification.
+            fun_exit :: Int -> Int -> (Int, Box Int)
+            fun_exit x_f i = (i, Box x_f)
+            {-# NOINLINE fun_exit #-}
+        in if b then fun_exit x' 0 else fun_exit x' 1
+{-# NOINLINE foo #-}
+
+main :: IO ()
+main =
+  case foo True 42 of
+    (_, bx) -> do
+      c <- noinline getClosureData bx
+      noinline print (tipe (info c))
