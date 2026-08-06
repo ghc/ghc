8b9880c7db741df5573d3b6975db049bcd35a5ce
Luite Stegeman <stegeman@gmail.com>
2026-08-06 09:25:30 +0200

    rts: make stg_threadLabelzh return a valid pointer for unlabeled threads.
    This fixes a segfault in the GC caused by stg_threadLabelzh returning
    a 0 pointer in a GC pointer field.
    
    stg_threadLabelzh returns a tuple of type (# Int#, ByteArray# #). If a
    thread has no label, the second field is unused. We must still return
    a valid heap object pointer.
    
    Instead of returning 0, we now return stg_DEAD_SLOT_closure.
    
    fixes #27618

--- rts/include/stg/MiscClosures.h
+++ rts/include/stg/MiscClosures.h
@@ -200,6 +200,7 @@ RTS_ENTRY(stg_SRT_16);
 
 RTS_CLOSURE(stg_END_TSO_QUEUE_closure);
 RTS_CLOSURE(stg_NO_FINALIZER_closure);
+RTS_CLOSURE(stg_DEAD_SLOT_closure);
 RTS_CLOSURE(stg_dummy_ret_closure);
 RTS_CLOSURE(stg_forceIO_closure);
 RTS_CLOSURE(stg_CLOSURE_TABLE_NULL_closure);
@@ -211,6 +212,7 @@ RTS_CLOSURE(stg_END_STM_CHUNK_LIST_closure);
 RTS_CLOSURE(stg_NO_TREC_closure);
 
 RTS_ENTRY(stg_NO_FINALIZER);
+RTS_ENTRY(stg_DEAD_SLOT);
 
 #if IN_STG_CODE
 extern StgWordArray stg_CHARLIKE_closure;
