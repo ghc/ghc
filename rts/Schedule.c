26b4579d35065db8d2f288a0fbe0c4a3cea431af
Luite Stegeman <stegeman@gmail.com>
2026-08-06 23:41:33 +0200

    rts: fix ctoi_tuple_spill_words getting out of sync
    Fix a few places that were not updating ctoi_tuple_spill_words
    correctly, leading to corruption/crashes when dealing with large
    unboxed tuples in bytecode:
    
     - captureContinuationAndAbort
     - findRetryFrameHelper/findAtomicallyFrameHelper
     - interpretBCO bci_BRK_FUN
    
    fixes #27633
    
    (cherry picked from commit 192be0b6b861725613a9be5cb1587fb8cdf68bd3)

--- rts/Schedule.c
+++ rts/Schedule.c
@@ -3252,6 +3252,10 @@ findRetryFrameHelper (Capability *cap, StgTSO *tso)
     default:
       ASSERT(info->i.type != CATCH_FRAME);
       ASSERT(info->i.type != STOP_FRAME);
+      // see Note [GHCi unboxed tuples stack spills] in StgMiscClosures.cmm
+      if (*p == (StgWord)&stg_ctoi_t_info) {
+          tso->ctoi_tuple_spill_words = p[CTOI_OLD_TUPLE_SPILL_WORDS_OFFSET]; // restore old_spill
+      }
       p = next;
       continue;
     }
@@ -3318,6 +3322,10 @@ findAtomicallyFrameHelper (Capability *cap, StgTSO *tso)
     default:
       ASSERT(info->i.type != CATCH_FRAME);
       ASSERT(info->i.type != STOP_FRAME);
+      // see Note [GHCi unboxed tuples stack spills] in StgMiscClosures.cmm
+      if (*p == (StgWord)&stg_ctoi_t_info) {
+          tso->ctoi_tuple_spill_words = p[CTOI_OLD_TUPLE_SPILL_WORDS_OFFSET]; // restore old_spill
+      }
       p = next;
       continue;
     }
