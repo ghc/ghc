5304a39917c4a465f0ce06805846d22205b81c7a
Wolfgang Jeltsch <wolfgang@well-typed.com>
2026-08-10 14:49:02 +0300

    Make `${project_root}` available to all code

--- .gitlab/base-ci.sh
+++ .gitlab/base-ci.sh
@@ -31,34 +31,31 @@ mkdir other-ghcs/opt
 project_root=$PWD
 
 # Create Cabal file for `base`
-(
-  cd libraries/base
-  sed -E -e 's/^( *ghc-internal)[^[:alnum:]-].*(,|$)/\1\2/' \
-    < base.cabal.in \
-    > base.cabal
-)
+cd libraries/base
+sed -E -e 's/^( *ghc-internal)[^[:alnum:]-].*(,|$)/\1\2/' \
+  < base.cabal.in \
+  > base.cabal
+cd ${project_root}
 
 # Build `base` with the different GHCs
 for ghc_version in ${ghc_versions}
 do
   # Install the GHC
-  (
-    cd other-ghcs/src
-    archive_file=ghc-${ghc_version}-${platform}.tar.xz
-    curl https://downloads.haskell.org/~ghc/${ghc_version}/${archive_file} \
-      >${ghc_version}.tar.xz
-    tar -xJf ${ghc_version}.tar.xz
-    cd ghc-${ghc_version}-*
-    ./configure --prefix "${project_root}/opt/${ghc_version}"
-    make install
-  )
+  cd other-ghcs/src
+  archive_file=ghc-${ghc_version}-${platform}.tar.xz
+  curl https://downloads.haskell.org/~ghc/${ghc_version}/${archive_file} \
+    >${ghc_version}.tar.xz
+  tar -xJf ${ghc_version}.tar.xz
+  cd ghc-${ghc_version}-*
+  ./configure --prefix "${project_root}/opt/${ghc_version}"
+  make install
+  cd ${project_root}
 
   # Build `base` with the installed GHC
-  (
-    cd libraries/base
-    cabal build
-      --with-compiler "${project_root}/other-ghcs/opt/${ghc_version}/bin/ghc" \
-      --allow-boot-library-installs \
-      -O0
-  )
+  cd libraries/base
+  cabal build
+    --with-compiler "${project_root}/other-ghcs/opt/${ghc_version}/bin/ghc" \
+    --allow-boot-library-installs \
+    -O0
+  cd ${project_root}
 done
