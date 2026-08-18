a99812b5f270579754cb13984249ca520db3c4d5
Zubin Duggal <zubin.duggal@gmail.com>
2026-08-18 14:50:34 +0530

    An abstract TyCon may hide a unary class
    A class declared in an hs-boot file is an AbstractTyCon inside the
    module loop, and compiling the real declaration may reveal it to be a
    UnaryClassTyCon.
    
    - isTerminatingType returned True for such AbstractTyCons
    
    - IfaceToCore set the unary flag to False in the DFunId
    
    So we could end up speculating bottom dictionaries because inside a module
    loop we see an UnaryClassTyCon as an AbstractTyCon
    
    Use isTerminatingTyCon, which returns False for an abstract TyCon.
    
    The Bool in DFunId is now a cache for isTerminatingTyCon, set in
    mkDFunIdDetails.
    
    Fixes #27704
    
    (cherry picked from commit cd5c6bcc0a59c7b4dc4627fdc7471dc7938d07c2)

--- compiler/GHC/Core/Type.hs
+++ compiler/GHC/Core/Type.hs
@@ -2502,9 +2502,7 @@ isTerminatingType :: HasDebugCallStack => Type -> Bool
 -- NB: unlifted types are not terminating types!
 --     e.g. you can write a term (loop 1)::Int# that diverges.
 isTerminatingType ty = case tyConAppTyCon_maybe ty of
-    Just tc -> isClassTyCon tc && not (isUnaryClassTyCon tc)
-               -- A non-unary class TyCon is terminating
-               -- See (UCM3) in Note [Unary class magic] in GHC.Core.TyCon
+    Just tc -> isTerminatingTyCon tc
     _       -> False
 
 isPrimitiveType :: Type -> Bool
