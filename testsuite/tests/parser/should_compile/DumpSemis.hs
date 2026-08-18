d38e771de7f515707c5095aef8a4e7fd28963463
Alan Zimmerman <alan.zimm@gmail.com>
2026-08-18 21:31:18 +0100

    EPA some tests for layout. WIP

--- testsuite/tests/parser/should_compile/DumpSemis.hs
+++ testsuite/tests/parser/should_compile/DumpSemis.hs
@@ -43,3 +43,12 @@ fot x =
           ;;;   -- case 4
    }
 ;
+fon x =
+  case x of
+     ;;; -- leading
+     0 -> 'a';  -- case 0
+     1 -> 'b'   -- case 1
+     2 -> 'c' ; -- case 2
+     3 -> 'd'   -- case 3
+          ;;;   -- case 4
+;
