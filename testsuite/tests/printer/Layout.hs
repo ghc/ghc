739934cb267eb6dfff51bd5a64b1d4e25fbc1fb4
Alan Zimmerman <alan.zimm@gmail.com>
2026-08-18 21:31:18 +0100

    EPA some tests for layout. WIP

---
+++ testsuite/tests/printer/Layout.hs
@@ -0,0 +1,17 @@
+module Layout where
+
+-- do, mdo and rec should all open layouts
+
+f :: IO ()
+f = do print 'a'
+       print 'b'
+
+g :: IO ()
+g = mdo print 'a'
+        print 'b'
+
+h :: IO ()
+h = do print 'a'
+       rec print 'b'
+           print 'c'
+       print 'd'
