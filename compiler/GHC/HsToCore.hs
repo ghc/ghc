3a176ec9116a141d9b2f64b55e8fa712b7e6ab4a
Florian Ragwitz <florian.ragwitz@gmail.com>
2026-09-03 19:36:04 -0700

    hpc: Provide coverage information for class instances
    After fixing #17155, we no longer create top-level boxes for the source spans
    of instance heads (previously that was the source span used for
    inherited/generated instance methods).
    
    With those boxes gone, we can introduce a new top-level box for each class
    instance with that instance head source span, and provide more meaningful
    coverage semantics for it.
    
    We make the use of any of the instance's methods (user-written or generated)
    tick this box, providing the programmer with useful information about whether
    or not the instance was exercised at run-time, and whether the instance might
    be unnecessary.

--- compiler/GHC/HsToCore.hs
+++ compiler/GHC/HsToCore.hs
@@ -171,7 +171,8 @@ deSugar hsc_env
                                        (hsc_logger hsc_env)
                                        (initTicksConfig (hsc_dflags hsc_env))
                                        mod mod_loc
-                                       export_set (typeEnvTyCons type_env) inst_meths binds
+                                       export_set (typeEnvTyCons type_env)
+                                       insts inst_meths binds
                               else return (binds, Nothing)
         ; let modBreaks
                 | Just (_, _, breakpointSpecs) <- m_tickInfo
