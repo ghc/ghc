ghc/ghc

commit 5c2a179efea36296c35f2c273eb614e3efd89f78

Allocate static constructors for bytecode
This commit adds support for static constructors when compiling and
linking ByteCode objects.

Top-level StgRhsCon get lowered to ProtoStaticCons rather than to
ProtoBCOs. A ProtoStaticCon gets allocated directly as a data con
application on the heap (using the new primop newConApp#).

Previously, we would allocate a ProtoBCO which, when evaluated, would
PACK and return the constructor.
A few more details are given in Note [Static constructors in Bytecode].

Secondly, this commit also fixes issue #25636 which was caused by
linking *unlifted* constructors in BCO instructions as
    - (1) a thunk indexing the array of BCOs in a module
    - (2) which evaluated to a BCO which still had to be evaluated to
      return the unlifted constructor proper.

The (2) issue has been resolved by allocating the static constructors
directly. The (1) issue can be resolved by ensuring that we allocate all
unlifted top-level constructors eagerly, and leave the knot-tying for
the lifted BCOs and top-level constructors only.

The top-level unlifted constructors are never mutually recursive, so we
can allocate them all in one go as long as we do it in topological
order. Lifted fields of unlifted constructors can still be filled by the
knot-tied lifted variables since in those fields it is fine to keep
those thunks. See Note [Tying the knot in createBCOs] for more details.

Fixes #25636

-------------------------
Metric Decrease:
    LinkableUsage01
-------------------------

rts/PrimOps.cmm
@@ -2129,6 +2129,44 @@ for:
     return (bco);
 }
 
+// Ptr InfoTable, [Literals] [Ptrs] ==> CONSTR heap closure
+stg_newConAppObjzh ( W_ datacon_info, P_ literals, P_ ptrs , W_ arity )
+{
+    W_ con_obj, bytes;
+
+    bytes = SIZEOF_StgHeader + WDS(arity);
+
+    ALLOC_PRIM (bytes);
+    con_obj = Hp - bytes + WDS(1);
+
+    // No memory barrier necessary as this is a new allocation.
+    SET_HDR(con_obj, datacon_info, CCS_MAIN);
+
+    // Copy the ptrs followed by nonptrs into the constructor payload
+    W_ i, n_ptrs;
+    n_ptrs = StgMutArrPtrs_ptrs(ptrs);
+    i = 0;
+loop1:
+    if (i < n_ptrs) {
+        StgClosure_payload(con_obj,i) = StgMutArrPtrs_payload(ptrs,i);
+        i = i + 1;
+        goto loop1;
+    }
+    i = 0;
+loop2:
+    if (i < BYTE_ARR_WDS(literals)) {
+        W_ offset;
+        offset = n_ptrs + i;
+        StgClosure_payload(con_obj,offset) = StgArrBytes_payload(literals,i);
+        i = i + 1;
+        goto loop2;
+    }
+
+    W_ tagged_con_obj;
+    (tagged_con_obj) = ccall tagConstr(con_obj);
+    return (tagged_con_obj);
+}
+
 stg_mkApUpd0zh ( P_ bco )
 {
     W_ ap;

compiler/GHC/Builtin/primops.txt.pp
@@ -3942,6 +3942,18 @@ primop  NewBCOOp "newBCO#" GenPrimOp
    effect = ReadWriteEffect
    out_of_line      = True
 
+primop  NewConAppObjOp "newConAppObj#" GenPrimOp
+   Addr# -> ByteArray# -> Array# a_levpoly -> Word# -> State# s -> (# State# s, b_levpoly #)
+   { @'newConAppObj#' datacon_itbl lits ptrs arity@ creates a new constructor
+     application object on the heap from the info table pointer of the data
+     constructor and the data arguments given in @ptrs@ and @lits@. The
+     resulting object is a heap closure for the constructor application. It is
+     evaluated and properly tagged. The given @arity@ gives the total size of
+     pointers and literals in number of words. }
+   with
+   effect = ReadWriteEffect
+   out_of_line      = True
+
 primop  UnpackClosureOp "unpackClosure#" GenPrimOp
    a -> (# Addr#, ByteArray#, Array# b #)
    { @'unpackClosure#' closure@ copies the closure and pointers in the

compiler/GHC/ByteCode/Binary.hs
@@ -156,22 +156,39 @@ instance Binary CompiledByteCode where
     put_ bh bc_spt_entries
 
 instance Binary UnlinkedBCO where
-  get bh =
-    UnlinkedBCO
-      <$> getViaBinName bh
-      <*> get bh
-      <*> get bh
-      <*> get bh
-      <*> get bh
-      <*> get bh
+  get bh = do
+    t <- getByte bh
+    case t of
+      0 -> UnlinkedBCO
+        <$> getViaBinName bh
+        <*> get bh
+        <*> get bh
+        <*> get bh
+        <*> get bh
+        <*> get bh
+      1 -> UnlinkedStaticCon
+        <$> getViaBinName bh
+        <*> getViaBinName bh
+        <*> get bh
+        <*> get bh
+        <*> get bh
+      _ -> panic "Binary UnlinkedBCO: invalid byte"
 
   put_ bh UnlinkedBCO {..} = do
+    putByte bh 0
     putViaBinName bh unlinkedBCOName
     put_ bh unlinkedBCOArity
     put_ bh unlinkedBCOInstrs
     put_ bh unlinkedBCOBitmap
     put_ bh unlinkedBCOLits
     put_ bh unlinkedBCOPtrs
+  put_ bh UnlinkedStaticCon {..} = do
+    putByte bh 1
+    putViaBinName bh unlinkedStaticConName
+    putViaBinName bh unlinkedStaticConDataConName
+    put_ bh unlinkedStaticConLits
+    put_ bh unlinkedStaticConPtrs
+    put_ bh unlinkedStaticConIsUnlifted
 
 -- Also see Note [BCOByteArray serialization]. This instance is unlike
 -- the `Binary` instances in `ghci`, which are for the `Binary` class

compiler/GHC/ByteCode/Instr.hs
@@ -16,6 +16,7 @@ import GHC.Cmm.Type (Width)
 import GHC.StgToCmm.Layout     ( ArgRep(..) )
 import GHC.Utils.Outputable
 import GHC.Types.Name
+import GHC.Types.Id
 import GHC.Types.Literal
 import GHC.Types.Unique
 import GHC.Core.DataCon
@@ -43,9 +44,71 @@ data ProtoBCO
         protoBCOBitmap     :: [StgWord],
         protoBCOBitmapSize :: Word,
         protoBCOArity      :: Int,
-        -- what the BCO came from, for debugging only
+        -- | What the BCO came from, for debugging only
         protoBCOExpr       :: Either [CgStgAlt] CgStgRhs
    }
+   -- | A top-level static constructor application object
+   -- See Note [Static constructors in Bytecode]
+   | ProtoStaticCon {
+        protoStaticConName :: Name,
+        -- ^ The name to which this static constructor is bound,
+        -- not to be confused with the DataCon itself.
+        protoStaticCon     :: DataCon,
+        -- ^ The DataCon being constructed.
+        -- We use this to construct the right info table.
+        protoStaticConData :: [Either Literal Id],
+        -- ^ The static constructor pointer and non-pointer arguments, sorted
+        -- in the order they should appear at runtime (see
+        -- 'mkVirtHeapOffsetsWithPadding' in 'schemeTopBind').
+        --
+        -- The non-pointer arguments are meant to be laid contiguously in
+        -- memory using the width of each literal individually. The padding is
+        -- given as Literals of value 0 with the appropriate width.
+        protoStaticConNonPtrsSize :: Int,
+        -- ^ How many words needed to store the non-pointer arguments.
+        -- Note that this may be smaller than the number of non-pointer
+        -- arguments, since subword arguments need to be packed.
+        protoStaticConExpr :: CgStgRhs
+        -- ^ What the static con came from, for debugging only
+   }
+
+{-
+Note [Static constructors in Bytecode]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+In bytecode, top-level 'StgRhsCon's are lowered to 'ProtoStaticCon' rather than
+'ProtoBCO'. A 'ProtoStaticCon' represents directly a heap allocated data
+constructor application. We can do this only for top-level 'StgRhsCon's, where
+all the data arguments to the constructor are statically known.
+
+'StgRhsCon's which have free variables are compiled down to BCOs which push the
+arguments and then 'PACK' the constructor, just like 'StgConApp's.
+
+Example:
+
+  Haskell:
+
+    data X = X Char# Char
+    x = X 'a'# 'b'
+
+  Stg:
+
+    x1  = GHC.Types.C#! ['b'#];
+    X.x = X.X! ['a#' x2];
+
+    X.X = \r [arg1 arg2] X.X [arg1 arg2];
+
+  ByteCode:
+    ProtoStaticCon x1:
+      C# [Left 'b'#]
+    ProtoStaticCon X.x:
+      X.X [Left 'a'#, Right x1]
+
+    ProtoBCO X.X:
+     PUSH_LL  0 1
+     PACK     X.X 2
+     SLIDE    1 2
+     RETURN   P
+-}
 
 -- | A local block label (e.g. identifying a case alternative).
 newtype LocalLabel = LocalLabel { getLocalLabel :: Word32 }
@@ -278,6 +341,12 @@ data BCInstr
 -- Printing bytecode instructions
 
 instance Outputable ProtoBCO where
+   ppr (ProtoStaticCon nm con args nonPtrsSize origin)
+      = text "ProtoStaticCon" <+> ppr nm <> colon
+        $$ nest 3 (pprStgRhsShort shortStgPprOpts origin)
+        $$ nest 3 (text "constructor: "  <+> ppr con)
+        $$ nest 3 (text "sorted args: "  <+> ppr args)
+        $$ nest 3 (text "non-ptrs (packed) size: " <+> int (fromIntegral nonPtrsSize) <+> text "words")
    ppr (ProtoBCO { protoBCOName       = name
                  , protoBCOInstrs     = instrs
                  , protoBCOBitmap     = bitmap
@@ -469,7 +538,8 @@ instance Outputable BCInstr where
 -- stack high water mark, but it doesn't seem worth the hassle.
 
 protoBCOStackUse :: ProtoBCO -> Word
-protoBCOStackUse bco = sum (map bciStackUse (protoBCOInstrs bco))
+protoBCOStackUse ProtoBCO{protoBCOInstrs} = sum (map bciStackUse protoBCOInstrs)
+protoBCOStackUse ProtoStaticCon{} = 0
 
 bciStackUse :: BCInstr -> Word
 bciStackUse STKCHECK{}            = 0

compiler/GHC/ByteCode/Linker.hs
@@ -12,6 +12,7 @@ module GHC.ByteCode.Linker
   , lookupStaticPtr
   , lookupIE
   , linkFail
+  , BCOIx(..)
   )
 where
 
@@ -50,34 +51,71 @@ import Data.Functor
 import Foreign.Ptr
 import GHC.Exts
 
-{-
+{- |
   Linking interpretables into something we can run
 -}
-
 linkBCO
   :: Interp
   -> PkgsLoaded
   -> BytecodeLoaderState
-  -> NameEnv Int
+  -> NameEnv BCOIx
+  -- ^ A mapping from names to references to other BCOs
+  --   or static constructors in this group.
   -> UnlinkedBCO
   -> IO ResolvedBCO
-linkBCO interp pkgs_loaded bytecode_state bco_ix
-           (UnlinkedBCO _ arity insns bitmap lits0 ptrs0) = do
-  -- fromIntegral Word -> Word64 should be a no op if Word is Word64
-  -- otherwise it will result in a cast to longlong on 32bit systems.
-  litsMut <- unsafeNewArray_ (0, fromIntegral (sizeFlatBag lits0) - 1)
-  foldM_ (\(!i) lit -> (unsafeWrite litsMut i =<< lookupLiteral interp pkgs_loaded bytecode_state lit) $> succ i) 0 lits0
-  lits <- unsafeFreezeIOUArray litsMut
-  ptrsMut <- newSmallArrayIO (fromIntegral (sizeFlatBag ptrs0)) undefined
-  foldM_ (\(!i) ptr -> (writeSmallArrayIO ptrsMut i =<< resolvePtr interp pkgs_loaded bytecode_state bco_ix ptr) $> succ i) 0 ptrs0
-  ptrs <- unsafeFreezeSmallArrayIO ptrsMut
-  return $ ResolvedBCO { resolvedBCOIsLE   = isLittleEndian
-                       , resolvedBCOArity  = arity
-                       , resolvedBCOInstrs = insns
-                       , resolvedBCOBitmap = bitmap
-                       , resolvedBCOLits   = mkBCOByteArray lits
-                       , resolvedBCOPtrs   = ptrs
-                       }
+linkBCO interp pkgs_loaded bytecode_state bco_ix unl_bco = do
+  case unl_bco of
+    UnlinkedBCO _ arity insns
+           bitmap lits0 ptrs0 -> do
+        lits <- doLits lits0
+        ptrs <- doPtrs ptrs0
+        return ResolvedBCO
+          { resolvedBCOIsLE   = isLittleEndian
+          , resolvedBCOArity  = arity
+          , resolvedBCOInstrs = insns
+          , resolvedBCOBitmap = bitmap
+          , resolvedBCOLits   = lits
+          , resolvedBCOPtrs   = ptrs
+          }
+
+    UnlinkedStaticCon
+      { unlinkedStaticConLits = lits0
+      , unlinkedStaticConPtrs = ptrs0
+      , unlinkedStaticConDataConName
+      , unlinkedStaticConIsUnlifted
+      } -> do
+        Ptr itbl_ptr# <- lookupIE interp pkgs_loaded bytecode_state unlinkedStaticConDataConName
+        lits <- doLits lits0
+        ptrs <- doPtrs ptrs0
+        return ResolvedStaticCon
+          { resolvedBCOIsLE = isLittleEndian
+          , resolvedStaticConInfoPtr = W# (int2Word# (addr2Int# itbl_ptr#))
+          , resolvedStaticConArity = sizeFlatBag lits0 + sizeFlatBag ptrs0
+          , resolvedStaticConLits = lits
+          , resolvedStaticConPtrs = ptrs
+          , resolvedStaticConIsUnlifted = unlinkedStaticConIsUnlifted
+          }
+  where
+    doLits lits0 = do
+      litsMut <- unsafeNewArray_ (0, fromIntegral (sizeFlatBag lits0) - 1)
+      foldM_ (\(!i) lit -> (unsafeWrite litsMut i =<< lookupLiteral interp pkgs_loaded bytecode_state lit) $> succ i) 0 lits0
+      lits <- unsafeFreezeIOUArray litsMut
+      return $ mkBCOByteArray lits
+    doPtrs ptrs0 = do
+      ptrsMut <- newSmallArrayIO (fromIntegral (sizeFlatBag ptrs0)) undefined
+      foldM_ (\(!i) ptr -> (writeSmallArrayIO ptrsMut i =<< resolvePtr interp pkgs_loaded bytecode_state bco_ix ptr) $> succ i) 0 ptrs0
+      unsafeFreezeSmallArrayIO ptrsMut
+
+-- | An index into a BCO or Static Constructor in this group.
+--
+-- We distinguish between lifted and unlifted static constructors because
+-- lifted ones get resolved by tying a knot, since there may be circular
+-- dependencies between them, whereas unlifted ones get constructed in a first
+-- pass.
+data BCOIx = BCOIx !Int
+           | LiftedStaticConIx !Int
+           | UnliftedStaticConIx !Int
+  deriving (Eq, Ord, Show)
 
 lookupLiteral :: Interp -> PkgsLoaded -> BytecodeLoaderState -> BCONPtr -> IO Word
 lookupLiteral interp pkgs_loaded bytecode_state ptr = case ptr of
@@ -164,13 +202,16 @@ resolvePtr
   :: Interp
   -> PkgsLoaded
   -> BytecodeLoaderState
-  -> NameEnv Int
+  -> NameEnv BCOIx
   -> BCOPtr
   -> IO ResolvedBCOPtr
 resolvePtr interp pkgs_loaded bco_loader_state bco_ix ptr = case ptr of
   BCOPtrName nm
-    | Just ix <- lookupNameEnv bco_ix nm
-    -> return (ResolvedBCORef ix) -- ref to another BCO in this group
+    | Just bix <- lookupNameEnv bco_ix nm
+    -> return $ case bix of
+        BCOIx ix               -> ResolvedBCORef ix
+        LiftedStaticConIx ix   -> ResolvedStaticConRef ix
+        UnliftedStaticConIx ix -> ResolvedUnliftedStaticConRef ix
 
     | Just (_, rhv) <- lookupNameBytecodeState bco_loader_state nm
     -> return (ResolvedBCOPtr (unsafeForeignRefToRemoteRef rhv))

compiler/GHC/ByteCode/Types.hs
@@ -32,6 +32,7 @@ module GHC.ByteCode.Types
   ) where
 
 import GHC.Prelude
+import qualified Data.ByteString.Char8 as BS8
 
 import GHC.Data.FastString
 import GHC.Data.FlatBag
@@ -257,16 +258,33 @@ data UnlinkedBCO
    = UnlinkedBCO {
         unlinkedBCOName   :: !Name,
         unlinkedBCOArity  :: {-# UNPACK #-} !Int,
-        unlinkedBCOInstrs :: !(BCOByteArray Word16),      -- insns
-        unlinkedBCOBitmap :: !(BCOByteArray Word),      -- bitmap
-        unlinkedBCOLits   :: !(FlatBag BCONPtr),       -- non-ptrs
-        unlinkedBCOPtrs   :: !(FlatBag BCOPtr)         -- ptrs
+        unlinkedBCOInstrs :: !(BCOByteArray Word16),  -- insns
+        unlinkedBCOBitmap :: !(BCOByteArray Word),    -- bitmap
+        unlinkedBCOLits   :: !(FlatBag BCONPtr),      -- non-ptrs
+        unlinkedBCOPtrs   :: !(FlatBag BCOPtr)        -- ptrs
+   }
+   -- | An unlinked top-level static constructor
+   -- See Note [Static constructors in Bytecode]
+   | UnlinkedStaticCon {
+        unlinkedStaticConName :: !Name,
+        -- ^ The name to which this static constructor is bound, not to be
+        -- confused with the name of the static constructor itself
+        -- ('unlinkedStaticConDataConName')
+        unlinkedStaticConDataConName :: !Name,
+        unlinkedStaticConLits :: !(FlatBag BCONPtr),
+        -- ^ non-ptrs full words, where sub-word literals have already been
+        -- packed into full words as needed
+        unlinkedStaticConPtrs :: !(FlatBag BCOPtr),  -- ptrs
+        unlinkedStaticConIsUnlifted :: !Bool
    }
 
 instance NFData UnlinkedBCO where
   rnf UnlinkedBCO{..} =
     rnf unlinkedBCOLits `seq`
     rnf unlinkedBCOPtrs
+  rnf UnlinkedStaticCon{..} =
+    rnf unlinkedStaticConLits `seq`
+    rnf unlinkedStaticConPtrs
 
 data BCOPtr
   = BCOPtrName   !Name
@@ -278,6 +296,12 @@ data BCOPtr
 instance NFData BCOPtr where
   rnf (BCOPtrBCO bco) = rnf bco
   rnf x = x `seq` ()
+
+instance Outputable BCOPtr where
+  ppr (BCOPtrName nm)        = text "BCOPtrName" <+> ppr nm
+  ppr (BCOPtrPrimOp op)      = text "BCOPtrPrimOp" <+> ppr op
+  ppr (BCOPtrBCO bco)        = text "BCOPtrBCO" <+> ppr bco
+  ppr (BCOPtrBreakArray mod) = text "<break array for" <+> ppr mod <> char '>'
 
 data BCONPtr
   = BCONPtrWord  {-# UNPACK #-} !Word
@@ -296,6 +320,16 @@ data BCONPtr
   -- | A 'CostCentre' remote pointer array's respective 'BreakpointId'
   | BCONPtrCostCentre !InternalBreakpointId
 
+instance Outputable BCONPtr where
+  ppr (BCONPtrWord w)         = integer (fromIntegral w)
+  ppr (BCONPtrLbl lbl)        = text "<label:" <> ftext lbl <> char '>'
+  ppr (BCONPtrItbl nm)        = text "<itbl:" <+> ppr nm <> char '>'
+  ppr (BCONPtrAddr nm)        = text "<addr:" <+> ppr nm <> char '>'
+  ppr (BCONPtrStr bs)         = text "<string literal: " <+> text (BS8.unpack bs) <> char '>'
+  ppr (BCONPtrFS fs)          = text "<fast string literal:" <+> ftext fs <> char '>'
+  ppr (BCONPtrFFIInfo _)      = text "<FFIInfo>"
+  ppr (BCONPtrCostCentre bid) = text "<CostCentre for BreakpointId:" <+> ppr bid <> char '>'
+
 instance NFData BCONPtr where
   rnf x = x `seq` ()
 
@@ -303,6 +337,12 @@ instance Outputable UnlinkedBCO where
    ppr (UnlinkedBCO nm _arity _insns _bitmap lits ptrs)
       = sep [text "BCO", ppr nm, text "with",
              ppr (sizeFlatBag lits), text "lits",
+             ppr (sizeFlatBag ptrs), text "ptrs" ]
+   ppr (UnlinkedStaticCon nm dc_nm lits ptrs unl)
+      = sep [text "StaticCon", ppr nm, text "for",
+             if unl then text "unlifted" else text "lifted",
+             ppr dc_nm, text "with",
+             ppr (sizeFlatBag lits), text "lits", parens (text "(packed) full words"),
              ppr (sizeFlatBag ptrs), text "ptrs" ]
 
 instance Binary FFIInfo where

compiler/GHC/Cmm/Liveness.hs
@@ -63,7 +63,7 @@ cmmGlobalLiveness platform graph =
 
 -- | On entry to the procedure, there had better not be any LocalReg's live-in.
 -- If you see this error it most likely means you are trying to use a variable
--- without it being defined in the given scope.
+-- without it being defined, or initialized, in the given scope.
 noLiveOnEntry :: BlockId -> CmmLive LocalReg -> a -> a
 noLiveOnEntry bid in_fact x =
   if nullRegSet in_fact then x

commit 301e8d21d4892c0730d103a8bdbc80fa53890d67

Interpreter: size interpreted constructors from the info table
An interpreted constructor with no fields still needs a payload word: the
copying collector overwrites an evacuated object with a forwarding pointer,
which is the info pointer plus one word naming the destination, so every heap
closure must have room for MIN_PAYLOAD_SIZE words. Two places got this wrong.

GHCi.InfoTable.mkConInfoTable stored the caller's ptr/non-ptr counts verbatim,
so a caller asking for a nullary constructor got an info table that declares a
one-word closure. GHC.ByteCode.InfoTable pads before calling it, so the
compiler itself was safe, but a direct caller of the ghci library was not:
testsuite/tests/ghci/should_run/T25636f, which builds constructors by hand
through createBCOs, aborts under the debug RTS with

  ASSERTION FAILED: file rts/sm/Sanity.c, line 605

that is, checkHeapChain's minimum-size assertion. Pad in mkConInfoTable, the
one place that builds such an info table, and record why in a Note. The
padding in GHC.ByteCode.InfoTable becomes redundant but stays correct, since
padding an already padded payload is the identity.

stg_newConAppObjzh sized the allocation from the constructor's arity, which
counts fields, rather than from the info table, which counts payload words
including the padding. For a nullary constructor it therefore allocated one
word for a closure the collector copies two words of, overwriting the word
after it when it writes the forwarding pointer. Size from the info table, as
bci_PACK already does. This is reached by ordinary interpreted code: the
worker of a nullary data constructor is compiled to a ProtoStaticCon of arity
zero, and its info table is padded, so the mismatch is there whenever such a
module is linked into the interpreter.

Noticed on the way, and left alone as out of scope: the Cmm sizeW_fromITBL in
rts/include/Cmm.h does not widen the 32-bit layout fields, so a use of it
fails Cmm lint with "I32[...] * 8 ... op is expecting: [W32, W32]". Its only
other use is inside a ticky macro that compiles away, which is why this has
gone unnoticed. The widening is written out here with TO_W_ instead, as
stg_unpackClosurezh does.

Both defects predate this branch's base commit: they were introduced by
5c2a179efe "Allocate static constructors for bytecode", and are independent of

libraries/ghci/GHCi/InfoTable.hsc
@@ -29,6 +29,9 @@ import GHC.Platform.ArchOS
 -- NOTE: Must return a pointer acceptable for use in the header of a closure.
 -- If tables_next_to_code is enabled, then it must point the 'code' field.
 -- Otherwise, it should point to the start of the StgInfoTable.
+--
+-- The payload is padded to MIN_PAYLOAD_SIZE, see Note [Padding the payload of
+-- an interpreted constructor].
 mkConInfoTable
    :: Bool    -- TABLES_NEXT_TO_CODE
    -> Int     -- ptr words
@@ -46,18 +49,42 @@ mkConInfoTable tables_next_to_code ptr_words nonptr_words tag ptrtag con_desc = 
     then Just <$> mkJumpToAddr entry_addr
     else pure Nothing
   let
+     -- See Note [Padding the payload of an interpreted constructor]
+     nonptr_words'
+       | ptr_words + nonptr_words >= (#const MIN_PAYLOAD_SIZE) = nonptr_words
+       | otherwise = (#const MIN_PAYLOAD_SIZE) - ptr_words
+
      itbl  = StgInfoTable {
                  entry = if tables_next_to_code
                          then Nothing
                          else Just entry_addr,
                  ptrs  = fromIntegral ptr_words,
-                 nptrs = fromIntegral nonptr_words,
+                 nptrs = fromIntegral nonptr_words',
                  tipe  = CONSTR,
                  srtlen = fromIntegral tag,
                  code  = code'
               }
   castFunPtrToPtr <$> newExecConItbl tables_next_to_code itbl con_desc
 
+{-
+Note [Padding the payload of an interpreted constructor]
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Every heap closure must have room for at least MIN_PAYLOAD_SIZE words of
+payload, because the copying collector overwrites an object it has evacuated
+with a forwarding pointer: the info pointer and one word naming the
+destination. A nullary constructor has no fields at all, so its info table
+must claim a payload word that does not correspond to any field.
+
+We therefore pad here, in the one place that builds an info table for an
+interpreted constructor, rather than in the callers. 'mkITbl' in
+GHC.ByteCode.InfoTable pads as well; that is now redundant but harmless, as
+padding an already padded payload is the identity.
+
+Whoever allocates the closure must use the *padded* size from the info table,
+not the constructor's arity: see stg_newConAppObjzh in rts/PrimOps.cmm and
+bci_PACK in rts/Interpreter.c, both of which read the layout back out of the
+info table.
+-}
 
 -- -----------------------------------------------------------------------------
 -- Building machine code fragments for a constructor's entry code

rts/PrimOps.cmm
@@ -2169,9 +2169,21 @@ for:
 // Ptr InfoTable, [Literals] [Ptrs] ==> CONSTR heap closure
 stg_newConAppObjzh ( W_ datacon_info, P_ literals, P_ ptrs , W_ arity )
 {
-    W_ con_obj, bytes;
+    W_ con_obj, bytes, info, itbl_ptrs, itbl_nptrs;
 
-    bytes = SIZEOF_StgHeader + WDS(arity);
+    // Size the closure from the info table rather than from the arity: the
+    // payload of a nullary constructor is padded, so the info table claims a
+    // word that no field corresponds to. Allocating WDS(arity) instead would
+    // be a word short of what the collector copies. bci_PACK sizes the same
+    // way. See Note [Padding the payload of an interpreted constructor] in
+    // GHCi.InfoTable. The fields are filled in from the arrays below, so
+    // 'arity' is now only worth an assertion: it counts the fields, which
+    // must fit in the payload the info table declares.
+    info = %STD_INFO(datacon_info);
+    itbl_ptrs  = TO_W_(%INFO_PTRS(info));
+    itbl_nptrs = TO_W_(%INFO_NPTRS(info));
+    ASSERT(arity <= itbl_ptrs + itbl_nptrs);
+    bytes = SIZEOF_StgHeader + WDS(itbl_ptrs + itbl_nptrs);
 
     ALLOC_PRIM (bytes);
     con_obj = Hp - bytes + WDS(1);